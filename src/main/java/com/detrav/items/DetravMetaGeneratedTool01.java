6616c456d6efb845991e8843257a70046922a2f4
Detrav <witalyezep@gmail.com>
2016-03-19 13:57:06 +0300

    Added some behaviour

--- src/main/java/com/detrav/items/DetravMetaGeneratedTool01.java
+++ src/main/java/com/detrav/items/DetravMetaGeneratedTool01.java
@@ -1,6 +1,7 @@
 package com.detrav.items;
 
 import com.detrav.DetravScannerMod;
+import com.detrav.tools.BehaviourDetravToolProPick;
 import com.detrav.tools.DetravToolProPick;
 import gregtech.api.enums.TC_Aspects;
 import gregtech.api.items.GT_MetaGenerated_Tool;
@@ -16,7 +17,7 @@ public class DetravMetaGeneratedTool01 extends GT_MetaGenerated_Tool {
         INSTANCE = this;
         addTool(0, "ProPick", "", new DetravToolProPick(), new Object[]{new TC_Aspects.TC_AspectStack(TC_Aspects.INSTRUMENTUM, 2L), new TC_Aspects.TC_AspectStack(TC_Aspects.METALLUM, 4L)});
         setCreativeTab(DetravScannerMod.TAB_DETRAV);
-        //addItemBehavior(0,)
+        addItemBehavior(0,new BehaviourDetravToolProPick());
     }
 
 
--- src/main/java/com/detrav/tools/BehaviourDetravToolProPick.java
+++ src/main/java/com/detrav/tools/BehaviourDetravToolProPick.java
@@ -1,7 +1,24 @@
 package com.detrav.tools;
 
+import gregtech.api.items.GT_MetaBase_Item;
+import gregtech.common.items.behaviors.Behaviour_None;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.world.World;
+
 /**
  * Created by wital_000 on 19.03.2016.
  */
-public class BehaviourDetravToolProPick {
+public class BehaviourDetravToolProPick extends Behaviour_None {
+
+    public boolean onItemUse(GT_MetaBase_Item aItem, ItemStack aStack, EntityPlayer aPlayer, World aWorld, int aX, int aY, int aZ, int aSide, float hitX, float hitY, float hitZ) {
+
+        if (aWorld.isRemote)
+        {
+            return true;
+        }
+        aPlayer.addChatMessage(new ChatComponentText("Tested"));
+        return false;
+    }
 }
