2cde8af6ad4c66fe106b69fa257592631af05b5c
Draknyte1 <Draknyte1@hotmail.com>
2016-07-05 15:19:57 +1000

    - Removed the Electric Blast Furnace clone I used for testing.

--- src/Java/miscutil/core/handler/registration/gregtech/GregtechSteamCondenser.java
+++ src/Java/miscutil/core/handler/registration/gregtech/GregtechSteamCondenser.java
@@ -3,7 +3,6 @@ package miscutil.core.handler.registration.gregtech;
 import miscutil.core.util.Utils;
 import miscutil.core.xmod.gregtech.api.enums.GregtechItemList;
 import miscutil.core.xmod.gregtech.api.metatileentity.implementations.GregtechMetaCondensor;
-import miscutil.core.xmod.gregtech.common.tileentities.machines.multi.GregtechMetaTileEntityElectricBlastFurnace;
 import miscutil.core.xmod.gregtech.common.tileentities.machines.multi.GregtechMetaTileEntityIronBlastFurnace;
 
 public class GregtechSteamCondenser
@@ -28,7 +27,7 @@ public class GregtechSteamCondenser
 
 
 		GregtechItemList.Machine_Iron_BlastFurnace.set(new GregtechMetaTileEntityIronBlastFurnace(768, "ironmachine.blastfurnace", "Iron Plated Blast Furnace").getStackForm(1L));
-		GregtechItemList.Machine_Electric_BlastFurnace.set(new GregtechMetaTileEntityElectricBlastFurnace(796, "electric.blastfurnace", "Electric Blast Furnace").getStackForm(1L));
+		//GregtechItemList.Machine_Electric_BlastFurnace.set(new GregtechMetaTileEntityElectricBlastFurnace(796, "electric.blastfurnace", "Electric Blast Furnace").getStackForm(1L));
 		
 		//ItemUtils.recipeBuilder(slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, resultItem);
 		
