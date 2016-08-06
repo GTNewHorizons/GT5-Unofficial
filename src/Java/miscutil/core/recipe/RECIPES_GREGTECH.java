7d131651abf413de2dc457ed6225cbbcebb947a7
Draknyte1 <Draknyte1@hotmail.com>
2016-08-06 12:16:47 +1000

    % Tweaker run-time and eu cost for helium production recipes.

--- src/Java/miscutil/core/recipe/RECIPES_GREGTECH.java
+++ src/Java/miscutil/core/recipe/RECIPES_GREGTECH.java
@@ -117,7 +117,7 @@ public class RECIPES_GREGTECH {
 	private static void distilleryRecipes(){
 		Utils.LOG_INFO("Registering Distillery/Distillation Tower Recipes.");
 		GT_Values.RA.addDistilleryRecipe(ItemList.Circuit_Integrated.getWithDamage(0L, 4L, new Object[0]), FluidUtils.getFluidStack("air", 1000), FluidUtils.getFluidStack("helium", 1), 400, 30, false);
-		GT_Values.RA.addDistillationTowerRecipe(FluidUtils.getFluidStack("air", 20000), FluidUtils.getFluidStackArray("helium", 25), null, 160, 60);
+		GT_Values.RA.addDistillationTowerRecipe(FluidUtils.getFluidStack("air", 20000), FluidUtils.getFluidStackArray("helium", 25), null, 200, 60);
 	}
 
 	private static void addFuels(){
