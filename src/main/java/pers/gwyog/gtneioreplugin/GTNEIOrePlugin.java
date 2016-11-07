4d9e74936b20d24020b0fdebed723caad9cbf9b4
GWYOG <jjn1jjn2@163.com>
2016-11-07 23:06:46 +0800

    Update mod version to 1.0.2

--- src/main/java/pers/gwyog/gtneioreplugin/GTNEIOrePlugin.java
+++ src/main/java/pers/gwyog/gtneioreplugin/GTNEIOrePlugin.java
@@ -17,7 +17,7 @@ import pers.gwyog.gtneioreplugin.util.GTSmallOreHelper;
 public class GTNEIOrePlugin {
     public static final String MODID = "gtneioreplugin";
     public static final String NAME = "GT NEI Ore Plugin";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
     
     @Mod.Instance(MODID)
     public static GTNEIOrePlugin instance;
