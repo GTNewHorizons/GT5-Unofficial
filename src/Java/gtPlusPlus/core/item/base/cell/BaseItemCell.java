0d013ecbe292c75ea37f0c43cbd3853b10a4b55c
Draknyte1 <Draknyte1@hotmail.com>
2017-01-02 20:12:46 +1000

    $ Fixed Cell items having invalid recolouring. Now only the fluid inside the cell is recoloured, not the entire thing.

--- src/Java/gtPlusPlus/core/item/base/cell/BaseItemCell.java
+++ src/Java/gtPlusPlus/core/item/base/cell/BaseItemCell.java
@@ -3,7 +3,9 @@ package gtPlusPlus.core.item.base.cell;
 import gtPlusPlus.core.item.base.BaseItemComponent;
 import gtPlusPlus.core.lib.CORE;
 import gtPlusPlus.core.material.Material;
+import gtPlusPlus.core.util.Utils;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -13,7 +15,7 @@ public class BaseItemCell extends BaseItemComponent{
 	private IIcon base;
 	private IIcon overlay;
 	ComponentTypes Cell = ComponentTypes.CELL;
-	
+
 	public BaseItemCell(Material material) {
 		super(material, BaseItemComponent.ComponentTypes.CELL);		
 	}
@@ -29,7 +31,15 @@ public class BaseItemCell extends BaseItemComponent{
 		this.base = i.registerIcon(CORE.MODID + ":" + "item"+Cell.getComponent());	
 		this.overlay = i.registerIcon(CORE.MODID + ":" + "item"+Cell.getComponent()+"_Overlay");
 	}
-	
+
+	@Override
+	public int getColorFromItemStack(ItemStack stack, int renderPass) {
+		if (renderPass == 0){
+			return Utils.rgbtoHexValue(230, 230, 230);			
+		}		
+		return componentColour;
+	}
+
 
 	@Override
 	public IIcon getIconFromDamageForRenderPass(int damage, int pass) {
