b810e6391e2ab13909b84ae42a822d48b2acf47f
Draknyte1 <Draknyte1@hotmail.com>
2017-04-30 12:15:36 +1000

    ^ Version bumped to 1.5.0-Beta > First 1.50 release, long overdue.

--- src/Java/gtPlusPlus/core/lib/CORE.java
+++ src/Java/gtPlusPlus/core/lib/CORE.java
@@ -29,7 +29,7 @@ public class CORE {
 
 	public static final String name = "GT++";
 	public static final String MODID = "miscutils";
-	public static final String VERSION = "1.4.9.86-Beta";
+	public static final String VERSION = "1.5.0-Beta";
 	public static final String MASTER_VERSION = NetworkUtils.getContentFromURL("https://raw.githubusercontent.com/draknyte1/GTplusplus/master/Recommended.txt").toLowerCase();
 	public static final String USER_COUNTRY = GeoUtils.determineUsersCountry();
 	public static boolean isModUpToDate = Utils.isModUpToDate();
