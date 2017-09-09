39d0855113cb088777f7a7b0f736dffae61936c4
Alkalus <draknyte1@hotmail.com>
2017-09-09 18:45:53 +1000

    + Added ability for High-Amp Transformers to swap between 2A:8A and 4A:16A modes.

--- src/Java/gtPlusPlus/xmod/gregtech/api/metatileentity/implementations/base/GregtechMetaTransformerHiAmp.java
+++ src/Java/gtPlusPlus/xmod/gregtech/api/metatileentity/implementations/base/GregtechMetaTransformerHiAmp.java
@@ -6,9 +6,16 @@ import gregtech.api.interfaces.ITexture;
 import gregtech.api.interfaces.metatileentity.IMetaTileEntity;
 import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
 import gtPlusPlus.core.lib.CORE;
+import gtPlusPlus.core.util.Utils;
+import gtPlusPlus.core.util.player.PlayerUtils;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
 import gregtech.api.metatileentity.implementations.GT_MetaTileEntity_Transformer;
 
 public class GregtechMetaTransformerHiAmp extends GT_MetaTileEntity_Transformer {
+	
+	private boolean mHalfMode = false;
+	
 	public GregtechMetaTransformerHiAmp(int aID, String aName, String aNameRegional, int aTier, String aDescription) {
 		super(aID, aName, aNameRegional, aTier, aDescription);
 	}
@@ -19,16 +26,22 @@ public class GregtechMetaTransformerHiAmp extends GT_MetaTileEntity_Transformer 
 
 	@Override
 	public long maxEUStore() {
-		return ((512L + gregtech.api.enums.GT_Values.V[(this.mTier + 1)] * 2L)*4);
+		return ((512L + gregtech.api.enums.GT_Values.V[(this.mTier + 1)] * 2L)*8);
 	}
 
 	@Override
 	public long maxAmperesOut() {
+		if (this.mHalfMode){
+			return ((getBaseMetaTileEntity().isAllowedToWork())	? 8L : 2L);			
+		}
 		return ((getBaseMetaTileEntity().isAllowedToWork())	? 16L : 4L);
 	}
 
 	@Override
 	public long maxAmperesIn() {
+		if (this.mHalfMode){
+			return ((getBaseMetaTileEntity().isAllowedToWork())	? 2L : 8L);			
+		}
 		return ((getBaseMetaTileEntity().isAllowedToWork()) ? 4L : 16L);
 	}
 	@Override
@@ -59,5 +72,29 @@ public class GregtechMetaTransformerHiAmp extends GT_MetaTileEntity_Transformer 
 	@Override
 	public String[] getDescription() {
 		return new String[] {this.mDescription, "Accepts 4A and outputs 16A", CORE.GT_Tooltip};
+	}
+
+	@Override
+	public void saveNBTData(NBTTagCompound aNBT) {
+		aNBT.setBoolean("mHalfMode", this.mHalfMode);
+		super.saveNBTData(aNBT);
+	}
+
+	@Override
+	public void loadNBTData(NBTTagCompound aNBT) {
+		this.mHalfMode = aNBT.getBoolean("mHalfMode");
+		super.loadNBTData(aNBT);
+	}
+
+	@Override
+	public void onScrewdriverRightClick(byte aSide, EntityPlayer aPlayer, float aX, float aY, float aZ) {
+		this.mHalfMode = Utils.invertBoolean(mHalfMode);
+		if (this.mHalfMode){
+			PlayerUtils.messagePlayer(aPlayer, "Transformer is now running at 2A:8A in/out Ratio.");
+		}
+		else {
+
+			PlayerUtils.messagePlayer(aPlayer, "Transformer is now running at 4A:16A in/out Ratio.");
+		}
 	}
 }
