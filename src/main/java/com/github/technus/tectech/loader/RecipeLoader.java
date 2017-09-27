cd4dcc6df78aff3c145f166a43115cc72111ee0b
Technus <daniel112092@gmail.com>
2017-09-27 18:58:34 +0200

    upate gt++ and core add some recipes

--- src/main/java/com/github/technus/tectech/loader/RecipeLoader.java
+++ src/main/java/com/github/technus/tectech/loader/RecipeLoader.java
@@ -2,7 +2,17 @@ package com.github.technus.tectech.loader;
 
 import com.github.technus.tectech.compatibility.dreamcraft.DreamCraftRecipeLoader;
 import com.github.technus.tectech.elementalMatter.definitions.complex.dAtomDefinition;
+import com.github.technus.tectech.thing.block.QuantumGlassBlock;
+import com.github.technus.tectech.thing.casing.GT_Block_HintTT;
+import com.github.technus.tectech.thing.casing.TT_Container_Casings;
 import cpw.mods.fml.common.Loader;
+import gregtech.api.enums.Materials;
+import gregtech.api.enums.OrePrefixes;
+import gregtech.api.util.GT_OreDictUnificator;
+import gregtech.api.util.GT_Utility;
+import net.minecraft.item.ItemStack;
+
+import static gregtech.api.enums.GT_Values.RA;
 
 /**
  * Created by danie_000 on 16.11.2016.
@@ -15,7 +25,12 @@ public class RecipeLoader implements Runnable {
         // Recipes init - common goes here rest goes into methods below
         // ===================================================================================================
 
-
+        for(int i=0;i<=15;i++)
+            RA.addAssemblerRecipe(new ItemStack[]{
+                    GT_Utility.getIntegratedCircuit(i),
+                    GT_OreDictUnificator.get(OrePrefixes.dust,Materials.Cobalt,1)},
+                    Materials.Aluminium.getMolten(864),
+                    new ItemStack(TT_Container_Casings.sHintCasingsTT, 1,i),32,120);
 
         if (Loader.isModLoaded("dreamcraft")) new DreamCraftRecipeLoader().run();//TODO init recipes for GTNH version
         else new BloodyRecipeLoader().run();//TODO init recipes for NON-GTNH version
