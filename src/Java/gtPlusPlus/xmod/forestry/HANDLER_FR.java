39adfbdb35ca9c040a990b3ef3ad4744f984c36b
Alkalus <draknyte1@hotmail.com>
2017-11-05 21:31:39 +1000

    $ Should fix #147. Forestry Crash if not loaded.

--- src/Java/gtPlusPlus/xmod/forestry/HANDLER_FR.java
+++ src/Java/gtPlusPlus/xmod/forestry/HANDLER_FR.java
@@ -5,6 +5,7 @@ import forestry.core.blocks.BlockRegistry;
 import forestry.core.proxy.Proxies;
 import gtPlusPlus.core.lib.CORE;
 import gtPlusPlus.core.lib.LoadedMods;
+import gtPlusPlus.core.util.reflect.ReflectionUtils;
 import gtPlusPlus.xmod.forestry.bees.items.FR_ItemRegistry;
 import gtPlusPlus.xmod.forestry.bees.recipe.FR_Gregtech_Recipes;
 import net.minecraft.block.Block;
@@ -16,18 +17,13 @@ public class HANDLER_FR extends BlockRegistry {
 
 	public static void preInit(){
 		if (LoadedMods.Forestry){
-			FR_ItemRegistry.Register();
-			if (CORE.configSwitches.enableCustomAlvearyBlocks){
-				//AlvearyHandler.run();
-			}
+			FR_ItemRegistry.Register();			
 		}
 	}
 
 	public static void Init(){
 		if (LoadedMods.Forestry){
-			//apiculture = new BlockDenseBeeHouse();
-			//apiculture.setBlockName("gtpp." + "beehouse");
-			//GameRegistry.registerBlock(apiculture, ItemBlockForestry.class, StringUtil.cleanBlockName(apiculture));
+		
 		}
 	}
 
@@ -46,7 +42,13 @@ public class HANDLER_FR extends BlockRegistry {
 
 	@Optional.Method(modid = "Forestry")
 	private static void createBlockBreakParticles_INTERNAL(final World world, final int x, final int y, final int z, final Block block){
-		Proxies.common.addBlockDestroyEffects(world, x, y, z, block, 0);
+		if (LoadedMods.Forestry){
+			forestry.core.proxy.ProxyCommon mCommonProxy = ReflectionUtils.getField("forestry.core.proxy.ProxyCommon", "common");
+			if (mCommonProxy != null){
+				mCommonProxy.addBlockDestroyEffects(world, x, y, z, block, 0);
+			}
+		}
+		
 	}
 
 
