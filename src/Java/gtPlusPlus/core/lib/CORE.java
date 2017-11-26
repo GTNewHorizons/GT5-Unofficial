0d18c5ebd32acf0fc58643d40229947eee547e49
Alkalus <draknyte1@hotmail.com>
2017-11-27 00:59:09 +1000

    ^ Version Bump.

--- src/Java/gtPlusPlus/core/lib/CORE.java
+++ src/Java/gtPlusPlus/core/lib/CORE.java
@@ -1,6 +1,11 @@
 package gtPlusPlus.core.lib;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 import com.mojang.authlib.GameProfile;
@@ -35,7 +40,7 @@ public class CORE {
 
 	public static final String name = "GT++";
 	public static final String MODID = "miscutils";
-	public static final String VERSION = "1.6.94-release";
+	public static final String VERSION = "1.6.97-release";
 	public static String MASTER_VERSION = NetworkUtils.getContentFromURL("https://raw.githubusercontent.com/draknyte1/GTplusplus/master/Recommended.txt").toLowerCase();
 	public static String USER_COUNTRY = GeoUtils.determineUsersCountry();
 	public static boolean isModUpToDate = Utils.isModUpToDate();
@@ -77,7 +82,7 @@ public class CORE {
 
 	//Book List
 	public static final Map<String, ItemStack> sBookList = new ConcurrentHashMap<String, ItemStack>();
-	
+
 	/**
 	 * File Paths and Resource Paths
 	 */
