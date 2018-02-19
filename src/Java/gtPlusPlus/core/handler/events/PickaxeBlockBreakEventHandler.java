941add7ddac9c6db5dce5d05507b5ed69893f9fc
Jordan Byrne <draknyte1@hotmail.com>
2018-02-19 12:01:53 +1000

    $ Improved the shitty handler for Safes.

--- src/Java/gtPlusPlus/core/handler/events/PickaxeBlockBreakEventHandler.java
+++ src/Java/gtPlusPlus/core/handler/events/PickaxeBlockBreakEventHandler.java
@@ -5,10 +5,8 @@ import java.util.UUID;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import gregtech.api.interfaces.metatileentity.IMetaTileEntity;
 import gregtech.api.metatileentity.*;
-import gtPlusPlus.api.objects.Logger;
 import gtPlusPlus.core.util.player.PlayerUtils;
 import gtPlusPlus.xmod.gregtech.api.metatileentity.implementations.base.machines.GregtechMetaSafeBlockBase;
-import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
@@ -20,25 +18,14 @@ public class PickaxeBlockBreakEventHandler {
 		try{
 			final TileEntity entity = event.world.getTileEntity(event.x, event.y, event.z);
 			if (entity != null){
-				final EntityPlayer playerInternal = event.getPlayer();
-				Logger.WARNING(entity.getClass().getSimpleName());
-				if (entity.getClass().getSimpleName().equals("")){
-
-				}
-				if ((entity instanceof BaseMetaTileEntity) && !(entity instanceof BaseMetaPipeEntity)){
+				final EntityPlayer playerInternal = event.getPlayer();				
+				if ((entity instanceof BaseMetaTileEntity) && !(BaseMetaPipeEntity.class.isInstance(entity))){
 					final IMetaTileEntity X = ((BaseMetaTileEntity)entity).getMetaTileEntity();
-					final Block ThisBlock = X.getBaseMetaTileEntity().getBlock(event.x, event.y, event.z);
+					//final Block ThisBlock = X.getBaseMetaTileEntity().getBlock(event.x, event.y, event.z);
 					if (X instanceof GregtechMetaSafeBlockBase){
-
 						final UUID ownerUUID = ((GregtechMetaSafeBlockBase)X).ownerUUID;
 						final UUID accessorUUID = playerInternal.getUniqueID();
-						Logger.WARNING("Owner UUID: "+ownerUUID);
-						Logger.WARNING("Accessor UUID: "+accessorUUID);
-
 						if (((GregtechMetaSafeBlockBase)X).bUnbreakable){
-
-							Logger.INFO("UUID info. Accessor: "+accessorUUID + " | Owner: "+ownerUUID);
-
 							if (accessorUUID == ownerUUID){
 								PlayerUtils.messagePlayer(playerInternal, "Since you own this block, it has been destroyed.");
 								event.setCanceled(false);
@@ -47,15 +34,13 @@ public class PickaxeBlockBreakEventHandler {
 								event.setCanceled(true);
 								PlayerUtils.messagePlayer(playerInternal, "Since you do not own this block, it has not been destroyed.");
 							}
-							//
 						}
 					}
 				}
 			}
-
 		}
 		catch (final NullPointerException e) {
-			System.out.print("Caught a NullPointerException involving Safe Blocks. Cause: "+e.getCause());
+			//System.out.print("Caught a NullPointerException involving Safe Blocks. Cause: "+e.getCause());
 		}
 	}
 
