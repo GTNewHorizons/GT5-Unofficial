f62cc3c4487beae950e21b939a48dc7fc782923d
Jordan Byrne <draknyte1@hotmail.com>
2018-02-24 00:55:52 +1000

    % Improved LFTR tooltip.

--- src/Java/gtPlusPlus/xmod/gregtech/common/tileentities/machines/multi/GregtechMTE_NuclearReactor.java
+++ src/Java/gtPlusPlus/xmod/gregtech/common/tileentities/machines/multi/GregtechMTE_NuclearReactor.java
@@ -69,6 +69,7 @@ public class GregtechMTE_NuclearReactor extends GT_MetaTileEntity_MultiBlockBase
 				"Outputs U233 every 10 seconds, on average",
 				"Input Fluorine and Helium for bonus byproducts",
 				"Input Li2BeF4 and a molten salt as fuel.",
+				"LiFBeF2ThF4UF4, LiFBeF2ZrF4UF4 or﻿ LiFBeF2ZrF4U235"
 				CORE.GT_Tooltip};
 	}
 
