805bb5c06ebb22361f56d3d769945d7062bc34cc
David Vierra <codewarrior@hawaii.rr.com>
2018-03-31 16:27:45 -1000

    Rewrite Exotic Seed Farmer as Exotic Seed Module

--- src/Java/gtPlusPlus/xmod/sc2/HANDLER_SC2.java
+++ src/Java/gtPlusPlus/xmod/sc2/HANDLER_SC2.java
@@ -1,103 +1,20 @@
 package gtPlusPlus.xmod.sc2;
 
-import java.lang.reflect.Field;
-import java.util.HashMap;
-
-import cpw.mods.fml.common.registry.GameRegistry;
-
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-
-import gtPlusPlus.api.objects.Logger;
 import gtPlusPlus.core.lib.LoadedMods;
-import gtPlusPlus.core.util.reflect.ReflectionUtils;
-import vswe.stevescarts.Items.ItemCartModule;
-import vswe.stevescarts.ModuleData.ModuleData;
-import vswe.stevescarts.ModuleData.ModuleDataLoader;
+import vswe.stevescarts.ModuleData.GppModuleData;
 
 public class HANDLER_SC2 {
 
-	private static final String MODITEMS = "vswe.stevescarts.Items.ModItems";
-	private static Class<?> MODITEMSCLASS;
-	public static ItemCartModule modules;
-
-	@SuppressWarnings("unchecked")
-	public synchronized static void preInit(){
-		if (LoadedMods.StevesCarts){	
-
-			HashMap<Byte, Boolean> validModulesOld = null;	
-			HashMap<Byte, Boolean> validModulesNew = new HashMap<Byte, Boolean>();			
-			try {
-				MODITEMSCLASS = Class.forName(MODITEMS);
-				if (MODITEMSCLASS == null) {
-					Logger.REFLECTION("Failed Registering Custom SC2 Modules. [1]");
-					return;
-				}				
-				Field validModulesField = ReflectionUtils.getField(MODITEMSCLASS, "validModules");
-				if (validModulesField != null) {
-					validModulesOld = (HashMap<Byte, Boolean>) validModulesField.get(null);
-				}
-			}
-			catch (NoSuchFieldException | IllegalArgumentException | IllegalAccessException | ClassNotFoundException e) {
-				e.printStackTrace();
-				Logger.REFLECTION("Failed Registering Custom SC2 Modules. [2]");
-				return;
-			}
-
-			if (validModulesOld == null) {
-				Logger.REFLECTION("Failed Registering Custom SC2 Modules. [3]");
-				return;
-			}
-			else {
-				validModulesNew.putAll(validModulesOld);
-			}
-
-			if (validModulesNew == null || validModulesNew.isEmpty()) {
-				Logger.REFLECTION("Failed Registering Custom SC2 Modules. [4]");
-				return;
-			}
-
-			modules = vswe.stevescarts.Items.ModItems.modules;
-			//GameRegistry.registerItem((Item) modules, "CartModule++");
-			ModuleDataLoader.load();
-			for (final ModuleData module : ModuleDataLoader.moduleListCustom.values()) {
-				if (!module.getIsLocked()) {
-					Logger.REFLECTION("Mapping Custom SC2 Module. Using ID: "+module.getID());
-					validModulesNew.put(module.getID(), true);
-				}
-			}
-			for (final ModuleData module : ModuleDataLoader.moduleListCustom.values()) {
-				final ItemStack submodule = new ItemStack((Item) modules, 1, (int) module.getID());
-				if (submodule != null) {
-					Logger.REFLECTION("Registering Custom SC2 Module. Using ID: "+module.getID());
-					GameRegistry.registerCustomItemStack(submodule.getUnlocalizedName(), submodule);					
-				}
-			}
-
-			try {
-				Logger.REFLECTION("Setting 'validModules' field in "+MODITEMS+". Old Map was "+validModulesOld.size()+" objects, New map is "+validModulesNew.size()+" objects.");
-				ReflectionUtils.setFieldValue(MODITEMSCLASS, "validModules", validModulesNew);
-			}
-			catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		else {
-			Logger.REFLECTION("Failed Registering Custom SC2 Modules. [0]");
-			return;
+	public synchronized static void preInit() {
+		if (LoadedMods.StevesCarts){
+			GppModuleData.loadGpp();
 		}
 	}
 
 	public static void init(){
-		if (LoadedMods.StevesCarts){
-			ModuleDataLoader.load2();
-		}
 	}
 
 	public static void postInit(){
-		if (LoadedMods.StevesCarts){
-
-		}
 	}
 
 
---
+++ src/Java/vswe/stevescarts/ModuleData/GppModuleData.java
@@ -0,0 +1,48 @@
+package vswe.stevescarts.ModuleData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import gtPlusPlus.core.util.reflect.ReflectionUtils;
+import gtPlusPlus.xmod.sc2.modules.ModuleExoticSeeds;
+import net.minecraft.init.Items;
+import vswe.stevescarts.Helpers.ComponentTypes;
+import vswe.stevescarts.Items.ModItems;
+import vswe.stevescarts.Modules.ModuleBase;
+
+public class GppModuleData extends ModuleData {
+
+	static byte ID_ExoticSeedAddon = 105;
+	public static ArrayList<ModuleData> moduleListCustom = new ArrayList<>();
+
+	public GppModuleData(int id, String name, Class<? extends ModuleBase> moduleClass, int modularCost) {
+		super(id, name, moduleClass, modularCost);
+	}
+
+	public static void loadGpp() {
+		try {
+			HashMap<Byte, ModuleData> moduleList =
+					(HashMap<Byte, ModuleData>) (ReflectionUtils.getField(ModuleData.class, "moduleList").get(ModuleData.class));
+			ModuleData netherWartModule = moduleList.get((byte)58);
+
+			ModuleDataGroup farmerGroup = netherWartModule.getRequirement().get(0);
+
+			ModuleData exoticSeedModule =
+					new ModuleData(ID_ExoticSeedAddon, "Crop: Exotic Seeds", ModuleExoticSeeds.class, 20).addRequirement(farmerGroup)
+					.addRecipe(new Object[][]{{Items.wheat_seeds},
+							{ComponentTypes.EMPTY_DISK.getItemStack()}
+					});
+
+			moduleListCustom.add(exoticSeedModule);
+
+			HashMap<Byte, Boolean> validModules =
+					(HashMap<Byte, Boolean>) (ReflectionUtils.getField(ModItems.class, "validModules").get(ModItems.class));
+
+			validModules.put(ID_ExoticSeedAddon, true);
+
+		} catch (NoSuchFieldException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
