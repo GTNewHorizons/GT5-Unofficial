07057093486610bd952a61862c0ea098af853f2a
Alkalus <3060479+draknyte1@users.noreply.github.com>
2018-05-26 13:15:00 +1000

    + Added more names to en_US.lang. % Fixed a fluid naming issue.

--- src/Java/gtPlusPlus/core/item/chemistry/NuclearChem.java
+++ src/Java/gtPlusPlus/core/item/chemistry/NuclearChem.java
@@ -13,8 +13,8 @@ public class NuclearChem {
 
 		//Create Coal Gas
 		Burnt_LiFBeF2ThF4UF4 = FluidUtils.generateFluidNonMolten("BurntLiFBeF2ThF4UF4", "Burnt LiFBeF2ThF4UF4 Salt", 545, new short[]{48, 48, 175, 100}, null, null);
-		Burnt_LiFBeF2ZrF4UF4 = FluidUtils.generateFluidNonMolten("Burnt_LiFBeF2ZrF4UF4", "Burnt LiFBeF2ZrF4UF4 Salt", 520, new short[]{48, 68, 165, 100}, null, null);
-		Burnt_LiFBeF2ZrF4U235 = FluidUtils.generateFluidNonMolten("Burnt_LiFBeF2ZrF4U235", "Burnt LiFBeF2ZrF4U235 Salt", 533, new short[]{68, 48, 185, 100}, null, null);
+		Burnt_LiFBeF2ZrF4UF4 = FluidUtils.generateFluidNonMolten("BurntLiFBeF2ZrF4UF4", "Burnt LiFBeF2ZrF4UF4 Salt", 520, new short[]{48, 68, 165, 100}, null, null);
+		Burnt_LiFBeF2ZrF4U235 = FluidUtils.generateFluidNonMolten("BurntLiFBeF2ZrF4U235", "Burnt LiFBeF2ZrF4U235 Salt", 533, new short[]{68, 48, 185, 100}, null, null);
 		
 		createRecipes();
 
