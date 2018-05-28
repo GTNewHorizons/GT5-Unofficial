524a1242befd9b1ddb3dc591ac7a16148b272ea8
Alkalus <3060479+draknyte1@users.noreply.github.com>
2018-05-28 21:35:54 +1000

    $ Made the Keyboard checks safer. Should prevent console spam/logging.

--- src/Java/gtPlusPlus/core/util/sys/KeyboardUtils.java
+++ src/Java/gtPlusPlus/core/util/sys/KeyboardUtils.java
@@ -7,17 +7,33 @@ import net.minecraft.client.Minecraft;
 public class KeyboardUtils {
 
 	public static boolean isCtrlKeyDown(){
-		// prioritize CONTROL, but allow OPTION as well on Mac (note: GuiScreen's isCtrlKeyDown only checks for the OPTION key on Mac)
-		boolean isCtrlKeyDown = Keyboard.isKeyDown(Keyboard.KEY_LCONTROL) || Keyboard.isKeyDown(Keyboard.KEY_RCONTROL);
-		if (!isCtrlKeyDown && Minecraft.isRunningOnMac)
-			isCtrlKeyDown = Keyboard.isKeyDown(Keyboard.KEY_LMETA) || Keyboard.isKeyDown(Keyboard.KEY_RMETA);
-	
-		return isCtrlKeyDown;
+		try {
+			if (!Keyboard.isCreated()) {
+				return false;
+			}	
+			// prioritize CONTROL, but allow OPTION as well on Mac (note: GuiScreen's isCtrlKeyDown only checks for the OPTION key on Mac)
+			boolean isCtrlKeyDown = Keyboard.isKeyDown(Keyboard.KEY_LCONTROL) || Keyboard.isKeyDown(Keyboard.KEY_RCONTROL);
+			if (!isCtrlKeyDown && Minecraft.isRunningOnMac)
+				isCtrlKeyDown = Keyboard.isKeyDown(Keyboard.KEY_LMETA) || Keyboard.isKeyDown(Keyboard.KEY_RMETA);
+
+			return isCtrlKeyDown;
+		}
+		catch (Throwable t) {
+			return false;
+		}
 	}
 
 	public static boolean isShiftKeyDown(){
-		return Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT);
-		
+		try {
+			if (!Keyboard.isCreated()) {
+				return false;
+			}		
+			return Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT);
+		}
+		catch (Throwable t) {
+			return false;
+		}
+
 	}
-	
+
 }
