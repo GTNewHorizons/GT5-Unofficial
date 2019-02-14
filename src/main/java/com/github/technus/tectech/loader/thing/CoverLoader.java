efe018ba6922e9dbaa2cfdbb3ff7e229de9aec50
Bass <tudurap.com@gmail.com>
2019-02-14 18:09:49 +0000

    Cover Loader Fix
    TecTech Cover Loader now actually working :D

--- src/main/java/com/github/technus/tectech/loader/thing/CoverLoader.java
+++ src/main/java/com/github/technus/tectech/loader/thing/CoverLoader.java
@@ -3,17 +3,15 @@ package com.github.technus.tectech.loader.thing;
 import com.github.technus.tectech.TecTech;
 
 import com.github.technus.tectech.thing.cover.GT_Cover_TM_TeslaCoil;
+import com.github.technus.tectech.thing.item.TeslaCoilCover;
 import gregtech.api.GregTech_API;
 import gregtech.api.enums.Textures;
 import gregtech.api.objects.GT_RenderedTexture;
-import gregtech.api.util.GT_ModHandler;
+import net.minecraft.item.ItemStack;
 
 public class CoverLoader implements Runnable {
-    @Override
-    public void run() {
-        //TODO Create a techTechModItem method, but for now I need it to actually USE the tesla cover item
-        //GregTech_API.registerCover(GT_ModHandler.getModItem("tectech", "tm.teslaCoilCover", 1L), new GT_RenderedTexture(Textures.BlockIcons.VENT_ADVANCED), new GT_Cover_TM_TeslaCoil());
-        GregTech_API.registerCover(GT_ModHandler.getIC2Item("reactorReflector", 1L, 1), new GT_RenderedTexture(Textures.BlockIcons.VENT_ADVANCED), new GT_Cover_TM_TeslaCoil());
+    public void run(){
+        GregTech_API.registerCover(new ItemStack(TeslaCoilCover.INSTANCE, 1), new GT_RenderedTexture(Textures.BlockIcons.VENT_ADVANCED), new GT_Cover_TM_TeslaCoil());
         TecTech.LOGGER.info("Cover functionality registered");
     }
 }
