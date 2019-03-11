928159a9e889d502eecb30d8882ff78f3aa7cc59
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2019-03-11 02:17:58 +0100

    small fixes -oredicted some recipes -fixed vacuum freezer voiding items -fixed non localised gui -version increase
    Signed-off-by: bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
    
    Former-commit-id: b2353ef18f835520ae3fa40bbb119093ba01c720

--- src/main/java/com/github/bartimaeusnek/bartworks/common/tileentities/multis/mega/GT_TileEntity_MegaVacuumFreezer.java
+++ src/main/java/com/github/bartimaeusnek/bartworks/common/tileentities/multis/mega/GT_TileEntity_MegaVacuumFreezer.java
@@ -78,7 +78,7 @@ public class GT_TileEntity_MegaVacuumFreezer extends GT_MetaTileEntity_VacuumFre
         int processed = 0;
         long nominalV = BW_Util.getnominalVoltage(this);
         while (this.getStoredInputs().size() > 0 && processed < ConfigHandler.megaMachinesMax) {
-            if (tRecipe != null && tRecipe.isRecipeInputEqual(true, null, tInputs) && (tRecipe.mEUt*processed) < nominalV ) {
+            if (tRecipe != null && (tRecipe.mEUt*(processed+1)) < nominalV  && tRecipe.isRecipeInputEqual(true, null, tInputs)) {
                 found_Recipe = true;
                 for (int i = 0; i < tRecipe.mOutputs.length; i++) {
                     outputItems.add(tRecipe.getOutput(i));
