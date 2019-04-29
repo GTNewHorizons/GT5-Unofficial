4109c9575dd6d8a89f03e1242493dca228255570
Alkalus <3060479+draknyte1@users.noreply.github.com>
2019-04-29 12:55:19 +1000

    + Added Framework for Thermal power based upon the code used for RF. > This is more effective than trying to use the GT EU code for the exact same thing.

---
+++ src/Java/gtPlusPlus/api/thermal/energy/IThermalConnection.java
@@ -0,0 +1,7 @@
+package gtPlusPlus.api.thermal.energy;
+
+import net.minecraftforge.common.util.ForgeDirection;
+
+public interface IThermalConnection {
+	boolean canConnectThermalEnergy(ForgeDirection arg0);
+}
---
+++ src/Java/gtPlusPlus/api/thermal/energy/ThermalStorage.java
@@ -0,0 +1,116 @@
+package gtPlusPlus.api.thermal.energy;
+
+import net.minecraft.nbt.NBTTagCompound;
+
+public class ThermalStorage implements IThermalStorage {
+	
+	protected int thermal_energy;
+	protected int capacity;
+	protected int maxReceive;
+	protected int maxExtract;
+
+	public ThermalStorage(int arg0) {
+		this(arg0, arg0, arg0);
+	}
+
+	public ThermalStorage(int arg0, int arg1) {
+		this(arg0, arg1, arg1);
+	}
+
+	public ThermalStorage(int arg0, int arg1, int arg2) {
+		this.capacity = arg0;
+		this.maxReceive = arg1;
+		this.maxExtract = arg2;
+	}
+
+	public ThermalStorage readFromNBT(NBTTagCompound arg0) {
+		this.thermal_energy = arg0.getInteger("Energy");
+		if (this.thermal_energy > this.capacity) {
+			this.thermal_energy = this.capacity;
+		}
+		return this;
+	}
+
+	public NBTTagCompound writeToNBT(NBTTagCompound arg0) {
+		if (this.thermal_energy < 0) {
+			this.thermal_energy = 0;
+		}
+		arg0.setInteger("Energy", this.thermal_energy);
+		return arg0;
+	}
+
+	public void setCapacity(int arg0) {
+		this.capacity = arg0;
+		if (this.thermal_energy > arg0) {
+			this.thermal_energy = arg0;
+		}
+
+	}
+
+	public void setMaxTransfer(int arg0) {
+		this.setMaxReceive(arg0);
+		this.setMaxExtract(arg0);
+	}
+
+	public void setMaxReceive(int arg0) {
+		this.maxReceive = arg0;
+	}
+
+	public void setMaxExtract(int arg0) {
+		this.maxExtract = arg0;
+	}
+
+	public int getMaxReceive() {
+		return this.maxReceive;
+	}
+
+	public int getMaxExtract() {
+		return this.maxExtract;
+	}
+
+	public void setEnergyStored(int arg0) {
+		this.thermal_energy = arg0;
+		if (this.thermal_energy > this.capacity) {
+			this.thermal_energy = this.capacity;
+		} else if (this.thermal_energy < 0) {
+			this.thermal_energy = 0;
+		}
+
+	}
+
+	public void modifyEnergyStored(int arg0) {
+		this.thermal_energy += arg0;
+		if (this.thermal_energy > this.capacity) {
+			this.thermal_energy = this.capacity;
+		} else if (this.thermal_energy < 0) {
+			this.thermal_energy = 0;
+		}
+
+	}
+
+	public int receiveThermalEnergy(int arg0, boolean arg1) {
+		int arg2 = Math.min(this.capacity - this.thermal_energy, Math.min(this.maxReceive, arg0));
+		if (!arg1) {
+			this.thermal_energy += arg2;
+		}
+
+		return arg2;
+	}
+
+	public int extractThermalEnergy(int arg0, boolean arg1) {
+		int arg2 = Math.min(this.thermal_energy, Math.min(this.maxExtract, arg0));
+		if (!arg1) {
+			this.thermal_energy -= arg2;
+		}
+
+		return arg2;
+	}
+
+	public int getThermalEnergyStored() {
+		return this.thermal_energy;
+	}
+
+	public int getMaxThermalEnergyStored() {
+		return this.capacity;
+	}
+}
