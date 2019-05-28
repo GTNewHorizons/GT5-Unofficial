ade454d5dd1aaf2402cc65873c4de4d0c75944b4
Спартак <spartak_korolev@mail.ru>
2019-05-28 18:52:10 +0300

    Fix Stick processing

--- src/main/java/gregtech/loaders/oreprocessing/ProcessingStick.java
+++ src/main/java/gregtech/loaders/oreprocessing/ProcessingStick.java
@@ -16,6 +16,7 @@ public class ProcessingStick implements gregtech.api.interfaces.IOreRecipeRegist
         GT_ModHandler.addCraftingRecipe(GT_OreDictUnificator.get(OrePrefixes.springSmall, aMaterial, 1L), new Object[]{" s ", "fPx", 'P', OrePrefixes.stick.get(aMaterial)});
         if (!aMaterial.contains(gregtech.api.enums.SubTag.NO_WORKING)) {
             GT_Values.RA.addLatheRecipe(aMaterial.contains(SubTag.CRYSTAL) ? GT_OreDictUnificator.get(OrePrefixes.gem, aMaterial, 1L) : GT_OreDictUnificator.get(OrePrefixes.ingot, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.stick, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.dustSmall, aMaterial.mMacerateInto, 2L), (int) Math.max(aMaterial.getMass() * 5L, 1L), 16);
+            GT_Values.RA.addLatheRecipe(GT_OreDictUnificator.get(OrePrefixes.stick, aMaterial, 2L), GT_OreDictUnificator.get(OrePrefixes.stickLong, aMaterial, 1L), null, (int) Math.max(aMaterial.getMass() * 3L, 1L), 16);
             GT_Values.RA.addCutterRecipe(GT_Utility.copyAmount(1L, new Object[]{aStack}), GT_OreDictUnificator.get(OrePrefixes.bolt, aMaterial, 4L), null, (int) Math.max(aMaterial.getMass() * 2L, 1L), 4);
             if ((aMaterial.mUnificatable) && (aMaterial.mMaterialInto == aMaterial)) {
                 GT_ModHandler.addCraftingRecipe(GT_OreDictUnificator.get(OrePrefixes.stick, aMaterial, 2L), GT_Proxy.tBits, new Object[]{"s", "X", Character.valueOf('X'), OrePrefixes.stickLong.get(aMaterial)});
