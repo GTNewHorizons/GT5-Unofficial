ef951d663fcd41b2e5064acdbb3596146fc6eabe
Technus <daniel112092@gmail.com>
2019-07-21 21:18:05 +0200

    Include avrcore

---
+++ src/main/java/com/github/technus/tectech/thing/tileEntity/MicroControllerTileEntity.java
@@ -0,0 +1,26 @@
+package com.github.technus.tectech.thing.tileEntity;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+
+public class MicroControllerTileEntity extends TileEntity {
+    @Override
+    public void readFromNBT(NBTTagCompound p_145839_1_) {
+        super.readFromNBT(p_145839_1_);
+    }
+
+    @Override
+    public void writeToNBT(NBTTagCompound p_145841_1_) {
+        super.writeToNBT(p_145841_1_);
+    }
+
+    @Override
+    public void invalidate() {
+        super.invalidate();
+    }
+
+    @Override
+    public void updateEntity() {
+            super.updateEntity();
+    }
+}
