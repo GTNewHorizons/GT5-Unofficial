fc196e2d117e3cc61e2d8c9324c83e8a53e27460
Alkalus <3060479+draknyte1@users.noreply.github.com>
2019-07-29 18:03:24 +0100

    % Moved Registration of Pyrotheum as a Tinker's Smeltery Fuel to Post-Init, may resolve #523.

--- src/Java/gtPlusPlus/xmod/tinkers/HANDLER_Tinkers.java
+++ src/Java/gtPlusPlus/xmod/tinkers/HANDLER_Tinkers.java
@@ -24,18 +24,18 @@ public class HANDLER_Tinkers {
 
 	public static final void init() {
 		if (LoadedMods.TiCon) {
-			//Migrate TiCon further back in the oreDict so that I never grab items from it.
-			//TinkersUtils.stopTiconLoadingFirst();
-			Fluid pyrotheumFluid = FluidRegistry.getFluid("pyrotheum");
-			if (pyrotheumFluid != null) {
-				//Enable Pyrotheum as Fuel for the Smeltery
-				TinkersUtils.addSmelteryFuel(pyrotheumFluid, 5000, 70); // pyrotheum lasts 3.5 seconds per 15 mb
-			}
+			
 		}
 	}
 
 	public static final void postInit() {		
 		if (LoadedMods.TiCon) {
+			
+			Fluid pyrotheumFluid = FluidRegistry.getFluid("pyrotheum");
+			if (pyrotheumFluid != null) {
+				//Enable Pyrotheum as Fuel for the Smeltery
+				TinkersUtils.addSmelteryFuel(pyrotheumFluid, 5000, 70); // pyrotheum lasts 3.5 seconds per 15 mb
+			}
 			
 				for (BaseTinkersMaterial y : mTinkerMaterials) {
 					//y.generate();
