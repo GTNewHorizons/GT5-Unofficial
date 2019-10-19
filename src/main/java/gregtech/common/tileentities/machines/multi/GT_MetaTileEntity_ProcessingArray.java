8aa51a8af5898b1a7876239adecf0267b56142ea
Спартак <spartak_korolev@mail.ru>
2019-10-19 21:40:14 +0300

    Add WireAssembler in ProcessingArray

--- src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_ProcessingArray.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_ProcessingArray.java
@@ -174,8 +174,9 @@ public class GT_MetaTileEntity_ProcessingArray extends GT_MetaTileEntity_MultiBl
             return GT_Recipe.GT_Recipe_Map.sAmplifiers;
         } else if (tmp.startsWith("circuitassembler")) {
             return GT_Recipe.GT_Recipe_Map.sCircuitAssemblerRecipes;
+        } else if (tmp.startsWith("wireassembler")) {
+            return GT_Recipe.GT_Recipe_Map.sWireAssemblerRecipes;
         }
-
         return null;
     }
 
