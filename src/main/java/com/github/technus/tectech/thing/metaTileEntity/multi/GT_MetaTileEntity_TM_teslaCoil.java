01088f97ac790a062308e193de1732f8775abc63
Bass <tudurap.com@gmail.com>
2019-11-14 17:55:50 +0000

    Fixed crash on Tesla tower removal
    nested the onRemoval() due to an occasional java.lang.NullPointerException: Ticking memory connection

--- src/main/java/com/github/technus/tectech/thing/metaTileEntity/multi/GT_MetaTileEntity_TM_teslaCoil.java
+++ src/main/java/com/github/technus/tectech/thing/metaTileEntity/multi/GT_MetaTileEntity_TM_teslaCoil.java
@@ -35,6 +35,7 @@ import java.util.Map;
 
 import static com.github.technus.tectech.CommonValues.V;
 import static com.github.technus.tectech.Util.*;
+import static com.github.technus.tectech.loader.TecTechConfig.DEBUG_MODE;
 import static com.github.technus.tectech.thing.casing.GT_Block_CasingsTT.texturePage;
 import static com.github.technus.tectech.thing.casing.TT_Container_Casings.sBlockCasingsBA0;
 import static com.github.technus.tectech.thing.metaTileEntity.multi.base.LedStatus.*;
@@ -441,10 +442,16 @@ public class GT_MetaTileEntity_TM_teslaCoil extends GT_MetaTileEntity_Multiblock
 
     @Override
     public void onRemoval() {
-        super.onRemoval();
-        for (GT_MetaTileEntity_Hatch_Capacitor cap : eCapacitorHatches) {
-            cap.getBaseMetaTileEntity().setActive(false);
+        try {
+            for (GT_MetaTileEntity_Hatch_Capacitor cap : eCapacitorHatches) {
+                cap.getBaseMetaTileEntity().setActive(false);
+            }
+        } catch (Exception e) {
+            if (DEBUG_MODE) {
+                e.printStackTrace();
+            }
         }
+        super.onRemoval();
     }
 
     @Override
