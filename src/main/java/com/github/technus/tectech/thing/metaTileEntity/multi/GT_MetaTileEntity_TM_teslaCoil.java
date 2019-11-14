02e565d15a37f51daa9740cd5cd461d2fe72430e
Bass <tudurap.com@gmail.com>
2019-11-14 18:48:27 +0000

    Actually fixed crash on Tesla tower removal

--- src/main/java/com/github/technus/tectech/thing/metaTileEntity/multi/GT_MetaTileEntity_TM_teslaCoil.java
+++ src/main/java/com/github/technus/tectech/thing/metaTileEntity/multi/GT_MetaTileEntity_TM_teslaCoil.java
@@ -35,6 +35,7 @@ import java.util.Map;
 
 import static com.github.technus.tectech.CommonValues.V;
 import static com.github.technus.tectech.Util.*;
+import static com.github.technus.tectech.loader.TecTechConfig.DEBUG_MODE;
 import static com.github.technus.tectech.thing.casing.GT_Block_CasingsTT.texturePage;
 import static com.github.technus.tectech.thing.casing.TT_Container_Casings.sBlockCasingsBA0;
 import static com.github.technus.tectech.thing.metaTileEntity.multi.base.LedStatus.*;
@@ -441,6 +442,15 @@ public class GT_MetaTileEntity_TM_teslaCoil extends GT_MetaTileEntity_Multiblock
 
     @Override
     public void onRemoval() {
+        for (GT_MetaTileEntity_Hatch_Capacitor cap : eCapacitorHatches) {
+            try {
+                cap.getBaseMetaTileEntity().setActive(false);
+            } catch (Exception e) {
+                if (DEBUG_MODE) {
+                    e.printStackTrace();
+                }
+            }
+        }
         super.onRemoval();
         for (GT_MetaTileEntity_Hatch_Capacitor cap : eCapacitorHatches) {
             cap.getBaseMetaTileEntity().setActive(false);
