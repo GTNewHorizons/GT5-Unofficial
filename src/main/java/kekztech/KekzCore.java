193e453b3fc84f2b2c8ad90db6372ca9f48abca4
kekzdealer <kekzdealer@gmail.com>
2020-03-04 16:22:09 +0100

    updated version number

--- src/main/java/kekztech/KekzCore.java
+++ src/main/java/kekztech/KekzCore.java
@@ -67,7 +67,7 @@ public class KekzCore {
 	
 	public static final String NAME = "KekzTech";
 	public static final String MODID = "kekztech";
-	public static final String VERSION = "0.2.2";
+	public static final String VERSION = "0.2.3";
 	
 	@Mod.Instance("kekztech")
 	public static KekzCore instance;
