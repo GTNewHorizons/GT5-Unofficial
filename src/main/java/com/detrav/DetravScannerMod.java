728d97731db5d81acc59b9a707780bb39cdae379
DreamMasterXXL <dream-master@gmx.net>
2020-04-25 20:05:29 +0200

    add new fluid colors for underground fluids bump version

--- src/main/java/com/detrav/DetravScannerMod.java
+++ src/main/java/com/detrav/DetravScannerMod.java
@@ -25,7 +25,7 @@ import net.minecraftforge.common.config.Configuration;
 public class DetravScannerMod
 {
     public static final String MODID = "detravscannermod";
-    public static final String VERSION = "1.5.6";
+    public static final String VERSION = "1.5.7";
     public static final String DEBUGOVERRIDE = "@false";
     public static final boolean DEBUGBUILD = Boolean.parseBoolean(DEBUGOVERRIDE.substring(1));
     public static final CreativeTabs TAB_DETRAV = new DetravCreativeTab();
