2adf92e2966311d4179b8253c8f70b0bd42413c6
kekzdealer <kekzdealer@gmail.com>
2020-05-12 11:11:34 +0200

    bug fix version bumb

--- src/main/java/kekztech/KekzCore.java
+++ src/main/java/kekztech/KekzCore.java
@@ -41,7 +41,7 @@ public class KekzCore {
 	
 	public static final String NAME = "KekzTech";
 	public static final String MODID = "kekztech";
-	public static final String VERSION = "0.4";
+	public static final String VERSION = "0.4.1";
 
 	public static final Logger LOGGER = LogManager.getLogger(NAME);
 
