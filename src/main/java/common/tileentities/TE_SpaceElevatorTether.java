a785ef53e7e135fe59a95d3412b73a3a6a31da38
kekzdealer <kekzdealer@gmail.com>
2020-05-18 13:36:05 +0200

    Fixed thether beam being culled when source block is out of sight

--- src/main/java/common/tileentities/TE_SpaceElevatorTether.java
+++ src/main/java/common/tileentities/TE_SpaceElevatorTether.java
@@ -3,6 +3,7 @@ package common.tileentities;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 
 public class TE_SpaceElevatorTether extends TileEntity {
 
@@ -11,5 +12,12 @@ public class TE_SpaceElevatorTether extends TileEntity {
     public double getMaxRenderDistanceSquared() {
         // 4k is standard, 65k is what the vanilla beacon uses
         return 65536.0D;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public AxisAlignedBB getRenderBoundingBox() {
+        // Make it so the beam is still rendered even when the source block is out of sight
+        return INFINITE_EXTENT_AABB;
     }
 }
