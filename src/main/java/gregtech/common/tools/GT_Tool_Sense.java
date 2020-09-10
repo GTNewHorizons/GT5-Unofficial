5b943972313e28364b7dac99d1cc8515b8f4dcb9
Lex <50260829+4gname@users.noreply.github.com>
2020-09-10 23:03:44 +0700

    Fix for sense block digging (now only leaves)

--- src/main/java/gregtech/common/tools/GT_Tool_Sense.java
+++ src/main/java/gregtech/common/tools/GT_Tool_Sense.java
@@ -8,6 +8,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
@@ -41,8 +42,16 @@ public class GT_Tool_Sense
             for (int i = -2; i < 3; i++) {
                 for (int j = -2; j < 3; j++) {
                     for (int k = -2; k < 3; k++) {
-                        if (((i != 0) || (j != 0) || (k != 0)) && (aStack.getItem().getDigSpeed(aStack, aPlayer.worldObj.getBlock(aX + i, aY + j, aZ + k), aPlayer.worldObj.getBlockMetadata(aX + i, aY + j, aZ + k)) > 0.0F) && (((EntityPlayerMP) aPlayer).theItemInWorldManager.tryHarvestBlock(aX + i, aY + j, aZ + k))) {
-                            rConversions++;
+
+                        if (!((aEvent.world.getBlock(aX + i, aY + j, aZ + k) == Blocks.leaves) ||
+                                (aEvent.world.getBlock(aX + i, aY + j, aZ + k) == Blocks.leaves2))) {
+                        } else {
+                            if (((i != 0) || (j != 0) || (k != 0)) && (aStack.getItem().getDigSpeed(aStack,
+                                    aPlayer.worldObj.getBlock(aX + i, aY + j, aZ + k),
+                                    aPlayer.worldObj.getBlockMetadata(aX + i, aY + j, aZ + k)) > 0.0F) &&
+                                    (((EntityPlayerMP) aPlayer).theItemInWorldManager.tryHarvestBlock(aX + i, aY + j, aZ + k))) {
+                                rConversions++;
+                            }
                         }
                     }
                 }
