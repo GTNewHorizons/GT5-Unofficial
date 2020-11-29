bd9322e8352b50f5d816bb65d92bfaeea7bd072c
basdxz <tudurap.com@gmail.com>
2020-11-29 17:28:00 +0000

    More robust Tesla covers

--- src/main/java/com/github/technus/tectech/mechanics/tesla/TeslaCoverConnection.java
+++ src/main/java/com/github/technus/tectech/mechanics/tesla/TeslaCoverConnection.java
@@ -3,12 +3,17 @@ package com.github.technus.tectech.mechanics.tesla;
 import com.github.technus.tectech.util.Vec3Impl;
 import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
 
+import static com.github.technus.tectech.mechanics.tesla.ITeslaConnectable.TeslaUtil.teslaSimpleNodeSetAdd;
+import static com.github.technus.tectech.mechanics.tesla.ITeslaConnectable.TeslaUtil.teslaSimpleNodeSetRemove;
+
 public class TeslaCoverConnection implements ITeslaConnectableSimple {
     private final IGregTechTileEntity IGT;
+    private final Vec3Impl pos;
     private final byte teslaReceptionCapability;
 
     public TeslaCoverConnection(IGregTechTileEntity IGT, byte teslaReceptionCapability) {
         this.IGT = IGT;
+        this.pos = new Vec3Impl(IGT);
         this.teslaReceptionCapability = teslaReceptionCapability;
     }
 
@@ -34,7 +39,7 @@ public class TeslaCoverConnection implements ITeslaConnectableSimple {
 
     @Override
     public Vec3Impl getTeslaPosition() {
-        return new Vec3Impl(IGT);
+        return pos;
     }
 
     @Override
@@ -45,6 +50,14 @@ public class TeslaCoverConnection implements ITeslaConnectableSimple {
     @Override
     public boolean teslaInjectEnergy(long teslaVoltageInjected) {
         //Same as in the microwave transmitters, this does not account for amp limits
-        return IGT.injectEnergyUnits((byte) 1, teslaVoltageInjected, 1L) > 0L;
+        boolean output = false;
+
+        if (!IGT.isDead()){
+            output = IGT.injectEnergyUnits((byte) 1, teslaVoltageInjected, 1L) > 0L;
+        } else {
+            teslaSimpleNodeSetRemove(this);
+        }
+
+        return output;
     }
 }
