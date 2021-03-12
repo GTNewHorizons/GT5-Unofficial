d72bc905090623048f3da4c9979464d746168a05
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2021-03-12 11:37:33 +0100

    Removed old dependency annotation
    (cherry picked from commit 6ae8b900c872195d1e7134b66c2799a66bdbfc9e [formerly d8109a84906b2ce8e3969d3bc89d4a6e8e3ee74c])
    
    
    Former-commit-id: eb00e7c09a0ba36911f0d954238a0cc824285421

--- src/main/java/com/github/bartimaeusnek/crossmod/BartWorksCrossmod.java
+++ src/main/java/com/github/bartimaeusnek/crossmod/BartWorksCrossmod.java
@@ -42,17 +42,18 @@ import org.apache.logging.log4j.Logger;
 import java.io.StringReader;
 
 @Mod(
-        modid = BartWorksCrossmod.MOD_ID, name = BartWorksCrossmod.NAME, version = BartWorksCrossmod.VERSION,
-        dependencies = "required-after:IC2; "
-                + "required-after:gregtech; "
-                + "required-after:bartworks;"
-                + "before:TGregworks; "
-                + "after:GalacticraftMars; "
-                + "after:GalacticraftCore; "
-                + "after:Micdoodlecore; "
-                + "after:miscutils;"
-                + "after:EMT;"
-                + "after:tectech;"
+        modid        = BartWorksCrossmod.MOD_ID, 
+        name         = BartWorksCrossmod.NAME, 
+        version      = BartWorksCrossmod.VERSION,
+        dependencies =   "required-after:IC2;      "
+                       + "required-after:gregtech; "
+                       + "required-after:bartworks;"
+                       + "after:GalacticraftMars;  "
+                       + "after:GalacticraftCore;  "
+                       + "after:Micdoodlecore;     "
+                       + "after:miscutils;         "
+                       + "after:EMT;               "
+                       + "after:tectech;           "
 )
 public class BartWorksCrossmod {
     public static final String NAME = "BartWorks Mod Additions";
