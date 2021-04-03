8b9767c1ac9f63834b6d67cac5bfd7394ba18bb2
charles <johnch18@isu.edu>
2021-04-03 00:38:57 -0600

    Added misc. functions to make usage easier.

--- src/main/java/gregtech/api/items/GT_Configurator_Item.java
+++ src/main/java/gregtech/api/items/GT_Configurator_Item.java
@@ -1,5 +1,7 @@
 package gregtech.api.items;
 
+import gregtech.api.util.GT_Utility;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 
 public class GT_Configurator_Item extends Item {
@@ -7,11 +9,33 @@ public class GT_Configurator_Item extends Item {
     private int setting = 0;
     private static final int MIN_SETTING = 0, MAX_SETTING = 32;
 
+    public void adjust() {
+        setting = setting % MAX_SETTING;
+    }
+
     public void setSetting(int newSetting) {
-        setting = newSetting % MAX_SETTING;
+        setting = newSetting;
+        adjust();
     }
 
     public int getSetting() {
         return setting;
     }
+
+    public int bumpUp() {
+        setting++;
+        adjust();
+        return setting;
+    }
+
+    public int bumpDown() {
+        setting--;
+        adjust();
+        return setting;
+    }
+
+    public void notify(EntityPlayer aPlayer) {
+        GT_Utility.sendChatToPlayer(aPlayer, "Configuration: " + setting);
+    }
+
 }
