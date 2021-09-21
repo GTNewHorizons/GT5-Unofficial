32e0860a938ffa7e377a38c7b14e26ae208d15ee
GlodBlock <1356392126@qq.com>
2021-09-21 23:30:05 +0800

    make display number more human readable

--- src/main/java/GoodGenerator/Client/GUI/YOTTankGUIClient.java
+++ src/main/java/GoodGenerator/Client/GUI/YOTTankGUIClient.java
@@ -1,6 +1,7 @@
 package GoodGenerator.Client.GUI;
 
 import GoodGenerator.Common.Container.YOTTankGUIContainer;
+import GoodGenerator.util.CharExchanger;
 import com.github.technus.tectech.thing.metaTileEntity.multi.base.GT_GUIContainer_MultiMachineEM;
 import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
 import net.minecraft.entity.player.InventoryPlayer;
@@ -19,9 +20,9 @@ public class YOTTankGUIClient extends GT_GUIContainer_MultiMachineEM {
     protected void drawGuiContainerForegroundLayer(int par1, int par2) {
         super.drawGuiContainerForegroundLayer(par1, par2);
         if (this.mContainer.mDisplayErrorCode == 0) {
-            fontRendererObj.drawString(StatCollector.translateToLocal("gui.YOTTank.0") + " " + containerYOTTank.getCap() + " L", 7, 40, 16448255);
-            fontRendererObj.drawString(StatCollector.translateToLocal("gui.YOTTank.1") + " " + containerYOTTank.getFluidName(), 7, 48, 16448255);
-            fontRendererObj.drawString(StatCollector.translateToLocal("gui.YOTTank.2") + " " + containerYOTTank.getStorage() + " L", 7, 56, 16448255);
+            fontRendererObj.drawString(StatCollector.translateToLocal("gui.YOTTank.0") + " " + CharExchanger.formatNumber(containerYOTTank.getCap()) + " L", 7, 40, 16448255);
+            fontRendererObj.drawString(StatCollector.translateToLocal("gui.YOTTank.1") + " " + CharExchanger.formatNumber(containerYOTTank.getFluidName()), 7, 48, 16448255);
+            fontRendererObj.drawString(StatCollector.translateToLocal("gui.YOTTank.2") + " " + CharExchanger.formatNumber(containerYOTTank.getStorage()) + " L", 7, 56, 16448255);
         }
     }
 }
