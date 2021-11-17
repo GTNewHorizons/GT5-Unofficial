075e31f6160174f1087707d26b36c9d22c607492
iouter <3155186831@qq.com>
2021-11-17 08:59:58 +0800

    add GUI for overflow valve

--- src/Java/gtPlusPlus/xmod/gregtech/common/covers/GTPP_Cover_Overflow.java
+++ src/Java/gtPlusPlus/xmod/gregtech/common/covers/GTPP_Cover_Overflow.java
@@ -1,5 +1,6 @@
 package gtPlusPlus.xmod.gregtech.common.covers;
 
+import gregtech.api.gui.GT_GUICover;
 import gregtech.api.interfaces.tileentity.ICoverable;
 import gregtech.api.util.GT_CoverBehavior;
 import gregtech.api.util.GT_Utility;
@@ -9,6 +10,10 @@ import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.IFluidHandler;
+import gregtech.api.gui.widgets.GT_GuiIntegerTextBox;
+import gregtech.api.enums.GT_Values;
+import gregtech.api.net.GT_Packet_TileEntityCover;
+
 
 public class GTPP_Cover_Overflow extends GT_CoverBehavior {
 
@@ -133,5 +138,110 @@ public class GTPP_Cover_Overflow extends GT_CoverBehavior {
 
 	public int getTickRate(byte aSide, int aCoverID, int aCoverVariable, ICoverable aTileEntity) {
 		return 5;
+	}
+
+	//GUI
+	@Override
+	public boolean hasCoverGUI() {
+		return true;
+	}
+
+	@Override
+	public Object getClientGUI(byte aSide, int aCoverID, int coverData, ICoverable aTileEntity) {
+		return new GTPP_Cover_Overflow.GUI(aSide, aCoverID, coverData, aTileEntity);
+	}
+
+	private class GUI extends GT_GUICover {
+		private final byte side;
+		private final int coverID;
+		private GT_GuiIntegerTextBox tBox;
+		private int coverVariable;
+
+		private static final int startX = 10;
+		private static final int startY = 25;
+		private static final int spaceX = 18;
+		private static final int spaceY = 18;
+
+		private boolean warn = false;
+
+		public GUI(byte aSide, int aCoverID, int aCoverVariable, ICoverable aTileEntity) {
+			super(aTileEntity, 176, 107, GT_Utility.intToStack(aCoverID));
+			this.side = aSide;
+			this.coverID = aCoverID;
+			this.coverVariable = aCoverVariable;
+
+			tBox = new GT_GuiIntegerTextBox(this, 2, startX + spaceX * 0, startY + spaceY * 0 + 8, spaceX * 4 - 3, 12);
+			tBox.setText(String.valueOf(this.coverVariable));
+			tBox.setMaxStringLength(10);
+		}
+
+		@Override
+		protected void onInitGui(int guiLeft, int guiTop, int gui_width, int gui_height) {
+			tBox.setFocused(true);
+		}
+
+		@Override
+		public void drawExtras(int mouseX, int mouseY, float parTicks) {
+			super.drawExtras(mouseX, mouseY, parTicks);
+			this.getFontRenderer().drawString(trans("010", "L"), startX + spaceX * 4, 4 + startY + spaceY * 0 + 8, 0xFF555555);
+			if (warn)
+				this.getFontRenderer().drawString(String.format(trans("011", "Max: %d L"), coverVariable), startX + spaceX * 0, 4 + startY + spaceY * 1 + 6, 0xffff0000);
+			else
+				this.getFontRenderer().drawString(String.format(trans("011", "Now: %d L"), coverVariable), startX + spaceX * 0, 4 + startY + spaceY * 1 + 6, 0xFF555555);
+		}
+		@Override
+		public void onMouseWheel(int x, int y, int delta) {
+			for (GT_GuiIntegerTextBox box : textBoxes){
+				if (box.isFocused()) {
+					int step = Math.max(1, Math.abs(delta / 120));
+					step = (isShiftKeyDown() ? 50 : isCtrlKeyDown() ? 5 : 1) * (delta > 0 ? step : -step);
+					long i;
+					try {
+						i = Long.parseLong(box.getText());
+					} catch (NumberFormatException e) {
+						return;
+					}
+					if (i > (Long.MAX_VALUE-1000))
+						break;
+
+					i = i + step;
+					if (i <= 0)
+						i = 0;
+					box.setText(String.valueOf(i));
+					break;
+				}
+			}
+		}
+
+		@Override
+		public void applyTextBox(GT_GuiIntegerTextBox box) {
+			long i;
+			String s = box.getText().trim();
+			try {
+				i = Long.parseLong(s);
+			} catch (NumberFormatException e) {
+				resetTextBox(box);
+				return;
+			}
+
+			warn = false;
+			if (box.id == 2) {
+				if (i > (long) mTransferRate * 10) {
+					i = mTransferRate * 10;
+					warn = true;
+				} else if (i < 0) {
+					i = 0;
+				}
+				coverVariable = (int) i;
+			}
+			box.setText(String.valueOf(i));
+			GT_Values.NW.sendToServer(new GT_Packet_TileEntityCover(side, coverID, coverVariable, tile));
+		}
+
+		@Override
+		public void resetTextBox(GT_GuiIntegerTextBox box) {
+			if (box.id == 2)
+				box.setText(String.valueOf(coverVariable));
+		}
 	}
 }
