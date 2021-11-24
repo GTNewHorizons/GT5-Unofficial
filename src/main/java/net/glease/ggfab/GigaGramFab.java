b0698efc456a3dc92a8b7a059b01433d461866e3
Glease <4586901+Glease@users.noreply.github.com>
2021-11-24 22:21:21 +0800

    Initial commit
    Signed-off-by: Glease <4586901+Glease@users.noreply.github.com>

---
+++ src/main/java/net/glease/ggfab/GigaGramFab.java
@@ -0,0 +1,22 @@
+package net.glease.ggfab;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.*;
+
+@Mod(modid = GigaGramFab.MODID, version = "${version}", name = "GigaGramFab", acceptedMinecraftVersions = "[1.7.10]", dependencies = "required-after:IC2;")
+public class GigaGramFab {
+    public static final String MODID = "ggfab";
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+    }
+
+}
