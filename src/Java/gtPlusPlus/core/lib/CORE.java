9974e212bed10edb130229a97c1d9b314ec160ce
Alkalus <3060479+draknyte1@users.noreply.github.com>
2021-11-28 11:36:02 +0000

    Version bump.

--- src/Java/gtPlusPlus/core/lib/CORE.java
+++ src/Java/gtPlusPlus/core/lib/CORE.java
@@ -52,7 +52,7 @@ public class CORE {
 
 	public static final String name = "GT++";
 	public static final String MODID = "miscutils";
-	public static final String VERSION = "1.7.06-GTNH";
+	public static final String VERSION = "1.7.15-GTNH";
 
 	//Tweakables
 	public static int EVERGLADES_ID = 227;
