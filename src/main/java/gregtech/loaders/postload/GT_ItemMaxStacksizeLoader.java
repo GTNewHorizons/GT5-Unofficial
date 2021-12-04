c287ff6b24e0144c623a40ddda680fa9ec388792
Martin Robertz <dream-master@gmx.net>
2021-12-04 12:12:36 +0100

    https://github.com/GTNewHorizons/GT-New-Horizons-Modpack/pull/8984#issuecomment-986010006 put stackzize from snwoballs, enderpearls and eggs into gt code

--- src/main/java/gregtech/loaders/postload/GT_ItemMaxStacksizeLoader.java
+++ src/main/java/gregtech/loaders/postload/GT_ItemMaxStacksizeLoader.java
@@ -17,6 +17,9 @@ public class GT_ItemMaxStacksizeLoader implements Runnable {
         Items.cake.setMaxStackSize(64);
         Items.wooden_door.setMaxStackSize(8);
         Items.iron_door.setMaxStackSize(8);
+        Items.ender_pearl.setMaxStackSize(64);
+        Items.egg.setMaxStackSize(64);
+        Items.snowball.setMaxStackSize(64);
         if (OrePrefixes.plank.mDefaultStackSize < 64) {
             Item.getItemFromBlock(Blocks.wooden_slab).setMaxStackSize(OrePrefixes.plank.mDefaultStackSize);
             Item.getItemFromBlock(Blocks.double_wooden_slab).setMaxStackSize(OrePrefixes.plank.mDefaultStackSize);
