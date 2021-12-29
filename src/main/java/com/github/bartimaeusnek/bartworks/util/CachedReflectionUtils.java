109151216f6ba3c0109ff5f8f848c3a37d543a84
Glease <4586901+Glease@users.noreply.github.com>
2021-12-29 20:55:23 +0800

    Fix copy pasting error (#78)
    Former-commit-id: e3ad540439325619fd59374a65a7fd88e089355d

--- src/main/java/com/github/bartimaeusnek/bartworks/util/CachedReflectionUtils.java
+++ src/main/java/com/github/bartimaeusnek/bartworks/util/CachedReflectionUtils.java
@@ -15,7 +15,7 @@ public class CachedReflectionUtils {
     }
 
     public static Field getDeclaredField(final Class<?> cls, final String fieldName) {
-        return fields.get(cls).computeIfAbsent(fieldName, f -> FieldUtils.getDeclaredField(cls, f, true));
+        return declaredFields.get(cls).computeIfAbsent(fieldName, f -> FieldUtils.getDeclaredField(cls, f, true));
     }
 
     private static class ConcurrentMapClassValue extends ClassValue<Map<String, Field>> {
