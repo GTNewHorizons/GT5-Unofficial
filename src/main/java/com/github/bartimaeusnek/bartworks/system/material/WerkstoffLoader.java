54320d45e52134ca823d3cb8bab8d2b6c87ebdcb
Elisis <jarrad.thomson@gmail.com>
2022-01-11 23:36:39 +1100

    Reserve Werkstoff ID (#79)
    Former-commit-id: d2490021aba4cb844786ad8104e276cf4d624d4d

--- src/main/java/com/github/bartimaeusnek/bartworks/system/material/WerkstoffLoader.java
+++ src/main/java/com/github/bartimaeusnek/bartworks/system/material/WerkstoffLoader.java
@@ -192,9 +192,10 @@ public class WerkstoffLoader {
     }
 
     //TODO:
-    //FREE ID RANGE:                11_000-28_998
+    //FREE ID RANGE:                11_500-28_998
     //bartimaeusnek reserved             0-10_000
     //GlodBlock reserved range      10_001-10_999
+    //Elisis reserved range         11_000-11_499
     //bot reserved range            29_899-29_999
     //Tec & basdxz reserved range   30_000-31_000
     //GT Material range reserved on 31_767-32_767
