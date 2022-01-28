2bacd44e1f24b16ad46e5e21056e059911a6df7f
glowredman <35727266+glowredman@users.noreply.github.com>
2022-01-28 21:28:18 +0100

    Reserve IDs for GalaxySpace (#81)
    Former-commit-id: 1cfb8e9a10611aa43bdda0367f75c0b3011ba126

--- src/main/java/com/github/bartimaeusnek/bartworks/system/material/WerkstoffLoader.java
+++ src/main/java/com/github/bartimaeusnek/bartworks/system/material/WerkstoffLoader.java
@@ -192,10 +192,11 @@ public class WerkstoffLoader {
     }
 
     //TODO:
-    //FREE ID RANGE:                11_500-28_998
+    //FREE ID RANGE:                12_000-28_998
     //bartimaeusnek reserved             0-10_000
     //GlodBlock reserved range      10_001-10_999
     //Elisis reserved range         11_000-11_499
+    //glowredman reserved range     11_500-11_999
     //bot reserved range            29_899-29_999
     //Tec & basdxz reserved range   30_000-31_000
     //GT Material range reserved on 31_767-32_767
