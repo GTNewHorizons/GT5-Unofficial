01ccba8c538a43cfd6a0e1d169c7659871a2f305
Tec <daniel112092@gmail.com>
2022-01-31 21:29:47 +0100

    Make font not use UTF-8 literal

--- src/main/java/com/github/technus/tectech/font/TecTechFontRender.java
+++ src/main/java/com/github/technus/tectech/font/TecTechFontRender.java
@@ -14,14 +14,14 @@ import org.lwjgl.opengl.GL11;
 public class TecTechFontRender extends FontRenderer {
     public static final TecTechFontRender INSTANCE = new TecTechFontRender();
 
-    private static float DISTANCE_L  = .125F;
-    private static float DISTANCE_L2 = DISTANCE_L * 2F;
+    private static final float DISTANCE_L  = .125F;
+    private static final float DISTANCE_L2 = DISTANCE_L * 2F;
 
-    private static float DISTANCE_M  = 0.06F;
-    private static float DISTANCE_M2 = DISTANCE_M * 2F;
+    private static final float DISTANCE_M  = 0.06F;
+    private static final float DISTANCE_M2 = DISTANCE_M * 2F;
 
-    private static float DISTANCE_A  = 0.06F;
-    private static float DISTANCE_A2 = DISTANCE_A * 2F;
+    private static final float DISTANCE_A  = 0.06F;
+    private static final float DISTANCE_A2 = DISTANCE_A * 2F;
 
 
     private TecTechFontRender() {
@@ -119,7 +119,7 @@ public class TecTechFontRender extends FontRenderer {
 
                 ++i;
             } else {
-                j = "ÀÁÂÈÊËÍÓÔÕÚßãõğİıŒœŞşŴŵžȇ\u0000\u0000\u0000\u0000\u0000\u0000\u0000 !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\u0000ÇüéâäàåçêëèïîìÄÅÉæÆôöòûùÿÖÜø£Ø×ƒáíóúñÑªº¿®¬½¼¡«»░▒▓│┤╡╢╖╕╣║╗╝╜╛┐└┴┬├─┼╞╟╚╔╩╦╠═╬╧╨╤╥╙╘╒╓╫╪┘┌█▄▌▐▀αβΓπΣσμτΦΘΩδ∞∅∈∩≡±≥≤⌠⌡÷≈°∙·√ⁿ²■\u0000".indexOf(c0);
+                j = "\u00C0\u00C1\u00C2\u00C8\u00CA\u00CB\u00CD\u00D3\u00D4\u00D5\u00DA\u00DF\u00E3\u00F5\u011F\u0130\u0131\u0152\u0153\u015E\u015F\u0174\u0175\u017E\u0207\u0000\u0000\u0000\u0000\u0000\u0000\u0000 !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\u0000\u00C7\u00FC\u00E9\u00E2\u00E4\u00E0\u00E5\u00E7\u00EA\u00EB\u00E8\u00EF\u00EE\u00EC\u00C4\u00C5\u00C9\u00E6\u00C6\u00F4\u00F6\u00F2\u00FB\u00F9\u00FF\u00D6\u00DC\u00F8\u00A3\u00D8\u00D7\u0192\u00E1\u00ED\u00F3\u00FA\u00F1\u00D1\u00AA\u00BA\u00BF\u00AE\u00AC\u00BD\u00BC\u00A1\u00AB\u00BB\u2591\u2592\u2593\u2502\u2524\u2561\u2562\u2556\u2555\u2563\u2551\u2557\u255D\u255C\u255B\u2510\u2514\u2534\u252C\u251C\u2500\u253C\u255E\u255F\u255A\u2554\u2569\u2566\u2560\u2550\u256C\u2567\u2568\u2564\u2565\u2559\u2558\u2552\u2553\u256B\u256A\u2518\u250C\u2588\u2584\u258C\u2590\u2580\u03B1\u03B2\u0393\u03C0\u03A3\u03C3\u03BC\u03C4\u03A6\u0398\u03A9\u03B4\u221E\u2205\u2208\u2229\u2261\u00B1\u2265\u2264\u2320\u2321\u00F7\u2248\u00B0\u2219\u00B7\u221A\u207F\u00B2\u25A0\u0000".indexOf(c0);
                 if (this.randomStyle && j != -1) {
                     do {
                         k = this.fontRandom.nextInt(this.charWidth.length);
