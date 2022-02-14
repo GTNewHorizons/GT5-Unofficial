d98b6ea4049e752011a2a22b3067f827ea55d7ab
repo_alt <wvk17@yandex.ru>
2022-02-14 18:48:09 +0300

    Server side metaitems localization fixes https://github.com/GTNewHorizons/GT-New-Horizons-Modpack/issues/8778

--- src/main/java/gregtech/api/util/GT_LanguageManager.java
+++ src/main/java/gregtech/api/util/GT_LanguageManager.java
@@ -2,19 +2,23 @@ package gregtech.api.util;
 
 import cpw.mods.fml.common.registry.LanguageRegistry;
 import gregtech.api.GregTech_API;
+import gregtech.api.enums.GT_Values;
+
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.StatCollector;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.common.config.Property;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import static gregtech.api.enums.GT_Values.E;
 
 public class GT_LanguageManager {
-    public static final HashMap<String, String> TEMPMAP = new HashMap<String, String>(), BUFFERMAP = new HashMap<String, String>(), LANGMAP = new HashMap<String, String>();
+    public static final HashMap<String, String> TEMPMAP = new HashMap<>(), BUFFERMAP = new HashMap<>(), LANGMAP = new HashMap<>();
     public static Configuration sEnglishFile;
 	public static String sLanguage = "en_US";
     public static boolean sUseEnglishFile = false;
@@ -78,8 +82,9 @@ public class GT_LanguageManager {
             rTranslation = StatCollector.translateToLocal(tTrimmedKey);
             if (GT_Utility.isStringInvalid(rTranslation) || tTrimmedKey.equals(rTranslation)) {
                 if (aKey.endsWith(".name")) {
-                    rTranslation = StatCollector.translateToLocal(tTrimmedKey.substring(0, tTrimmedKey.length() - 5));
-                    if (GT_Utility.isStringInvalid(rTranslation) || tTrimmedKey.substring(0, tTrimmedKey.length() - 5).equals(rTranslation)) {
+                    String trimmedKeyNoName = tTrimmedKey.substring(0, tTrimmedKey.length() - 5);
+                    rTranslation = StatCollector.translateToLocal(trimmedKeyNoName);
+                    if (GT_Utility.isStringInvalid(rTranslation) || trimmedKeyNoName.equals(rTranslation)) {
                         return aKey;
                     }
                 } else {
@@ -365,5 +370,23 @@ public class GT_LanguageManager {
         addStringLocalization("Item_DESCRIPTION_Index_500", "Turbine Efficiency (Loose): %s");
         addStringLocalization("Item_DESCRIPTION_Index_501", "Optimal Steam flow (Loose): %s L/t");
     }
-    
+
+    @SuppressWarnings("rawtypes, unchecked")
+    public static void propagateLocalizationServerSide() {
+        if (!GT_Values.GT.isServerSide())
+            return;
+        try {
+            Class cls = Class.forName("net.minecraft.util.StringTranslate");
+            Field languageList = cls.getDeclaredField("languageList");
+            languageList.setAccessible(true);
+            Field instance = cls.getDeclaredField("instance");
+            instance.setAccessible(true);
+            Object m = languageList.get(instance.get(null));
+            if (!(m instanceof Map))
+                return;
+            ((Map)m).putAll(LANGMAP);
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+        }
+    }
 }
