b9d2723cca1cbb05a421809b9cce0f1a2a1ba0be
Glease <4586901+Glease@users.noreply.github.com>
2022-03-06 20:28:47 +0800

    fix id drift in bartworks

--- src/main/java/gregtech/api/enums/ItemList.java
+++ src/main/java/gregtech/api/enums/ItemList.java
@@ -1700,11 +1700,6 @@ public enum ItemList implements IItemContainer {
     Circuit_Parts_TransistorASMD,
     Circuit_Parts_CapacitorASMD,
 
-    Circuit_Parts_ResistorXSMD,
-    Circuit_Parts_DiodeXSMD,
-    Circuit_Parts_TransistorXSMD,
-    Circuit_Parts_CapacitorXSMD,
-
     Circuit_Silicon_Ingot,
     Circuit_Silicon_Ingot2,
     Circuit_Silicon_Ingot3,
@@ -1832,6 +1827,11 @@ public enum ItemList implements IItemContainer {
     ZPM_Coil,
     UV_Coil,
     UHV_Coil,
+
+    Circuit_Parts_ResistorXSMD,
+    Circuit_Parts_DiodeXSMD,
+    Circuit_Parts_TransistorXSMD,
+    Circuit_Parts_CapacitorXSMD,
 
     VOLUMETRIC_FLASK;
 	
