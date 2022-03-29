aa0d37acc501ff6151cd46a759386e424a41a573
Martin Robertz <dream-master@gmx.net>
2022-03-29 10:16:36 +0200

    Add new Blocks doe Sieg and Sisg move cryolite to metal blocks change Boule recipes to use new SI variants

--- src/main/java/gregtech/api/GregTech_API.java
+++ src/main/java/gregtech/api/GregTech_API.java
@@ -276,7 +276,7 @@ public class GregTech_API {
             sBlockOresUb2,
             sBlockOresUb3,
     /*sBlockGem,*/
-    sBlockMetal1,
+            sBlockMetal1,
             sBlockMetal2,
             sBlockMetal3,
             sBlockMetal4,
@@ -284,6 +284,7 @@ public class GregTech_API {
             sBlockMetal6,
             sBlockMetal7,
             sBlockMetal8,
+            sBlockMetal9,
             sBlockGem1,
             sBlockGem2,
             sBlockGem3,
