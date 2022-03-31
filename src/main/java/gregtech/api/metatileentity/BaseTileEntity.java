124ae441dff42c797ded1dbc868a779709a9524a
Jakub Szewczyk <git@kubasz.xyz>
2022-03-31 10:57:49 +0100

    Avoid any potential NPE in markDirty

--- src/main/java/gregtech/api/metatileentity/BaseTileEntity.java
+++ src/main/java/gregtech/api/metatileentity/BaseTileEntity.java
@@ -16,6 +16,7 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.fluids.IFluidHandler;
@@ -469,7 +470,10 @@ public abstract class BaseTileEntity extends TileEntity implements IHasWorldObje
     @Override
     public void markDirty() {
         // Avoid sending neighbor updates, just mark the chunk as dirty to make sure it gets saved
-        worldObj.getChunkFromBlockCoords(xCoord, zCoord).setChunkModified();
+        Chunk chunk = worldObj.getChunkFromBlockCoords(xCoord, zCoord);
+        if(chunk != null) {
+            chunk.setChunkModified();
+        }
     }
 
     @Deprecated
