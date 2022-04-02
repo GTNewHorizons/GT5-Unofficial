748eb7a0d6ff1fd352c7a1f2bb1303c25a96c5d3
Martin Robertz <dream-master@gmx.net>
2022-04-02 16:12:49 +0200

    add 4 output slots to cutting machines (hv and up) add Silicon dust to Wafer recipes (recycling)
    (cherry picked from commit 7a5bb72fcc9337a6841e7c7f0e487eee3d8b363b)

--- src/main/java/gregtech/api/interfaces/internal/IGT_RecipeAdder.java
+++ src/main/java/gregtech/api/interfaces/internal/IGT_RecipeAdder.java
@@ -476,6 +476,8 @@ public interface IGT_RecipeAdder {
 
     boolean addCutterRecipe(ItemStack[] aInputs, ItemStack[] aOutputs, int aDuration, int aEUt, int aSpecial);
 
+    boolean addCutterRecipe(ItemStack[] aInputs, ItemStack[] aOutputs, int aDuration, int aEUt, boolean aCleanroom);
+
     /**
      * Adds a Boxing Recipe
      */
