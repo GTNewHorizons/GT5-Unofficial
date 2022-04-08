75efe58ff064fa59c42e68bb2b6020b23bdd49dd
MuXiu1997 <MuXiu1997@Gmail.com>
2022-04-09 03:02:13 +0800

    Add a voiding mode for Super/Quantum tanks (#1009)
    * Add a voiding mode for Super/Quantum tanks
    
    * Fix things
    
    * Fix things

--- src/main/java/gregtech/common/tileentities/storage/GT_MetaTileEntity_QuantumTank.java
+++ src/main/java/gregtech/common/tileentities/storage/GT_MetaTileEntity_QuantumTank.java
@@ -3,159 +3,25 @@ package gregtech.common.tileentities.storage;
 import gregtech.api.interfaces.ITexture;
 import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
 import gregtech.api.metatileentity.MetaTileEntity;
-import gregtech.api.metatileentity.implementations.GT_MetaTileEntity_BasicTank;
-import gregtech.api.render.TextureFactory;
 import gregtech.api.util.GT_Utility;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumChatFormatting;
-import net.minecraftforge.common.util.ForgeDirection;
 
-import static gregtech.api.enums.Textures.BlockIcons.MACHINE_CASINGS;
-import static gregtech.api.enums.Textures.BlockIcons.OVERLAY_QTANK;
-import static gregtech.api.enums.Textures.BlockIcons.OVERLAY_QTANK_GLOW;
-
-public class GT_MetaTileEntity_QuantumTank extends GT_MetaTileEntity_BasicTank {
-
-    private boolean voidBreak;
-
+public class GT_MetaTileEntity_QuantumTank extends GT_MetaTileEntity_DigitalTankBase {
     public GT_MetaTileEntity_QuantumTank(int aID, String aName, String aNameRegional, int aTier) {
-        super(aID, aName, aNameRegional, aTier, 3, new String[]{
-                "Stores " + GT_Utility.formatNumbers(commonSizeCompute(aTier)) + "L of fluid",
-                "Can keep its contents when harvested",
-                "Sneak when harvesting to void its contents"
-        });
-    }
-
-    private static int commonSizeCompute(int tier) {
-        switch (tier) {
-            case 6:
-                return 128000000;
-            case 7:
-                return 256000000;
-            case 8:
-                return 512000000;
-            case 9:
-                return 1024000000;
-            case 10:
-                return 2147483640;
-            default:
-                return 0;
-        }
+        super(aID, aName, aNameRegional, aTier);
     }
 
     public GT_MetaTileEntity_QuantumTank(String aName, int aTier, String aDescription, ITexture[][][] aTextures) {
-        super(aName, aTier, 3, aDescription, aTextures);
+        super(aName, aTier, aDescription, aTextures);
     }
 
     public GT_MetaTileEntity_QuantumTank(String aName, int aTier, String[] aDescription, ITexture[][][] aTextures) {
-        super(aName, aTier, 3, aDescription, aTextures);
-    }
-
-    @Override
-    public ITexture[][][] getTextureSet(ITexture[] aTextures) {
-        return new ITexture[0][0][0];
-    }
-
-    @Override
-    public void setItemNBT(NBTTagCompound aNBT) {
-        if (!voidBreak) {
-            if (mFluid != null && mFluid.amount > 0)
-                aNBT.setTag("mFluid", mFluid.writeToNBT(new NBTTagCompound()));
-        }
-        super.setItemNBT(aNBT);
-    }
-
-    @Override
-    public boolean isSimpleMachine() {
-        return true;
-    }
-
-    @Override
-    public boolean doesFillContainers() {
-        return true;
-    }
-
-    @Override
-    public boolean doesEmptyContainers() {
-        return true;
-    }
-
-    @Override
-    public boolean canTankBeFilled() {
-        return true;
-    }
-
-    @Override
-    public boolean canTankBeEmptied() {
-        return true;
-    }
-
-    @Override
-    public boolean displaysItemStack() {
-        return true;
-    }
-
-    @Override
-    public boolean displaysStackSize() {
-        return false;
+        super(aName, aTier, aDescription, aTextures);
     }
 
     @Override
     public MetaTileEntity newMetaEntity(IGregTechTileEntity aTileEntity) {
         return new GT_MetaTileEntity_QuantumTank(mName, mTier, mDescriptionArray, mTextures);
-    }
-
-    @Override
-    public ITexture[] getTexture(IGregTechTileEntity aBaseMetaTileEntity, byte aSide, byte aFacing, byte aColorIndex, boolean aActive, boolean aRedstone) {
-        if (aSide != ForgeDirection.UP.ordinal()) return new ITexture[]{MACHINE_CASINGS[mTier][aColorIndex + 1]};
-        return new ITexture[]{
-                MACHINE_CASINGS[mTier][aColorIndex + 1],
-                TextureFactory.of(OVERLAY_QTANK),
-                TextureFactory.builder().addIcon(OVERLAY_QTANK_GLOW).glow().build()
-        };
-    }
-
-    @Override
-    public boolean onRightclick(IGregTechTileEntity aBaseMetaTileEntity, EntityPlayer aPlayer) {
-        if (!aBaseMetaTileEntity.isClientSide()) aBaseMetaTileEntity.openGUI(aPlayer);
-        return true;
-    }
-
-    @Override
-    public void onLeftclick(IGregTechTileEntity aBaseMetaTileEntity, EntityPlayer aPlayer) {
-        voidBreak = aPlayer.isSneaking();
-        super.onLeftclick(aBaseMetaTileEntity, aPlayer);
-    }
-
-    @Override
-    public final byte getUpdateData() {
-        return 0x00;
-    }
-
-    @Override
-    public boolean isFacingValid(byte aFacing) {
-        return true;
-    }
-
-    @Override
-    public boolean isAccessAllowed(EntityPlayer aPlayer) {
-        return true;
-    }
-
-    @Override
-    public int getTankPressure() {
-        return 100;
-    }
-
-    @Override
-    public int getCapacity() {
-        return commonSizeCompute(mTier);
-    }
-
-    @Override
-    public boolean isGivingInformation() {
-        return true;
     }
 
     @Override
