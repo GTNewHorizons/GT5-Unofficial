e0cd8e5078a7710a4d2b94caf8fde4666a79074a
Yang Xizhi <60341015+GlodBlock@users.noreply.github.com>
2022-06-10 17:13:18 +0800

    correct tooltip (#45)

--- src/main/java/goodgenerator/blocks/tileEntity/PreciseAssembler.java
+++ src/main/java/goodgenerator/blocks/tileEntity/PreciseAssembler.java
@@ -342,7 +342,7 @@ public class PreciseAssembler extends GT_MetaTileEntity_TooltipMultiBlockBase_EM
                 .addInfo("UHV Machine Casing will unlock all voltage.")
                 .addInfo("Precise Electronic Unit Casing won't limit recipe in Normal Mode.")
                 .addInfo("But gives more parallel with more advanced one.")
-                .addInfo("It is 50% faster in Normal Mode.")
+                .addInfo("It is 100% faster in Normal Mode.")
                 .addInfo("MK-I = 32x, MK-II = 64x, MK-III = 128x")
                 .addPollutionAmount(getPollutionPerSecond(null))
                 .addInfo("The structure is too complex!")
