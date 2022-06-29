16e557ca13d66caf486344f9f736f2e2ac93df3e
GTNH-Colen <54497873+GTNH-Colen@users.noreply.github.com>
2022-06-29 18:19:04 +0100

    Fix incorrect colouring on text.

--- src/main/java/gregtech/api/util/GT_Utility.java
+++ src/main/java/gregtech/api/util/GT_Utility.java
@@ -2283,10 +2283,10 @@ public class GT_Utility {
             try {
                 if (tTileEntity instanceof IMachineProgress) {
                     if (((IMachineProgress) tTileEntity).isAllowedToWork()) {
-                        tList.add("Disabled." + EnumChatFormatting.RED + EnumChatFormatting.RESET);
+                        tList.add(EnumChatFormatting.RED + "Disabled." + EnumChatFormatting.RESET);
                     }
                     if (((IMachineProgress) tTileEntity).wasShutdown()) {
-                        tList.add("Shut down due to power loss." + EnumChatFormatting.RED + EnumChatFormatting.RESET);
+                        tList.add(EnumChatFormatting.RED + "Shut down due to power loss." + EnumChatFormatting.RESET);
                     }
                     rEUAmount += 400;
                     int tValue = 0;
