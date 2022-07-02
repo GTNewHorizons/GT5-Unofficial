c90850e18cb51f6398456cd1b8b6c3b646c5c4f1
miozune <miozune@gmail.com>
2022-07-02 16:18:50 +0900

    Add IMC for NEI RecipeCatalyst

--- src/main/java/gregtech/loaders/preload/GT_Loader_Item_Block_And_Fluid.java
+++ src/main/java/gregtech/loaders/preload/GT_Loader_Item_Block_And_Fluid.java
@@ -794,34 +794,6 @@ public class GT_Loader_Item_Block_And_Fluid implements Runnable {
         GT_OreDictUnificator.set(OrePrefixes.dust, Materials.Cocoa, GT_ModHandler.getModItem("harvestcraft", "cocoapowderItem", 1L, 0));
         GT_OreDictUnificator.set(OrePrefixes.dust, Materials.Coffee, ItemList.IC2_CoffeePowder.get(1L, new Object[0]));
 
-    {
-        NBTTagCompound aNBT = new NBTTagCompound();
-            aNBT.setString("handler","gt.recipe.plasmaforge");
-            aNBT.setString("modName","GregTech");
-            aNBT.setString("modId","gregtech");
-            aNBT.setBoolean("modRequired",true);
-            aNBT.setString("itemName","gregtech:gt.blockmachines:1004");
-            aNBT.setInteger("handlerHeight",135);
-            aNBT.setInteger("handlerWidth",166);
-            aNBT.setInteger("maxRecipesPerPage",2);
-            aNBT.setInteger("yShift",6);
-            FMLInterModComms.sendMessage("NotEnoughItems","registerHandlerInfo",aNBT);
-    }
-
-    {
-        NBTTagCompound aNBT = new NBTTagCompound();
-        aNBT.setString("handler","gt.recipe.complexfusionreactor");
-        aNBT.setString("modName","GregTech");
-        aNBT.setString("modId","gregtech");
-        aNBT.setBoolean("modRequired",true);
-        aNBT.setString("itemName","gregtech:gt.blockmachines:1193");
-        aNBT.setInteger("handlerHeight",135);
-        aNBT.setInteger("handlerWidth",166);
-        aNBT.setInteger("maxRecipesPerPage",2);
-        aNBT.setInteger("yShift",6);
-        FMLInterModComms.sendMessage("NotEnoughItems","registerHandlerInfo",aNBT);
-    }
-
 
         //TODO ADD LATER @Technus why it crash if enable?
         //FluidContainerRegistry.registerFluidContainer(new FluidContainerRegistry.FluidContainerData(Materials.Naquadah.getMolten(1000L), GT_OreDictUnificator.get(OrePrefixes.cell, Materials.Naquadah, 1L), GT_OreDictUnificator.get(OrePrefixes.cell, Materials.Empty, 1L)));
