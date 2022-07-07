11dd4a177bddcc3a82e614e83b46a9379addecdb
GlodBlock <1356392126@qq.com>
2022-07-07 13:03:52 +0800

    kevlar line

--- src/main/java/gregtech/api/enums/Materials.java
+++ src/main/java/gregtech/api/enums/Materials.java
@@ -1637,6 +1637,8 @@ public class Materials implements IColorModulationContainer, ISubTagContainer {
         MaterialsKevlar.LiquidCrystalKevlar.mChemicalFormula = "[-CO-C\u2086H\u2084-CO-NH-C\u2086H\u2084-NH-]n";
         MaterialsKevlar.RhodiumChloride.mChemicalFormula = "RhCl\u2083";
         MaterialsKevlar.OrganorhodiumCatalyst.mChemicalFormula = "RhHCO(P(C\u2086H\u2085)\u2083)\u2083";
+        MaterialsKevlar.CobaltIINitrate.mChemicalFormula = "Co(NO\u2083)\u2082";
+        MaterialsKevlar.CobaltIIHydroxide.mChemicalFormula = "Co(OH)\u2082";
         SiliconSG.mChemicalFormula = "SIsg";
         NetherQuartz.mChemicalFormula = "SIO\u2082";
         Quartzite.mChemicalFormula = "SIO\u2082";
