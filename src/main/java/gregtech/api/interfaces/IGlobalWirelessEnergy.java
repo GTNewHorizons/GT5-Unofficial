643a6268e1a9f0cb44a7a114c47c6ba94d4f8716
GTNH-Colen <54497873+GTNH-Colen@users.noreply.github.com>
2022-07-20 04:45:25 +0100

    Add default method

--- src/main/java/gregtech/api/interfaces/IGlobalWirelessEnergy.java
+++ src/main/java/gregtech/api/interfaces/IGlobalWirelessEnergy.java
@@ -1,7 +1,44 @@
 package gregtech.api.interfaces;
 
+import net.minecraft.world.World;
+
+import java.io.File;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 
+// If you are adding very late-game content feel free to tap into this interface.
+// The eventual goal is to bypass laser/dynamo stuff and have energy deposited directly from ultra-endgame
+// multi-blocks directly into the users network.
 public interface IGlobalWirelessEnergy {
-    HashMap<String, Long> GlobalEnergyMap = new HashMap<>(100, 0.9f);
+    HashMap<String, BigInteger> GlobalEnergyMap = new HashMap<>(100, 0.9f);
+    String GlobalEnergyMapFileName = "GlobalEnergyMap";
+
+    default void loadGlobalEnergyMap(World world) {
+        try {
+            System.out.println("TEST1321KDW");
+            Path path = Paths.get("./saves/" + world.getWorldInfo().getWorldName() + "/" + GlobalEnergyMapFileName + ".txt").toAbsolutePath();
+            System.out.println("DWMKDOAKW");
+
+            String[] data;
+            for(String line : Files.readAllLines(path)) {
+                data = line.split(":");
+
+                String UUID = data[0];
+                BigInteger num = new BigInteger(data[1]);
+
+                GlobalEnergyMap.put(UUID, num);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e) {
+            System.out.println("TEST39129JE");
+        }
+    };
+
 }
+
+
