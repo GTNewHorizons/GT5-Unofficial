d972393f858cfc079ab8e3266a7f1bca8e54b782
minecraft7771 <maxim235@gmx.de>
2022-07-23 19:02:01 +0200

    Reverted accidental format change

--- src/main/java/com/github/technus/tectech/thing/metaTileEntity/hatch/gui/GT_Container_ParamAdv.java
+++ src/main/java/com/github/technus/tectech/thing/metaTileEntity/hatch/gui/GT_Container_ParamAdv.java
@@ -243,7 +243,7 @@ public class GT_Container_ParamAdv extends GT_ContainerMetaTile_Machine {
     @Override
     @SideOnly(Side.CLIENT)
     public void updateProgressBar(int par1, int par2) {
-            super.updateProgressBar(par1, par2);
+        super.updateProgressBar(par1, par2);
         switch (par1) {
             case 100:
             case 101:
