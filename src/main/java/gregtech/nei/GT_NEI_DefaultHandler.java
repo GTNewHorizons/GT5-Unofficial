2bc0f89fc934306bdf9e351263e2d9a0709aa111
miozune <miozune@gmail.com>
2022-08-08 18:08:49 +0900

    Fix original voltage shows up when voltage is "unspecified"

--- src/main/java/gregtech/nei/GT_NEI_DefaultHandler.java
+++ src/main/java/gregtech/nei/GT_NEI_DefaultHandler.java
@@ -376,8 +376,11 @@ public class GT_NEI_DefaultHandler extends RecipeMapHandler {
                 if (GT_Mod.gregtechproxy.mNEIOriginalVoltage) {
                     Power originalPower = getPowerFromRecipeMap();
                     originalPower.computePowerUsageAndDuration(recipe.mEUt, recipe.mDuration);
-                    drawLine(lineCounter, GT_Utility.trans("228", "Original voltage: ") + originalPower.getVoltageString());
-                    lineCounter++;
+                    String powerVoltage = originalPower.getVoltageString();
+                    if (!powerVoltage.equals("unspecified")) {
+                        drawLine(lineCounter, GT_Utility.trans("228", "Original voltage: ") + powerVoltage);
+                        lineCounter++;
+                    }
                 }
                 if (amperage != null && !amperage.equals("unspecified") && !amperage.equals("1")) {
                     drawLine(lineCounter, GT_Utility.trans("155", "Amperage: ") + amperage);
