73cc63047b7c8acc315b1501f8d2f15fb04acea4
Glease <4586901+Glease@users.noreply.github.com>
2022-08-09 17:17:49 +0800

    loosen the generic restrictions on hatch elements
    It honestly never occurred to me that SOMEONE would not derive from GT_MetaTileEntity_EnhancedMultiblockBase

--- src/main/java/gregtech/api/enums/GT_HatchElement.java
+++ src/main/java/gregtech/api/enums/GT_HatchElement.java
@@ -19,52 +19,52 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public enum GT_HatchElement implements IHatchElement<GT_MetaTileEntity_EnhancedMultiBlockBase<?>> {
+public enum GT_HatchElement implements IHatchElement<GT_MetaTileEntity_MultiBlockBase> {
     Muffler(GT_MetaTileEntity_MultiBlockBase::addMufflerToMachineList, GT_MetaTileEntity_Hatch_Muffler.class) {
         @Override
-        public long count(GT_MetaTileEntity_EnhancedMultiBlockBase<?> t) {
+        public long count(GT_MetaTileEntity_MultiBlockBase t) {
             return t.mMufflerHatches.size();
         }
     },
     Maintenance(GT_MetaTileEntity_MultiBlockBase::addMaintenanceToMachineList, GT_MetaTileEntity_Hatch_Maintenance.class) {
         @Override
-        public long count(GT_MetaTileEntity_EnhancedMultiBlockBase<?> t) {
+        public long count(GT_MetaTileEntity_MultiBlockBase t) {
             return t.mMaintenanceHatches.size();
         }
     },
     InputHatch(GT_MetaTileEntity_MultiBlockBase::addInputHatchToMachineList, GT_MetaTileEntity_Hatch_Input.class) {
         @Override
-        public long count(GT_MetaTileEntity_EnhancedMultiBlockBase<?> t) {
+        public long count(GT_MetaTileEntity_MultiBlockBase t) {
             return t.mInputHatches.size();
         }
     },
     InputBus(GT_MetaTileEntity_MultiBlockBase::addInputBusToMachineList, GT_MetaTileEntity_Hatch_InputBus.class) {
         @Override
-        public long count(GT_MetaTileEntity_EnhancedMultiBlockBase<?> t) {
+        public long count(GT_MetaTileEntity_MultiBlockBase t) {
             return t.mInputBusses.size();
         }
     },
     OutputHatch(GT_MetaTileEntity_MultiBlockBase::addOutputHatchToMachineList, GT_MetaTileEntity_Hatch_Output.class) {
         @Override
-        public long count(GT_MetaTileEntity_EnhancedMultiBlockBase<?> t) {
+        public long count(GT_MetaTileEntity_MultiBlockBase t) {
             return t.mOutputHatches.size();
         }
     },
     OutputBus(GT_MetaTileEntity_MultiBlockBase::addOutputBusToMachineList, GT_MetaTileEntity_Hatch_OutputBus.class) {
         @Override
-        public long count(GT_MetaTileEntity_EnhancedMultiBlockBase<?> t) {
+        public long count(GT_MetaTileEntity_MultiBlockBase t) {
             return t.mOutputBusses.size();
         }
     },
     Energy(GT_MetaTileEntity_MultiBlockBase::addEnergyInputToMachineList, GT_MetaTileEntity_Hatch_Energy.class) {
         @Override
-        public long count(GT_MetaTileEntity_EnhancedMultiBlockBase<?> t) {
+        public long count(GT_MetaTileEntity_MultiBlockBase t) {
             return t.mEnergyHatches.size();
         }
     },
     Dynamo(GT_MetaTileEntity_MultiBlockBase::addDynamoToMachineList, GT_MetaTileEntity_Hatch_Dynamo.class) {
         @Override
-        public long count(GT_MetaTileEntity_EnhancedMultiBlockBase<?> t) {
+        public long count(GT_MetaTileEntity_MultiBlockBase t) {
             return t.mDynamoHatches.size();
         }
     },
@@ -75,16 +75,16 @@ public enum GT_HatchElement implements IHatchElement<GT_MetaTileEntity_EnhancedM
         }
 
         @Override
-        public long count(GT_MetaTileEntity_EnhancedMultiBlockBase<?> t) {
+        public long count(GT_MetaTileEntity_MultiBlockBase t) {
             return t.getExoticEnergyHatches().size();
         }
     },
     ;
     private final List<Class<? extends IMetaTileEntity>> mteClasses;
-    private final IGT_HatchAdder<GT_MetaTileEntity_EnhancedMultiBlockBase<?>> adder;
+    private final IGT_HatchAdder<GT_MetaTileEntity_MultiBlockBase> adder;
 
     @SafeVarargs
-    GT_HatchElement(IGT_HatchAdder<GT_MetaTileEntity_EnhancedMultiBlockBase<?>> adder, Class<? extends IMetaTileEntity>... mteClasses) {
+    GT_HatchElement(IGT_HatchAdder<GT_MetaTileEntity_MultiBlockBase> adder, Class<? extends IMetaTileEntity>... mteClasses) {
         this.mteClasses = Collections.unmodifiableList(Arrays.asList(mteClasses));
         this.adder = adder;
     }
@@ -94,7 +94,7 @@ public enum GT_HatchElement implements IHatchElement<GT_MetaTileEntity_EnhancedM
         return mteClasses;
     }
 
-    public IGT_HatchAdder<? super GT_MetaTileEntity_EnhancedMultiBlockBase<?>> adder() {
+    public IGT_HatchAdder<? super GT_MetaTileEntity_MultiBlockBase> adder() {
         return adder;
     }
 }
