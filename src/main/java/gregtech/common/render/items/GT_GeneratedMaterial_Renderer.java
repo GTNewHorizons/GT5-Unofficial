6ef0db4cc3f637b2427f16c6b551098850372f46
Vladislav Laetansky <vlaetansky@gmail.com>
2022-08-19 12:36:28 +0300

    Properly render overlay

--- src/main/java/gregtech/common/render/items/GT_GeneratedMaterial_Renderer.java
+++ src/main/java/gregtech/common/render/items/GT_GeneratedMaterial_Renderer.java
@@ -1,8 +1,6 @@
 package gregtech.common.render.items;
 
-import gregtech.api.interfaces.IContainerMaterialRenderer;
 import gregtech.api.interfaces.IIconContainer;
-import gregtech.api.interfaces.IRegularMaterialRenderer;
 import gregtech.api.items.GT_MetaGenerated_Item;
 import gregtech.api.util.GT_Utility;
 import gregtech.common.render.GT_RenderUtil;
@@ -16,7 +14,7 @@ import net.minecraftforge.client.IItemRenderer;
 import net.minecraftforge.fluids.FluidStack;
 import org.lwjgl.opengl.GL11;
 
-public class GT_GeneratedMaterial_Renderer implements IItemRenderer, IRegularMaterialRenderer, IContainerMaterialRenderer {
+public class GT_GeneratedMaterial_Renderer implements IItemRenderer {
 
     @Override
     public boolean handleRenderType(ItemStack item, ItemRenderType type) {
@@ -32,7 +30,7 @@ public class GT_GeneratedMaterial_Renderer implements IItemRenderer, IRegularMat
     }
 
     @Override
-    public final void renderItem(ItemRenderType type, ItemStack aStack, Object... data) {
+    public void renderItem(ItemRenderType type, ItemStack aStack, Object... data) {
         short aMetaData = (short) aStack.getItemDamage();
         GT_MetaGenerated_Item aItem = (GT_MetaGenerated_Item) aStack.getItem();
 
@@ -46,47 +44,53 @@ public class GT_GeneratedMaterial_Renderer implements IItemRenderer, IRegularMat
         IIcon tOverlay = aIconContainer.getOverlayIcon();
 
         if (tIcon != null) {
-            renderRegularItem(type, aStack, tIcon, tOverlay);
+            renderRegularItem(type, aStack, tIcon);
         }
 
         FluidStack aFluid = GT_Utility.getFluidForFilledItem(aStack, true);
         if (tOverlay != null && aFluid != null && aFluid.getFluid() != null) {
             IIcon fluidIcon = aFluid.getFluid().getIcon(aFluid);
             if (fluidIcon != null) {
-                renderContainedFluid(type, aFluid, fluidIcon, tOverlay);
+                renderContainedFluid(type, aFluid, fluidIcon);
+            }
+        }
+
+        if (tOverlay != null) {
+            GL11.glColor3f(1.0F, 1.0F, 1.0F);
+            Minecraft.getMinecraft().renderEngine.bindTexture(TextureMap.locationItemsTexture);
+            if (type.equals(IItemRenderer.ItemRenderType.INVENTORY)) {
+                GT_RenderUtil.renderItemIcon(tOverlay, 16.0D, 0.001D, 0.0F, 0.0F, -1.0F);
+            } else {
+                ItemRenderer.renderItemIn2D(Tessellator.instance, tOverlay.getMaxU(), tOverlay.getMinV(), tOverlay.getMinU(), tOverlay.getMaxV(), tOverlay.getIconWidth(), tOverlay.getIconHeight(), 0.0625F);
             }
         }
     }
 
-    public void renderRegularItem(ItemRenderType type, ItemStack aStack, IIcon icon, IIcon overlay) {
+    public void renderRegularItem(ItemRenderType type, ItemStack aStack, IIcon icon) {
         GT_MetaGenerated_Item aItem = (GT_MetaGenerated_Item) aStack.getItem();
 
         enableBlendingItemTexture();
 
         short[] tModulation = aItem.getRGBa(aStack);
         GL11.glColor3f(tModulation[0] / 255.0F, tModulation[1] / 255.0F, tModulation[2] / 255.0F);
+        Minecraft.getMinecraft().renderEngine.bindTexture(TextureMap.locationItemsTexture);
 
         if (type.equals(IItemRenderer.ItemRenderType.INVENTORY)) {
             GT_RenderUtil.renderItemIcon(icon, 16.0D, 0.001D, 0.0F, 0.0F, -1.0F);
-            if (overlay != null) {
-                GT_RenderUtil.renderItemIcon(overlay, 16.0D, 0.001D, 0.0F, 0.0F, -1.0F);
-            }
         } else {
             ItemRenderer.renderItemIn2D(Tessellator.instance, icon.getMaxU(), icon.getMinV(), icon.getMinU(), icon.getMaxV(), icon.getIconWidth(), icon.getIconHeight(), 0.0625F);
-            if (overlay != null) {
-                ItemRenderer.renderItemIn2D(Tessellator.instance, overlay.getMaxU(), overlay.getMinV(), overlay.getMinU(), overlay.getMaxV(), overlay.getIconWidth(), overlay.getIconHeight(), 0.0625F);
-            }
         }
 
         GL11.glDisable(GL11.GL_BLEND);
     }
 
-    public void renderContainedFluid(ItemRenderType type, FluidStack tFluid, IIcon fluidIcon, IIcon overlay) {
+    public void renderContainedFluid(ItemRenderType type, FluidStack tFluid, IIcon fluidIcon) {
         enableBlendingItemTexture();
 
         int tColor = tFluid.getFluid().getColor(tFluid);
         GL11.glColor3f((tColor >> 16 & 0xFF) / 255.0F, (tColor >> 8 & 0xFF) / 255.0F, (tColor & 0xFF) / 255.0F);
         Minecraft.getMinecraft().renderEngine.bindTexture(TextureMap.locationBlocksTexture);
+
         GL11.glDepthFunc(GL11.GL_EQUAL);
         if (type.equals(IItemRenderer.ItemRenderType.INVENTORY)) {
             GT_RenderUtil.renderItemIcon(fluidIcon, 16.0D, 0.001D, 0.0F, 0.0F, -1.0F);
@@ -95,20 +99,11 @@ public class GT_GeneratedMaterial_Renderer implements IItemRenderer, IRegularMat
         }
         GL11.glDepthFunc(GL11.GL_LEQUAL);
 
-        GL11.glColor3f(1.0F, 1.0F, 1.0F);
-        Minecraft.getMinecraft().renderEngine.bindTexture(TextureMap.locationItemsTexture);
-        if (type.equals(IItemRenderer.ItemRenderType.INVENTORY)) {
-            GT_RenderUtil.renderItemIcon(overlay, 16.0D, 0.001D, 0.0F, 0.0F, -1.0F);
-        } else {
-            ItemRenderer.renderItemIn2D(Tessellator.instance, overlay.getMaxU(), overlay.getMinV(), overlay.getMinU(), overlay.getMaxV(), overlay.getIconWidth(), overlay.getIconHeight(), 0.0625F);
-        }
-
         GL11.glDisable(GL11.GL_BLEND);
     }
 
     protected void enableBlendingItemTexture() {
         GL11.glEnable(GL11.GL_BLEND);
-        Minecraft.getMinecraft().renderEngine.bindTexture(TextureMap.locationItemsTexture);
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
         GL11.glEnable(GL11.GL_ALPHA_TEST);
         GL11.glColor3f(1.0F, 1.0F, 1.0F);
