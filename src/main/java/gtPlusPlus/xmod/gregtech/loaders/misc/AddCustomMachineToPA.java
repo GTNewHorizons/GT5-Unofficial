74f5f3b087f1549fb6dac4f18557f2c16406dee3
BlueWeabo <76872108+BlueWeabo@users.noreply.github.com>
2022-08-19 12:56:34 +0300

    GTpp machines pa fix (#283)
    * Add Dehydrator and use new PA code for recipe map
    
    * removed dehydrator from the PA

--- src/main/java/gtPlusPlus/xmod/gregtech/loaders/misc/AddCustomMachineToPA.java
+++ src/main/java/gtPlusPlus/xmod/gregtech/loaders/misc/AddCustomMachineToPA.java
@@ -6,22 +6,10 @@ import gregtech.api.util.GT_Recipe.GT_Recipe_Map;
 import gtPlusPlus.api.objects.Logger;
 
 public class AddCustomMachineToPA {
-
-	public static final void registerRecipeMapForID(int aID, GT_Recipe_Map aMap) {
-		Logger.INFO("Attempting to add map "+aMap.mNEIName+" to Processing Array for Meta Tile "+aID+". Success? "+GT_ProcessingArray_Manager.registerRecipeMapForMeta(aID, aMap));
-	}
-
-	public static final void registerRecipeMapBetweenRangeOfIDs(int aMin, int aMax, GT_Recipe_Map aMap) {
-		for (int i=aMin; i<=aMax;i++) {
-			registerRecipeMapForID(i, aMap);
-		}	
-	}
-
 	public static void register() {
 
 		// Simple Washers
-		registerRecipeMapForID(767, GTPP_Recipe.GTPP_Recipe_Map.sSimpleWasherRecipes);
-		registerRecipeMapBetweenRangeOfIDs(31017, 31020, GTPP_Recipe.GTPP_Recipe_Map.sSimpleWasherRecipes);
+		GT_ProcessingArray_Manager.addRecipeMapToPA("simplewasher.01", GTPP_Recipe.GTPP_Recipe_Map.sSimpleWasherRecipes);
 
 	}
 
