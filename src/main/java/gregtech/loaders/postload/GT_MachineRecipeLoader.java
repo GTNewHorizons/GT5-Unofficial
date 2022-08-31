4f363a1ff8b8685d9e152232c299b8de527be0fc
Martin Robertz <dream-master@gmx.net>
2022-08-31 14:58:20 +0200

    use fluid heater recipe without input fluid

--- src/main/java/gregtech/loaders/postload/GT_MachineRecipeLoader.java
+++ src/main/java/gregtech/loaders/postload/GT_MachineRecipeLoader.java
@@ -14542,7 +14542,7 @@ public class GT_MachineRecipeLoader implements Runnable {
                 480);
         // Fluid Sodium
         GT_Values.RA.addFluidHeaterRecipe(
-                Materials.Sodium.getDust(1), GT_Values.NF, Materials.Sodium.getFluid(1000), 200, 120);
+                Materials.Sodium.getDust(1), Materials.Sodium.getFluid(1000), 200, 120);
 
         // 2CH3COOH = CH3COCH3 + CO2 + H
         GT_Values.RA.addMultiblockChemicalRecipe(
