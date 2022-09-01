69a2c7fa2952f843210e444a331ddc166df25479
Glease <4586901+Glease@users.noreply.github.com>
2022-09-01 12:53:49 +0800

    fix BlockIcons declaration

--- src/main/java/net/glease/ggfab/BlockIcons.java
+++ src/main/java/net/glease/ggfab/BlockIcons.java
@@ -1,30 +1,37 @@
 package net.glease.ggfab;
 
-import gregtech.api.enums.Textures.BlockIcons.CustomIcon;
+import gregtech.api.GregTech_API;
 import gregtech.api.interfaces.IIconContainer;
+import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.util.IIcon;
 import net.minecraft.util.ResourceLocation;
 
-public enum BlockIcons implements IIconContainer {
+public enum BlockIcons implements IIconContainer, Runnable {
     ;
-    private final CustomIcon backing;
+    public static final String RES_PATH = GGConstants.MODID + ":";
+    private IIcon mIcon;
 
     BlockIcons() {
-        backing = new CustomIcon("iconsets/" + name());
+        GregTech_API.sGTBlockIconload.add(this);
     }
 
     @Override
     public IIcon getIcon() {
-        return backing.getIcon();
+        return mIcon;
     }
 
     @Override
     public IIcon getOverlayIcon() {
-        return backing.getOverlayIcon();
+        return null;
     }
 
     @Override
     public ResourceLocation getTextureFile() {
-        return backing.getTextureFile();
+        return TextureMap.locationBlocksTexture;
+    }
+
+    @Override
+    public void run() {
+        mIcon = GregTech_API.sBlockIcons.registerIcon(RES_PATH + "iconsets/" + this);
     }
 }
