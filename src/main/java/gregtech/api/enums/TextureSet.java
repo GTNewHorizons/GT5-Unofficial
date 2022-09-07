aa094aee4db85c9a08c7ee4054f14459c4ce4cc1
Glease <4586901+Glease@users.noreply.github.com>
2022-09-07 17:20:57 +0800

    revert to spritesheet for block textures for gaia spirit (#1351)
    * revert to spritesheet for block textures for gaia spirit
    
    also removed now-unused transcendent metal block textures
    
    * Spotless apply for branch fix/animated-material-texture-fix for #1351 (#1352)
    
    Co-authored-by: Glease <4586901+Glease@users.noreply.github.com>
    Co-authored-by: GitHub GTNH Actions <>
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/gregtech/api/enums/TextureSet.java
+++ src/main/java/gregtech/api/enums/TextureSet.java
@@ -187,4 +187,30 @@ public class TextureSet {
         this("CUSTOM/" + aSetName);
         this.is_custom = is_custom;
     }
+
+    /**
+     * Construct a TextureSet that will delegate some of its textures to the origin TextureSet.
+     *
+     * This assumes you want to construct a custom texture set.
+     */
+    private TextureSet(String aSetName, TextureSet origin, boolean overrideBlock, boolean overrideItem) {
+        this("CUSTOM/" + aSetName);
+        this.is_custom = true;
+
+        for (int i = 0; i < mTextures.length; i++) {
+            if (mTextures[i] instanceof Textures.ItemIcons.CustomIcon) {
+                if (!overrideItem) {
+                    mTextures[i] = origin.mTextures[i];
+                }
+            } else if (mTextures[i] instanceof Textures.BlockIcons.CustomIcon) {
+                if (!overrideBlock) {
+                    mTextures[i] = origin.mTextures[i];
+                }
+            }
+        }
+    }
+
+    public TextureSet withBlockTextures(String aNewSetName) {
+        return new TextureSet(aNewSetName, this, true, false);
+    }
 }
