2d7883310272e08b18bf020bb4a2302f0ec94593
Vladislav Laetansky <vlaetansky@gmail.com>
2022-09-10 14:31:30 +0300

    Address review

--- src/main/java/gregtech/api/util/GT_Waila.java
+++ src/main/java/gregtech/api/util/GT_Waila.java
@@ -1,22 +1,20 @@
 package gregtech.api.util;
 
-import joptsimple.internal.Strings;
-
 public abstract class GT_Waila {
 
     public static String getMachineProgressString(boolean isActive, int maxProgress, int progress) {
-        StringBuilder stringBuilder = new StringBuilder(Strings.EMPTY);
+        final String result;
 
         if (isActive) {
-            if (maxProgress > 20) {
-                stringBuilder.append(String.format("Progress: %d s / %d s", progress / 20, maxProgress / 20));
+            if (maxProgress < 20) {
+                result = String.format("Progress: %d ticks remaining", maxProgress);
             } else {
-                stringBuilder.append(String.format("Progress: %d ticks remaining", maxProgress));
+                result = String.format("Progress: %d s / %d s", progress / 20, maxProgress / 20);
             }
         } else {
-            stringBuilder.append("Idle");
+            result = "Idle";
         }
 
-        return stringBuilder.toString();
+        return result;
     }
 }
