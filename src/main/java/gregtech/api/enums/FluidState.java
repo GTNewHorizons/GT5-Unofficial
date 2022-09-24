dd8ca2db2c551224b6fcc8812a9121f6e3a83398
Léa Gris <lea.gris@noiraude.net>
2022-09-24 23:37:56 +0200

    hotfix off-by-one in FluidState.fromValue

--- src/main/java/gregtech/api/enums/FluidState.java
+++ src/main/java/gregtech/api/enums/FluidState.java
@@ -10,7 +10,7 @@ public enum FluidState {
     public static final FluidState[] VALID_STATES = new FluidState[] {SLURRY, LIQUID, GAS, PLASMA, MOLTEN};
 
     public static FluidState fromValue(int stateValue) {
-        return stateValue > 0 && stateValue < FluidState.VALID_STATES.length
+        return stateValue >= 0 && stateValue < FluidState.VALID_STATES.length
                 ? FluidState.VALID_STATES[stateValue]
                 : FluidState.LIQUID;
     }
