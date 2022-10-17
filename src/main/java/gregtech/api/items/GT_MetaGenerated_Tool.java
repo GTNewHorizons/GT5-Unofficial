bc463793934ad1d9c32c34f37c40e66a5107ebed
Glease <4586901+Glease@users.noreply.github.com>
2022-10-17 17:38:59 +0800

    add PR screwdriver compat to GT screwdriver (#1475)
    * add PR screwdriver compat to GT screwdriver
    
    * Spotless apply for branch feature/pr-screwdriver for #1475 (#1476)
    
    Co-authored-by: Glease <4586901+Glease@users.noreply.github.com>
    Co-authored-by: GitHub GTNH Actions <>
    
    * make it optional
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/gregtech/api/items/GT_MetaGenerated_Tool.java
+++ src/main/java/gregtech/api/items/GT_MetaGenerated_Tool.java
@@ -28,6 +28,7 @@ import java.util.List;
 import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
 import mods.railcraft.api.core.items.IToolCrowbar;
+import mrtjp.projectred.api.IScrewdriver;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
@@ -63,10 +64,11 @@ import net.minecraftforge.event.world.BlockEvent;
             @Optional.Interface(iface = "forestry.api.arboriculture.IToolGrafter", modid = "ForestryAPI|arboriculture"),
             @Optional.Interface(iface = "mods.railcraft.api.core.items.IToolCrowbar", modid = "RailcraftAPI|items"),
             @Optional.Interface(iface = "buildcraft.api.tools.IToolWrench", modid = "BuildCraftAPI|tools"),
-            @Optional.Interface(iface = "crazypants.enderio.api.tool.ITool", modid = "EnderIOAPI|Tools")
+            @Optional.Interface(iface = "crazypants.enderio.api.tool.ITool", modid = "EnderIOAPI|Tools"),
+            @Optional.Interface(iface = "mrtjp.projectred.api.IScrewdriver", modid = "ProjRed|Core"),
         })
 public abstract class GT_MetaGenerated_Tool extends GT_MetaBase_Item
-        implements IDamagableItem, IToolGrafter, IToolCrowbar, IToolWrench, ITool {
+        implements IDamagableItem, IToolGrafter, IToolCrowbar, IToolWrench, ITool, IScrewdriver {
     /**
      * All instances of this Item Class are listed here.
      * This gets used to register the Renderer to all Items of this Type, if useStandardMetaItemRenderer() returns true.
@@ -816,6 +818,23 @@ public abstract class GT_MetaGenerated_Tool extends GT_MetaBase_Item
     @Override
     public boolean canUse(ItemStack stack, EntityPlayer player, int x, int y, int z) {
         return canWrench(player, x, y, z);
+    }
+
+    // ProjectRed screwdriver
+    @Override
+    public boolean canUse(EntityPlayer player, ItemStack stack) {
+        if (player == null) return false;
+        if (GT_Utility.isStackInvalid(stack) || !isItemStackUsable(stack)) return false;
+        IToolStats tStats = getToolStats(stack);
+        return tStats != null && tStats.isScrewdriver();
+    }
+
+    @Override
+    public void damageScrewdriver(EntityPlayer player, ItemStack stack) {
+        if (player == null) return;
+        if (GT_Utility.isStackInvalid(stack) || !isItemStackUsable(stack)) return;
+        IToolStats tStats = getToolStats(stack);
+        if (tStats != null) doDamage(stack, tStats.getToolDamagePerEntityAttack());
     }
 
     @Override
