672eefc229b38f5649b8b75f53bfae89e8fe6991
Yang Xizhi <60341015+GlodBlock@users.noreply.github.com>
2022-10-23 01:53:31 +0800

    ore line for some useless ore (#1210)
    * oreline
    
    * ilmenite
    
    * update BS+SA
    
    * Revert "update BS+SA"
    
    This reverts commit 00c0d26b5d4963a42c7cd6721cc9444fac8fd2c2.
    
    * add missing import
    
    * garnet
    
    * Spotless apply for branch ore for #1210 (#1481)
    
    * Remove convert gendustry bees code (#1187)
    
    * Remove convert gendustry bees code
    
    * fix derp. run BS update
    
    Co-authored-by: Martin Robertz <dream-master@gmx.net>
    
    * Added check to API if the block should drop its inventory on break (#1479)
    
    * Added check to API if the block should drop its inventory on break
    
    * Apply spotless
    
    * Changed should drop flag to take index argument
    
    * spotlessApply
    
    Co-authored-by: DianeXD <64360468+DianeXD@users.noreply.github.com>
    Co-authored-by: Martin Robertz <dream-master@gmx.net>
    Co-authored-by: Maxim <maxim235@gmx.de>
    Co-authored-by: Yang Xizhi <60341015+GlodBlock@users.noreply.github.com>
    Co-authored-by: GitHub GTNH Actions <>
    
    * green vitriol recipe
    
    Co-authored-by: Martin Robertz <dream-master@gmx.net>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: DianeXD <64360468+DianeXD@users.noreply.github.com>
    Co-authored-by: Maxim <maxim235@gmx.de>

--- src/main/java/gregtech/api/enums/ItemList.java
+++ src/main/java/gregtech/api/enums/ItemList.java
@@ -2090,6 +2090,7 @@ public enum ItemList implements IItemContainer {
             sDrillingFluid,
             sBlueVitriol,
             sNickelSulfate,
+            sGreenVitriol,
             sToluene,
             sNitrationMixture,
             sRocketFuel,
