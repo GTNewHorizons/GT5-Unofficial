e930f36c03b6c2cca0a8c5556e0e8b35de58e201
Martin Robertz <dream-master@gmx.net>
2022-10-22 17:24:35 +0200

    add tpv as metal via tag
    (cherry picked from commit 75dba7283e26b6e2f717a3230d6369e4cbe20c8e)

--- src/main/java/gregtech/api/enums/Materials.java
+++ src/main/java/gregtech/api/enums/Materials.java
@@ -2174,6 +2174,7 @@ public class Materials implements IColorModulationContainer, ISubTagContainer {
                 BlueSteel,
                 DamascusSteel,
                 TungstenSteel,
+                TPV,
                 AstralSilver,
                 Mithril,
                 BlueAlloy,
