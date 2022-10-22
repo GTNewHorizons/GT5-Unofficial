c39664ab27e75437d8922cc5a0cf3d91fa1f7a68
GlodBlock <1356392126@qq.com>
2022-10-22 21:03:58 +0800

    garnet

--- src/main/java/gregtech/api/enums/MaterialsOreAlum.java
+++ src/main/java/gregtech/api/enums/MaterialsOreAlum.java
@@ -47,6 +47,28 @@ public class MaterialsOreAlum {
             .setRGB(163, 38, 38)
             .setColor(Dyes.dyeBrown)
             .constructMaterial();
+    public static Materials GreenSapphireJuice = new MaterialBuilder(415, TextureSet.SET_FLUID, "Green Sapphire Juice")
+        .setName("GreenSapphireJuice")
+        .addCell()
+        .addFluid()
+        .setRGB(100, 200, 130)
+        .setColor(Dyes.dyeGreen)
+        .constructMaterial();
+    public static Materials SapphireJuice = new MaterialBuilder(416, TextureSet.SET_FLUID, "Sapphire Juice")
+        .setName("SapphireJuice")
+        .addCell()
+        .addFluid()
+        .setRGB(100, 100, 200)
+        .setColor(Dyes.dyeBlue)
+        .constructMaterial();
+    public static Materials RubyJuice = new MaterialBuilder(417, TextureSet.SET_FLUID, "Ruby Juice")
+        .setName("RubyJuice")
+        .addCell()
+        .addFluid()
+        .setRGB(255, 100, 100)
+        .setColor(Dyes.dyeRed)
+        .constructMaterial();
+
 
     /**
      * called by Materials. Can be safely called multiple times. exists to allow Materials ensure this class is initialized
