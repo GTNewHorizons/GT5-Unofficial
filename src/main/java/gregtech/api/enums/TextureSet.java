38f6009879b499e4c39a2a9e4ee851690e72545a
BlueWeabo <76872108+BlueWeabo@users.noreply.github.com>
2022-10-31 09:46:18 +0200

    new nanite textures

--- src/main/java/gregtech/api/enums/TextureSet.java
+++ src/main/java/gregtech/api/enums/TextureSet.java
@@ -187,7 +187,7 @@ public class TextureSet {
         "/toolHeadScrewdriver",
         "/toolHeadBuzzSaw",
         "/toolHeadSoldering",
-        "/dust",
+        "/nanites",
         "/wireFine",
         "/gearGtSmall",
         "/rotor",
