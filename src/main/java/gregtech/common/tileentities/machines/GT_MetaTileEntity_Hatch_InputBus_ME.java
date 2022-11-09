2c9370a7920a4139c3cbb9a797d82f7986910580
miozune <miozune@gmail.com>
2022-11-10 01:31:11 +0900

    Move AESlotWidget to public class

--- src/main/java/gregtech/common/tileentities/machines/GT_MetaTileEntity_Hatch_InputBus_ME.java
+++ src/main/java/gregtech/common/tileentities/machines/GT_MetaTileEntity_Hatch_InputBus_ME.java
@@ -12,23 +12,16 @@ import appeng.api.networking.security.MachineSource;
 import appeng.api.storage.IMEMonitor;
 import appeng.api.storage.data.IAEItemStack;
 import appeng.api.util.AECableType;
-import appeng.client.render.AppEngRenderItem;
-import appeng.core.AELog;
 import appeng.me.GridAccessException;
 import appeng.me.helpers.AENetworkProxy;
 import appeng.me.helpers.IGridProxyable;
-import appeng.util.Platform;
 import appeng.util.item.AEItemStack;
 import com.gtnewhorizons.modularui.api.screen.ModularWindow;
 import com.gtnewhorizons.modularui.api.screen.UIBuildContext;
-import com.gtnewhorizons.modularui.common.internal.wrapper.BaseSlot;
-import com.gtnewhorizons.modularui.common.internal.wrapper.ModularGui;
 import com.gtnewhorizons.modularui.common.widget.DrawableWidget;
 import com.gtnewhorizons.modularui.common.widget.SlotGroup;
 import com.gtnewhorizons.modularui.common.widget.SlotWidget;
 import cpw.mods.fml.common.Optional;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import gregtech.api.GregTech_API;
 import gregtech.api.enums.ItemList;
 import gregtech.api.gui.modularui.GT_UITextures;
@@ -39,12 +32,10 @@ import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.implementations.GT_MetaTileEntity_Hatch_InputBus;
 import gregtech.api.render.TextureFactory;
 import gregtech.api.util.GT_Utility;
-import net.minecraft.client.renderer.entity.RenderItem;
+import gregtech.common.gui.modularui.widget.AESlotWidget;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.common.util.ForgeDirection;
 
@@ -372,52 +363,5 @@ public class GT_MetaTileEntity_Hatch_InputBus_ME extends GT_MetaTileEntity_Hatch
                 .setDrawable(getGregTechLogo())
                 .setSize(17, 17)
                 .setPos(80, 63));
-    }
-
-    private static class AESlotWidget extends SlotWidget {
-
-        public AESlotWidget(BaseSlot slot) {
-            super(slot);
-        }
-
-        @Override
-        public void detectAndSendChanges(boolean init) {
-            super.detectAndSendChanges(init);
-            if (getMcSlot().getHasStack()) {
-                syncToClient(
-                        99, buffer -> buffer.writeVarIntToBuffer(getMcSlot().getStack().stackSize));
-            }
-        }
-
-        @Override
-        public void readOnClient(int id, PacketBuffer buf) {
-            super.readOnClient(id, buf);
-            if (id == 99) {
-                if (getMcSlot().getHasStack()) {
-                    getMcSlot().getStack().stackSize = buf.readVarIntFromBuffer();
-                }
-            }
-        }
-
-        @Override
-        @SideOnly(Side.CLIENT)
-        protected void drawSlot(Slot slotIn) {
-            final AppEngRenderItem aeRenderItem = new AppEngRenderItem();
-            final RenderItem pIR = this.setItemRender(aeRenderItem);
-            try {
-                aeRenderItem.setAeStack(Platform.getAEStackInSlot(slotIn));
-                super.drawSlot(slotIn, false);
-            } catch (final Exception err) {
-                AELog.warn("[AppEng] AE prevented crash while drawing slot: " + err);
-            }
-            this.setItemRender(pIR);
-        }
-
-        @SideOnly(Side.CLIENT)
-        private RenderItem setItemRender(final RenderItem item) {
-            final RenderItem ri = ModularGui.getItemRenderer();
-            ModularGui.setItemRenderer(item);
-            return ri;
-        }
     }
 }
