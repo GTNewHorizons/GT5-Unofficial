d76948f4c064c8b6c3665e6c062b7d181f1f148c
Glease <4586901+Glease@users.noreply.github.com>
2022-11-11 13:33:43 +0800

    fix the build

--- src/main/java/net/glease/ggfab/GGConstants.java
+++ src/main/java/net/glease/ggfab/GGConstants.java
@@ -4,7 +4,7 @@ import net.minecraft.util.EnumChatFormatting;
 
 public class GGConstants {
     public static final String MODID = "ggfab";
-    public static final String MODNAME = "GRADLETOKEN_MODNAME";
+    public static final String MODNAME = "GigaGramFab";
     public static final String VERSION = "GRADLETOKEN_VERSION";
 
     public static final String GGMARK = EnumChatFormatting.GOLD + "GigaGram" + EnumChatFormatting.RESET + "Fab";
