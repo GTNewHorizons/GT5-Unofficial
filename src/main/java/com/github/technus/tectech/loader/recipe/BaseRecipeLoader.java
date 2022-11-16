9b25e475dfab1a7a5e10d0fc1476b8894a9bd8d3
miozune <miozune@gmail.com>
2022-11-16 21:31:08 +0900

    Deprecate Data Reader

--- src/main/java/com/github/technus/tectech/loader/recipe/BaseRecipeLoader.java
+++ src/main/java/com/github/technus/tectech/loader/recipe/BaseRecipeLoader.java
@@ -120,26 +120,6 @@ public class BaseRecipeLoader {
                     OrePrefixes.gem.get(Materials.EnderPearl)
                 });
 
-        // Data reader
-        GT_ModHandler.addCraftingRecipe(
-                CustomItemList.Machine_DataReader.get(1),
-                GT_ModHandler.RecipeBits.BUFFERED | GT_ModHandler.RecipeBits.NOT_REMOVABLE,
-                new Object[] {
-                    "BdB",
-                    "GES",
-                    "PwP",
-                    'B',
-                    OrePrefixes.screw.get(Materials.Iridium),
-                    'P',
-                    OrePrefixes.plate.get(Materials.Iridium),
-                    'G',
-                    ItemList.Cover_Screen,
-                    'S',
-                    OrePrefixes.circuit.get(Materials.Elite),
-                    'E',
-                    ItemList.Hull_IV
-                });
-
         // Data Bank
         RA.addAssemblylineRecipe(
                 ItemList.Hatch_DataAccess_EV.get(1),
