c8908858b1dfea74bc89e01d6daea71170341918
GTNH-Colen <54497873+GTNH-Colen@users.noreply.github.com>
2022-12-21 03:29:30 +0000

    Refactor values slightly (not used elsewhere)

--- src/main/java/com/github/technus/tectech/util/CommonValues.java
+++ src/main/java/com/github/technus/tectech/util/CommonValues.java
@@ -6,6 +6,7 @@ import net.minecraft.util.EnumChatFormatting;
 /**
  * Created by danie_000 on 11.01.2017.
  */
+
 @SuppressWarnings("SpellCheckingInspection")
 public final class CommonValues {
     public static final String TEC_MARK_SHORT = EnumChatFormatting.BLUE + "Tec" + EnumChatFormatting.DARK_BLUE + "Tech";
@@ -22,14 +23,13 @@ public final class CommonValues {
     public static final byte MULTI_PURGE_2_AT = 8; // multiblocks clean their hatches 2
     public static final byte OVERFLOW_AT = 10; // then hatches clean themselves
     // - in case some hatches are not in multiblock structure
-    public static final byte MULTI_CHECK_AT = 12; // multiblock checks it's state
+    public static final byte MULTI_CHECK_AT = 12; // multiblock checks its state
     public static final byte DISPERSE_AT = 14; // overflow hatches perform disperse
     public static final byte TRANSFER_AT = 16;
 
     public static final long[] AatV = new long[] {
         268435455, 67108863, 16777215, 4194303, 1048575, 262143, 65535, 16383, 4095, 1023, 255, 63, 15, 3, 1, 1
     };
-    public static final String[] VOLTAGE_NAMES = GT_Values.VOLTAGE_NAMES;
     public static final String[] VN = GT_Values.VN;
     public static final long[] V = GT_Values.V;
 
