80140b74c0a97ea869d1bff3b9b912770b28b92e
GTNH-Colen <54497873+GTNH-Colen@users.noreply.github.com>
2023-01-01 02:25:59 +0000

    All sides working!

--- src/main/java/com/github/technus/tectech/thing/block/RenderEyeOfHarmony.java
+++ src/main/java/com/github/technus/tectech/thing/block/RenderEyeOfHarmony.java
@@ -279,10 +279,10 @@ public class RenderEyeOfHarmony extends TileEntitySpecialRenderer {
             minV = texture.getMinV();
             maxV = texture.getMaxV();
 
-            tes.addVertexWithUV(X[3], Y[3], Z[3], maxU, maxV);
-            tes.addVertexWithUV(X[2], Y[2], Z[2], maxU, minV);
-            tes.addVertexWithUV(X[5], Y[5], Z[5], minU, minV);
-            tes.addVertexWithUV(X[4], Y[4], Z[4], minU, maxV);
+            tes.addVertexWithUV(X[5], Y[5], Z[5], maxU, maxV);
+            tes.addVertexWithUV(X[4], Y[4], Z[4], maxU, minV);
+            tes.addVertexWithUV(X[3], Y[3], Z[3], minU, minV);
+            tes.addVertexWithUV(X[2], Y[2], Z[2], minU, maxV);
         }
 
         {
@@ -293,10 +293,10 @@ public class RenderEyeOfHarmony extends TileEntitySpecialRenderer {
             minV = texture.getMinV();
             maxV = texture.getMaxV();
 
-            tes.addVertexWithUV(X[7], Y[7], Z[7], maxU, maxV);
-            tes.addVertexWithUV(X[0], Y[0], Z[0], maxU, minV);
-            tes.addVertexWithUV(X[3], Y[3], Z[3], minU, minV);
-            tes.addVertexWithUV(X[4], Y[4], Z[4], minU, maxV);
+            tes.addVertexWithUV(X[3], Y[3], Z[3], maxU, maxV);
+            tes.addVertexWithUV(X[4], Y[4], Z[4], maxU, minV);
+            tes.addVertexWithUV(X[7], Y[7], Z[7], minU, minV);
+            tes.addVertexWithUV(X[0], Y[0], Z[0], minU, maxV);
         }
 
         {
