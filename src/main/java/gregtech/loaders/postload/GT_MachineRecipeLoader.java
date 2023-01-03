b7cffe9ab4822bc040f995653634b0581a1ea7c0
chochem <40274384+chochem@users.noreply.github.com>
2023-01-03 17:25:05 +0000

    Fix Blue Steel and Bismuth Bronze mixing (#1629)
    * blue steel
    
    * bismuth bronze

--- src/main/java/gregtech/loaders/postload/GT_MachineRecipeLoader.java
+++ src/main/java/gregtech/loaders/postload/GT_MachineRecipeLoader.java
@@ -375,14 +375,14 @@ public class GT_MachineRecipeLoader implements Runnable {
                 8);
         GT_Values.RA.addMixerRecipe(
                 GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Bismuth, 1L),
-                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Brass, 1L),
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Brass, 4L),
                 GT_Values.NI,
                 GT_Values.NI,
                 GT_Values.NI,
                 GT_Utility.getIntegratedCircuit(1),
                 GT_Values.NF,
                 GT_Values.NF,
-                GT_OreDictUnificator.getDust(Materials.BismuthBronze, 2L * OrePrefixes.dust.mMaterialAmount),
+                GT_OreDictUnificator.getDust(Materials.BismuthBronze, 5L * OrePrefixes.dust.mMaterialAmount),
                 (int) (500L * OrePrefixes.dust.mMaterialAmount / 3628800L),
                 8);
         GT_Values.RA.addMixerRecipe(
@@ -449,8 +449,8 @@ public class GT_MachineRecipeLoader implements Runnable {
                 (int) (1200L * OrePrefixes.dust.mMaterialAmount / 3628800L),
                 480);
         GT_Values.RA.addMixerRecipe(
-                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Copper, 31L),
-                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Gold, 4L),
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Copper, 19L),
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Gold, 16L),
                 GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Zinc, 5L),
                 GT_OreDictUnificator.get(OrePrefixes.dust, Materials.BlackSteel, 64L),
                 GT_OreDictUnificator.get(OrePrefixes.dust, Materials.BlackSteel, 16L),
