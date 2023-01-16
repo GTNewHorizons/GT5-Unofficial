b7b76c633cc84c3928ba2bbea325afc6e85bdd9a
miozune <miozune@gmail.com>
2023-01-17 00:23:53 +0900

    Add Output Hatch to the list of valid hatches on tooltip (#502)

--- src/main/java/gtPlusPlus/xmod/gregtech/common/tileentities/machines/multi/processing/GregtechMetaTileEntity_IndustrialWashPlant.java
+++ src/main/java/gtPlusPlus/xmod/gregtech/common/tileentities/machines/multi/processing/GregtechMetaTileEntity_IndustrialWashPlant.java
@@ -81,6 +81,7 @@ public class GregtechMetaTileEntity_IndustrialWashPlant
                 .addInputBus("Any Casing", 1)
                 .addOutputBus("Any Casing", 1)
                 .addInputHatch("Any Casing", 1)
+                .addOutputHatch("Any Casing", 1)
                 .addEnergyHatch("Any Casing", 1)
                 .addMaintenanceHatch("Any Casing", 1)
                 .addMufflerHatch("Any Casing", 1)
