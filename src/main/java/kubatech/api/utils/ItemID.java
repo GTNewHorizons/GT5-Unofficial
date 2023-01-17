570f3ab14be37436205ea865ecfc2df4e966d491
kuba6000 <kuba.123123.6000@gmail.com>
2023-01-17 17:36:37 +0100

    New year

--- src/main/java/kubatech/api/utils/ItemID.java
+++ src/main/java/kubatech/api/utils/ItemID.java
@@ -1,6 +1,6 @@
 /*
  * KubaTech - Gregtech Addon
- * Copyright (C) 2022  kuba6000
+ * Copyright (C) 2022 - 2023  kuba6000
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
