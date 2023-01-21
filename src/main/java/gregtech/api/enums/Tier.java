58a3ef42b19fed14396a6b80aa447d8cf34a8186
GTNH-Colen <54497873+GTNH-Colen@users.noreply.github.com>
2023-01-21 15:16:06 +0000

    Adjust enums to use VP rather than recalculate

--- src/main/java/gregtech/api/enums/Tier.java
+++ src/main/java/gregtech/api/enums/Tier.java
@@ -27,21 +27,21 @@ public class Tier {
     public static final long MAX = V[14];
 
     // Use  me for recipes.
-    public static final long RECIPE_ULV = (long) (RECIPE_DISCOUNT * V[0]);
-    public static final long RECIPE_LV = (long) (RECIPE_DISCOUNT * V[1]);
-    public static final long RECIPE_MV = (long) (RECIPE_DISCOUNT * V[2]);
-    public static final long RECIPE_HV = (long) (RECIPE_DISCOUNT * V[3]);
-    public static final long RECIPE_EV = (long) (RECIPE_DISCOUNT * V[4]);
-    public static final long RECIPE_IV = (long) (RECIPE_DISCOUNT * V[5]);
-    public static final long RECIPE_LuV = (long) (RECIPE_DISCOUNT * V[6]);
-    public static final long RECIPE_ZPM = (long) (RECIPE_DISCOUNT * V[7]);
-    public static final long RECIPE_UV = (long) (RECIPE_DISCOUNT * V[8]);
-    public static final long RECIPE_UHV = (long) (RECIPE_DISCOUNT * V[9]);
-    public static final long RECIPE_UEV = (long) (RECIPE_DISCOUNT * V[10]);
-    public static final long RECIPE_UIV = (long) (RECIPE_DISCOUNT * V[11]);
-    public static final long RECIPE_UMV = (long) (RECIPE_DISCOUNT * V[12]);
-    public static final long RECIPE_UXV = (long) (RECIPE_DISCOUNT * V[13]);
-    public static final long RECIPE_MAX = (long) (RECIPE_DISCOUNT * V[14]);
+    public static final long RECIPE_ULV = GT_Values.VP[0];
+    public static final long RECIPE_LV = GT_Values.VP[1];
+    public static final long RECIPE_MV = GT_Values.VP[2];
+    public static final long RECIPE_HV = GT_Values.VP[3];
+    public static final long RECIPE_EV = GT_Values.VP[4];
+    public static final long RECIPE_IV = GT_Values.VP[5];
+    public static final long RECIPE_LuV = GT_Values.VP[6];
+    public static final long RECIPE_ZPM = GT_Values.VP[7];
+    public static final long RECIPE_UV = GT_Values.VP[8];
+    public static final long RECIPE_UHV = GT_Values.VP[9];
+    public static final long RECIPE_UEV = GT_Values.VP[10];
+    public static final long RECIPE_UIV = GT_Values.VP[11];
+    public static final long RECIPE_UMV = GT_Values.VP[12];
+    public static final long RECIPE_UXV = GT_Values.VP[13];
+    public static final long RECIPE_MAX = GT_Values.VP[14];
 
     public static final Tier[]
             ELECTRIC =
