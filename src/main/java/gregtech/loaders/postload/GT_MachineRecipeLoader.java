026e6c471c36fa9778df46d03c0e7e7c4ab7d573
boubou19 <miisterunknown@gmail.com>
2023-01-25 22:41:13 +0100

    move printer recipes

--- src/main/java/gregtech/loaders/postload/GT_MachineRecipeLoader.java
+++ src/main/java/gregtech/loaders/postload/GT_MachineRecipeLoader.java
@@ -113,41 +113,6 @@ public class GT_MachineRecipeLoader implements Runnable {
                 160,
                 96);
 
-        GT_Values.RA.addPrinterRecipe(
-                GT_OreDictUnificator.get(OrePrefixes.plateDouble, Materials.Paper, 1L),
-                getFluidStack("squidink", 36),
-                GT_Values.NI,
-                ItemList.Paper_Punch_Card_Empty.get(1L),
-                100,
-                2);
-        GT_Values.RA.addPrinterRecipe(
-                ItemList.Paper_Punch_Card_Empty.get(1L),
-                getFluidStack("squidink", 36),
-                ItemList.Tool_DataStick.getWithName(0L, "With Punch Card Data"),
-                ItemList.Paper_Punch_Card_Encoded.get(1L),
-                100,
-                2);
-        GT_Values.RA.addPrinterRecipe(
-                GT_OreDictUnificator.get(OrePrefixes.plate, Materials.Paper, 3L),
-                getFluidStack("squidink", 144),
-                ItemList.Tool_DataStick.getWithName(0L, "With Scanned Book Data"),
-                ItemList.Paper_Printed_Pages.get(1L),
-                400,
-                2);
-        GT_Values.RA.addPrinterRecipe(
-                new ItemStack(Items.map, 1, 32767),
-                getFluidStack("squidink", 144),
-                ItemList.Tool_DataStick.getWithName(0L, "With Scanned Map Data"),
-                new ItemStack(Items.filled_map, 1, 0),
-                400,
-                2);
-        GT_Values.RA.addPrinterRecipe(
-                new ItemStack(Items.book, 1, 32767),
-                getFluidStack("squidink", 144),
-                GT_Values.NI,
-                GT_Utility.getWrittenBook("Manual_Printer", ItemList.Book_Written_01.get(1L)),
-                400,
-                2);
 
         GT_Values.RA.addMixerRecipe(
                 GT_OreDictUnificator.get(OrePrefixes.dust, Materials.EnderPearl, 1L),
