149fcf0eb78cdac5b06cf8b2114ec47231e107fe
boubou19 <miisterunknown@gmail.com>
2023-01-26 02:16:13 +0100

    moved some canner recipes

--- src/main/java/gregtech/loaders/postload/GT_MachineRecipeLoader.java
+++ src/main/java/gregtech/loaders/postload/GT_MachineRecipeLoader.java
@@ -796,51 +796,6 @@ public class GT_MachineRecipeLoader implements Runnable {
                 64,
                 16);
 
-        // fuel rod canner recipes
-        GT_Values.RA.addCannerRecipe(
-                GT_ModHandler.getIC2Item("fuelRod", 1),
-                GT_OreDictUnificator.get(OrePrefixes.dustTiny, Materials.Lithium, 1L),
-                GT_ModHandler.getIC2Item("reactorLithiumCell", 1, 1),
-                null,
-                16,
-                64);
-
-        GT_Values.RA.addCannerRecipe(
-                GT_ModHandler.getIC2Item("fuelRod", 1),
-                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Thorium, 3),
-                ItemList.ThoriumCell_1.get(1L),
-                null,
-                30,
-                16);
-        GT_Values.RA.addCannerRecipe(
-                ItemList.Large_Fluid_Cell_TungstenSteel.get(1L),
-                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.NaquadahEnriched, 3),
-                ItemList.NaquadahCell_1.get(1L),
-                null,
-                30,
-                16);
-        GT_Values.RA.addCannerRecipe(
-                ItemList.Large_Fluid_Cell_TungstenSteel.get(1L),
-                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Naquadria, 3),
-                ItemList.MNqCell_1.get(1L),
-                null,
-                30,
-                16);
-        GT_Values.RA.addCannerRecipe(
-                GT_ModHandler.getIC2Item("fuelRod", 1),
-                GT_ModHandler.getIC2Item("UranFuel", 1),
-                ItemList.Uraniumcell_1.get(1),
-                null,
-                30,
-                16);
-        GT_Values.RA.addCannerRecipe(
-                GT_ModHandler.getIC2Item("fuelRod", 1),
-                GT_ModHandler.getIC2Item("MOXFuel", 1),
-                ItemList.Moxcell_1.get(1),
-                null,
-                30,
-                16);
-
 
         RA.addThermalCentrifugeRecipe(
                 ItemList.SunnariumCell.get(1),
