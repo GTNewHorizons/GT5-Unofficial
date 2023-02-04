1df3dc2e952d5b47a9d4c25e6c63c48e1b6cb94a
BlueWeabo <76872108+BlueWeabo@users.noreply.github.com>
2023-02-04 10:12:08 +0200

    location enums

---
+++ src/main/java/gregtech/common/misc/spaceprojects/enums/SolarSystem.java
@@ -0,0 +1,70 @@
+package gregtech.common.misc.spaceprojects.enums;
+
+import static gregtech.common.misc.spaceprojects.enums.SpaceBodyType.*;
+import static gregtech.common.misc.spaceprojects.enums.StarType.*;
+
+import gregtech.common.misc.spaceprojects.interfaces.ISpaceBody;
+
+public enum SolarSystem implements ISpaceBody {
+
+    Sol(Star, GClass),
+    Overworld(Planet),
+    Moon(NaturalSatellite),
+    Mars(Planet),
+    Deimos(NaturalSatellite),
+    Phobos(NaturalSatellite),
+    Mercury(Planet),
+    Venus(Planet),
+    Jupiter(GasGiant),
+    Io(NaturalSatellite),
+    Ganymede(NaturalSatellite),
+    Europa(NaturalSatellite),
+    Callisto(NaturalSatellite),
+    Saturn(GasGiant),
+    Mimas(NaturalSatellite),
+    Enceladus(NaturalSatellite),
+    Tethys(NaturalSatellite),
+    Rhea(NaturalSatellite),
+    Titan(NaturalSatellite),
+    Hyperion(NaturalSatellite),
+    Iapetus(NaturalSatellite),
+    Phoebe(NaturalSatellite),
+    Uranus(IceGiant),
+    Miranda(NaturalSatellite),
+    Ariel(NaturalSatellite),
+    Umbriel(NaturalSatellite),
+    Titania(NaturalSatellite),
+    Oberon(NaturalSatellite),
+    Neptune(IceGiant),
+    Proteus(NaturalSatellite),
+    Triton(NaturalSatellite),
+    Nereid(NaturalSatellite),
+    Ceres(DwarfPlanet),
+    Pluto(DwarfPlanet),
+    Arrokoth(DwarfPlanet),
+    MakeMake(DwarfPlanet),
+    KuiperBelt(AsteroidBelt),
+    NONE(SpaceBodyType.NONE);
+
+    private SpaceBodyType mType;
+    private StarType mStarType;
+
+    SolarSystem(SpaceBodyType aType) {
+        this(aType, NotAStar);
+    }
+
+    SolarSystem(SpaceBodyType aType, StarType aStarType) {
+        mStarType = aStarType;
+        mType = aType;
+    }
+
+    @Override
+    public StarType getStarType() {
+        return mStarType;
+    }
+
+    @Override
+    public SpaceBodyType getType() {
+        return mType;
+    }
+}
