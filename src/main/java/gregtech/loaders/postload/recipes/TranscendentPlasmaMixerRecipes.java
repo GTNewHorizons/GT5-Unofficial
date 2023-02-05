19c47e74eb79739d67c5eb22e7e1c8269fee36c5
GitHub GTNH Actions <>
2023-02-05 16:19:49 +0000

    spotlessApply

--- src/main/java/gregtech/loaders/postload/recipes/TranscendentPlasmaMixerRecipes.java
+++ src/main/java/gregtech/loaders/postload/recipes/TranscendentPlasmaMixerRecipes.java
@@ -16,67 +16,67 @@ public class TranscendentPlasmaMixerRecipes implements Runnable {
     private static final int EXOTIC_EU_PER_L = 1_073_007_393;
 
     private static void addTranscendentPlasmaMixerRecipe(final FluidStack fluidOutput, final int circuitNumber,
-                                                         final FluidStack[] fluidInput, final int EUPerL) {
+            final FluidStack[] fluidInput, final int EUPerL) {
         sTranscendentPlasmaMixerRecipes.addRecipe(
-            false,
-            new ItemStack[] { GT_Utility.getIntegratedCircuit(circuitNumber) },
-            null,
-            null,
-            fluidInput,
-            new FluidStack[] { fluidOutput },
-            100,
-            EUPerL,
-            0);
+                false,
+                new ItemStack[] { GT_Utility.getIntegratedCircuit(circuitNumber) },
+                null,
+                null,
+                fluidInput,
+                new FluidStack[] { fluidOutput },
+                100,
+                EUPerL,
+                0);
     }
 
     @Override
     public void run() {
 
         addTranscendentPlasmaMixerRecipe(
-            Materials.ExcitedDTCC.getFluid(1000L),
-            1,
-            new FluidStack[] { Materials.Helium.getPlasma(1000), Materials.Iron.getPlasma(1000),
-                Materials.Calcium.getPlasma(1000), Materials.Niobium.getPlasma(1000) },
-            CRUDE_EU_PER_L);
+                Materials.ExcitedDTCC.getFluid(1000L),
+                1,
+                new FluidStack[] { Materials.Helium.getPlasma(1000), Materials.Iron.getPlasma(1000),
+                        Materials.Calcium.getPlasma(1000), Materials.Niobium.getPlasma(1000) },
+                CRUDE_EU_PER_L);
 
         addTranscendentPlasmaMixerRecipe(
-            Materials.ExcitedDTPC.getFluid(1000L),
-            2,
-            new FluidStack[] { Materials.Helium.getPlasma(1000), Materials.Iron.getPlasma(1000),
-                Materials.Calcium.getPlasma(1000), Materials.Niobium.getPlasma(1000),
+                Materials.ExcitedDTPC.getFluid(1000L),
+                2,
+                new FluidStack[] { Materials.Helium.getPlasma(1000), Materials.Iron.getPlasma(1000),
+                        Materials.Calcium.getPlasma(1000), Materials.Niobium.getPlasma(1000),
 
-                Materials.Nitrogen.getPlasma(1000), Materials.Zinc.getPlasma(1000),
-                Materials.Silver.getPlasma(1000), Materials.Titanium.getPlasma(1000), },
-            PROSAIC_EU_PER_L);
-
-        addTranscendentPlasmaMixerRecipe(
-            Materials.ExcitedDTRC.getFluid(1000L),
-            3,
-            new FluidStack[] { Materials.Helium.getPlasma(1000), Materials.Iron.getPlasma(1000),
-                Materials.Calcium.getPlasma(1000), Materials.Niobium.getPlasma(1000),
-
-                Materials.Nitrogen.getPlasma(1000), Materials.Zinc.getPlasma(1000),
-                Materials.Silver.getPlasma(1000), Materials.Titanium.getPlasma(1000),
-
-                Materials.Radon.getPlasma(1000), Materials.Nickel.getPlasma(1000),
-                Materials.Boron.getPlasma(1000), Materials.Sulfur.getPlasma(1000), },
-            RESPLENDENT_EU_PER_L);
+                        Materials.Nitrogen.getPlasma(1000), Materials.Zinc.getPlasma(1000),
+                        Materials.Silver.getPlasma(1000), Materials.Titanium.getPlasma(1000), },
+                PROSAIC_EU_PER_L);
 
         addTranscendentPlasmaMixerRecipe(
-            Materials.ExcitedDTEC.getFluid(1000L),
-            4,
-            new FluidStack[] { Materials.Helium.getPlasma(1000), Materials.Iron.getPlasma(1000),
-                Materials.Calcium.getPlasma(1000), Materials.Niobium.getPlasma(1000),
+                Materials.ExcitedDTRC.getFluid(1000L),
+                3,
+                new FluidStack[] { Materials.Helium.getPlasma(1000), Materials.Iron.getPlasma(1000),
+                        Materials.Calcium.getPlasma(1000), Materials.Niobium.getPlasma(1000),
 
-                Materials.Nitrogen.getPlasma(1000), Materials.Zinc.getPlasma(1000),
-                Materials.Silver.getPlasma(1000), Materials.Titanium.getPlasma(1000),
+                        Materials.Nitrogen.getPlasma(1000), Materials.Zinc.getPlasma(1000),
+                        Materials.Silver.getPlasma(1000), Materials.Titanium.getPlasma(1000),
 
-                Materials.Radon.getPlasma(1000), Materials.Nickel.getPlasma(1000),
-                Materials.Boron.getPlasma(1000), Materials.Sulfur.getPlasma(1000),
+                        Materials.Radon.getPlasma(1000), Materials.Nickel.getPlasma(1000),
+                        Materials.Boron.getPlasma(1000), Materials.Sulfur.getPlasma(1000), },
+                RESPLENDENT_EU_PER_L);
 
-                Materials.Americium.getPlasma(1000), Materials.Bismuth.getPlasma(1000),
-                Materials.Oxygen.getPlasma(1000), Materials.Tin.getPlasma(1000), },
-            EXOTIC_EU_PER_L);
+        addTranscendentPlasmaMixerRecipe(
+                Materials.ExcitedDTEC.getFluid(1000L),
+                4,
+                new FluidStack[] { Materials.Helium.getPlasma(1000), Materials.Iron.getPlasma(1000),
+                        Materials.Calcium.getPlasma(1000), Materials.Niobium.getPlasma(1000),
+
+                        Materials.Nitrogen.getPlasma(1000), Materials.Zinc.getPlasma(1000),
+                        Materials.Silver.getPlasma(1000), Materials.Titanium.getPlasma(1000),
+
+                        Materials.Radon.getPlasma(1000), Materials.Nickel.getPlasma(1000),
+                        Materials.Boron.getPlasma(1000), Materials.Sulfur.getPlasma(1000),
+
+                        Materials.Americium.getPlasma(1000), Materials.Bismuth.getPlasma(1000),
+                        Materials.Oxygen.getPlasma(1000), Materials.Tin.getPlasma(1000), },
+                EXOTIC_EU_PER_L);
 
     }
 }
