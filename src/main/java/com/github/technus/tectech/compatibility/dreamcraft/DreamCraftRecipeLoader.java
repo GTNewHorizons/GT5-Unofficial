748fc7ffd53112f297a3fb845dcbba9fa10ded28
chochem <40274384+chochem@users.noreply.github.com>
2023-02-10 12:14:03 +0000

    sa

--- src/main/java/com/github/technus/tectech/compatibility/dreamcraft/DreamCraftRecipeLoader.java
+++ src/main/java/com/github/technus/tectech/compatibility/dreamcraft/DreamCraftRecipeLoader.java
@@ -5052,9 +5052,12 @@ public class DreamCraftRecipeLoader {
 
         // EOH Spacetime Compression
         {
-            final ItemStack ME_Singularity = getModItem("appliedenergistics2", "item.ItemExtremeStorageCell.Singularity", 1); // ME
-                                                                                                                   // Digital
-                                                                                                                   // singularity.
+            final ItemStack ME_Singularity = getModItem(
+                    "appliedenergistics2",
+                    "item.ItemExtremeStorageCell.Singularity",
+                    1); // ME
+            // Digital
+            // singularity.
             final ItemStack baseCasing = CustomItemList.EOH_Reinforced_Spacetime_Casing.get(1);
 
             int baseCompPerSec = 16_384;
