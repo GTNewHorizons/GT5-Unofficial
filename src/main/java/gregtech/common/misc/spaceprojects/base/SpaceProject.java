a2aca3442f40afa0b4bc266fcf067a78796a3ba4
BlueWeabo <76872108+BlueWeabo@users.noreply.github.com>
2023-02-10 21:23:34 +0200

    move things around and new things

--- src/main/java/gregtech/common/misc/spaceprojects/base/SpaceProject.java
+++ src/main/java/gregtech/common/misc/spaceprojects/base/SpaceProject.java
@@ -1,15 +1,18 @@
-package gregtech.common.misc.spaceprojects;
+package gregtech.common.misc.spaceprojects.base;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.StatCollector;
 import net.minecraftforge.fluids.FluidStack;
 
+import gregtech.common.misc.spaceprojects.SpaceProjectManager;
 import gregtech.common.misc.spaceprojects.enums.UpgradeStatus;
+import gregtech.common.misc.spaceprojects.interfaces.ISpaceBody;
 import gregtech.common.misc.spaceprojects.interfaces.ISpaceProject;
 
 public class SpaceProject implements ISpaceProject {
@@ -24,10 +27,12 @@ public class SpaceProject implements ISpaceProject {
     protected int mProjectTier;
     protected int mCurrentStage;
     protected int mTotalStages;
-    protected List<ISP_Upgrade> mUpgrades;
+    protected Map<String, ISP_Upgrade> mUpgrades;
+    protected ISP_Requirements mRequirements;
     protected ISP_Upgrade mCurrentUpgrade;
     protected ItemStack[] mItemCosts;
     protected FluidStack[] mFluidCosts;
+    protected ISpaceBody mLocation;
 
     /*
      * Getter Section
@@ -74,7 +79,7 @@ public class SpaceProject implements ISpaceProject {
     @Override
     public List<ISP_Upgrade> getUpgradesAvailable() {
         List<ISP_Upgrade> tUpgrades = new ArrayList<>();
-        for (ISP_Upgrade tUpgrade : mUpgrades) {
+        for (ISP_Upgrade tUpgrade : mUpgrades.values()) {
             if (tUpgrade.getStatus() == UpgradeStatus.Unlocked) {
                 tUpgrades.add(tUpgrade);
             }
@@ -90,6 +95,18 @@ public class SpaceProject implements ISpaceProject {
     @Override
     public ItemStack[] getItemCostPerStage() {
         return mItemCosts;
+    }
+
+    @Override
+    public ItemStack[] getCurrentItemProgress() {
+        ItemStack[] tCurrentItemProgress = new ItemStack[mItemCosts.length];
+        int index = 0;
+        for (ItemStack tItem : mItemCosts) {
+            ItemStack tCopy = tItem.copy();
+            tCopy.stackSize *= getCurrentStage();
+            tCurrentItemProgress[index++] = tCopy;
+        }
+        return tCurrentItemProgress;
     }
 
     @Override
@@ -110,6 +127,18 @@ public class SpaceProject implements ISpaceProject {
     }
 
     @Override
+    public FluidStack[] getCurrentFluidProgress() {
+        FluidStack[] tCurrentFluidProgress = new FluidStack[mFluidCosts.length];
+        int index = 0;
+        for (FluidStack tFluid : mFluidCosts) {
+            FluidStack tCopy = tFluid.copy();
+            tCopy.amount *= getCurrentStage();
+            tCurrentFluidProgress[index++] = tCopy;
+        }
+        return tCurrentFluidProgress;
+    }
+
+    @Override
     public FluidStack[] getTotalFluidCost() {
         FluidStack[] tTotalFluidCost = new FluidStack[mFluidCosts.length];
         int index = 0;
@@ -124,6 +153,11 @@ public class SpaceProject implements ISpaceProject {
     @Override
     public ISP_Upgrade getUpgradeBeingBuilt() {
         return mCurrentUpgrade;
+    }
+
+    @Override
+    public int getCurrentStage() {
+        return mCurrentStage;
     }
 
     /*
@@ -165,7 +199,9 @@ public class SpaceProject implements ISpaceProject {
     }
 
     public SpaceProject setUpgrades(ISP_Upgrade... aUpgrades) {
-        mUpgrades = Arrays.asList(aUpgrades);
+        for (ISP_Upgrade tUpgrade : aUpgrades) {
+            mUpgrades.put(tUpgrade.getUpgradeName(), tUpgrade);
+        }
         return this;
     }
 
@@ -199,13 +235,55 @@ public class SpaceProject implements ISpaceProject {
         SpaceProject aCopy = new SpaceProject().setProjectName(mSpaceProjectName)
                 .setProjectUnlocalizedName(mSpaceProjectUnlocalizedName).setProjectVoltage(mVoltage)
                 .setProjectBuildTime(mBuildTime).setItemCosts(mItemCosts).setFluidCosts(mFluidCosts)
-                .setUpgrades(mUpgrades.toArray(new ISP_Upgrade[0])).setTotalStages(mTotalStages);
+                .setTotalStages(mTotalStages);
+        if (mUpgrades != null) {
+            aCopy.setUpgrades(mUpgrades.values().toArray(new ISP_Upgrade[0]));
+        }
         return aCopy;
     }
 
     @Override
     public void goToNextStage() {
-        if (mCurrentStage == mTotalStages) return;
+        if (mCurrentStage == mTotalStages) {
+            return;
+        }
         mCurrentStage++;
+    }
+
+    @Override
+    public boolean meetsRequirements(UUID aTeam, ISpaceBody aLocation) {
+        if (mRequirements == null) {
+            return true;
+        }
+
+        if (mRequirements.getBodyType() != null) {
+            if (!mRequirements.getBodyType().equals(mLocation.getType())) {
+                return false;
+            }
+        }
+
+        if (mRequirements.getStarType() != null) {
+            if (!mRequirements.getStarType().equals(mLocation.getStarType())) {
+                return false;
+            }
+        }
+
+        if (mRequirements.getProjects() != null) {
+            for (ISpaceProject tProject : mRequirements.getProjects()) {
+                if (!SpaceProjectManager.teamHasProject(aTeam, tProject)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof ISpaceProject)) {
+            return false;
+        }
+        return getProjectName().equals(((ISpaceProject) obj).getProjectName());
     }
 }
