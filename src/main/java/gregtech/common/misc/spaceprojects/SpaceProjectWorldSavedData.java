9fb6f219e6f6e1b0f5621d598f14f325a7dc1160
BlueWeabo <76872108+BlueWeabo@users.noreply.github.com>
2023-02-13 21:12:57 +0200

    switch to saving to json string. this is fine

--- src/main/java/gregtech/common/misc/spaceprojects/SpaceProjectWorldSavedData.java
+++ src/main/java/gregtech/common/misc/spaceprojects/SpaceProjectWorldSavedData.java
@@ -3,12 +3,12 @@ package gregtech.common.misc.spaceprojects;
 import static gregtech.common.misc.spaceprojects.SpaceProjectManager.mSpaceTeamProjects;
 import static gregtech.common.misc.spaceprojects.SpaceProjectManager.mSpaceTeams;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.lang.reflect.Type;
 import java.util.Map;
 import java.util.UUID;
 
@@ -20,6 +20,10 @@ import net.minecraftforge.event.world.WorldEvent;
 
 import org.apache.commons.lang3.tuple.Pair;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonWriter;
+
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import gregtech.common.misc.spaceprojects.interfaces.ISpaceBody;
 import gregtech.common.misc.spaceprojects.interfaces.ISpaceProject;
@@ -28,75 +32,80 @@ public class SpaceProjectWorldSavedData extends WorldSavedData {
 
     public static SpaceProjectWorldSavedData INSTANCE;
 
+    private static final Gson GSON = new Gson();
+
     private static final String DATA_NAME = "GT_SpaceProjectData";
 
     private static final String SPACE_TEAM_PROJECTS = "GT_SpaceTeamProjectsNBT";
 
     private static final String SPACE_TEAMS = "GT_SpaceTeamsNBT";
 
+    private static final String SPACE_TEAM_PROJECTS_JSON = "spaceTeamProject.json";
+
+    private static final String SPACE_TEAMS_JSON = "spaceTeams.json";
+
+    private static World mWorld;
+
     public SpaceProjectWorldSavedData() {
         super(DATA_NAME);
     }
 
-    public SpaceProjectWorldSavedData(String aName) {
-        super(aName);
+    public SpaceProjectWorldSavedData(String aData) {
+        super(aData);
     }
 
     @Override
     public void readFromNBT(NBTTagCompound aNBT) {
-        try {
-            byte[] ba = aNBT.getByteArray(SPACE_TEAM_PROJECTS);
-            InputStream byteArrayInputStream = new ByteArrayInputStream(ba);
-            ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
-            Object data = objectInputStream.readObject();
-            mSpaceTeamProjects = (Map<UUID, Map<Pair<ISpaceBody, String>, ISpaceProject>>) data;
-        } catch (IOException | ClassNotFoundException exception) {
-            System.out.println(SPACE_TEAM_PROJECTS + " FAILED");
-            exception.printStackTrace();
-        }
-        try {
-            byte[] ba = aNBT.getByteArray(SPACE_TEAMS);
-            InputStream byteArrayInputStream = new ByteArrayInputStream(ba);
-            ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
-            Object data = objectInputStream.readObject();
-            mSpaceTeams = (Map<UUID, UUID>) data;
-        } catch (IOException | ClassNotFoundException exception) {
-            System.out.println(SPACE_TEAMS + " FAILED");
-            exception.printStackTrace();
-        }
+        Type tTeamProjectsType = new TypeToken<Map<UUID, Map<Pair<ISpaceBody, String>, ISpaceProject>>>() {}.getType();
+        mSpaceTeamProjects = GSON.fromJson(aNBT.getString(SPACE_TEAM_PROJECTS), tTeamProjectsType);
+        Type tTeamsType = new TypeToken<Map<UUID, UUID>>() {}.getType();
+        mSpaceTeams = GSON.fromJson(aNBT.getString(SPACE_TEAMS), tTeamsType);
     }
 
     @Override
     public void writeToNBT(NBTTagCompound aNBT) {
-        try {
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
-            objectOutputStream.writeObject(mSpaceTeamProjects);
-            objectOutputStream.flush();
-            byte[] data = byteArrayOutputStream.toByteArray();
-            aNBT.setByteArray(SPACE_TEAM_PROJECTS, data);
-        } catch (IOException exception) {
-            System.out.println(SPACE_TEAM_PROJECTS + " SAVE FAILED");
-            exception.printStackTrace();
+        File tTeamProjectsFile = new File(mWorld.getSaveHandler().getWorldDirectory(), SPACE_TEAM_PROJECTS_JSON);
+        try (JsonWriter writer = new JsonWriter(new FileWriter(tTeamProjectsFile))) {
+            GSON.toJson(mSpaceTeamProjects, mSpaceTeamProjects.getClass(), writer);
+        } catch (IOException ex) {
+            System.out.print("FAILED TO SAVE: " + SPACE_TEAM_PROJECTS_JSON);
+            ex.printStackTrace();
         }
-        try {
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
-            objectOutputStream.writeObject(mSpaceTeams);
-            objectOutputStream.flush();
-            byte[] data = byteArrayOutputStream.toByteArray();
-            aNBT.setByteArray(SPACE_TEAMS, data);
-        } catch (IOException exception) {
-            System.out.println(SPACE_TEAMS + " SAVE FAILED");
-            exception.printStackTrace();
-        }
-    }
 
-    @SubscribeEvent
-    public void onWorldLoad(WorldEvent.Load aEvent) {
-        if (!aEvent.world.isRemote && aEvent.world.provider.dimensionId == 0) {
-            loadInstance(aEvent.world);
+        String tSpaceTeamProjectsJsonString = "";
+        try (FileReader reader = new FileReader(tTeamProjectsFile); BufferedReader br = new BufferedReader(reader)) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                tSpaceTeamProjectsJsonString += line;
+            }
+        } catch (IOException e) {
+            System.out.print("FAILED TO SAVE: " + SPACE_TEAM_PROJECTS_JSON);
+            e.printStackTrace();
         }
+        aNBT.setString(SPACE_TEAM_PROJECTS, tSpaceTeamProjectsJsonString);
+
+        File tSpaceTeamsFile = new File(mWorld.getSaveHandler().getWorldDirectory(), SPACE_TEAMS_JSON);
+        try (JsonWriter writer = new JsonWriter(new FileWriter(tTeamProjectsFile))) {
+            GSON.toJson(mSpaceTeams, mSpaceTeams.getClass(), writer);
+        } catch (IOException ex) {
+            System.out.print("FAILED TO SAVE: " + SPACE_TEAMS_JSON);
+            ex.printStackTrace();
+        }
+
+        String tSpaceTeamsJsonString = "";
+        try (FileReader reader = new FileReader(tSpaceTeamsFile); BufferedReader br = new BufferedReader(reader)) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                tSpaceTeamsJsonString += line;
+            }
+        } catch (IOException e) {
+            System.out.print("FAILED TO SAVE: " + SPACE_TEAMS_JSON);
+            e.printStackTrace();
+        }
+
+        aNBT.setString(SPACE_TEAMS, tSpaceTeamsJsonString);
+
+        mWorld = null;
     }
 
     private static void loadInstance(World aWorld) {
@@ -108,6 +117,14 @@ public class SpaceProjectWorldSavedData extends WorldSavedData {
             INSTANCE = new SpaceProjectWorldSavedData();
             tStorage.setData(DATA_NAME, INSTANCE);
         }
+        mWorld = aWorld;
         INSTANCE.markDirty();
+    }
+
+    @SubscribeEvent
+    public void onWorldLoad(WorldEvent.Load aEvent) {
+        if (!aEvent.world.isRemote && aEvent.world.provider.dimensionId == 0) {
+            loadInstance(aEvent.world);
+        }
     }
 }
