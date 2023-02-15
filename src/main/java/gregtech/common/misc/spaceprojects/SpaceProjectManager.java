f7b60c2d72ddd7b7831c8c72b7f884aacdd20d1d
BlueWeabo <76872108+BlueWeabo@users.noreply.github.com>
2023-02-15 20:09:56 +0200

    rename fields

--- src/main/java/gregtech/common/misc/spaceprojects/SpaceProjectManager.java
+++ src/main/java/gregtech/common/misc/spaceprojects/SpaceProjectManager.java
@@ -17,91 +17,109 @@ public class SpaceProjectManager {
     /**
      * Do not use! Only meant to be used in SpaceProjectWorldSavedData.java
      */
-    public static Map<UUID, Map<Pair<ISpaceBody, String>, SpaceProject>> mSpaceTeamProjects = new HashMap<>();
+    public static Map<UUID, Map<Pair<ISpaceBody, String>, SpaceProject>> spaceTeamProjects = new HashMap<>();
     /**
      * Do not use! Only meant to be used in SpaceProjectWorldSavedData.java Stores a Players UUID to the Leader UUID,
      * players in lone groups give back their own UUID.
      */
-    public static Map<UUID, UUID> mSpaceTeams = new HashMap<>();
+    public static Map<UUID, UUID> spaceTeams = new HashMap<>();
 
-    private static final HashMap<String, ISpaceBody> mSpaceLocations = new HashMap<>();
+    private static final HashMap<String, ISpaceBody> spaceLocations = new HashMap<>();
 
-    private static final Map<String, SpaceProject> mSpaceProjects = new HashMap<>();
+    private static final Map<String, SpaceProject> spaceProjects = new HashMap<>();
 
     /*
      * Team Section
      */
-    public static SpaceProject getTeamProject(UUID aTeam, ISpaceBody aLocation, String aProjectName) {
-        Map<Pair<ISpaceBody, String>, SpaceProject> tMap = mSpaceTeamProjects.get(aTeam);
-        if (tMap == null) {
+    public static SpaceProject getTeamProject(UUID team, ISpaceBody location, String projectName) {
+        Map<Pair<ISpaceBody, String>, SpaceProject> map = spaceTeamProjects.get(team);
+        if (map == null) {
             return null;
         }
-        return tMap.get(Pair.of(aLocation, aProjectName));
+        return map.get(Pair.of(location, projectName));
     }
 
-    public static boolean addTeamProject(UUID aTeam, ISpaceBody aLocation, String aProjectName, SpaceProject aProject) {
-        if (!mSpaceTeamProjects.containsKey(getLeader(aTeam)) || mSpaceTeamProjects.get(getLeader(aTeam)) == null) {
-            mSpaceTeamProjects.put(getLeader(aTeam), new HashMap<Pair<ISpaceBody, String>, SpaceProject>());
+    public static boolean addTeamProject(UUID team, ISpaceBody location, String projectName, SpaceProject project) {
+        if (!spaceTeamProjects.containsKey(getLeader(team)) || spaceTeamProjects.get(getLeader(team)) == null) {
+            spaceTeamProjects.put(getLeader(team), new HashMap<Pair<ISpaceBody, String>, SpaceProject>());
         }
-        Map<Pair<ISpaceBody, String>, SpaceProject> tMap = mSpaceTeamProjects.get(getLeader(aTeam));
-        if (tMap.containsKey(Pair.of(aLocation, aProjectName))) return false;
-        tMap.put(Pair.of(aLocation, aProjectName), aProject);
+
+        Map<Pair<ISpaceBody, String>, SpaceProject> map = spaceTeamProjects.get(getLeader(team));
+        if (map.containsKey(Pair.of(location, projectName))) {
+            return false;
+        }
+
+        project.setProjectLocation(location);
+        map.put(Pair.of(location, projectName), project);
         SpaceProjectWorldSavedData.INSTANCE.markDirty();
         return true;
     }
 
-    public static boolean teamHasProject(UUID aTeam, SpaceProject aProject) {
-        Map<Pair<ISpaceBody, String>, SpaceProject> tMap = mSpaceTeamProjects.get(getLeader(aTeam));
-        if (tMap == null) {
+    public static boolean teamHasProject(UUID team, SpaceProject project) {
+        Map<Pair<ISpaceBody, String>, SpaceProject> map = spaceTeamProjects.get(getLeader(team));
+        if (map == null) {
             return false;
         }
-        return tMap.containsValue(aProject);
+
+        return map.containsValue(project);
     }
 
-    public static void putInTeam(UUID aTeamMember, UUID aTeamLeader) {
-        if (aTeamMember.equals(aTeamLeader)) {
-            mSpaceTeams.put(aTeamMember, aTeamLeader);
-        } else if (!mSpaceTeams.get(aTeamLeader).equals(aTeamLeader)) {
-            putInTeam(aTeamMember, mSpaceTeams.get(aTeamLeader));
+    public static void putInTeam(UUID teamMember, UUID teamLeader) {
+        if (teamMember.equals(teamLeader)) {
+            spaceTeams.put(teamMember, teamLeader);
+        } else if (!spaceTeams.get(teamLeader).equals(teamLeader)) {
+            putInTeam(teamMember, spaceTeams.get(teamLeader));
         } else {
-            mSpaceTeams.put(aTeamMember, aTeamLeader);
+            spaceTeams.put(teamMember, teamLeader);
         }
+
         SpaceProjectWorldSavedData.INSTANCE.markDirty();
     }
 
-    public static UUID getLeader(UUID aTeamMember) {
-        return mSpaceTeams.get(aTeamMember);
+    public static UUID getLeader(UUID teamMember) {
+        return spaceTeams.get(teamMember);
     }
 
-    public static void checkOrCreateTeam(UUID aTeamMember) {
-        if (mSpaceTeams.containsKey(aTeamMember)) {
+    public static void checkOrCreateTeam(UUID teamMember) {
+        if (spaceTeams.containsKey(teamMember)) {
             return;
         }
-        mSpaceTeams.put(aTeamMember, aTeamMember);
+
+        spaceTeams.put(teamMember, teamMember);
         SpaceProjectWorldSavedData.INSTANCE.markDirty();
     }
 
-    public static Collection<SpaceProject> getTeamSpaceProjects(UUID aTeam) {
-        Map<Pair<ISpaceBody, String>, SpaceProject> tMap = mSpaceTeamProjects.get(getLeader(aTeam));
-        if (tMap == null) {
+    public static Collection<SpaceProject> getTeamSpaceProjects(UUID team) {
+        Map<Pair<ISpaceBody, String>, SpaceProject> map = spaceTeamProjects.get(getLeader(team));
+        if (map == null) {
             return null;
         }
-        return tMap.values();
+
+        return map.values();
+    }
+
+    public static SpaceProject getTeamProjectOrCopy(UUID team, String projectName, ISpaceBody location) {
+        Map<Pair<ISpaceBody, String>, SpaceProject> map = spaceTeamProjects.get(getLeader(team));
+        if (map == null) {
+            return getProject(projectName);
+        }
+
+        return map.getOrDefault(Pair.of(location, projectName), getProject(projectName));
     }
 
     /*
      * General Project Helpers
      */
-    public static void addProject(SpaceProject aProject) {
-        mSpaceProjects.put(aProject.getProjectName(), aProject);
+    public static void addProject(SpaceProject project) {
+        spaceProjects.put(project.getProjectName(), project);
     }
 
     /**
-     * @param aProjectName Internal name of the project
+     * @param projectName Internal name of the project
      * @return a copy of the stored project
      */
-    public static SpaceProject getProject(String aProjectName) {
-        SpaceProject tProject = mSpaceProjects.get(aProjectName);
+    public static SpaceProject getProject(String projectName) {
+        SpaceProject tProject = spaceProjects.get(projectName);
         return tProject != null ? tProject.copy() : null;
     }
 
@@ -109,36 +127,36 @@ public class SpaceProjectManager {
      * Should only be used for GUIs!
      */
     public static Map<String, SpaceProject> getAllProjects() {
-        return mSpaceProjects;
+        return spaceProjects;
     }
 
     public static Collection<SpaceProject> getProjects() {
-        return mSpaceProjects.values();
+        return spaceProjects.values();
     }
 
     /*
      * Location Helper
      */
-    public static void addLocation(ISpaceBody aLocation) {
-        mSpaceLocations.put(aLocation.getName(), aLocation);
+    public static void addLocation(ISpaceBody location) {
+        spaceLocations.put(location.getName(), location);
     }
 
     public static Collection<ISpaceBody> getLocations() {
-        return mSpaceLocations.values();
+        return spaceLocations.values();
     }
 
-    public static ISpaceBody getLocation(String aLocationName) {
-        return mSpaceLocations.get(aLocationName);
+    public static ISpaceBody getLocation(String locationName) {
+        return spaceLocations.get(locationName);
     }
 
     /*
      * Helpers
      */
-    public static UUID getPlayerUUIDFromName(String aPlayerName) {
-        return MinecraftServer.getServer().func_152358_ax().func_152655_a(aPlayerName).getId();
+    public static UUID getPlayerUUIDFromName(String playerName) {
+        return MinecraftServer.getServer().func_152358_ax().func_152655_a(playerName).getId();
     }
 
-    public static String getPlayerNameFromUUID(UUID aPlayerUUID) {
-        return MinecraftServer.getServer().func_152358_ax().func_152652_a(aPlayerUUID).getName();
+    public static String getPlayerNameFromUUID(UUID playerUUID) {
+        return MinecraftServer.getServer().func_152358_ax().func_152652_a(playerUUID).getName();
     }
 }
