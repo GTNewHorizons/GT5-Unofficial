b966b7e6c2829fe175b4ab5ff7fdadbfe8ca7375
BlueWeabo <76872108+BlueWeabo@users.noreply.github.com>
2023-02-18 22:19:30 +0200

    documentation part 2 and rework of json saving

---
+++ src/main/java/gregtech/common/misc/spaceprojects/enums/JsonVariables.java
@@ -0,0 +1,17 @@
+package gregtech.common.misc.spaceprojects.enums;
+
+public class JsonVariables {
+
+    public static final String PAIR_LEFT = "pairLeft";
+    public static final String PAIR_RIGHT = "pairRight";
+    public static final String PROJECT_NAME = "projectName";
+
+    public static final String PROJECT_CURRENT_STAGE = "projectCurrentStage";
+    public static final String PROJECT_UPGRADES_BUILT = "projectUpgradesBuilt";
+    public static final String PROJECT_LOCATION = "projectLocation";
+    public static final String PROJECT_CURRENT_UPGRADE = "projectCurrentUpgrade";
+
+    public static final String UPGRADE_NAME = "upgradeName";
+    public static final String UPGRADE_CURRENT_STAGE = "upgradeCurrentStage";
+    public static final String UPGRADE_PROJECT_PARENT = "upgradeProjectParent";
+}
