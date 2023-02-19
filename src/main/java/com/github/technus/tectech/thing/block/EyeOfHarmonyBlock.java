cd3b48899b70ed735070d15378c63fb9aac78967
minecraft7771 <maxim235@gmx.de>
2023-02-19 13:14:18 +0100

    Only allow changing of renderer when debug mode is enabled

--- src/main/java/com/github/technus/tectech/thing/block/EyeOfHarmonyBlock.java
+++ src/main/java/com/github/technus/tectech/thing/block/EyeOfHarmonyBlock.java
@@ -9,6 +9,7 @@ import net.minecraft.util.ChatComponentText;
 import net.minecraft.world.World;
 
 import com.github.technus.tectech.TecTech;
+import com.github.technus.tectech.thing.metaTileEntity.multi.GT_MetaTileEntity_EM_EyeOfHarmony;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.relauncher.Side;
@@ -65,6 +66,9 @@ public class EyeOfHarmonyBlock extends Block {
     @Override
     public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int p_149727_6_,
             float p_149727_7_, float p_149727_8_, float p_149727_9_) {
+
+        if (!GT_MetaTileEntity_EM_EyeOfHarmony.EOH_DEBUG_MODE) return false;
+
         TileEyeOfHarmony tile = (TileEyeOfHarmony) world.getTileEntity(x, y, z);
 
         if (player.isSneaking()) {
