bad96978b3657093bbdaf6721f5817f8b9924dae
minecraft7771 <maxim235@gmx.de>
2023-02-22 17:28:13 +0100

    Added disabled button and toggle button

--- src/main/java/gregtech/api/gui/modularui/GT_UITextures.java
+++ src/main/java/gregtech/api/gui/modularui/GT_UITextures.java
@@ -190,8 +190,12 @@ public class GT_UITextures {
             .adaptableTexture(MODID, "gui/tab/title_angular_%s", 28, 28, 4);
 
     public static final UITexture BUTTON_STANDARD = AdaptableUITexture.of(MODID, "gui/button/standard", 18, 18, 1);
+    public static final UITexture BUTTON_STANDARD_DISABLED = AdaptableUITexture
+            .of(MODID, "gui/button/standard_disabled", 18, 18, 1);
     public static final UITexture BUTTON_STANDARD_TOGGLE = AdaptableUITexture
             .of(MODID, "gui/button/standard_toggle", 18, 18, 1);
+    public static final UITexture BUTTON_STANDARD_TOGGLE_DISABLED = AdaptableUITexture
+            .of(MODID, "gui/button/standard_toggle_disabled", 18, 18, 1);
     public static final UITexture BUTTON_COVER_NORMAL = UITexture.fullImage(MODID, "gui/button/cover_normal");
     public static final UITexture BUTTON_COVER_NORMAL_HOVERED = UITexture
             .fullImage(MODID, "gui/button/cover_normal_hovered");
