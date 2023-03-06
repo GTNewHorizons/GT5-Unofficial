6d901e9a93c63f0478e6b66ff3281c2136f2350c
NexusNull <p.wellershaus@googlemail.com>
2023-03-06 18:30:39 +0100

    Revert "fix: fix Steam Valve not being configurable"
    This reverts commit 505d9e273b48315fde154490e116d58fed46ffaf.

--- src/main/java/gregtech/common/covers/GT_Cover_SteamValve.java
+++ src/main/java/gregtech/common/covers/GT_Cover_SteamValve.java
@@ -3,9 +3,10 @@ package gregtech.common.covers;
 import net.minecraftforge.fluids.FluidStack;
 
 import gregtech.api.interfaces.ITexture;
+import gregtech.api.interfaces.tileentity.ICoverable;
 import gregtech.api.util.GT_ModHandler;
 
-public class GT_Cover_SteamValve extends GT_Cover_FluidRegulator {
+public class GT_Cover_SteamValve extends GT_Cover_Pump {
 
     /**
      * @deprecated use {@link #GT_Cover_SteamValve(int aTransferRate, ITexture coverTexture)} instead
@@ -17,6 +18,12 @@ public class GT_Cover_SteamValve extends GT_Cover_FluidRegulator {
 
     public GT_Cover_SteamValve(int aTransferRate, ITexture coverTexture) {
         super(aTransferRate, coverTexture);
+    }
+
+    @Override
+    public boolean isRedstoneSensitive(byte aSide, int aCoverID, int aCoverVariable, ICoverable aTileEntity,
+            long aTimer) {
+        return false;
     }
 
     @Override
