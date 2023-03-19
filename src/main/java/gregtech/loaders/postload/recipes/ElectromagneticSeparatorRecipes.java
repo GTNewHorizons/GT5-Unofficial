b1822ee1ec7416da1ccfb62293acff2da6498684
boubou19 <miisterunknown@gmail.com>
2023-03-19 19:44:15 +0100

    migrate electromagnetic separator recipes

--- src/main/java/gregtech/loaders/postload/recipes/ElectromagneticSeparatorRecipes.java
+++ src/main/java/gregtech/loaders/postload/recipes/ElectromagneticSeparatorRecipes.java
@@ -3,18 +3,34 @@ package gregtech.loaders.postload.recipes;
 import gregtech.api.enums.GT_Values;
 import gregtech.api.enums.Materials;
 import gregtech.api.enums.MaterialsOreAlum;
+import gregtech.api.util.GT_RecipeBuilder;
+
+import static gregtech.api.util.GT_Recipe.GT_Recipe_Map.sElectroMagneticSeparatorRecipes;
+import static gregtech.api.util.GT_RecipeBuilder.SECONDS;
 
 public class ElectromagneticSeparatorRecipes implements Runnable {
 
     @Override
     public void run() {
-        GT_Values.RA.addElectromagneticSeparatorRecipe(
-                MaterialsOreAlum.SluiceSand.getDust(1),
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                MaterialsOreAlum.SluiceSand.getDust(1)
+            )
+            .itemOutputs(
                 Materials.Iron.getDust(1),
                 Materials.Neodymium.getDust(1),
-                Materials.Chrome.getDust(1),
-                new int[] { 4000, 2000, 2000 },
-                200,
-                240);
+                Materials.Chrome.getDust(1)
+            )
+            .outputChances(
+                4000,
+                2000,
+                2000
+            )
+            .noFluidInputs()
+            .noFluidOutputs()
+            .duration(10 * SECONDS)
+            .eut(240)
+            .addTo(sElectroMagneticSeparatorRecipes);
+
     }
 }
