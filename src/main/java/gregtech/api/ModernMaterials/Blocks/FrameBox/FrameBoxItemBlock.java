7506f984f4dd25ded2e4fca07942d99596b745ca
GTNH-Colen <54497873+GTNH-Colen@users.noreply.github.com>
2023-03-21 05:20:10 +0000

    Actually implement said methods in example

--- src/main/java/gregtech/api/ModernMaterials/Blocks/FrameBox/FrameBoxItemBlock.java
+++ src/main/java/gregtech/api/ModernMaterials/Blocks/FrameBox/FrameBoxItemBlock.java
@@ -1,10 +1,27 @@
 package gregtech.api.ModernMaterials.Blocks.FrameBox;
 
+import gregtech.api.ModernMaterials.Blocks.BlocksEnum;
 import gregtech.api.ModernMaterials.Blocks.DumbBase.DumbItemBlock;
+import gregtech.api.ModernMaterials.ModernMaterial;
+import gregtech.api.ModernMaterials.ModernMaterialUtilities;
 import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
+
 
 public class FrameBoxItemBlock extends DumbItemBlock {
     public FrameBoxItemBlock(Block block) {
         super(block);
+    }
+
+    @Override
+    public String getItemStackDisplayName(ItemStack itemStack) {
+        final ModernMaterial associatedMaterial = ModernMaterialUtilities.materialIDToMaterial.get(itemStack.getItemDamage());
+
+        return getBlockEnum().getLocalisedName(associatedMaterial);
+    }
+
+    @Override
+    public BlocksEnum getBlockEnum() {
+        return BlocksEnum.FrameBox;
     }
 }
