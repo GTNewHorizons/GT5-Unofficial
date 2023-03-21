b4a902be97270d675cb49e9d75805c8b4abd22e4
GTNH-Colen <54497873+GTNH-Colen@users.noreply.github.com>
2023-03-21 05:19:34 +0000

    Add new method to localise the name of a block

--- src/main/java/gregtech/api/ModernMaterials/Blocks/BlocksEnum.java
+++ src/main/java/gregtech/api/ModernMaterials/Blocks/BlocksEnum.java
@@ -6,12 +6,16 @@ import java.util.ArrayList;
 
 public enum BlocksEnum {
 
-    FrameBox("dwakodpo");
+    FrameBox("% Frame Box");
 
-    final private String nameToBeLocalised;
+    public String getLocalisedName(ModernMaterial material) {
+        return unlocalisedName.replace("%", material.getName());
+    }
+
+    final private String unlocalisedName;
     public final ArrayList<ModernMaterial> associatedMaterials = new ArrayList<>();
 
-    BlocksEnum(final String nameToBeLocalised) {
-        this.nameToBeLocalised = nameToBeLocalised;
+    BlocksEnum(final String unlocalisedName) {
+        this.unlocalisedName = unlocalisedName;
     }
 }
