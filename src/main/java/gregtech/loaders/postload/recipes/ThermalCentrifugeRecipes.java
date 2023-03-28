09f13720682f2a58c0a13e51e703a74c86519632
boubou19 <miisterunknown@gmail.com>
2023-03-28 12:14:30 +0200

    migrate thermal centrifuge recipes

--- src/main/java/gregtech/loaders/postload/recipes/ThermalCentrifugeRecipes.java
+++ src/main/java/gregtech/loaders/postload/recipes/ThermalCentrifugeRecipes.java
@@ -1,25 +1,32 @@
 package gregtech.loaders.postload.recipes;
 
 import static gregtech.api.enums.GT_Values.RA;
+import static gregtech.api.util.GT_Recipe.GT_Recipe_Map.sThermalCentrifugeRecipes;
+import static gregtech.api.util.GT_RecipeBuilder.SECONDS;
 
+import gregtech.api.enums.*;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
-import gregtech.api.enums.ItemList;
-import gregtech.api.enums.Materials;
-import gregtech.api.enums.OrePrefixes;
 import gregtech.api.util.GT_OreDictUnificator;
 
 public class ThermalCentrifugeRecipes implements Runnable {
 
     @Override
     public void run() {
-        RA.addThermalCentrifugeRecipe(
-                ItemList.SunnariumCell.get(1),
-                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Sunnarium, 1L),
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                ItemList.SunnariumCell.get(1)
+            )
+            .itemOutputs(
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Sunnarium, 1),
                 new ItemStack(Items.glowstone_dust, 2),
-                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Iron, 1L),
-                500,
-                48);
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Iron, 1)
+            )
+            .noFluidInputs()
+            .noFluidOutputs()
+            .duration(25*SECONDS)
+            .eut(TierEU.RECIPE_LV)
+            .addTo(sThermalCentrifugeRecipes);
     }
 }
