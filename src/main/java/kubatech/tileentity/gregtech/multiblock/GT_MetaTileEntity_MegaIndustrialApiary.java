bd1913da8ad916a27d2dc9ebe3f3c04719817e39
Jakub <53441451+kuba6000@users.noreply.github.com>
2023-03-29 22:18:48 +0200

    Update production modifier for the new bees formula in Mega Apiary (#66)
    * Update production modifier for the new bees formula in Mega Apiary
    
    * Update GT_MetaTileEntity_MegaIndustrialApiary.java
    
    * Wrong one

--- src/main/java/kubatech/tileentity/gregtech/multiblock/GT_MetaTileEntity_MegaIndustrialApiary.java
+++ src/main/java/kubatech/tileentity/gregtech/multiblock/GT_MetaTileEntity_MegaIndustrialApiary.java
@@ -97,7 +97,7 @@ public class GT_MetaTileEntity_MegaIndustrialApiary
     private static final String STRUCTURE_PIECE_MAIN = "main";
     private static final String STRUCTURE_PIECE_MAIN_SURVIVAL = "mainsurvival";
     private static final int CONFIGURATION_WINDOW_ID = 999;
-    private static final int MEGA_APIARY_STORAGE_VERSION = 1;
+    private static final int MEGA_APIARY_STORAGE_VERSION = 2;
 
     private static final String[][] struct = transpose(
             new String[][] { // spotless:off
@@ -1052,6 +1052,7 @@ public class GT_MetaTileEntity_MegaIndustrialApiary
 
         private static class BeeDrop {
 
+            private static final float MAX_PRODUCTION_MODIFIER_FROM_UPGRADES = 17.19926784f; // 4*1.2^8
             final ItemStack stack;
             double amount;
             final GT_Utility.ItemId id;
@@ -1077,7 +1078,7 @@ public class GT_MetaTileEntity_MegaIndustrialApiary
             }
 
             public void evaluate() {
-                this.amount = Bee.getFinalChance(chance, beeSpeed, 2.f, t);
+                this.amount = Bee.getFinalChance(chance, beeSpeed, MAX_PRODUCTION_MODIFIER_FROM_UPGRADES, t);
             }
 
             public double getAmount(double speedModifier) {
