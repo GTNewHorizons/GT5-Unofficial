bda23ce67a3ab6ad20c2b78e528e42930896b4fc
boubou19 <miisterunknown@gmail.com>
2023-03-31 23:22:51 +0200

    migrate compressor recipes

--- src/main/java/gregtech/loaders/postload/recipes/CompressorRecipes.java
+++ src/main/java/gregtech/loaders/postload/recipes/CompressorRecipes.java
@@ -2,6 +2,8 @@ package gregtech.loaders.postload.recipes;
 
 import static gregtech.api.enums.ModIDs.*;
 import static gregtech.api.util.GT_ModHandler.getModItem;
+import static gregtech.api.util.GT_Recipe.GT_Recipe_Map.sCompressorRecipes;
+import static gregtech.api.util.GT_RecipeBuilder.SECONDS;
 
 import mods.railcraft.common.blocks.aesthetics.cube.EnumCube;
 import mods.railcraft.common.items.RailcraftToolItems;
@@ -22,57 +24,243 @@ public class CompressorRecipes implements Runnable {
 
     @Override
     public void run() {
-        GT_ModHandler.addCompressionRecipe(
-                getModItem(GTPlusPlus.modID, "blockRainforestOakSapling", 8, 0),
-                ItemList.IC2_Plantball.get(1));
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                getModItem(GTPlusPlus.modID, "blockRainforestOakSapling", 8, 0)
+            )
+            .itemOutputs(
+                ItemList.IC2_Plantball.get(1)
+            )
+            .noFluidInputs()
+            .noFluidOutputs()
+            .duration(15 * SECONDS)
+            .eut(2)
+            .addTo(sCompressorRecipes);
 
-        GT_Values.RA.addCompressorRecipe(
-                ItemList.IC2_Compressed_Coal_Chunk.get(1L),
-                ItemList.IC2_Industrial_Diamond.get(1L),
-                300,
-                2);
-        GT_ModHandler.addCompressionRecipe(
-                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Uranium, 1L),
-                GT_ModHandler.getIC2Item("Uran238", 1L));
-        GT_ModHandler.addCompressionRecipe(
-                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Uranium235, 1L),
-                GT_ModHandler.getIC2Item("Uran235", 1L));
-        GT_ModHandler.addCompressionRecipe(
-                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Plutonium, 1L),
-                GT_ModHandler.getIC2Item("Plutonium", 1L));
-        GT_ModHandler.addCompressionRecipe(
-                GT_OreDictUnificator.get(OrePrefixes.dustTiny, Materials.Uranium235, 1L),
-                GT_ModHandler.getIC2Item("smallUran235", 1L));
-        GT_ModHandler.addCompressionRecipe(
-                GT_OreDictUnificator.get(OrePrefixes.dustTiny, Materials.Plutonium, 1L),
-                GT_ModHandler.getIC2Item("smallPlutonium", 1L));
-        GT_ModHandler.addCompressionRecipe(new ItemStack(Blocks.ice, 2, 32767), new ItemStack(Blocks.packed_ice, 1, 0));
-        GT_ModHandler.addCompressionRecipe(
-                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ice, 1L),
-                new ItemStack(Blocks.ice, 1, 0));
-        GT_ModHandler.addCompressionRecipe(
-                GT_OreDictUnificator.get(OrePrefixes.gem, Materials.CertusQuartz, 4L),
-                getModItem(AppliedEnergistics2.modID, "tile.BlockQuartz", 1L));
-        GT_ModHandler.addCompressionRecipe(
-                getModItem(AppliedEnergistics2.modID, "item.ItemMultiMaterial", 8L, 10),
-                getModItem(AppliedEnergistics2.modID, "tile.BlockQuartz", 1L));
-        GT_ModHandler.addCompressionRecipe(
-                getModItem(AppliedEnergistics2.modID, "item.ItemMultiMaterial", 8L, 11),
-                new ItemStack(Blocks.quartz_block, 1, 0));
-        GT_ModHandler.addCompressionRecipe(
-                getModItem(AppliedEnergistics2.modID, "item.ItemMultiMaterial", 8L, 12),
-                getModItem(AppliedEnergistics2.modID, "tile.BlockFluix", 1L));
-        GT_ModHandler.addCompressionRecipe(new ItemStack(Items.quartz, 4, 0), new ItemStack(Blocks.quartz_block, 1, 0));
-        // GT_ModHandler.addCompressionRecipe(new ItemStack(Items.wheat, 9, 0), new ItemStack(Blocks.hay_block, 1, 0));
-        GT_ModHandler.addCompressionRecipe(
-                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Glowstone, 4L),
-                new ItemStack(Blocks.glowstone, 1));
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                ItemList.IC2_Compressed_Coal_Chunk.get(1)
+            )
+            .itemOutputs(
+                ItemList.IC2_Industrial_Diamond.get(1)
+            )
+            .noFluidInputs()
+            .noFluidOutputs()
+            .duration(15 * SECONDS)
+            .eut(2)
+            .addTo(sCompressorRecipes);
 
-        GT_Values.RA.addCompressorRecipe(Materials.Fireclay.getDust(1), ItemList.CompressedFireclay.get(1), 80, 4);
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Uranium, 1)
+            )
+            .itemOutputs(
+                GT_ModHandler.getIC2Item("Uran238", 1)
+            )
+            .noFluidInputs()
+            .noFluidOutputs()
+            .duration(15 * SECONDS)
+            .eut(2)
+            .addTo(sCompressorRecipes);
+
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Uranium235, 1)
+            )
+            .itemOutputs(
+                GT_ModHandler.getIC2Item("Uran235", 1)
+            )
+            .noFluidInputs()
+            .noFluidOutputs()
+            .duration(15 * SECONDS)
+            .eut(2)
+            .addTo(sCompressorRecipes);
+
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Plutonium, 1)
+            )
+            .itemOutputs(
+                GT_ModHandler.getIC2Item("Plutonium", 1)
+            )
+            .noFluidInputs()
+            .noFluidOutputs()
+            .duration(15 * SECONDS)
+            .eut(2)
+            .addTo(sCompressorRecipes);
+
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                GT_OreDictUnificator.get(OrePrefixes.dustTiny, Materials.Uranium235, 1)
+            )
+            .itemOutputs(
+                GT_ModHandler.getIC2Item("smallUran235", 1)
+            )
+            .noFluidInputs()
+            .noFluidOutputs()
+            .duration(15 * SECONDS)
+            .eut(2)
+            .addTo(sCompressorRecipes);
+
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                GT_OreDictUnificator.get(OrePrefixes.dustTiny, Materials.Plutonium, 1)
+            )
+            .itemOutputs(
+                GT_ModHandler.getIC2Item("smallPlutonium", 1)
+            )
+            .noFluidInputs()
+            .noFluidOutputs()
+            .duration(15 * SECONDS)
+            .eut(2)
+            .addTo(sCompressorRecipes);
+
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                new ItemStack(Blocks.ice, 2, 32767)
+            )
+            .itemOutputs(
+                new ItemStack(Blocks.packed_ice, 1, 0)
+            )
+            .noFluidInputs()
+            .noFluidOutputs()
+            .duration(15 * SECONDS)
+            .eut(2)
+            .addTo(sCompressorRecipes);
+
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ice, 1)
+            )
+            .itemOutputs(
+                new ItemStack(Blocks.ice, 1, 0)
+            )
+            .noFluidInputs()
+            .noFluidOutputs()
+            .duration(15 * SECONDS)
+            .eut(2)
+            .addTo(sCompressorRecipes);
+
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                GT_OreDictUnificator.get(OrePrefixes.gem, Materials.CertusQuartz, 4)
+            )
+            .itemOutputs(
+                getModItem(AppliedEnergistics2.modID, "tile.BlockQuartz", 1)
+            )
+            .noFluidInputs()
+            .noFluidOutputs()
+            .duration(15 * SECONDS)
+            .eut(2)
+            .addTo(sCompressorRecipes);
+
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                getModItem(AppliedEnergistics2.modID, "item.ItemMultiMaterial", 8L, 10)
+            )
+            .itemOutputs(
+                getModItem(AppliedEnergistics2.modID, "tile.BlockQuartz", 1)
+            )
+            .noFluidInputs()
+            .noFluidOutputs()
+            .duration(15 * SECONDS)
+            .eut(2)
+            .addTo(sCompressorRecipes);
+
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                getModItem(AppliedEnergistics2.modID, "item.ItemMultiMaterial", 8L, 11)
+            )
+            .itemOutputs(
+                new ItemStack(Blocks.quartz_block, 1, 0)
+            )
+            .noFluidInputs()
+            .noFluidOutputs()
+            .duration(15 * SECONDS)
+            .eut(2)
+            .addTo(sCompressorRecipes);
+
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                getModItem(AppliedEnergistics2.modID, "item.ItemMultiMaterial", 8L, 12)
+            )
+            .itemOutputs(
+                getModItem(AppliedEnergistics2.modID, "tile.BlockFluix", 1)
+            )
+            .noFluidInputs()
+            .noFluidOutputs()
+            .duration(15 * SECONDS)
+            .eut(2)
+            .addTo(sCompressorRecipes);
+
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                new ItemStack(Items.quartz, 4, 0)
+            )
+            .itemOutputs(
+                new ItemStack(Blocks.quartz_block, 1, 0)
+            )
+            .noFluidInputs()
+            .noFluidOutputs()
+            .duration(15 * SECONDS)
+            .eut(2)
+            .addTo(sCompressorRecipes);
+
+/*        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                new ItemStack(Items.wheat, 9, 0)
+            )
+            .itemOutputs(
+                new ItemStack(Blocks.hay_block, 1, 0)
+            )
+            .noFluidInputs()
+            .noFluidOutputs()
+            .duration(15 * SECONDS)
+            .eut(2)
+            .addTo(sCompressorRecipes);
+ */
+
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Glowstone, 4)
+            )
+            .itemOutputs(
+                new ItemStack(Blocks.glowstone, 1)
+            )
+            .noFluidInputs()
+            .noFluidOutputs()
+            .duration(15 * SECONDS)
+            .eut(2)
+            .addTo(sCompressorRecipes);
+
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                Materials.Fireclay.getDust(1)
+            )
+            .itemOutputs(
+                ItemList.CompressedFireclay.get(1)
+            )
+            .noFluidInputs()
+            .noFluidOutputs()
+            .duration(4 * SECONDS)
+            .eut(4)
+            .addTo(sCompressorRecipes);
+
         GameRegistry.addSmelting(ItemList.CompressedFireclay.get(1), ItemList.Firebrick.get(1), 0);
 
         if (Railcraft.isModLoaded()) {
-            GT_Values.RA.addCompressorRecipe(RailcraftToolItems.getCoalCoke(9), EnumCube.COKE_BLOCK.getItem(), 300, 2);
+            GT_Values.RA.stdBuilder()
+                .itemInputs(
+                    RailcraftToolItems.getCoalCoke(9)
+                )
+                .itemOutputs(
+                    EnumCube.COKE_BLOCK.getItem()
+                )
+                .noFluidInputs()
+                .noFluidOutputs()
+                .duration(15 * SECONDS)
+                .eut(2)
+                .addTo(sCompressorRecipes);
         }
     }
 }
