57efbef2ed2bfe5cd3bd01a5cc44e4dc344a771f
boubou19 <miisterunknown@gmail.com>
2023-04-01 01:53:25 +0200

    migrated packager recipes

--- src/main/java/gregtech/loaders/postload/recipes/PackagerRecipes.java
+++ src/main/java/gregtech/loaders/postload/recipes/PackagerRecipes.java
@@ -6,33 +6,68 @@ import gregtech.api.enums.Materials;
 import gregtech.api.enums.OrePrefixes;
 import gregtech.api.util.GT_OreDictUnificator;
 
+import static gregtech.api.util.GT_Recipe.GT_Recipe_Map.sBoxinatorRecipes;
+import static gregtech.api.util.GT_RecipeBuilder.SECONDS;
+import static gregtech.api.util.GT_RecipeBuilder.TICKS;
+
 public class PackagerRecipes implements Runnable {
 
     @Override
     public void run() {
-        GT_Values.RA.addBoxingRecipe(
-                ItemList.IC2_Scrap.get(9L),
-                ItemList.Schematic_3by3.get(0L),
-                ItemList.IC2_Scrapbox.get(1L),
-                16,
-                1);
-        GT_Values.RA.addBoxingRecipe(
-                ItemList.Food_Fries.get(1L),
-                GT_OreDictUnificator.get(OrePrefixes.plateDouble, Materials.Paper, 1L),
-                ItemList.Food_Packaged_Fries.get(1L),
-                64,
-                16);
-        GT_Values.RA.addBoxingRecipe(
-                ItemList.Food_PotatoChips.get(1L),
-                GT_OreDictUnificator.get(OrePrefixes.foil, Materials.Aluminium, 1L),
-                ItemList.Food_Packaged_PotatoChips.get(1L),
-                64,
-                16);
-        GT_Values.RA.addBoxingRecipe(
-                ItemList.Food_ChiliChips.get(1L),
-                GT_OreDictUnificator.get(OrePrefixes.foil, Materials.Aluminium, 1L),
-                ItemList.Food_Packaged_ChiliChips.get(1L),
-                64,
-                16);
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                ItemList.IC2_Scrap.get(9),
+                ItemList.Schematic_3by3.get(0)
+            )
+            .itemOutputs(
+                ItemList.IC2_Scrapbox.get(1)
+            )
+            .noFluidInputs()
+            .noFluidOutputs()
+            .duration(16 * TICKS)
+            .eut(1)
+            .addTo(sBoxinatorRecipes);
+
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                ItemList.Food_Fries.get(1),
+                GT_OreDictUnificator.get(OrePrefixes.plateDouble, Materials.Paper, 1)
+            )
+            .itemOutputs(
+                ItemList.Food_Packaged_Fries.get(1)
+            )
+            .noFluidInputs()
+            .noFluidOutputs()
+            .duration(3 * SECONDS + 4 * TICKS)
+            .eut(16)
+            .addTo(sBoxinatorRecipes);
+
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                ItemList.Food_PotatoChips.get(1),
+                GT_OreDictUnificator.get(OrePrefixes.foil, Materials.Aluminium, 1)
+            )
+            .itemOutputs(
+                ItemList.Food_Packaged_PotatoChips.get(1)
+            )
+            .noFluidInputs()
+            .noFluidOutputs()
+            .duration(3 * SECONDS + 4 * TICKS)
+            .eut(16)
+            .addTo(sBoxinatorRecipes);
+
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                ItemList.Food_ChiliChips.get(1),
+                GT_OreDictUnificator.get(OrePrefixes.foil, Materials.Aluminium, 1)
+            )
+            .itemOutputs(
+                ItemList.Food_Packaged_ChiliChips.get(1)
+            )
+            .noFluidInputs()
+            .noFluidOutputs()
+            .duration(3 * SECONDS + 4 * TICKS)
+            .eut(16)
+            .addTo(sBoxinatorRecipes);
     }
 }
