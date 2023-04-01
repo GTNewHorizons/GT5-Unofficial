d32623f050b2dd074a617ade9f3644d34c51ce54
boubou19 <miisterunknown@gmail.com>
2023-04-01 02:12:48 +0200

    migrated TPM recipes

--- src/main/java/gregtech/loaders/postload/recipes/TranscendentPlasmaMixerRecipes.java
+++ src/main/java/gregtech/loaders/postload/recipes/TranscendentPlasmaMixerRecipes.java
@@ -2,6 +2,7 @@ package gregtech.loaders.postload.recipes;
 
 import static gregtech.api.util.GT_Recipe.GT_Recipe_Map.sTranscendentPlasmaMixerRecipes;
 
+import gregtech.api.enums.GT_Values;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
 
@@ -15,6 +16,7 @@ public class TranscendentPlasmaMixerRecipes implements Runnable {
     private static final int RESPLENDENT_EU_PER_L = 269_326_451;
     private static final int EXOTIC_EU_PER_L = 1_073_007_393;
 
+    @Deprecated
     private static void addTranscendentPlasmaMixerRecipe(final FluidStack fluidOutput, final int circuitNumber,
             final FluidStack[] fluidInput, final int EUPerL) {
         sTranscendentPlasmaMixerRecipes.addRecipe(
@@ -32,53 +34,84 @@ public class TranscendentPlasmaMixerRecipes implements Runnable {
     @Override
     public void run() {
 
-        addTranscendentPlasmaMixerRecipe(
-                Materials.ExcitedDTCC.getFluid(1000L),
-                1,
-                new FluidStack[] { Materials.Helium.getPlasma(1000), Materials.Iron.getPlasma(1000),
-                        Materials.Calcium.getPlasma(1000), Materials.Niobium.getPlasma(1000) },
-                CRUDE_EU_PER_L);
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                GT_Utility.getIntegratedCircuit(1)
+            )
+            .noItemOutputs()
+            .fluidInputs(
+                Materials.ExcitedDTCC.getFluid(1000L)
+            )
+            .fluidOutputs(
+                Materials.Helium.getPlasma(1000),
+                Materials.Iron.getPlasma(1000),
+                Materials.Calcium.getPlasma(1000),
+                Materials.Niobium.getPlasma(1000))
+            .duration(100)
+            .eut(CRUDE_EU_PER_L)
+            .addTo(sTranscendentPlasmaMixerRecipes);
 
-        addTranscendentPlasmaMixerRecipe(
-                Materials.ExcitedDTPC.getFluid(1000L),
-                2,
-                new FluidStack[] { Materials.Helium.getPlasma(1000), Materials.Iron.getPlasma(1000),
-                        Materials.Calcium.getPlasma(1000), Materials.Niobium.getPlasma(1000),
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                GT_Utility.getIntegratedCircuit(2)
+            )
+            .noItemOutputs()
+            .fluidInputs(
+                Materials.ExcitedDTPC.getFluid(1000L)
+            )
+            .fluidOutputs(
+                Materials.Helium.getPlasma(1000),
+                Materials.Iron.getPlasma(1000),
+                Materials.Calcium.getPlasma(1000),
+                Materials.Niobium.getPlasma(1000),
+                Materials.Radon.getPlasma(1000),
+                Materials.Nickel.getPlasma(1000),
+                Materials.Boron.getPlasma(1000),
+                Materials.Sulfur.getPlasma(1000))
+            .duration(100)
+            .eut(PROSAIC_EU_PER_L)
+            .addTo(sTranscendentPlasmaMixerRecipes);
 
-                        Materials.Radon.getPlasma(1000), Materials.Nickel.getPlasma(1000),
-                        Materials.Boron.getPlasma(1000), Materials.Sulfur.getPlasma(1000),
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                GT_Utility.getIntegratedCircuit(3)
+            )
+            .noItemOutputs()
+            .fluidInputs(
+                Materials.ExcitedDTRC.getFluid(1000L)
+            )
+            .fluidOutputs(
+                Materials.Helium.getPlasma(1000), Materials.Iron.getPlasma(1000),
+                Materials.Calcium.getPlasma(1000), Materials.Niobium.getPlasma(1000),
+                Materials.Radon.getPlasma(1000), Materials.Nickel.getPlasma(1000),
+                Materials.Boron.getPlasma(1000), Materials.Sulfur.getPlasma(1000),
+                Materials.Nitrogen.getPlasma(1000), Materials.Zinc.getPlasma(1000),
+                Materials.Silver.getPlasma(1000), Materials.Titanium.getPlasma(1000))
+            .duration(100)
+            .eut(RESPLENDENT_EU_PER_L)
+            .addTo(sTranscendentPlasmaMixerRecipes);
 
-                },
-                PROSAIC_EU_PER_L);
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                GT_Utility.getIntegratedCircuit(4)
+            )
+            .noItemOutputs()
+            .fluidInputs(
+                Materials.ExcitedDTEC.getFluid(1000L)
+            )
+            .fluidOutputs(
+                Materials.Helium.getPlasma(1000), Materials.Iron.getPlasma(1000),
+                Materials.Calcium.getPlasma(1000), Materials.Niobium.getPlasma(1000),
+                Materials.Radon.getPlasma(1000), Materials.Nickel.getPlasma(1000),
+                Materials.Boron.getPlasma(1000), Materials.Sulfur.getPlasma(1000),
+                Materials.Nitrogen.getPlasma(1000), Materials.Zinc.getPlasma(1000),
+                Materials.Silver.getPlasma(1000), Materials.Titanium.getPlasma(1000),
+                Materials.Americium.getPlasma(1000), Materials.Bismuth.getPlasma(1000),
+                Materials.Oxygen.getPlasma(1000), Materials.Tin.getPlasma(1000))
+            .duration(100)
+            .eut(EXOTIC_EU_PER_L)
+            .addTo(sTranscendentPlasmaMixerRecipes);
 
-        addTranscendentPlasmaMixerRecipe(
-                Materials.ExcitedDTRC.getFluid(1000L),
-                3,
-                new FluidStack[] { Materials.Helium.getPlasma(1000), Materials.Iron.getPlasma(1000),
-                        Materials.Calcium.getPlasma(1000), Materials.Niobium.getPlasma(1000),
-
-                        Materials.Radon.getPlasma(1000), Materials.Nickel.getPlasma(1000),
-                        Materials.Boron.getPlasma(1000), Materials.Sulfur.getPlasma(1000),
-
-                        Materials.Nitrogen.getPlasma(1000), Materials.Zinc.getPlasma(1000),
-                        Materials.Silver.getPlasma(1000), Materials.Titanium.getPlasma(1000), },
-                RESPLENDENT_EU_PER_L);
-
-        addTranscendentPlasmaMixerRecipe(
-                Materials.ExcitedDTEC.getFluid(1000L),
-                4,
-                new FluidStack[] { Materials.Helium.getPlasma(1000), Materials.Iron.getPlasma(1000),
-                        Materials.Calcium.getPlasma(1000), Materials.Niobium.getPlasma(1000),
-
-                        Materials.Radon.getPlasma(1000), Materials.Nickel.getPlasma(1000),
-                        Materials.Boron.getPlasma(1000), Materials.Sulfur.getPlasma(1000),
-
-                        Materials.Nitrogen.getPlasma(1000), Materials.Zinc.getPlasma(1000),
-                        Materials.Silver.getPlasma(1000), Materials.Titanium.getPlasma(1000),
-
-                        Materials.Americium.getPlasma(1000), Materials.Bismuth.getPlasma(1000),
-                        Materials.Oxygen.getPlasma(1000), Materials.Tin.getPlasma(1000), },
-                EXOTIC_EU_PER_L);
 
     }
 }
