d479d87996736ec7402b9916cc1f4116a89bc316
boubou19 <miisterunknown@gmail.com>
2023-04-03 13:57:30 +0200

    depracte old strings

--- src/main/java/gregtech/api/enums/GT_Values.java
+++ src/main/java/gregtech/api/enums/GT_Values.java
@@ -190,17 +190,62 @@ public class GT_Values {
     /**
      * MOD ID Strings, since they are very common Parameters.
      */
-    public static final String MOD_ID = "gregtech", MOD_ID_IC2 = "IC2", MOD_ID_NC = "IC2NuclearControl",
-            MOD_ID_TC = "Thaumcraft", MOD_ID_TF = "TwilightForest", MOD_ID_RC = "Railcraft",
-            MOD_ID_TE = "ThermalExpansion", MOD_ID_AE = "appliedenergistics2", MOD_ID_TFC = "terrafirmacraft",
-            MOD_ID_PFAA = "PFAAGeologica", MOD_ID_FR = "Forestry", MOD_ID_HaC = "harvestcraft",
-            MOD_ID_APC = "AppleCore", MOD_ID_MaCr = "magicalcrops", MOD_ID_GaEn = "ganysend",
-            MOD_ID_GaSu = "ganyssurface", MOD_ID_GaNe = "ganysnether", MOD_ID_BC_SILICON = "BuildCraft|Silicon",
-            MOD_ID_BC_TRANSPORT = "BuildCraft|Transport", MOD_ID_BC_FACTORY = "BuildCraft|Factory",
-            MOD_ID_BC_ENERGY = "BuildCraft|Energy", MOD_ID_BC_BUILDERS = "BuildCraft|Builders",
-            MOD_ID_BC_CORE = "BuildCraft|Core", MOD_ID_GC_CORE = "GalacticraftCore",
-            MOD_ID_GC_MARS = "GalacticraftMars", MOD_ID_GC_PLANETS = "GalacticraftPlanets", MOD_ID_DC = "dreamcraft",
-            MOD_ID_GTPP = "miscutils";
+    @Deprecated
+    public static final String MOD_ID = "gregtech";
+    @Deprecated
+    public static final String MOD_ID_IC2 = "IC2";
+    @Deprecated
+    public static final String MOD_ID_NC = "IC2NuclearControl";
+    @Deprecated
+    public static final String MOD_ID_TC = "Thaumcraft";
+    @Deprecated
+    public static final String MOD_ID_TF = "TwilightForest";
+    @Deprecated
+    public static final String MOD_ID_RC = "Railcraft";
+    @Deprecated
+    public static final String MOD_ID_TE = "ThermalExpansion";
+    @Deprecated
+    public static final String MOD_ID_AE = "appliedenergistics2";
+    @Deprecated
+    public static final String MOD_ID_TFC = "terrafirmacraft";
+    @Deprecated
+    public static final String MOD_ID_PFAA = "PFAAGeologica";
+    @Deprecated
+    public static final String MOD_ID_FR = "Forestry";
+    @Deprecated
+    public static final String MOD_ID_HaC = "harvestcraft";
+    @Deprecated
+    public static final String MOD_ID_APC = "AppleCore";
+    @Deprecated
+    public static final String MOD_ID_MaCr = "magicalcrops";
+    @Deprecated
+    public static final String MOD_ID_GaEn = "ganysend";
+    @Deprecated
+    public static final String MOD_ID_GaSu = "ganyssurface";
+    @Deprecated
+    public static final String MOD_ID_GaNe = "ganysnether";
+    @Deprecated
+    public static final String MOD_ID_BC_SILICON = "BuildCraft|Silicon";
+    @Deprecated
+    public static final String MOD_ID_BC_TRANSPORT = "BuildCraft|Transport";
+    @Deprecated
+    public static final String MOD_ID_BC_FACTORY = "BuildCraft|Factory";
+    @Deprecated
+    public static final String MOD_ID_BC_ENERGY = "BuildCraft|Energy";
+    @Deprecated
+    public static final String MOD_ID_BC_BUILDERS = "BuildCraft|Builders";
+    @Deprecated
+    public static final String MOD_ID_BC_CORE = "BuildCraft|Core";
+    @Deprecated
+    public static final String MOD_ID_GC_CORE = "GalacticraftCore";
+    @Deprecated
+    public static final String MOD_ID_GC_MARS = "GalacticraftMars";
+    @Deprecated
+    public static final String MOD_ID_GC_PLANETS = "GalacticraftPlanets";
+    @Deprecated
+    public static final String MOD_ID_DC = "dreamcraft";
+    @Deprecated
+    public static final String MOD_ID_GTPP = "miscutils";
     /**
      * File Paths and Resource Paths
      */
