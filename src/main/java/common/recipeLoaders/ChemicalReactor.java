4a117a2be20c4a8366a1f94e8759872f328a02ca
boubou19 <miisterunknown@gmail.com>
2023-04-11 01:02:36 +0200

    RA2 for chemical reactor recipes

--- src/main/java/common/recipeLoaders/ChemicalReactor.java
+++ src/main/java/common/recipeLoaders/ChemicalReactor.java
@@ -1,5 +1,7 @@
 package common.recipeLoaders;
 
+import gregtech.api.enums.TierEU;
+import gregtech.api.util.GT_ModHandler;
 import kekztech.Items;
 
 import common.items.MetaItem_CraftingComponent;
@@ -7,6 +9,10 @@ import common.items.MetaItem_CraftingComponent;
 import gregtech.api.enums.GT_Values;
 import gregtech.api.enums.Materials;
 import gregtech.api.util.GT_Utility;
+import net.minecraft.item.ItemStack;
+
+import static gregtech.api.util.GT_RecipeBuilder.SECONDS;
+import static gregtech.api.util.GT_RecipeConstants.UniversalChemical;
 
 public class ChemicalReactor implements Runnable {
 
@@ -15,14 +21,19 @@ public class ChemicalReactor implements Runnable {
         final MetaItem_CraftingComponent craftingItem = MetaItem_CraftingComponent.getInstance();
 
         // Ceria Dust
-        GT_Values.RA.addChemicalRecipe(
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
                 Materials.Cerium.getDust(2),
-                GT_Utility.getIntegratedCircuit(6),
-                Materials.Oxygen.getGas(3000),
-                null,
-                craftingItem.getStackOfAmountFromDamage(Items.CeriaDust.getMetaID(), 2),
-                null,
-                400,
-                30);
+                GT_Utility.getIntegratedCircuit(6)
+            )
+            .itemOutputs(
+                craftingItem.getStackOfAmountFromDamage(Items.CeriaDust.getMetaID(), 2)
+            )
+            .fluidInputs(Materials.Oxygen.getGas(3000))
+            .noFluidOutputs()
+            .duration(20 * SECONDS)
+            .eut(TierEU.RECIPE_LV)
+            .addTo(UniversalChemical);
+
     }
 }
