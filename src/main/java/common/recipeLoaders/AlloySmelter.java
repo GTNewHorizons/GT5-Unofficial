dbf70394081c2666755d63d45001bfe4ddebfd41
boubou19 <miisterunknown@gmail.com>
2023-04-11 00:23:55 +0200

    RA2 for alloy smelter

--- src/main/java/common/recipeLoaders/AlloySmelter.java
+++ src/main/java/common/recipeLoaders/AlloySmelter.java
@@ -1,29 +1,35 @@
 package common.recipeLoaders;
 
+import gregtech.api.enums.TierEU;
 import kekztech.Items;
 
 import common.items.MetaItem_CraftingComponent;
-
-import cpw.mods.fml.common.Loader;
 import gregtech.api.enums.GT_Values;
 import gregtech.api.enums.ItemList;
 
 import static gregtech.api.enums.Mods.BartWorks;
+import static gregtech.api.util.GT_Recipe.GT_Recipe_Map.sAlloySmelterRecipes;
+import static gregtech.api.util.GT_RecipeBuilder.SECONDS;
 
 public class AlloySmelter implements Runnable {
 
     @Override
     public void run() {
-        final MetaItem_CraftingComponent craftingItem = MetaItem_CraftingComponent.getInstance();
+        if (BartWorks.isModLoaded()) {
+            final MetaItem_CraftingComponent craftingItem = MetaItem_CraftingComponent.getInstance();
 
-        // YSZ Cermic Plate
-        GT_Values.RA.addAlloySmelterRecipe(
-                craftingItem.getStackOfAmountFromDamage(
-                        Items.YSZCeramicDust.getMetaID(),
-                        BartWorks.isModLoaded() ? 3 : 10),
-                ItemList.Shape_Mold_Plate.get(0),
-                craftingItem.getStackOfAmountFromDamage(Items.YSZCeramicPlate.getMetaID(), 1),
-                400,
-                480);
+            // YSZ Cermic Plate
+            GT_Values.RA.stdBuilder()
+                .itemInputs(
+                    craftingItem.getStackOfAmountFromDamage(Items.YSZCeramicDust.getMetaID(),3),
+                    ItemList.Shape_Mold_Plate.get(0)
+                )
+                .itemOutputs(craftingItem.getStackOfAmountFromDamage(Items.YSZCeramicPlate.getMetaID(), 1))
+                .noFluidInputs()
+                .noFluidOutputs()
+                .duration(20*SECONDS)
+                .eut(TierEU.RECIPE_HV)
+                .addTo(sAlloySmelterRecipes);
+        }
     }
 }
