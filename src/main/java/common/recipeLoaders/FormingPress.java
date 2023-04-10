3b2832bb953b755fb474ba5d971cfa63e8956ed8
boubou19 <miisterunknown@gmail.com>
2023-04-11 01:03:33 +0200

    RA2 for forming press recipes

--- src/main/java/common/recipeLoaders/FormingPress.java
+++ src/main/java/common/recipeLoaders/FormingPress.java
@@ -1,11 +1,20 @@
 package common.recipeLoaders;
 
+import gregtech.api.enums.Materials;
+import gregtech.api.enums.OrePrefixes;
+import gregtech.api.enums.TierEU;
+import gregtech.api.util.GT_OreDictUnificator;
 import kekztech.Items;
 
 import common.items.MetaItem_CraftingComponent;
 
 import gregtech.api.enums.GT_Values;
 import gregtech.api.enums.ItemList;
+
+import static gregtech.api.enums.Mods.BuildCraftSilicon;
+import static gregtech.api.util.GT_ModHandler.getModItem;
+import static gregtech.api.util.GT_Recipe.GT_Recipe_Map.sPressRecipes;
+import static gregtech.api.util.GT_RecipeBuilder.SECONDS;
 
 public class FormingPress implements Runnable {
 
@@ -14,11 +23,18 @@ public class FormingPress implements Runnable {
         final MetaItem_CraftingComponent craftingItem = MetaItem_CraftingComponent.getInstance();
 
         // YSZ Ceramic Plate
-        GT_Values.RA.addFormingPressRecipe(
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
                 craftingItem.getStackOfAmountFromDamage(Items.GDCCeramicDust.getMetaID(), 10),
-                ItemList.Shape_Mold_Plate.get(0),
-                craftingItem.getStackOfAmountFromDamage(Items.GDCCeramicPlate.getMetaID(), 1),
-                800,
-                480);
+                ItemList.Shape_Mold_Plate.get(0)
+            )
+            .itemOutputs(
+                craftingItem.getStackOfAmountFromDamage(Items.GDCCeramicPlate.getMetaID(), 1)
+            )
+            .noFluidInputs()
+            .noFluidOutputs()
+            .duration(40 * SECONDS)
+            .eut(TierEU.RECIPE_HV)
+            .addTo(sPressRecipes);
     }
 }
