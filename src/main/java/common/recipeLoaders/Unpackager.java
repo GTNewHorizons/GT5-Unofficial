c803522e05d5ee1a70a74782fab7e4045f45cb9f
boubou19 <miisterunknown@gmail.com>
2023-04-11 01:08:48 +0200

    RA2 for unpacker recipes

--- src/main/java/common/recipeLoaders/Unpackager.java
+++ src/main/java/common/recipeLoaders/Unpackager.java
@@ -13,6 +13,9 @@ import gregtech.api.enums.TierEU;
 import gregtech.api.util.GT_ModHandler;
 import gregtech.api.util.GT_OreDictUnificator;
 
+import static gregtech.api.util.GT_Recipe.GT_Recipe_Map.sUnboxinatorRecipes;
+import static gregtech.api.util.GT_RecipeBuilder.SECONDS;
+
 public class Unpackager implements Runnable {
 
     @Override
@@ -20,125 +23,277 @@ public class Unpackager implements Runnable {
 
         // TFFT Recycling
         {
-            GT_Values.RA.addUnboxingRecipe(
+            GT_Values.RA.stdBuilder()
+                .itemInputs(
                     new ItemStack(Blocks.tfftStorageField, 1, 1),
-                    GT_OreDictUnificator.get(OrePrefixes.frameGt, Materials.CrudeSteel, 1),
-                    GT_OreDictUnificator.get(OrePrefixes.plate, Materials.CrudeSteel, 6),
-                    100,
-                    BW_Util.getMachineVoltageFromTier(3));
-            GT_Values.RA.addUnboxingRecipe(
+                    GT_OreDictUnificator.get(OrePrefixes.frameGt, Materials.CrudeSteel, 1)
+                )
+                .itemOutputs(
+                    GT_OreDictUnificator.get(OrePrefixes.plate, Materials.CrudeSteel, 6)
+                )
+                .noFluidInputs()
+                .noFluidOutputs()
+                .duration(5 * SECONDS)
+                .eut(TierEU.RECIPE_HV)
+                .addTo(sUnboxinatorRecipes);
+
+            GT_Values.RA.stdBuilder()
+                .itemInputs(
                     new ItemStack(Blocks.tfftStorageField, 1, 2),
-                    ItemList.Casing_Tank_1.get(1),
-                    GT_OreDictUnificator.get(OrePrefixes.plate, Materials.EnergeticSilver, 6),
-                    100,
-                    BW_Util.getMachineVoltageFromTier(3));
-            GT_Values.RA.addUnboxingRecipe(
+                    ItemList.Casing_Tank_1.get(1)
+                )
+                .itemOutputs(
+                    GT_OreDictUnificator.get(OrePrefixes.plate, Materials.EnergeticSilver, 6)
+                )
+                .noFluidInputs()
+                .noFluidOutputs()
+                .duration(5 * SECONDS)
+                .eut(TierEU.RECIPE_HV)
+                .addTo(sUnboxinatorRecipes);
+
+            GT_Values.RA.stdBuilder()
+                .itemInputs(
                     new ItemStack(Blocks.tfftStorageField, 1, 3),
-                    ItemList.Casing_Tank_3.get(1),
-                    GT_OreDictUnificator.get(OrePrefixes.plate, Materials.VividAlloy, 6),
-                    100,
-                    BW_Util.getMachineVoltageFromTier(3));
-            GT_Values.RA.addUnboxingRecipe(
+                    ItemList.Casing_Tank_3.get(1)
+                )
+                .itemOutputs(
+                    GT_OreDictUnificator.get(OrePrefixes.plate, Materials.VividAlloy, 6)
+                )
+                .noFluidInputs()
+                .noFluidOutputs()
+                .duration(5 * SECONDS)
+                .eut(TierEU.RECIPE_HV)
+                .addTo(sUnboxinatorRecipes);
+
+            GT_Values.RA.stdBuilder()
+                .itemInputs(
                     new ItemStack(Blocks.tfftStorageField, 1, 4),
-                    ItemList.Casing_Tank_5.get(1),
-                    GT_OreDictUnificator.get(OrePrefixes.plate, Materials.Enderium, 6),
-                    100,
-                    BW_Util.getMachineVoltageFromTier(3));
-            GT_Values.RA.addUnboxingRecipe(
+                    ItemList.Casing_Tank_5.get(1)
+                )
+                .itemOutputs(
+                    GT_OreDictUnificator.get(OrePrefixes.plate, Materials.Enderium, 6)
+                )
+                .noFluidInputs()
+                .noFluidOutputs()
+                .duration(5 * SECONDS)
+                .eut(TierEU.RECIPE_HV)
+                .addTo(sUnboxinatorRecipes);
+
+            GT_Values.RA.stdBuilder()
+                .itemInputs(
                     new ItemStack(Blocks.tfftStorageField, 1, 5),
-                    ItemList.Casing_Tank_7.get(1),
-                    GT_OreDictUnificator.get(OrePrefixes.plate, Materials.CrystallineAlloy, 6),
-                    100,
-                    BW_Util.getMachineVoltageFromTier(3));
-            GT_Values.RA.addUnboxingRecipe(
+                    ItemList.Casing_Tank_7.get(1)
+                )
+                .itemOutputs(
+                    GT_OreDictUnificator.get(OrePrefixes.plate, Materials.CrystallineAlloy, 6)
+                )
+                .noFluidInputs()
+                .noFluidOutputs()
+                .duration(5 * SECONDS)
+                .eut(TierEU.RECIPE_HV)
+                .addTo(sUnboxinatorRecipes);
+
+            GT_Values.RA.stdBuilder()
+                .itemInputs(
                     new ItemStack(Blocks.tfftStorageField, 1, 6),
-                    ItemList.Casing_Tank_7.get(4),
-                    GT_OreDictUnificator.get(OrePrefixes.plateQuadruple, Materials.CrystallinePinkSlime, 6),
-                    100,
-                    BW_Util.getMachineVoltageFromTier(3));
-            GT_Values.RA.addUnboxingRecipe(
+                    ItemList.Casing_Tank_7.get(4)
+                )
+                .itemOutputs(
+                    GT_OreDictUnificator.get(OrePrefixes.plateQuadruple, Materials.CrystallinePinkSlime, 6)
+                )
+                .noFluidInputs()
+                .noFluidOutputs()
+                .duration(5 * SECONDS)
+                .eut(TierEU.RECIPE_HV)
+                .addTo(sUnboxinatorRecipes);
+
+            GT_Values.RA.stdBuilder()
+                .itemInputs(
                     new ItemStack(Blocks.tfftStorageField, 1, 7),
-                    ItemList.Casing_Tank_10.get(16),
-                    GT_OreDictUnificator.get(OrePrefixes.plateQuadruple, Materials.MelodicAlloy, 6),
-                    100,
-                    BW_Util.getMachineVoltageFromTier(3));
-            GT_Values.RA.addUnboxingRecipe(
+                    ItemList.Casing_Tank_10.get(16)
+                )
+                .itemOutputs(
+                    GT_OreDictUnificator.get(OrePrefixes.plateQuadruple, Materials.MelodicAlloy, 6)
+                )
+                .noFluidInputs()
+                .noFluidOutputs()
+                .duration(5 * SECONDS)
+                .eut(TierEU.RECIPE_HV)
+                .addTo(sUnboxinatorRecipes);
+
+            GT_Values.RA.stdBuilder()
+                .itemInputs(
                     new ItemStack(Blocks.tfftStorageField, 1, 8),
-                    ItemList.Quantum_Tank_IV.get(1),
-                    GT_OreDictUnificator.get(OrePrefixes.plateDense, Materials.StellarAlloy, 12),
-                    100,
-                    BW_Util.getMachineVoltageFromTier(3));
-            GT_Values.RA.addUnboxingRecipe(
+                    ItemList.Quantum_Tank_IV.get(1)
+                )
+                .itemOutputs(
+                    GT_OreDictUnificator.get(OrePrefixes.plateDense, Materials.StellarAlloy, 12)
+                )
+                .noFluidInputs()
+                .noFluidOutputs()
+                .duration(5 * SECONDS)
+                .eut(TierEU.RECIPE_HV)
+                .addTo(sUnboxinatorRecipes);
+
+            GT_Values.RA.stdBuilder()
+                .itemInputs(
                     new ItemStack(Blocks.tfftStorageField, 1, 9),
-                    ItemList.Quantum_Tank_IV.get(4),
-                    GT_OreDictUnificator.get(OrePrefixes.plateDense, Materials.TranscendentMetal, 12),
-                    100,
-                    BW_Util.getMachineVoltageFromTier(3));
-            GT_Values.RA.addUnboxingRecipe(
+                    ItemList.Quantum_Tank_IV.get(4)
+                )
+                .itemOutputs(
+                    GT_OreDictUnificator.get(OrePrefixes.plateDense, Materials.TranscendentMetal, 12)
+                )
+                .noFluidInputs()
+                .noFluidOutputs()
+                .duration(5 * SECONDS)
+                .eut(TierEU.RECIPE_HV)
+                .addTo(sUnboxinatorRecipes);
+
+            GT_Values.RA.stdBuilder()
+                .itemInputs(
                     new ItemStack(Blocks.tfftStorageField, 1, 10),
-                    ItemList.Quantum_Tank_IV.get(16),
-                    GT_OreDictUnificator.get(OrePrefixes.plateDense, Materials.SpaceTime, 12),
-                    100,
-                    BW_Util.getMachineVoltageFromTier(3));
+                    ItemList.Quantum_Tank_IV.get(16)
+                )
+                .itemOutputs(
+                    GT_OreDictUnificator.get(OrePrefixes.plateDense, Materials.SpaceTime, 12)
+                )
+                .noFluidInputs()
+                .noFluidOutputs()
+                .duration(5 * SECONDS)
+                .eut(TierEU.RECIPE_HV)
+                .addTo(sUnboxinatorRecipes);
+
         }
 
         // Capacitor recycling
         {
-            // Capacitor recycling
-            GT_Values.RA.addUnboxingRecipe(
+
+            GT_Values.RA.stdBuilder()
+                .itemInputs(
                     new ItemStack(Blocks.lscLapotronicEnergyUnit, 1, 7),
-                    GT_ModHandler.getIC2Item("lapotronCrystal", 1L, 26),
-                    new ItemStack(Blocks.lscLapotronicEnergyUnit, 1, 6),
-                    1200,
-                    (int) TierEU.RECIPE_LV);
-            GT_Values.RA.addUnboxingRecipe(
+                    GT_ModHandler.getIC2Item("lapotronCrystal", 1L, 26)
+                )
+                .itemOutputs(
+                    new ItemStack(Blocks.lscLapotronicEnergyUnit, 1, 6)
+                )
+                .noFluidInputs()
+                .noFluidOutputs()
+                .duration(60 * SECONDS)
+                .eut(TierEU.RECIPE_LV)
+                .addTo(sUnboxinatorRecipes);
+
+            GT_Values.RA.stdBuilder()
+                .itemInputs(
                     new ItemStack(Blocks.lscLapotronicEnergyUnit, 1, 1),
-                    ItemList.Energy_LapotronicOrb.get(1L),
-                    new ItemStack(Blocks.lscLapotronicEnergyUnit, 1, 6),
-                    1200,
-                    (int) TierEU.RECIPE_LV);
-            GT_Values.RA.addUnboxingRecipe(
+                    ItemList.Energy_LapotronicOrb.get(1L)
+                )
+                .itemOutputs(
+                    new ItemStack(Blocks.lscLapotronicEnergyUnit, 1, 6)
+                )
+                .noFluidInputs()
+                .noFluidOutputs()
+                .duration(60 * SECONDS)
+                .eut(TierEU.RECIPE_LV)
+                .addTo(sUnboxinatorRecipes);
+
+            GT_Values.RA.stdBuilder()
+                .itemInputs(
                     new ItemStack(Blocks.lscLapotronicEnergyUnit, 1, 2),
-                    ItemList.Energy_LapotronicOrb2.get(1L),
-                    GT_OreDictUnificator.get(OrePrefixes.screw, Materials.Osmiridium, 24),
-                    1200,
-                    (int) TierEU.RECIPE_LV);
-            GT_Values.RA.addUnboxingRecipe(
+                    ItemList.Energy_LapotronicOrb2.get(1L)
+                )
+                .itemOutputs(
+                    GT_OreDictUnificator.get(OrePrefixes.screw, Materials.Osmiridium, 24)
+                )
+                .noFluidInputs()
+                .noFluidOutputs()
+                .duration(60 * SECONDS)
+                .eut(TierEU.RECIPE_LV)
+                .addTo(sUnboxinatorRecipes);
+
+            GT_Values.RA.stdBuilder()
+                .itemInputs(
                     new ItemStack(Blocks.lscLapotronicEnergyUnit, 1, 3),
-                    ItemList.Energy_Module.get(1L),
-                    GT_OreDictUnificator.get(OrePrefixes.screw, Materials.NaquadahAlloy, 24),
-                    1200,
-                    (int) TierEU.RECIPE_LV);
-            GT_Values.RA.addUnboxingRecipe(
+                    ItemList.Energy_Module.get(1L)
+                )
+                .itemOutputs(
+                    GT_OreDictUnificator.get(OrePrefixes.screw, Materials.NaquadahAlloy, 24)
+                )
+                .noFluidInputs()
+                .noFluidOutputs()
+                .duration(60 * SECONDS)
+                .eut(TierEU.RECIPE_LV)
+                .addTo(sUnboxinatorRecipes);
+
+            GT_Values.RA.stdBuilder()
+                .itemInputs(
                     new ItemStack(Blocks.lscLapotronicEnergyUnit, 1, 4),
-                    ItemList.Energy_Cluster.get(1L),
-                    GT_OreDictUnificator.get(OrePrefixes.screw, Materials.Neutronium, 24),
-                    1200,
-                    (int) TierEU.RECIPE_LV);
-            GT_Values.RA.addUnboxingRecipe(
+                    ItemList.Energy_Cluster.get(1L)
+                )
+                .itemOutputs(
+                    GT_OreDictUnificator.get(OrePrefixes.screw, Materials.Neutronium, 24)
+                )
+                .noFluidInputs()
+                .noFluidOutputs()
+                .duration(60 * SECONDS)
+                .eut(TierEU.RECIPE_LV)
+                .addTo(sUnboxinatorRecipes);
+
+            GT_Values.RA.stdBuilder()
+                .itemInputs(
                     new ItemStack(Blocks.lscLapotronicEnergyUnit, 1, 5),
-                    ItemList.ZPM3.get(1L),
-                    GT_OreDictUnificator.get(OrePrefixes.screw, Materials.CosmicNeutronium, 24),
-                    1200,
-                    (int) TierEU.RECIPE_LV);
-            GT_Values.RA.addUnboxingRecipe(
+                    ItemList.ZPM3.get(1L)
+                )
+                .itemOutputs(
+                    GT_OreDictUnificator.get(OrePrefixes.screw, Materials.CosmicNeutronium, 24)
+                )
+                .noFluidInputs()
+                .noFluidOutputs()
+                .duration(60 * SECONDS)
+                .eut(TierEU.RECIPE_LV)
+                .addTo(sUnboxinatorRecipes);
+
+            GT_Values.RA.stdBuilder()
+                .itemInputs(
                     new ItemStack(Blocks.lscLapotronicEnergyUnit, 1, 8),
-                    ItemList.ZPM4.get(1L),
-                    GT_OreDictUnificator.get(OrePrefixes.screw, Materials.Infinity, 24),
-                    1200,
-                    (int) TierEU.RECIPE_LV);
-            GT_Values.RA.addUnboxingRecipe(
+                    ItemList.ZPM4.get(1L)
+                )
+                .itemOutputs(
+                    GT_OreDictUnificator.get(OrePrefixes.screw, Materials.Infinity, 24)
+                )
+                .noFluidInputs()
+                .noFluidOutputs()
+                .duration(60 * SECONDS)
+                .eut(TierEU.RECIPE_LV)
+                .addTo(sUnboxinatorRecipes);
+
+            GT_Values.RA.stdBuilder()
+                .itemInputs(
                     new ItemStack(Blocks.lscLapotronicEnergyUnit, 1, 9),
-                    ItemList.ZPM5.get(1L),
-                    GT_OreDictUnificator.get(OrePrefixes.screw, Materials.TranscendentMetal, 24),
-                    1200,
-                    (int) TierEU.RECIPE_LV);
-            GT_Values.RA.addUnboxingRecipe(
+                    ItemList.ZPM5.get(1L)
+                )
+                .itemOutputs(
+                    GT_OreDictUnificator.get(OrePrefixes.screw, Materials.TranscendentMetal, 24)
+                )
+                .noFluidInputs()
+                .noFluidOutputs()
+                .duration(60 * SECONDS)
+                .eut(TierEU.RECIPE_LV)
+                .addTo(sUnboxinatorRecipes);
+
+            GT_Values.RA.stdBuilder()
+                .itemInputs(
                     new ItemStack(Blocks.lscLapotronicEnergyUnit, 1, 10),
-                    ItemList.ZPM6.get(1L),
-                    GT_OreDictUnificator.get(OrePrefixes.screw, Materials.SpaceTime, 24),
-                    1200,
-                    (int) TierEU.RECIPE_LV);
+                    ItemList.ZPM6.get(1L)
+                )
+                .itemOutputs(
+                    GT_OreDictUnificator.get(OrePrefixes.screw, Materials.SpaceTime, 24)
+                )
+                .noFluidInputs()
+                .noFluidOutputs()
+                .duration(60 * SECONDS)
+                .eut(TierEU.RECIPE_LV)
+                .addTo(sUnboxinatorRecipes);
+
         }
     }
 }
