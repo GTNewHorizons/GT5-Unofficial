d0d82610185d9f6db0624fd150b4a1dc29cb1307
Glease <4586901+Glease@users.noreply.github.com>
2023-04-12 20:19:19 +0800

    spotless be like

--- src/main/java/gregtech/common/tileentities/generators/GT_MetaTileEntity_LightningRod.java
+++ src/main/java/gregtech/common/tileentities/generators/GT_MetaTileEntity_LightningRod.java
@@ -101,8 +101,9 @@ public class GT_MetaTileEntity_LightningRod extends GT_MetaTileEntity_TieredMach
                         .increaseStoredEnergyUnits(maxEUStore() - aBaseMetaTileEntity.getStoredEU(), false);
                     aWorld.addWeatherEffect(new EntityLightningBolt(aWorld, aX, aY + aRodValue, aZ));
                     // randomly break a rod
-                    if (aWorld.isThundering())
+                    if (aWorld.isThundering()) {
                         aWorld.setBlockToAir(aX, aY + XSTR_INSTANCE.nextInt(aRodValue) + 1, aZ);
+                    }
                 }
             }
         }
