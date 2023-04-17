55372fef17fed3e71bf37a900d1b4077c8f8f55f
GDCloud <gdcloudstrike@gmail.com>
2023-04-17 20:20:03 +0200

    TPM recipe

--- src/main/java/gregtech/loaders/postload/recipes/TranscendentPlasmaMixerRecipes.java
+++ src/main/java/gregtech/loaders/postload/recipes/TranscendentPlasmaMixerRecipes.java
@@ -1,8 +1,10 @@
 package gregtech.loaders.postload.recipes;
 
+import static gregtech.api.enums.Mods.GTPlusPlus;
 import static gregtech.api.util.GT_Recipe.GT_Recipe_Map.sTranscendentPlasmaMixerRecipes;
 
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
 
 import gregtech.api.enums.GT_Values;
@@ -15,6 +17,7 @@ public class TranscendentPlasmaMixerRecipes implements Runnable {
     private static final int PROSAIC_EU_PER_L = 66_768_460;
     private static final int RESPLENDENT_EU_PER_L = 269_326_451;
     private static final int EXOTIC_EU_PER_L = 1_073_007_393;
+    private static final int STELLAR_EU_PER_HALF_L = 2_137_858_448;
 
     @Deprecated
     private static void addTranscendentPlasmaMixerRecipe(final FluidStack fluidOutput, final int circuitNumber,
@@ -113,6 +116,38 @@ public class TranscendentPlasmaMixerRecipes implements Runnable {
             .eut(EXOTIC_EU_PER_L)
             .noOptimize()
             .addTo(sTranscendentPlasmaMixerRecipes);
+
+        if (GTPlusPlus.isModLoaded()) {
+            GT_Values.RA.stdBuilder()
+                .itemInputs(GT_Utility.getIntegratedCircuit(5))
+                .noItemOutputs()
+                .fluidInputs(
+                    Materials.Helium.getPlasma(1000),
+                    Materials.Iron.getPlasma(1000),
+                    Materials.Calcium.getPlasma(1000),
+                    Materials.Niobium.getPlasma(1000),
+                    Materials.Radon.getPlasma(1000),
+                    Materials.Nickel.getPlasma(1000),
+                    Materials.Boron.getPlasma(1000),
+                    Materials.Sulfur.getPlasma(1000),
+                    Materials.Nitrogen.getPlasma(1000),
+                    Materials.Zinc.getPlasma(1000),
+                    Materials.Silver.getPlasma(1000),
+                    Materials.Titanium.getPlasma(1000),
+                    Materials.Americium.getPlasma(1000),
+                    Materials.Bismuth.getPlasma(1000),
+                    Materials.Oxygen.getPlasma(1000),
+                    Materials.Tin.getPlasma(1000),
+                    new FluidStack(FluidRegistry.getFluid("plasma.neptunium"), 1000),
+                    new FluidStack(FluidRegistry.getFluid("plasma.fermium"), 1000),
+                    Materials.Plutonium241.getPlasma(1000L),
+                    Materials.RawStarMatter.getFluid(1000L))
+                .fluidOutputs(Materials.ExcitedDTSC.getFluid(1000L))
+                .duration(200)
+                .eut(STELLAR_EU_PER_HALF_L)
+                .noOptimize()
+                .addTo(sTranscendentPlasmaMixerRecipes);
+        }
 
     }
 }
