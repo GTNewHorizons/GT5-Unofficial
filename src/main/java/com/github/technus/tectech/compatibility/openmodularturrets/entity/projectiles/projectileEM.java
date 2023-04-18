bf2adf120b7db5d6a1de9624fbf26b8ebfa698b6
glowredman <35727266+glowredman@users.noreply.github.com>
2023-04-18 21:42:11 +0200

    Add default case

--- src/main/java/com/github/technus/tectech/compatibility/openmodularturrets/entity/projectiles/projectileEM.java
+++ src/main/java/com/github/technus/tectech/compatibility/openmodularturrets/entity/projectiles/projectileEM.java
@@ -165,6 +165,8 @@ public class projectileEM extends LaserProjectile {
                                     true);
                         }
                         break;
+                    default:
+                        break;
                 }
             }
             setDead();
