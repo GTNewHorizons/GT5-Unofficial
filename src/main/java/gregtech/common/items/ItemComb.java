c1b4bc30296133a23a5d47e216e5282249b608aa
Martin Robertz <dream-master@gmx.net>
2023-04-20 09:15:04 +0200

    comment it out

--- src/main/java/gregtech/common/items/ItemComb.java
+++ src/main/java/gregtech/common/items/ItemComb.java
@@ -1740,16 +1740,6 @@ public class ItemComb extends Item implements IGT_ItemWithMaterialRenderer {
         if (aItem.length > 6) {
             chance = Arrays.copyOf(chance, 6);
         }
-
-        GT_Values.RA.stdBuilder()
-            .itemInputs(tComb)
-            .itemOutputs(aItem)
-            .outputChances(chance)
-            .noFluidInputs()
-            .noFluidOutputs()
-            .duration(duration)
-            .eut(volt.getSimpleEnergy())
-            .addTo(sCentrifugeRecipes);
     }
 
     public void registerOreDict() {
