d057f306c3a575ff3f5e128af376068338bab517
glowredman <35727266+glowredman@users.noreply.github.com>
2023-04-23 22:08:53 +0200

    Adjust color, enable ores only

--- src/main/java/gregtech/api/enums/OrePrefixes.java
+++ src/main/java/gregtech/api/enums/OrePrefixes.java
@@ -1227,7 +1227,6 @@ public enum OrePrefixes {
                         || aMaterial == Materials.YttriumBariumCuprate
                         || aMaterial == Materials.NiobiumTitanium
                         || aMaterial == Materials.CertusQuartz
-                        || aMaterial == Materials.CertusQuartzCharged
                         || aMaterial == Materials.NetherQuartz
                         || aMaterial == Materials.Lazurite
                         || aMaterial == Materials.Lapis
