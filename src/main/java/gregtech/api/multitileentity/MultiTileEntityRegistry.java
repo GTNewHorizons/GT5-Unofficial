026060a5d671ce85b670271e9d0ea1feead6555d
BlueWeabo <76872108+BlueWeabo@users.noreply.github.com>
2023-05-03 17:24:57 +0300

    allow mute registers to be made outside of gt5u

--- src/main/java/gregtech/api/multitileentity/MultiTileEntityRegistry.java
+++ src/main/java/gregtech/api/multitileentity/MultiTileEntityRegistry.java
@@ -22,7 +22,6 @@ import com.gtnewhorizon.gtnhlib.util.map.ItemStackMap;
 
 import appeng.core.CreativeTab;
 import cpw.mods.fml.common.registry.GameRegistry;
-import gregtech.api.GregTech_API;
 import gregtech.api.enums.GT_Values;
 import gregtech.api.multitileentity.base.MultiTileEntity;
 import gregtech.api.multitileentity.interfaces.IMultiTileEntity;
@@ -70,8 +69,6 @@ public class MultiTileEntityRegistry {
      * @param aNameInternal the internal Name of the Item
      */
     public MultiTileEntityRegistry(String aNameInternal, MultiTileEntityBlockInternal aBlock) {
-        if (!GregTech_API.sPreloadStarted || GregTech_API.sPreloadFinished) throw new IllegalStateException(
-            "The MultiTileEntity Registry must be initialised during Preload Phase and not before");
         mNameInternal = aNameInternal;
         mBlock = aBlock;
         GT_FML_LOGGER.info(aNameInternal + " " + Block.getIdFromBlock(aBlock) + "This is the answer");
