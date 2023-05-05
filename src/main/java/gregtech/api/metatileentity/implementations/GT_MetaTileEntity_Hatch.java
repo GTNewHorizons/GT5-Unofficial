228a726aaed50cd35346ed65369f1c1fddd2cd26
Raven Szewczyk <git@eigenraven.me>
2023-05-05 19:06:25 +0100

    Display multiblock name&icon in AE2 interface terminal and crafting tree view instead of the hatch (#1944)
    * Update buildscript
    
    * Apply spotless
    
    * Update ae2 and other dependencies
    
    * Display multiblock name&icon in AE2 interface terminal and crafting tree view instead of the hatch
    
    * Remove no longer needed Avaritia dep
    
    ---------
    
    Co-authored-by: Martin Robertz <dream-master@gmx.net>

--- src/main/java/gregtech/api/metatileentity/implementations/GT_MetaTileEntity_Hatch.java
+++ src/main/java/gregtech/api/metatileentity/implementations/GT_MetaTileEntity_Hatch.java
@@ -1,8 +1,10 @@
 package gregtech.api.metatileentity.implementations;
 
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.common.util.ForgeDirection;
 
+import appeng.api.crafting.ICraftingIconProvider;
 import gregtech.api.enums.GT_Values;
 import gregtech.api.enums.Textures;
 import gregtech.api.interfaces.ITexture;
@@ -11,7 +13,7 @@ import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
 /**
  * Handles texture changes internally. No special calls are necessary other than updateTexture in add***ToMachineList.
  */
-public abstract class GT_MetaTileEntity_Hatch extends GT_MetaTileEntity_BasicTank {
+public abstract class GT_MetaTileEntity_Hatch extends GT_MetaTileEntity_BasicTank implements ICraftingIconProvider {
 
     public enum ConnectionType {
         CABLE,
@@ -27,6 +29,8 @@ public abstract class GT_MetaTileEntity_Hatch extends GT_MetaTileEntity_BasicTan
 
     private byte mTexturePage = 0;
     private byte actualTexture = 0;
+
+    private ItemStack ae2CraftingIcon;
 
     public GT_MetaTileEntity_Hatch(int aID, String aName, String aNameRegional, int aTier, int aInvSlotCount,
         String aDescription, ITexture... aTextures) {
@@ -115,6 +119,19 @@ public abstract class GT_MetaTileEntity_Hatch extends GT_MetaTileEntity_BasicTan
     public final void updateTexture(int id) {
         onValueUpdate((byte) id);
         onTexturePageUpdate((byte) (id >> 7));
+    }
+
+    /**
+     * Sets the icon for the owning multiblock used for AE2 crafting display of attached interfaces, called on add to
+     * machine list
+     */
+    public final void updateCraftingIcon(ItemStack icon) {
+        this.ae2CraftingIcon = icon;
+    }
+
+    @Override
+    public ItemStack getMachineCraftingIcon() {
+        return this.ae2CraftingIcon;
     }
 
     /**
