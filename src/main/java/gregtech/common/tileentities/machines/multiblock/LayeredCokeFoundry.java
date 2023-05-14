8dd3d1a2847daee4400f925c613c2d786d498825
Daniel Mendes <steelux7@gmail.com>
2023-05-14 18:52:57 +0100

    Buildable stackable structure

--- src/main/java/gregtech/common/tileentities/machines/multiblock/LayeredCokeFoundry.java
+++ src/main/java/gregtech/common/tileentities/machines/multiblock/LayeredCokeFoundry.java
@@ -1,7 +1,6 @@
 package gregtech.common.tileentities.machines.multiblock;
 
-import static com.gtnewhorizon.structurelib.structure.StructureUtility.ofChain;
-import static com.gtnewhorizon.structurelib.structure.StructureUtility.transpose;
+import static com.gtnewhorizon.structurelib.structure.StructureUtility.*;
 import static gregtech.api.multitileentity.multiblock.base.MultiBlockPart.ENERGY_IN;
 import static gregtech.api.multitileentity.multiblock.base.MultiBlockPart.FLUID_IN;
 import static gregtech.api.multitileentity.multiblock.base.MultiBlockPart.FLUID_OUT;
@@ -9,6 +8,10 @@ import static gregtech.api.multitileentity.multiblock.base.MultiBlockPart.ITEM_I
 import static gregtech.api.multitileentity.multiblock.base.MultiBlockPart.ITEM_OUT;
 import static gregtech.api.multitileentity.multiblock.base.MultiBlockPart.NOTHING;
 
+import gregtech.api.GregTech_API;
+import gregtech.api.enums.Materials;
+import gregtech.api.multitileentity.enums.GT_MultiTileCasing;
+import gregtech.api.util.GT_StructureUtility;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 
@@ -28,7 +31,14 @@ import gregtech.api.util.GT_Recipe;
 import gregtech.api.util.GT_Recipe.GT_Recipe_Map;
 
 public class LayeredCokeFoundry extends StackableController<LayeredCokeFoundry> {
-    private static IStructureDefinition<LayeredCokeFoundry> STRUCTURE_DEFINITION = null;
+    private static IStructureDefinition<LayeredCokeFoundry> STRUCTURE_DEFINITION_MEGA = null;
+    protected static final String STRUCTURE_PIECE_BASE = "T1";
+    private static final Vec3Impl STRUCTURE_OFFSET_BASE = new Vec3Impl(2, 2, 0);
+    private static final Vec3Impl STRUCTURE_OFFSET_MEGA_POSITION = new Vec3Impl(4, 7, -4);
+    private static final Vec3Impl STRUCTURE_OFFSET_MEGA_START = new Vec3Impl(0, 0, -3);
+    private static final Vec3Impl STRUCTURE_OFFSET_MEGA_STACK = new Vec3Impl(0, 0, -2);
+    private static final Vec3Impl STRUCTURE_OFFSET_MEGA_STOP = new Vec3Impl(0, 0, -1);
+    private boolean isMega = true;
 
     public LayeredCokeFoundry() {
         super();
@@ -40,31 +50,121 @@ public class LayeredCokeFoundry extends StackableController<LayeredCokeFoundry> 
     }
 
     @Override
+    public Vec3Impl getStartingStructureOffset() {
+        return STRUCTURE_OFFSET_BASE;
+    }
+
+    @Override
+    public void construct(ItemStack trigger, boolean hintsOnly) {
+        final int blueprintCount = (trigger.stackSize - 1) + getMinStacks();
+        final int stackCount = Math.min(blueprintCount, getMaxStacks());
+
+        buildState.startBuilding(getStartingStructureOffset());
+        buildPiece(STRUCTURE_PIECE_BASE, trigger, hintsOnly, buildState.getCurrentOffset());
+        buildState.addOffset(getMegaPositionOffset());
+
+        if (stackCount >= 2) {
+            buildPiece(STACKABLE_BOTTOM, trigger, hintsOnly, buildState.getCurrentOffset());
+            buildState.addOffset(getStartingStackOffset());
+
+            for (int i = 0; i < stackCount; i++) {
+                buildPiece(STACKABLE_MIDDLE, trigger, hintsOnly, buildState.getCurrentOffset());
+                buildState.addOffset(getPerStackOffset());
+            }
+            if (hasTop()) {
+                buildPiece(STACKABLE_TOP, trigger, hintsOnly, buildState.stopBuilding());
+            } else {
+                buildState.stopBuilding();
+            }
+        } else {
+            buildState.stopBuilding();
+        }
+
+
+    }
+
+    @Override
     public IStructureDefinition<LayeredCokeFoundry> getStructureDefinition() {
-        if (STRUCTURE_DEFINITION == null) {
-            STRUCTURE_DEFINITION = StructureDefinition.<LayeredCokeFoundry>builder()
-                .addShape(STACKABLE_TOP, transpose(new String[][] { { " CCC ", "CCCCC", "CCCCC", "CCCCC", " CCC " }, }))
+        if (STRUCTURE_DEFINITION_MEGA == null) {
+            STRUCTURE_DEFINITION_MEGA = StructureDefinition.<LayeredCokeFoundry>builder()
+                .addShape(
+                    STRUCTURE_PIECE_BASE,
+                    transpose(
+                        new String[][]{
+                            {" AAA ","AAAAA","AAAAA","AAAAA"},
+                            {" AAA ","A   A","A   A","AAAAA"},
+                            {" A~A ","A   A","A   A","AAAAA"},
+                            {" AAA ","A   A","A   A","AAAAA"},
+                            {" AAA ","AAAAA","AAAAA","AAAAA"}
+                        }))
+                .addShape(
+                    STACKABLE_TOP,
+                    transpose(
+                        new String[][]{
+                            {"ADADADADADADA","AAAAAAAAAAAAA"},
+                            {" B B B B B B ","A A A A A A A"},
+                            {"CB B B B B BC","ACACACACACACA"},
+                            {" B B B B B B ","A A A A A A A"},
+                            {" B B B B B B ","A A A A A A A"},
+                            {" B B B B B B ","A A A A A A A"},
+                            {" B B B B B B ","A A A A A A A"},
+                            {" B B B B B B ","A A A A A A A"},
+                            {" B B B B B B ","A A A A A A A"},
+                            {"CB B B B B BC","ACACACACACACA"},
+                            {" B B B B B B ","A A A A A A A"},
+                            {"AAAAAAAAAAAAA","AAAAAAAAAAAAA"}
+                        }))
                 .addShape(
                     STACKABLE_MIDDLE,
-                    transpose(new String[][] { { "  BBB  ", " B---B ", "DC---CD", " B---B ", "  BBB  " }, }))
+                    transpose(
+                        new String[][]{
+                            {"ADAAAAAAAAADA","AAAAAAAAAAAAA"},
+                            {" B         B ","A           A"},
+                            {"CB         BC","A           A"},
+                            {" B         B ","A           A"},
+                            {" B         B ","A           A"},
+                            {" B         B ","A           A"},
+                            {" B         B ","A           A"},
+                            {" B         B ","A           A"},
+                            {" B         B ","A           A"},
+                            {"CB         BC","A           A"},
+                            {" B         B ","A           A"},
+                            {"AAAAAAAAAAAAA","AAAAAAAAAAAAA"}
+                        }))
                 .addShape(
                     STACKABLE_BOTTOM,
-                    transpose(new String[][] { { " G~F ", "AAAAA", "AAAAA", "AAAAA", " AAA " }, }))
-                .addElement('A', ofChain(addMultiTileCasing("gt.multitileentity.casings", getCasingMeta(), ENERGY_IN)))
+                    transpose(
+                        new String[][]{
+                            {"AAAAAAAAAAAAA","ADADADADADADA","AAAAAAAAAAAAA"},
+                            {"A A A A A A A"," B B B B B B ","A           A"},
+                            {"ACACACACACACA","CB B B B B BC","A           A"},
+                            {"A A A A A A A"," B B B B B B ","A           A"},
+                            {"A A A A A A A"," B B B B B B ","A           A"},
+                            {"A A A A A A A"," B B B B B B ","A           A"},
+                            {"A A A A A A A"," B B B B B B ","A           A"},
+                            {"A A A A A A A"," B B B B B B ","A           A"},
+                            {"A A A A A A A"," B B B B B B ","A           A"},
+                            {"ACACA A ACACA","CB B B B B BC","A           A"},
+                            {"A A A A A A A"," B B B B B B ","A           A"},
+                            {"AAAAAAAAAAAAA","AAAAAAAAAAAAA","AAAAAAAAAAAAA"}
+                        }))
+                .addElement(
+                    'A',
+                    addMultiTileCasing(
+                        "gt.multitileentity.casings",
+                        GT_MultiTileCasing.Chemical.getId(),
+                        FLUID_IN | ITEM_IN | FLUID_OUT | ITEM_OUT | ENERGY_IN))
                 .addElement(
                     'B',
-                    ofChain(
-                        addMultiTileCasing(
-                            "gt.multitileentity.casings",
-                            getCasingMeta(),
-                            FLUID_IN | ITEM_IN | FLUID_OUT | ITEM_OUT)))
-                .addElement('C', addMultiTileCasing("gt.multitileentity.casings", getCasingMeta(), NOTHING))
-                .addElement('D', addMultiTileCasing("gt.multitileentity.casings", getCasingMeta(), NOTHING))
-                .addElement('F', addMotorCasings(NOTHING))
-                .addElement('G', addMultiTileCasing("gt.multitileentity.component.casings", 10000, NOTHING))
+                    addMultiTileCasing(
+                        "gt.multitileentity.casings",
+                        getCasingMeta(),
+                        FLUID_IN | ITEM_IN | FLUID_OUT | ITEM_OUT | ENERGY_IN))
+                .addElement('C', ofBlock(GregTech_API.sBlockCasings4, 1))
+                .addElement('D', GT_StructureUtility.ofFrame(Materials.Steel))
                 .build();
         }
-        return STRUCTURE_DEFINITION;
+        return STRUCTURE_DEFINITION_MEGA;
     }
 
     @Override
@@ -112,24 +212,23 @@ public class LayeredCokeFoundry extends StackableController<LayeredCokeFoundry> 
         return 10;
     }
 
-    @Override
-    public Vec3Impl getStartingStructureOffset() {
-        return new Vec3Impl(2, 0, 0);
+    public Vec3Impl getMegaPositionOffset() {
+        return STRUCTURE_OFFSET_MEGA_POSITION;
     }
 
     @Override
     public Vec3Impl getStartingStackOffset() {
-        return new Vec3Impl(1, 1, 0);
+        return STRUCTURE_OFFSET_MEGA_START;
     }
 
     @Override
     public Vec3Impl getPerStackOffset() {
-        return new Vec3Impl(0, 1, 0);
+        return STRUCTURE_OFFSET_MEGA_STACK;
     }
 
     @Override
     public Vec3Impl getAfterLastStackOffset() {
-        return new Vec3Impl(-1, 0, 0);
+        return STRUCTURE_OFFSET_MEGA_STOP;
     }
 
     @Override
