3bd55ef0edc33bbde932811c41ac6da9eeefe748
miozune <miozune@gmail.com>
2023-05-21 01:51:13 +0900

    Fix typo in SteamPower

--- src/main/java/gregtech/common/power/SteamPower.java
+++ src/main/java/gregtech/common/power/SteamPower.java
@@ -39,7 +39,7 @@ public class SteamPower extends Power {
     @Override
     public String getPowerUsageString() {
         // 2L normal steam == 1EU
-        return GT_Utility.formatNumbers(20L * 2L * recipeEuPerTick) + " L/s  Steam";
+        return GT_Utility.formatNumbers(20L * 2L * recipeEuPerTick) + " L/s Steam";
     }
 
     @Override
