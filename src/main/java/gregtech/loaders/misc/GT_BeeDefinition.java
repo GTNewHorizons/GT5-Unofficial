8909f87c7b1f3234453d1c355bfc55a7ac9148c0
Runakai1 <Runatson@gmail.com>
2023-06-07 11:44:19 +0200

    Add comment

--- src/main/java/gregtech/loaders/misc/GT_BeeDefinition.java
+++ src/main/java/gregtech/loaders/misc/GT_BeeDefinition.java
@@ -2355,6 +2355,7 @@ public enum GT_BeeDefinition implements IBeeDefinition {
         AlleleHelper.instance.set(template, FLOWERING, Flowering.AVERAGE);
     }, dis -> {
         IBeeMutationCustom tMutation = dis.registerMutation(OIL, INFINITYCATALYST, 4);
+        //UHV Replicator (UU-Matter)
         tMutation.requireResource(GameRegistry.findBlock(GregTech.ID, "gt.blockmachines"), 11003);
     }),
 
