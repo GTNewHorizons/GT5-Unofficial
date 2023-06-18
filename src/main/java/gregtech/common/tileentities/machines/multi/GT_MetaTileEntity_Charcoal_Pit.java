3e8ecc1c8c230d80a4dd712120b1a9b9b691087e
minecraft7771 <maxim235@gmx.de>
2023-06-18 18:48:58 +0200

    Migrated charcoal pit

--- src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_Charcoal_Pit.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_Charcoal_Pit.java
@@ -68,7 +68,7 @@ public class GT_MetaTileEntity_Charcoal_Pit extends GT_MetaTileEntity_TooltipMul
     }
 
     @Override
-    public boolean checkRecipe(ItemStack aStack) {
+    protected boolean checkRecipe() {
         if (!checkRecursiveBlocks()) {
             mEfficiency = 0;
             mEfficiencyIncrease = 0;
