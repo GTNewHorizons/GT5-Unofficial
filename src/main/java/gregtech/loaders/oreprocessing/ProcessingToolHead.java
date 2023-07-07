d115b2ed5de8faf5fdd4bf7ef565098efe87240d
DotJason <SOleg413@mail.ru>
2023-07-07 20:16:32 +0300

    clean code style

--- src/main/java/gregtech/loaders/oreprocessing/ProcessingToolHead.java
+++ src/main/java/gregtech/loaders/oreprocessing/ProcessingToolHead.java
@@ -1179,8 +1179,7 @@ public class ProcessingToolHead implements gregtech.api.interfaces.IOreRecipeReg
                             GT_Utility.getIntegratedCircuit(14))
                         .itemOutputs(
                             GT_MetaGenerated_Tool_01.INSTANCE.getToolWithStats(
-                                aProducesSoftMallet
-                                    ? GT_MetaGenerated_Tool_01.SOFTMALLET
+                                aProducesSoftMallet ? GT_MetaGenerated_Tool_01.SOFTMALLET
                                     : GT_MetaGenerated_Tool_01.HARDHAMMER,
                                 1,
                                 aMaterial,
@@ -1195,8 +1194,7 @@ public class ProcessingToolHead implements gregtech.api.interfaces.IOreRecipeReg
                 if ((aMaterial != Materials.Stone) && (aMaterial != Materials.Flint)) {
                     GT_ModHandler.addShapelessCraftingRecipe(
                         GT_MetaGenerated_Tool_01.INSTANCE.getToolWithStats(
-                            aProducesSoftMallet
-                                ? GT_MetaGenerated_Tool_01.SOFTMALLET
+                            aProducesSoftMallet ? GT_MetaGenerated_Tool_01.SOFTMALLET
                                 : GT_MetaGenerated_Tool_01.HARDHAMMER,
                             1,
                             aMaterial,
@@ -1206,8 +1204,7 @@ public class ProcessingToolHead implements gregtech.api.interfaces.IOreRecipeReg
                         new Object[] { aOreDictName, OrePrefixes.stick.get(aMaterial.mHandleMaterial) });
                     GT_ModHandler.addCraftingRecipe(
                         GT_MetaGenerated_Tool_01.INSTANCE.getToolWithStats(
-                            aProducesSoftMallet
-                                ? GT_MetaGenerated_Tool_01.SOFTMALLET
+                            aProducesSoftMallet ? GT_MetaGenerated_Tool_01.SOFTMALLET
                                 : GT_MetaGenerated_Tool_01.HARDHAMMER,
                             1,
                             aMaterial,
@@ -1220,8 +1217,7 @@ public class ProcessingToolHead implements gregtech.api.interfaces.IOreRecipeReg
                             'S', OrePrefixes.stick.get(aMaterial.mHandleMaterial) });
                     GT_ModHandler.addCraftingRecipe(
                         GT_MetaGenerated_Tool_01.INSTANCE.getToolWithStats(
-                            aProducesSoftMallet
-                                ? GT_MetaGenerated_Tool_01.SOFTMALLET
+                            aProducesSoftMallet ? GT_MetaGenerated_Tool_01.SOFTMALLET
                                 : GT_MetaGenerated_Tool_01.HARDHAMMER,
                             1,
                             aMaterial,
