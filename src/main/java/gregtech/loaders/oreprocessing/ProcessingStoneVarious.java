5e8e8bc1de5548deb44c5761b972eea7cf2b1f55
chochem <40274384+chochem@users.noreply.github.com>
2023-07-22 15:14:56 +0100

    remove old base gt recipe that we dont want

--- src/main/java/gregtech/loaders/oreprocessing/ProcessingStoneVarious.java
+++ src/main/java/gregtech/loaders/oreprocessing/ProcessingStoneVarious.java
@@ -1,14 +1,8 @@
 package gregtech.loaders.oreprocessing;
 
-import static gregtech.api.util.GT_Recipe.GT_Recipe_Map.sAssemblerRecipes;
-import static gregtech.api.util.GT_RecipeBuilder.SECONDS;
-
-import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 
-import gregtech.api.enums.GT_Values;
 import gregtech.api.enums.OrePrefixes;
-import gregtech.api.util.GT_Utility;
 
 public class ProcessingStoneVarious implements gregtech.api.interfaces.IOreRecipeRegistrator {
 
@@ -26,25 +20,6 @@ public class ProcessingStoneVarious implements gregtech.api.interfaces.IOreRecip
     @Override
     public void registerOre(OrePrefixes aPrefix, gregtech.api.enums.Materials aMaterial, String aOreDictName,
         String aModName, ItemStack aStack) {
-        if (aPrefix == OrePrefixes.stoneSmooth) {
-
-            GT_Values.RA.stdBuilder()
-                .itemInputs(GT_Utility.copyAmount(1L, aStack), GT_Utility.getIntegratedCircuit(1))
-                .itemOutputs(new ItemStack(Blocks.stone_button, 1))
-                .noFluidInputs()
-                .noFluidOutputs()
-                .duration(5 * SECONDS)
-                .eut(4)
-                .addTo(sAssemblerRecipes);
-
-            GT_Values.RA.stdBuilder()
-                .itemInputs(GT_Utility.copyAmount(2L, aStack), GT_Utility.getIntegratedCircuit(2))
-                .itemOutputs(new ItemStack(Blocks.stone_pressure_plate, 1))
-                .noFluidInputs()
-                .noFluidOutputs()
-                .duration(5 * SECONDS)
-                .eut(4)
-                .addTo(sAssemblerRecipes);
-        }
+        // no recipes currently.
     }
 }
