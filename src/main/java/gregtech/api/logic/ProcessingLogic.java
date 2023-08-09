d5c31725276514343f892f4acfee4f74af87e573
BlueWeabo <ilia.iliev2005@gmail.com>
2023-08-09 18:42:45 +0900

    Add a Generic Processing Logic and extract methods (#1992)
    * add a generic processing logic
    
    * calculate tier in another method
    
    * calculate power logic in another method

--- src/main/java/gregtech/api/logic/ProcessingLogic.java
+++ src/main/java/gregtech/api/logic/ProcessingLogic.java
@@ -133,6 +133,36 @@ public class ProcessingLogic {
         return this;
     }
 
+    public ProcessingLogic setAmperage(long ampere) {
+        this.ampere = ampere;
+        return this;
+    }
+
+    public ProcessingLogic setVoltage(long voltage) {
+        this.voltage = voltage;
+        return this;
+    }
+
+    public ProcessingLogic setController(Controller<?> controller) {
+        this.controller = controller;
+        return this;
+    }
+
+    public ProcessingLogic setVoidProtection(boolean voidProtection) {
+        this.voidProtection = voidProtection;
+        return this;
+    }
+
+    public ProcessingLogic setPerfectOverclock(boolean perfectOverclock) {
+        this.perfectOverclock = perfectOverclock;
+        return this;
+    }
+
+    public ProcessingLogic setMaxParallel(int maxParallel) {
+        this.maxParallel = maxParallel;
+        return this;
+    }
+
     /**
      * Sets method to get max amount of parallel.
      */
