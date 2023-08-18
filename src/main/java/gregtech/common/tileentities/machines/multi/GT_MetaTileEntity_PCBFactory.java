c009412fcf4f7febc984dc9eecfdf8e21efddaf1
Eraldoe <Eraldoe@users.noreply.github.com>
2023-08-17 23:32:59 -0700

    Correct PCB Factory Energy Hatch description (#2237)
    * Correct PCB Factory Energy Hatch description
    
    Changes the PCB factory description (the one seen when holding shift) which currently says "Energy Hatches: 1+"
    
    I believe this is incorrect and that the correct description is 1-2 energy hatches or 1 TT energy hatch.
    
    I believe the PCB factory uses this, which checks for 1-2 or 1 TT:
    
    public boolean checkExoticAndNormalEnergyHatches() {
            if (mExoticEnergyHatches.isEmpty() && mEnergyHatches.isEmpty()) {
                return false;
            }
    
            if (!mExoticEnergyHatches.isEmpty()) {
                if (!mEnergyHatches.isEmpty()) {
                    return false;
                }
    
                if (mExoticEnergyHatches.size() != 1) {
                    return false;
                }
            }
    
            return mEnergyHatches.size() <= 2;
        }
    
    * gradlew spotlessApply

--- src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_PCBFactory.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_PCBFactory.java
@@ -883,7 +883,19 @@ public class GT_MetaTileEntity_PCBFactory extends
             .beginStructureBlock(30, 38, 13, false)
             .addSeparator()
             .addMaintenanceHatch(EnumChatFormatting.GOLD + "1", 1)
-            .addEnergyHatch(EnumChatFormatting.GOLD + "1" + EnumChatFormatting.GRAY + "+", 1)
+            .addEnergyHatch(
+                EnumChatFormatting.GOLD + "1"
+                    + EnumChatFormatting.GRAY
+                    + "-"
+                    + EnumChatFormatting.GOLD
+                    + "2"
+                    + EnumChatFormatting.GRAY
+                    + " or "
+                    + EnumChatFormatting.GOLD
+                    + "1"
+                    + EnumChatFormatting.GRAY
+                    + " TT energy hatch.",
+                1)
             .addInputBus(EnumChatFormatting.GOLD + "1" + EnumChatFormatting.GRAY + "+", 1)
             .addOutputBus(EnumChatFormatting.GOLD + "1" + EnumChatFormatting.GRAY + "+", 1)
             .addInputHatch(EnumChatFormatting.GOLD + "1" + EnumChatFormatting.GRAY + "+", 1)
