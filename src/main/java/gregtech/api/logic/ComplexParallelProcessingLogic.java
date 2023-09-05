e4fab31cf2680072bdb053b558466a96455ed8ce
BlueWeabo <76872108+BlueWeabo@users.noreply.github.com>
2023-09-05 14:01:56 +0300

    fix up complex processing logic

--- src/main/java/gregtech/api/logic/ComplexParallelProcessingLogic.java
+++ src/main/java/gregtech/api/logic/ComplexParallelProcessingLogic.java
@@ -2,110 +2,38 @@ package gregtech.api.logic;
 
 import java.util.stream.LongStream;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
 
-import gregtech.GT_Mod;
-import gregtech.api.util.GT_OverclockCalculator;
-import gregtech.api.util.GT_ParallelHelper;
-import gregtech.api.util.GT_Recipe;
+import gregtech.api.recipe.check.FindRecipeResult;
+import gregtech.api.util.GT_Recipe.GT_Recipe_Map;
 
 public class ComplexParallelProcessingLogic<P extends ComplexParallelProcessingLogic<P>> extends ProcessingLogic<P> {
 
-    protected GT_Recipe.GT_Recipe_Map recipeMap;
     protected boolean hasPerfectOverclock;
     protected int maxComplexParallels;
     protected ItemStack[][] outputItems;
-    protected ItemStack[][] inputItems;
-    protected FluidStack[][] inputFluids;
     protected FluidStack[][] outputFluids;
-    protected long[] availableEut;
-    protected long[] eut;
+    protected long[] calculatedEutValues;
     protected int[] durations;
-    protected boolean[] isItemVoidProtected;
-    protected boolean[] isFluidVoidProtected;
+    protected int[] progresses;
     protected boolean isCleanroom;
 
-    public ComplexParallelProcessingLogic() {
-        this(null, 0);
-    }
-
-    public ComplexParallelProcessingLogic(int maxComplexParallels) {
-        this(null, maxComplexParallels);
-    }
-
-    public ComplexParallelProcessingLogic(GT_Recipe.GT_Recipe_Map recipeMap, int maxComplexParallels) {
-        this.maxComplexParallels = maxComplexParallels;
-        this.recipeMap = recipeMap;
-        inputItems = new ItemStack[maxComplexParallels][];
-        outputItems = new ItemStack[maxComplexParallels][];
-        inputFluids = new FluidStack[maxComplexParallels][];
-        outputFluids = new FluidStack[maxComplexParallels][];
-        eut = new long[maxComplexParallels];
-        availableEut = new long[maxComplexParallels];
-        durations = new int[maxComplexParallels];
-        isItemVoidProtected = new boolean[maxComplexParallels];
-        isFluidVoidProtected = new boolean[maxComplexParallels];
-    }
-
-    public boolean process(int index) {
-        if (recipeMap == null) {
-            return false;
-        }
-        GT_Recipe recipe = recipeMap
-            .findRecipe(null, false, false, availableEut[index], inputFluids[index], inputItems[index]);
-        if (recipe == null) {
-            return false;
-        }
-
-        if (recipe.mSpecialValue == -200 && GT_Mod.gregtechproxy.mEnableCleanroom && !isCleanroom) {
-            return false;
-        }
-
-        GT_ParallelHelper helper = new GT_ParallelHelper().setRecipe(recipe)
-            .setItemInputs(inputItems[index])
-            .setFluidInputs(inputFluids[index])
-            .setAvailableEUt(availableEut[index])
-            .setMachine(null, isItemVoidProtected[index], isFluidVoidProtected[index])
-            .setConsumption(true)
-            .setOutputCalculation(true);
-
-        helper.build();
-
-        if (helper.getCurrentParallel() <= 0) {
-            return false;
-        }
-
-        GT_OverclockCalculator calculator = new GT_OverclockCalculator().setRecipeEUt(recipe.mEUt)
-            .setDuration(recipe.mDuration)
-            .setEUt(availableEut[index]);
-
-        if (hasPerfectOverclock) {
-            calculator.enablePerfectOC();
-        }
-
-        if (calculator.getConsumption() == Long.MAX_VALUE - 1 || calculator.getDuration() == Integer.MAX_VALUE - 1) {
-            return false;
-        }
-
-        durations[index] = calculator.getDuration();
-        eut[index] = calculator.getConsumption();
-        outputItems[index] = helper.getItemOutputs();
-        outputFluids[index] = helper.getFluidOutputs();
-
-        return true;
-    }
-
-    public int getDuration(int index) {
-        if (index >= 0 && index < maxComplexParallels) {
-            return durations[index];
-        }
-        return 0;
-    }
-
-    public long getTotalEU() {
-        return LongStream.of(eut)
-            .sum();
+    @Override
+    @Nonnull
+    protected FindRecipeResult findRecipe(@Nullable GT_Recipe_Map map) {
+        if (map == null) return FindRecipeResult.NOT_FOUND;
+        return map.findRecipeWithResult(
+            lastRecipe,
+            false,
+            false,
+            amperageOC ? availableVoltage * availableAmperage : availableVoltage,
+            inputFluids,
+            specialSlotItem,
+            inputItems);
     }
 
     public ItemStack[] getOutputItems(int index) {
@@ -120,5 +48,55 @@ public class ComplexParallelProcessingLogic<P extends ComplexParallelProcessingL
             return outputFluids[index];
         }
         return null;
+    }
+
+    @Override
+    public boolean canWork() {
+        boolean allBusy = true;
+        for (int i = 0; i < maxComplexParallels; i++) {
+            allBusy &= progresses[i] < durations[i];
+        }
+        return !allBusy;
+    }
+
+    @Override
+    public P clear() {
+        // TODO Auto-generated method stub
+        return super.clear();
+    }
+
+    @Override
+    public long getCalculatedEut() {
+        return LongStream.of(this.calculatedEutValues)
+            .sum();
+    }
+
+    @Override
+    public void progress() {
+        for (int i = 0; i < maxComplexParallels; i++) {
+            if (progresses[i] == durations[i]) {
+                progresses[i] = 0;
+                durations[i] = 0;
+                output(i);
+            }
+            progresses[i] = progresses[i] + 1;
+        }
+    }
+
+    @Override
+    public void startCheck() {
+        for (int i = 0; i < maxComplexParallels; i++) {
+            if (durations[i] > 0) continue;
+            recipeResult = process();
+            calculatedEutValues[i] = calculatedEut;
+            durations[i] = duration;
+            progresses[i] = 0;
+        }
+    }
+
+    protected void output(int index) {
+        setOutputItems(getOutputItems(index));
+        setOutputFluids(getOutputFluids(index));
+        output();
     }
 }
