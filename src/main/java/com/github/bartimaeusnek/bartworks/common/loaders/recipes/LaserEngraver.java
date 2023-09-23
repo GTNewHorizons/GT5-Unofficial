af3a8361c437e216742712b79a9ba11f135714ba
chochem <40274384+chochem@users.noreply.github.com>
2023-09-23 13:46:35 +0100

    fix plasmids (#362)
    Former-commit-id: 24ffa333701e695b2ec6f39e5bd93be81b806127

--- src/main/java/com/github/bartimaeusnek/bartworks/common/loaders/recipes/LaserEngraver.java
+++ src/main/java/com/github/bartimaeusnek/bartworks/common/loaders/recipes/LaserEngraver.java
@@ -6,6 +6,8 @@ import static gregtech.api.util.GT_RecipeBuilder.SECONDS;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
+import com.github.bartimaeusnek.bartworks.common.loaders.BioItemList;
+
 import gregtech.api.enums.GT_Values;
 import gregtech.api.enums.TierEU;
 import gregtech.api.util.GT_Utility;
@@ -16,7 +18,8 @@ public class LaserEngraver implements Runnable {
     public void run() {
 
         GT_Values.RA.stdBuilder().itemInputs(new ItemStack(Items.emerald), GT_Utility.getIntegratedCircuit(17))
-                .duration(5 * SECONDS).eut(TierEU.RECIPE_LV).addTo(sLaserEngraverRecipes);
+                .itemOutputs(BioItemList.getPlasmidCell(null)).duration(5 * SECONDS).eut(TierEU.RECIPE_LV)
+                .addTo(sLaserEngraverRecipes);
 
     }
 }
