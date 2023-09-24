052775aca81c365085ca3485aba0d38021868bf2
Nxer <306803661@qq.com>
2023-09-24 17:12:57 +0800

    spotless

--- src/main/java/com/elisis/gtnhlanth/loader/RecipeLoader.java
+++ src/main/java/com/elisis/gtnhlanth/loader/RecipeLoader.java
@@ -2071,9 +2071,8 @@ public class RecipeLoader {
 
         // TODO Cerium-doped Lutetium Aluminium Garnet (Ce:LuAG)
         /**
-         * 1/9 Ce + 3 Lu + 5 Sapphire = 8 LuAG Blend
-         * 1/9 Ce + 3 Lu + 10 Green Sapphire = 8 LuAG Blend
-         * 2/9 Ce + 6 Lu + 25 Alumina + 9 Oxygen = 12 LuAG Blend
+         * 1/9 Ce + 3 Lu + 5 Sapphire = 8 LuAG Blend 1/9 Ce + 3 Lu + 10 Green Sapphire = 8 LuAG Blend 2/9 Ce + 6 Lu + 25
+         * Alumina + 9 Oxygen = 12 LuAG Blend
          *
          * 1 Ce + 60 Lu + 100 Sapphire = 160 LuAG Blend 1 Ce + 60 Lu +200 Green Sapphire = 160 LuAG Blend
          *
