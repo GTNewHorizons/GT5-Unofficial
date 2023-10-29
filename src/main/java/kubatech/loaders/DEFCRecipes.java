65c9f07759c09809c54037eb3d7a8889abdc56c0
kuba6000 <kuba.123123.6000@gmail.com>
2023-10-29 14:58:00 +0100

    RA2

--- src/main/java/kubatech/loaders/DEFCRecipes.java
+++ src/main/java/kubatech/loaders/DEFCRecipes.java
@@ -2,6 +2,8 @@ package kubatech.loaders;
 
 import static gregtech.api.enums.GT_Values.E;
 import static gregtech.api.enums.Mods.GregTech;
+import static gregtech.api.util.GT_Recipe.GT_Recipe_Map.sAssemblerRecipes;
+import static gregtech.api.util.GT_Recipe.GT_Recipe_Map.sMixerRecipes;
 
 import java.util.HashSet;
 
@@ -47,70 +49,84 @@ public class DEFCRecipes {
 
         // Dragonblood recipe for magics haters
         if (LoaderReference.GTPlusPlus) {
-            GT_Values.RA.addMixerRecipe(
-                new ItemStack[] { new ItemStack(Blocks.dragon_egg, 1),
+            GT_Values.RA.stdBuilder()
+                .itemInputs(
+                    new ItemStack(Blocks.dragon_egg, 1),
                     GT_OreDictUnificator.get(OrePrefixes.dust, Materials.DraconiumAwakened, 64L),
-                    GT_OreDictUnificator.get(OrePrefixes.dust, Materials.DraconiumAwakened, 64L) },
-                new FluidStack[] { Materials.Radon.getPlasma(144) },
-                new ItemStack[] { GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 8L) },
-                new FluidStack[] { new FluidStack(FluidRegistry.getFluid("molten.dragonblood"), 288) },
-                14000,
-                1966080);
+                    GT_OreDictUnificator.get(OrePrefixes.dust, Materials.DraconiumAwakened, 64L))
+                .fluidInputs(Materials.Radon.getPlasma(144))
+                .itemOutputs(GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 8L))
+                .fluidOutputs(new FluidStack(FluidRegistry.getFluid("molten.dragonblood"), 288))
+                .eut(1_966_080)
+                .duration(14_000)
+                .addTo(sMixerRecipes);
         }
 
         // Casings
 
-        GT_Values.RA.addAssemblerRecipe(
-            new ItemStack[] { GT_OreDictUnificator.get(OrePrefixes.frameGt, Materials.NaquadahAlloy, 6L),
-                GT_OreDictUnificator.get(OrePrefixes.plateDense, Materials.NaquadahAlloy, 6L) },
-            Materials.Void.getMolten(1152L),
-            kubatech.api.enums.ItemList.DEFCCasingBase.get(1),
-            24000,
-            491520);
-        GT_Values.RA.addAssemblerRecipe(
-            new ItemStack[] { GT_ModHandler.getModItem("dreamcraft", "tile.BloodyIchorium", 1, 0),
-                GT_OreDictUnificator.get(OrePrefixes.plateDense, Materials.Osmiridium, 6L) },
-            Materials.Void.getMolten(1152L),
-            kubatech.api.enums.ItemList.DEFCCasingT1.get(1),
-            24000,
-            491520);
-        GT_Values.RA.addAssemblerRecipe(
-            new ItemStack[] { kubatech.api.enums.ItemList.DEFCCasingT1.get(1),
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                GT_OreDictUnificator.get(OrePrefixes.frameGt, Materials.NaquadahAlloy, 6L),
+                GT_OreDictUnificator.get(OrePrefixes.plateDense, Materials.NaquadahAlloy, 6L))
+            .fluidInputs(Materials.Void.getMolten(1152L))
+            .itemOutputs(kubatech.api.enums.ItemList.DEFCCasingBase.get(1))
+            .eut(491_520)
+            .duration(24000)
+            .addTo(sAssemblerRecipes);
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                GT_ModHandler.getModItem("dreamcraft", "tile.BloodyIchorium", 1, 0),
+                GT_OreDictUnificator.get(OrePrefixes.plateDense, Materials.Osmiridium, 6L))
+            .fluidInputs(Materials.Void.getMolten(1152L))
+            .itemOutputs(kubatech.api.enums.ItemList.DEFCCasingT1.get(1))
+            .eut(491_520)
+            .duration(24000)
+            .addTo(sAssemblerRecipes);
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                kubatech.api.enums.ItemList.DEFCCasingT1.get(1),
                 GT_OreDictUnificator.get(OrePrefixes.plateDense, Materials.Draconium, 6L),
-                GT_ModHandler.getModItem("DraconicEvolution", "draconicCore", 1, 0) },
-            Materials.Void.getMolten(2304L),
-            kubatech.api.enums.ItemList.DEFCCasingT2.get(1),
-            24000,
-            491520); // uv
-        GT_Values.RA.addAssemblerRecipe(
-            new ItemStack[] { kubatech.api.enums.ItemList.DEFCCasingT2.get(1),
+                GT_ModHandler.getModItem("DraconicEvolution", "draconicCore", 1, 0))
+            .fluidInputs(Materials.Void.getMolten(2304L))
+            .itemOutputs(kubatech.api.enums.ItemList.DEFCCasingT2.get(1))
+            .eut(491_520)
+            .duration(24000)
+            .addTo(sAssemblerRecipes);
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                kubatech.api.enums.ItemList.DEFCCasingT2.get(1),
                 GT_OreDictUnificator.get(OrePrefixes.plateDense, Materials.CosmicNeutronium, 6L),
-                GT_ModHandler.getModItem("DraconicEvolution", "wyvernCore", 2, 0) },
-            Materials.Void.getMolten(4608L),
-            kubatech.api.enums.ItemList.DEFCCasingT3.get(1),
-            12000,
-            1996080); // uhv, you need uhv to get the thing anyway?
-        GT_Values.RA.addAssemblerRecipe(
-            new ItemStack[] { kubatech.api.enums.ItemList.DEFCCasingT3.get(1),
+                GT_ModHandler.getModItem("DraconicEvolution", "wyvernCore", 2, 0))
+            .fluidInputs(Materials.Void.getMolten(4608L))
+            .itemOutputs(kubatech.api.enums.ItemList.DEFCCasingT3.get(1))
+            .eut(1_996_080)
+            .duration(12000)
+            .addTo(sAssemblerRecipes);
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                kubatech.api.enums.ItemList.DEFCCasingT3.get(1),
                 GT_OreDictUnificator.get(OrePrefixes.plateDense, Materials.DraconiumAwakened, 6L),
-                GT_ModHandler.getModItem("DraconicEvolution", "awakenedCore", 3, 0) },
-            Materials.Void.getMolten(9216L),
-            kubatech.api.enums.ItemList.DEFCCasingT4.get(1),
-            12000,
-            7864320); // uev
-        GT_Values.RA.addAssemblerRecipe(
-            new ItemStack[] { kubatech.api.enums.ItemList.DEFCCasingT4.get(1),
+                GT_ModHandler.getModItem("DraconicEvolution", "awakenedCore", 3, 0))
+            .fluidInputs(Materials.Void.getMolten(9216L))
+            .itemOutputs(kubatech.api.enums.ItemList.DEFCCasingT4.get(1))
+            .eut(7_864_320)
+            .duration(12000)
+            .addTo(sAssemblerRecipes);
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                kubatech.api.enums.ItemList.DEFCCasingT4.get(1),
                 GT_OreDictUnificator.get(OrePrefixes.plateDense, Materials.Infinity, 6L),
-                GT_ModHandler.getModItem("DraconicEvolution", "chaoticCore", 4, 0) },
-            Materials.Void.getMolten(18432L),
-            kubatech.api.enums.ItemList.DEFCCasingT5.get(1),
-            12000,
-            31457280); // uiv, use the multi
+                GT_ModHandler.getModItem("DraconicEvolution", "chaoticCore", 4, 0))
+            .fluidInputs(Materials.Void.getMolten(18432L))
+            .itemOutputs(kubatech.api.enums.ItemList.DEFCCasingT5.get(1))
+            .eut(31_457_280)
+            .duration(12000)
+            .addTo(sAssemblerRecipes);
 
         fusionRecipes();
     }
 
-    private static void fusionRecipes(){
+    private static void fusionRecipes() {
         // CORES
 
         GT_Values.RA.stdBuilder()
