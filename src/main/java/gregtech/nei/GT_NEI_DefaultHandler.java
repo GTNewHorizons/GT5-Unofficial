d3cc83603e054e113706973f068a51c60894b126
unknown <gtandemmodding@gmail.com>
2023-11-14 20:02:34 +1100

    Actually fix merge

--- src/main/java/gregtech/nei/GT_NEI_DefaultHandler.java
+++ src/main/java/gregtech/nei/GT_NEI_DefaultHandler.java
@@ -20,12 +20,6 @@ import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.FontRenderer;
-import net.minecraft.client.gui.inventory.GuiContainer;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.fluids.FluidStack;
-
 import org.apache.commons.lang3.Range;
 import org.lwjgl.opengl.GL11;
 
@@ -67,15 +61,11 @@ import gregtech.api.util.GT_Utility;
 import gregtech.common.blocks.GT_Item_Machines;
 import gregtech.common.gui.modularui.UIHelper;
 import gregtech.common.power.Power;
-<<<<<<< HEAD
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
-=======
-import gregtech.nei.GT_NEI_DefaultHandler.FixedPositionedStack;
->>>>>>> d7c5acea2137d463e5f8126a0835ea5b1498e772
 
 public class GT_NEI_DefaultHandler extends RecipeMapHandler {
 
@@ -763,13 +753,8 @@ public class GT_NEI_DefaultHandler extends RecipeMapHandler {
                             .getItemHandler() == itemOutputsInventory) {
                                 int i = widget.getMcSlot()
                                     .getSlotIndex();
-<<<<<<< HEAD
-                                ItemStack output = aRecipe.getRepresentativeOutput(i);
                                 
-=======
-                                Object output = aRecipe.getRepresentativeOutput(i);
-
->>>>>>> d7c5acea2137d463e5f8126a0835ea5b1498e772
+                                ItemStack output = aRecipe.getRepresentativeOutput(i);     
                                 if (output != null) {
                                     mOutputs.add(
                                         new FixedPositionedStack(
