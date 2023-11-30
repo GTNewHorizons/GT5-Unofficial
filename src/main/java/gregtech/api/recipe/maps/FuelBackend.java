00c60b6ec7b59569f0fb2414972350bb74549221
miozune <miozune@gmail.com>
2023-11-30 15:52:09 +0900

    Some changes for FuelBackend

--- src/main/java/gregtech/api/recipe/maps/FuelBackend.java
+++ src/main/java/gregtech/api/recipe/maps/FuelBackend.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 
+import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidStack;
 
 import gregtech.api.recipe.RecipeMapBackend;
@@ -42,22 +43,33 @@ public class FuelBackend extends RecipeMapBackend {
         super.compileRecipe(recipe);
         if (recipe.mInputs != null && GT_Utility.getNonnullElementCount(recipe.mInputs) == 1
             && (recipe.mFluidInputs == null || GT_Utility.getNonnullElementCount(recipe.mFluidInputs) == 0)) {
-            FluidStack tFluid = GT_Utility.getFluidForFilledItem(recipe.mInputs[0], true);
-            if (tFluid != null) {
-                tFluid.amount = 0;
-                recipesByFluidInput.put(tFluid.getUnlocalizedName(), recipe);
+            FluidStack fluidStack = GT_Utility.getFluidForFilledItem(recipe.mInputs[0], true);
+            if (fluidStack != null) {
+                fluidStack.amount = 0;
+                recipesByFluidInput.put(
+                    fluidStack.getFluid()
+                        .getName(),
+                    recipe);
             }
         } else if ((recipe.mInputs == null || GT_Utility.getNonnullElementCount(recipe.mInputs) == 0)
             && recipe.mFluidInputs != null
-            && GT_Utility.getNonnullElementCount(recipe.mFluidInputs) == 1
+            && GT_Utility.getNonnullElementCount(recipe.mFluidInputs) >= 1
             && recipe.mFluidInputs[0] != null) {
-                recipesByFluidInput.put(recipe.mFluidInputs[0].getUnlocalizedName(), recipe);
+                recipesByFluidInput.put(
+                    recipe.mFluidInputs[0].getFluid()
+                        .getName(),
+                    recipe);
             }
         return recipe;
     }
 
     @Nullable
-    public GT_Recipe findFuel(FluidStack aFluidInput) {
-        return recipesByFluidInput.get(aFluidInput.getUnlocalizedName());
+    public GT_Recipe findFuel(FluidStack fluidStack) {
+        return findFuel(fluidStack.getFluid());
+    }
+
+    @Nullable
+    public GT_Recipe findFuel(Fluid fluid) {
+        return recipesByFluidInput.get(fluid.getName());
     }
 }
