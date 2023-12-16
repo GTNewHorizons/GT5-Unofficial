be7281bf0ea3101548a3960b8b4fcf283250fb02
GTNH-Colen <54497873+GTNH-Colen@users.noreply.github.com>
2023-12-16 03:30:58 +0000

    Expand conversion to (all?) GT items, but not itemblocks yet.

--- src/main/java/gregtech/api/modernmaterials/transition/TransitionItems.java
+++ src/main/java/gregtech/api/modernmaterials/transition/TransitionItems.java
@@ -10,25 +10,52 @@ import net.minecraft.item.Item;
 import net.minecraft.nbt.NBTTagCompound;
 
 import static gregtech.api.modernmaterials.items.partclasses.ItemsEnum.Bolt;
+import static gregtech.api.modernmaterials.items.partclasses.ItemsEnum.CrushedCentrifugedOre;
+import static gregtech.api.modernmaterials.items.partclasses.ItemsEnum.CrushedOre;
 import static gregtech.api.modernmaterials.items.partclasses.ItemsEnum.DensePlate;
 import static gregtech.api.modernmaterials.items.partclasses.ItemsEnum.Dust;
 import static gregtech.api.modernmaterials.items.partclasses.ItemsEnum.Foil;
 import static gregtech.api.modernmaterials.items.partclasses.ItemsEnum.Gem;
+import static gregtech.api.modernmaterials.items.partclasses.ItemsEnum.ImpureDust;
 import static gregtech.api.modernmaterials.items.partclasses.ItemsEnum.Ingot;
 import static gregtech.api.modernmaterials.items.partclasses.ItemsEnum.Lens;
 import static gregtech.api.modernmaterials.items.partclasses.ItemsEnum.Nugget;
 import static gregtech.api.modernmaterials.items.partclasses.ItemsEnum.Plate;
+import static gregtech.api.modernmaterials.items.partclasses.ItemsEnum.PurifiedDust;
+import static gregtech.api.modernmaterials.items.partclasses.ItemsEnum.PurifiedOre;
 import static gregtech.api.modernmaterials.items.partclasses.ItemsEnum.Ring;
 import static gregtech.api.modernmaterials.items.partclasses.ItemsEnum.Rod;
 import static gregtech.api.modernmaterials.items.partclasses.ItemsEnum.Round;
 import static gregtech.api.modernmaterials.items.partclasses.ItemsEnum.Screw;
+import static gregtech.api.modernmaterials.items.partclasses.ItemsEnum.SmallDust;
 
 public abstract class TransitionItems {
 
     private static Pair<OrePrefixes, ItemsEnum> getPrefixFromIDGroup01(int metadata) {
         switch (metadata / 1000) {
+            case 0 -> {
+                return new Pair<>(OrePrefixes.dustTiny, ItemsEnum.TinyDust);
+            }
+            case 1 -> {
+                return new Pair<>(OrePrefixes.dustSmall, SmallDust);
+            }
             case 2 -> {
                 return new Pair<>(OrePrefixes.dust, Dust);
+            }
+            case 3 -> {
+                return new Pair<>(OrePrefixes.dustImpure, ImpureDust);
+            }
+            case 4 -> {
+                return new Pair<>(OrePrefixes.dustPure, PurifiedDust);
+            }
+            case 5 -> {
+                return new Pair<>(OrePrefixes.crushed, CrushedOre);
+            }
+            case 6 -> {
+                return new Pair<>(OrePrefixes.crushedPurified, PurifiedOre);
+            }
+            case 7 -> {
+                return new Pair<>(OrePrefixes.crushedCentrifuged, CrushedCentrifugedOre);
             }
             case 8 -> {
                 return new Pair<>(OrePrefixes.gem, Gem);
@@ -43,7 +70,16 @@ public abstract class TransitionItems {
                 return new Pair<>(OrePrefixes.plate, Plate);
             }
             case 18 -> {
-                return new Pair<>(OrePrefixes.plateDouble, null);
+                return new Pair<>(OrePrefixes.plateDouble, null); // Todo review.
+            }
+            case 19 -> {
+                return new Pair<>(OrePrefixes.plateTriple, null); // Todo review.
+            }
+            case 20 -> {
+                return new Pair<>(OrePrefixes.plateQuadruple, null); // Todo review.
+            }
+            case 21 -> {
+                return new Pair<>(OrePrefixes.plateQuintuple, null); // Todo review.
             }
             case 22 -> {
                 return new Pair<>(OrePrefixes.plateDense, DensePlate);
@@ -84,6 +120,9 @@ public abstract class TransitionItems {
             case 16 -> {
                 return new Pair<>(OrePrefixes.turbineBlade, ItemsEnum.TurbineBlade);
             }
+            case 18 -> {
+                return new Pair<>(OrePrefixes.itemCasing, ItemsEnum.ItemCasing);
+            }
             case 19 -> {
                 return new Pair<>(OrePrefixes.wireFine, ItemsEnum.FineWire);
             }
@@ -96,11 +135,17 @@ public abstract class TransitionItems {
             case 22 -> {
                 return new Pair<>(OrePrefixes.stickLong, ItemsEnum.LongRod);
             }
+            case 23 -> {
+                return new Pair<>(OrePrefixes.springSmall, ItemsEnum.SmallSpring);
+            }
             case 24 -> {
                 return new Pair<>(OrePrefixes.spring, ItemsEnum.Spring);
             }
             case 25 -> {
                 return new Pair<>(OrePrefixes.arrowGtWood, null);
+            }
+            case 26 -> {
+                return new Pair<>(OrePrefixes.arrowGtPlastic, null);
             }
             case 27 -> {
                 return new Pair<>(OrePrefixes.gemChipped, ItemsEnum.ChippedGem);
@@ -116,6 +161,14 @@ public abstract class TransitionItems {
             }
             case 31 -> {
                 return new Pair<>(OrePrefixes.gearGt, ItemsEnum.Gear);
+            }
+        }
+        return null; // Not yet set-up
+    }
+    private static Pair<OrePrefixes, ItemsEnum> getPrefixFromIDGroup03(int metadata) {
+        switch (metadata / 1000) {
+            case 4 -> {
+                return new Pair<>(OrePrefixes.nanite, ItemsEnum.Nanites);
             }
         }
         return null; // Not yet set-up
@@ -177,11 +230,39 @@ public abstract class TransitionItems {
         return tag;
     }
 
+    private static NBTTagCompound convertMeta03(NBTTagCompound tag) {
+        short metadata = tag.getShort("Damage");
+        if (metadata < 4000 || metadata >= 5000) return tag; // Do nothing, this is not a valid material part.
+
+        int materialID = metadata % 1000;
+
+        Pair<OrePrefixes, ItemsEnum> prefix = getPrefixFromIDGroup03(metadata);
+        if (prefix == null) return tag;
+
+        if (prefix.second() == null) {
+            // Invalid items we want effectively "purged"
+            // e.g. arrows as these are deprecated now.
+            return convertToCobblestone(tag);
+        }
+
+        { // Conversion occurs
+            Item underlyingItem = prefix.second().getItem();
+
+            ModernMaterial material = ModernMaterial.getMaterialFromID(materialID);
+            if (material == null) return tag;
+
+            tag.setShort("id", (short) Item.getIdFromItem(underlyingItem));
+            tag.setShort("Damage", (short) materialID);
+        }
+
+        return tag;
+    }
 
     private static final short cobbleStoneID = (short) Item.getIdFromItem(Item.getItemFromBlock(Blocks.cobblestone));
     private static NBTTagCompound convertToCobblestone(NBTTagCompound tag) {
         tag.setShort("id", cobbleStoneID);
         tag.setShort("Damage", (short) 0);
+        tag.setTag("tag", new NBTTagCompound()); // Remove extraneous info.
 
         return tag;
     }
@@ -189,6 +270,7 @@ public abstract class TransitionItems {
     public static void fixWorld() {
         ItemStackReplacementManager.addItemReplacement("gregtech:gt.metaitem.01", TransitionItems::convertMeta01);
         ItemStackReplacementManager.addItemReplacement("gregtech:gt.metaitem.02", TransitionItems::convertMeta02);
+        ItemStackReplacementManager.addItemReplacement("gregtech:gt.metaitem.03", TransitionItems::convertMeta03);
     }
 
 }
