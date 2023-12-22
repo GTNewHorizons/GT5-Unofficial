e8fb9a3a3576d9cbfce3e3d1a3a7614f9ebc8d74
Andrei Laiff <q.laiff@gmail.com>
2023-12-22 15:22:22 +0200

    Fix z ordering for stack size in `Stocking Input`

--- src/main/java/gregtech/common/gui/modularui/widget/AESlotWidget.java
+++ src/main/java/gregtech/common/gui/modularui/widget/AESlotWidget.java
@@ -1,12 +1,16 @@
 package gregtech.common.gui.modularui.widget;
 
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.entity.RenderItem;
+import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 
 import com.gtnewhorizons.modularui.common.internal.wrapper.BaseSlot;
 import com.gtnewhorizons.modularui.common.internal.wrapper.ModularGui;
 import com.gtnewhorizons.modularui.common.widget.SlotWidget;
 
+import appeng.api.storage.IItemDisplayRegistry.ItemRenderHook;
 import appeng.client.render.AppEngRenderItem;
 import appeng.core.AELog;
 import appeng.util.Platform;
@@ -14,6 +18,19 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 public class AESlotWidget extends SlotWidget {
+
+    static ItemRenderHook SKIP_ITEM_STACK_SIZE_HOOK = new ItemRenderHook() {
+
+        @Override
+        public boolean renderOverlay(FontRenderer fr, TextureManager tm, ItemStack is, int x, int y) {
+            return true;
+        }
+
+        @Override
+        public boolean showStackSize(ItemStack is) {
+            return false;
+        }
+    };
 
     public AESlotWidget(BaseSlot slot) {
         super(slot);
@@ -23,13 +40,15 @@ public class AESlotWidget extends SlotWidget {
     @SideOnly(Side.CLIENT)
     protected void drawSlot(Slot slotIn) {
         final AppEngRenderItem aeRenderItem = new AppEngRenderItem();
+        AppEngRenderItem.POST_HOOKS.add(SKIP_ITEM_STACK_SIZE_HOOK);
         final RenderItem pIR = this.setItemRender(aeRenderItem);
         try {
             aeRenderItem.setAeStack(Platform.getAEStackInSlot(slotIn));
-            super.drawSlot(slotIn, false);
+            super.drawSlot(slotIn, true);
         } catch (final Exception err) {
             AELog.warn("[AppEng] AE prevented crash while drawing slot: " + err);
         }
+        AppEngRenderItem.POST_HOOKS.remove(SKIP_ITEM_STACK_SIZE_HOOK);
         this.setItemRender(pIR);
     }
 
