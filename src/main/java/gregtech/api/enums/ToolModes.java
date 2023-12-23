5879e92d3e2b274fc5490a7fcfa3654e39e4f62d
LewisSaber <mama040508@gmail.com>
2023-12-23 21:45:59 +0200

    Add enum

---
+++ src/main/java/gregtech/api/enums/ToolModes.java
@@ -0,0 +1,19 @@
+package gregtech.api.enums;
+
+public enum ToolModes {
+    REGULAR(0),
+    WRENCH_LINE(1);
+
+
+    private final byte modeValue;
+
+    ToolModes(byte modeValue) {
+        this.modeValue = modeValue;
+    }
+
+    public byte get() {
+        return modeValue;
+    }
+
+
+}
