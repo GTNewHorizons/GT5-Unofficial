3f5ae2bbd91e10a4e79acf5d8043928a5c1c7ccb
UltraHex <matthew.william.noel@gmail.com>
2023-12-23 20:34:24 -0800

    Rework item renderers
    API breaking changes (minor):
    Subclasses of GT_GeneratedItem_Renderer may need updating
    Subclasses of GT_GeneratedMaterial_Renderer may need updating
    Subclasses of GT_DataStick_Renderer may need updating

--- src/main/java/gregtech/common/render/GT_RenderUtil.java
+++ src/main/java/gregtech/common/render/GT_RenderUtil.java
@@ -1,10 +1,15 @@
 package gregtech.common.render;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.ItemRenderer;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.entity.RenderItem;
+import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
+import net.minecraftforge.client.IItemRenderer;
 import net.minecraftforge.common.util.ForgeDirection;
 
 import org.lwjgl.opengl.GL11;
@@ -58,5 +63,79 @@ public class GT_RenderUtil {
             Tessellator.instance.addVertexWithUV(xStart, yStart, z, icon.getMinU(), icon.getMinV());
         }
         Tessellator.instance.draw();
+    }
+
+    @SuppressWarnings("RedundantLabeledSwitchRuleCodeBlock")
+    public static void renderItem(IItemRenderer.ItemRenderType type, IIcon icon) {
+        Tessellator tessellator = Tessellator.instance;
+        float maxU = icon.getMaxU();
+        float minV = icon.getMinV();
+        float minU = icon.getMinU();
+        float maxV = icon.getMaxV();
+
+        switch (type) {
+            case ENTITY -> {
+                if (Minecraft.getMinecraft().gameSettings.fancyGraphics) {
+                    ItemRenderer.renderItemIn2D(
+                        tessellator,
+                        maxU,
+                        minV,
+                        minU,
+                        maxV,
+                        icon.getIconWidth(),
+                        icon.getIconHeight(),
+                        0.0625F);
+                } else {
+                    GL11.glPushMatrix();
+
+                    if (!RenderItem.renderInFrame) {
+                        GL11.glRotatef(180.0F - RenderManager.instance.playerViewY, 0.0F, 1.0F, 0.0F);
+                    }
+
+                    tessellator.startDrawingQuads();
+                    tessellator.setNormal(0.0F, 1.0F, 0.0F);
+                    tessellator.addVertexWithUV(0.0F - 0.5F, 0.0F - 0.25F, 0.0D, minU, maxV);
+                    tessellator.addVertexWithUV(1.0F - 0.5F, 0.0F - 0.25F, 0.0D, maxU, maxV);
+                    tessellator.addVertexWithUV(1.0F - 0.5F, 1.0F - 0.25F, 0.0D, maxU, minV);
+                    tessellator.addVertexWithUV(0.0F - 0.5F, 1.0F - 0.25F, 0.0D, minU, minV);
+                    tessellator.draw();
+
+                    GL11.glPopMatrix();
+                }
+            }
+            case EQUIPPED, EQUIPPED_FIRST_PERSON -> {
+                ItemRenderer.renderItemIn2D(
+                    tessellator,
+                    maxU,
+                    minV,
+                    minU,
+                    maxV,
+                    icon.getIconWidth(),
+                    icon.getIconHeight(),
+                    0.0625F);
+            }
+            case INVENTORY -> {
+                renderItemIcon(icon, 16.0D, 0.001, 0.0F, 0.0F, -1.0F);
+            }
+            default -> {}
+        }
+    }
+
+    public static void applyStandardItemTransform(IItemRenderer.ItemRenderType type) {
+        if (type == IItemRenderer.ItemRenderType.ENTITY) {
+            if (RenderItem.renderInFrame) {
+                // Magic numbers calculated from vanilla code
+                GL11.glScalef(1.025641F, 1.025641F, 1.025641F);
+                GL11.glTranslatef(0.0F, -0.05F, 0.0F);
+            }
+
+            if (Minecraft.getMinecraft().gameSettings.fancyGraphics) {
+                if (RenderItem.renderInFrame) {
+                    GL11.glRotatef(180.0F, 0.0F, 1.0F, 0.0F);
+                }
+                // Magic numbers calculated from vanilla code
+                GL11.glTranslatef(-0.5F, -0.25F, 0.0421875F);
+            }
+        }
     }
 }
--- src/main/java/gregtech/common/render/items/UniversiumRenderer.java
+++ src/main/java/gregtech/common/render/items/UniversiumRenderer.java
@@ -3,12 +3,12 @@ package gregtech.common.render.items;
 import static gregtech.api.enums.Mods.Avaritia;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.ItemRenderer;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import net.minecraft.util.MathHelper;
@@ -21,6 +21,7 @@ import codechicken.lib.render.TextureUtils;
 import fox.spiteful.avaritia.render.CosmicRenderShenanigans;
 import gregtech.api.enums.ItemList;
 import gregtech.api.interfaces.IGT_ItemWithMaterialRenderer;
+import gregtech.common.render.GT_RenderUtil;
 
 @SuppressWarnings("RedundantLabeledSwitchRuleCodeBlock")
 public class UniversiumRenderer extends GT_GeneratedMaterial_Renderer {
@@ -28,17 +29,14 @@ public class UniversiumRenderer extends GT_GeneratedMaterial_Renderer {
     private static final float cosmicOpacity = 2.5f;
 
     @Override
-    public boolean shouldUseRenderHelper(ItemRenderType type, ItemStack item, ItemRendererHelper helper) {
-        return helper == ItemRendererHelper.ENTITY_ROTATION || helper == ItemRendererHelper.ENTITY_BOBBING;
-    }
-
-    @Override
     public boolean renderFluidDisplayItem(ItemRenderType type, ItemStack aStack, Object... data) {
+        Item item = aStack.getItem();
+        if (item == null) return false;
+
         magicRenderMethod(
             type,
             ItemList.Emitter_UEV.get(1), // hack to make it render correctly
-            aStack.getItem()
-                .getIconFromDamage(aStack.getItemDamage()),
+            item.getIconFromDamage(aStack.getItemDamage()),
             true,
             data);
         return true;
@@ -89,133 +87,81 @@ public class UniversiumRenderer extends GT_GeneratedMaterial_Renderer {
         RenderItem r = RenderItem.getInstance();
         Minecraft mc = Minecraft.getMinecraft();
         Tessellator t = Tessellator.instance;
+        float minU = tIcon.getMinU();
+        float maxU = tIcon.getMaxU();
+        float minV = tIcon.getMinV();
+        float maxV = tIcon.getMaxV();
 
         processLightLevel(type, data);
 
-        switch (type) {
-            case ENTITY -> {
-                GL11.glPushMatrix();
-                if (aStack.isOnItemFrame()) GL11.glTranslatef(0F, -0.3F, 0.01F);
-                render(tIcon);
-                GL11.glPopMatrix();
-
-            }
-            case EQUIPPED, EQUIPPED_FIRST_PERSON -> {
-                render(tIcon);
-            }
-            case INVENTORY -> {
-                GL11.glPushMatrix();
-                GL11.glEnable(GL11.GL_BLEND);
-                GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-                RenderHelper.enableGUIStandardItemLighting();
-
-                GL11.glDisable(GL11.GL_ALPHA_TEST);
-                GL11.glDisable(GL11.GL_DEPTH_TEST);
-
-                if (fluidDisplay) {
-                    // this somehow makes shader render correctly
-                    ResourceLocation resourcelocation = mc.getTextureManager()
-                        .getResourceLocation(aStack.getItemSpriteNumber());
-                    mc.getTextureManager()
-                        .bindTexture(resourcelocation);
-                } else {
-                    r.renderItemIntoGUI(mc.fontRenderer, mc.getTextureManager(), aStack, 0, 0, true);
-                }
-
-                GL11.glEnable(GL11.GL_BLEND);
-                GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-                RenderHelper.enableGUIStandardItemLighting();
-
-                GL11.glDisable(GL11.GL_ALPHA_TEST);
-                GL11.glDisable(GL11.GL_DEPTH_TEST);
-
-                if (fluidDisplay) {
-                    GL11.glDisable(GL11.GL_BLEND);
-                }
-
-                CosmicRenderShenanigans.cosmicOpacity = cosmicOpacity;
-                CosmicRenderShenanigans.inventoryRender = true;
-                CosmicRenderShenanigans.useShader();
-
-                GL11.glColor4d(1, 1, 1, 1);
-
-                float minu = tIcon.getMinU();
-                float maxu = tIcon.getMaxU();
-                float minv = tIcon.getMinV();
-                float maxv = tIcon.getMaxV();
-
-                // Draw cosmic overlay
-                t.startDrawingQuads();
-                t.addVertexWithUV(0, 0, 0, minu, minv);
-                t.addVertexWithUV(0, 16, 0, minu, maxv);
-                t.addVertexWithUV(16, 16, 0, maxu, maxv);
-                t.addVertexWithUV(16, 0, 0, maxu, minv);
-                t.draw();
-
-                CosmicRenderShenanigans.releaseShader();
-                CosmicRenderShenanigans.inventoryRender = false;
-
-                GL11.glEnable(GL11.GL_ALPHA_TEST);
-                GL11.glEnable(GL12.GL_RESCALE_NORMAL);
-                GL11.glEnable(GL11.GL_DEPTH_TEST);
-
-                r.renderWithColor = true;
-
-                GL11.glDisable(GL11.GL_BLEND);
-                GL11.glPopMatrix();
-            }
-            default -> {}
-        }
-    }
-
-    private void render(IIcon icon) {
         GL11.glPushMatrix();
         GL11.glEnable(GL11.GL_BLEND);
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-        GL11.glColor4f(1F, 1F, 1F, 1F);
 
-        float f, f1, f2, f3;
-        float scale = 1F / 16F;
+        if (type == ItemRenderType.INVENTORY) {
+            RenderHelper.enableGUIStandardItemLighting();
 
-        f = icon.getMinU();
-        f1 = icon.getMaxU();
-        f2 = icon.getMinV();
-        f3 = icon.getMaxV();
+            GL11.glDisable(GL11.GL_ALPHA_TEST);
+            GL11.glDisable(GL11.GL_DEPTH_TEST);
 
-        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+            if (fluidDisplay) {
+                // this somehow makes shader render correctly
+                ResourceLocation resourcelocation = mc.getTextureManager()
+                    .getResourceLocation(aStack.getItemSpriteNumber());
+                mc.getTextureManager()
+                    .bindTexture(resourcelocation);
+            } else {
+                r.renderItemIntoGUI(mc.fontRenderer, mc.getTextureManager(), aStack, 0, 0, true);
+            }
 
-        // RENDER ITEM IN HAND
-        ItemRenderer
-            .renderItemIn2D(Tessellator.instance, f1, f2, f, f3, icon.getIconWidth(), icon.getIconHeight(), scale);
+            GL11.glEnable(GL11.GL_BLEND);
+            GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+            RenderHelper.enableGUIStandardItemLighting();
 
-        GL11.glDisable(GL11.GL_ALPHA_TEST);
-        GL11.glDepthFunc(GL11.GL_EQUAL);
-        CosmicRenderShenanigans.cosmicOpacity = cosmicOpacity;
-        CosmicRenderShenanigans.useShader();
+            GL11.glDisable(GL11.GL_ALPHA_TEST);
+            GL11.glDisable(GL11.GL_DEPTH_TEST);
 
-        float minu = icon.getMinU();
-        float maxu = icon.getMaxU();
-        float minv = icon.getMinV();
-        float maxv = icon.getMaxV();
+            if (fluidDisplay) {
+                GL11.glDisable(GL11.GL_BLEND);
+            }
 
-        // RENDER COSMIC OVERLAY IN HAND
-        ItemRenderer.renderItemIn2D(
-            Tessellator.instance,
-            maxu,
-            minv,
-            minu,
-            maxv,
-            icon.getIconWidth(),
-            icon.getIconHeight(),
-            scale);
-        CosmicRenderShenanigans.releaseShader();
-        GL11.glDepthFunc(GL11.GL_LEQUAL);
+            CosmicRenderShenanigans.cosmicOpacity = cosmicOpacity;
+            CosmicRenderShenanigans.inventoryRender = true;
+            CosmicRenderShenanigans.useShader();
+
+            GL11.glColor4d(1, 1, 1, 1);
+
+            // Draw cosmic overlay
+            t.startDrawingQuads();
+            t.addVertexWithUV(0, 0, 0, minU, minV);
+            t.addVertexWithUV(0, 16, 0, minU, maxV);
+            t.addVertexWithUV(16, 16, 0, maxU, maxV);
+            t.addVertexWithUV(16, 0, 0, maxU, minV);
+            t.draw();
+
+            CosmicRenderShenanigans.releaseShader();
+            CosmicRenderShenanigans.inventoryRender = false;
+
+            GL11.glEnable(GL12.GL_RESCALE_NORMAL);
+        } else {
+            // RENDER ITEM
+            GT_RenderUtil.renderItem(type, tIcon);
+
+            GL11.glDisable(GL11.GL_ALPHA_TEST);
+            GL11.glDepthFunc(GL11.GL_EQUAL);
+            CosmicRenderShenanigans.cosmicOpacity = cosmicOpacity;
+            CosmicRenderShenanigans.useShader();
+
+            // RENDER COSMIC OVERLAY
+            GT_RenderUtil.renderItem(type, tIcon);
+            CosmicRenderShenanigans.releaseShader();
+            GL11.glDepthFunc(GL11.GL_LEQUAL);
+        }
+
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
         GL11.glEnable(GL11.GL_ALPHA_TEST);
-
         GL11.glDisable(GL11.GL_BLEND);
         GL11.glPopMatrix();
-
-        GL11.glColor4f(1F, 1F, 1F, 1F);
     }
 
     private void processLightLevel(ItemRenderType type, Object... data) {
