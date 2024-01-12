a3adb6c9e2b0093b3e6ed6f2d4ce9c0ae5fe3520
Matthew William Noel <matthew.william.noel@gmail.com>
2024-01-12 11:20:02 -0800

    Rework item renderers (#2424)
    * Rework item renderers
    
    API breaking changes (minor):
    Subclasses of GT_GeneratedItem_Renderer may need updating
    Subclasses of GT_GeneratedMaterial_Renderer may need updating
    Subclasses of GT_DataStick_Renderer may need updating
    
    * Improve Universium shader compatibility
    
    still looks wrong, but won't screw up stuff rendered after it

--- src/main/java/gregtech/common/render/items/GT_GeneratedMaterial_Renderer.java
+++ src/main/java/gregtech/common/render/items/GT_GeneratedMaterial_Renderer.java
@@ -2,8 +2,7 @@ package gregtech.common.render.items;
 
 import static gregtech.api.enums.Mods.HodgePodge;
 
-import net.minecraft.client.renderer.ItemRenderer;
-import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import net.minecraftforge.client.IItemRenderer;
@@ -30,7 +29,8 @@ public class GT_GeneratedMaterial_Renderer implements IItemRenderer {
 
     @Override
     public boolean shouldUseRenderHelper(ItemRenderType type, ItemStack item, ItemRendererHelper helper) {
-        return type == ItemRenderType.ENTITY;
+        return type == ItemRenderType.ENTITY && helper == ItemRendererHelper.ENTITY_BOBBING
+            || (helper == ItemRendererHelper.ENTITY_ROTATION && Minecraft.getMinecraft().gameSettings.fancyGraphics);
     }
 
     /**
@@ -104,19 +104,7 @@ public class GT_GeneratedMaterial_Renderer implements IItemRenderer {
             GL11.glColor3f(tModulation[0] / 255.0F, tModulation[1] / 255.0F, tModulation[2] / 255.0F);
         }
 
-        if (type.equals(IItemRenderer.ItemRenderType.INVENTORY)) {
-            GT_RenderUtil.renderItemIcon(icon, 16.0D, 0.001D, 0.0F, 0.0F, -1.0F);
-        } else {
-            ItemRenderer.renderItemIn2D(
-                Tessellator.instance,
-                icon.getMaxU(),
-                icon.getMinV(),
-                icon.getMinU(),
-                icon.getMaxV(),
-                icon.getIconWidth(),
-                icon.getIconHeight(),
-                0.0625F);
-        }
+        GT_RenderUtil.renderItem(type, icon);
     }
 
     protected void renderContainedFluid(ItemRenderType type, FluidStack aFluidStack, IIcon fluidIcon) {
@@ -126,36 +114,12 @@ public class GT_GeneratedMaterial_Renderer implements IItemRenderer {
         TextureUtils.bindAtlas(aFluid.getSpriteNumber());
 
         GL11.glDepthFunc(GL11.GL_EQUAL);
-        if (type.equals(IItemRenderer.ItemRenderType.INVENTORY)) {
-            GT_RenderUtil.renderItemIcon(fluidIcon, 16.0D, 0.001D, 0.0F, 0.0F, -1.0F);
-        } else {
-            ItemRenderer.renderItemIn2D(
-                Tessellator.instance,
-                fluidIcon.getMaxU(),
-                fluidIcon.getMinV(),
-                fluidIcon.getMinU(),
-                fluidIcon.getMaxV(),
-                fluidIcon.getIconWidth(),
-                fluidIcon.getIconHeight(),
-                0.0625F);
-        }
+        GT_RenderUtil.renderItem(type, fluidIcon);
         GL11.glDepthFunc(GL11.GL_LEQUAL);
     }
 
     protected void renderItemOverlay(ItemRenderType type, IIcon overlay) {
-        if (type.equals(IItemRenderer.ItemRenderType.INVENTORY)) {
-            GT_RenderUtil.renderItemIcon(overlay, 16.0D, 0.001D, 0.0F, 0.0F, -1.0F);
-        } else {
-            ItemRenderer.renderItemIn2D(
-                Tessellator.instance,
-                overlay.getMaxU(),
-                overlay.getMinV(),
-                overlay.getMinU(),
-                overlay.getMaxV(),
-                overlay.getIconWidth(),
-                overlay.getIconHeight(),
-                0.0625F);
-        }
+        GT_RenderUtil.renderItem(type, overlay);
     }
 
     protected void markNeedsAnimationUpdate(IIcon icon) {
