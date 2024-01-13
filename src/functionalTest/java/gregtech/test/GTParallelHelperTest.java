b8b3e215b1c6f56f9bde9c1afb9e0262ec863d29
HoleFish <1594248739@qq.com>
2024-01-14 02:05:05 +0800

    Add properties

--- src/functionalTest/java/gregtech/test/GTParallelHelperTest.java
+++ src/functionalTest/java/gregtech/test/GTParallelHelperTest.java
@@ -1,6 +1,7 @@
 package gregtech.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
@@ -28,9 +29,9 @@ public class GTParallelHelperTest {
         ItemStack sulfurDust = Materials.Sulfur.getDust(1);
         rubberRecipe = new GT_Recipe(
             new ItemStack[] { rubberDust.copy(), sulfurDust.copy() },
+            new ItemStack[] { Materials.Rubber.getDust(1), Materials.Rubber.getDustTiny(1) },
             null,
-            null,
-            null,
+            new int[] { 10000, 6667 },
             null,
             new FluidStack[] { Materials.Rubber.getMolten(1000) },
             1,
@@ -82,5 +83,40 @@ public class GTParallelHelperTest {
             .setConsumption(false)
             .build();
         assertEquals(Integer.MAX_VALUE, helperWithBatchMode.getCurrentParallel());
+    }
+
+    @Test
+    void chanceMultiplier() {
+        GT_ParallelHelper helper = new GT_ParallelHelper().setRecipe(rubberRecipe)
+            .setMachine(machine, false, false)
+            .setItemInputs(inputItems)
+            .setMaxParallel(10)
+            .setAvailableEUt(100)
+            .setConsumption(false)
+            .setOutputCalculation(true)
+            .setChanceMultiplier(10)
+            .build();
+
+        int rubberDustAmount = helper.getItemOutputs()[0].stackSize;
+        int rubberDustTinyAmount = helper.getItemOutputs()[1].stackSize;
+
+        assertEquals(100, rubberDustAmount);
+        assertTrue(rubberDustTinyAmount >= 60 && rubberDustTinyAmount <= 70);
+    }
+
+    @Test
+    void outputMultiplier() {
+        GT_ParallelHelper helper = new GT_ParallelHelper().setRecipe(rubberRecipe)
+            .setMachine(machine, false, false)
+            .setItemInputs(inputItems)
+            .setMaxParallel(1)
+            .setAvailableEUt(1)
+            .setConsumption(false)
+            .setOutputCalculation(true)
+            .setOutputMultiplier(2)
+            .build();
+
+        assertEquals(2000, helper.getFluidOutputs()[0].amount);
+        assertEquals(2, helper.getItemOutputs()[0].stackSize);
     }
 }
