56410df35acd4034637bb7d8ffeb8d3dff70bee2
ZRP <doctormdkzrp@gmail.com>
2024-03-13 11:50:11 +0800

    1.1、2、3级油气钻机工作范围调整（从2，4，6调整为4，6，8） 2.等离子锻炉代码自动调整

--- src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_OilDrill1.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_OilDrill1.java
@@ -43,7 +43,7 @@ public class GT_MetaTileEntity_OilDrill1 extends GT_MetaTileEntity_OilDrillBase 
 
     @Override
     protected int getRangeInChunks() {
-        return 2;
+        return 4;
     }
 
     @Override
