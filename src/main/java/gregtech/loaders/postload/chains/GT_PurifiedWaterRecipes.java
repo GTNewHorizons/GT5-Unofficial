2292d4983d7ce16ecf5f630b429bc26f94575075
NotAPenguin <michiel.vandeginste@gmail.com>
2024-04-17 18:26:09 +0200

    add some item outputs to sifter unit

--- src/main/java/gregtech/loaders/postload/chains/GT_PurifiedWaterRecipes.java
+++ src/main/java/gregtech/loaders/postload/chains/GT_PurifiedWaterRecipes.java
@@ -2,6 +2,9 @@ package gregtech.loaders.postload.chains;
 
 import static gregtech.api.recipe.RecipeMaps.purificationPlantGrade1Recipes;
 
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+
 import gregtech.api.enums.GT_Values;
 import gregtech.api.enums.Materials;
 import gregtech.api.enums.TierEU;
@@ -16,6 +19,8 @@ public class GT_PurifiedWaterRecipes {
         GT_Values.RA.stdBuilder()
             .fluidInputs(GT_ModHandler.getDistilledWater(1000L))
             .fluidOutputs(Materials.Grade1PurifiedWater.getFluid(900L))
+            .itemOutputs(new ItemStack(Items.stick, 1), Materials.Stone.getDust(1), Materials.Gold.getNuggets(1))
+            .outputChances(100, 50, 10)
             .duration(duration)
             .eut(TierEU.RECIPE_LuV)
             .addTo(purificationPlantGrade1Recipes);
