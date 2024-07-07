98cef6e1174d886f3d9add3cbee7deb9f66df323
boubou19 <miisterunknown@gmail.com>
2024-07-07 23:22:22 +0200

    remove RA1 circuit assembler recipe usage

--- src/main/java/com/github/technus/tectech/loader/recipe/CircuitAssembler.java
+++ src/main/java/com/github/technus/tectech/loader/recipe/CircuitAssembler.java
@@ -1,5 +1,7 @@
 package com.github.technus.tectech.loader.recipe;
 
+import gregtech.api.enums.TierEU;
+import gregtech.api.util.GT_RecipeBuilder;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
@@ -13,6 +15,9 @@ import gregtech.api.enums.Materials;
 import gregtech.api.enums.OrePrefixes;
 import gregtech.api.util.GT_OreDictUnificator;
 
+import static gregtech.api.recipe.RecipeMaps.circuitAssemblerRecipes;
+import static gregtech.api.util.GT_RecipeBuilder.SECONDS;
+
 public class CircuitAssembler implements Runnable {
 
     @Override
@@ -22,25 +27,31 @@ public class CircuitAssembler implements Runnable {
             : FluidRegistry.getFluid("molten.solderingalloy");
 
         // Optical Processor
-        GT_Values.RA.addCircuitAssemblerRecipe(
-            new ItemStack[] { ItemList.Optically_Perfected_CPU.get(1L), ItemList.Optically_Compatible_Memory.get(2L),
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                ItemList.Optically_Perfected_CPU.get(1L), ItemList.Optically_Compatible_Memory.get(2L),
                 ItemList.Circuit_Parts_CapacitorXSMD.get(16L), ItemList.Circuit_Parts_DiodeXSMD.get(16L),
                 CustomItemList.DATApipe.get(4L),
-                GT_OreDictUnificator.get(OrePrefixes.bolt, Materials.EnrichedHolmium, 16) },
-            new FluidStack(solderUEV, 288),
-            ItemList.Circuit_OpticalProcessor.get(1L),
-            20 * 20,
-            614400,
-            true);
+                GT_OreDictUnificator.get(OrePrefixes.bolt, Materials.EnrichedHolmium, 16)
+            )
+            .itemOutputs(ItemList.Circuit_OpticalProcessor.get(1L))
+            .fluidInputs(new FluidStack(solderUEV, 288))
+            .requiresCleanRoom()
+            .duration(20* SECONDS)
+            .eut(614400)
+            .addTo(circuitAssemblerRecipes);
 
         // Parametrizer Memory Card
-        GT_Values.RA.addCircuitAssemblerRecipe(
-            new ItemStack[] { GT_OreDictUnificator.get(OrePrefixes.circuit, Materials.Basic, 2),
-                GT_OreDictUnificator.get(OrePrefixes.foil, Materials.Gold, 4), },
-            Materials.Plastic.getMolten(72),
-            CustomItemList.parametrizerMemory.get(1),
-            200,
-            480,
-            true);
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                GT_OreDictUnificator.get(OrePrefixes.circuit, Materials.Basic, 2),
+                GT_OreDictUnificator.get(OrePrefixes.foil, Materials.Gold, 4)
+            )
+            .itemOutputs(CustomItemList.parametrizerMemory.get(1))
+            .fluidInputs(Materials.Plastic.getMolten(72))
+            .requiresCleanRoom()
+            .duration(10*SECONDS)
+            .eut(TierEU.RECIPE_HV)
+            .addTo(circuitAssemblerRecipes);
     }
 }
