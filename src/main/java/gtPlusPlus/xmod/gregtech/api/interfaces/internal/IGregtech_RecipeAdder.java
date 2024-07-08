65a7ddab55a7a31bd20d8ff3c904dd7f07128d28
boubou19 <miisterunknown@gmail.com>
2024-07-09 00:58:52 +0200

    obliterate RA1 entries in interfaces (9/?)

--- src/main/java/gtPlusPlus/xmod/gregtech/api/interfaces/internal/IGregtech_RecipeAdder.java
+++ src/main/java/gtPlusPlus/xmod/gregtech/api/interfaces/internal/IGregtech_RecipeAdder.java
@@ -10,13 +10,6 @@ import gtPlusPlus.core.material.Material;
 public interface IGregtech_RecipeAdder {
 
     /**
-     * Adds a Recipe for the Quantum Force Smelter (up to 9 Inputs)
-     *
-     **/
-    boolean addQuantumTransformerRecipe(ItemStack[] aInput, FluidStack[] aFluidInput, FluidStack[] aFluidOutput,
-        ItemStack[] aOutputStack, int[] aChances, int aDuration, int aEUt, int aSpecialValue);
-
-    /**
      * Adds a custom Semifluid fuel for the GT++ SemiFluid Generators.
      *
      * @param aFuelItem  - A Fluidstack to be consumed.
@@ -34,41 +27,6 @@ public interface IGregtech_RecipeAdder {
      */
     boolean addSemifluidFuel(ItemStack aFuelItem, int aFuelValue);
 
-    boolean addFissionFuel(FluidStack aInput1, FluidStack aInput2, FluidStack aInput3, FluidStack aInput4,
-        FluidStack aInput5, FluidStack aInput6, FluidStack aInput7, FluidStack aInput8, FluidStack aInput9,
-        FluidStack aOutput1, FluidStack aOutput2, int aDuration, int aEUt);
-
-    boolean addFissionFuel(boolean aOptimise, FluidStack aInput1, FluidStack aInput2, FluidStack aInput3,
-        FluidStack aInput4, FluidStack aInput5, FluidStack aInput6, FluidStack aInput7, FluidStack aInput8,
-        FluidStack aInput9, FluidStack aOutput1, FluidStack aOutput2, int aDuration, int aEUt);
-
-    boolean addCyclotronRecipe(ItemStack aInput, FluidStack aFluidInput, ItemStack[] aOutputs, FluidStack aFluidOutput,
-        int[] aChances, int aDuration, int aEUt, int aSpecialValue);
-
-    boolean addCyclotronRecipe(ItemStack[] aInputs, FluidStack aFluidInput, ItemStack[] aOutput,
-        FluidStack aFluidOutput, int[] aChances, int aDuration, int aEUt, int aSpecialValue);
-
-    @Deprecated
-    boolean addBrewingRecipe(ItemStack aIngredient, FluidStack aInput, FluidStack aOutput, int aTime, int aEu,
-        boolean aHidden);
-
-    @Deprecated
-    boolean addSmeltingAndAlloySmeltingRecipe(ItemStack aDust, ItemStack aOutput);
-
-    @Deprecated
-    boolean addVacuumFurnaceRecipe(ItemStack[] aInputs, FluidStack[] aFluidInputs, ItemStack[] aOutputs,
-        FluidStack[] aFluidOutputs, int aDuration, int aEUt, int aLevel);
-
-    boolean addChemicalPlantRecipe(ItemStack[] aInputs, FluidStack[] aInputFluids, ItemStack[] aOutputs,
-        FluidStack[] aFluidOutputs, int time, long eu, int aTier);
-
-    boolean addChemicalPlantRecipe(ItemStack[] aInputs, FluidStack[] aInputFluids, ItemStack[] aOutputs,
-        FluidStack[] aFluidOutputs, int[] aChances, int time, long eu, int aTier);
-
-    @Deprecated
-    boolean addDistilleryRecipe(ItemStack aCircuit, FluidStack aInput, FluidStack aOutput, ItemStack aSolidOutput,
-        int aDuration, int aEUt, boolean aHidden);
-
     boolean addMillingRecipe(Material aMat, int aEU);
 
     boolean addFlotationRecipe(Materials aMat, ItemStack aXanthate, FluidStack[] aInputFluids,
@@ -77,24 +35,5 @@ public interface IGregtech_RecipeAdder {
     boolean addFlotationRecipe(Material aMat, ItemStack aXanthate, FluidStack[] aInputFluids,
         FluidStack[] aOutputFluids, int aTime, int aEU);
 
-    @Deprecated
-    boolean addpackagerRecipe(ItemStack aRecipeType, ItemStack aInput1, ItemStack aInput2, ItemStack aOutputStack1);
-
     boolean addFuelForRTG(ItemStack aFuelPellet, int aFuelDays, int aVoltage);
-
-    boolean addColdTrapRecipe(int aCircuit, ItemStack aInput, FluidStack aFluidInput, ItemStack[] aOutputs,
-        int[] aChances, FluidStack aFluidOutput, int aTime, int aEU);
-
-    boolean addReactorProcessingUnitRecipe(ItemStack aInput1, ItemStack aInput2, FluidStack aFluidInput,
-        ItemStack[] aOutputs, int[] aChances, FluidStack aFluidOutput, int aTime, int aEU);
-
-    @Deprecated
-    boolean addFluidHeaterRecipe(ItemStack aCircuit, FluidStack aInput, FluidStack aOutput, int aDuration, int aEUt);
-
-    @Deprecated
-    boolean addVacuumFreezerRecipe(ItemStack aInput, ItemStack aOutput, int aDuration, int aEU);
-
-    boolean addMolecularTransformerRecipe(ItemStack aInput, ItemStack aOutput, int aDuration, int aEU);
-
-    boolean addMolecularTransformerRecipe(ItemStack aInput, ItemStack aOutput, int aDuration, int aEU, int aAmps);
 }
