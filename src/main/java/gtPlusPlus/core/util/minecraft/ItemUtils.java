a3a4d156e5482a2928a23bcf2ab7fe30308a39a5
boubou19 <miisterunknown@gmail.com>
2024-07-08 23:25:17 +0200

    remove packager usage

--- src/main/java/gtPlusPlus/core/util/minecraft/ItemUtils.java
+++ src/main/java/gtPlusPlus/core/util/minecraft/ItemUtils.java
@@ -4,6 +4,8 @@ import static gregtech.api.enums.Mods.GTPlusPlus;
 import static gregtech.api.enums.Mods.GregTech;
 import static gregtech.api.enums.Mods.IndustrialCraft2;
 import static gregtech.api.enums.Mods.Minecraft;
+import static gregtech.api.recipe.RecipeMaps.packagerRecipes;
+import static gregtech.api.util.GT_RecipeBuilder.SECONDS;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -381,7 +383,25 @@ public class ItemUtils {
         final ItemStack smallDust = ItemUtils.getSimpleStack(output[1]);
         final ItemStack tinyDust = ItemUtils.getSimpleStack(output[2]);
 
-        CORE.RA.addpackagerRecipe(ItemList.Schematic_Dust.get(0), smallDust, tinyDust, normalDust);
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                GT_Utility.copyAmount(4, smallDust),
+                ItemList.Schematic_Dust.get(0)
+            )
+            .itemOutputs(normalDust)
+            .duration(5*SECONDS)
+            .eut(4)
+            .addTo(packagerRecipes);
+
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                GT_Utility.copyAmount(9, tinyDust),
+                ItemList.Schematic_Dust.get(0)
+            )
+            .itemOutputs(normalDust)
+            .duration(5*SECONDS)
+            .eut(4)
+            .addTo(packagerRecipes);
 
         if (ItemUtils.checkForInvalidItems(tinyDust) && ItemUtils.checkForInvalidItems(normalDust)) {
             if (RecipeUtils.addShapedRecipe(
