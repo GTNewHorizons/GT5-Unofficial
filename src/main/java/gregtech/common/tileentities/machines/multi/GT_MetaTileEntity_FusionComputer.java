918a28028af0323966750beeb6ce0b225b113545
evgengoldwar <evgeniy10102003kuptsov@gmail.com>
2024-07-20 01:00:56 +0400

    Fix sound for Fusion

--- src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_FusionComputer.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_FusionComputer.java
@@ -17,10 +17,13 @@ import java.util.Set;
 
 import javax.annotation.Nonnull;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.StatCollector;
 import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.fluids.FluidStack;
@@ -460,6 +463,8 @@ public abstract class GT_MetaTileEntity_FusionComputer
             aBaseMetaTileEntity
                 .setErrorDisplayID((aBaseMetaTileEntity.getErrorDisplayID() & ~127) | (mMachine ? 0 : 64));
             aBaseMetaTileEntity.setActive(mMaxProgresstime > 0);
+        } else {
+            doActivitySound(getActivitySoundLoop());
         }
     }
 
@@ -541,9 +546,10 @@ public abstract class GT_MetaTileEntity_FusionComputer
         return survivialBuildPiece(STRUCTURE_PIECE_MAIN, stackSize, 7, 1, 12, elementBudget, env, false, true);
     }
 
+    @SideOnly(Side.CLIENT)
     @Override
-    protected SoundResource getProcessStartSound() {
-        return SoundResource.GT_MACHINES_FUSION_LOOP;
+    protected ResourceLocation getActivitySoundLoop() {
+        return SoundResource.GT_MACHINES_FUSION_LOOP.resourceLocation;
     }
 
     @Override
