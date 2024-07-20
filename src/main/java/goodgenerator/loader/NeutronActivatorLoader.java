97ef5df307f54a8d02f6b2eed9da08dba4fbd70a
GitHub GTNH Actions <>
2024-07-20 14:58:59 +0000

    spotlessApply

--- src/main/java/goodgenerator/loader/NeutronActivatorLoader.java
+++ src/main/java/goodgenerator/loader/NeutronActivatorLoader.java
@@ -1,16 +1,16 @@
 package goodgenerator.loader;
 
 import static goodgenerator.api.recipe.GoodGeneratorRecipeMaps.neutronActivatorRecipes;
-import static goodgenerator.util.MyRecipeAdder.computeRangeNKE;
-import static gregtech.api.util.GT_RecipeBuilder.MINUTES;
-import static gregtech.api.util.GT_RecipeBuilder.SECONDS;
-import static gregtech.api.util.GT_RecipeConstants.NKE_RANGE;
 import static goodgenerator.items.MyMaterial.plutoniumBasedLiquidFuel;
 import static goodgenerator.items.MyMaterial.plutoniumBasedLiquidFuelExcited;
 import static goodgenerator.items.MyMaterial.thoriumBasedLiquidFuelDepleted;
 import static goodgenerator.items.MyMaterial.thoriumBasedLiquidFuelExcited;
 import static goodgenerator.items.MyMaterial.uraniumBasedLiquidFuel;
 import static goodgenerator.items.MyMaterial.uraniumBasedLiquidFuelExcited;
+import static goodgenerator.util.MyRecipeAdder.computeRangeNKE;
+import static gregtech.api.util.GT_RecipeBuilder.MINUTES;
+import static gregtech.api.util.GT_RecipeBuilder.SECONDS;
+import static gregtech.api.util.GT_RecipeConstants.NKE_RANGE;
 
 import gregtech.api.enums.GT_Values;
 import gregtech.api.enums.Materials;
