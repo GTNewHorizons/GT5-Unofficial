f975d67f62c455533922d3d64d02b3b20b4543ba
Sampsa <69092953+S4mpsa@users.noreply.github.com>
2024-07-25 17:27:27 +0300

    Add expedited recipe checks for autopulling Stocking Hatch and Bus (#2748)
    * Add expedited recipe checks for autopulling Stocking Hatch and Bus
    
    * Change to proper Item and FluidStack comparison operators
    
    * Forgot one
    
    * And a negation
    
    * Spotless apply for branch stockingHatchNotifications for #2748 (#2765)
    
    spotlessApply
    
    Co-authored-by: GitHub GTNH Actions <>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/main/java/gregtech/common/tileentities/machines/ISmartInputHatch.java
@@ -0,0 +1,13 @@
+package gregtech.common.tileentities.machines;
+
+public interface ISmartInputHatch {
+
+    /*
+     * An interface to allow advanced interaction between hatches and a multiblock controller
+     * Adapted from the Crafting Input Buffer functionality
+     */
+
+    // Have the contents of the hatch changed since the last check?
+    boolean justUpdated();
+
+}
