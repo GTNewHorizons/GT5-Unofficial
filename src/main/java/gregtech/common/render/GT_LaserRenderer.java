427745107afab29324d59b7d84c3032a16cb32a5
Mary Hopson <doyoumined@gmail.com>
2024-07-25 00:03:11 -0400

    Laser renderer is kind of functional

--- src/main/java/gregtech/common/render/GT_LaserRenderer.java
+++ src/main/java/gregtech/common/render/GT_LaserRenderer.java
@@ -11,8 +11,10 @@ import org.lwjgl.opengl.GL11;
 
 public class GT_LaserRenderer extends TileEntitySpecialRenderer {
 
+    private static char facing = 'r';
+
     private static final IModelCustom lasermodel = AdvancedModelLoader
-        .loadModel(new ResourceLocation("gregtech", "models/laser.obj"));;
+        .loadModel(new ResourceLocation("gregtech", "textures/model/laser.obj"));;
 
     public GT_LaserRenderer() {
         ClientRegistry.bindTileEntitySpecialRenderer(TileLaser.class, this);
@@ -21,9 +23,10 @@ public class GT_LaserRenderer extends TileEntitySpecialRenderer {
     @Override
     public void renderTileEntityAt(TileEntity tile, double x, double y, double z, float timeSinceLastTick) {
         GL11.glPushMatrix();
-        GL11.glTranslated(x + 0.5, y + 0.5, z + 0.5);
+        GL11.glTranslated(x, y, z);
         GL11.glDisable(GL11.GL_LIGHTING);
         GL11.glEnable(GL11.GL_BLEND);
+        bindTexture(new ResourceLocation("gregtech","textures/model/laser.png"));
         lasermodel.renderAll();
         GL11.glPopMatrix();
     }
