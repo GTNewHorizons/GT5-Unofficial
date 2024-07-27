0f1caa82b194ceb0d40988dc243216bf4c5c0985
LekKit <50500857+LekKit@users.noreply.github.com>
2024-07-27 12:38:42 +0300

    render: New laser renderer
    - Use GL quad rendering instead of line rendering to fix scaling
    - Set lightmap coords (Emit bloom glow with shaders)
    - Set opacity to 1.0
    - Slight cleanup

--- src/main/java/gregtech/common/render/GT_LaserRenderer.java
+++ src/main/java/gregtech/common/render/GT_LaserRenderer.java
@@ -1,6 +1,7 @@
 package gregtech.common.render;
 
 import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
@@ -16,8 +17,14 @@ import gregtech.common.tileentities.render.TileLaser;
 
 public class GT_LaserRenderer extends TileEntitySpecialRenderer {
 
-    private double zOffset = 0.0F;
-    private double xOffset = 0.0F;
+    private double zOffset = 0.0;
+    private double xOffset = 0.0;
+
+    // Relative to block size
+    private double lineRadius = 0.02;
+
+    // Originally was 0.6F
+    private float lineOpacity = 1.0F;
 
     private static final IModelCustom lasermodel = AdvancedModelLoader
         .loadModel(new ResourceLocation("gregtech", "textures/model/laser.obj"));
@@ -32,54 +39,56 @@ public class GT_LaserRenderer extends TileEntitySpecialRenderer {
         xOffset = 0.5 + 0.45 * Math.sin(0.5 * Math.PI * tc);
     }
 
+    private void renderFakeLine(TileLaser laser, double x1, double y1, double z1, double x2, double y2, double z2) {
+        Tessellator tessellator = Tessellator.instance;
+        tessellator.startDrawingQuads();
+        tessellator.setColorRGBA_F(laser.getRed(), laser.getGreen(), laser.getBlue(), lineOpacity);
+        tessellator.addVertex(x1 - lineRadius,  y1,  z1);
+        tessellator.addVertex(x1 + lineRadius,  y1,  z1);
+        tessellator.addVertex(x2 + lineRadius,  y2,  z2);
+        tessellator.addVertex(x2 - lineRadius,  y2,  z2);
+
+        tessellator.addVertex(x1,  y1,  z1 - lineRadius);
+        tessellator.addVertex(x1,  y1,  z1 + lineRadius);
+        tessellator.addVertex(x2,  y2,  z2 + lineRadius);
+        tessellator.addVertex(x2,  y2,  z2 - lineRadius);
+        tessellator.draw();
+    }
+
     @Override
     public void renderTileEntityAt(TileEntity tile, double x, double y, double z, float timeSinceLastTick) {
         final TileLaser ltile = (TileLaser) tile;
+
         if (ltile.getShouldRender()) {
+
+            // Push GL state
+            GL11.glPushMatrix();
             GL11.glPushAttrib(GL11.GL_ENABLE_BIT);
-            GL11.glDisable(GL11.GL_CULL_FACE);
+
             GL11.glDisable(GL11.GL_LIGHTING);
+            GL11.glDisable(GL11.GL_CULL_FACE);
             GL11.glDisable(GL11.GL_TEXTURE_2D);
-
             GL11.glEnable(GL11.GL_BLEND);
-            GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-            GL11.glColor4f(ltile.getRed(), ltile.getGreen(), ltile.getBlue(), 0.6F); // change this for your colour
-            GL11.glLineWidth(3F);
-            GL11.glDepthMask(false);
 
-            Tessellator tessellator = Tessellator.instance;
-            tessellator.startDrawing(GL11.GL_LINES);
-            if (ltile.realism) tessellator.addVertex(x + xOffset, y + 4, z + zOffset);
-            else tessellator.addVertex(x + 0.5, y + 4, z + 0.5);
+            // Full brightness on this thing (Emits glow with shaders)
+            OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240.f, 240.f);
 
-            tessellator.addVertex(x + xOffset, y + 0.5, z + zOffset);
+            if (ltile.realism) {
+                renderFakeLine(ltile, x + xOffset, y + 4.0, z + zOffset, x + xOffset, y + 0.5, z + zOffset);
+            } else {
+                renderFakeLine(ltile, x + 0.5, y + 4.0, z + 0.5, x + xOffset, y + 0.5, z + zOffset);
+            }
+
+            // Pop GL state
+            GL11.glPopAttrib();
+            GL11.glPopMatrix();
+
+            // Movement calculations
             maths(ltile.counter);
             ltile.counter += 0.6F;
             if (ltile.counter >= 80) {
                 ltile.counter = 0;
             }
-            tessellator.draw();
-
-            GL11.glDepthMask(true);
-            GL11.glPopAttrib();
-        }
-        if (ltile.getShouldRender()) {
-            /*
-             * GL11.glPushMatrix();
-             * GL11.glDrawElements(GL11.GL_LINE, 1, GL11.GL_UNSIGNED_INT, 1);
-             * GL11.glTranslated(x + xOffset, y + 0.5, z + zOffset);
-             * maths(ltile.counter);
-             * ltile.counter += 0.2F;
-             * if (ltile.counter >= 80) {
-             * ltile.counter = 0;
-             * }
-             * GL11.glColor3f(ltile.getRed(), ltile.getGreen(), ltile.getBlue());
-             * GL11.glDisable(GL11.GL_LIGHTING);
-             * GL11.glEnable(GL11.GL_BLEND);
-             * bindTexture(new ResourceLocation("gregtech", "textures/model/laser.png"));
-             * lasermodel.renderAll();
-             * GL11.glPopMatrix();
-             */
         }
     }
 }
