1e67d9524e3eeea708892e6aac073d7a231c0ac9
volence <steve@volence.dev>
2024-07-30 14:55:48 -0400

    remove excess variable

--- src/main/java/gtPlusPlus/xmod/gregtech/common/tileentities/machines/multi/production/algae/GregtechMTE_AlgaePondBase.java
+++ src/main/java/gtPlusPlus/xmod/gregtech/common/tileentities/machines/multi/production/algae/GregtechMTE_AlgaePondBase.java
@@ -64,7 +64,6 @@ public class GregtechMTE_AlgaePondBase extends GregtechMeta_MultiBlockBase<Gregt
     private int mCasing;
     private static IStructureDefinition<GregtechMTE_AlgaePondBase> STRUCTURE_DEFINITION = null;
     private int checkMeta;
-    private int minTierOfHatch;
     private static final Class<?> cofhWater;
 
     static {
@@ -179,7 +178,7 @@ public class GregtechMTE_AlgaePondBase extends GregtechMeta_MultiBlockBase<Gregt
         mCasing = 0;
         mLevel = 0;
         checkMeta = 0;
-        minTierOfHatch = 100;
+
         if (checkPiece(mName, 4, 2, 0) && mCasing >= 64
             && checkMeta > 0
             && mInputHatches.size() >= 1
