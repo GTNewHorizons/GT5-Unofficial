59faa6efb886938587bf5eed371f6706630c36c3
Mary Hopson <doyoumined@gmail.com>
2024-07-31 17:32:16 -0400

    BIG RECIPE

--- src/main/java/gregtech/loaders/postload/recipes/NeutroniumCompressorRecipes.java
+++ src/main/java/gregtech/loaders/postload/recipes/NeutroniumCompressorRecipes.java
@@ -7,6 +7,8 @@ import gregtech.api.enums.Materials;
 import gregtech.api.enums.OrePrefixes;
 import gregtech.api.enums.TierEU;
 import gregtech.api.util.GT_OreDictUnificator;
+import gregtech.api.util.GT_Utility;
+import net.minecraft.item.ItemStack;
 
 import static gregtech.api.recipe.RecipeMaps.neutroniumCompressorRecipes;
 import static gregtech.api.util.GT_RecipeBuilder.SECONDS;
@@ -15,8 +17,9 @@ public class NeutroniumCompressorRecipes implements Runnable {
 
     @Override
     public void run() {
+        ItemStack ironblocks = GT_Utility.copyAmountUnsafe(7000, GT_OreDictUnificator.get(OrePrefixes.block, Materials.Iron, 1));
         GT_Values.RA.stdBuilder()
-            .special(new ItemStackLong(GT_OreDictUnificator.get(OrePrefixes.block, Materials.Iron, 1), 15000))
+            .itemInputs(ironblocks)
             .itemOutputs(ItemList.Sensor_IV.get(1))
             .duration(3 * SECONDS)
             .eut(TierEU.RECIPE_HV)
