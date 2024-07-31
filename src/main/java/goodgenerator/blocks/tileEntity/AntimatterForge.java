03df603860e6b8cb7948cce413d5e519a51d0590
Sampsa <sampo.vanninen@aalto.fi>
2024-07-31 10:29:00 +0300

    Fix autobuild crashing

--- src/main/java/goodgenerator/blocks/tileEntity/AntimatterForge.java
+++ src/main/java/goodgenerator/blocks/tileEntity/AntimatterForge.java
@@ -181,6 +181,19 @@ public class AntimatterForge extends GT_MetaTileEntity_ExtendedPowerMultiBlockBa
         final GT_Multiblock_Tooltip_Builder tt = new GT_Multiblock_Tooltip_Builder();
         tt.addMachineType("Antimatter Forge")
             .addInfo("Dimensions not included!");
+            .addSeparator()
+            .addCasingInfo("Placeholder", 1664)
+            .addCasingInfo("Placeholder", 560)
+            .addCasingInfo("Placeholder", 128)
+            .addCasingInfo("Placeholder", 63)
+            .addEnergyHatch("1-32, Hint block with dot 2", 2)
+            .addInputHatch("1-16, Hint block with dot 1", 1)
+            .addOutputHatch("1-16, Hint block with dot 1", 1)
+            .addStructureInfo(
+                "ALL Hatches must be " + GT_Utility.getColoredTierNameFromTier((byte) hatchTier())
+                    + EnumChatFormatting.GRAY
+                    + " or better")
+            .toolTipFinisher("Good Generator");
         return tt;
     }
 
