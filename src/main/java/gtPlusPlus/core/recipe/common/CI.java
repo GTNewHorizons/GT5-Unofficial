0c1b60f93701a9f53d502d0118fbc93ab07b591d
chochem <40274384+chochem@users.noreply.github.com>
2024-08-02 21:55:48 +0100

    Fix gtpp hv-iv material progression (#2812)
    * use new tier materials
    
    * remove outdated comments
    
    * fix material progression
    
    * sa
    
    * remove more outdated commented code
    
    * use gt api
    
    * materials.uv is broken

--- src/main/java/gtPlusPlus/core/recipe/common/CI.java
+++ src/main/java/gtPlusPlus/core/recipe/common/CI.java
@@ -9,6 +9,7 @@ import gregtech.api.enums.ItemList;
 import gregtech.api.enums.Materials;
 import gregtech.api.enums.OrePrefixes;
 import gregtech.api.util.GT_ModHandler;
+import gregtech.api.util.GT_OreDictUnificator;
 import gregtech.api.util.GT_Utility;
 import gtPlusPlus.api.objects.Logger;
 import gtPlusPlus.core.item.chemistry.AgriculturalChem;
@@ -411,7 +412,7 @@ public class CI {
     }
 
     private static final Material[] aMaterial_Main = new Material[] { ALLOY.POTIN, ALLOY.TUMBAGA, ALLOY.EGLIN_STEEL,
-        ALLOY.INCONEL_625, ALLOY.INCOLOY_DS, ALLOY.NITINOL_60, ALLOY.ZERON_100, ALLOY.PIKYONIUM,
+        ALLOY.TANTALUM_CARBIDE, ALLOY.INCOLOY_DS, ALLOY.INCONEL_625, ALLOY.ZERON_100, ALLOY.PIKYONIUM,
         ELEMENT.STANDALONE.ADVANCED_NITINOL, ALLOY.ABYSSAL, ALLOY.QUANTUM, ELEMENT.STANDALONE.HYPOGEN };
 
     private static final Material[] aMaterial_Secondary = new Material[] { ALLOY.STEEL, ALLOY.SILICON_CARBIDE,
@@ -428,9 +429,9 @@ public class CI {
         Materials.AnnealedCopper, Materials.Gold, Materials.Titanium, Materials.Nichrome, Materials.Platinum,
         Materials.YttriumBariumCuprate, Materials.Naquadah, Materials.Duranium, Materials.SuperconductorUHV, };
 
-    private static final Materials[] aMaterial_Circuits = new Materials[] { Materials.Primitive, Materials.Basic,
-        Materials.Good, Materials.Advanced, Materials.Data, Materials.Data, Materials.Elite, Materials.Master,
-        Materials.Ultimate, Materials.SuperconductorUHV, Materials.Infinite, };
+    private static final Materials[] aMaterial_Circuits = new Materials[] { Materials.ULV, Materials.LV, Materials.MV,
+        Materials.HV, Materials.EV, Materials.IV, Materials.LuV, Materials.ZPM, Materials.SuperconductorUHV,
+        Materials.UHV, Materials.UEV, };
 
     private static final Material[][] aMaster = new Material[][] { aMaterial_Main, aMaterial_Secondary,
         aMaterial_Tertiary };
@@ -448,14 +449,6 @@ public class CI {
     }
 
     public static FluidStack getTieredFluid(int aTier, int aAmount, int aType) {
-        // Weird Legacy handling
-        /*
-         * ItemStack aCell = getTieredComponent(OrePrefixes.liquid, aTier, 1); FluidStack a =
-         * GT_Utility.getFluidForFilledItem(aCell, true); if (a == null) { a = aMaster[aType][aTier].getFluid(aAmount);
-         * }
-         */
-
-        // Modern Handling
         FluidStack a = aMaster[aType][aTier].getFluidStack(aAmount);
         if (a == null) {
             ItemStack aCell = getTieredComponent(OrePrefixes.liquid, aTier, 1);
@@ -523,11 +516,7 @@ public class CI {
         }
 
         if (aPrefix == OrePrefixes.circuit) {
-            /*
-             * if (aTier == 4) { return ItemUtils.getSimpleStack(CI.getDataStick(), aAmount); } else if (aTier == 5) {
-             * return ItemUtils.getSimpleStack(CI.getDataOrb(), aAmount); }
-             */
-            return ItemUtils.getOrePrefixStack(OrePrefixes.circuit, aMaterial_Circuits[aTier], aAmount);
+            return GT_OreDictUnificator.get(OrePrefixes.circuit, aMaterial_Circuits[aTier], aAmount);
         }
 
         // Check for Cables first, catch SuperConductor case and swap to wire.
