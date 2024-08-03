a6e3a097a7ba87b06ada9011b093f5cc299aa11d
GirixK <GirixK@users.noreply.github.com>
2024-08-04 01:06:43 +0200

    Limit LEG's amperage base on catalyst fluid

--- src/main/java/goodgenerator/blocks/tileEntity/LargeEssentiaGenerator.java
+++ src/main/java/goodgenerator/blocks/tileEntity/LargeEssentiaGenerator.java
@@ -6,6 +6,8 @@ import static gregtech.api.util.GT_StructureUtility.buildHatchAdder;
 
 import java.util.ArrayList;
 
+import gregtech.api.enums.MaterialsUEVplus;
+import gtPlusPlus.core.material.ELEMENT;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -418,6 +420,21 @@ public class LargeEssentiaGenerator extends GT_MetaTileEntity_TooltipMultiBlockB
         long EUt = mLeftEnergy;
         long EUVoltage = voltageLimit, EUAmp = 1;
 
+            //TO-DO Replace these fluids with actual fluids, adjust their values, and remove unnecessary imports needed for these fluids
+            {
+            if (depleteInput(MaterialsUEVplus.QuarkGluonPlasma.getMolten(420))) {
+                ampLimit = 64; //T5 Catalyst Fluid, UV Gated
+            } else if (depleteInput(MaterialsUEVplus.MagMatter.getMolten(1337))) {
+                ampLimit = 32; //T4 Catalyst Fluid, ZPM Gated
+            } else if (depleteInput(MaterialsUEVplus.Universium.getMolten(42))) {
+                ampLimit = 16; //T3 Catalyst fluid, LuV Gated
+            } else if (depleteInput(MaterialsUEVplus.SpaceTime.getMolten(9999))) {
+                ampLimit = 8; //T2 Catalyst fluid, IV Gated
+            } else if (depleteInput(ELEMENT.STANDALONE.HYPOGEN.getFluidStack(69))) {
+                ampLimit = 4; //T1 Catalyst fluid, EV Gated
+            } else
+                ampLimit = 1; //Default Behaviour, EV Gated
+        };
         for (EssentiaHatch hatch : this.mEssentiaHatch) {
             AspectList aspects = hatch.getAspects();
             for (Aspect aspect : aspects.aspects.keySet()) {
