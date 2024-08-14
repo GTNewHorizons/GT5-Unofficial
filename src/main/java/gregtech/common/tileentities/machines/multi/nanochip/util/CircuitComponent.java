45e7a22a199c87ec9afb98c864fdefb0d1b01e56
NotAPenguin <michiel.vandeginste@gmail.com>
2024-08-15 00:45:26 +0200

    fiddle with recipemap and fake items

--- src/main/java/gregtech/common/tileentities/machines/multi/nanochip/util/CircuitComponent.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/nanochip/util/CircuitComponent.java
@@ -1,10 +1,12 @@
 package gregtech.common.tileentities.machines.multi.nanochip.util;
 
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.StatCollector;
+
+import gregtech.api.items.GT_CircuitComponent_FakeItem;
 
 public enum CircuitComponent {
 
-    Invalid(-1, "gt.circuitcomponent.invalid"),
     Wire(0, "gt.circuitcomponent.wire"),
     SMDTransistor(1, "gt.circuitcomponent.smd.transistor"),
     SMDInductor(2, "gt.circuitcomponent.smd.inductor"),
@@ -13,10 +15,13 @@ public enum CircuitComponent {
 
     public final int id;
     public final String unlocalizedName;
+    // ItemStack of a fake item representing this component
+    public final ItemStack stack;
 
     CircuitComponent(int id, String unlocalizedName) {
         this.id = id;
         this.unlocalizedName = unlocalizedName;
+        this.stack = new ItemStack(new GT_CircuitComponent_FakeItem(), 1, id);
     }
 
     public String getLocalizedName() {
