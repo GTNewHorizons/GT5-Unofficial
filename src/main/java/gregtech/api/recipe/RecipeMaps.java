5161755e19923375db006d3e836bc60c3c0dc1e1
NotAPenguin <michiel.vandeginste@gmail.com>
2024-08-15 01:22:47 +0200

    injecting fake items into recipemap works

--- src/main/java/gregtech/api/recipe/RecipeMaps.java
+++ src/main/java/gregtech/api/recipe/RecipeMaps.java
@@ -1326,6 +1326,6 @@ public final class RecipeMaps {
 
     static {
         // Add transformer from circuit assembler recipes to nanochip assembly matrix recipe
-        RecipeMaps.circuitAssemblerRecipes.addDownstream(AssemblyMatrix.recipeTransformer);
+        RecipeMaps.circuitAssemblerRecipes.addDownstream(assemblyMatrixRecipeTransformer);
     }
 }
