7f835c1870ee9a5249f0c3d96dfcc99a568abe3c
NotAPenguin <michiel.vandeginste@gmail.com>
2024-08-14 17:19:51 +0200

    use proper pipe type and merge circuit component inputs

--- src/main/java/gregtech/common/tileentities/machines/multi/nanochip/util/CircuitComponentPacket.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/nanochip/util/CircuitComponentPacket.java
@@ -1,4 +1,16 @@
 package gregtech.common.tileentities.machines.multi.nanochip.util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CircuitComponentPacket {
+
+    private final Map<CircuitComponentType, Integer> components = new HashMap<>();
+
+    // Accept more circuit components from a new packet
+    public void acceptMore(CircuitComponentPacket other) {
+        for (Map.Entry<CircuitComponentType, Integer> entry : other.components.entrySet()) {
+            components.merge(entry.getKey(), entry.getValue(), Integer::sum);
+        }
+    }
 }
