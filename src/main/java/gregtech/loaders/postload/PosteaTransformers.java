eb17c5269fb54e47b31733cf526a8828886560a4
NotAPenguin <michiel.vandeginste@gmail.com>
2024-08-15 20:55:44 +0200

    dont modify tes we dont want to modify (needs future postea update)

--- src/main/java/gregtech/loaders/postload/PosteaTransformers.java
+++ src/main/java/gregtech/loaders/postload/PosteaTransformers.java
@@ -24,18 +24,28 @@ public class PosteaTransformers implements Runnable {
         // that does not use TEs by default
 
         TileEntityReplacementManager.tileEntityTransformer("BaseMetaPipeEntity", (tag, world) -> {
-            int meta = 0;
+            // Read the MTE ID from the NBT data and try to figure out if this is a frame box
+            int id = tag.getInteger("mID");
+            // Framebox IDs start at 4096
+            int indexInMaterialList = id - 4096;
+            // The offset from 4096 is the index in the material list, so if this is outside the valid range then this
+            // is not a valid frame box, and we don't want to modify it
+            if (indexInMaterialList < 0 || indexInMaterialList >= GregTech_API.sGeneratedMaterials.length) {
+                // Do not modify this TE, so return null
+                return null;
+            }
 
+            // Now we know for sure that the tileentity is a frame box
             // If this frame has a cover on it, we need to keep it, but we still need to make sure the block
             // is the new frame block. We can make sure to keep the TE using a pass-through transformer.
             // This works because between the old and new frame systems, the TileEntity used for covered frames
             // is still the same
             if (tag.hasKey(GT_Values.NBT.COVERS)) {
-                return new BlockInfo(GregTech_API.sBlockFrames, meta, PosteaTransformers::passthrough);
+                return new BlockInfo(GregTech_API.sBlockFrames, indexInMaterialList, PosteaTransformers::passthrough);
             }
 
             // If this frame has no covers, simply return a block and delete the TileEntity
-            return new BlockInfo(GregTech_API.sBlockFrames, meta);
+            return new BlockInfo(GregTech_API.sBlockFrames, indexInMaterialList);
         });
     }
 }
