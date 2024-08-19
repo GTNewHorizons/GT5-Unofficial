ddb1619c5885608eca48b5fb6bbc5fa37f791a19
NotAPenguin <michiel.vandeginste@gmail.com>
2024-08-19 23:20:43 +0200

    spotless

--- src/main/java/gregtech/common/misc/techtree/gui/TechSelectorButton.java
+++ src/main/java/gregtech/common/misc/techtree/gui/TechSelectorButton.java
@@ -5,6 +5,7 @@ import com.cleanroommc.modularui.theme.WidgetTheme;
 import com.cleanroommc.modularui.widgets.ButtonWidget;
 
 public class TechSelectorButton<W extends TechSelectorButton<W>> extends ButtonWidget<W> {
+
     @Override
     public WidgetTheme getWidgetTheme(ITheme theme) {
         return theme.getFallback();
