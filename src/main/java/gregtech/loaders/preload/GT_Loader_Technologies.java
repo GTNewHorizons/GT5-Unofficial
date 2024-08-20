4dca85a49954a6fc9e472a311a8970daac079c8a
NotAPenguin <michiel.vandeginste@gmail.com>
2024-08-20 15:26:59 +0200

    bake tech tree construction on game load and log its timing

--- src/main/java/gregtech/loaders/preload/GT_Loader_Technologies.java
+++ src/main/java/gregtech/loaders/preload/GT_Loader_Technologies.java
@@ -1,17 +1,18 @@
 package gregtech.loaders.preload;
 
-import gregtech.api.util.GT_Log;
+import gregtech.GT_Mod;
 import gregtech.common.misc.techtree.TechList;
 import gregtech.common.misc.techtree.TechnologyRegistry;
 import gregtech.common.misc.techtree.base.Technology;
 import gregtech.common.misc.techtree.base.TechnologyBuilder;
+import gregtech.common.misc.techtree.gui.TechTreeLayout;
 import gregtech.common.misc.techtree.interfaces.IPrerequisite;
 
 public class GT_Loader_Technologies implements Runnable {
 
     @Override
     public void run() {
-        GT_Log.out.println("GT_Mod: Loading researchable technologies");
+        GT_Mod.GT_FML_LOGGER.info("Loading researchable technologies");
 
         // Construct with constructor manually because we don't want to assign itself as a prerequisite and mess
         // everything up
@@ -39,5 +40,13 @@ public class GT_Loader_Technologies implements Runnable {
         TechList.TechG = new TechnologyBuilder("TechG").unlocalizedName("gt.tech.g.name")
             .prerequisites(TechList.TechC)
             .buildAndRegister();
+
+        int numTechnologies = TechnologyRegistry.numTechnologies();
+        GT_Mod.GT_FML_LOGGER.info("Constructing tech tree layout");
+        long start = System.currentTimeMillis();
+        TechTreeLayout.constructOrGet();
+        long end = System.currentTimeMillis();
+        GT_Mod.GT_FML_LOGGER.info(String.format("Loaded %d technologies.", numTechnologies));
+        GT_Mod.GT_FML_LOGGER.info(String.format("Constructing tech tree layout took %d ms.", end - start));
     }
 }
