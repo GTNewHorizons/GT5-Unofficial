e64b2852bdf65c0c87bc139382ace3b434a4b26d
boubou19 <miisterunknown@gmail.com>
2024-08-24 17:48:57 +0200

    delete duplicate printing of MTE IDs in GT5U clients

--- src/main/java/gregtech/common/GT_Client.java
+++ src/main/java/gregtech/common/GT_Client.java
@@ -649,24 +649,6 @@ public class GT_Client extends GT_Proxy implements Runnable {
     public void onPostLoad() {
         super.onPostLoad();
 
-        if (GregTech_API.sClientDataFile.get("debug", "PrintMetaIDs", false)) {
-            try {
-                for (int i = 1; i < GregTech_API.METATILEENTITIES.length; i++) {
-                    try {
-                        if (GregTech_API.METATILEENTITIES[i] != null) {
-                            GregTech_API.METATILEENTITIES[i].getStackForm(1L)
-                                .getTooltip(null, true);
-                            GT_Log.out.println("META " + i + " " + GregTech_API.METATILEENTITIES[i].getMetaName());
-                        }
-                    } catch (Throwable e) {
-                        e.printStackTrace(GT_Log.err);
-                    }
-                }
-            } catch (Throwable e) {
-                e.printStackTrace(GT_Log.err);
-            }
-        }
-
         // reobf doesn't work with lambda, so this must be a class
         // noinspection Convert2Lambda
         ((IReloadableResourceManager) Minecraft.getMinecraft()
