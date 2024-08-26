c8f76d8effffc31618ecaac582350a02b3d8dd4c
RecursivePineapple <recursive_pineapple@proton.me>
2024-08-26 14:52:17 -0400

    worked on placing logic some more

--- src/main/java/gtPlusPlus/core/item/general/matterManipulator/PendingBuild.java
+++ src/main/java/gtPlusPlus/core/item/general/matterManipulator/PendingBuild.java
@@ -14,11 +14,13 @@ import gregtech.GT_Mod;
 import gtPlusPlus.core.item.general.matterManipulator.NBTState.PendingBlock;
 import ic2.api.item.ElectricItem;
 import net.minecraft.block.Block;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemBlock;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.world.World;
 
 public class PendingBuild {
     public LinkedList<PendingBlock> pendingBlocks;
@@ -26,9 +28,9 @@ public class PendingBuild {
     public NBTState manipulator;
     public Future<LinkedList<PendingBlock>> assembleTask;
 
-    private static final int BLOCKS_PER_PLACE = 10;
+    private static final int BLOCKS_PER_PLACE = 256;
     private static final int MAX_PLACE_DISTANCE = 512 * 512;
-    private static final double EU_PER_BLOCK = 512.0, TE_PENALTY = 16.0;
+    private static final double EU_PER_BLOCK = 128.0, TE_PENALTY = 16.0;
 
     public void tryPlaceBlocks(ItemStack stack, EntityPlayer player) {
         if(pendingBlocks == null) {
@@ -37,7 +39,7 @@ public class PendingBuild {
                     pendingBlocks = assembleTask.get();
                 } catch (InterruptedException | ExecutionException e) {
                     e.printStackTrace();
-                    player.addChatMessage(new ChatComponentText("Error assembling blocks to place: " + e.getClass().getName() + ": " + e.getMessage()));
+                    player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "An error occurred while determining which blocks to place: " + e.getClass().getName() + ": " + e.getMessage()));
                     player.setItemInUse(null, 0);
                     return;
                 }
@@ -47,64 +49,140 @@ public class PendingBuild {
             }
         }
 
-        if(pendingBlocks.isEmpty()) {
-            player.addChatMessage(new ChatComponentText(EnumChatFormatting.ITALIC.toString() + EnumChatFormatting.GRAY + "Finished placing blocks."));
-            player.setItemInUse(null, 0);
-            return;
-        }
-
         ArrayList<PendingBlock> list = new ArrayList<>(BLOCKS_PER_PLACE);
 
         Integer lastChunkX = null, lastChunkZ = null;
-        boolean anyUnplaceable = false;
+        int shuffleCount = 0;
+
+        var world = placingPlayer.worldObj;
+
+        ArrayList<ItemStack> drops = new ArrayList<>();
 
         while(list.size() < BLOCKS_PER_PLACE && pendingBlocks.size() > 0) {
             var next = pendingBlocks.getFirst();
 
-            if(placingPlayer.getDistanceSq(next.location.x, next.location.y, next.location.z) >= MAX_PLACE_DISTANCE) {
+            int x = next.x, y = next.y, z = next.z;
+
+            if(placingPlayer.getDistanceSq(x, y, z) >= MAX_PLACE_DISTANCE) {
+                pendingBlocks.addLast(pendingBlocks.removeFirst());
+                shuffleCount++;
+
+                if(shuffleCount > pendingBlocks.size()) {
+                    break;
+                } else {
+                    continue;
+                }
+            }
+
+            int chunkX = x >> 4;
+            int chunkZ = z >> 4;
+
+            if(!Objects.equal(chunkX, lastChunkX) || !Objects.equal(chunkZ, lastChunkZ)) {
+                if(!world.getChunkProvider().chunkExists(chunkX, chunkZ)) {
+                    pendingBlocks.removeFirst();
+                    continue;
+                } else {
+                    lastChunkX = chunkX;
+                    lastChunkZ = chunkZ;
+                }
+            }
+
+            if (!list.isEmpty() && !areBlocksBasicallyEqual(next, list.get(0))) {
+                break;
+            }
+
+            Block block = next.block;
+            Block existing = world.getBlock(x, y, z);
+            int existingMeta = world.getBlockMetadata(x, y, z);
+
+            if(existing == block && existingMeta == next.metadata) {
+                applyNBT(next);
+
                 pendingBlocks.removeFirst();
                 continue;
             }
 
-            int cx = next.location.x >> 4;
-            int cz = next.location.z >> 4;
+            boolean canPlace = switch(manipulator.config.removeMode) {
+                case NONE -> existing.isAir(world, x, y, z);
+                case REPLACEABLE -> existing.isReplaceable(world, x, y, z);
+                case ALL -> true;
+            };
 
-            if(!Objects.equal(cx, lastChunkX) || !Objects.equal(cz, lastChunkZ)) {
-                if(!placingPlayer.worldObj.getChunkProvider().chunkExists(cx, cz)) {
-                    pendingBlocks.removeFirst();
-                    continue;
+            if (!canPlace) {
+                pendingBlocks.addLast(pendingBlocks.removeFirst());
+                shuffleCount++;
+
+                if(shuffleCount > pendingBlocks.size()) {
+                    break;
                 } else {
-                    lastChunkX = cx;
-                    lastChunkZ = cz;
+                    continue;
                 }
             }
 
-            if (list.get(0) != null && !areStacksBasicallyEqual(next.block, list.get(0).block)) {
-                break;
+            if (!existing.isAir(world, x, y, z)) {
+                if (!tryConsumePower(stack, new PendingBlock(world.provider.dimensionId, x, y, z, new ItemStack(existing, existingMeta)))) {
+                    player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Matter Manipulator ran out of EU."));
+                    break;
+                }
+
+                removeBlock(world, x, y, z, existing, existingMeta, drops);
             }
 
             // shuffle unplaceable blocks to the end of the queue
-            if(!((ItemBlock)next.block.getItem()).field_150939_a.canPlaceBlockAt(placingPlayer.worldObj, next.location.x, next.location.y, next.location.z)) {
+            if(!block.canPlaceBlockAt(world, next.x, next.y, next.z)) {
                 pendingBlocks.addLast(pendingBlocks.removeFirst());
-                anyUnplaceable = true;
-                continue;
+                shuffleCount++;
+
+                if(shuffleCount > pendingBlocks.size()) {
+                    break;
+                } else {
+                    continue;
+                }
             }
 
             if(!tryConsumePower(stack, next)) {
+                player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Matter Manipulator ran out of EU."));
                 break;
             }
 
             list.add(pendingBlocks.removeFirst());
         }
 
-        if(list.isEmpty() && anyUnplaceable) {
-            player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Could not place remaining blocks."));
+        if (placingPlayer.capabilities.isCreativeMode) {
+            drops.clear();
+        }
+
+        ArrayList<ItemStack> splitDrops = new ArrayList<>();
+
+        for(var drop : drops) {
+            while(drop.stackSize > 64) {
+                splitDrops.add(drop.splitStack(64));
+            }
+
+            if(drop.stackSize > 0) {
+                splitDrops.add(drop);
+            }
+        }
+
+        for(var drop : splitDrops) {
+            if (!placingPlayer.inventory.addItemStackToInventory(drop)) {
+                world.spawnEntityInWorld(new EntityItem(world, placingPlayer.posX, placingPlayer.posY, placingPlayer.posZ, drop));
+            }
+        }
+
+        if(list.isEmpty()) {
+            if(!pendingBlocks.isEmpty()) {
+                player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Could not place " + pendingBlocks.size() + " remaining blocks."));
+            } else {
+                player.addChatMessage(new ChatComponentText(EnumChatFormatting.ITALIC.toString() + EnumChatFormatting.GRAY + "Finished placing blocks."));
+            }
+
             player.setItemInUse(null, 0);
             return;
         }
 
-        ItemStack item = list.get(0).block.copy();
-        item.stackSize = list.size();
+        ItemStack item = new ItemStack(list.get(0).block, list.size());
+        item.setTagCompound(list.get(0).nbt);
 
         if(!tryConsumeItems(item)) {
             player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Could not find item: " + item.getDisplayName()));
@@ -112,12 +190,8 @@ public class PendingBuild {
             return;
         }
 
-        for(var pendingBlock : list) {
-            var block = ((ItemBlock)pendingBlock.block.getItem()).field_150939_a;
-
-            int x = pendingBlock.location.x, y = pendingBlock.location.y, z = pendingBlock.location.z;
-
-            placingPlayer.worldObj.setBlock(x, y, z, block, pendingBlock.block.getItemDamage(), 3);
+        for(var pending : list) {
+            world.setBlock(pending.x, pending.y, pending.z, pending.block, pending.metadata, 3);
         }
     }
 
@@ -176,10 +250,17 @@ public class PendingBuild {
         return true;
     }
 
-    private static boolean areStacksBasicallyEqual(ItemStack a, ItemStack b) {
-        return a.getItem() == b.getItem() && a.getItemDamage() == b.getItemDamage() && ItemStack.areItemStackTagsEqual(a, b);
+    private static boolean areBlocksBasicallyEqual(PendingBlock a, PendingBlock b) {
+        return a.block == b.block && a.metadata == b.metadata && Objects.equal(a.nbt, b.nbt);
     }
 
+    private static boolean areStacksBasicallyEqual(ItemStack a, ItemStack b) {
+        if(a == null || b == null) {
+            return a == null && b == null;
+        }
+
+        return a.getItem() == b.getItem() && a.getItemDamage() == b.getItemDamage() && ItemStack.areItemStackTagsEqual(a, b);
+    }
 
     private static final MethodHandle IS_BLOCK_CONTAINER;
 
@@ -196,10 +277,10 @@ public class PendingBuild {
     private boolean tryConsumePower(ItemStack stack, PendingBlock pendingBlock) {
         double euUsage = 1.0;
 
-        euUsage *= placingPlayer.getDistanceSq(pendingBlock.location.x, pendingBlock.location.y, pendingBlock.location.z) * EU_PER_BLOCK;
+        euUsage *= placingPlayer.getDistanceSq(pendingBlock.x, pendingBlock.y, pendingBlock.z) * EU_PER_BLOCK;
 
         try {
-            if((boolean)IS_BLOCK_CONTAINER.invoke(((ItemBlock)pendingBlock.block.getItem()).field_150939_a)) {
+            if((boolean)IS_BLOCK_CONTAINER.invoke(pendingBlock.block)) {
                 euUsage *= TE_PENALTY;
             }
         } catch (Throwable e) {
@@ -207,5 +288,28 @@ public class PendingBuild {
         }
 
         return ElectricItem.manager.use(stack, euUsage, placingPlayer);
+    }
+
+    private void removeBlock(World world, int x, int y, int z, Block existing, int existingMeta, ArrayList<ItemStack> pendingDrops) {
+        var drops = existing.getDrops(world, x, y, z, existingMeta, 0);
+
+        for(var drop : drops) {
+            for(var pending : pendingDrops) {
+                if (areStacksBasicallyEqual(drop, pending)) {
+                    pending.stackSize += drop.stackSize;
+                    drop.stackSize = 0;
+                }
+            }
+
+            if(drop.stackSize > 0) {
+                pendingDrops.add(drop);
+            }
+        }
+
+        world.setBlock(x, y, z, Blocks.air);
+    }
+
+    private void applyNBT(PendingBlock block) {
+
     }
 }
