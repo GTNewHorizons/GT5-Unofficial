5b1a493e2e82c128db16cfc3fd0d93628c1d4a7c
HoleFish <1594248739@qq.com>
2024-08-26 16:04:33 +0800

    machine message

--- src/main/java/gregtech/api/recipe/check/CheckRecipeResult.java
+++ src/main/java/gregtech/api/recipe/check/CheckRecipeResult.java
@@ -1,8 +1,6 @@
 package gregtech.api.recipe.check;
 
-import javax.annotation.Nonnull;
-
-import net.minecraft.network.PacketBuffer;
+import gregtech.api.util.IMachineMessage;
 
 /**
  * Class to indicate the result of recipe check in the machine. It doesn't need to be actual result of recipemap check,
@@ -10,40 +8,12 @@ import net.minecraft.network.PacketBuffer;
  * <p>
  * Sample instance must be registered to {@link CheckRecipeResultRegistry}.
  */
-public interface CheckRecipeResult {
-
-    /**
-     * @return Unique registry ID
-     */
-    @Nonnull
-    String getID();
+public interface CheckRecipeResult extends IMachineMessage<CheckRecipeResult> {
 
     /**
      * @return If recipe check is successful
      */
     boolean wasSuccessful();
-
-    /**
-     * @return Actual text to show on client GUI
-     */
-    @Nonnull
-    String getDisplayString();
-
-    /**
-     * Create new instance to receive packet.
-     */
-    @Nonnull
-    CheckRecipeResult newInstance();
-
-    /**
-     * Encode value to sync.
-     */
-    void encode(@Nonnull PacketBuffer buffer);
-
-    /**
-     * Decode synced value.
-     */
-    void decode(PacketBuffer buffer);
 
     /**
      * @return If this message should stay on GUI when the machine is shut down.
