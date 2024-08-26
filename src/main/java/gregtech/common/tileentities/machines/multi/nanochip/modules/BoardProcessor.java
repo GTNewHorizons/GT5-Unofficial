07d76fea0f676ced2f39c4c27943aae958d9eeff
NotAPenguin <michiel.vandeginste@gmail.com>
2024-08-26 16:12:59 +0200

    Add simple modules with no real behaviour and fix consumption bug

---
+++ src/main/java/gregtech/common/tileentities/machines/multi/nanochip/modules/BoardProcessor.java
@@ -0,0 +1,102 @@
+package gregtech.common.tileentities.machines.multi.nanochip.modules;
+
+import static com.gtnewhorizon.structurelib.structure.StructureUtility.ofBlock;
+
+import net.minecraft.item.ItemStack;
+
+import com.gtnewhorizon.structurelib.structure.IStructureDefinition;
+import com.gtnewhorizon.structurelib.structure.ISurvivalBuildEnvironment;
+
+import gregtech.api.GregTech_API;
+import gregtech.api.interfaces.metatileentity.IMetaTileEntity;
+import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
+import gregtech.api.recipe.RecipeMap;
+import gregtech.api.recipe.RecipeMaps;
+import gregtech.api.util.GT_Multiblock_Tooltip_Builder;
+import gregtech.common.tileentities.machines.multi.nanochip.GT_MetaTileEntity_NanochipAssemblyModuleBase;
+import gregtech.common.tileentities.machines.multi.nanochip.util.CircuitComponent;
+import gregtech.common.tileentities.machines.multi.nanochip.util.ModuleStructureDefinition;
+
+public class BoardProcessor extends GT_MetaTileEntity_NanochipAssemblyModuleBase<BoardProcessor> {
+
+    protected static final int STRUCTURE_OFFSET_X = 3;
+    protected static final int STRUCTURE_OFFSET_Y = 3;
+    protected static final int STRUCTURE_OFFSET_Z = -2;
+
+    protected static final String STRUCTURE_PIECE_MAIN = "main";
+    private static final String[][] structure = new String[][] { { "  AAA  ", "  AAA  ", "  AAA  " },
+        { "  AAA  ", "  A A  ", "  AAA  " }, { "  AAA  ", "  AAA  ", "  AAA  " } };
+
+    public static final IStructureDefinition<BoardProcessor> STRUCTURE_DEFINITION = ModuleStructureDefinition
+        .<BoardProcessor>builder()
+        .addShape(STRUCTURE_PIECE_MAIN, structure)
+        .addElement('A', ofBlock(GregTech_API.sBlockCasings4, 0))
+        .build();
+
+    public BoardProcessor(int aID, String aName, String aNameRegional) {
+        super(aID, aName, aNameRegional);
+    }
+
+    protected BoardProcessor(String aName) {
+        super(aName);
+    }
+
+    @Override
+    public IStructureDefinition<BoardProcessor> getStructureDefinition() {
+        return STRUCTURE_DEFINITION;
+    }
+
+    @Override
+    public void construct(ItemStack trigger, boolean hintsOnly) {
+        // Should only construct the main structure, since the base structure is built by the nanochip assembly complex.
+        buildPiece(
+            STRUCTURE_PIECE_MAIN,
+            trigger,
+            hintsOnly,
+            STRUCTURE_OFFSET_X,
+            STRUCTURE_OFFSET_Y,
+            STRUCTURE_OFFSET_Z);
+    }
+
+    @Override
+    public int survivalConstruct(ItemStack trigger, int elementBudget, ISurvivalBuildEnvironment env) {
+        // Should only construct the main structure, since the base structure is built by the nanochip assembly complex.
+        return survivialBuildPiece(
+            STRUCTURE_PIECE_MAIN,
+            trigger,
+            STRUCTURE_OFFSET_X,
+            STRUCTURE_OFFSET_Y,
+            STRUCTURE_OFFSET_Z,
+            elementBudget,
+            env,
+            false,
+            true);
+    }
+
+    @Override
+    public boolean checkMachine(IGregTechTileEntity aBaseMetaTileEntity, ItemStack aStack) {
+        // Check base structure
+        if (!super.checkMachine(aBaseMetaTileEntity, aStack)) return false;
+        // Now check module structure
+        return checkPiece(STRUCTURE_PIECE_MAIN, STRUCTURE_OFFSET_X, STRUCTURE_OFFSET_Y, STRUCTURE_OFFSET_Z);
+    }
+
+    @Override
+    protected GT_Multiblock_Tooltip_Builder createTooltip() {
+        return new GT_Multiblock_Tooltip_Builder().toolTipFinisher("GregTech");
+    }
+
+    @Override
+    public IMetaTileEntity newMetaEntity(IGregTechTileEntity aTileEntity) {
+        return new BoardProcessor(this.mName);
+    }
+
+    public static void registerLocalName(String unprocessedName, CircuitComponent component) {
+        component.fallbackLocalizedName = unprocessedName + " Die";
+    }
+
+    @Override
+    public RecipeMap<?> getRecipeMap() {
+        return RecipeMaps.nanochipBoardProcessorRecipes;
+    }
+}
