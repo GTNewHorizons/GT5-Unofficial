b0ec76230c4a78240358089d7a546e68d6bd580e
NotAPenguin <michiel.vandeginste@gmail.com>
2024-09-05 20:42:45 +0200

    remove comma

--- src/main/java/gregtech/common/items/IDMetaItem03.java
+++ src/main/java/gregtech/common/items/IDMetaItem03.java
@@ -232,7 +232,7 @@ public enum IDMetaItem03 {
     Transdimensional_Alignment_Matrix(758),
     Thermal_Superconductor(759),
     Relativistic_Heat_Capacitor(760),
-    Phononic_Seed_Crystal(761),;
+    Phononic_Seed_Crystal(761);
 
     public final int ID;
 
