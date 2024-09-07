25dac2d272ba4f14fb0e848076da4e543935e6df
Lorenzo Todisco <49115537+zTotto@users.noreply.github.com>
2024-09-08 00:12:16 +0200

    improved laser beam

--- src/main/java/gregtech/common/render/MeteorMinerRenderer.java
+++ src/main/java/gregtech/common/render/MeteorMinerRenderer.java
@@ -20,20 +20,60 @@ public class MeteorMinerRenderer extends TileEntitySpecialRenderer {
         ClientRegistry.bindTileEntitySpecialRenderer(TileEntityLaserBeacon.class, this);
     }
 
-    private void renderFakeLine(TileEntityLaserBeacon laser, double x1, double y1, double z1, double x2, double y2,
-        double z2) {
+    private void renderFakeLine(TileEntityLaserBeacon laser, double x, double y1, double z, double y2) {
         Tessellator tessellator = Tessellator.instance;
-        tessellator.startDrawingQuads();
-        tessellator.setColorRGBA_F(laser.getRed(), laser.getGreen(), laser.getBlue(), lineOpacity);
-        tessellator.addVertex(x1 - lineRadius, y1, z1);
-        tessellator.addVertex(x1 + lineRadius, y1, z1);
-        tessellator.addVertex(x2 + lineRadius, y2, z2);
-        tessellator.addVertex(x2 - lineRadius, y2, z2);
+        // tessellator.setColorRGBA_F(laser.getRed(), laser.getGreen(), laser.getBlue(), lineOpacity);
+        double x1 = x - lineRadius;
+        double x2 = x + lineRadius;
+        double z1 = z - lineRadius;
+        double z2 = z + lineRadius;
 
-        tessellator.addVertex(x1, y1, z1 - lineRadius);
-        tessellator.addVertex(x1, y1, z1 + lineRadius);
-        tessellator.addVertex(x2, y2, z2 + lineRadius);
-        tessellator.addVertex(x2, y2, z2 - lineRadius);
+        // Bottom face
+        tessellator.startDrawingQuads();
+        tessellator.addVertex(x1, y1, z1);
+        tessellator.addVertex(x2, y1, z1);
+        tessellator.addVertex(x2, y1, z2);
+        tessellator.addVertex(x1, y1, z2);
+        tessellator.draw();
+
+        // Top face
+        tessellator.startDrawingQuads();
+        tessellator.addVertex(x1, y2, z1);
+        tessellator.addVertex(x2, y2, z1);
+        tessellator.addVertex(x2, y2, z2);
+        tessellator.addVertex(x1, y2, z2);
+        tessellator.draw();
+
+        // Side face 1
+        tessellator.startDrawingQuads();
+        tessellator.addVertex(x1, y1, z1);
+        tessellator.addVertex(x2, y1, z1);
+        tessellator.addVertex(x2, y2, z1);
+        tessellator.addVertex(x1, y2, z1);
+        tessellator.draw();
+
+        // Side face 2
+        tessellator.startDrawingQuads();
+        tessellator.addVertex(x2, y1, z1);
+        tessellator.addVertex(x2, y1, z2);
+        tessellator.addVertex(x2, y2, z2);
+        tessellator.addVertex(x2, y2, z1);
+        tessellator.draw();
+
+        // Side face 3
+        tessellator.startDrawingQuads();
+        tessellator.addVertex(x2, y1, z2);
+        tessellator.addVertex(x1, y1, z2);
+        tessellator.addVertex(x1, y2, z2);
+        tessellator.addVertex(x2, y2, z2);
+        tessellator.draw();
+
+        // Side face 4
+        tessellator.startDrawingQuads();
+        tessellator.addVertex(x1, y1, z2);
+        tessellator.addVertex(x1, y1, z1);
+        tessellator.addVertex(x1, y2, z1);
+        tessellator.addVertex(x1, y2, z2);
         tessellator.draw();
     }
 
@@ -58,7 +98,7 @@ public class MeteorMinerRenderer extends TileEntitySpecialRenderer {
             GL11.glRotated(ltile.rotationAngle, ltile.rotAxisX, ltile.rotAxisY, ltile.rotAxisZ);
             GL11.glTranslated(-x - 0.5, -y - 0.5, -z - 0.5);
 
-            renderFakeLine(ltile, x + 0.5, y + 35.5, z + 0.5, x + 0.5, y + 0.5, z + 0.5);
+            renderFakeLine(ltile, x + 0.5, y + 35.5, z + 0.5, y + 0.5);
 
             GL11.glPopAttrib();
             GL11.glPopMatrix();
