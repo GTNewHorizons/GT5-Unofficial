c24d456111bfaf9d20ea956dc51af26254eabce5
NotAPenguin <michiel.vandeginste@gmail.com>
2024-09-09 11:10:20 +0200

    buff creon/mellion freeze

--- src/main/java/gregtech/loaders/postload/recipes/VacuumFreezerRecipes.java
+++ src/main/java/gregtech/loaders/postload/recipes/VacuumFreezerRecipes.java
@@ -435,7 +435,7 @@ public class VacuumFreezerRecipes implements Runnable {
             .fluidInputs(MaterialsUEVplus.Creon.getPlasma(1296L))
             .itemOutputs(GTOreDictUnificator.get(OrePrefixes.ingot, MaterialsUEVplus.Mellion, 9L))
             .fluidOutputs(MaterialsUEVplus.Creon.getMolten(1296L))
-            .duration(40 * SECONDS)
+            .duration(20 * SECONDS)
             .eut(TierEU.RECIPE_UMV)
             .addTo(vacuumFreezerRecipes);
     }
