eb3e0a3c1b1c5a5201bf2af56a63ccbedc7980df
PlayfulPiano <pianoboy8lp@gmail.com>
2024-09-09 21:48:59 -0400

    Steam Compressor Multi - Adjustments
    * L:EU ratio changed from 1.33 to 1.25
    * Recipe Duration multiplier changed from 1.5 to 1.6
    (Discount is unchanged, values now have less rounding issues at max parallel)
    
    * Tiers renamed to "Low Pressure" and "High Pressure"
    * Tooltip description made to be more consistent with other multiblocks
    * Controller name fixed in Tooltip

--- src/main/java/gtPlusPlus/xmod/gregtech/common/tileentities/machines/multi/processing/steam/MTESteamCompressor.java
+++ src/main/java/gtPlusPlus/xmod/gregtech/common/tileentities/machines/multi/processing/steam/MTESteamCompressor.java
@@ -256,8 +256,8 @@ public class MTESteamCompressor extends MTESteamMultiBase<MTESteamCompressor> im
             @Nonnull
             protected OverclockCalculator createOverclockCalculator(@NotNull GTRecipe recipe) {
                 return OverclockCalculator.ofNoOverclock(recipe)
-                    .setEUtDiscount(1.33 * tierMachine)
-                    .setSpeedBoost(1.5 / tierMachine);
+                    .setEUtDiscount(1.25 * tierMachine)
+                    .setSpeedBoost(1.6 / tierMachine);
             }
         }.setMaxParallelSupplier(this::getMaxParallelRecipes);
     }
@@ -271,11 +271,11 @@ public class MTESteamCompressor extends MTESteamMultiBase<MTESteamCompressor> im
     protected MultiblockTooltipBuilder createTooltip() {
         MultiblockTooltipBuilder tt = new MultiblockTooltipBuilder();
         tt.addMachineType(getMachineType())
-            .addInfo("Controller Block for the Steam Compressor")
-            .addInfo("33.3% faster than the single block Steam Compressor")
-            .addInfo("Uses only 66.6% of the steam/s that the single block Steam Compressor uses")
-            .addInfo("Processes 8x parallel")
-            .addInfo("Steel tier produces at twice the speed but with twice the steam consumption")
+            .addInfo("Controller Block for the Steam Squasher")
+            .addInfo("25% faster than using single block steam machines of the same pressure")
+            .addInfo("Only consumes steam at 62.5% of the L/s normally required")
+            .addInfo("Processes up to 8 items at once")
+            .addInfo("Items are processed at twice the speed but with twice the steam consumption under High Pressure")
             .addSeparator()
             .beginStructureBlock(3, 3, 4, false)
             .addInputBus(EnumChatFormatting.GOLD + "1" + EnumChatFormatting.GRAY + " Any casing", 1)
@@ -287,10 +287,10 @@ public class MTESteamCompressor extends MTESteamMultiBase<MTESteamCompressor> im
                     + EnumChatFormatting.GRAY
                     + " Any casing")
             .addStructureInfo("")
-            .addStructureInfo(EnumChatFormatting.BLUE + "Tier " + EnumChatFormatting.DARK_PURPLE + 1)
+            .addStructureInfo(EnumChatFormatting.BLUE + "Low Pressure " + EnumChatFormatting.DARK_PURPLE + "Tier")
             .addStructureInfo(EnumChatFormatting.GOLD + "25-30x" + EnumChatFormatting.GRAY + " Bronze Plated Bricks")
             .addStructureInfo("")
-            .addStructureInfo(EnumChatFormatting.BLUE + "Tier " + EnumChatFormatting.DARK_PURPLE + 2)
+            .addStructureInfo(EnumChatFormatting.BLUE + "High Pressure " + EnumChatFormatting.DARK_PURPLE + "Tier")
             .addStructureInfo(
                 EnumChatFormatting.GOLD + "25-30x" + EnumChatFormatting.GRAY + " Solid Steel Machine Casing")
             .addStructureInfo("")
