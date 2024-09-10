532c325a18e3a534b3a14eb15892318b2100578a
PlayfulPiano <pianoboy8lp@gmail.com>
2024-09-10 00:09:52 -0400

    Update MTESteamCompressor.java - shorter HP tooltip

--- src/main/java/gtPlusPlus/xmod/gregtech/common/tileentities/machines/multi/processing/steam/MTESteamCompressor.java
+++ src/main/java/gtPlusPlus/xmod/gregtech/common/tileentities/machines/multi/processing/steam/MTESteamCompressor.java
@@ -275,7 +275,7 @@ public class MTESteamCompressor extends MTESteamMultiBase<MTESteamCompressor> im
             .addInfo("25% faster than using single block steam machines of the same pressure")
             .addInfo("Only consumes steam at 62.5% of the L/s normally required")
             .addInfo("Processes up to 8 items at once")
-            .addInfo("Items are processed at twice the speed but with twice the steam consumption under High Pressure")
+            .addInfo("Processing Speed & Steam Consumption is doubled under High Pressure")
             .addSeparator()
             .beginStructureBlock(3, 3, 4, false)
             .addInputBus(EnumChatFormatting.GOLD + "1" + EnumChatFormatting.GRAY + " Any casing", 1)
