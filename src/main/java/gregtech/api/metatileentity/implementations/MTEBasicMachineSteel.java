26b23ea0595f553286564feb56b8c3e4c66da425
PlayfulPiano <pianoboy8lp@gmail.com>
2024-09-10 00:46:55 -0400

    Update MTEBasicMachineSteel.java - fix attempt 2

--- src/main/java/gregtech/api/metatileentity/implementations/MTEBasicMachineSteel.java
+++ src/main/java/gregtech/api/metatileentity/implementations/MTEBasicMachineSteel.java
@@ -8,6 +8,7 @@ import static gregtech.api.enums.Textures.BlockIcons.MACHINE_STEEL_SIDE;
 import static gregtech.api.enums.Textures.BlockIcons.MACHINE_STEEL_TOP;
 import static gregtech.api.enums.Textures.BlockIcons.OVERLAY_PIPE_OUT;
 
+import java.util.List;
 import gregtech.api.enums.Dyes;
 import gregtech.api.enums.SteamVariant;
 import gregtech.api.interfaces.ITexture;
