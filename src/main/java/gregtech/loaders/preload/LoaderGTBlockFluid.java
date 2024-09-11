2d622156f1f5ce579e09a095664abeb61e4a3cd4
Martin Robertz <dream-master@gmx.net>
2024-09-11 08:17:31 +0200

    sa

--- src/main/java/gregtech/loaders/preload/LoaderGTBlockFluid.java
+++ src/main/java/gregtech/loaders/preload/LoaderGTBlockFluid.java
@@ -1322,7 +1322,7 @@ public class LoaderGTBlockFluid implements Runnable {
                 GTOreDictUnificator.get(OrePrefixes.cell, MaterialsUEVplus.Protomatter, 1L),
                 ItemList.Cell_Empty.get(1L));
 
- GTFluidFactory.builder("InfinityPlasma")
+        GTFluidFactory.builder("InfinityPlasma")
             .withLocalizedName("Infinity Plasma")
             .withStateAndTemperature(PLASMA, 10000)
             .buildAndRegister()
