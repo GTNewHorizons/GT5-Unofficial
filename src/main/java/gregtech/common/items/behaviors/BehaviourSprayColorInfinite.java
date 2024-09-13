3edb2da0fcd064f96a27fb0d5f2cd3875e190b75
Querns <querns@gbs.io>
2024-09-13 16:04:30 -0500

    Removes unused import

--- src/main/java/gregtech/common/items/behaviors/BehaviourSprayColorInfinite.java
+++ src/main/java/gregtech/common/items/behaviors/BehaviourSprayColorInfinite.java
@@ -7,7 +7,6 @@ import java.util.List;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
