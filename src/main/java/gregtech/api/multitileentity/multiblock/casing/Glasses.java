f86d20f9a7b872a64ae000335866eeff5842840b
BlueWeabo <ilia.iliev@tuta.com>
2024-09-15 13:07:20 +0300

    Revert deletion of Glasses

--- src/main/java/gregtech/api/multitileentity/multiblock/casing/Glasses.java
+++ src/main/java/gregtech/api/multitileentity/multiblock/casing/Glasses.java
@@ -1,6 +1,9 @@
 package gregtech.api.multitileentity.multiblock.casing;
 
+<<<<<<< HEAD
 import static com.gtnewhorizon.structurelib.structure.StructureUtility.ofBlockAnyMeta;
+=======
+>>>>>>> a3c50d9014 (Revert deletion of Glasses)
 import static com.gtnewhorizon.structurelib.structure.StructureUtility.ofBlockUnlocalizedName;
 import static com.gtnewhorizon.structurelib.structure.StructureUtility.ofChain;
 import static gregtech.api.enums.Mods.BartWorks;
@@ -10,8 +13,11 @@ import static gregtech.api.enums.Mods.Thaumcraft;
 
 import com.gtnewhorizon.structurelib.structure.IStructureElementChain;
 
+<<<<<<< HEAD
 import gregtech.api.GregTechAPI;
 
+=======
+>>>>>>> a3c50d9014 (Revert deletion of Glasses)
 public class Glasses {
 
     /** support all Bart, Botania, Ic2, Thaumcraft glasses for multiblock structure **/
@@ -28,9 +34,12 @@ public class Glasses {
             ofBlockUnlocalizedName(BartWorks.ID, "BW_GlasBlocks", 0, true),
             ofBlockUnlocalizedName(BartWorks.ID, "BW_GlasBlocks2", 0, true),
 
+<<<<<<< HEAD
             // Tinted Industrial Glass
             ofBlockAnyMeta(GregTechAPI.sBlockTintedGlass, 0),
 
+=======
+>>>>>>> a3c50d9014 (Revert deletion of Glasses)
             // warded glass
             ofBlockUnlocalizedName(Thaumcraft.ID, "blockCosmeticOpaque", 2, false));
     }
