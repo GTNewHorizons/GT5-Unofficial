51c8cb9811f0a11f9d5df5901353cf0d28b3c840
boubou19 <miisterunknown@gmail.com>
2024-09-19 21:23:10 +0200

    remove final

--- src/main/java/ggfab/ConfigurationHandler.java
+++ src/main/java/ggfab/ConfigurationHandler.java
@@ -8,5 +8,5 @@ import gregtech.api.enums.Mods;
 public class ConfigurationHandler {
     @Config.Comment("Laser overclock penalty factor. This will incredibly change the game balance. Even a small step from 0.2 to 0.3 can have very significant impact. Tweak with caution!")
     @Config.RangeFloat(min=0.0f, max=10.0f)
-    public static final float laserOCPenaltyFactor = 0.3f;
+    public static float laserOCPenaltyFactor = 0.3f;
 }
