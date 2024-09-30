e54b1bbd94685d904baff041e44f30f8a4efb8c2
miozune <miozune@gmail.com>
2024-09-30 14:36:40 +0900

    shutter

---
+++ src/main/java/gregtech/common/gui/modularui2/cover/CoverShutterGui.java
@@ -0,0 +1,68 @@
+package gregtech.common.gui.modularui2.cover;
+
+import com.cleanroommc.modularui.api.drawable.IKey;
+import com.cleanroommc.modularui.utils.Alignment;
+import com.cleanroommc.modularui.value.sync.EnumSyncValue;
+import com.cleanroommc.modularui.value.sync.PanelSyncManager;
+import com.cleanroommc.modularui.widgets.ToggleButton;
+import com.cleanroommc.modularui.widgets.layout.Flow;
+import com.cleanroommc.modularui.widgets.layout.Grid;
+
+import gregtech.api.modularui2.CoverGuiData;
+import gregtech.api.modularui2.GTGuiTextures;
+import gregtech.api.util.GTUtility;
+import gregtech.common.covers.CoverShutter;
+import gregtech.common.covers.modes.ShutterMode;
+import gregtech.common.gui.modularui2.LinkedBoolValue;
+
+public class CoverShutterGui extends CoverGui<CoverShutter> {
+
+    @Override
+    protected String getGuiId() {
+        return "cover.shutter";
+    }
+
+    @Override
+    public void addUIWidgets(CoverGuiData guiData, PanelSyncManager syncManager, Flow column) {
+        CoverShutter cover = getCover(guiData);
+        EnumSyncValue<ShutterMode> modeSyncValue = new EnumSyncValue<>(
+            ShutterMode.class,
+            cover::getShutterMode,
+            cover::setShutterMode);
+        syncManager.syncValue("mode", modeSyncValue);
+
+        column.child(
+            new Grid().marginLeft(WIDGET_MARGIN)
+                .coverChildren()
+                .minElementMarginRight(WIDGET_MARGIN)
+                .minElementMarginBottom(2)
+                .minElementMarginTop(0)
+                .minElementMarginLeft(0)
+                .alignment(Alignment.CenterLeft)
+                .row(
+                    new ToggleButton().value(LinkedBoolValue.of(modeSyncValue, ShutterMode.OPEN_IF_ENABLED))
+                        .overlay(true, GTGuiTextures.OVERLAY_BUTTON_CHECKMARK)
+                        .size(16),
+                    IKey.str(GTUtility.trans("082", "Open if work enabled"))
+                        .asWidget())
+                .row(
+                    new ToggleButton().value(LinkedBoolValue.of(modeSyncValue, ShutterMode.OPEN_IF_DISABLED))
+                        .overlay(true, GTGuiTextures.OVERLAY_BUTTON_CHECKMARK)
+                        .size(16),
+                    IKey.str(GTUtility.trans("083", "Open if work disabled"))
+                        .asWidget())
+                .row(
+                    new ToggleButton().value(LinkedBoolValue.of(modeSyncValue, ShutterMode.ONLY_OUTPUT))
+                        .overlay(true, GTGuiTextures.OVERLAY_BUTTON_CHECKMARK)
+                        .size(16),
+                    IKey.str(GTUtility.trans("084", "Only Output allowed"))
+                        .asWidget())
+                .row(
+                    new ToggleButton().value(LinkedBoolValue.of(modeSyncValue, ShutterMode.ONLY_INPUT))
+                        .overlay(true, GTGuiTextures.OVERLAY_BUTTON_CHECKMARK)
+                        .size(16),
+                    IKey.str(GTUtility.trans("085", "Only Input allowed"))
+                        .asWidget()));
+    }
+
+}
