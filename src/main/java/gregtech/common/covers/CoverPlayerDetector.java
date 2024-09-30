49eb91f3dc16ccac71d58fd4dd65c49d178c7e27
miozune <miozune@gmail.com>
2024-09-30 14:36:40 +0900

    player detector

--- src/main/java/gregtech/common/covers/CoverPlayerDetector.java
+++ src/main/java/gregtech/common/covers/CoverPlayerDetector.java
@@ -5,10 +5,19 @@ import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.fluids.Fluid;
 
+import com.cleanroommc.modularui.api.drawable.IKey;
+import com.cleanroommc.modularui.utils.Alignment;
+import com.cleanroommc.modularui.value.sync.EnumSyncValue;
+import com.cleanroommc.modularui.value.sync.PanelSyncManager;
+import com.cleanroommc.modularui.widgets.ToggleButton;
+import com.cleanroommc.modularui.widgets.layout.Flow;
+import com.cleanroommc.modularui.widgets.layout.Grid;
 import com.gtnewhorizons.modularui.api.screen.ModularWindow;
 import com.gtnewhorizons.modularui.common.widget.TextWidget;
 
 import gregtech.api.gui.modularui.CoverUIBuildContext;
+import gregtech.api.gui.modularui2.CoverGuiData;
+import gregtech.api.gui.modularui2.GTGuiTextures;
 import gregtech.api.interfaces.ITexture;
 import gregtech.api.interfaces.tileentity.ICoverable;
 import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
@@ -17,6 +26,7 @@ import gregtech.api.util.GTUtility;
 import gregtech.api.util.ISerializableObject;
 import gregtech.common.gui.modularui.widget.CoverDataControllerWidget;
 import gregtech.common.gui.modularui.widget.CoverDataFollowerToggleButtonWidget;
+import gregtech.common.gui.modularui2.LinkedBoolValue;
 
 public class CoverPlayerDetector extends CoverBehavior {
 
@@ -143,6 +153,74 @@ public class CoverPlayerDetector extends CoverBehavior {
     @Override
     public boolean hasCoverGUI() {
         return true;
+    }
+
+    @Override
+    protected String getGuiId() {
+        return "cover.player_detector";
+    }
+
+    @Override
+    public void addUIWidgets(CoverGuiData guiData, PanelSyncManager syncManager, Flow column) {
+        EnumSyncValue<Mode> modeSyncValue = new EnumSyncValue<>(
+            Mode.class,
+            () -> getMode(guiData),
+            value -> setMode(value, guiData));
+        syncManager.syncValue("mode", modeSyncValue);
+
+        column.child(
+            new Grid().marginLeft(WIDGET_MARGIN)
+                .coverChildren()
+                .minElementMarginRight(WIDGET_MARGIN)
+                .minElementMarginBottom(2)
+                .minElementMarginTop(0)
+                .minElementMarginLeft(0)
+                .alignment(Alignment.CenterLeft)
+                .row(
+                    new ToggleButton().value(LinkedBoolValue.of(modeSyncValue, Mode.ANY_PLAYER))
+                        .overlay(true, GTGuiTextures.OVERLAY_BUTTON_CHECKMARK)
+                        .addTooltipLine(IKey.str(GTUtility.trans("068.1", "Emit if any Player is close")))
+                        .size(16),
+                    IKey.str(GTUtility.trans("319", "Any player"))
+                        .asWidget())
+                .row(
+                    new ToggleButton().value(LinkedBoolValue.of(modeSyncValue, Mode.OTHER_PLAYERS))
+                        .overlay(true, GTGuiTextures.OVERLAY_BUTTON_CHECKMARK)
+                        .addTooltipLine(IKey.str(GTUtility.trans("069.1", "Emit if other Player is close")))
+                        .size(16),
+                    IKey.str(GTUtility.trans("320", "Other players"))
+                        .asWidget())
+                .row(
+                    new ToggleButton().value(LinkedBoolValue.of(modeSyncValue, Mode.ONLY_OWNER))
+                        .overlay(true, GTGuiTextures.OVERLAY_BUTTON_CHECKMARK)
+                        .addTooltipLine(IKey.str(GTUtility.trans("070", "Emit if you are close")))
+                        .size(16),
+                    IKey.str(GTUtility.trans("321", "Only owner"))
+                        .asWidget()));
+    }
+
+    private enum Mode {
+
+        ANY_PLAYER,
+        OTHER_PLAYERS,
+        ONLY_OWNER;
+
+        private static final Mode[] VALUES = values();
+    }
+
+    private Mode getMode(CoverGuiData guiData) {
+        int coverVariable = convert(getCoverData(guiData));
+        if (coverVariable >= 0 && coverVariable < Mode.VALUES.length) {
+            return Mode.VALUES[coverVariable];
+        }
+        return Mode.ANY_PLAYER;
+    }
+
+    private void setMode(Mode mode, CoverGuiData guiData) {
+        int coverVariable = convert(getCoverData(guiData));
+        if (mode.ordinal() == coverVariable) return;
+
+        guiData.setCoverData(new ISerializableObject.LegacyCoverData(mode.ordinal()));
     }
 
     @Override
