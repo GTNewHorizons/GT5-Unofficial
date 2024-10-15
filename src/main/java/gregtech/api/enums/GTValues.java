1d894089d0d14efde4195afc967c9be9d3db97dd
RecursivePineapple <recursive_pineapple@proton.me>
2024-10-15 16:20:27 -0400

    Improved & cleaned up wormhole generator (#3334)

--- src/main/java/gregtech/api/enums/GTValues.java
+++ src/main/java/gregtech/api/enums/GTValues.java
@@ -683,6 +683,8 @@ public class GTValues {
         + EnumChatFormatting.BOLD
         + "APenguin";
 
+    public static final String AuthorPineapple = "Author: " + EnumChatFormatting.BLUE + "Recursive Pineapple";
+
     // 7.5F comes from GT_Tool_Turbine_Large#getBaseDamage() given huge turbines are the most efficient now.
     public static double getMaxPlasmaTurbineEfficiencyFromMaterial(Materials material) {
         return (5F + (7.5F + material.mToolQuality)) / 10.0;
