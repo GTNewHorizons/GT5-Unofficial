08a92dbc0b2aedaff7689ded911e35e1f0722f6e
Allison-Rodrigo <95299389+Nockyx@users.noreply.github.com>
2024-10-24 22:06:08 -0300

    Revert "Merge branch 'master' of C:\Users\allis\IdeaProjects\GT5-Unofficial with conflicts."
    This reverts commit b7078a033336478263d91f611df7bbab4899d8f8.

--- src/main/java/gregtech/api/enums/Materials.java
+++ src/main/java/gregtech/api/enums/Materials.java
@@ -2518,10 +2518,6 @@ public class Materials implements IColorModulationContainer, ISubTagContainer {
         Carbon.add(SubTag.NO_SMELTING);
         Boron.add(SubTag.SMELTING_TO_FLUID);
 
-<<<<<<< HEAD
-        Infinity.add(SubTag.BLACK_HOLE);
-=======
->>>>>>> origin/master
         MaterialsUEVplus.TranscendentMetal.add(SubTag.BLACK_HOLE);
     }
 
