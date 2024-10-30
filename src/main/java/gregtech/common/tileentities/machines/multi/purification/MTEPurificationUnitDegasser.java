01adad25602ffb9619c10bdb3ff38df00665c9d2
NotAPenguin <michiel.vandeginste@gmail.com>
2024-10-30 12:09:01 +0100

    factor in parallel in T7 water plant bonus output

--- src/main/java/gregtech/common/tileentities/machines/multi/purification/MTEPurificationUnitDegasser.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/purification/MTEPurificationUnitDegasser.java
@@ -670,7 +670,7 @@ public class MTEPurificationUnitDegasser extends MTEPurificationUnitBase<MTEPuri
             FluidStack waterOutput = currentRecipe.mFluidOutputs[0];
             FluidStack bonusOutput = new FluidStack(
                 waterOutput.getFluid(),
-                (int) (waterOutput.amount * (outputMultiplier - 1.0f)));
+                (int) (this.effectiveParallel * waterOutput.amount * (outputMultiplier - 1.0f)));
             this.addOutput(bonusOutput);
         }
     }
