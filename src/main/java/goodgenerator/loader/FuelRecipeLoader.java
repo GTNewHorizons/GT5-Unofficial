296ee2903ed8d4b1ade60c85d3b4f1ef3e98b9f6
StaffiX <32968022+StaffiX@users.noreply.github.com>
2024-10-31 22:42:50 +0100

    fix shirabon usage

--- src/main/java/goodgenerator/loader/FuelRecipeLoader.java
+++ src/main/java/goodgenerator/loader/FuelRecipeLoader.java
@@ -192,7 +192,7 @@ public class FuelRecipeLoader {
                 GTOreDictUnificator.get(OrePrefixes.dust, Materials.Tritanium, 48))
             .fluidInputs(
                 GGMaterial.naquadahBasedFuelMkV.getFluidOrGas(2000),
-                FluidRegistry.getFluidStack("molten.shirabon", 1440),
+                FluidRegistry.getFluidStack("molten.shirabon", 360),
                 MaterialsUEVplus.RawStarMatter.getFluid(72L))
             .fluidOutputs(GGMaterial.naquadahBasedFuelMkVI.getFluidOrGas(1250))
             .duration(3 * SECONDS)
