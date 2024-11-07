0ef3b5a80a68b211149d58f8ed38c0a7edfdd96e
StaffiX <32968022+StaffiX@users.noreply.github.com>
2024-11-07 23:43:51 +0100

    Fix EHE NEI special info (#3457)
    Co-authored-by: Martin Robertz <dream-master@gmx.net>

--- src/main/java/goodgenerator/api/recipe/ExtremeHeatExchangerFrontend.java
+++ src/main/java/goodgenerator/api/recipe/ExtremeHeatExchangerFrontend.java
@@ -43,15 +43,20 @@ public class ExtremeHeatExchangerFrontend extends RecipeMapFrontend {
         public List<String> format(RecipeDisplayInfo recipeInfo) {
             FluidStack[] Inputs = recipeInfo.recipe.mFluidInputs;
             FluidStack[] Outputs = recipeInfo.recipe.mFluidOutputs;
+            int denseMultiplier = 1;
+            if (Inputs[0].getUnlocalizedName()
+                .contains("plasma")) {
+                denseMultiplier = 1000;
+            }
             int threshold = recipeInfo.recipe.mSpecialValue;
             return Arrays.asList(
                 StatCollector.translateToLocal("value.extreme_heat_exchanger.0") + " "
                     + GTUtility.formatNumbers(Inputs[0].amount)
                     + " L/s",
                 StatCollector.translateToLocal("value.extreme_heat_exchanger.1"),
-                GTUtility.formatNumbers(Outputs[0].amount / 160) + " L/s",
+                GTUtility.formatNumbers(Outputs[0].amount / 160 * denseMultiplier) + " L/s",
                 StatCollector.translateToLocal("value.extreme_heat_exchanger.2"),
-                GTUtility.formatNumbers(Outputs[1].amount / 160) + " L/s",
+                GTUtility.formatNumbers(Outputs[1].amount / 160 * denseMultiplier) + " L/s",
                 StatCollector.translateToLocal("value.extreme_heat_exchanger.4") + " " + threshold + " L/s");
         }
     }
