cd2030ee15e6e147696e397349dc976345ab62a5
BlueHero233 <jeleiacraft@gmail.com>
2024-11-14 17:04:39 -0300

    Add Raw Ores to Isamill recipes

--- src/main/java/gtPlusPlus/core/item/base/ore/BaseItemMilledOre.java
+++ src/main/java/gtPlusPlus/core/item/base/ore/BaseItemMilledOre.java
@@ -20,37 +20,45 @@ public class BaseItemMilledOre extends BaseOreComponent {
         super(material, BaseOreComponent.ComponentTypes.MILLED);
 
         ItemStack oreStack = material.getOre(16);
+        ItemStack rawStack = material.getRawOre(16);
         ItemStack crushedStack = material.getCrushed(16);
-
-        ItemStack milledStackOres1 = material.getMilled(64);
-        ItemStack milledStackCrushed1 = material.getMilled(32);
-        ItemStack milledStackOres2 = material.getMilled(48);
-        ItemStack milledStackCrushed2 = material.getMilled(16);
 
         ItemStack millingBall_Alumina = GregtechItemList.Milling_Ball_Alumina.get(0);
         ItemStack millingBall_Soapstone = GregtechItemList.Milling_Ball_Soapstone.get(0);
 
         GTValues.RA.stdBuilder()
             .itemInputs(GTUtility.getIntegratedCircuit(10), oreStack, millingBall_Alumina)
-            .itemOutputs(milledStackOres1)
+            .itemOutputs(material.getMilled(64))
             .duration(2 * MINUTES)
             .eut(materialEU)
             .addTo(millingRecipes);
         GTValues.RA.stdBuilder()
             .itemInputs(GTUtility.getIntegratedCircuit(11), oreStack, millingBall_Soapstone)
-            .itemOutputs(milledStackOres2)
+            .itemOutputs(material.getMilled(48))
             .duration(2 * MINUTES + 30 * SECONDS)
             .eut(materialEU)
             .addTo(millingRecipes);
         GTValues.RA.stdBuilder()
+            .itemInputs(GTUtility.getIntegratedCircuit(12), rawStack, millingBall_Soapstone)
+            .itemOutputs(material.getMilled(48))
+            .duration(2 * MINUTES + 15 * SECONDS)
+            .eut(materialEU)
+            .addTo(millingRecipes);
+        GTValues.RA.stdBuilder()
+            .itemInputs(GTUtility.getIntegratedCircuit(12), rawStack, millingBall_Alumina)
+            .itemOutputs(material.getMilled(56))
+            .duration(1 * MINUTES + 45 * SECONDS)
+            .eut(materialEU)
+            .addTo(millingRecipes);
+        GTValues.RA.stdBuilder()
             .itemInputs(GTUtility.getIntegratedCircuit(10), crushedStack, millingBall_Alumina)
-            .itemOutputs(milledStackCrushed1)
+            .itemOutputs(material.getMilled(32))
             .duration(1 * MINUTES)
             .eut(materialEU)
             .addTo(millingRecipes);
         GTValues.RA.stdBuilder()
             .itemInputs(GTUtility.getIntegratedCircuit(11), crushedStack, millingBall_Soapstone)
-            .itemOutputs(milledStackCrushed2)
+            .itemOutputs(material.getMilled(16))
             .duration(1 * MINUTES + 15 * SECONDS)
             .eut(materialEU)
             .addTo(millingRecipes);
