1c1974e762350a756959b840b5d0d60dcb55223e
GitHub GTNH Actions <>
2024-12-26 19:20:50 +0000

    spotlessApply

--- src/mixin/java/gregtech/mixin/mixins/early/minecraft/ItemToolMaterialMixin.java
+++ src/mixin/java/gregtech/mixin/mixins/early/minecraft/ItemToolMaterialMixin.java
@@ -1,6 +1,7 @@
 package gregtech.mixin.mixins.early.minecraft;
 
 import net.minecraft.item.Item;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -8,9 +9,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Item.ToolMaterial.class)
 public class ItemToolMaterialMixin {
+
     @Inject(method = "getEfficiencyOnProperMaterial", at = @At("HEAD"), cancellable = true)
-    public void gt5u$getEfficiencyOnProperMaterial(CallbackInfoReturnable<Float> cir){
-        if ((Object)this == Item.ToolMaterial.WOOD){
+    public void gt5u$getEfficiencyOnProperMaterial(CallbackInfoReturnable<Float> cir) {
+        if ((Object) this == Item.ToolMaterial.WOOD) {
             cir.setReturnValue(4.0F);
             cir.cancel();
         }
