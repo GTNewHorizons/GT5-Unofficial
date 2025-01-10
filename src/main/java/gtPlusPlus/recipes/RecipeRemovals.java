2b5e35cd2b15fd8e07a164f7836b1f0b28a8a989
Maya <10861407+serenibyss@users.noreply.github.com>
2025-01-10 16:19:06 -0600

    spotless because our action is broken rn

--- src/main/java/gtPlusPlus/recipes/RecipeRemovals.java
+++ src/main/java/gtPlusPlus/recipes/RecipeRemovals.java
@@ -1,23 +1,7 @@
 package gtPlusPlus.recipes;
 
-import static gtPlusPlus.core.util.minecraft.MaterialUtils.getMaterialName;
-
-import java.util.Collection;
-
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-
-import org.apache.commons.lang3.ArrayUtils;
-
 import advsolar.common.AdvancedSolarPanel;
-import gregtech.api.enums.Materials;
 import gregtech.api.enums.Mods;
-import gregtech.api.items.MetaGeneratedTool;
-import gregtech.api.recipe.RecipeMaps;
-import gregtech.api.util.GTRecipe;
-import gregtech.common.items.MetaGeneratedTool01;
-import gtPlusPlus.api.objects.Logger;
-import gtPlusPlus.core.config.Configuration;
 import gtPlusPlus.core.util.minecraft.ItemUtils;
 import gtPlusPlus.core.util.minecraft.RecipeUtils;
 
