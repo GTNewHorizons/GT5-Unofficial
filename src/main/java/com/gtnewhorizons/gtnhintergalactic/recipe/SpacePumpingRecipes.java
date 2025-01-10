f2623b52168dce15de5ee4fc3d386019fb710d47
Yoshy <60155833+Yoshy2002@users.noreply.github.com>
2025-01-10 18:16:05 +0100

    add missing Fluids/Gasses to Space Pump (#98)
    Co-authored-by: Martin Robertz <dream-master@gmx.net>
    Co-authored-by: Maya <10861407+serenibyss@users.noreply.github.com>

--- src/main/java/com/gtnewhorizons/gtnhintergalactic/recipe/SpacePumpingRecipes.java
+++ src/main/java/com/gtnewhorizons/gtnhintergalactic/recipe/SpacePumpingRecipes.java
@@ -29,14 +29,21 @@ public class SpacePumpingRecipes {
         // T2
         RECIPES.put(Pair.of(2, 1), Materials.Chlorobenzene.getFluid(896000));
 
-        // T3 (the end?)
+        // T3
         RECIPES.put(Pair.of(3, 1), FluidRegistry.getFluidStack("endergoo", 32000));
+        RECIPES.put(Pair.of(3, 2), Materials.OilExtraHeavy.getFluid(1400000));
+        RECIPES.put(Pair.of(3, 3), Materials.Lava.getFluid(1800000));
+        RECIPES.put(Pair.of(3, 4), Materials.NatruralGas.getGas(1400000));
 
         // T4
         RECIPES.put(Pair.of(4, 1), Materials.SulfuricAcid.getFluid(784000));
         RECIPES.put(Pair.of(4, 2), Materials.Iron.getMolten(896000));
         RECIPES.put(Pair.of(4, 3), Materials.Oil.getFluid(1400000));
         RECIPES.put(Pair.of(4, 4), Materials.OilHeavy.getFluid(1792000));
+        RECIPES.put(Pair.of(4, 5), Materials.Lead.getMolten(896000));
+        RECIPES.put(Pair.of(4, 6), Materials.OilMedium.getFluid(1400000));
+        RECIPES.put(Pair.of(4, 7), Materials.OilLight.getFluid(780000));
+        RECIPES.put(Pair.of(4, 8), FluidRegistry.getFluidStack("carbondioxide", 1680000));
 
         // T5
         RECIPES.put(Pair.of(5, 1), Materials.CarbonMonoxide.getGas(4480000));
@@ -46,6 +53,8 @@ public class SpacePumpingRecipes {
         RECIPES.put(Pair.of(5, 5), Materials.LiquidOxygen.getGas(896000));
         RECIPES.put(Pair.of(5, 7), Materials.Argon.getGas(32000));
         RECIPES.put(Pair.of(5, 9), Materials.Methane.getGas(1792000));
+        RECIPES.put(Pair.of(5, 10), FluidRegistry.getFluidStack("liquid_hydricsulfur", 392000));
+        RECIPES.put(Pair.of(5, 11), Materials.Ethane.getGas(1194000));
 
         // T6
         RECIPES.put(Pair.of(6, 1), Materials.Deuterium.getGas(1568000));
@@ -65,6 +74,7 @@ public class SpacePumpingRecipes {
         RECIPES.put(Pair.of(8, 3), Materials.Copper.getMolten(672000));
         RECIPES.put(Pair.of(8, 5), FluidRegistry.getFluidStack("ic2distilledwater", 17920000));
         RECIPES.put(Pair.of(8, 6), Materials.Radon.getGas(64000));
+        RECIPES.put(Pair.of(8, 7), Materials.Tin.getMolten(672000));
 
         if (Mods.BartWorks.isModLoaded()) {
             // T5
