f4727c0625e9a3400b0570ef9673a69504c9caf3
Mary Hopson <doyoumined@gmail.com>
2025-01-19 12:01:37 -0500

    Make solars gooder

--- src/main/java/gregtech/common/tileentities/generators/MTESolarGenerator.java
+++ src/main/java/gregtech/common/tileentities/generators/MTESolarGenerator.java
@@ -2,22 +2,38 @@ package gregtech.common.tileentities.generators;
 
 import static gregtech.api.enums.GTValues.V;
 import static gregtech.api.enums.Textures.BlockIcons.OVERLAYS_ENERGY_OUT;
+import static gregtech.api.util.GTUtility.formatNumbers;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
+import com.gtnewhorizons.modularui.api.math.Alignment;
+import com.gtnewhorizons.modularui.api.screen.ModularWindow;
+import com.gtnewhorizons.modularui.api.screen.UIBuildContext;
+import com.gtnewhorizons.modularui.common.internal.wrapper.BaseSlot;
+import com.gtnewhorizons.modularui.common.widget.DrawableWidget;
+import com.gtnewhorizons.modularui.common.widget.FakeSyncWidget;
+import com.gtnewhorizons.modularui.common.widget.ProgressBar;
+import com.gtnewhorizons.modularui.common.widget.SlotGroup;
+import com.gtnewhorizons.modularui.common.widget.TextWidget;
+
 import gregtech.api.enums.GTValues;
 import gregtech.api.enums.Textures;
+import gregtech.api.gui.modularui.GTUIInfos;
+import gregtech.api.gui.modularui.GTUITextures;
 import gregtech.api.interfaces.ITexture;
+import gregtech.api.interfaces.modularui.IAddGregtechLogo;
+import gregtech.api.interfaces.modularui.IAddUIWidgets;
 import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.implementations.MTETieredMachineBlock;
 import gregtech.api.render.TextureFactory;
 
-public class MTESolarGenerator extends MTETieredMachineBlock {
+public class MTESolarGenerator extends MTETieredMachineBlock implements IAddUIWidgets, IAddGregtechLogo {
 
     public MTESolarGenerator(int aID, String aName, String aNameRegional, int aTier) {
         super(
@@ -25,7 +41,7 @@ public class MTESolarGenerator extends MTETieredMachineBlock {
             aName,
             aNameRegional,
             aTier,
-            0,
+            4,
             new String[] { "Generates EU From Solar Power", "Does not generate power when raining",
                 "Cleans itself automatically", "Does not explode in rain!" });
     }
@@ -85,16 +101,107 @@ public class MTESolarGenerator extends MTETieredMachineBlock {
         return true;
     }
 
+    // No logic for charge vs decharge because generator should not be chargeable
+    @Override
+    public int rechargerSlotCount() {
+        return 4;
+    }
+
+    @Override
+    public boolean onRightclick(IGregTechTileEntity aBaseMetaTileEntity, EntityPlayer aPlayer, ForgeDirection side,
+        float aX, float aY, float aZ) {
+        GTUIInfos.openGTTileEntityUI(aBaseMetaTileEntity, aPlayer);
+        return true;
+    }
+
+    protected long clientEU;
+
+    @Override
+    public void addUIWidgets(ModularWindow.Builder builder, UIBuildContext buildContext) {
+        addGregTechLogo(builder);
+        addConditionalImages(builder);
+        builder.widget(
+            SlotGroup.ofItemHandler(inventoryHandler, 2)
+                .startFromSlot(0)
+                .endAtSlot(3)
+                .slotCreator(index -> new BaseSlot(inventoryHandler, index) {
+
+                    @Override
+                    public int getSlotStackLimit() {
+                        return 1;
+                    }
+                })
+                .background(getGUITextureSet().getItemSlot(), GTUITextures.OVERLAY_SLOT_CHARGER)
+                .build()
+                .setPos(100, 15))
+            .widget(
+                new ProgressBar()
+                    .setProgress(
+                        () -> (float) getBaseMetaTileEntity().getStoredEU() / getBaseMetaTileEntity().getEUCapacity())
+                    .setDirection(ProgressBar.Direction.RIGHT)
+                    .setTexture(GTUITextures.PROGRESSBAR_STORED_EU, 147)
+                    .setPos(14, 74)
+                    .setSize(147, 5))
+            .widget(
+                new TextWidget()
+                    .setStringSupplier(
+                        () -> formatNumbers(clientEU) + "/"
+                            + formatNumbers(getBaseMetaTileEntity().getEUCapacity())
+                            + " EU")
+                    .setTextAlignment(Alignment.Center)
+                    .setPos(14, 66)
+                    .setSize(147, 5))
+            .widget(new FakeSyncWidget.LongSyncer(() -> getBaseMetaTileEntity().getStoredEU(), val -> clientEU = val));
+    }
+
+    public void addConditionalImages(ModularWindow.Builder builder) {
+        builder
+            .widget(
+                new DrawableWidget()
+                    .setDrawable(
+                        () -> dayTime ? GTUITextures.OVERLAY_BUTTON_CHECKMARK : GTUITextures.OVERLAY_BUTTON_CROSS)
+                    .setPos(5, 10)
+                    .setSize(16, 16))
+            .widget(new TextWidget(StatCollector.translateToLocal("GT5U.machines.solarindicator1")).setPos(21, 15))
+            .widget(new FakeSyncWidget.BooleanSyncer(() -> dayTime, val -> dayTime = val))
+            .widget(
+                new DrawableWidget()
+                    .setDrawable(
+                        () -> noRain ? GTUITextures.OVERLAY_BUTTON_CHECKMARK : GTUITextures.OVERLAY_BUTTON_CROSS)
+                    .setPos(5, 26)
+                    .setSize(16, 16))
+            .widget(new TextWidget(StatCollector.translateToLocal("GT5U.machines.solarindicator2")).setPos(21, 31))
+            .widget(new FakeSyncWidget.BooleanSyncer(() -> noRain, val -> noRain = val))
+            .widget(
+                new DrawableWidget()
+                    .setDrawable(
+                        () -> seesSky ? GTUITextures.OVERLAY_BUTTON_CHECKMARK : GTUITextures.OVERLAY_BUTTON_CROSS)
+                    .setPos(5, 42)
+                    .setSize(16, 16))
+            .widget(new TextWidget(StatCollector.translateToLocal("GT5U.machines.solarindicator3")).setPos(21, 47))
+            .widget(new FakeSyncWidget.BooleanSyncer(() -> seesSky, val -> seesSky = val));
+    }
+
+    @Override
+    public void addGregTechLogo(ModularWindow.Builder builder) {
+        builder.widget(
+            new DrawableWidget().setDrawable(GTUITextures.PICTURE_GT_LOGO_17x17_TRANSPARENT)
+                .setSize(17, 17)
+                .setPos(154, 5));
+    }
+
     private boolean valid = true;
 
     @Override
     public void onPostTick(IGregTechTileEntity aBaseMetaTileEntity, long aTick) {
-        // Check every 5 seconds for world conditions
-        if (aTick % 100 == 0) {
-            doWorldChecks(aBaseMetaTileEntity.getWorld(), aBaseMetaTileEntity);
-        }
-        if (aTick % 20 == 0 && valid) {
-            aBaseMetaTileEntity.increaseStoredEnergyUnits(maxEUOutput() * 20, false);
+        if (aBaseMetaTileEntity.isServerSide()) {
+            // Check every 5 seconds for world conditions
+            if (aTick % 100 == 0) {
+                doWorldChecks(aBaseMetaTileEntity.getWorld(), aBaseMetaTileEntity);
+            }
+            if (aTick % 20 == 0 && valid) {
+                aBaseMetaTileEntity.increaseStoredEnergyUnits(maxEUOutput() * 20, false);
+            }
         }
         super.onPostTick(aBaseMetaTileEntity, aTick);
     }
@@ -105,9 +212,17 @@ public class MTESolarGenerator extends MTETieredMachineBlock {
         super.onFirstTick(aBaseMetaTileEntity);
     }
 
+    // Checks are independent for the sake of the ui indicators
+    private boolean noRain = false;
+    private boolean dayTime = false;
+    private boolean seesSky = false;
+
     private void doWorldChecks(World world, IGregTechTileEntity aBaseMetaTileEntity) {
-        valid = (!(world.isRaining() && aBaseMetaTileEntity.getBiome().rainfall > 0.0F) && world.isDaytime()
-            && aBaseMetaTileEntity.getSkyAtSide(ForgeDirection.UP));
+        noRain = !(world.isRaining() && aBaseMetaTileEntity.getBiome().rainfall > 0.0F);
+        dayTime = world.isDaytime();
+        seesSky = aBaseMetaTileEntity.getSkyAtSide(ForgeDirection.UP);
+
+        valid = noRain && dayTime && seesSky;
     }
 
     @Override
