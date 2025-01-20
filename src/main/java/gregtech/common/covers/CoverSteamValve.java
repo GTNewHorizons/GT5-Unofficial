7ba143513f73e140842ddfa1d13f1b429625b5b9
Guillaume Mercier <10gui-gui10@live.ca>
2025-01-20 15:45:17 -0500

    Add proper support for dense steam to steam valve and regulator
    - Adds more effective support for dense steams to the valve and regulator.
    - Standardizes the fluid check between both cover types to a single location.

--- src/main/java/gregtech/common/covers/CoverSteamValve.java
+++ src/main/java/gregtech/common/covers/CoverSteamValve.java
@@ -3,6 +3,7 @@ package gregtech.common.covers;
 import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.fluids.FluidStack;
 
+import gregtech.api.enums.Materials;
 import gregtech.api.interfaces.ITexture;
 import gregtech.api.interfaces.tileentity.ICoverable;
 import gregtech.api.util.GTModHandler;
@@ -21,11 +22,17 @@ public class CoverSteamValve extends CoverPump {
 
     @Override
     protected boolean canTransferFluid(FluidStack fluid) {
-        if (fluid == null) return false;
+        return isFluidCompatible(fluid);
+    }
+
+    public static boolean isFluidCompatible(FluidStack fluid) {
+        if (fluid == null || fluid.getFluid() == null) return false;
         String fluidname = fluid.getFluid()
             .getName();
         return GTModHandler.isAnySteam(fluid) || GTModHandler.isSuperHeatedSteam(fluid)
             || fluidname.equals("supercriticalsteam")
-            || fluidname.equals("densesupercriticalsteam");
+            || fluid.getFluid() == Materials.DenseSteam.mGas
+            || fluid.getFluid() == Materials.DenseSuperheatedSteam.mGas
+            || fluid.getFluid() == Materials.DenseSupercriticalSteam.mGas;
     }
 }
