f51c4c92e119a24995aa531532d299ea9e55d9ca
Maya <10861407+serenibyss@users.noreply.github.com>
2025-01-21 18:06:25 -0600

    dense, sh, dense sh, sc, dense sc steam support

--- src/main/java/gregtech/common/misc/teams/PipelessSteamManager.java
+++ src/main/java/gregtech/common/misc/teams/PipelessSteamManager.java
@@ -6,26 +6,84 @@ import net.minecraft.nbt.NBTTagCompound;
 
 public class PipelessSteamManager implements ITeamData {
 
+    private static final BigInteger ONE_HUNDRED = BigInteger.valueOf(100);
+
     private BigInteger storedSteam = BigInteger.ZERO;
+    private BigInteger storedSuperheatedSteam = BigInteger.ZERO;
+    private BigInteger storedSupercriticalSteam = BigInteger.ZERO;
 
     @Override
     public void writeToNBT(NBTTagCompound NBT) {
         NBT.setByteArray("PipelessSteam", storedSteam.toByteArray());
+        NBT.setByteArray("PipelessSuperheatedSteam", storedSuperheatedSteam.toByteArray());
+        NBT.setByteArray("PipelessSupercriticalSteam", storedSupercriticalSteam.toByteArray());
     }
 
     @Override
     public void readFromNBT(NBTTagCompound NBT) {
         storedSteam = new BigInteger(NBT.getByteArray("PipelessSteam"));
+        storedSuperheatedSteam = new BigInteger(NBT.getByteArray("PipelessSuperheatedSteam"));
+        storedSupercriticalSteam = new BigInteger(NBT.getByteArray("PipelessSupercriticalSteam"));
     }
 
-    public void fillPipelessNet(long steam) {
-        storedSteam = storedSteam.add(BigInteger.valueOf(steam));
+    public void fillSteam(long steam) {
+        fillSteam(BigInteger.valueOf(steam));
+    }
+
+    public void fillDenseSteam(long denseSteam) {
+        fillSteam(
+            BigInteger.valueOf(denseSteam)
+                .multiply(ONE_HUNDRED));
+    }
+
+    private void fillSteam(BigInteger steam) {
+        storedSteam = storedSteam.add(steam);
         TeamWorldSavedData.INSTANCE.markDirty();
     }
 
-    public long drainPipelessNet(long steam, boolean simulate) {
-        BigInteger request = BigInteger.valueOf(steam);
+    public void fillSuperheatedSteam(long superheatedSteam) {
+        fillSuperheatedSteam(BigInteger.valueOf(superheatedSteam));
+    }
 
+    public void fillDenseSuperheatedSteam(long denseSuperheatedSteam) {
+        fillSuperheatedSteam(
+            BigInteger.valueOf(denseSuperheatedSteam)
+                .multiply(ONE_HUNDRED));
+    }
+
+    private void fillSuperheatedSteam(BigInteger superheatedSteam) {
+        storedSuperheatedSteam = storedSuperheatedSteam.add(superheatedSteam);
+        TeamWorldSavedData.INSTANCE.markDirty();
+    }
+
+    public void fillSupercriticalSteam(long supercriticalSteam) {
+        fillSupercriticalSteam(BigInteger.valueOf(supercriticalSteam));
+    }
+
+    public void fillDenseSupercriticalSteam(long denseSupercriticalSteam) {
+        fillSupercriticalSteam(
+            BigInteger.valueOf(denseSupercriticalSteam)
+                .multiply(ONE_HUNDRED));
+    }
+
+    private void fillSupercriticalSteam(BigInteger supercriticalSteam) {
+        storedSupercriticalSteam = storedSupercriticalSteam.add(supercriticalSteam);
+        TeamWorldSavedData.INSTANCE.markDirty();
+    }
+
+    public long drainSteam(long steam, boolean simulate) {
+        return drainSteam(BigInteger.valueOf(steam), simulate).longValue();
+    }
+
+    public long drainDenseSteam(long denseSteam, boolean simulate) {
+        return drainSteam(
+            BigInteger.valueOf(denseSteam)
+                .multiply(ONE_HUNDRED),
+            simulate).divide(ONE_HUNDRED)
+                .longValue();
+    }
+
+    private BigInteger drainSteam(BigInteger request, boolean simulate) {
         int cmp = storedSteam.compareTo(request);
 
         if (cmp > 0) {
@@ -34,14 +92,14 @@ public class PipelessSteamManager implements ITeamData {
                 storedSteam = storedSteam.subtract(request);
                 TeamWorldSavedData.INSTANCE.markDirty();
             }
-            return steam;
+            return request;
         } else if (cmp == 0) {
             // we can exactly fulfill the request
             if (!simulate) {
                 storedSteam = BigInteger.ZERO;
                 TeamWorldSavedData.INSTANCE.markDirty();
             }
-            return steam;
+            return request;
         } else {
             // we can only partially fulfill the request
             BigInteger oldVal = storedSteam;
@@ -49,7 +107,87 @@ public class PipelessSteamManager implements ITeamData {
                 storedSteam = BigInteger.ZERO;
                 TeamWorldSavedData.INSTANCE.markDirty();
             }
-            return oldVal.longValue();
+            return oldVal;
+        }
+    }
+
+    public long drainSuperheatedSteam(long superheatedSteam, boolean simulate) {
+        return drainSuperheatedSteam(BigInteger.valueOf(superheatedSteam), simulate).longValue();
+    }
+
+    public long drainDenseSuperheatedSteam(long denseSuperheatedSteam, boolean simulate) {
+        return drainSuperheatedSteam(
+            BigInteger.valueOf(denseSuperheatedSteam)
+                .multiply(ONE_HUNDRED),
+            simulate).divide(ONE_HUNDRED)
+                .longValue();
+    }
+
+    private BigInteger drainSuperheatedSteam(BigInteger request, boolean simulate) {
+        int cmp = storedSuperheatedSteam.compareTo(request);
+
+        if (cmp > 0) {
+            // we can accommodate the full request
+            if (!simulate) {
+                storedSuperheatedSteam = storedSuperheatedSteam.subtract(request);
+                TeamWorldSavedData.INSTANCE.markDirty();
+            }
+            return request;
+        } else if (cmp == 0) {
+            // we can exactly fulfill the request
+            if (!simulate) {
+                storedSuperheatedSteam = BigInteger.ZERO;
+                TeamWorldSavedData.INSTANCE.markDirty();
+            }
+            return request;
+        } else {
+            // we can only partially fulfill the request
+            BigInteger oldVal = storedSuperheatedSteam;
+            if (!simulate) {
+                storedSuperheatedSteam = BigInteger.ZERO;
+                TeamWorldSavedData.INSTANCE.markDirty();
+            }
+            return oldVal;
+        }
+    }
+
+    public long drainSupercriticalSteam(long supercriticalSteam, boolean simulate) {
+        return drainSupercriticalSteam(BigInteger.valueOf(supercriticalSteam), simulate).longValue();
+    }
+
+    public long drainDenseSupercriticalSteam(long denseSupercriticalSteam, boolean simulate) {
+        return drainSupercriticalSteam(
+            BigInteger.valueOf(denseSupercriticalSteam)
+                .multiply(ONE_HUNDRED),
+            simulate).divide(ONE_HUNDRED)
+                .longValue();
+    }
+
+    private BigInteger drainSupercriticalSteam(BigInteger request, boolean simulate) {
+        int cmp = storedSupercriticalSteam.compareTo(request);
+
+        if (cmp > 0) {
+            // we can accommodate the full request
+            if (!simulate) {
+                storedSupercriticalSteam = storedSupercriticalSteam.subtract(request);
+                TeamWorldSavedData.INSTANCE.markDirty();
+            }
+            return request;
+        } else if (cmp == 0) {
+            // we can exactly fulfill the request
+            if (!simulate) {
+                storedSupercriticalSteam = BigInteger.ZERO;
+                TeamWorldSavedData.INSTANCE.markDirty();
+            }
+            return request;
+        } else {
+            // we can only partially fulfill the request
+            BigInteger oldVal = storedSupercriticalSteam;
+            if (!simulate) {
+                storedSupercriticalSteam = BigInteger.ZERO;
+                TeamWorldSavedData.INSTANCE.markDirty();
+            }
+            return oldVal;
         }
     }
 }
