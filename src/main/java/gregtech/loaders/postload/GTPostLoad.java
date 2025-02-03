77f1f9ffeb83ce983d0eca05e6a4267d7f6086bd
EnderProyects <enderproyects@gmail.com>
2025-02-03 03:54:39 +0100

    aghh
    It seems comented code count to forbidden

--- src/main/java/gregtech/loaders/postload/GTPostLoad.java
+++ src/main/java/gregtech/loaders/postload/GTPostLoad.java
@@ -367,8 +367,6 @@ public class GTPostLoad {
                 GTModHandler.getModItem(Mods.EtFuturumRequiem.ID, "blue_ice", 0, 0),
                 GTUtility.getIntegratedCircuit(1))
             .itemOutputs(GTOreDictUnificator.get(OrePrefixes.cobblestone, Materials.Basalt, 1L))
-            // GTModHandler.getModItem(Mods.GregTech.ID, "gt.blockstones", 1, 9))
-
             .duration(16 * TICKS)
             .eut(TierEU.RECIPE_LV)
             .ignoreCollision()
