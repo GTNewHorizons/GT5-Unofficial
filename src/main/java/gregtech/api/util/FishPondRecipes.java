d26cf7a0d1e869dca6a6743caa34aa143d1fc80c
PureBluez <notpurebluez@gmail.com>
2025-03-02 12:42:27 -0600

    tweak values

--- src/main/java/gregtech/api/util/FishPondRecipes.java
+++ src/main/java/gregtech/api/util/FishPondRecipes.java
@@ -12,7 +12,6 @@ import net.minecraftforge.common.FishingHooks;
 import gregtech.api.enums.GTValues;
 import gtPlusPlus.api.objects.Logger;
 import gtPlusPlus.api.recipe.GTPPRecipeMaps;
-import gtPlusPlus.core.util.minecraft.ItemUtils;
 import gtPlusPlus.xmod.gregtech.common.tileentities.machines.multi.production.MTEIndustrialFishingPond;
 
 public class FishPondRecipes {
@@ -30,9 +29,9 @@ public class FishPondRecipes {
                 .getField(FishingHooks.class, "treasure")
                 .get(null);
             final Field stackField = GTUtility.getField(WeightedRandomFishable.class, "field_150711_b");
-            generateRecipeForFishable(MTEIndustrialFishingPond.FISH_MODE, fishList, stackField);
-            generateRecipeForFishable(MTEIndustrialFishingPond.JUNK_MODE, junkList, stackField);
-            generateRecipeForFishable(MTEIndustrialFishingPond.TREASURE_MODE, treasureList, stackField);
+            generateRecipeForFishable(MTEIndustrialFishingPond.FISH_MODE, fishList, stackField, 0.8);
+            generateRecipeForFishable(MTEIndustrialFishingPond.JUNK_MODE, junkList, stackField, 1.35);
+            generateRecipeForFishable(MTEIndustrialFishingPond.TREASURE_MODE, treasureList, stackField, 20D);
         } catch (Exception e) {
             Logger.INFO("Error reading the vanilla fishing loot table.");
             e.printStackTrace();
@@ -40,22 +39,18 @@ public class FishPondRecipes {
     }
 
     private static void generateRecipeForFishable(int circuitType, ArrayList<WeightedRandomFishable> lootTable,
-        Field stackField) {
-        double totalWeight = 0;
+        Field stackField, double chanceMultiplier) {
         int[] chances = new int[lootTable.size()];
         ItemStack[] outputs = new ItemStack[lootTable.size()];
 
         for (int i = 0; i < lootTable.size(); i++) {
-            totalWeight += lootTable.get(i).itemWeight;
+            chances[i] = (int) (lootTable.get(i).itemWeight * 100 * chanceMultiplier);
             try {
-                outputs[i] = ItemUtils.getSimpleStack((ItemStack) stackField.get(lootTable.get(i)), 1);
+                outputs[i] = (ItemStack) stackField.get(lootTable.get(i));
             } catch (IllegalArgumentException | IllegalAccessException e1) {
                 Logger.INFO("Error generating Fish Pond Recipes");
                 e1.printStackTrace();
             }
-        }
-        for (int i = 0; i < lootTable.size(); i++) {
-            chances[i] = (int) ((((double) lootTable.get(i).itemWeight) / totalWeight) * 10000);
         }
 
         GTValues.RA.stdBuilder()
