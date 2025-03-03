f0e429d4d535ded91328f6cfe92c739014a9caac
FrostyFire1 <kamilbaron02@gmail.com>
2025-03-03 13:50:38 +0100

    block highlight, filter for advanced page

--- src/main/java/gregtech/api/net/PacketTeleportPlayer.java
+++ src/main/java/gregtech/api/net/PacketTeleportPlayer.java
@@ -1,6 +1,9 @@
 package gregtech.api.net;
 
+import appeng.api.util.DimensionalCoord;
+import appeng.client.render.BlockPosHighlighter;
 import com.google.common.io.ByteArrayDataInput;
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
@@ -9,7 +12,12 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.INetHandler;
 import net.minecraft.network.NetHandlerPlayServer;
+import net.minecraft.server.management.ServerConfigurationManager;
+import net.minecraft.util.ChatComponentText;
 import net.minecraft.world.IBlockAccess;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PacketTeleportPlayer extends GTPacket{
     private int dim;
@@ -47,11 +55,31 @@ public class PacketTeleportPlayer extends GTPacket{
     }
     @Override
     public void process(IBlockAccess world) {
-        System.out.printf("Processing teleport request to %d, %d, %d, dimension %d\n",this.coords[0],this.coords[1],this.coords[2], this.dim);
-        System.out.println(player.getDisplayName());
-        player.mcServer.getConfigurationManager().transferPlayerToDimension(player, this.dim);
-        player.dimension = this.dim;
-        player.setPositionAndUpdate(this.coords[0], this.coords[1], this.coords[2]);
+        int x = this.coords[0];
+        int y = this.coords[1];
+        int z = this.coords[2];
+        System.out.printf("Processing teleport request to %d, %d, %d, dimension %d\n",x,y,z, this.dim);
+        ServerConfigurationManager manager = player.mcServer.getConfigurationManager();
+        if(manager.func_152596_g(player.getGameProfile())){ //Check if player can /tp
+            if(player.dimension != this.dim){
+                manager.transferPlayerToDimension(player, this.dim);
+            }
+            player.playerNetServerHandler.setPlayerLocation(x, y, z, player.cameraYaw, player.cameraPitch);
+        } else{
+            ArrayList<DimensionalCoord> list = new ArrayList<>();
+            list.add(new DimensionalCoord(x,y,z,this.dim));
+            double deltaX = x - player.posX;
+            double deltaY = y - player.posY;
+            double deltaZ = z  - player.posZ;
+
+            double distanceXZ = Math.sqrt(deltaX * deltaX + deltaZ * deltaZ);
+
+            float yaw = (float) Math.toDegrees(Math.atan2(-deltaX, deltaZ));
+            float pitch = (float) Math.toDegrees(Math.atan2(-deltaY, distanceXZ));
+            player.playerNetServerHandler.setPlayerLocation(player.posX, player.posY, player.posZ, yaw, pitch);
+            BlockPosHighlighter.highlightBlocks(player, list, "yay", "nay");
+        }
+
 
     }
 
