a5ff05128accde0b938d86388680365420e96635
miozune <miozune@gmail.com>
2025-03-12 06:12:58 +0900

    Refactor pipe registration & move item pipes from coremod (#4022)
    Co-authored-by: Martin Robertz <dream-master@gmx.net>
    Co-authored-by: Maya <10861407+serenibyss@users.noreply.github.com>

--- src/main/java/gregtech/GTMod.java
+++ src/main/java/gregtech/GTMod.java
@@ -120,6 +120,7 @@ import gregtech.loaders.preload.LoaderCircuitBehaviors;
 import gregtech.loaders.preload.LoaderGTBlockFluid;
 import gregtech.loaders.preload.LoaderGTItemData;
 import gregtech.loaders.preload.LoaderGTOreDictionary;
+import gregtech.loaders.preload.LoaderMetaPipeEntities;
 import gregtech.loaders.preload.LoaderMetaTileEntities;
 import gregtech.loaders.preload.LoaderOreProcessing;
 import ic2.api.recipe.IRecipeInput;
@@ -286,6 +287,7 @@ public class GTMod implements IGTMod {
         new LoaderGTItemData().run();
         new LoaderGTBlockFluid().run();
         new LoaderMetaTileEntities().run();
+        new LoaderMetaPipeEntities().run();
 
         new LoaderCircuitBehaviors().run();
         new CoverBehaviorLoader().run();
