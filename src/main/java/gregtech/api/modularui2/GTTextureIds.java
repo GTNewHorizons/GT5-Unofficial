52e10d6d90d70a3c4ea4746c478ae87359f6c6a2
miozune <miozune@gmail.com>
2025-03-14 20:47:18 +0900

    Rename background texture ids

--- src/main/java/gregtech/api/modularui2/GTTextureIds.java
+++ src/main/java/gregtech/api/modularui2/GTTextureIds.java
@@ -2,30 +2,30 @@ package gregtech.api.modularui2;
 
 public class GTTextureIds {
 
-    public static final String BACKGROUND_STANDARD = "gregtech:standard_bg";
-    public static final String BACKGROUND_BRONZE = "gregtech:bronze_bg";
-    public static final String BACKGROUND_STEEL = "gregtech:steel_bg";
-    public static final String BACKGROUND_PRIMITIVE = "gregtech:primitive_bg";
+    public static final String BACKGROUND_STANDARD = "gregtech:bg_standard";
+    public static final String BACKGROUND_BRONZE = "gregtech:bg_bronze";
+    public static final String BACKGROUND_STEEL = "gregtech:bg_steel";
+    public static final String BACKGROUND_PRIMITIVE = "gregtech:bg_primitive";
 
-    public static final String BACKGROUND_COVER_TAB_NORMAL_STANDARD = "gregtech:cover_tab_normal_standard_bg";
-    public static final String BACKGROUND_COVER_TAB_HIGHLIGHT_STANDARD = "gregtech:cover_tab_highlight_standard_bg";
-    public static final String BACKGROUND_COVER_TAB_DISABLED_STANDARD = "gregtech:cover_tab_disabled_standard_bg";
-    public static final String BACKGROUND_COVER_TAB_NORMAL_BRONZE = "gregtech:cover_tab_normal_bronze_bg";
-    public static final String BACKGROUND_COVER_TAB_HIGHLIGHT_BRONZE = "gregtech:cover_tab_highlight_bronze_bg";
-    public static final String BACKGROUND_COVER_TAB_DISABLED_BRONZE = "gregtech:cover_tab_disabled_bronze_bg";
-    public static final String BACKGROUND_COVER_TAB_NORMAL_STEEL = "gregtech:cover_tab_normal_steel_bg";
-    public static final String BACKGROUND_COVER_TAB_HIGHLIGHT_STEEL = "gregtech:cover_tab_highlight_steel_bg";
-    public static final String BACKGROUND_COVER_TAB_DISABLED_STEEL = "gregtech:cover_tab_disabled_steel_bg";
-    public static final String BACKGROUND_COVER_TAB_NORMAL_PRIMITIVE = "gregtech:cover_tab_normal_primitive_bg";
-    public static final String BACKGROUND_COVER_TAB_HIGHLIGHT_PRIMITIVE = "gregtech:cover_tab_highlight_primitive_bg";
-    public static final String BACKGROUND_COVER_TAB_DISABLED_PRIMITIVE = "gregtech:cover_tab_disabled_primitive_bg";
+    public static final String BACKGROUND_COVER_TAB_NORMAL_STANDARD = "gregtech:bg_cover_tab_normal_standard";
+    public static final String BACKGROUND_COVER_TAB_HIGHLIGHT_STANDARD = "gregtech:bg_cover_tab_highlight_standard";
+    public static final String BACKGROUND_COVER_TAB_DISABLED_STANDARD = "gregtech:bg_cover_tab_disabled_standard";
+    public static final String BACKGROUND_COVER_TAB_NORMAL_BRONZE = "gregtech:bg_cover_tab_normal_bronze";
+    public static final String BACKGROUND_COVER_TAB_HIGHLIGHT_BRONZE = "gregtech:bg_cover_tab_highlight_bronze";
+    public static final String BACKGROUND_COVER_TAB_DISABLED_BRONZE = "gregtech:bg_cover_tab_disabled_bronze";
+    public static final String BACKGROUND_COVER_TAB_NORMAL_STEEL = "gregtech:bg_cover_tab_normal_steel";
+    public static final String BACKGROUND_COVER_TAB_HIGHLIGHT_STEEL = "gregtech:bg_cover_tab_highlight_steel";
+    public static final String BACKGROUND_COVER_TAB_DISABLED_STEEL = "gregtech:bg_cover_tab_disabled_steel";
+    public static final String BACKGROUND_COVER_TAB_NORMAL_PRIMITIVE = "gregtech:bg_cover_tab_normal_primitive";
+    public static final String BACKGROUND_COVER_TAB_HIGHLIGHT_PRIMITIVE = "gregtech:bg_cover_tab_highlight_primitive";
+    public static final String BACKGROUND_COVER_TAB_DISABLED_PRIMITIVE = "gregtech:bg_cover_tab_disabled_primitive";
 
-    public static final String BACKGROUND_POPUP = "gregtech:popup_bg";
+    public static final String BACKGROUND_POPUP = "gregtech:bg_popup";
 
-    public static final String BACKGROUND_TITLE_STANDARD = "gregtech:title_standard_bg";
-    public static final String BACKGROUND_TITLE_BRONZE = "gregtech:title_bronze_bg";
-    public static final String BACKGROUND_TITLE_STEEL = "gregtech:title_steel_bg";
-    public static final String BACKGROUND_TITLE_PRIMITIVE = "gregtech:title_primitive_bg";
+    public static final String BACKGROUND_TITLE_STANDARD = "gregtech:bg_title_standard";
+    public static final String BACKGROUND_TITLE_BRONZE = "gregtech:bg_title_bronze";
+    public static final String BACKGROUND_TITLE_STEEL = "gregtech:bg_title_steel";
+    public static final String BACKGROUND_TITLE_PRIMITIVE = "gregtech:bg_title_primitive";
 
     public static final String SLOT_ITEM_STANDARD = "gregtech:slot_item_standard";
     public static final String SLOT_ITEM_BRONZE = "gregtech:slot_item_bronze";
