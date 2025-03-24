21c914edc3ac74d5286eec68c8513d0e1ea0891d
Yannick Marcotte-Gourde <yannickmg@gmail.com>
2025-03-24 14:32:21 -0400

    Require sneaking to place cover on pipes, wires and frame boxes (#4104)

--- src/main/java/gregtech/api/metatileentity/BaseMetaPipeEntity.java
+++ src/main/java/gregtech/api/metatileentity/BaseMetaPipeEntity.java
@@ -827,7 +827,7 @@ public class BaseMetaPipeEntity extends CommonBaseMetaTileEntity
         final ForgeDirection effectiveSide = (!hasCoverAtSide(side)) ? wrenchingSide : side;
         Cover effectiveSideCover = getCoverAtSide(effectiveSide);
         if (isClientSide()) {
-            // Configure Cover, sneak can also be: screwdriver, wrench, side cutter, soldering iron
+            // Place/configure Cover, sneak can also be: screwdriver, wrench, side cutter, soldering iron
             if (aPlayer.isSneaking()) {
                 return (effectiveSideCover.hasCoverGUI());
             }
@@ -974,7 +974,7 @@ public class BaseMetaPipeEntity extends CommonBaseMetaTileEntity
                 }
 
                 if (!hasCoverAtSide(effectiveSide)) {
-                    if (CoverRegistry.isCover(tCurrentItem)) {
+                    if (aPlayer.isSneaking() && CoverRegistry.isCover(tCurrentItem)) {
                         if (CoverRegistry.getCoverPlacer(tCurrentItem)
                             .isCoverPlaceable(effectiveSide, tCurrentItem, this)
                             && mMetaTileEntity.allowCoverOnSide(effectiveSide, tCurrentItem)) {
