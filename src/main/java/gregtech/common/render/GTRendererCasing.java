GTNewHorizons/GT5-Unofficial

commit 7a5b03a804122945bb71c78cf747d1d72f6e23f9

zfighting free block overlay (#3973)
Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
Co-authored-by: Martin Robertz <dream-master@gmx.net>
Co-authored-by: Maya <10861407+serenibyss@users.noreply.github.com>

src/main/java/gregtech/common/render/GTRendererCasing.java
@@ -1,0 +1,121 @@
+package gregtech.common.render;
+
+import static gregtech.api.enums.GTValues.*;
+import static gregtech.common.render.GTRendererBlock.*;
+import static net.minecraftforge.common.util.ForgeDirection.VALID_DIRECTIONS;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.common.util.ForgeDirection;
+
+import org.lwjgl.opengl.GL11;
+
+import com.gtnewhorizons.angelica.api.ThreadSafeISBRH;
+
+import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
+import cpw.mods.fml.client.registry.RenderingRegistry;
+import gregtech.api.interfaces.ITexture;
+import gregtech.api.render.RenderOverlay;
+import gregtech.api.render.TextureFactory;
+
+/**
+ * This renderer is almost the same as vanilla simple block renderer, with the exception that it can render overlays
+ * registered to {@link RenderOverlay}
+ */
+@ThreadSafeISBRH(perThread = true)
+public class GTRendererCasing implements ISimpleBlockRenderingHandler {
+
+    private static final GTRendererCasing INSTANCE = new GTRendererCasing();
+    public static int mRenderID;
+
+    public static void register() {
+        mRenderID = RenderingRegistry.getNextAvailableRenderId();
+        RenderingRegistry.registerBlockHandler(INSTANCE);
+    }
+
+    private final ITexture[][] textureArray = new ITexture[6][2];
+
+    @Override
+    public void renderInventoryBlock(Block aBlock, int aMeta, int aModelID, RenderBlocks aRenderer) {
+        aRenderer.enableAO = false;
+        aRenderer.useInventoryTint = true;
+
+        setupBlockTexturesOnly(aBlock, aMeta);
+
+        GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F);
+        GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
+
+        aBlock.setBlockBoundsForItemRender();
+        aRenderer.setRenderBoundsFromBlock(aBlock);
+        // spotless:off
+        renderNegativeYFacing(null, aRenderer, aBlock, 0, 0, 0, textureArray[SIDE_DOWN], true);
+        renderPositiveYFacing(null, aRenderer, aBlock, 0, 0, 0, textureArray[SIDE_UP], true);
+        renderNegativeZFacing(null, aRenderer, aBlock, 0, 0, 0, textureArray[SIDE_NORTH], true);
+        renderPositiveZFacing(null, aRenderer, aBlock, 0, 0, 0, textureArray[SIDE_SOUTH], true);
+        renderNegativeXFacing(null, aRenderer, aBlock, 0, 0, 0, textureArray[SIDE_WEST], true);
+        renderPositiveXFacing(null, aRenderer, aBlock, 0, 0, 0, textureArray[SIDE_EAST], true);
+        // spotless:on
+        aBlock.setBlockBounds(blockMin, blockMin, blockMin, blockMax, blockMax, blockMax);
+
+        aRenderer.setRenderBoundsFromBlock(aBlock);
+
+        GL11.glTranslatef(0.5F, 0.5F, 0.5F);
+        aRenderer.useInventoryTint = false;
+    }
+
+    private void setupBlockTexturesOnly(Block aBlock, int aMeta) {
+        ForgeDirection[] validDirections = VALID_DIRECTIONS;
+        for (int i = 0, validDirectionsLength = validDirections.length; i < validDirectionsLength; i++) {
+            ForgeDirection tFace = validDirections[i];
+            textureArray[i][0] = TextureFactory.of(aBlock, aMeta, tFace);
+            textureArray[i][1] = null;
+        }
+    }
+
+    @Override
+    public boolean renderWorldBlock(IBlockAccess aWorld, int aX, int aY, int aZ, Block aBlock, int aModelID,
+        RenderBlocks aRenderer) {
+        aRenderer.enableAO = Minecraft.isAmbientOcclusionEnabled();
+        aRenderer.useInventoryTint = false;
+
+        int tMeta = aWorld.getBlockMetadata(aX, aY, aZ);
+
+        ITexture[] overlayed = RenderOverlay.get(aWorld, aX, aY, aZ);
+        if (overlayed == null) {
+            setupBlockTexturesOnly(aBlock, tMeta);
+        } else {
+            ForgeDirection[] validDirections = VALID_DIRECTIONS;
+            for (int i = 0, validDirectionsLength = validDirections.length; i < validDirectionsLength; i++) {
+                ForgeDirection tFace = validDirections[i];
+                textureArray[i][0] = TextureFactory.of(aBlock, tMeta, tFace);
+                textureArray[i][1] = overlayed[i];
+            }
+        }
+
+        aBlock.setBlockBounds(blockMin, blockMin, blockMin, blockMax, blockMax, blockMax);
+        aRenderer.setRenderBoundsFromBlock(aBlock);
+
+        // spotless:off
+        renderNegativeYFacing(aWorld, aRenderer, aBlock, aX, aY, aZ, textureArray[SIDE_DOWN], true);
+        renderPositiveYFacing(aWorld, aRenderer, aBlock, aX, aY, aZ, textureArray[SIDE_UP], true);
+        renderNegativeZFacing(aWorld, aRenderer, aBlock, aX, aY, aZ, textureArray[SIDE_NORTH], true);
+        renderPositiveZFacing(aWorld, aRenderer, aBlock, aX, aY, aZ, textureArray[SIDE_SOUTH], true);
+        renderNegativeXFacing(aWorld, aRenderer, aBlock, aX, aY, aZ, textureArray[SIDE_WEST], true);
+        renderPositiveXFacing(aWorld, aRenderer, aBlock, aX, aY, aZ, textureArray[SIDE_EAST], true);
+        // spotless:on
+
+        return true;
+    }
+
+    @Override
+    public boolean shouldRender3DInInventory(int aModel) {
+        return true;
+    }
+
+    @Override
+    public int getRenderId() {
+        return mRenderID;
+    }
+}

src/main/java/gregtech/common/tileentities/machines/multi/MTEAirFilterBase.java
@@ -15,6 +15,7 @@ import static java.lang.Math.min;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 import net.minecraft.block.Block;
@@ -50,6 +51,7 @@ import gregtech.api.metatileentity.implementations.MTEHatchMuffler;
 import gregtech.api.objects.XSTR;
 import gregtech.api.recipe.check.CheckRecipeResult;
 import gregtech.api.recipe.check.CheckRecipeResultRegistry;
+import gregtech.api.render.RenderOverlay;
 import gregtech.api.render.TextureFactory;
 import gregtech.api.util.GTModHandler;
 import gregtech.api.util.GTUtility;
@@ -78,6 +80,7 @@ public abstract class MTEAirFilterBase extends MTEEnhancedMultiBlockBase<MTEAirF
     protected boolean isFilterLoaded = false;
     protected int filterUsageRemaining = 0;
     protected int tickCounter = 0; // because we can't trust the world tick, it may be in a dim with eternal day, etc
+    protected final List<RenderOverlay.OverlayTicket> overlayTickets = new ArrayList<>();
     private boolean mFormed;
     protected static final String STRUCTURE_PIECE_MAIN = "main";
     protected static final ClassValue<IStructureDefinition<MTEAirFilterBase>> STRUCTURE_DEFINITION = new ClassValue<>() {
@@ -490,6 +493,13 @@ public abstract class MTEAirFilterBase extends MTEEnhancedMultiBlockBase<MTEAirF
     @Override
     public void onValueUpdate(byte aValue) {
         mFormed = aValue == 1;
+        setTurbineOverlay();
+    }
+
+    @Override
+    public void onRemoval() {
+        super.onRemoval();
+        if (getBaseMetaTileEntity().isClientSide()) GTUtilityClient.clearTurbineOverlay(overlayTickets);
     }
 
     @Override
@@ -498,8 +508,30 @@ public abstract class MTEAirFilterBase extends MTEEnhancedMultiBlockBase<MTEAirF
     }
 
     @Override
+    public void onFirstTick(IGregTechTileEntity aBaseMetaTileEntity) {
+        super.onFirstTick(aBaseMetaTileEntity);
+        setTurbineOverlay();
+    }
+
+    protected void setTurbineOverlay() {
+        IGregTechTileEntity tile = getBaseMetaTileEntity();
+        if (tile.isServerSide()) return;
+
+        IIconContainer[] tTextures = getBaseMetaTileEntity().isActive() ? TURBINE_NEW_ACTIVE : TURBINE_NEW;
+
+        GTUtilityClient.setTurbineOverlay(
+            tile.getWorld(),
+            tile.getXCoord(),
+            tile.getYCoord(),
+            tile.getZCoord(),
+            getExtendedFacing(),
+            tTextures,
+            overlayTickets);
+    }
+
+    @Override
     public boolean renderInWorld(IBlockAccess aWorld, int aX, int aY, int aZ, Block aBlock, RenderBlocks aRenderer) {
-        if (!mFormed) return false;
+        if (!mFormed || !overlayTickets.isEmpty()) return false;
         int[] xyz = new int[3];
         ExtendedFacing ext = getExtendedFacing();
         ext.getWorldOffset(new int[] { 0, -3, 1 }, xyz);

src/main/java/goodgenerator/blocks/regularBlock/BlockTurbineCasing.java
@@ -1,17 +1,11 @@
 package goodgenerator.blocks.regularBlock;
 
-import net.minecraft.block.Block;
-import net.minecraft.world.IBlockAccess;
-import net.minecraftforge.common.util.ForgeDirection;
-
-import goodgenerator.client.render.BlockRenderHandler;
 import goodgenerator.main.GoodGenerator;
 import gregtech.api.enums.Textures;
 import gregtech.api.interfaces.IIconContainer;
-import gregtech.api.interfaces.ITexture;
-import gregtech.api.render.TextureFactory;
+import gregtech.common.render.GTRendererCasing;
 
-public class BlockTurbineCasing extends BlockCasing implements ITextureBlock {
+public class BlockTurbineCasing extends BlockCasing {
 
     public IIconContainer base;
 
@@ -21,21 +15,7 @@ public class BlockTurbineCasing extends BlockCasing implements ITextureBlock {
     }
 
     @Override
-    public ITexture[] getTexture(Block aBlock, ForgeDirection side, IBlockAccess aWorld, int xCoord, int yCoord,
-        int zCoord) {
-        return getTexture(aBlock, side);
-    }
-
-    @Override
-    public ITexture[] getTexture(Block aBlock, int aMeta, ForgeDirection side) {
-        return new ITexture[] { TextureFactory.of(base) };
-    }
-
-    @Override
     public int getRenderType() {
-        if (BlockRenderHandler.INSTANCE == null) {
-            return super.getRenderType();
-        }
-        return BlockRenderHandler.INSTANCE.mRenderID;
+        return GTRendererCasing.mRenderID;
     }
 }

src/main/java/goodgenerator/blocks/tileEntity/base/MTELargeTurbineBase.java
@@ -11,15 +11,14 @@ import static gregtech.api.util.GTStructureUtility.*;
 import static gregtech.api.util.GTUtility.validMTEList;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import net.minecraft.block.Block;
-import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.StatCollector;
-import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.fluids.FluidStack;
 
@@ -39,6 +38,7 @@ import gregtech.api.metatileentity.implementations.MTEEnhancedMultiBlockBase;
 import gregtech.api.metatileentity.implementations.MTEHatchDynamo;
 import gregtech.api.recipe.check.CheckRecipeResult;
 import gregtech.api.recipe.check.CheckRecipeResultRegistry;
+import gregtech.api.render.RenderOverlay;
 import gregtech.api.util.GTUtility;
 import gregtech.api.util.GTUtilityClient;
 import gregtech.api.util.TurbineStatCalculator;
@@ -85,6 +85,7 @@ public abstract class MTELargeTurbineBase extends MTEEnhancedMultiBlockBase<MTEL
     protected long maxPower = 0;
 
     // client side stuff
+    protected final List<RenderOverlay.OverlayTicket> overlayTickets = new ArrayList<>();
     protected boolean mHasTurbine;
     // mMachine got overwritten by StructureLib extended facing query response
     // so we use a separate field for this
@@ -137,6 +138,31 @@ public abstract class MTELargeTurbineBase extends MTEEnhancedMultiBlockBase<MTEL
 
     public abstract int getCasingTextureIndex();
 
+    @Override
+    public void onFirstTick(IGregTechTileEntity aBaseMetaTileEntity) {
+        super.onFirstTick(aBaseMetaTileEntity);
+        setTurbineOverlay();
+    }
+
+    protected void setTurbineOverlay() {
+        IGregTechTileEntity tile = getBaseMetaTileEntity();
+        if (tile.isServerSide()) return;
+
+        IIconContainer[] tTextures;
+        if (tile.isActive()) tTextures = getTurbineTextureActive();
+        else if (hasTurbine()) tTextures = getTurbineTextureFull();
+        else tTextures = getTurbineTextureEmpty();
+
+        GTUtilityClient.setTurbineOverlay(
+            tile.getWorld(),
+            tile.getXCoord(),
+            tile.getYCoord(),
+            tile.getZCoord(),
+            getExtendedFacing(),
+            tTextures,
+            overlayTickets);
+    }
+
     public boolean isNewStyleRendering() {
         return false;
     }
@@ -154,23 +180,17 @@ public abstract class MTELargeTurbineBase extends MTEEnhancedMultiBlockBase<MTEL
     }
 
     @Override
-    public boolean renderInWorld(IBlockAccess aWorld, int aX, int aY, int aZ, Block aBlock, RenderBlocks aRenderer) {
-        if (!isNewStyleRendering() || !mFormed) return false;
-
-        IIconContainer[] tTextures;
-        if (getBaseMetaTileEntity().isActive()) tTextures = getTurbineTextureActive();
-        else if (hasTurbine()) tTextures = getTurbineTextureFull();
-        else tTextures = getTurbineTextureEmpty();
-        GTUtilityClient
-            .renderTurbineOverlay(aWorld, aX, aY, aZ, aRenderer, getExtendedFacing(), getCasingBlock(), tTextures);
-        return false;
-    }
-
-    @Override
     public void onValueUpdate(byte aValue) {
         mHasTurbine = (aValue & 0x1) != 0;
         mFormed = (aValue & 0x2) != 0;
+        setTurbineOverlay();
         super.onValueUpdate(aValue);
+    }
+
+    @Override
+    public void onRemoval() {
+        super.onRemoval();
+        if (getBaseMetaTileEntity().isClientSide()) GTUtilityClient.clearTurbineOverlay(overlayTickets);
     }
 
     @Override
@@ -370,7 +390,8 @@ public abstract class MTELargeTurbineBase extends MTEEnhancedMultiBlockBase<MTEL
     }
 
     public boolean hasTurbine() {
-        return this.getMaxEfficiency(mInventory[1]) > 0;
+        return getBaseMetaTileEntity() != null && getBaseMetaTileEntity().isClientSide() ? mHasTurbine
+            : this.getMaxEfficiency(mInventory[1]) > 0;
     }
 
     @Override

src/main/java/goodgenerator/client/render/BlockRenderHandler.java
@@ -1,133 +1,0 @@
-package goodgenerator.client.render;
-
-import static gregtech.common.render.GTRendererBlock.*;
-import static net.minecraftforge.common.util.ForgeDirection.*;
-import static net.minecraftforge.common.util.ForgeDirection.EAST;
-
-import net.minecraft.block.Block;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.RenderBlocks;
-import net.minecraft.world.IBlockAccess;
-
-import org.lwjgl.opengl.GL11;
-
-import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
-import cpw.mods.fml.client.registry.RenderingRegistry;
-import goodgenerator.blocks.regularBlock.ITextureBlock;
-import gregtech.GTMod;
-
-public class BlockRenderHandler implements ISimpleBlockRenderingHandler {
-
-    public static final float blockMin = 0.0F;
-    public static final float blockMax = 1.0F;
-    public static BlockRenderHandler INSTANCE;
-    public final int mRenderID;
-
-    public BlockRenderHandler() {
-        this.mRenderID = RenderingRegistry.getNextAvailableRenderId();
-        INSTANCE = this;
-        RenderingRegistry.registerBlockHandler(this);
-    }
-
-    @Override
-    public void renderInventoryBlock(Block aBlock, int metadata, int modelId, RenderBlocks aRenderer) {
-        aRenderer.enableAO = false;
-        aRenderer.useInventoryTint = true;
-
-        GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F);
-        GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
-
-        if (aBlock instanceof ITextureBlock tc) {
-            aBlock.setBlockBoundsForItemRender();
-            aRenderer.setRenderBoundsFromBlock(aBlock);
-            renderNegativeYFacing(null, aRenderer, aBlock, 0, 0, 0, tc.getTexture(aBlock, metadata, DOWN), true);
-            renderPositiveYFacing(null, aRenderer, aBlock, 0, 0, 0, tc.getTexture(aBlock, metadata, UP), true);
-            renderNegativeZFacing(null, aRenderer, aBlock, 0, 0, 0, tc.getTexture(aBlock, metadata, NORTH), true);
-            renderPositiveZFacing(null, aRenderer, aBlock, 0, 0, 0, tc.getTexture(aBlock, metadata, SOUTH), true);
-            renderNegativeXFacing(null, aRenderer, aBlock, 0, 0, 0, tc.getTexture(aBlock, metadata, WEST), true);
-            renderPositiveXFacing(null, aRenderer, aBlock, 0, 0, 0, tc.getTexture(aBlock, metadata, EAST), true);
-        }
-
-        aBlock.setBlockBounds(blockMin, blockMin, blockMin, blockMax, blockMax, blockMax);
-        aRenderer.setRenderBoundsFromBlock(aBlock);
-
-        GL11.glTranslatef(0.5F, 0.5F, 0.5F);
-        aRenderer.useInventoryTint = false;
-    }
-
-    @Override
-    public boolean renderWorldBlock(IBlockAccess aWorld, int aX, int aY, int aZ, Block aBlock, int aModelID,
-        RenderBlocks aRenderer) {
-        aRenderer.enableAO = Minecraft.isAmbientOcclusionEnabled() && GTMod.gregtechproxy.mRenderTileAmbientOcclusion;
-        aRenderer.useInventoryTint = false;
-        if (aBlock instanceof ITextureBlock tc) {
-            aBlock.setBlockBounds(blockMin, blockMin, blockMin, blockMax, blockMax, blockMax);
-            aRenderer.setRenderBoundsFromBlock(aBlock);
-            renderNegativeYFacing(
-                aWorld,
-                aRenderer,
-                aBlock,
-                aX,
-                aY,
-                aZ,
-                tc.getTexture(aBlock, DOWN, aWorld, aX, aY, aZ),
-                true);
-            renderPositiveYFacing(
-                aWorld,
-                aRenderer,
-                aBlock,
-                aX,
-                aY,
-                aZ,
-                tc.getTexture(aBlock, UP, aWorld, aX, aY, aZ),
-                true);
-            renderNegativeZFacing(
-                aWorld,
-                aRenderer,
-                aBlock,
-                aX,
-                aY,
-                aZ,
-                tc.getTexture(aBlock, NORTH, aWorld, aX, aY, aZ),
-                true);
-            renderPositiveZFacing(
-                aWorld,
-                aRenderer,
-                aBlock,
-                aX,
-                aY,
-                aZ,
-                tc.getTexture(aBlock, SOUTH, aWorld, aX, aY, aZ),
-                true);
-            renderNegativeXFacing(
-                aWorld,
-                aRenderer,
-                aBlock,
-                aX,
-                aY,
-                aZ,
-                tc.getTexture(aBlock, WEST, aWorld, aX, aY, aZ),
-                true);
-            renderPositiveXFacing(
-                aWorld,
-                aRenderer,
-                aBlock,
-                aX,
-                aY,
-                aZ,
-                tc.getTexture(aBlock, EAST, aWorld, aX, aY, aZ),
-                true);
-        }
-        return false;
-    }
-
-    @Override
-    public boolean shouldRender3DInInventory(int modelId) {
-        return true;
-    }
-
-    @Override
-    public int getRenderId() {
-        return this.mRenderID;
-    }
-}

src/main/java/goodgenerator/loader/Loaders.java
@@ -7,7 +7,6 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 
-import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.registry.GameRegistry;
 import goodgenerator.blocks.myFluids.FluidsBuilder;
 import goodgenerator.blocks.regularBlock.AntimatterRenderBlock;
@@ -43,7 +42,6 @@ import goodgenerator.blocks.tileEntity.MTESupercriticalFluidTurbine;
 import goodgenerator.blocks.tileEntity.MTEUniversalChemicalFuelEngine;
 import goodgenerator.blocks.tileEntity.MTEYottaFluidTank;
 import goodgenerator.blocks.tileEntity.render.TileAntimatter;
-import goodgenerator.client.render.BlockRenderHandler;
 import goodgenerator.crossmod.ic2.CropsLoader;
 import goodgenerator.crossmod.nei.NEIConfig;
 import goodgenerator.crossmod.thaumcraft.LargeEssentiaEnergyData;
@@ -556,11 +554,6 @@ public class Loaders {
     }
 
     public static void initLoad() {
-        if (FMLCommonHandler.instance()
-            .getSide()
-            .isClient()) {
-            new BlockRenderHandler();
-        }
         GTMetaTileRegister();
         initLoadRecipes();
         CropsLoader.registerCrops();

src/main/java/gregtech/api/enums/Mods.java
@@ -15,6 +15,7 @@ public enum Mods {
     AdvancedSolarPanel(Names.ADVANCED_SOLAR_PANEL),
     AdventureBackpack(Names.ADVENTURE_BACKPACK),
     AkashicTome(Names.AKASHIC_TOME),
+    Angelica(Names.ANGELICA),
     AppleCore(Names.APPLE_CORE),
     AppliedEnergistics2(Names.APPLIED_ENERGISTICS2),
     ArchitectureCraft(Names.ARCHITECTURE_CRAFT),
@@ -196,6 +197,7 @@ public enum Mods {
         public static final String ADVANCED_SOLAR_PANEL = "AdvancedSolarPanel";
         public static final String ADVENTURE_BACKPACK = "adventurebackpack";
         public static final String AKASHIC_TOME = "akashictome";
+        public static final String ANGELICA = "angelica";
         public static final String APPLE_CORE = "AppleCore";
         public static final String APPLIED_ENERGISTICS2 = "appliedenergistics2";
         public static final String ARCHITECTURE_CRAFT = "ArchitectureCraft";

commit ed8e1dd6f51558f3f8c392d19108856992702497

fix rendering issues introduced by #3973

src/main/java/gregtech/common/render/GTRendererCasing.java
@@ -42,7 +42,7 @@ public class GTRendererCasing implements ISimpleBlockRenderingHandler {
         aRenderer.enableAO = false;
         aRenderer.useInventoryTint = true;
 
-        setupBlockTexturesOnly(aBlock, aMeta);
+        setupBlockTexturesOnly(aBlock, aMeta, true);
 
         GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F);
         GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
@@ -65,11 +65,19 @@ public class GTRendererCasing implements ISimpleBlockRenderingHandler {
         aRenderer.useInventoryTint = false;
     }
 
-    private void setupBlockTexturesOnly(Block aBlock, int aMeta) {
+    private void setupBlockTexturesOnly(Block aBlock, int aMeta, boolean noCoord) {
         ForgeDirection[] validDirections = VALID_DIRECTIONS;
         for (int i = 0, validDirectionsLength = validDirections.length; i < validDirectionsLength; i++) {
             ForgeDirection tFace = validDirections[i];
-            textureArray[i][0] = TextureFactory.of(aBlock, aMeta, tFace);
+            if (noCoord) {
+                textureArray[i][0] = TextureFactory.builder()
+                    .setFromBlock(aBlock, aMeta)
+                    .setFromSide(tFace)
+                    .noWorldCoord()
+                    .build();
+            } else {
+                textureArray[i][0] = TextureFactory.of(aBlock, aMeta, tFace);
+            }
             textureArray[i][1] = null;
         }
     }
@@ -84,7 +92,7 @@ public class GTRendererCasing implements ISimpleBlockRenderingHandler {
 
         ITexture[] overlayed = RenderOverlay.get(aWorld, aX, aY, aZ);
         if (overlayed == null) {
-            setupBlockTexturesOnly(aBlock, tMeta);
+            setupBlockTexturesOnly(aBlock, tMeta, false);
         } else {
             ForgeDirection[] validDirections = VALID_DIRECTIONS;
             for (int i = 0, validDirectionsLength = validDirections.length; i < validDirectionsLength; i++) {

src/main/java/gregtech/common/tileentities/machines/multi/MTEAirFilterBase.java
@@ -519,11 +519,14 @@ public abstract class MTEAirFilterBase extends MTEEnhancedMultiBlockBase<MTEAirF
 
         IIconContainer[] tTextures = getBaseMetaTileEntity().isActive() ? TURBINE_NEW_ACTIVE : TURBINE_NEW;
 
+        int[] xyz = new int[3];
+        ExtendedFacing ext = getExtendedFacing();
+        ext.getWorldOffset(new int[] { 0, -3, 1 }, xyz);
         GTUtilityClient.setTurbineOverlay(
             tile.getWorld(),
-            tile.getXCoord(),
-            tile.getYCoord(),
-            tile.getZCoord(),
+            xyz[0] + tile.getXCoord(),
+            xyz[1] + tile.getYCoord(),
+            xyz[2] + tile.getZCoord(),
             getExtendedFacing(),
             tTextures,
             overlayTickets);