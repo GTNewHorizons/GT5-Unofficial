cb8209bce7707ef11383570faa12c3081dab0a9f
FrostyFire1 <kamilbaron02@gmail.com>
2025-03-31 00:29:24 +0200

    implement toEngineering(), measure every 5 seconds

--- src/main/java/gregtech/common/handlers/PowerGogglesHudHandler.java
+++ src/main/java/gregtech/common/handlers/PowerGogglesHudHandler.java
@@ -18,6 +18,7 @@ import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiChat;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import org.lwjgl.opengl.GL11;
 
@@ -29,6 +30,13 @@ import java.util.List;
 public class PowerGogglesHudHandler {
     static List<Text> hudList = new ArrayList<>();
     static Minecraft mc = Minecraft.getMinecraft();
+    static int ticks = 0;
+    static final int ticksBetweenMeasurements = 100;
+    static BigInteger currentEU = BigInteger.valueOf(0);
+    static BigInteger lastChange = BigInteger.valueOf(0);
+    static BigInteger measurement = BigInteger.valueOf(0);
+    static int measurements = 0;
+
 
     @SubscribeEvent
     @SideOnly(Side.CLIENT)
@@ -57,16 +65,50 @@ public class PowerGogglesHudHandler {
         GL11.glPushMatrix();
         GuiHelper.drawHoveringText(hudList, new Pos2d(x, y), new Size(width, height), width, 90/100F, true, Alignment.CenterLeft, false);
 
-
+        GL11.glDisable(GL11.GL_LIGHTING);
         GL11.glPopMatrix();
     }
     @SideOnly(Side.CLIENT)
     public static void clientTick() {
-        hudList = new ArrayList<>();
-        hudList.add(new Text(toEngineering(WirelessNetworkManager.getUserEU(mc.thePlayer.getUniqueID()))).color(Color.rgb(255,255,255)));
+        if(ticks % ticksBetweenMeasurements == 0){
+            measurement = WirelessNetworkManager.getUserEU(mc.thePlayer.getUniqueID());
+            lastChange = measurements == 0 ? BigInteger.valueOf(0) : measurement.subtract(currentEU);
+            currentEU = measurement;
+            EnumChatFormatting changeColor = EnumChatFormatting.WHITE;
+            int lastChangeDiff = lastChange.compareTo(BigInteger.valueOf(0));
+
+
+
+            hudList = new ArrayList<>();
+            if(measurements > 0){
+                if(lastChangeDiff < 0) {
+                    changeColor = EnumChatFormatting.RED;
+                } else if(lastChangeDiff > 0) {
+                    changeColor = EnumChatFormatting.GREEN;
+                }
+            }
+            ++measurements;
+            hudList.add(new Text(EnumChatFormatting.WHITE+"Wireless EU: " + changeColor+toEngineering(currentEU)));
+            hudList.add(new Text(EnumChatFormatting.WHITE+"5s: " + changeColor+toEngineering(lastChange) + (lastChangeDiff == 0 ? String.format(" (%s eu/t) ",toEngineering(lastChange.divide(BigInteger.valueOf(ticksBetweenMeasurements)))) : "")));
+
+        }
+        ++ticks;
     }
     public static String toEngineering(BigInteger EU){
+        if(EU.abs().compareTo(BigInteger.valueOf(1)) < 0){
+            return "0";
+        }
+        int exponent = BigIntegerMath.log10(EU.abs(), RoundingMode.FLOOR);
+        int remainder = exponent % 3;
+
         String euString = EU.toString();
-        return String.format("%sE%d",euString.substring(0,2), euString.length()/3);
+        if(EU.abs().compareTo(BigInteger.valueOf(1000)) < 0){
+            return euString;
+        }
+        int negative = EU.compareTo(BigInteger.valueOf(1)) < 0 ? 1 : 0;
+        String base = euString.substring(0,remainder+1+negative);
+        String decimal = euString.substring(remainder+1+negative,Math.min(exponent, remainder+4));
+        int E = exponent-remainder; //Round down to nearest 10^3k
+        return String.format("%s.%sE%d",base, decimal, E);
     }
 }
