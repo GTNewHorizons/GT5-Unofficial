0ccdbf5baced6b42d10ed95f17a56cbeedde79b7
FrostyFire1 <kamilbaron02@gmail.com>
2025-03-31 20:36:36 +0200

    server/client sync for lsc link

--- src/main/java/gregtech/common/handlers/PowerGogglesEventHandler.java
+++ src/main/java/gregtech/common/handlers/PowerGogglesEventHandler.java
@@ -2,27 +2,28 @@ package gregtech.common.handlers;
 
 import static gregtech.api.enums.GTValues.NW;
 
-import appeng.api.util.DimensionalCoord;
-import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
-import kekztech.common.tileentities.MTELapotronicSuperCapacitor;
+import java.math.BigInteger;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.tileentity.TileEntity;
 
+import appeng.api.util.DimensionalCoord;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
 import cpw.mods.fml.common.network.FMLNetworkEvent;
 import cpw.mods.fml.relauncher.Side;
+import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
 import gregtech.api.net.GTPacketUpdatePowerGoggles;
 import gregtech.common.misc.WirelessNetworkManager;
-import net.minecraft.tileentity.TileEntity;
-
-import java.math.BigInteger;
+import kekztech.common.tileentities.MTELapotronicSuperCapacitor;
 
 public class PowerGogglesEventHandler {
 
     private static int ticks = 0;
     public static Minecraft mc;
     public static DimensionalCoord lscLink = null;
+
     @SubscribeEvent
     public void playerTickStart(TickEvent.PlayerTickEvent event) {
         if (event.phase != TickEvent.Phase.START) return;
@@ -32,12 +33,11 @@ public class PowerGogglesEventHandler {
         } else {
             if ((ticks % PowerGogglesHudHandler.ticksBetweenMeasurements) == 0) {
                 EntityPlayerMP player = (EntityPlayerMP) event.player;
-                if(lscLink != null) {
+                if (lscLink != null) {
                     TileEntity tileEntity = player.worldObj.getTileEntity(lscLink.x, lscLink.y, lscLink.z);
-                    MTELapotronicSuperCapacitor lsc = (MTELapotronicSuperCapacitor) ((IGregTechTileEntity) tileEntity).getMetaTileEntity();
-                    NW.sendToPlayer(
-                        new GTPacketUpdatePowerGoggles(BigInteger.valueOf(lsc.getEUVar())),
-                        player);
+                    MTELapotronicSuperCapacitor lsc = (MTELapotronicSuperCapacitor) ((IGregTechTileEntity) tileEntity)
+                        .getMetaTileEntity();
+                    NW.sendToPlayer(new GTPacketUpdatePowerGoggles(BigInteger.valueOf(lsc.getEUVar())), player);
                 } else {
                     NW.sendToPlayer(
                         new GTPacketUpdatePowerGoggles(WirelessNetworkManager.getUserEU((player).getUniqueID())),
