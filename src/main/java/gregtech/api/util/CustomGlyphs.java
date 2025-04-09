321f1d43525afc4b70d1659f692926e6cfdbd47f
redspah <redspah@gmail.com>
2025-04-09 14:52:42 +0100

    Adds chemical formulas for many materials that previously didn't have it
    Fixes the display of subscript and superscript numbers with custom characters
    
    Fixes chemical formulas of Platline, Monaline, Bastline, and others
    
    Makes no gameplay changes

--- src/main/java/gregtech/api/util/CustomGlyphs.java
+++ src/main/java/gregtech/api/util/CustomGlyphs.java
@@ -6,4 +6,22 @@ public class CustomGlyphs {
     public static final String EARTH = "\uE001";
     public static final String FIRE = "\uE002";
     public static final String WATER = "\uE003";
+    public static final String BRIMSTONE = "\uE004";
+    public static final String CIRCLE_CROSS = "\uE005";
+    public static final String CIRCLE_STAR = "\uE006";
+    public static final String PICKAXE = "\uE007";
+    public static final String ARROW_CORNER_SOUTH_EAST = "\uE008";
+    public static final String ARROW_CORNER_NORTH_WEST = "\uE009";
+    public static final String HIGH_VOLTAGE = "\uE00A";
+    public static final String SUPERSCRIPT0 = "\uE010";
+    public static final String SUPERSCRIPT1 = "\uE011";
+    public static final String SUPERSCRIPT2 = "\uE012";
+    public static final String SUPERSCRIPT3 = "\uE013";
+    public static final String SUPERSCRIPT4 = "\uE014";
+    public static final String SUPERSCRIPT5 = "\uE015";
+    public static final String SUPERSCRIPT6 = "\uE016";
+    public static final String SUPERSCRIPT7 = "\uE017";
+    public static final String SUPERSCRIPT8 = "\uE018";
+    public static final String SUPERSCRIPT9 = "\uE019";
+    public static final String SUBSCRIPT0 = "\uE01A";
 }
