77facd958e58855d5115785c73adbb71d0a04625
reobf <117543727+reobf@users.noreply.github.com>
2025-04-09 11:56:53 +0800

    Allow DualInputInventory fallback to non-cached processing procedure (#4134)
    Co-authored-by: Maya <10861407+serenibyss@users.noreply.github.com>

--- src/main/java/gregtech/common/tileentities/machines/IDualInputHatch.java
+++ src/main/java/gregtech/common/tileentities/machines/IDualInputHatch.java
@@ -1,9 +1,11 @@
 package gregtech.common.tileentities.machines;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Optional;
 
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.FluidStack;
 
 import gregtech.api.logic.ProcessingLogic;
 
@@ -24,4 +26,46 @@ public interface IDualInputHatch {
     ItemStack[] getSharedItems();
 
     void setProcessingLogic(ProcessingLogic pl);
+
+    default ItemStack[] getAllItems() {
+        ArrayList<ItemStack> rList = new ArrayList<>();
+        for (ItemStack item : getSharedItems()) {
+            if (item != null) {
+                rList.add(item);
+            }
+        }
+        Iterator<? extends IDualInputInventory> inventoryIterator = this.inventories();
+        while (inventoryIterator.hasNext()) {
+            ItemStack[] items = inventoryIterator.next()
+                .getItemInputs();
+            if (items == null) {
+                continue;
+            }
+            for (ItemStack item : items) {
+                if (item != null) {
+                    rList.add(item);
+                }
+            }
+        }
+        return rList.toArray(new ItemStack[rList.size()]);
+    }
+
+    default FluidStack[] getAllFluids() {
+        ArrayList<FluidStack> rList = new ArrayList<>();
+        Iterator<? extends IDualInputInventory> inventoryIterator = this.inventories();
+        while (inventoryIterator.hasNext()) {
+            FluidStack[] fluids = inventoryIterator.next()
+                .getFluidInputs();
+            if (fluids == null) {
+                continue;
+            }
+            for (FluidStack fluid : fluids) {
+                if (fluid != null) {
+                    rList.add(fluid);
+                }
+            }
+        }
+        return rList.toArray(new FluidStack[rList.size()]);
+
+    };
 }
