9d52912dd6d1587b5889e6c457991047c39b9b92
Mary Hopson <doyoumined@gmail.com>
2025-04-11 19:40:43 -0400

    Change casing names, add flavor text

--- src/main/java/gregtech/common/blocks/BlockCasings12.java
+++ src/main/java/gregtech/common/blocks/BlockCasings12.java
@@ -1,13 +1,16 @@
 package gregtech.common.blocks;
 
+import java.util.List;
+
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
+import net.minecraft.util.StatCollector;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import gregtech.api.enums.ItemList;
 import gregtech.api.enums.Textures;
-import gregtech.api.util.GTLanguageManager;
 
 /**
  * The casings are split into separate files because they are registered as regular blocks, and a regular block can have
@@ -17,13 +20,10 @@ public class BlockCasings12 extends BlockCasingsAbstract {
 
     public BlockCasings12() {
         super(ItemCasings.class, "gt.blockcasings12", MaterialCasings.INSTANCE, 16);
-        GTLanguageManager.addStringLocalization(getUnlocalizedName() + ".0.name", "Simple Vat Casing");
-        GTLanguageManager.addStringLocalization(getUnlocalizedName() + ".1.name", "Advanced Vat Casing");
-        GTLanguageManager.addStringLocalization(getUnlocalizedName() + ".2.name", "Biologically Enhanced Vat Casing");
 
-        ItemList.Casing_Vat_T1.set(new ItemStack(this, 1, 0));
-        ItemList.Casing_Vat_T2.set(new ItemStack(this, 1, 1));
-        ItemList.Casing_Vat_T3.set(new ItemStack(this, 1, 2));
+        register(0, ItemList.Casing_Vat_T1, "Hazard-Shielded Casing");
+        register(1, ItemList.Casing_Vat_T2, "Dynamic Bio-Regulation Casing");
+        register(2, ItemList.Casing_Vat_T3, "Symbiotic Vat Casing");
     }
 
     @Override
@@ -40,5 +40,16 @@ public class BlockCasings12 extends BlockCasingsAbstract {
             case 2 -> Textures.BlockIcons.STERILE_VAT_CASING_T3.getIcon();
             default -> Textures.BlockIcons.MACHINE_CASING_ROBUST_TUNGSTENSTEEL.getIcon();
         };
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, EntityPlayer player, List<String> tooltip, boolean advancedTooltips) {
+        super.addInformation(stack, player, tooltip, advancedTooltips);
+
+        switch (stack.getItemDamage()) {
+            case 0 -> tooltip.add(StatCollector.translateToLocal("gt.casing.hazard-shielded"));
+            case 1 -> tooltip.add(StatCollector.translateToLocal("gt.casing.dynamic-bio-regulation"));
+            case 2 -> tooltip.add(StatCollector.translateToLocal("gt.casing.symbiotic-vat"));
+        }
     }
 }
