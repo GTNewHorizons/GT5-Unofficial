1fe4166fe86c8d6d85a9a8ca73282f3f4fc03bf0
FrostyFire1 <kamilbaron02@gmail.com>
2025-04-11 10:41:06 +0200

    spotless

--- src/main/java/gregtech/common/items/ItemPowerGoggles.java
+++ src/main/java/gregtech/common/items/ItemPowerGoggles.java
@@ -4,7 +4,6 @@ import static gregtech.api.enums.GTValues.NW;
 
 import java.util.List;
 
-import net.minecraft.client.entity.EntityClientPlayerMP;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
