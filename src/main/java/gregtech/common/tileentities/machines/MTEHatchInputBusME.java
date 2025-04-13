31a5494f05acc791135f0f51c86a8906def414ba
Jurre Groenendijk <j.j.k.groenendijk@student.tudelft.nl>
2025-04-13 20:01:00 +0200

    Remove hard limit on slot 0 for adv assline

--- src/main/java/gregtech/common/tileentities/machines/MTEHatchInputBusME.java
+++ src/main/java/gregtech/common/tileentities/machines/MTEHatchInputBusME.java
@@ -668,6 +668,10 @@ public class MTEHatchInputBusME extends MTEHatchInputBus
         return shadowInventory[index];
     }
 
+    public int getShadowInventorySize() {
+        return shadowInventory.length;
+    }
+
     @Override
     public boolean isValidSlot(int aIndex) {
         return aIndex == getManualSlot();
