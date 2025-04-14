80c35a1a77b6a36fa6f85b487caefe3248ecaaa9
FrostyFire1 <kamilbaron02@gmail.com>
2025-04-15 00:29:22 +0200

    spotless

--- src/main/java/gregtech/common/tools/ToolVajraButNotARR.java
+++ src/main/java/gregtech/common/tools/ToolVajraButNotARR.java
@@ -5,23 +5,13 @@ import static gregtech.api.enums.GTValues.VN;
 
 import java.util.List;
 
-import bartworks.system.material.BWMetaGeneratedOres;
-import gregtech.common.blocks.BlockOres;
-import gtPlusPlus.core.block.base.BlockBaseOre;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockAir;
 import net.minecraft.block.ITileEntityProvider;
-import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.multiplayer.PlayerControllerMP;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -29,9 +19,12 @@ import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.world.World;
 
+import bartworks.system.material.BWMetaGeneratedOres;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import gregtech.api.items.ItemTool;
+import gregtech.common.blocks.BlockOres;
+import gtPlusPlus.core.block.base.BlockBaseOre;
 import ic2.api.item.ElectricItem;
 import ic2.api.item.IElectricItem;
 
@@ -134,16 +127,18 @@ public class ToolVajraButNotARR extends ItemTool implements IElectricItem {
     }
 
     @Override
-    public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, World world, int x, int y, int z,
-                                  int side, float hitX, float hitY, float hitZ) {
-        Block target = world.getBlock(x,y,z);
-        int metaData = world.getBlockMetadata(x,y,z);
+    public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side,
+        float hitX, float hitY, float hitZ) {
+        Block target = world.getBlock(x, y, z);
+        int metaData = world.getBlockMetadata(x, y, z);
 
-        if(!ElectricItem.manager.canUse(stack, baseCost * target.blockHardness)) return super.onItemUseFirst(stack, player, world, x, y, z, side, hitX, hitY, hitZ);
-        if(target instanceof ITileEntityProvider && !isTileEntityOre(target) && !player.isSneaking()) return super.onItemUseFirst(stack, player, world, x, y, z, side, hitX, hitY, hitZ);
+        if (!ElectricItem.manager.canUse(stack, baseCost * target.blockHardness))
+            return super.onItemUseFirst(stack, player, world, x, y, z, side, hitX, hitY, hitZ);
+        if (target instanceof ITileEntityProvider && !isTileEntityOre(target) && !player.isSneaking())
+            return super.onItemUseFirst(stack, player, world, x, y, z, side, hitX, hitY, hitZ);
 
-        if(world.isRemote){
-            Minecraft.getMinecraft().playerController.onPlayerDestroyBlock(x,y,z,side);
+        if (world.isRemote) {
+            Minecraft.getMinecraft().playerController.onPlayerDestroyBlock(x, y, z, side);
         } else {
             target.onBlockHarvested(world, x, y, z, metaData, player);
             if (target.removedByPlayer(world, player, x, y, z, true)) {
@@ -151,7 +146,8 @@ public class ToolVajraButNotARR extends ItemTool implements IElectricItem {
                 target.harvestBlock(world, player, x, y, z, metaData);
             }
         }
-        stack.getTagCompound().setBoolean("harvested",true); //prevent onItemRightClick from going through
+        stack.getTagCompound()
+            .setBoolean("harvested", true); // prevent onItemRightClick from going through
         ElectricItem.manager.use(stack, baseCost * target.blockHardness, player);
         return super.onItemUseFirst(stack, player, world, x, y, z, side, hitX, hitY, hitZ);
     }
@@ -163,19 +159,20 @@ public class ToolVajraButNotARR extends ItemTool implements IElectricItem {
     @Override
     public ItemStack onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer player) {
         NBTTagCompound tag = itemStackIn.hasTagCompound() ? itemStackIn.getTagCompound() : new NBTTagCompound();
-        if(!worldIn.isRemote && !itemStackIn.hasTagCompound()){
-            tag.setBoolean("silk",false);
+        if (!worldIn.isRemote && !itemStackIn.hasTagCompound()) {
+            tag.setBoolean("silk", false);
             itemStackIn.setTagCompound(tag);
         }
-        if(tag.getBoolean("harvested")) {
+        if (tag.getBoolean("harvested")) {
             tag.removeTag("harvested");
-            return super.onItemRightClick(itemStackIn,worldIn,player);
+            return super.onItemRightClick(itemStackIn, worldIn, player);
         }
 
-        if(!worldIn.isRemote && player.isSneaking()){
-            if(itemStackIn.getTagCompound().getBoolean("silk")){
+        if (!worldIn.isRemote && player.isSneaking()) {
+            if (itemStackIn.getTagCompound()
+                .getBoolean("silk")) {
                 tag.removeTag("ench");
-                tag.setBoolean("silk",false);
+                tag.setBoolean("silk", false);
                 player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Disabled vajra silk touch"));
             } else {
                 itemStackIn.addEnchantment(Enchantment.silkTouch, 1);
@@ -183,6 +180,6 @@ public class ToolVajraButNotARR extends ItemTool implements IElectricItem {
                 player.addChatMessage(new ChatComponentText(EnumChatFormatting.GREEN + "Enabled vajra silk touch"));
             }
         }
-        return super.onItemRightClick(itemStackIn,worldIn,player);
+        return super.onItemRightClick(itemStackIn, worldIn, player);
     }
 }
