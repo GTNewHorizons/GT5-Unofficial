0351b541737e3d364def2d459e1c55955e8f8131
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2025-04-17 10:17:32 +0800

    Revert tooltip changes, keep the Electric Implosion Compressor

--- src/main/java/gregtech/common/blocks/ItemStorage.java
+++ src/main/java/gregtech/common/blocks/ItemStorage.java
@@ -11,19 +11,11 @@ import gregtech.api.GregTechAPI;
 
 public class ItemStorage extends ItemBlock {
 
-    public final BlockMetal blockMetal;
-
     public ItemStorage(Block block) {
         super(block);
         setMaxDamage(0);
         setHasSubtypes(true);
         setCreativeTab(GregTechAPI.TAB_GREGTECH_MATERIALS);
-
-        if (block instanceof BlockMetal) {
-            this.blockMetal = (BlockMetal) block;
-        } else {
-            this.blockMetal = null;
-        }
     }
 
     @Override
@@ -51,7 +43,5 @@ public class ItemStorage extends ItemBlock {
     @Override
     public void addInformation(ItemStack aStack, EntityPlayer aPlayer, List<String> aList, boolean aF3_H) {
         super.addInformation(aStack, aPlayer, aList, aF3_H);
-
-        blockMetal.addInformation(aStack, aPlayer, aList, aF3_H);
     }
 }
