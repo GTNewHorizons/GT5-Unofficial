58d8009ea91cfa2e084dccfb9691ebd1238f1fae
llyyaacn <llyyaacn@163.com>
2025-04-21 11:45:42 -0500

    Large naquadahReactor recipes should use Fuel backend

--- src/main/java/goodgenerator/blocks/tileEntity/MTEPreciseAssembler.java
+++ src/main/java/goodgenerator/blocks/tileEntity/MTEPreciseAssembler.java
@@ -13,7 +13,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
