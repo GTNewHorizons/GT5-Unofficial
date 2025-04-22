dea4f42134e565a88a2bea10f43705a89f979e46
Raven Szewczyk <git@eigenraven.me>
2025-04-22 18:06:37 +0100

    Use lowercase modid for gregtech_nh

--- src/main/java/gregtech/GTNHMod.java
+++ src/main/java/gregtech/GTNHMod.java
@@ -4,6 +4,6 @@ import cpw.mods.fml.common.Mod;
 
 // Mod class to provide a mod id in the GTNH GT that makes resolving mod id collisions with GT6 easier
 @SuppressWarnings("unused")
-@Mod(modid = "gregtechNH", name = "GregTech GTNH", version = GT_Version.VERSION)
+@Mod(modid = "gregtech_nh", name = "GregTech GTNH", version = GT_Version.VERSION)
 public class GTNHMod {
 }
