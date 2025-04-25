1961e1e7773f7d7b7e20071725cf1c6c24b06030
Noc <95299389+Nockyx@users.noreply.github.com>
2025-04-25 09:16:46 -0300

    Netherite: Let the Hell be with us (#4074)
    Co-authored-by: Martin Robertz <dream-master@gmx.net>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Maya <10861407+serenibyss@users.noreply.github.com>

--- src/main/java/gregtech/api/enums/ItemList.java
+++ src/main/java/gregtech/api/enums/ItemList.java
@@ -2604,6 +2604,12 @@ public enum ItemList implements IItemContainer {
     Machine_Multi_AirFilterT1,
     Machine_Multi_AirFilterT2,
     Machine_Multi_AirFilterT3,
+    Heavy_Hellish_Mud,
+    Netherite_Scrap_Seed,
+    Brittle_Netherite_Scrap,
+    Netherite_Nanoparticles,
+    Intensely_Bonded_Netherite_Nanoparticles,
+    Hot_Netherite_Scrap,
     LargeMolecularAssembler,
 
     SpaceElevatorController,
