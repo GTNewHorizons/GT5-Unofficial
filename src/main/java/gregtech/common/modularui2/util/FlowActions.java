2914ef2c9d1c44d581b5b0bb4d760c701fda22dd
YannickMG <yannickmg@gmail.com>
2025-04-27 11:34:52 -0400

    Add code cleanup TODO

--- src/main/java/gregtech/common/modularui2/util/FlowActions.java
+++ src/main/java/gregtech/common/modularui2/util/FlowActions.java
@@ -6,6 +6,7 @@ import com.cleanroommc.modularui.widgets.layout.Flow;
 
 public class FlowActions {
 
+    // TODO: Inline this method once https://github.com/CleanroomMC/ModularUI/pull/132 is backported
     public static void resize(Flow column) {
         if (NetworkUtils.isClient()) {
             WidgetTree.resize(column);
