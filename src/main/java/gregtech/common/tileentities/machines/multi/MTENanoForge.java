dbc8bd81be92451b35f5e01c2eb07430e13f2eb7
GDCloud <gdcloudstrike@gmail.com>
2025-04-28 21:55:09 +0200

    merge cleanup

--- src/main/java/gregtech/common/tileentities/machines/multi/MTENanoForge.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/MTENanoForge.java
@@ -754,7 +754,6 @@ public class MTENanoForge extends MTEExtendedPowerMultiBlockBase<MTENanoForge> i
     @Override
     public final void onScrewdriverRightClick(ForgeDirection side, EntityPlayer aPlayer, float aX, float aY, float aZ,
         ItemStack aTool) {
-        inputSeparation = !inputSeparation;
         renderDisabled = !renderDisabled;
         GTUtility.sendChatToPlayer(
             aPlayer,
