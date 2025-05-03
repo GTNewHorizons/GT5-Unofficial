46d7aa544b35453d0e883690d672320633b348f1
FrostyFire1 <kamilbaron02@gmail.com>
2025-05-03 23:40:14 +0200

    localize asteroid name display

--- src/main/java/gtnhintergalactic/recipe/SpaceMiningData.java
+++ src/main/java/gtnhintergalactic/recipe/SpaceMiningData.java
@@ -2,6 +2,8 @@ package gtnhintergalactic.recipe;
 
 import java.util.Objects;
 
+import net.minecraft.util.StatCollector;
+
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 public class SpaceMiningData {
@@ -44,6 +46,11 @@ public class SpaceMiningData {
         if (computation != that.computation) return false;
         if (recipeWeight != that.recipeWeight) return false;
         return asteroidName.equals(that.asteroidName);
+    }
+
+    public String getAsteroidNameLocalized() {
+        return StatCollector.translateToLocal("ig.asteroid." + asteroidName) + " "
+            + StatCollector.translateToLocal("ig.asteroid");
     }
 
     @Override
