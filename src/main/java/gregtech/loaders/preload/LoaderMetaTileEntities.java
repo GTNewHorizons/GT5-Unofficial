b3865cc90091d53afc2927a7e2b7ffe30ba08ebd
Jurre Groenendijk <jurre@jilles.com>
2025-05-06 14:02:12 +0200

    Nac changes (#4266)
    Co-authored-by: Jurre Groenendijk <j.j.k.groenendijk@student.tudelft.nl>

--- src/main/java/gregtech/loaders/preload/LoaderMetaTileEntities.java
+++ src/main/java/gregtech/loaders/preload/LoaderMetaTileEntities.java
@@ -1482,6 +1482,48 @@ public class LoaderMetaTileEntities implements Runnable { // TODO CHECK CIRCUIT 
             new MTELargeFluidExtractor(LARGE_FLUID_EXTRACTOR.ID, "multimachine.fluidextractor", "Large Fluid Extractor")
                 .getStackForm(1));
 
+        ItemList.Machine_Multi_NanochipAssemblyComplex.set(
+            new MTENanochipAssemblyComplex(
+                NANOCHIP_ASSEMBLY_CONTROLLER.ID,
+                "multimachine.nanochipassemblycomplex",
+                "Nanochip Assembly Complex").getStackForm(1));
+        ItemList.NanoChipModule_AssemblyMatrix.set(
+            new AssemblyMatrix(
+                NANOCHIP_MODULE_ASSEMBLY_MATRIX.ID,
+                "multimachine.nanochipmodule.assemblymatrix",
+                "Nanochip Assembly Matrix").getStackForm(1));
+        ItemList.NanoChipModule_SMDProcessor.set(
+            new SMDProcessor(
+                NANOCHIP_MODULE_SMD_PROCESSOR.ID,
+                "multimachine.nanochipmodule.smdprocessor",
+                "Part Preparation Apparatus").getStackForm(1));
+        ItemList.NanoChipModule_BoardProcessor.set(
+            new BoardProcessor(
+                NANOCHIP_MODULE_BOARD_PROCESSOR.ID,
+                "multimachine.nanochipmodule.boadprocessor",
+                "Full-Board Immersion Device").getStackForm(1));
+        ItemList.NanoChipModule_EtchingArray.set(
+            new EtchingArray(
+                NANOCHIP_MODULE_ETCHING_ARRAY.ID,
+                "multimachine.nanochipmodule.etchingarray",
+                "Ultra-high Energy Etching Array").getStackForm(1));
+        ItemList.NanoChipModule_CuttingChamber.set(
+            new CuttingChamber(
+                NANOCHIP_MODULE_CUTTING_CHAMBER.ID,
+                "multimachine.nanochipmodule.cuttingchamber",
+                "Nanoprecision Cutting Chamber").getStackForm(1));
+        ItemList.NanoChipModule_WireTracer.set(
+            new WireTracer(
+                NANOCHIP_MODULE_WIRE_TRACER.ID,
+                "multimachine.nanochipmodule.wiretracer",
+                "Nanoprecision Wire Tracer").getStackForm(1));
+        ItemList.NanoChipModule_SuperconductorSplitter.set(
+            new SuperconductorSplitter(
+                NANOCHIP_MODULE_SUPERCONDUCTOR_SPLITTER.ID,
+                "multimachine.nanochipmodule.superconductorsplitter",
+                "Superconductive Strand Splitter").getStackForm(1));
+        ItemList.NanoChipModule_Splitter.set(
+            new Splitter(NANOCHIP_MODULE_SPLITTER.ID, "multimachine.nanochipmodule.splitter", "Nanopart Splitter")
         ItemList.EntropicProcessor.set(
             new MTEEntropicProcessor(ENTROPIC_PROCESSOR.ID, "multimachine.entropic-processor", "Entropic Processor")
                 .getStackForm(1));
