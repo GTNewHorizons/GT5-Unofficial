587362186f9c54f056e7f2595f64849372cdbf51
Jurre Groenendijk <jurre@jilles.com>
2025-05-07 14:30:14 +0200

    Register OOO module and fix EU/t module distribution (#4275)
    Co-authored-by: Jurre Groenendijk <j.j.k.groenendijk@student.tudelft.nl>

--- src/main/java/gregtech/api/enums/ItemList.java
+++ src/main/java/gregtech/api/enums/ItemList.java
@@ -1549,6 +1549,7 @@ public enum ItemList implements IItemContainer {
     NanoChipModule_WireTracer,
     NanoChipModule_Splitter,
     NanoChipModule_SuperconductorSplitter,
+    NanoChipModule_OpticalOrganizer,
 
     Machine_LV_Miner,
     Machine_MV_Miner,
