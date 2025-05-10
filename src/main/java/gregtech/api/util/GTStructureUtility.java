2f39a812e014aa0b40289e9534d7f7292e46dd25
Martin Robertz <dream-master@gmx.net>
2025-05-10 12:32:56 +0200

    remove star imports

--- src/main/java/gregtech/api/util/GTStructureUtility.java
+++ src/main/java/gregtech/api/util/GTStructureUtility.java
@@ -14,7 +14,6 @@ import static com.gtnewhorizon.structurelib.util.ItemStackPredicate.NBTMode.EXAC
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.*;
 import java.util.function.BiConsumer;
 import java.util.function.BiPredicate;
 import java.util.function.Consumer;
