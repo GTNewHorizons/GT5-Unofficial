bb695be14686f91cd3ba77e1c6fc557b3d059aef
jurrejelle <jurre@jilles.com>
2025-05-11 13:08:56 +0200

    Add assline frontend for assembly matrix

--- src/main/java/gregtech/api/recipe/RecipeMaps.java
+++ src/main/java/gregtech/api/recipe/RecipeMaps.java
@@ -1683,6 +1683,7 @@ public final class RecipeMaps {
                 AssemblyMatrix.registerLocalName(output.realCircuit, output);
             }
         })
+        .frontend(AssemblyLineFrontend::new)
         .build();
 
     public static final RecipeMap<RecipeMapBackend> nanochipSMDProcessorRecipes = RecipeMapBuilder
