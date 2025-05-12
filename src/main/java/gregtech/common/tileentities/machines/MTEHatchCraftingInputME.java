88f74c105aaef2d8f335bddea5c405f6ee53c72c
Taskeren <r0yalist@outlook.com>
2025-05-12 11:35:22 +0800

    Drops Fluid Packets instead of Drops

--- src/main/java/gregtech/common/tileentities/machines/MTEHatchCraftingInputME.java
+++ src/main/java/gregtech/common/tileentities/machines/MTEHatchCraftingInputME.java
@@ -321,7 +321,7 @@ public class MTEHatchCraftingInputME extends MTEHatchInputBus
                     World world = parentMTE.getBaseMetaTileEntity()
                         .getWorld();
 
-                    ItemStack fluidDropItemStack = ItemFluidDrop.newStack(fluidStack);
+                    ItemStack fluidDropItemStack = ItemFluidPacket.newStack(fluidStack);
                     if (fluidDropItemStack == null) continue;
 
                     EntityItem entityItem = new EntityItem(
