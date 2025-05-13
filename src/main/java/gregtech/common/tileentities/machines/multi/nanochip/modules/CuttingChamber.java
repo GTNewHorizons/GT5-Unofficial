65cea6f96f6e13b8db950c6ac8140dfc7a3e9339
TheEpicGamer274 <102255081+TheEpicGamer274@users.noreply.github.com>
2025-05-13 16:05:27 -0400

    Nanochip assembly complex nei & waila changes (#4310)

--- src/main/java/gregtech/common/tileentities/machines/multi/nanochip/modules/CuttingChamber.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/nanochip/modules/CuttingChamber.java
@@ -104,7 +104,8 @@ public class CuttingChamber extends MTENanochipAssemblyModuleBase<CuttingChamber
 
     @Override
     protected MultiblockTooltipBuilder createTooltip() {
-        return new MultiblockTooltipBuilder().addInfo(NAC_MODULE)
+        return new MultiblockTooltipBuilder().addMachineType("NAC Module")
+            .addInfo(NAC_MODULE)
             .addInfo("Cuts your Wafer " + TOOLTIP_CC + "s")
             .addInfo("Outputs into the VCO with the same color as the input VCI")
             .addStructureInfo("Any base casing - Vacuum Conveyor Input")
