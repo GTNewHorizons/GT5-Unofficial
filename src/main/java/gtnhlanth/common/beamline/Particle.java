c4faf93442548a348452b778602d3e358e40bd39
TheEpicGamer274 <102255081+TheEpicGamer274@users.noreply.github.com>
2025-05-16 02:01:57 -0400

    Add another particle + temp texture

--- src/main/java/gtnhlanth/common/beamline/Particle.java
+++ src/main/java/gtnhlanth/common/beamline/Particle.java
@@ -8,7 +8,8 @@ public enum Particle {
     PHOTON(false, 1, 0, 0, "photon", "\u03B3", 0, null),
     NEUTRON(false, 2, 939.57f, 15000, "neutron", "n\u2070", 0, null),
     PROTON(true, 3, 938.27f, 15000, "proton", "p\u207A", 1, null),
-    ALPHA(true, 4, 3727.38f, 8000, "alpha", "\u03B1", 2, null);
+    ALPHA(true, 4, 3727.38f, 8000, "alpha", "\u03B1", 2, null),
+    POSITRON(true, 5, 0.510f, 10000, "proton", "e\u207A", 1, null);
 
     public static final Particle[] VALUES = values();
 
