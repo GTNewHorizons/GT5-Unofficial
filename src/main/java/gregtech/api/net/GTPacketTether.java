c67d0a5561e8f408960bec82fb5c424206117281
Caedis <Caedis@users.noreply.github.com>
2025-05-27 11:57:43 -0500

    Packet

--- src/main/java/gregtech/api/net/GTPacketTether.java
+++ src/main/java/gregtech/api/net/GTPacketTether.java
@@ -1,11 +1,12 @@
 package gregtech.api.net;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.world.IBlockAccess;
 
 import com.google.common.io.ByteArrayDataInput;
 
 import gregtech.common.data.maglev.Tether;
-import gregtech.common.tileentities.machines.basic.MTEMagLevPylon;
+import gregtech.common.data.maglev.TetherManager;
 import io.netty.buffer.ByteBuf;
 
 public class GTPacketTether extends GTPacket {
@@ -16,8 +17,9 @@ public class GTPacketTether extends GTPacket {
         super();
     }
 
-    public GTPacketTether(MTEMagLevPylon pylon) {
-        tether = pylon.machineTether;
+    public GTPacketTether(Tether tether) {
+        super();
+        this.tether = tether;
     }
 
     @Override
@@ -28,15 +30,35 @@ public class GTPacketTether extends GTPacket {
     @Override
     public void encode(ByteBuf buffer) {
 
+        buffer.writeBoolean(tether == null); // hasTether
+        if (tether == null) return;
+        buffer.writeByte(0x1); // hasTether
+        buffer.writeInt(tether.sourceX());
+        buffer.writeInt(tether.sourceY());
+        buffer.writeInt(tether.sourceZ());
+        buffer.writeInt(tether.dimID());
+        buffer.writeInt(tether.range());
+        buffer.writeByte(tether.tier());
+
     }
 
     @Override
     public GTPacket decode(ByteArrayDataInput buffer) {
-        return null;
+        if (buffer.readBoolean()) {
+            return new GTPacketTether(null);
+        }
+        Tether newTether = new Tether(
+            buffer.readInt(),
+            buffer.readInt(),
+            buffer.readInt(),
+            buffer.readInt(),
+            buffer.readInt(),
+            buffer.readByte());
+        return new GTPacketTether(newTether);
     }
 
     @Override
     public void process(IBlockAccess world) {
-
+        TetherManager.PLAYER_TETHERS.replace(Minecraft.getMinecraft().thePlayer, tether);
     }
 }
