7023c8cebe452b1f9b6964432fbe05bfdb92c6b7
GitHub GTNH Actions <>
2025-05-28 01:55:39 +0000

    spotlessApply

--- src/main/java/gregtech/loaders/postload/recipes/AssemblerRecipes.java
+++ src/main/java/gregtech/loaders/postload/recipes/AssemblerRecipes.java
@@ -2746,7 +2746,10 @@ public class AssemblerRecipes implements Runnable {
             .addTo(assemblerRecipes);
 
         GTValues.RA.stdBuilder()
-            .itemInputs(GTOreDictUnificator.get(OrePrefixes.plate, Materials.Netherite, 1), GTOreDictUnificator.get(OrePrefixes.plate, Materials.HSSS, 4), ItemList.Casing_LuV.get(1))
+            .itemInputs(
+                GTOreDictUnificator.get(OrePrefixes.plate, Materials.Netherite, 1),
+                GTOreDictUnificator.get(OrePrefixes.plate, Materials.HSSS, 4),
+                ItemList.Casing_LuV.get(1))
             .itemOutputs(ItemList.Casing_Fusion.get(1))
             .fluidInputs(Materials.HSSG.getMolten(2 * INGOTS))
             .duration(5 * SECONDS)
