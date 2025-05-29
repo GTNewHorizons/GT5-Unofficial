18218664d96ff041f538d0780b4f5b4942f3ad97
Caedis <Caedis@users.noreply.github.com>
2025-05-28 20:23:07 -0500

    no non-static wildcard imports

--- src/main/java/gregtech/api/util/GTStructureUtility.java
+++ src/main/java/gregtech/api/util/GTStructureUtility.java
@@ -2,19 +2,13 @@ package gregtech.api.util;
 
 import static com.gtnewhorizon.structurelib.item.ItemConstructableTrigger.TriggerMode.BUILDING;
 import static com.gtnewhorizon.structurelib.item.ItemConstructableTrigger.TriggerMode.REMOVING;
-import static com.gtnewhorizon.structurelib.structure.IStructureElement.PlaceResult.ACCEPT;
-import static com.gtnewhorizon.structurelib.structure.IStructureElement.PlaceResult.ACCEPT_STOP;
-import static com.gtnewhorizon.structurelib.structure.IStructureElement.PlaceResult.REJECT;
-import static com.gtnewhorizon.structurelib.structure.IStructureElement.PlaceResult.SKIP;
-import static com.gtnewhorizon.structurelib.structure.StructureUtility.checkForRemoving;
-import static com.gtnewhorizon.structurelib.structure.StructureUtility.lazy;
-import static com.gtnewhorizon.structurelib.structure.StructureUtility.ofBlocksTiered;
+import static com.gtnewhorizon.structurelib.structure.IStructureElement.PlaceResult.*;
+import static com.gtnewhorizon.structurelib.structure.StructureUtility.*;
 import static com.gtnewhorizon.structurelib.util.ItemStackPredicate.NBTMode.EXACT;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.*;
 import java.util.function.BiConsumer;
 import java.util.function.BiPredicate;
 import java.util.function.Consumer;
