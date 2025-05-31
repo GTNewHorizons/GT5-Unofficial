1ed424405519e9e1d8a2fea9418e8e1e23ad04c6
boubou19 <miisterunknown@gmail.com>
2025-05-31 20:09:57 +0200

    fix imports

--- src/main/java/gtPlusPlus/xmod/gregtech/loaders/RecipeGenBlastSmelterGTNH.java
+++ src/main/java/gtPlusPlus/xmod/gregtech/loaders/RecipeGenBlastSmelterGTNH.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import gregtech.common.items.ItemIntegratedCircuit;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.oredict.OreDictionary;
