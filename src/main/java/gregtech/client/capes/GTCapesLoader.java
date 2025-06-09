ba7bd62a717f3e089e9f897f848c51f7b3ef3594
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2025-06-09 21:13:37 +0200

    add comment on the regex

--- src/main/java/gregtech/client/capes/GTCapesLoader.java
+++ src/main/java/gregtech/client/capes/GTCapesLoader.java
@@ -27,19 +27,17 @@ public class GTCapesLoader implements Runnable {
     private static final ResourceLocation CAPE_TITANIUM      = new ResourceLocation("gregtech:textures/Titanium.png");
     private static final ResourceLocation CAPE_NEUTRONIUM    = new ResourceLocation("gregtech:textures/Neutronium.png");
     private static final ResourceLocation CAPE_STARGATE      = new ResourceLocation("gregtech:textures/Stargate.png");
-    // spotless:on
 
     @Override
     public void run() {
+        /*                                       |------------------------------UUID------------------------------|:capeName(optional)*/
+        Pattern pattern = Pattern.compile("^([0-9a-z]{8}-?[0-9a-z]{4}-?[0-9a-z]{4}-?[0-9a-z]{4}-?[0-9a-z]{12})(?:$|\\:(cape\\w+).*$)");
         GTLog.out.println("GTMod: Downloading Cape List");
         String GTNH_CAPE_LIST_URL = "https://raw.githubusercontent.com/GTNewHorizons/CustomGTCapeHook-Cape-List/master/capes.txt";
         Map<UUID, ResourceLocation> map = new HashMap<>();
-        Pattern pattern = Pattern
-            .compile("^([0-9a-z]{8}-?[0-9a-z]{4}-?[0-9a-z]{4}-?[0-9a-z]{4}-?[0-9a-z]{12})(?:$|\\:(cape\\w+).*$)");
         try (final Scanner scanner = new Scanner(new URL(GTNH_CAPE_LIST_URL).openStream())) {
             while (scanner.hasNextLine()) {
-                String line = scanner.nextLine()
-                    .toLowerCase();
+                String line = scanner.nextLine().toLowerCase();
                 Matcher matcher = pattern.matcher(line);
                 if (matcher.find()) {
                     UUID uuid = uuidFromString(matcher.group(1));
@@ -58,15 +56,15 @@ public class GTCapesLoader implements Runnable {
             e.printStackTrace(GTLog.err);
         }
         // add scheduled task on the main thread
-        Minecraft.getMinecraft()
-            .func_152343_a(() -> {
-                if (!map.isEmpty()) {
-                    MinecraftForge.EVENT_BUS.register(new GTCapesEventHandler(map));
-                }
-                GTLog.out.println("GTMod: Loaded " + map.size() + " Capes");
-                return null;
-            });
+        Minecraft.getMinecraft().func_152343_a(() -> {
+            if (!map.isEmpty()) {
+                MinecraftForge.EVENT_BUS.register(new GTCapesEventHandler(map));
+            }
+            GTLog.out.println("GTMod: Loaded " + map.size() + " Capes");
+            return null;
+        });
     }
+    // spotless:on
 
     private static UUID uuidFromString(String uuid) {
         if (uuid == null) return null;
