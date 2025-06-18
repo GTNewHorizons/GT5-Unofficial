131414046f5427c62e38797cf937eb1881ef76bf
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2025-06-19 00:07:59 +0200

    exit early if world is null in ItemMagLevHarness

--- src/main/java/gregtech/common/items/ItemMagLevHarness.java
+++ src/main/java/gregtech/common/items/ItemMagLevHarness.java
@@ -53,7 +53,7 @@ public class ItemMagLevHarness extends GTGenericItem implements IBaubleExpanded 
 
     @Override
     public void onWornTick(ItemStack itemstack, EntityLivingBase entityLivingBase) {
-        if (entityLivingBase.worldObj != null && entityLivingBase.worldObj.isRemote) return;
+        if (entityLivingBase.worldObj == null || entityLivingBase.worldObj.isRemote) return;
         if (!(entityLivingBase instanceof EntityPlayer player)) return;
         if (player instanceof FakePlayer) return;
 
