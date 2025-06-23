3e686296493cbbc6a242d8445bfa507ec6b299b1
RecursivePineapple <recursive_pineapple@proton.me>
2025-06-23 14:39:53 -0400

    Add help subcommand + remove gray colour

--- src/main/java/gregtech/common/misc/GTPowerfailCommand.java
+++ src/main/java/gregtech/common/misc/GTPowerfailCommand.java
@@ -1,7 +1,5 @@
 package gregtech.common.misc;
 
-import static com.gtnewhorizon.gtnhlib.util.AnimatedTooltipHandler.GRAY;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -18,7 +16,7 @@ import gregtech.common.data.GTPowerfailTracker;
 
 public class GTPowerfailCommand extends CommandBase {
 
-    private static final String[] SUBCOMMANDS = { "clear", "clear-dim", "list", "show", "hide" };
+    private static final String[] SUBCOMMANDS = { "clear", "clear-dim", "list", "show", "hide", "help" };
 
     @Override
     public String getCommandName() {
@@ -32,7 +30,7 @@ public class GTPowerfailCommand extends CommandBase {
 
     @Override
     public String getCommandUsage(ICommandSender sender) {
-        return GRAY + "Usage: /powerfails <subcommand>. Valid subcommands are: " + String.join(", ", SUBCOMMANDS) + ".";
+        return "Usage: /powerfails <subcommand>. Valid subcommands are: " + String.join(", ", SUBCOMMANDS) + ".";
     }
 
     @Override
@@ -42,14 +40,15 @@ public class GTPowerfailCommand extends CommandBase {
 
     private void printHelp(ICommandSender sender) {
         sender.addChatMessage(new ChatComponentText(getCommandUsage(sender)));
-        sender.addChatMessage(new ChatComponentText(GRAY + " clear - Clears all powerfails for your team"));
+        sender.addChatMessage(new ChatComponentText(" clear - Clears all powerfails for your team"));
         sender.addChatMessage(
-            new ChatComponentText(GRAY + " clear-dim - Clears all powerfails for your team in the current dimension"));
-        sender.addChatMessage(new ChatComponentText(GRAY + " list - Prints all uncleared powerfails for your team"));
-        sender.addChatMessage(new ChatComponentText(GRAY + " show - Enables in-world powerfail icons"));
-        sender.addChatMessage(new ChatComponentText(GRAY + " hide - Disables in-world powerfail icons"));
+            new ChatComponentText(" clear-dim - Clears all powerfails for your team in the current dimension"));
+        sender.addChatMessage(new ChatComponentText(" list - Prints all uncleared powerfails for your team"));
+        sender.addChatMessage(new ChatComponentText(" show - Enables in-world powerfail icons"));
+        sender.addChatMessage(new ChatComponentText(" hide - Disables in-world powerfail icons"));
+        sender.addChatMessage(new ChatComponentText(" help - Prints this help text"));
         sender.addChatMessage(new ChatComponentText(""));
-        sender.addChatMessage(new ChatComponentText(GRAY + "Aliases: powerfails, powerfail, pf"));
+        sender.addChatMessage(new ChatComponentText("Aliases: powerfails, powerfail, pf"));
     }
 
     @Override
@@ -88,12 +87,12 @@ public class GTPowerfailCommand extends CommandBase {
         switch (args[0]) {
             case "clear" -> {
                 GTPowerfailTracker.clearPowerfails(player, OptionalInt.empty());
-                sender.addChatMessage(new ChatComponentText(GRAY + "Cleared all of your powerfails."));
+                sender.addChatMessage(new ChatComponentText("Cleared all of your powerfails."));
             }
             case "clear-dim" -> {
                 GTPowerfailTracker.clearPowerfails(player, OptionalInt.of(player.worldObj.provider.dimensionId));
-                sender.addChatMessage(
-                    new ChatComponentText(GRAY + "Cleared all of your powerfails in the current dimension."));
+                sender
+                    .addChatMessage(new ChatComponentText("Cleared all of your powerfails in the current dimension."));
             }
             case "list" -> {
                 final UUID playerId = player.getGameProfile()
@@ -103,18 +102,18 @@ public class GTPowerfailCommand extends CommandBase {
 
                 if (powerfails.isEmpty()) {
                     sender.addChatMessage(new ChatComponentText(""));
-                    sender.addChatMessage(new ChatComponentText(GRAY + "No powerfails have occurred."));
+                    sender.addChatMessage(new ChatComponentText("No powerfails have occurred."));
                     return;
                 }
 
                 sender.addChatMessage(new ChatComponentText(""));
-                sender.addChatMessage(new ChatComponentText(GRAY + "Uncleared powerfails:"));
+                sender.addChatMessage(new ChatComponentText("Uncleared powerfails:"));
 
                 if (powerfails.size() > 25) {
                     // poor bastard :kekw:
 
                     for (GTPowerfailTracker.Powerfail powerfail : powerfails.subList(0, 25)) {
-                        sender.addChatMessage(new ChatComponentText(GRAY + "- " + powerfail.toString()));
+                        sender.addChatMessage(new ChatComponentText("- " + powerfail.toString()));
                     }
 
                     sender
@@ -125,18 +124,23 @@ public class GTPowerfailCommand extends CommandBase {
                     }
                 }
 
-                sender.addChatMessage(new ChatComponentText(GRAY + "Run /powerfails clear to remove all powerfails"));
+                sender.addChatMessage(new ChatComponentText("Run /powerfails clear to remove all powerfails"));
                 sender.addChatMessage(
-                    new ChatComponentText(
-                        GRAY + "Run /powerfails clear-dim to remove all powerfails in your current world"));
+                    new ChatComponentText("Run /powerfails clear-dim to remove all powerfails in your current world"));
             }
             case "show" -> {
                 GTPowerfailTracker.showPowerfails(player);
-                sender.addChatMessage(new ChatComponentText(GRAY + "Enabled powerfail overlay rendering."));
+                sender.addChatMessage(new ChatComponentText("Enabled powerfail overlay rendering."));
             }
             case "hide" -> {
                 GTPowerfailTracker.hidePowerfails(player);
-                sender.addChatMessage(new ChatComponentText(GRAY + "Disabled powerfail overlay rendering."));
+                sender.addChatMessage(new ChatComponentText("Disabled powerfail overlay rendering."));
+            }
+            case "help" -> {
+                printHelp(sender);
+            }
+            default -> {
+                printHelp(sender);
             }
         }
     }
