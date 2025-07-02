07d798905b96881775f964f7eb7f5e1de617dac9
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2025-07-02 19:00:48 +0200

    use loop to init the GTValues#B field

--- src/main/java/gregtech/api/enums/GTValues.java
+++ src/main/java/gregtech/api/enums/GTValues.java
@@ -56,10 +56,12 @@ public class GTValues {
     /**
      * The first 32 Bits
      */
-    @SuppressWarnings("PointlessBitwiseExpression") // Nicer source layout this way
-    public static final int[] B = new int[] { 1 << 0, 1 << 1, 1 << 2, 1 << 3, 1 << 4, 1 << 5, 1 << 6, 1 << 7, 1 << 8,
-        1 << 9, 1 << 10, 1 << 11, 1 << 12, 1 << 13, 1 << 14, 1 << 15, 1 << 16, 1 << 17, 1 << 18, 1 << 19, 1 << 20,
-        1 << 21, 1 << 22, 1 << 23, 1 << 24, 1 << 25, 1 << 26, 1 << 27, 1 << 28, 1 << 29, 1 << 30, 1 << 31 };
+    public static final int[] B;
+
+    static {
+        B = new int[32];
+        for (int i = 0; i < B.length; i++) B[i] = 1 << i;
+    }
 
     /**
      * Renamed from "MATERIAL_UNIT" to just "M"
