f0a659758b6b9a69afbb802fab900b442039b714
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2025-07-10 02:48:20 +0200

    move is real player method to the only place it's used

--- src/main/java/gtPlusPlus/core/util/minecraft/PlayerUtils.java
+++ src/main/java/gtPlusPlus/core/util/minecraft/PlayerUtils.java
@@ -2,8 +2,6 @@ package gtPlusPlus.core.util.minecraft;
 
 import java.util.List;
 
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
 
@@ -12,11 +10,5 @@ public class PlayerUtils {
     public static List<EntityPlayerMP> getOnlinePlayers() {
         return MinecraftServer.getServer()
             .getConfigurationManager().playerEntityList;
-    }
-
-    public static boolean isRealPlayer(EntityLivingBase entity) {
-        return entity instanceof EntityPlayer p && !p.getClass()
-            .getName()
-            .contains("Fake");
     }
 }
