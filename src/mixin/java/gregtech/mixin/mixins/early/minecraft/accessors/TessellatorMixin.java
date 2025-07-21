9fdb00e66da44522b41d9d05d75d3ba61fdff491
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2025-07-21 23:02:32 +0200

    Delete a bunch of reflection (#4565)
    Co-authored-by: boubou19 <miisterunknown@gmail.com>

---
+++ src/mixin/java/gregtech/mixin/mixins/early/minecraft/accessors/TessellatorMixin.java
@@ -0,0 +1,20 @@
+package gregtech.mixin.mixins.early.minecraft.accessors;
+
+import net.minecraft.client.renderer.Tessellator;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import gregtech.mixin.interfaces.accessors.TesselatorAccessor;
+
+@Mixin(Tessellator.class)
+public class TessellatorMixin implements TesselatorAccessor {
+
+    @Shadow
+    private boolean isDrawing;
+
+    @Override
+    public boolean gt5u$isDrawing() {
+        return this.isDrawing;
+    }
+}
