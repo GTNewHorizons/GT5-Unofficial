63884c1892adfe4285225050aacd3dbf58ce9daf
RecursivePineapple <recursive_pineapple@proton.me>
2025-07-26 18:37:10 -0400

    Cache TE sides

--- src/main/java/gregtech/api/metatileentity/BaseTileEntity.java
+++ src/main/java/gregtech/api/metatileentity/BaseTileEntity.java
@@ -43,8 +43,6 @@ import com.gtnewhorizons.modularui.common.widget.SlotGroup;
 import com.gtnewhorizons.modularui.common.widget.SlotWidget;
 import com.gtnewhorizons.modularui.common.widget.TextWidget;
 
-import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.relauncher.Side;
 import gregtech.GTMod;
 import gregtech.api.enums.Dyes;
 import gregtech.api.enums.GTValues;
@@ -96,6 +94,8 @@ public abstract class BaseTileEntity extends TileEntity implements IHasWorldObje
     public boolean isDead = false;
 
     private final ChunkCoordinates mReturnedCoordinates = new ChunkCoordinates();
+
+    private boolean hasSide, isRemote;
 
     public static ForgeDirection getSideForPlayerPlacing(Entity player, ForgeDirection defaultFacing,
         boolean[] aAllowedFacings) {
@@ -164,21 +164,23 @@ public abstract class BaseTileEntity extends TileEntity implements IHasWorldObje
     }
 
     @Override
-    public final boolean isServerSide() {
-        if (worldObj == null) {
-            return FMLCommonHandler.instance()
-                .getEffectiveSide() == Side.SERVER;
+    public void setWorldObj(World worldIn) {
+        super.setWorldObj(worldIn);
+
+        if (worldIn != null) {
+            isRemote = worldIn.isRemote;
+            hasSide = true;
         }
-        return !worldObj.isRemote;
+    }
+
+    @Override
+    public final boolean isServerSide() {
+        return hasSide && !isRemote;
     }
 
     @Override
     public final boolean isClientSide() {
-        if (worldObj == null) {
-            return FMLCommonHandler.instance()
-                .getEffectiveSide() == Side.CLIENT;
-        }
-        return worldObj.isRemote;
+        return hasSide && isRemote;
     }
 
     @Override
