GTNewHorizons/GT5-Unofficial

commit 7bebf70449e1ba0a69bac14fea9545f598c41157

Fix Infuser not Charging with All Available Amps (#4409)

src/main/java/tectech/thing/metaTileEntity/multi/MTEEnergyInfuser.java
@@ -20,6 +20,7 @@ import com.gtnewhorizon.structurelib.structure.IStructureDefinition;
 
 import cofh.api.energy.IEnergyContainerItem;
 import gregtech.api.enums.Materials;
+import gregtech.api.enums.Mods;
 import gregtech.api.enums.SoundResource;
 import gregtech.api.interfaces.metatileentity.IMetaTileEntity;
 import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
@@ -30,7 +31,6 @@ import gregtech.api.util.MultiblockTooltipBuilder;
 import gregtech.common.tileentities.machines.MTEHatchInputBusME;
 import ic2.api.item.ElectricItem;
 import ic2.api.item.IElectricItem;
-import tectech.TecTech;
 import tectech.loader.ConfigHandler;
 import tectech.thing.casing.BlockGTCasingsTT;
 import tectech.thing.casing.TTCasingsContainer;
@@ -89,7 +89,7 @@ public class MTEEnergyInfuser extends TTMultiblockBase implements ISurvivalConst
         if (stack.stackSize == 1) {
             if (item instanceof IElectricItem) {
                 return ElectricItem.manager.getCharge(stack) >= ((IElectricItem) item).getMaxCharge(stack);
-            } else if (TecTech.hasCOFH && item instanceof IEnergyContainerItem) {
+            } else if (Mods.COFHCore.isModLoaded() && item instanceof IEnergyContainerItem) {
                 return ((IEnergyContainerItem) item).getEnergyStored(stack)
                     >= ((IEnergyContainerItem) item).getMaxEnergyStored(stack);
             }
@@ -109,7 +109,12 @@ public class MTEEnergyInfuser extends TTMultiblockBase implements ISurvivalConst
         try {
             double euDiff = item.getMaxCharge(stack) - ElectricItem.manager.getCharge(stack);
             long remove = (long) Math.ceil(
-                ElectricItem.manager.charge(stack, Math.min(euDiff, getEUVar()), item.getTier(stack), true, false));
+                ElectricItem.manager.charge(
+                    stack,
+                    Math.min(euDiff, getAverageInputVoltage() * getMaxInputAmps()),
+                    item.getTier(stack),
+                    true,
+                    false));
             setEUVar(getEUVar() - remove);
             if (getEUVar() < 0) {
                 setEUVar(0);
@@ -213,13 +218,6 @@ public class MTEEnergyInfuser extends TTMultiblockBase implements ISurvivalConst
                             }
                         }
                     }
-                    if (item instanceof IElectricItem) {
-                        doChargeItemStack((IElectricItem) item, itemStackInBus);
-                        return;
-                    } else if (TecTech.hasCOFH && item instanceof IEnergyContainerItem) {
-                        doChargeItemStackRF((IEnergyContainerItem) item, itemStackInBus);
-                        return;
-                    }
                 }
             }
         }
@@ -274,6 +272,29 @@ public class MTEEnergyInfuser extends TTMultiblockBase implements ISurvivalConst
     @Override
     protected SoundResource getActivitySoundLoop() {
         return SoundResource.TECTECH_MACHINES_FX_WHOOUM;
+    }
+
+    @Override
+    public void onPostTick(IGregTechTileEntity aBaseMetaTileEntity, long aTick) {
+        super.onPostTick(aBaseMetaTileEntity, aTick);
+        if (!this.isAllowedToWork()) return;
+        for (MTEHatchInputBus inputBus : mInputBusses) {
+            if (inputBus instanceof MTEHatchInputBusME) continue;
+            for (ItemStack stack : inputBus.mInventory) {
+                if (stack == null || stack.stackSize != 1 || isItemStackFullyCharged(stack)) continue;
+
+                Item item = stack.getItem();
+                if (item == null) continue;
+
+                if (item instanceof IElectricItem) {
+                    doChargeItemStack((IElectricItem) item, stack);
+                    return;
+                } else if (Mods.COFHCore.isModLoaded() && item instanceof IEnergyContainerItem) {
+                    doChargeItemStackRF((IEnergyContainerItem) item, stack);
+                    return;
+                }
+            }
+        }
     }
 
     @Override

src/main/java/tectech/TecTech.java
@@ -13,7 +13,6 @@ import cpw.mods.fml.common.event.FMLLoadCompleteEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import eu.usrv.yamcore.auxiliary.LogHelper;
-import gregtech.api.enums.Mods;
 import gregtech.api.objects.XSTR;
 import tectech.loader.ConfigHandler;
 import tectech.loader.MainLoader;
@@ -59,11 +58,6 @@ public class TecTech {
 
     public static EnderWorldSavedData enderWorldSavedData;
 
-    /**
-     * For Loader.isModLoaded checks during the runtime
-     */
-    public static boolean hasCOFH = false;
-
     public static final byte tectechTexturePage1 = 8;
 
     @Mod.EventHandler
@@ -85,8 +79,6 @@ public class TecTech {
     @Mod.EventHandler
     @SuppressWarnings("unused")
     public void Load(FMLInitializationEvent event) {
-        hasCOFH = Mods.COFHCore.isModLoaded();
-
         MainLoader.load();
     }
 

commit fb9f7c685bc440b884ed7343e254255d42d919cd

Fix Energy Infuser charge items without using power
Regression from 7bebf70449e1ba0a69bac14fea9545f598c41157.

src/main/java/tectech/thing/metaTileEntity/multi/MTEEnergyInfuser.java
@@ -108,13 +108,9 @@ public class MTEEnergyInfuser extends TTMultiblockBase implements ISurvivalConst
     private long doChargeItemStack(IElectricItem item, ItemStack stack) {
         try {
             double euDiff = item.getMaxCharge(stack) - ElectricItem.manager.getCharge(stack);
+            euDiff = Math.min(euDiff, getEUVar());
             long remove = (long) Math.ceil(
-                ElectricItem.manager.charge(
-                    stack,
-                    Math.min(euDiff, getAverageInputVoltage() * getMaxInputAmps()),
-                    item.getTier(stack),
-                    true,
-                    false));
+                ElectricItem.manager.charge(stack, euDiff, item.getTier(stack), true, false));
             setEUVar(getEUVar() - remove);
             if (getEUVar() < 0) {
                 setEUVar(0);