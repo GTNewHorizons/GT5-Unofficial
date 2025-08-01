6809254703357d518084c1bc63a0ede8f9edc1f3
Vlamonster <juliadijkstra97@gmail.com>
2025-08-01 19:20:57 +0200

    Convert Grade5PurifiedWater

--- src/main/java/gregtech/loaders/materials/MaterialsInit1.java
+++ src/main/java/gregtech/loaders/materials/MaterialsInit1.java
@@ -175,9 +175,9 @@ public class MaterialsInit1 {
         Materials.Grade2PurifiedWater = loadGrade2PurifiedWater();
         Materials.Grade3PurifiedWater = loadGrade3PurifiedWater();
         Materials.Grade4PurifiedWater = loadGrade4PurifiedWater();
+        Materials.Grade5PurifiedWater = loadGrade5PurifiedWater();
 
         // spotless:off
-        Materials.Grade5PurifiedWater                = new Materials(   558, TextureSet.SET_FLUID             ,   1.0F,      0,  2,         16             ,   152,   144, 255, 0,   "Grade5PurifiedWater"                ,   "Extreme-Temperature Treated Water (Grade 5)"                      ,    0,      0,         273,    0, false,  true,   2,   1,   1, Dyes.dyeBlue                 , Collections.singletonList(new TCAspects.TC_AspectStack(TCAspects.AQUA, 1))).setHasCorrespondingFluid(true);
         Materials.Grade6PurifiedWater                = new Materials(   559, TextureSet.SET_FLUID             ,   1.0F,      0,  2,         16            ,   168,   161, 255, 0,   "Grade6PurifiedWater"                ,   "Ultraviolet Treated Electrically Neutral Water (Grade 6)"                      ,    0,      0,         273,    0, false,  true,   2,   1,   1, Dyes.dyeBlue                 , Collections.singletonList(new TCAspects.TC_AspectStack(TCAspects.AQUA, 1))).setHasCorrespondingFluid(true);
         Materials.Grade7PurifiedWater                = new Materials(   560, TextureSet.SET_FLUID             ,   1.0F,      0,  2,         16             ,   183,   179, 255, 0,   "Grade7PurifiedWater"                ,   "Degassed Decontaminant-Free Water (Grade 7)"                      ,    0,      0,         273,    0, false,  true,   2,   1,   1, Dyes.dyeBlue                , Collections.singletonList(new TCAspects.TC_AspectStack(TCAspects.AQUA, 1))).setHasCorrespondingFluid(true);
         Materials.Grade8PurifiedWater                = new Materials(   561, TextureSet.SET_FLUID             ,   1.0F,      0,  2,         16             ,   197,   197, 255, 0,   "Grade8PurifiedWater"                ,   "Subatomically Perfect Water (Grade 8)"                      ,    0,      0,         273,    0, false,  true,   2,   1,   1, Dyes.dyeBlue                 , Collections.singletonList(new TCAspects.TC_AspectStack(TCAspects.AQUA, 1))).setHasCorrespondingFluid(true);
@@ -3459,6 +3459,21 @@ public class MaterialsInit1 {
             .setIconSet(TextureSet.SET_FLUID)
             .setColor(Dyes.dyeBlue)
             .setARGB(0x00877eff)
+            .addCell()
+            .setMeltingPoint(273)
+            .setOreValue(2)
+            .addAspect(new TCAspects.TC_AspectStack(TCAspects.AQUA, 1))
+            .constructMaterial()
+            .setHasCorrespondingFluid(true);
+    }
+
+    private static Materials loadGrade5PurifiedWater() {
+        return new MaterialBuilder().setName("Grade5PurifiedWater")
+            .setDefaultLocalName("Extreme-Temperature Treated Water (Grade 5)")
+            .setMetaItemSubID(558)
+            .setIconSet(TextureSet.SET_FLUID)
+            .setColor(Dyes.dyeBlue)
+            .setARGB(0x009890ff)
             .addCell()
             .setMeltingPoint(273)
             .setOreValue(2)
