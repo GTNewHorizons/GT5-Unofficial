644a3b1ff4fecc12199241bc1bb500fd8b6e893f
Vlamonster <juliadijkstra97@gmail.com>
2025-08-01 15:42:17 +0200

    Convert Scandium

--- src/main/java/gregtech/loaders/materials/MaterialsInit1.java
+++ src/main/java/gregtech/loaders/materials/MaterialsInit1.java
@@ -229,9 +229,9 @@ public class MaterialsInit1 {
         Materials.Radon = loadRadon();
         Materials.Rubidium = loadRubidium();
         Materials.Samarium = loadSamarium();
+        Materials.Scandium = loadScandium();
 
         // spotless:off
-        Materials.Scandium                = new Materials(  27, TextureSet.SET_METALLIC          ,   1.0F,      0,  2, 1|2  |8   |32             , 204, 204, 204,   0,   "Scandium"                ,   "Scandium"                      ,    0,       0,       1814, 1814,  true, false,   2,   1,   1, Dyes.dyeYellow      , Element.Sc        , Arrays.asList(new TCAspects.TC_AspectStack(TCAspects.METALLUM, 2), new TCAspects.TC_AspectStack(TCAspects.RADIO, 1)));
         Materials.Silicon                 = new Materials(  20, TextureSet.SET_METALLIC          ,   1.0F,      0,  2, 1|2  |8   |32             ,  60,  60,  80,   0,   "Silicon"                 ,   "Raw Silicon"                   ,    0,       0,       2273, 2273,  true, false,   1,   1,   1, Dyes.dyeBlack       , Element.Si        , Arrays.asList(new TCAspects.TC_AspectStack(TCAspects.METALLUM, 2), new TCAspects.TC_AspectStack(TCAspects.TENEBRAE, 1)));
         Materials.Silver                  = new Materials(  54, TextureSet.SET_SHINY             ,  10.0F,     64,  2, 1|2  |8   |32|64|128      , 220, 220, 255,   0,   "Silver"                  ,   "Silver"                        ,    0,       0,       1234,    0, false, false,   3,   1,   1, Dyes.dyeLightGray   , Element.Ag        , Arrays.asList(new TCAspects.TC_AspectStack(TCAspects.METALLUM, 2), new TCAspects.TC_AspectStack(TCAspects.LUCRUM, 1)));
         Materials.Sodium                  = new Materials(  17, TextureSet.SET_METALLIC          ,   1.0F,      0,  2, 1      |16|32             ,   0,   0, 150,   0,   "Sodium"                  ,   "Sodium"                        ,    0,       0,        370,    0, false, false,   1,   1,   1, Dyes.dyeBlue        , Element.Na        , Arrays.asList(new TCAspects.TC_AspectStack(TCAspects.VITREUS, 2), new TCAspects.TC_AspectStack(TCAspects.LUX, 1)));
@@ -2308,6 +2308,27 @@ public class MaterialsInit1 {
             .addAspect(new TCAspects.TC_AspectStack(TCAspects.METALLUM, 2))
             .addAspect(new TCAspects.TC_AspectStack(TCAspects.RADIO, 1))
             .addAspect(new TCAspects.TC_AspectStack(TCAspects.MAGNETO, 10))
+            .constructMaterial();
+    }
+
+    private static Materials loadScandium() {
+        return new MaterialBuilder().setName("Scandium")
+            .setDefaultLocalName("Scandium")
+            .setElement(Element.Sc)
+            .setMetaItemSubID(27)
+            .setIconSet(TextureSet.SET_METALLIC)
+            .setColor(Dyes.dyeYellow)
+            .setRGB(0xcccccc)
+            .addDustItems()
+            .addMetalItems()
+            .addOreItems()
+            .addPlasma()
+            .setMeltingPoint(1814)
+            .setBlastFurnaceTemp(1814)
+            .setBlastFurnaceRequired(true)
+            .setOreValue(2)
+            .addAspect(new TCAspects.TC_AspectStack(TCAspects.METALLUM, 2))
+            .addAspect(new TCAspects.TC_AspectStack(TCAspects.RADIO, 1))
             .constructMaterial();
     }
 
