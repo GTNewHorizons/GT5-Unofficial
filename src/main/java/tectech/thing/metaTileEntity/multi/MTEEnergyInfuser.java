33495e8d7630c18ab36f836a4fcade5de3da7c72
Vlamonster <juliadijkstra97@gmail.com>
2025-08-04 00:16:19 +0200

    Revert "Bring back mMaxProgresstime to make recipe work"
    This reverts commit 7e871a150eef658e8b1a959418eba611962452a8.

--- src/main/java/tectech/thing/metaTileEntity/multi/MTEEnergyInfuser.java
+++ src/main/java/tectech/thing/metaTileEntity/multi/MTEEnergyInfuser.java
@@ -172,7 +172,6 @@ public class MTEEnergyInfuser extends TTMultiblockBase implements ISurvivalConst
                         return SimpleCheckRecipeResult.ofFailure("insufficient_power_no_val");
                     }
                     mEfficiencyIncrease = 10000;
-                    mMaxProgresstime = 20;
                     return SimpleCheckRecipeResult.ofSuccess("charging");
                 }
             }
