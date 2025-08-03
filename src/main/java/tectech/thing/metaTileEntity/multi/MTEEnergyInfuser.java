ef1772e8551525222e3849683aed67ad71e42c97
Fabian Maurer <dark.shadow4@web.de>
2025-08-03 23:18:00 +0200

    Adjust energy infuser charging speed

--- src/main/java/tectech/thing/metaTileEntity/multi/MTEEnergyInfuser.java
+++ src/main/java/tectech/thing/metaTileEntity/multi/MTEEnergyInfuser.java
@@ -108,8 +108,9 @@ public class MTEEnergyInfuser extends TTMultiblockBase implements ISurvivalConst
     private long doChargeItemStack(IElectricItem item, ItemStack stack) {
         try {
             double euDiff = item.getMaxCharge(stack) - ElectricItem.manager.getCharge(stack);
-            long remove = (long) Math.ceil(
-                ElectricItem.manager.charge(stack, Math.min(euDiff, getEUVar()), item.getTier(stack), true, false));
+            euDiff = Math.min(euDiff, Math.min(getEUVar(), getAverageInputVoltage() * getMaxInputAmps()));
+            long remove = (long) Math
+                .ceil(ElectricItem.manager.charge(stack, euDiff, item.getTier(stack), true, false));
             setEUVar(getEUVar() - remove);
             if (getEUVar() < 0) {
                 setEUVar(0);
