2057f343dd44b0120724f27bf8eb384f7d466bae
Vlamonster <juliadijkstra97@gmail.com>
2025-08-08 11:45:03 +0200

    Convert InfusedOrder

--- src/main/java/gregtech/loaders/materials/MaterialsInit1.java
+++ src/main/java/gregtech/loaders/materials/MaterialsInit1.java
@@ -255,9 +255,9 @@ public class MaterialsInit1 {
         Materials.InfusedEarth = loadInfusedEarth();
         Materials.InfusedWater = loadInfusedWater();
         Materials.InfusedEntropy = loadInfusedEntropy();
+        Materials.InfusedOrder = loadInfusedOrder();
 
         // spotless:off
-        Materials.InfusedOrder            = new Materials( 545, TextureSet.SET_SHARDS            ,   8.0F,     64,  3, 1  |4|8      |64|128      , 252, 252, 252,   0,   "InfusedOrder"            ,   "Ordo"                          ,    5,     240,         -1,    0, false,  true,   3,   1,   1, Dyes.dyeWhite       , Arrays.asList(new TCAspects.TC_AspectStack(TCAspects.PRAECANTATIO, 1), new TCAspects.TC_AspectStack(TCAspects.ORDO, 2)));
         Materials.InfusedVis              = new Materials(  -1, TextureSet.SET_SHARDS            ,   8.0F,     64,  3, 1  |4|8      |64|128      , 255,   0, 255,   0,   "InfusedVis"              ,   "Auram"                         ,    5,     240,         -1,    0, false,  true,   3,   1,   1, Dyes.dyePurple      , Arrays.asList(new TCAspects.TC_AspectStack(TCAspects.PRAECANTATIO, 1), new TCAspects.TC_AspectStack(TCAspects.AURAM, 2)));
         Materials.InfusedDull             = new Materials(  -1, TextureSet.SET_SHARDS            ,  32.0F,     64,  3, 1  |4|8      |64|128      , 100, 100, 100,   0,   "InfusedDull"             ,   "Vacuus"                        ,    5,     160,         -1,    0, false,  true,   3,   1,   1, Dyes.dyeLightGray   , Arrays.asList(new TCAspects.TC_AspectStack(TCAspects.PRAECANTATIO, 1), new TCAspects.TC_AspectStack(TCAspects.VACUOS, 2)));
         Materials.Inolashite              = new Materials( 954, TextureSet.SET_NONE              ,   8.0F,   2304,  3, 1|2  |8      |64          , 148, 216, 187,   0,   "Inolashite"              ,   "Inolashite"                    ,    0,       0,         -1,    0, false, false,   1,   1,   1, Dyes.dyeGreen       );
@@ -4357,6 +4357,29 @@ public class MaterialsInit1 {
             .setFuelPower(320)
             .addAspect(new TCAspects.TC_AspectStack(TCAspects.PRAECANTATIO, 1))
             .addAspect(new TCAspects.TC_AspectStack(TCAspects.PERDITIO, 2))
+            .constructMaterial();
+    }
+
+    private static Materials loadInfusedOrder() {
+        return new MaterialBuilder().setName("InfusedOrder")
+            .setDefaultLocalName("Ordo")
+            .setMetaItemSubID(545)
+            .setIconSet(TextureSet.SET_SHARDS)
+            .setColor(Dyes.dyeWhite)
+            .setARGB(0x00fcfcfc)
+            .setToolSpeed(8.0f)
+            .setDurability(64)
+            .setToolQuality(3)
+            .addDustItems()
+            .addGemItems()
+            .addOreItems()
+            .addToolHeadItems()
+            .addGearItems()
+            .setOreValue(3)
+            .setFuelType(5)
+            .setFuelPower(240)
+            .addAspect(new TCAspects.TC_AspectStack(TCAspects.PRAECANTATIO, 1))
+            .addAspect(new TCAspects.TC_AspectStack(TCAspects.ORDO, 2))
             .constructMaterial();
     }
 
