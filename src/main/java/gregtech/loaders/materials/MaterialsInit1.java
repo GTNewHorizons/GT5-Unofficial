2607095edd388b189c7c55ede63fddc167945e87
Vlamonster <juliadijkstra97@gmail.com>
2025-08-08 11:39:41 +0200

    Convert InfusedAir

--- src/main/java/gregtech/loaders/materials/MaterialsInit1.java
+++ src/main/java/gregtech/loaders/materials/MaterialsInit1.java
@@ -250,9 +250,9 @@ public class MaterialsInit1 {
         Materials.Infernal = loadInfernal();
         Materials.Infuscolium = loadInfuscolium();
         Materials.InfusedGold = loadInfusedGold();
+        Materials.InfusedAir = loadInfusedAir();
 
         // spotless:off
-        Materials.InfusedAir              = new Materials( 540, TextureSet.SET_SHARDS            ,   8.0F,     64,  3, 1  |4|8      |64|128      , 255, 255,   0,   0,   "InfusedAir"              ,   "Aer"                           ,    5,     160,         -1,    0, false,  true,   3,   1,   1, Dyes.dyeYellow      , Arrays.asList(new TCAspects.TC_AspectStack(TCAspects.PRAECANTATIO, 1), new TCAspects.TC_AspectStack(TCAspects.AER, 2)));
         Materials.InfusedFire             = new Materials( 541, TextureSet.SET_SHARDS            ,   8.0F,     64,  3, 1  |4|8      |64|128      , 255,   0,   0,   0,   "InfusedFire"             ,   "Ignis"                         ,    5,     320,         -1,    0, false,  true,   3,   1,   1, Dyes.dyeRed         , Arrays.asList(new TCAspects.TC_AspectStack(TCAspects.PRAECANTATIO, 1), new TCAspects.TC_AspectStack(TCAspects.IGNIS, 2)));
         Materials.InfusedEarth            = new Materials( 542, TextureSet.SET_SHARDS            ,   8.0F,    256,  3, 1  |4|8      |64|128      ,   0, 255,   0,   0,   "InfusedEarth"            ,   "Terra"                         ,    5,     160,         -1,    0, false,  true,   3,   1,   1, Dyes.dyeGreen       , Arrays.asList(new TCAspects.TC_AspectStack(TCAspects.PRAECANTATIO, 1), new TCAspects.TC_AspectStack(TCAspects.TERRA, 2)));
         Materials.InfusedWater            = new Materials( 543, TextureSet.SET_SHARDS            ,   8.0F,     64,  3, 1  |4|8      |64|128      ,   0,   0, 255,   0,   "InfusedWater"            ,   "Aqua"                          ,    5,     160,         -1,    0, false,  true,   3,   1,   1, Dyes.dyeBlue        , Arrays.asList(new TCAspects.TC_AspectStack(TCAspects.PRAECANTATIO, 1), new TCAspects.TC_AspectStack(TCAspects.AQUA, 2)));
@@ -4241,6 +4241,30 @@ public class MaterialsInit1 {
             .addToolHeadItems()
             .addGearItems()
             .setOreValue(3)
+            .constructMaterial();
+    }
+
+    private static Materials loadInfusedAir() {
+        return new MaterialBuilder().setName("InfusedAir")
+            .setDefaultLocalName("Aer")
+            .setMetaItemSubID(540)
+            .setIconSet(TextureSet.SET_SHARDS)
+            .setColor(Dyes.dyeYellow)
+            .setARGB(0x00ffff00)
+            .setToolSpeed(8.0f)
+            .setDurability(64)
+            .setToolQuality(3)
+            .addDustItems()
+            .addGemItems()
+            .addOreItems()
+            .addToolHeadItems()
+            .addGearItems()
+            .setMeltingPoint(-1)
+            .setOreValue(3)
+            .setFuelType(5)
+            .setFuelPower(160)
+            .addAspect(new TCAspects.TC_AspectStack(TCAspects.PRAECANTATIO, 1))
+            .addAspect(new TCAspects.TC_AspectStack(TCAspects.AER, 2))
             .constructMaterial();
     }
 
