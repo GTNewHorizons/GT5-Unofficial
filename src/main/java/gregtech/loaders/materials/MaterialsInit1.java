360168696076fd74d9556dc932efeb8bf17dfd25
Vlamonster <juliadijkstra97@gmail.com>
2025-08-13 16:11:34 +0200

    Convert Randomite

--- src/main/java/gregtech/loaders/materials/MaterialsInit1.java
+++ src/main/java/gregtech/loaders/materials/MaterialsInit1.java
@@ -297,9 +297,9 @@ public class MaterialsInit1 {
         Materials.Phoenixite = loadPhoenixite();
         Materials.Prometheum = loadPrometheum();
         Materials.Quartzite = loadQuartzite();
+        Materials.Randomite = loadRandomite();
 
         // spotless:off
-        Materials.Randomite               = new Materials(  -1, TextureSet.SET_NONE              ,   1.0F,      0,  1, 1    |8                   , 255, 255, 255,   0,   "Randomite"               ,   "Randomite"                     ,    0,       0,         -1,    0, false, false,   1,   1,   1, Dyes._NULL          );
         Materials.Rhyolite                = new Materials( 875, TextureSet.SET_DULL              ,   1.0F,      0,  1, 1                         , 255, 255, 255,   0,   "Rhyolite"                ,   "Rhyolite"                      ,    0,       0,         -1,    0, false, false,   1,   1,   1, Dyes._NULL          );
         Materials.Rubracium               = new Materials( 488, TextureSet.SET_METALLIC          ,   1.0F,    128,  1, 1|2  |8      |64|128      , 151,  45,  45,   0,   "Rubracium"               ,   "Rubracium"                     ,    0,       0,         -1,    0, false, false,   1,   1,   1, Dyes.dyeRed         );
         Materials.Sand                    = new Materials(  -1, TextureSet.SET_NONE              ,   1.0F,      0,  1, 0                         , 255, 255, 255,   0,   "Sand"                    ,   "Sand"                          ,    0,       0,         -1,    0, false, false,   1,   1,   1, Dyes.dyeYellow      );
@@ -4813,6 +4813,14 @@ public class MaterialsInit1 {
             .setRGB(0xd2e6d2)
             .addDustItems()
             .addGemItems()
+            .addOreItems()
+            .constructMaterial();
+    }
+
+    private static Materials loadRandomite() {
+        return new MaterialBuilder().setName("Randomite")
+            .setDefaultLocalName("Randomite")
+            .addDustItems()
             .addOreItems()
             .constructMaterial();
     }
