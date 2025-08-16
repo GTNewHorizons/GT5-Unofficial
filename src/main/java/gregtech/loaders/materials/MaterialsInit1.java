18fb7d09de0034dc32383c4efd24106fbca54892
Vlamonster <juliadijkstra97@gmail.com>
2025-08-16 02:03:25 +0200

    Convert Quicklime

--- src/main/java/gregtech/loaders/materials/MaterialsInit1.java
+++ src/main/java/gregtech/loaders/materials/MaterialsInit1.java
@@ -204,9 +204,9 @@ public class MaterialsInit1 {
         Materials.CupricOxide = loadCupricOxide();
         Materials.Ferrosilite = loadFerrosilite();
         Materials.Magnesia = loadMagnesia();
+        Materials.Quicklime = loadQuicklime();
 
         // spotless:off
-        Materials.Quicklime                 = new MaterialBuilder(622, TextureSet.SET_DULL       ,                                                                                                     "Quicklime").addDustItems().setRGB(240, 240, 240).setColor(Dyes.dyeWhite).setMaterialList(new MaterialStack(Calcium, 1), new MaterialStack(Oxygen, 1)).addElectrolyzerRecipe().constructMaterial();
         Materials.Potash                    = new MaterialBuilder(623, TextureSet.SET_DULL       ,                                                                                                     "Potash").addDustItems().setRGB(120, 66, 55).setColor(Dyes.dyeBrown).setMaterialList(new MaterialStack(Potassium, 2), new MaterialStack(Oxygen, 1)).addElectrolyzerRecipe().constructMaterial();
         Materials.SodaAsh                   = new MaterialBuilder(624, TextureSet.SET_DULL       ,                                                                                                     "Soda Ash").addDustItems().setRGB(220, 220, 255).setColor(Dyes.dyeWhite).setMaterialList(new MaterialStack(Sodium, 2), new MaterialStack(Carbon, 1), new MaterialStack(Oxygen, 3)).addElectrolyzerRecipe().constructMaterial();
         Materials.BioDiesel                 = new MaterialBuilder(627, TextureSet.SET_FLUID      ,                                                                                                     "Bio Diesel").addCell().addFluid().setRGB(255, 128, 0).setColor(Dyes.dyeOrange).setFuelType(MaterialBuilder.DIESEL).setFuelPower(320).constructMaterial();
@@ -7599,6 +7599,15 @@ public class MaterialsInit1 {
             .setRGB(255, 225, 225)
             .setColor(Dyes.dyeWhite)
             .setMaterialList(new MaterialStack(Magnesium, 1), new MaterialStack(Oxygen, 1))
+            .constructMaterial();
+    }
+
+    private static Materials loadQuicklime() {
+        return new MaterialBuilder(622, TextureSet.SET_DULL, "Quicklime").addDustItems()
+            .setRGB(240, 240, 240)
+            .setColor(Dyes.dyeWhite)
+            .setMaterialList(new MaterialStack(Calcium, 1), new MaterialStack(Oxygen, 1))
+            .addElectrolyzerRecipe()
             .constructMaterial();
     }
 
