6ed7a79a57012c14901d571553434d703f2f4695
Vlamonster <juliadijkstra97@gmail.com>
2025-08-24 13:56:58 +0200

    Convert StyreneButadieneRubber

--- src/main/java/gregtech/loaders/materials/MaterialsInit1.java
+++ src/main/java/gregtech/loaders/materials/MaterialsInit1.java
@@ -224,9 +224,9 @@ public class MaterialsInit1 {
         Materials.Butene = loadButene();
         Materials.Butadiene = loadButadiene();
         Materials.RawStyreneButadieneRubber = loadRawStyreneButadieneRubber();
+        Materials.StyreneButadieneRubber = loadStyreneButadieneRubber();
 
         // spotless:off
-        Materials.StyreneButadieneRubber    = new MaterialBuilder(635, TextureSet.SET_SHINY      ,                                                                                                     "Styrene-Butadiene Rubber").addDustItems().addMetalItems().addToolHeadItems().addGearItems().setToolSpeed(3.0f).setDurability(128).setToolQuality(1).setRGB(33, 26, 24).setColor(Dyes.dyeBlack).setMaterialList(new MaterialStack(Styrene, 1), new MaterialStack(Butadiene, 3)).constructMaterial();
         Materials.Toluene                   = new MaterialBuilder(647, TextureSet.SET_FLUID      ,                                                                                                     "Toluene").addCell().setRGB(80, 29, 5).setColor(Dyes.dyeBrown).setFuelType(MaterialBuilder.GAS).setFuelPower(328).setMaterialList(new MaterialStack(Carbon, 7), new MaterialStack(Hydrogen, 8)).addElectrolyzerRecipe().constructMaterial();
         Materials.Epichlorohydrin           = new MaterialBuilder(648, TextureSet.SET_FLUID      ,                                                                                                     "Epichlorohydrin").addCell().setRGB(80, 29, 5).setColor(Dyes.dyeBrown).setMaterialList(new MaterialStack(Carbon, 3), new MaterialStack(Hydrogen, 5), new MaterialStack(Chlorine, 1), new MaterialStack(Oxygen, 1)).addElectrolyzerRecipe().constructMaterial();
         Materials.PolyvinylChloride         = new MaterialBuilder(649, TextureSet.SET_DULL       ,                                                                                                     "Polyvinyl Chloride").addDustItems().addMetalItems().addToolHeadItems().addGearItems().setToolSpeed(3.0f).setDurability(32).setToolQuality(1).setRGB(215, 230, 230).setColor(Dyes.dyeLightGray).setMaterialList(new MaterialStack(Carbon, 2), new MaterialStack(Hydrogen, 3), new MaterialStack(Chlorine, 1)).constructMaterial();
@@ -7820,6 +7820,20 @@ public class MaterialsInit1 {
         return new MaterialBuilder(634, TextureSet.SET_SHINY, "Raw Styrene-Butadiene Rubber").addDustItems()
             .setRGB(84, 64, 61)
             .setColor(Dyes.dyeGray)
+            .setMaterialList(new MaterialStack(Styrene, 1), new MaterialStack(Butadiene, 3))
+            .constructMaterial();
+    }
+
+    private static Materials loadStyreneButadieneRubber() {
+        return new MaterialBuilder(635, TextureSet.SET_SHINY, "Styrene-Butadiene Rubber").addDustItems()
+            .addMetalItems()
+            .addToolHeadItems()
+            .addGearItems()
+            .setToolSpeed(3.0f)
+            .setDurability(128)
+            .setToolQuality(1)
+            .setRGB(33, 26, 24)
+            .setColor(Dyes.dyeBlack)
             .setMaterialList(new MaterialStack(Styrene, 1), new MaterialStack(Butadiene, 3))
             .constructMaterial();
     }
