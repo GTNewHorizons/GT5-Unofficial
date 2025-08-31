6b2d119f1d47a2da88493edf2a967c278c108a86
Vlamonster <juliadijkstra97@gmail.com>
2025-08-31 12:25:57 +0200

    Replace deprecated `MaterialBuilder` in `MaterialsOreAlum.java`

--- src/main/java/gregtech/api/enums/MaterialsOreAlum.java
+++ src/main/java/gregtech/api/enums/MaterialsOreAlum.java
@@ -25,7 +25,10 @@ public class MaterialsOreAlum {
     }
 
     private static Materials loadBauxiteSlurry() {
-        return new MaterialBuilder(409, TextureSet.SET_FLUID, "Bauxite Slurry").setName("BauxiteSlurry")
+        return new MaterialBuilder().setName("BauxiteSlurry")
+            .setDefaultLocalName("Bauxite Slurry")
+            .setMetaItemSubID(409)
+            .setIconSet(TextureSet.SET_FLUID)
             .addCell()
             .addFluid()
             .setRGB(0x2543a8)
@@ -35,7 +38,10 @@ public class MaterialsOreAlum {
     }
 
     private static Materials loadHeatedBauxiteSlurry() {
-        return new MaterialBuilder(410, TextureSet.SET_FLUID, "Heated Bauxite Slurry").setName("HeadedBauxiteSlurry")
+        return new MaterialBuilder().setName("HeadedBauxiteSlurry")
+            .setDefaultLocalName("Heated Bauxite Slurry")
+            .setMetaItemSubID(410)
+            .setIconSet(TextureSet.SET_FLUID)
             .addCell()
             .addFluid()
             .setRGB(0x375cd4)
@@ -45,7 +51,10 @@ public class MaterialsOreAlum {
     }
 
     private static Materials loadSluiceJuice() {
-        return new MaterialBuilder(411, TextureSet.SET_FLUID, "Sluice Juice").setName("SluiceJuice")
+        return new MaterialBuilder().setName("SluiceJuice")
+            .setDefaultLocalName("Sluice Juice")
+            .setMetaItemSubID(411)
+            .setIconSet(TextureSet.SET_FLUID)
             .addCell()
             .addFluid()
             .setRGB(0x5c3c24)
@@ -55,7 +64,10 @@ public class MaterialsOreAlum {
     }
 
     private static Materials loadSluiceSand() {
-        return new MaterialBuilder(412, TextureSet.SET_FINE, "Sluice Sand").setName("SluiceSand")
+        return new MaterialBuilder().setName("SluiceSand")
+            .setDefaultLocalName("Sluice Sand")
+            .setMetaItemSubID(412)
+            .setIconSet(TextureSet.SET_FINE)
             .addDustItems()
             .setRGB(0xa5a578)
             .setColor(Dyes.dyeGray)
@@ -63,7 +75,10 @@ public class MaterialsOreAlum {
     }
 
     private static Materials loadBauxiteSlag() {
-        return new MaterialBuilder(413, TextureSet.SET_FINE, "Bauxite Slag").setName("BauxiteSlag")
+        return new MaterialBuilder().setName("BauxiteSlag")
+            .setDefaultLocalName("Bauxite Slag")
+            .setMetaItemSubID(413)
+            .setIconSet(TextureSet.SET_FINE)
             .addDustItems()
             .setRGB(0x6e1f1f)
             .setColor(Dyes.dyeRed)
@@ -71,7 +86,10 @@ public class MaterialsOreAlum {
     }
 
     private static Materials loadIlmeniteSlag() {
-        return new MaterialBuilder(414, TextureSet.SET_FINE, "Ilmenite Slag").setName("IlmeniteSlag")
+        return new MaterialBuilder().setName("IlmeniteSlag")
+            .setDefaultLocalName("Ilmenite Slag")
+            .setMetaItemSubID(414)
+            .setIconSet(TextureSet.SET_FINE)
             .addDustItems()
             .setRGB(0xa32626)
             .setColor(Dyes.dyeBrown)
@@ -79,7 +97,10 @@ public class MaterialsOreAlum {
     }
 
     private static Materials loadGreenSapphireJuice() {
-        return new MaterialBuilder(415, TextureSet.SET_FLUID, "Green Sapphire Juice").setName("GreenSapphireJuice")
+        return new MaterialBuilder().setName("GreenSapphireJuice")
+            .setDefaultLocalName("Green Sapphire Juice")
+            .setMetaItemSubID(415)
+            .setIconSet(TextureSet.SET_FLUID)
             .addCell()
             .addFluid()
             .setRGB(0x64c882)
@@ -88,7 +109,10 @@ public class MaterialsOreAlum {
     }
 
     private static Materials loadSapphireJuice() {
-        return new MaterialBuilder(416, TextureSet.SET_FLUID, "Sapphire Juice").setName("SapphireJuice")
+        return new MaterialBuilder().setName("SapphireJuice")
+            .setDefaultLocalName("Sapphire Juice")
+            .setMetaItemSubID(416)
+            .setIconSet(TextureSet.SET_FLUID)
             .addCell()
             .addFluid()
             .setRGB(0x6464c8)
@@ -97,7 +121,10 @@ public class MaterialsOreAlum {
     }
 
     private static Materials loadRubyJuice() {
-        return new MaterialBuilder(417, TextureSet.SET_FLUID, "Ruby Juice").setName("RubyJuice")
+        return new MaterialBuilder().setName("RubyJuice")
+            .setDefaultLocalName("Ruby Juice")
+            .setMetaItemSubID(417)
+            .setIconSet(TextureSet.SET_FLUID)
             .addCell()
             .addFluid()
             .setRGB(0xff6464)
