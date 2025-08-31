6903a0c840c4375879af5c16f457c014b0b09b45
chrombread <chrombread8@gmail.com>
2025-08-31 15:31:46 -0700

    change structure check order

--- src/main/java/gregtech/common/tileentities/machines/multi/MTEMultiSolidifier.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/MTEMultiSolidifier.java
@@ -106,7 +106,8 @@ public class MTEMultiSolidifier extends MTEExtendedPowerMultiBlockBase<MTEMultiS
         .addShape(
             STRUCTURE_PIECE_MAIN,
             (transpose(
-                new String[][] { { "       ", "BBBBBBB", "BBBBBBB", "BBBBBBB", "       " },
+                new String[][] {
+                    { "       ", "BBBBBBB", "BBBBBBB", "BBBBBBB", "       " },
                     { "BBBBBBB", "       ", "D D D D", "       ", "BBBBBBB" },
                     { "AAAAAAA", "       ", "       ", "       ", "AAAAAAA" },
                     { "CCCBCCC", "       ", "F F F F", "       ", "CCCCCCC" },
@@ -259,7 +260,7 @@ public class MTEMultiSolidifier extends MTEExtendedPowerMultiBlockBase<MTEMultiS
 
         if (checkPiece(STRUCTURE_PIECE_MAIN, 3, 4, 0)) {
             while (width < (6)) {
-                if (checkPiece(MS_RIGHT_MID, -4 - 2 * width, 4, 0) && checkPiece(MS_LEFT_MID, 5 + 2 * width, 4, 0)) {
+                if (checkPiece(MS_LEFT_MID, 5 + 2 * width, 4, 0) && checkPiece(MS_RIGHT_MID, -4 - 2 * width, 4, 0)) {
                     width++;
                 } else break;
             }
