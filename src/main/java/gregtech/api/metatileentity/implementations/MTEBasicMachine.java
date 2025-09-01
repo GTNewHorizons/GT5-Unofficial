f940ea44cbe817b20b3f8a38485779ae2edc97e5
PlayfulPiano <pianoboy8lp@gmail.com>
2025-09-01 16:10:58 -0400

    Hotfix - Fix SoundLoop continuing past Singleblock Powerfail
    Thanks heavily to @Miklebe and @sisyphussy for helping out in discord.

--- src/main/java/gregtech/api/metatileentity/implementations/MTEBasicMachine.java
+++ src/main/java/gregtech/api/metatileentity/implementations/MTEBasicMachine.java
@@ -545,7 +545,7 @@ public abstract class MTEBasicMachine extends MTEBasicTank implements RecipeMapW
 
             if (isActive && (mProgresstime >= 0 || aBaseMetaTileEntity.isAllowedToWork())) {
                 markDirty();
-                aBaseMetaTileEntity.setActive(true);
+                if (mProgresstime > 0) aBaseMetaTileEntity.setActive(true);
                 if (mProgresstime < 0 || drainEnergyForProcess(mEUt)) {
                     if (++mProgresstime >= mMaxProgresstime) {
                         for (int i = 0; i < mOutputItems.length; i++)
@@ -830,6 +830,7 @@ public abstract class MTEBasicMachine extends MTEBasicTank implements RecipeMapW
      */
     public void stutterProcess() {
         if (useStandardStutterSound()) sendSound((byte) 8);
+        aBaseMetaTileEntity.setActive(false);
     }
 
     /**
