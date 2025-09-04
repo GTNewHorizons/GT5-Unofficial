5ba155b2a6e15cfb171b82003b3019fef1db5ce6
SST-03 <70825063+SST-03@users.noreply.github.com>
2025-09-04 14:42:48 +0800

    Update GT5OreLayerHelper.java

--- src/main/java/gtneioreplugin/util/GT5OreLayerHelper.java
+++ src/main/java/gtneioreplugin/util/GT5OreLayerHelper.java
@@ -10,6 +10,7 @@ import net.minecraft.item.ItemStack;
 import gregtech.api.GregTechAPI;
 import gregtech.api.enums.Materials;
 import gregtech.api.enums.OreMixes;
+import gregtech.api.interfaces.ISubTagContainer;
 import gregtech.common.OreMixBuilder;
 
 public class GT5OreLayerHelper {
@@ -38,6 +39,9 @@ public class GT5OreLayerHelper {
     public static void init() {
         for (OreMixes mix : OreMixes.values())
             mapOreLayerWrapper.put(mix.oreMixBuilder.oreMixName, new OreLayerWrapper(mix.oreMixBuilder));
+        for (OreLayerWrapperBW layer : BWOreLayer.NEIMAP) {
+            mapOreLayerWrapper.put(layer.veinName, layer);
+        }
         for (OreLayerWrapper layer : mapOreLayerWrapper.values()) {
             bufferedDims.put(layer, DimensionHelper.getDims(layer));
         }
@@ -70,10 +74,10 @@ public class GT5OreLayerHelper {
         public final short randomWeight, size, density;
         public final Map<String, Boolean> allowedDimWithOrigNames;
 
-        public final Materials mPrimaryVeinMaterial;
-        public final Materials mSecondaryMaterial;
-        public final Materials mBetweenMaterial;
-        public final Materials mSporadicMaterial;
+        public final ISubTagContainer mPrimaryVeinMaterial;
+        public final ISubTagContainer mSecondaryMaterial;
+        public final ISubTagContainer mBetweenMaterial;
+        public final ISubTagContainer mSporadicMaterial;
 
         public OreLayerWrapper(OreMixBuilder mix) {
             this.veinName = mix.oreMixName;
@@ -113,6 +117,76 @@ public class GT5OreLayerHelper {
                 || Meta[OreVeinLayer.VEIN_SECONDARY] == materialIndex
                 || Meta[OreVeinLayer.VEIN_BETWEEN] == materialIndex
                 || Meta[OreVeinLayer.VEIN_SPORADIC] == materialIndex;
+        }
+    }
+
+    public static class OreLayerWrapperBW extends OreLayerWrapper {
+        public byte bwOres;
+        public OreLayerWrapperBW(string veinName, ISubTagContainer primary, ISubTagContainer secondary, ISubTagContainer between, ISubTagContainer sporadic, BWOreLayer layer) {
+            this.veinName = mix.oreMixName;
+            this.localizedName = mix.localizedName;
+            this.Meta[0] = getMeta(primary);
+            this.Meta[1] = getMeta(secondary);
+            this.Meta[2] = getMeta(between);
+            this.Meta[3] = getMeta(sporadic);
+            setBwOres(primary, secondary, between, sporadic)
+
+            this.mPrimaryVeinMaterial = mix.primary;
+            this.mSecondaryMaterial = mix.secondary;
+            this.mBetweenMaterial = mix.between;
+            this.mSporadicMaterial = mix.sporadic;
+
+            this.size = (short) layer.mSize;
+            this.density = (short) layer.mDensity;
+            this.worldGenHeightRange = layer.mMinY + "-" + layer.mMaxY;
+            this.randomWeight = (short) layer.mWeight;
+
+            this.allowedDimWithOrigNames = new HashMap<>();
+        }
+
+        private short getMeta(ISubTagContainer material) {
+            if (material instanceof Materials) return (short) ((Materials) material).mMetaItemSubID;
+            else if (material instanceof Werkstoff) return (short) ((Werkstoff) material).getmID();
+            else return 0;
+        }
+
+        private void setBwOres(ISubTagContainer top, ISubTagContainer bottom, ISubTagContainer between, ISubTagContainer sprinkled) {
+            if (top instanceof Werkstoff) this.bwOres = (byte) (this.bwOres | 0b1000);
+            if (bottom instanceof Werkstoff) this.bwOres = (byte) (this.bwOres | 0b0100);
+            if (between instanceof Werkstoff) this.bwOres = (byte) (this.bwOres | 0b0010);
+            if (sprinkled instanceof Werkstoff) this.bwOres = (byte) (this.bwOres | 0b0001);
+        }
+
+        public void addDimension(DimensionDef dim) {
+            this.allowedDimWithOrigNames.put(dim.modDimensionDef.getDimensionName(), true);
+        }
+
+        @Override
+        public boolean containsOre(short materialIndex) {
+            return ((this.bwOres & 0b1000) == 0 && Meta[OreVeinLayer.VEIN_PRIMARY] == materialIndex)
+                || ((this.bwOres & 0b0100) == 0 && Meta[OreVeinLayer.VEIN_SECONDARY] == materialIndex)
+                || ((this.bwOres & 0b0010) == 0 && Meta[OreVeinLayer.VEIN_BETWEEN] == materialIndex)
+                || ((this.bwOres & 0b0001) == 0 && Meta[OreVeinLayer.VEIN_SPORADIC] == materialIndex);
+        }
+
+        public boolean containsOreBW(short materialIndex) {
+            return ((this.bwOres & 0b1000) != 0 && Meta[OreVeinLayer.VEIN_PRIMARY] == materialIndex)
+                || ((this.bwOres & 0b0100) != 0 && Meta[OreVeinLayer.VEIN_SECONDARY] == materialIndex)
+                || ((this.bwOres & 0b0010) != 0 && Meta[OreVeinLayer.VEIN_BETWEEN] == materialIndex)
+                || ((this.bwOres & 0b0001) != 0 && Meta[OreVeinLayer.VEIN_SPORADIC] == materialIndex);
+        }
+
+        @Override
+        public List<ItemStack> getVeinLayerOre(int _unused, int veinLayer) {
+            List<ItemStack> stackList = new ArrayList<>();
+            stackList.add(getLayerOre(veinLayer, 0));
+            return stackList;
+        }
+        
+        @Override
+        public ItemStack getLayerOre(int veinLayer, int _unused) {
+            List<ItemStack> List = layer.getStacks();
+            return List.get(veinLayer);
         }
     }
 }
