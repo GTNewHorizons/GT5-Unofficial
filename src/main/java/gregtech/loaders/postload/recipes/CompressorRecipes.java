24ac4fae5a9f5130b26c8bc3543a876afbb09d72
JustACasualDay <samuel.kaltenegger@gmail.com>
2025-09-20 20:06:15 +0200

    added Red and Blackgranite Block Recipes

--- src/main/java/gregtech/loaders/postload/recipes/CompressorRecipes.java
+++ src/main/java/gregtech/loaders/postload/recipes/CompressorRecipes.java
@@ -31,6 +31,20 @@ public class CompressorRecipes implements Runnable {
     public void run() {
 
         GTValues.RA.stdBuilder()
+            .itemInputs(GTOreDictUnificator.get(OrePrefixes.dust, Materials.GraniteBlack, 4))
+            .itemOutputs(GTOreDictUnificator.get(OrePrefixes.block, Materials.GraniteBlack, 3))
+            .duration(5 * SECONDS)
+            .eut(2)
+            .addTo(compressorRecipes);
+
+        GTValues.RA.stdBuilder()
+            .itemInputs(GTOreDictUnificator.get(OrePrefixes.dust, Materials.GraniteRed, 4))
+            .itemOutputs(GTOreDictUnificator.get(OrePrefixes.block, Materials.GraniteRed, 3))
+            .duration(5 * SECONDS)
+            .eut(2)
+            .addTo(compressorRecipes);
+
+        GTValues.RA.stdBuilder()
             .itemInputs(GTOreDictUnificator.get(OrePrefixes.dust, Materials.Netherrack, 4))
             .itemOutputs(GTOreDictUnificator.get(new ItemStack(Blocks.netherrack, 3)))
             .duration(5 * SECONDS)
