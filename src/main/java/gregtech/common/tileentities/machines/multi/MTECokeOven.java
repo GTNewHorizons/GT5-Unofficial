4bac09e4177a480c4dca6ce302a9a5ebaa7aee12
Vlamonster <juliadijkstra97@gmail.com>
2025-09-22 17:28:41 +0200

    Create blank extension of `MTEEnhancedMultiBlockBase`

--- src/main/java/gregtech/common/tileentities/machines/multi/MTECokeOven.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/MTECokeOven.java
@@ -1,4 +1,77 @@
 package gregtech.common.tileentities.machines.multi;
 
-public class MTECokeOven {
+import static com.gtnewhorizon.structurelib.structure.StructureUtility.transpose;
+
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.util.ForgeDirection;
+
+import com.gtnewhorizon.structurelib.structure.IStructureDefinition;
+import com.gtnewhorizon.structurelib.structure.StructureDefinition;
+
+import gregtech.api.interfaces.ITexture;
+import gregtech.api.interfaces.metatileentity.IMetaTileEntity;
+import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
+import gregtech.api.metatileentity.implementations.MTEEnhancedMultiBlockBase;
+import gregtech.api.util.MultiblockTooltipBuilder;
+
+public class MTECokeOven extends MTEEnhancedMultiBlockBase<MTECokeOven> {
+
+    public MTECokeOven(String name) {
+        super(name);
+    }
+
+    public MTECokeOven(int ID, String name, String nameRegional) {
+        super(ID, name, nameRegional);
+    }
+
+    @Override
+    public void construct(ItemStack stackSize, boolean hintsOnly) {
+
+    }
+
+    // spotless:off
+    private static final String[][] shape = new String[][] {
+        { "CCC", "CCC", "CCC" },
+        { "C~C", "C-C", "CCC" },
+        { "CCC", "CCC", "CCC" } };
+    //spotless:on
+
+    private static final String STRUCTURE_PIECE_MAIN = "main";
+
+    private static final IStructureDefinition<MTECokeOven> STRUCTURE_DEFINITION = StructureDefinition
+        .<MTECokeOven>builder()
+        .addShape(STRUCTURE_PIECE_MAIN, transpose(shape))
+        .build();
+
+    @Override
+    public IStructureDefinition getStructureDefinition() {
+        return STRUCTURE_DEFINITION;
+    }
+
+    @Override
+    protected MultiblockTooltipBuilder createTooltip() {
+        return new MultiblockTooltipBuilder().addMachineType("Coke Oven")
+            .toolTipFinisher();
+    }
+
+    @Override
+    public boolean checkMachine(IGregTechTileEntity aBaseMetaTileEntity, ItemStack aStack) {
+        return false;
+    }
+
+    @Override
+    public boolean supportsVoidProtection() {
+        return true;
+    }
+
+    @Override
+    public IMetaTileEntity newMetaEntity(IGregTechTileEntity aTileEntity) {
+        return new MTECokeOven(this.mName);
+    }
+
+    @Override
+    public ITexture[] getTexture(IGregTechTileEntity baseMetaTileEntity, ForgeDirection side, ForgeDirection facing,
+        int colorIndex, boolean active, boolean redstoneLevel) {
+        return new ITexture[0];
+    }
 }
