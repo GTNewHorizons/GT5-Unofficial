52b0517a7e34088b3f7c60801f5d0aa15787a431
chrombread <119648279+chrombread@users.noreply.github.com>
2025-09-28 04:51:29 -0700

    Add the "Steam Hearth" multiblock (#4799)
    Co-authored-by: EnderProyects <45178400+EnderProyects@users.noreply.github.com>
    Co-authored-by: Martin Robertz <dream-master@gmx.net>

--- src/main/java/gtPlusPlus/xmod/gregtech/common/tileentities/machines/multi/processing/steam/MTESteamCompressor.java
+++ src/main/java/gtPlusPlus/xmod/gregtech/common/tileentities/machines/multi/processing/steam/MTESteamCompressor.java
@@ -342,12 +342,14 @@ public class MTESteamCompressor extends MTESteamMultiBase<MTESteamCompressor> im
     public void saveNBTData(NBTTagCompound aNBT) {
         super.saveNBTData(aNBT);
         aNBT.setInteger("tierMachine", tierMachine);
+        aNBT.setInteger("tierMachineCasing", tierMachineCasing);
     }
 
     @Override
     public void loadNBTData(final NBTTagCompound aNBT) {
         super.loadNBTData(aNBT);
         tierMachine = aNBT.getInteger("tierMachine");
+        tierMachineCasing = aNBT.getInteger("tierMachineCasing");
     }
 
     @SideOnly(Side.CLIENT)
