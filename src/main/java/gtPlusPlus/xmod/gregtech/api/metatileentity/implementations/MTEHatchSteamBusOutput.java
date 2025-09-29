3aecf247c6d82968a873afa654675ef8c37b49b1
JustACasualDay <samuel.kaltenegger@gmail.com>
2025-09-29 08:14:29 +0200

    spotless

--- src/main/java/gtPlusPlus/xmod/gregtech/api/metatileentity/implementations/MTEHatchSteamBusOutput.java
+++ src/main/java/gtPlusPlus/xmod/gregtech/api/metatileentity/implementations/MTEHatchSteamBusOutput.java
@@ -28,8 +28,8 @@ public class MTEHatchSteamBusOutput extends MTEHatchOutputBus {
             aName,
             aNameRegional,
             aTier,
-            new String[] { "Item Output for Steam Multiblocks", "Automatically export items",
-                "Capacity: 4 stacks", "Does not work with non-steam multiblocks", GTPPCore.GT_Tooltip.get() },
+            new String[] { "Item Output for Steam Multiblocks", "Automatically export items", "Capacity: 4 stacks",
+                "Does not work with non-steam multiblocks", GTPPCore.GT_Tooltip.get() },
             4);
     }
 
