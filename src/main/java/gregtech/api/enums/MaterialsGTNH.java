1e841facdc491d1c25c47af69db2846347c04f0a
Vlamonster <juliadijkstra97@gmail.com>
2025-09-29 15:44:34 +0200

    Convert Signalum

--- src/main/java/gregtech/api/enums/MaterialsGTNH.java
+++ src/main/java/gregtech/api/enums/MaterialsGTNH.java
@@ -84,29 +84,11 @@ public class MaterialsGTNH implements IMaterialHandler {
     }
 
     private static Materials loadSignalum() {
-        return new Materials(
-            -1,
-            TextureSet.SET_NONE,
-            1.0F,
-            0,
-            2,
-            1 | 2,
-            255,
-            255,
-            255,
-            0,
-            "Signalum",
-            "Signalum",
-            0,
-            0,
-            -1,
-            0,
-            false,
-            false,
-            3,
-            1,
-            1,
-            Dyes._NULL);
+        return new MaterialBuilder().setName("Signalum")
+            .setDefaultLocalName("Signalum")
+            .addDustItems()
+            .addMetalItems()
+            .constructMaterial();
     }
 
     private static Materials loadLumium() {
