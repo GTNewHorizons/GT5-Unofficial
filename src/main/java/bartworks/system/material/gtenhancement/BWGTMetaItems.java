e501409b5ec0b62fb5b3b8a3821d4b41546f9515
Vlamonster <juliadijkstra97@gmail.com>
2025-10-01 03:56:07 +0200

    Fix comments

--- src/main/java/bartworks/system/material/gtenhancement/BWGTMetaItems.java
+++ src/main/java/bartworks/system/material/gtenhancement/BWGTMetaItems.java
@@ -163,7 +163,7 @@ public class BWGTMetaItems extends BWMetaGeneratedItems {
             Materials material = Materials.values()[i];
             if (material == null) continue;
 
-            // This only falls through, returning false, when the material has no overlap with `materialGenerationBits`.
+            // This only falls through when the material has no overlap with `materialGenerationBits`.
             // spotless:off
             if ((materialGenerationBits & 1) == 0 || !material.hasDustItems())
                 if ((materialGenerationBits & 2) == 0 || !material.hasMetalItems())
@@ -195,7 +195,7 @@ public class BWGTMetaItems extends BWMetaGeneratedItems {
                 Materials material = GTMetaItemEnhancer.NoMetaValue.get(i);
                 if (material == null) continue;
 
-                // This only falls through, returning false, when the material has no overlap with `materialGenerationBits`.
+                // This only falls through when the material has no overlap with `materialGenerationBits`.
                 // spotless:off
                 if ((materialGenerationBits & 1) == 0 || !material.hasDustItems())
                     if ((materialGenerationBits & 2) == 0 || !material.hasMetalItems())
