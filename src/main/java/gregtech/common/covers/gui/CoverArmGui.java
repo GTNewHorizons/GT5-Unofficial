5220533f673486747e93536cd50df74ab16bb1f1
FrostyFire1 <kamilbaron02@gmail.com>
2025-10-05 11:59:17 +0200

    Add slot rows

--- src/main/java/gregtech/common/covers/gui/CoverArmGui.java
+++ src/main/java/gregtech/common/covers/gui/CoverArmGui.java
@@ -1,13 +1,17 @@
 package gregtech.common.covers.gui;
 
+import net.minecraft.inventory.IInventory;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.StatCollector;
 
 import com.cleanroommc.modularui.api.drawable.IKey;
 import com.cleanroommc.modularui.api.widget.IWidget;
 import com.cleanroommc.modularui.value.sync.BooleanSyncValue;
+import com.cleanroommc.modularui.value.sync.IntSyncValue;
 import com.cleanroommc.modularui.value.sync.PanelSyncManager;
 import com.cleanroommc.modularui.widgets.layout.Flow;
 
+import gregtech.api.interfaces.tileentity.ICoverable;
 import gregtech.api.modularui2.CoverGuiData;
 import gregtech.api.modularui2.GTGuiTextures;
 import gregtech.common.covers.CoverArm;
@@ -61,11 +65,66 @@ public class CoverArmGui extends CoverGui<CoverArm> {
     }
 
     private Flow createInternalSlotRow() {
-        return Flow.row();
+        return Flow.row()
+            .child(
+                // number field with 'Any' goes here
+                makeNumberField(50)
+                    .value(
+                        new IntSyncValue(() -> cover.getInternalSlotId() - 1, val -> cover.setInternalSlotId(val + 1)))
+                    .setDefaultNumber(-1)
+                    .setNumbers(
+                        -1,
+                        cover.getTile()
+                            .getSizeInventory() - 1)
+                    .marginRight(2))
+            .child(
+                IKey.lang("gt.interact.desc.arm.Internal_Slot")
+                    .asWidget()
+                    .tooltipBuilder(t -> t.addLine("-1 means 'Any' sorry for the inconvenience :(")));
     }
 
     private Flow createExternalSlotRow() {
-        return Flow.row();
+        return Flow.row()
+            .child(
+                // number field with 'Any' goes here
+                makeNumberField(50)
+                    .value(
+                        new IntSyncValue(() -> cover.getExternalSlotId() - 1, val -> cover.setExternalSlotId(val + 1)))
+                    .setDefaultNumber(-1)
+                    .setValidator(this::validateExternalSlotId)
+                    .marginRight(2))
+            .child(
+                IKey.lang("gt.interact.desc.arm.Adjacent_Slot")
+                    .asWidget()
+                    .tooltipBuilder(t -> t.addLine("-1 means 'Any' sorry for the inconvenience :(")));
+
     }
 
+    private String validateExternalSlotId(String proposedSlotId) {
+        int slotId;
+        try {
+            slotId = Integer.parseInt(proposedSlotId);
+        } catch (Exception e) {
+            return "-1";
+        }
+
+        int adjacentMaxSlot = getMaxExternalSlot();
+
+        return String.valueOf(Math.max(-1, Math.min(slotId, adjacentMaxSlot)));
+    }
+
+    // Stolen from ArmUIFactory to check what the max slot id is for the adjacent tile
+    private int getMaxExternalSlot() {
+        final ICoverable tile = cover.getTile();
+        int adjacentMaxSlot = -1;
+
+        if (tile instanceof TileEntity && !tile.isDead()) {
+            TileEntity adj = tile.getTileEntityAtSide(cover.getSide());
+            if (adj instanceof IInventory adjInv) {
+                adjacentMaxSlot = adjInv.getSizeInventory() - 1;
+            }
+        }
+
+        return adjacentMaxSlot;
+    }
 }
