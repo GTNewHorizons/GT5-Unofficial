dfc6c5051d152cb48a76693d2982614545d019ec
FrostyFire1 <kamilbaron02@gmail.com>
2025-10-08 22:22:57 +0200

    Reinstate the right click behavior

--- src/main/java/gregtech/common/covers/CoverRedstoneWirelessBase.java
+++ src/main/java/gregtech/common/covers/CoverRedstoneWirelessBase.java
@@ -2,6 +2,7 @@ package gregtech.common.covers;
 
 import static net.minecraft.util.StatCollector.translateToLocalFormatted;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagInt;
@@ -17,6 +18,7 @@ import gregtech.api.covers.CoverContext;
 import gregtech.api.gui.modularui.CoverUIBuildContext;
 import gregtech.api.interfaces.ITexture;
 import gregtech.api.interfaces.tileentity.ICoverable;
+import gregtech.api.util.GTUtility;
 import gregtech.common.gui.mui1.cover.RedstoneWirelessBaseUIFactory;
 import io.netty.buffer.ByteBuf;
 
@@ -26,55 +28,45 @@ public abstract class CoverRedstoneWirelessBase extends Cover {
     private boolean privateChannel;
     public static final int MAX_CHANNEL = 65535;
     private static final int PRIVATE_MASK = 0xFFFE0000;
-    public static final int PUBLIC_MASK = 0x0000FFFF;
+    public static final int CHANNEL_MASK = 0x0000FFFF;
     public static final int CHECKBOX_MASK = 0x00010000;
 
     public CoverRedstoneWirelessBase(CoverContext context, ITexture coverTexture) {
         super(context, coverTexture);
     }
 
-    // @Override
-    // public boolean onCoverRightClick(EntityPlayer aPlayer, float aX, float aY, float aZ) {
-    // if (((aX > 0.375D) && (aX < 0.625D)) || ((coverSide.offsetX != 0) && ((aY > 0.375D) && (aY < 0.625D)))) {
-    // GregTechAPI.sWirelessRedstone.put(coverData, (byte) 0);
-    // coverData = (coverData & (PRIVATE_MASK | CHECKBOX_MASK))
-    // | (((Integer) GTUtility.stackToInt(aPlayer.inventory.getCurrentItem())).hashCode() & PUBLIC_MASK);
-    // GTUtility.sendChatToPlayer(aPlayer, translateToLocalFormatted("gt.interact.desc.freq_format", coverData));
-    // return true;
-    // }
-    // return false;
-    // }
+    @Override
+    public boolean onCoverRightClick(EntityPlayer aPlayer, float aX, float aY, float aZ) {
+        if (((aX > 0.375D) && (aX < 0.625D)) || ((coverSide.offsetX != 0) && ((aY > 0.375D) && (aY < 0.625D)))) {
+            int newFrequency = (((Integer) GTUtility.stackToInt(aPlayer.inventory.getCurrentItem())).hashCode()
+                & CHANNEL_MASK);
+            processCoverData(newFrequency, this.privateChannel);
+            GTUtility.sendChatToPlayer(aPlayer, translateToLocalFormatted("gt.interact.desc.freq_format", frequency));
+            return true;
+        }
+        return false;
+    }
 
-    // @Override
-    // public void onCoverScrewdriverClick(EntityPlayer aPlayer, float aX, float aY, float aZ) {
-    // if (((aX > 0.375D) && (aX < 0.625D)) || ((coverSide.offsetX == 0)
-    // || (((aY > 0.375D) && (aY < 0.625D)) || ((((aZ <= 0.375D) || (aZ >= 0.625D))))))) {
-    // GregTechAPI.sWirelessRedstone.put(this.coverData, (byte) 0);
-    // final float[] tCoords = GTUtility.getClickedFacingCoords(coverSide, aX, aY, aZ);
-    //
-    // final short tAdjustVal = switch ((byte) ((byte) (int) (tCoords[0] * 2.0F)
-    // + 2 * (byte) (int) (tCoords[1] * 2.0F))) {
-    // case 0 -> -32;
-    // case 1 -> 32;
-    // case 2 -> -1024;
-    // case 3 -> 1024;
-    // default -> 0;
-    // };
-    //
-    // final int tPublicChannel = (this.coverData & PUBLIC_MASK) + tAdjustVal;
-    //
-    // if (tPublicChannel < 0) {
-    // this.coverData = this.coverData & ~PUBLIC_MASK;
-    // } else if (tPublicChannel > MAX_CHANNEL) {
-    // this.coverData = (this.coverData & (PRIVATE_MASK | CHECKBOX_MASK)) | MAX_CHANNEL;
-    // } else {
-    // this.coverData = (this.coverData & (PRIVATE_MASK | CHECKBOX_MASK)) | tPublicChannel;
-    // }
-    // }
-    // GTUtility.sendChatToPlayer(
-    // aPlayer,
-    // translateToLocalFormatted("gt.interact.desc.freq_format", this.coverData & PUBLIC_MASK));
-    // }
+    @Override
+    public void onCoverScrewdriverClick(EntityPlayer aPlayer, float aX, float aY, float aZ) {
+        if (((aX > 0.375D) && (aX < 0.625D)) || ((coverSide.offsetX == 0)
+            || (((aY > 0.375D) && (aY < 0.625D)) || ((((aZ <= 0.375D) || (aZ >= 0.625D))))))) {
+            final float[] tCoords = GTUtility.getClickedFacingCoords(coverSide, aX, aY, aZ);
+
+            final short adjustFrequencyValue = switch ((byte) ((byte) (int) (tCoords[0] * 2.0F)
+                + 2 * (byte) (int) (tCoords[1] * 2.0F))) {
+                case 0 -> -32;
+                case 1 -> 32;
+                case 2 -> -1024;
+                case 3 -> 1024;
+                default -> 0;
+            };
+
+            int newFrequency = Math.max(0, Math.min(MAX_CHANNEL, this.frequency + adjustFrequencyValue));
+            processCoverData(newFrequency, this.privateChannel);
+        }
+        GTUtility.sendChatToPlayer(aPlayer, translateToLocalFormatted("gt.interact.desc.freq_format", frequency));
+    }
 
     @Override
     public boolean letsEnergyIn() {
@@ -187,7 +179,7 @@ public abstract class CoverRedstoneWirelessBase extends Cover {
     }
 
     protected int getFlagFrequency(int coverVariable) {
-        return coverVariable & CoverRedstoneWirelessBase.PUBLIC_MASK;
+        return coverVariable & CoverRedstoneWirelessBase.CHANNEL_MASK;
     }
 
     protected boolean getFlagCheckbox(int coverVariable) {
