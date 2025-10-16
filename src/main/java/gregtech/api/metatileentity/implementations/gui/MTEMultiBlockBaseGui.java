f8b8d95be7e537ed4797db1113ccfcb55f3e129c
FrostyFire1 <kamilbaron02@gmail.com>
2025-10-16 12:51:44 +0200

    SPOTLESS

--- src/main/java/gregtech/api/metatileentity/implementations/gui/MTEMultiBlockBaseGui.java
+++ src/main/java/gregtech/api/metatileentity/implementations/gui/MTEMultiBlockBaseGui.java
@@ -16,7 +16,6 @@ import java.util.Set;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
-import gregtech.api.recipe.check.CheckRecipeResultRegistry;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketBuffer;
@@ -208,12 +207,14 @@ public class MTEMultiBlockBaseGui {
         syncManager.syncValue("startupCheck", startupCheckSyncer);
 
         return new ListWidget<>().widthRel(1)
-            .child(IKey.lang("GT5U.multiblock.startup").color(Color.WHITE.main)
-                .asWidget()
-                .alignment(Alignment.CenterLeft)
-                .setEnabledIf(w -> startupCheckSyncer.getValue() > 0)
-                .marginBottom(2)
-                .widthRel(1))
+            .child(
+                IKey.lang("GT5U.multiblock.startup")
+                    .color(Color.WHITE.main)
+                    .asWidget()
+                    .alignment(Alignment.CenterLeft)
+                    .setEnabledIf(w -> startupCheckSyncer.getValue() > 0)
+                    .marginBottom(2)
+                    .widthRel(1))
             .child(
                 new TextWidget<>(GTUtility.trans("142", "Running perfectly.")).color(Color.WHITE.main)
                     .setEnabledIf(widget -> base.getErrorDisplayID() == 0 && baseMetaTileEntity.isActive())
