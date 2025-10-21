37d4e305b7a09b80a36610c7b1e7395ec1d4cc69
Ranzu <66495944+Ranzuu@users.noreply.github.com>
2025-10-21 13:01:08 +0200

    yes

--- src/main/java/gregtech/common/covers/CoverPlayerDetector.java
+++ src/main/java/gregtech/common/covers/CoverPlayerDetector.java
@@ -98,9 +98,9 @@ public class CoverPlayerDetector extends CoverLegacyData {
             this.coverData = 2;
         }
         switch (this.coverData) {
-            case 0 -> GTUtility.sendChatToPlayer(aPlayer, GTUtility.trans("068.1", "Emit if any Player is close"));
-            case 1 -> GTUtility.sendChatToPlayer(aPlayer, GTUtility.trans("069.1", "Emit if other Player is close"));
-            case 2 -> GTUtility.sendChatToPlayer(aPlayer, GTUtility.trans("070", "Emit if you are close"));
+            case 0 -> GTUtility.sendChatToPlayer(aPlayer, GTUtility.translate("gt.chat.interact.emit_player_close"));
+            case 1 -> GTUtility.sendChatToPlayer(aPlayer, GTUtility.translate("gt.chat.interact.emit_player_no_close"));
+            case 2 -> GTUtility.sendChatToPlayer(aPlayer, GTUtility.translate("gt.chat.interact.emit_you_close"));
         }
     }
 
