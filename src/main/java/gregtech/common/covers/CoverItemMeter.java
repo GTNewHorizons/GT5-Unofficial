cba08a855415178dc613ab06a93178e630923a19
Ranzu <66495944+Ranzuu@users.noreply.github.com>
2025-10-22 12:57:39 +0200

    spotless

--- src/main/java/gregtech/common/covers/CoverItemMeter.java
+++ src/main/java/gregtech/common/covers/CoverItemMeter.java
@@ -155,8 +155,9 @@ public class CoverItemMeter extends Cover {
             slot++;
             if (slot > coverable.getSizeInventory()) slot = -1;
 
-            if (slot == -1)
-                GTUtility.sendChatToPlayer(aPlayer, GTUtility.translate("gt.chat.interact.desc.slot") + GTUtility.translate("gt.interact.desc.all"));
+            if (slot == -1) GTUtility.sendChatToPlayer(
+                aPlayer,
+                GTUtility.translate("gt.chat.interact.desc.slot") + GTUtility.translate("gt.interact.desc.all"));
             else GTUtility.sendChatToPlayer(aPlayer, GTUtility.translate("gt.chat.interact.desc.slot") + slot);
         }
     }
