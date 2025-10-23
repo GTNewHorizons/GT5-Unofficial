8f95b498c44767c6b76c80265fd956740b268ff8
SuperSoupr <26164930+SuperSoupr@users.noreply.github.com>
2025-10-23 21:37:11 +0300

    Revert "Use fancy waila fluid tooltip renderer in GT tanks (#5111)"
    This reverts commit 660d72e3b52da2a7accafc0ac7eb99e2d103c7ca.

--- src/main/java/gregtech/common/tileentities/storage/MTEDigitalTankBase.java
+++ src/main/java/gregtech/common/tileentities/storage/MTEDigitalTankBase.java
@@ -14,9 +14,12 @@ import java.util.List;
 import javax.annotation.Nonnull;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
@@ -45,6 +48,8 @@ import gregtech.api.render.TextureFactory;
 import gregtech.api.util.GTLanguageManager;
 import gregtech.api.util.GTUtility;
 import gregtech.common.gui.modularui.widget.FluidLockWidget;
+import mcp.mobius.waila.api.IWailaConfigHandler;
+import mcp.mobius.waila.api.IWailaDataAccessor;
 
 public abstract class MTEDigitalTankBase extends MTEBasicTank
     implements IFluidLockable, IAddUIWidgets, IAddGregtechLogo {
@@ -466,6 +471,35 @@ public abstract class MTEDigitalTankBase extends MTEBasicTank
     @Nonnull
     public FluidTankInfo[] getRealTankInfo(ForgeDirection side) {
         return new FluidTankInfo[] { new FluidTankInfo(getFluid(), getRealCapacity()) };
+    }
+
+    @Override
+    public void getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor,
+        IWailaConfigHandler config) {
+        super.getWailaBody(itemStack, currenttip, accessor, config);
+
+        NBTTagCompound tag = accessor.getNBTData();
+        FluidStack fluid = tag.hasKey("mFluid") ? FluidStack.loadFluidStackFromNBT(tag.getCompoundTag("mFluid")) : null;
+        if (fluid != null && fluid.amount >= 0) {
+            currenttip.remove(0);
+            currenttip.add(
+                0,
+                formatNumbers(fluid.amount) + " / "
+                    + formatNumbers(getRealCapacity())
+                    + " L "
+                    + fluid.getLocalizedName());
+        } else {
+            currenttip.add(0, "Tank Empty");
+        }
+    }
+
+    @Override
+    public void getWailaNBTData(EntityPlayerMP player, TileEntity tile, NBTTagCompound tag, World world, int x, int y,
+        int z) {
+        super.getWailaNBTData(player, tile, tag, world, x, y, z);
+        FluidStack fluid = getFluid();
+        if (fluid != null) tag.setTag("mFluid", fluid.writeToNBT(new NBTTagCompound()));
+        else if (tag.hasKey("mFluid")) tag.removeTag("mFluid");
     }
 
     protected static final NumberFormatMUI numberFormat = new NumberFormatMUI();
