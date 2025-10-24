45a6c9496cf56fd109d02ec66e611d5813e086e6
SuperSoupr <26164930+SuperSoupr@users.noreply.github.com>
2025-10-24 09:33:36 +0300

    Use special waila fluid renderer

--- src/main/java/gregtech/common/tileentities/storage/MTEDigitalTankBase.java
+++ src/main/java/gregtech/common/tileentities/storage/MTEDigitalTankBase.java
@@ -50,6 +50,7 @@ import gregtech.api.util.GTUtility;
 import gregtech.common.gui.modularui.widget.FluidLockWidget;
 import mcp.mobius.waila.api.IWailaConfigHandler;
 import mcp.mobius.waila.api.IWailaDataAccessor;
+import mcp.mobius.waila.api.SpecialChars;
 
 public abstract class MTEDigitalTankBase extends MTEBasicTank
     implements IFluidLockable, IAddUIWidgets, IAddGregtechLogo {
@@ -484,10 +485,13 @@ public abstract class MTEDigitalTankBase extends MTEBasicTank
             currenttip.remove(0);
             currenttip.add(
                 0,
-                formatNumbers(fluid.amount) + " / "
-                    + formatNumbers(getRealCapacity())
-                    + " L "
-                    + fluid.getLocalizedName());
+                SpecialChars.getRenderString(
+                    "waila.fluid",
+                    fluid.getFluid()
+                        .getName(),
+                    fluid.getLocalizedName(),
+                    fluid.amount + "",
+                    getRealCapacity() + ""));
         } else {
             currenttip.add(0, "Tank Empty");
         }
