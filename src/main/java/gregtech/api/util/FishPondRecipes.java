e426a8960437aebdbfe276cbe5dd03f99b637b23
UltraProdigy <187078471+UltraProdigy@users.noreply.github.com>
2025-10-27 03:41:57 -0400

    fishpond

--- src/main/java/gregtech/api/util/FishPondRecipes.java
+++ src/main/java/gregtech/api/util/FishPondRecipes.java
@@ -62,7 +62,7 @@ public class FishPondRecipes {
         }
 
         GTValues.RA.stdBuilder()
-            .itemInputs(GTUtility.getIntegratedCircuit(circuitType))
+            .iCircuit(circuitType)
             .itemOutputs(outputs, chances)
             .duration(10 * SECONDS)
             .eut(16)
