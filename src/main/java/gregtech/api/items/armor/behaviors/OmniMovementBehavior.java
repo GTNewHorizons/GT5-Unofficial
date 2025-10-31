87f18a1c73a2c111aa6980a3943b566b59cd0c64
chrombread <chrombread8@gmail.com>
2025-10-31 15:36:38 -0700

    missed a couple method changes

--- src/main/java/gregtech/api/items/armor/behaviors/OmniMovementBehavior.java
+++ src/main/java/gregtech/api/items/armor/behaviors/OmniMovementBehavior.java
@@ -6,6 +6,7 @@ import static gregtech.api.util.GTUtility.getOrCreateNbtCompound;
 import java.util.Collections;
 import java.util.Set;
 
+import gregtech.api.util.GTUtility;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
