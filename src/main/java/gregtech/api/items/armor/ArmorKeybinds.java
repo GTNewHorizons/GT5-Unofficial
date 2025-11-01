8f79fd56ecb4067283d060b86b98fcc284d4ae3e
Mary Hopson <doyoumined@gmail.com>
2025-11-01 17:49:29 -0400

    Add lang and category for keybinds

--- src/main/java/gregtech/api/items/armor/ArmorKeybinds.java
+++ src/main/java/gregtech/api/items/armor/ArmorKeybinds.java
@@ -9,23 +9,23 @@ import com.gtnewhorizon.gtnhlib.keybind.SyncedKeybind;
 public class ArmorKeybinds {
 
     public static final SyncedKeybind NIGHT_VISION_KEYBIND = SyncedKeybind
-        .createConfigurable("gregtech.toggle_night_vision", "key.categories.gameplay", Keyboard.KEY_R);
+        .createConfigurable("key.gt.toggle_night_vision", "Gregtech Armor", Keyboard.KEY_R);
     public static final SyncedKeybind JETPACK_HOVER_KEYBIND = SyncedKeybind
-        .createConfigurable("gregtech.toggle_jetpack_hover", "key.categories.gameplay", Keyboard.KEY_G);
+        .createConfigurable("key.gt.toggle_jetpack_hover", "Gregtech Armor", Keyboard.KEY_G);
     public static final SyncedKeybind JETPACK_KEYBIND = SyncedKeybind
-        .createConfigurable("gregtech.toggle_jetpack", "key.categories.gameplay", Keyboard.KEY_F);
+        .createConfigurable("key.gt.toggle_jetpack", "Gregtech Armor", Keyboard.KEY_F);
     public static final SyncedKeybind GOGGLES_OF_REVEALING_KEYBIND = SyncedKeybind
-        .createConfigurable("gregtech.toggle_goggles_of_revealing", "key.categories.gameplay", Keyboard.KEY_T);
+        .createConfigurable("key.gt.toggle_goggles_of_revealing", "Gregtech Armor", Keyboard.KEY_T);
     public static final SyncedKeybind INERTIA_CANCELING_KEYBIND = SyncedKeybind
-        .createConfigurable("gregtech.toggle_inertiacanceling", "key.categories.gameplay", Keyboard.KEY_T);
+        .createConfigurable("key.gt.toggle_inertiacanceling", "Gregtech Armor", Keyboard.KEY_T);
     public static final SyncedKeybind OMNI_MOVEMENT_KEYBIND = SyncedKeybind
-        .createConfigurable("gregtech.toggle_omnimovement", "key.categories.gameplay", Keyboard.KEY_T);
+        .createConfigurable("key.gt.toggle_omnimovement", "Gregtech Armor", Keyboard.KEY_T);
     public static final SyncedKeybind SPEED_INCREASE_KEYBIND = SyncedKeybind
-        .createConfigurable("gregtech.speed_increase", "key.categories.gameplay", Keyboard.KEY_EQUALS);
+        .createConfigurable("key.gt.speed_increase", "Gregtech Armor", Keyboard.KEY_EQUALS);
     public static final SyncedKeybind SPEED_DECREASE_KEYBIND = SyncedKeybind
-        .createConfigurable("gregtech.speed_decrease", "key.categories.gameplay", Keyboard.KEY_MINUS);
+        .createConfigurable("key.gt.speed_decrease", "Gregtech Armor", Keyboard.KEY_MINUS);
     public static final SyncedKeybind FORCE_FIELD_KEYBIND = SyncedKeybind
-        .createConfigurable("gregtech.force_field", "key.categories.gameplay", Keyboard.KEY_K);
+        .createConfigurable("key.gt.force_field", "Gregtech Armor", Keyboard.KEY_K);
 
     public static final SyncedKeybind VANILLA_JUMP = SyncedKeybind
         .createFromMC(() -> () -> Minecraft.getMinecraft().gameSettings.keyBindJump);
