8b697c0d24f37ce3538fbbba4cc66acf22c093d4
chrombread <chrombread8@gmail.com>
2025-11-04 12:43:09 -0800

    remove deprecation notice on latex

--- src/main/java/gregtech/common/tileentities/machines/multi/MTELatex.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/MTELatex.java
@@ -278,8 +278,6 @@ public class MTELatex extends MTEExtendedPowerMultiBlockBase<MTELatex> implement
                     + TooltipHelper.coloredText("Multi-Amp energy hatch", GREEN))
             .addSeparator()
             .addInfo(DARK_AQUA + "Make sure to cover up!")
-            .addSeparator()
-            .addInfo(DARK_RED + "Cable Recipes in the Assembler TO BE REMOVED in the next major update!")
             .beginStructureBlock(3, 5, 3, true)
             .addController("Front Center")
             .addCasingInfoMin("Chemically Inert Machine Casing", 14, false)
