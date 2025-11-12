6dda9fafc1eb7304984cc6326f35ac0d66817e1b
Maya <10861407+serenibyss@users.noreply.github.com>
2025-11-12 11:30:48 -0700

    clean up fluid tooltip

--- src/main/java/gregtech/common/gui/modularui/multiblock/godforge/util/ForgeOfGodsGuiUtil.java
+++ src/main/java/gregtech/common/gui/modularui/multiblock/godforge/util/ForgeOfGodsGuiUtil.java
@@ -2,29 +2,15 @@ package gregtech.common.gui.modularui.multiblock.godforge.util;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.audio.PositionedSoundRecord;
-import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.fluids.FluidStack;
 
-import org.jetbrains.annotations.NotNull;
-
-import com.cleanroommc.modularui.screen.RichTooltip;
 import com.cleanroommc.modularui.widgets.ButtonWidget;
 
 import gregtech.api.enums.Mods;
-import gregtech.common.items.ItemFluidDisplay;
 
 public class ForgeOfGodsGuiUtil {
 
     private static final ResourceLocation PRESS_SOUND = new ResourceLocation(Mods.TecTech.ID, "fx_click");
-
-    public static void addFluidNameInfo(RichTooltip tooltip, @NotNull FluidStack fluid) {
-        tooltip.addLine(EnumChatFormatting.WHITE + fluid.getLocalizedName());
-        String formula = ItemFluidDisplay.getChemicalFormula(fluid);
-        if (!formula.isEmpty()) {
-            tooltip.addLine(EnumChatFormatting.YELLOW + formula);
-        }
-    }
 
     public static Runnable getButtonSound() {
         return () -> Minecraft.getMinecraft()
