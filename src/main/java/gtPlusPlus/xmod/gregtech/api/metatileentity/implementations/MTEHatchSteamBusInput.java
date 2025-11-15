fac1203ce59602e4be92a8b83fa1dcfd4f95cc2c
chrombread <chrombread8@gmail.com>
2025-11-15 10:57:25 -0800

    anonymous class to override the slot group behavior a little bit

--- src/main/java/gtPlusPlus/xmod/gregtech/api/metatileentity/implementations/MTEHatchSteamBusInput.java
+++ src/main/java/gtPlusPlus/xmod/gregtech/api/metatileentity/implementations/MTEHatchSteamBusInput.java
@@ -1,5 +1,10 @@
 package gtPlusPlus.xmod.gregtech.api.metatileentity.implementations;
 
+import com.cleanroommc.modularui.factory.PosGuiData;
+import com.cleanroommc.modularui.screen.ModularPanel;
+import com.cleanroommc.modularui.screen.UISettings;
+import com.cleanroommc.modularui.value.sync.PanelSyncManager;
+import gregtech.common.gui.modularui.hatch.MTEHatchInputBusGui;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.util.ForgeDirection;
 
@@ -47,5 +52,17 @@ public class MTEHatchSteamBusInput extends MTEHatchInputBus {
     public boolean allowPullStack(IGregTechTileEntity aBaseMetaTileEntity, int aIndex, ForgeDirection side,
         ItemStack aStack) {
         return false;
+    }
+
+    @Override
+    public ModularPanel buildUI(PosGuiData data, PanelSyncManager syncManager, UISettings uiSettings) {
+        return new MTEHatchInputBusGui(this){
+
+            // steam input buses don't follow into the common formula that others do, so its changed here,
+            @Override
+            protected int getDimension() {
+                return 2;
+            }
+        }.build(data, syncManager, uiSettings);
     }
 }
