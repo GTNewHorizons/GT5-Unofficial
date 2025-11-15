00fbc41433b33022365c2fae19a048875e7bb7bf
chrombread <119648279+chrombread@users.noreply.github.com>
2025-11-15 00:57:49 -0800

    [NAC] adapting NAC ui's to mui2 (#5344)

--- src/main/java/gregtech/common/gui/modularui/multiblock/base/MTEMultiBlockBaseGui.java
+++ src/main/java/gregtech/common/gui/modularui/multiblock/base/MTEMultiBlockBaseGui.java
@@ -49,7 +49,6 @@ import com.cleanroommc.modularui.value.sync.PanelSyncManager;
 import com.cleanroommc.modularui.value.sync.StringSyncValue;
 import com.cleanroommc.modularui.widget.EmptyWidget;
 import com.cleanroommc.modularui.widget.ParentWidget;
-import com.cleanroommc.modularui.widget.Widget;
 import com.cleanroommc.modularui.widgets.ButtonWidget;
 import com.cleanroommc.modularui.widgets.CycleButtonWidget;
 import com.cleanroommc.modularui.widgets.DynamicSyncedWidget;
@@ -186,21 +185,24 @@ public class MTEMultiBlockBaseGui<T extends MTEMultiBlockBase> {
 
     protected Flow createTerminalRow(ModularPanel panel, PanelSyncManager syncManager) {
         return new Row().size(getTerminalRowWidth(), getTerminalRowHeight())
-            .child(
-                new ParentWidget<>().size(getTerminalWidgetWidth(), getTerminalWidgetHeight())
-                    .paddingTop(4)
-                    .paddingBottom(4)
-                    .paddingLeft(4)
-                    .paddingRight(0)
+            .child(createTerminalParentWidget(panel, syncManager));
+    }
 
-                    .widgetTheme(GTWidgetThemes.BACKGROUND_TERMINAL)
-                    .child(
-                        createTerminalTextWidget(syncManager, panel)
-                            .size(getTerminalWidgetWidth() - 4, getTerminalWidgetHeight() - 8)
-                            .collapseDisabledChild())
-                    .childIf(
-                        multiblock.supportsTerminalRightCornerColumn(),
-                        createTerminalRightCornerColumn(panel, syncManager)));
+    protected ParentWidget<?> createTerminalParentWidget(ModularPanel panel, PanelSyncManager syncManager) {
+        return new ParentWidget<>().size(getTerminalWidgetWidth(), getTerminalWidgetHeight())
+            .paddingTop(4)
+            .paddingBottom(4)
+            .paddingLeft(4)
+            .paddingRight(0)
+
+            .widgetTheme(GTWidgetThemes.BACKGROUND_TERMINAL)
+            .child(
+                createTerminalTextWidget(syncManager, panel)
+                    .size(getTerminalWidgetWidth() - 4, getTerminalWidgetHeight() - 8)
+                    .collapseDisabledChild())
+            .childIf(
+                multiblock.supportsTerminalRightCornerColumn(),
+                createTerminalRightCornerColumn(panel, syncManager));
     }
 
     protected Flow createTerminalRightCornerColumn(ModularPanel panel, PanelSyncManager syncManager) {
@@ -209,11 +211,13 @@ public class MTEMultiBlockBaseGui<T extends MTEMultiBlockBase> {
             .bottomRel(0, 6, 0)
             .childIf(multiblock.supportsShutdownReasonHoverable(), createShutdownReasonHoverableTerminal(syncManager))
             .childIf(multiblock.supportsMaintenanceIssueHoverable(), createMaintIssueHoverableTerminal(syncManager))
-            .childIf(
-                multiblock.supportsLogo(),
-                new Widget<>().size(18, 18)
-                    .marginTop(4)
-                    .widgetTheme(GTWidgetThemes.PICTURE_LOGO));
+            .childIf(multiblock.supportsLogo(), makeLogoWidget());
+    }
+
+    protected IDrawable.DrawableWidget makeLogoWidget() {
+        return new IDrawable.DrawableWidget(IDrawable.EMPTY).size(18)
+            .marginTop(4)
+            .widgetTheme(GTWidgetThemes.PICTURE_LOGO);
     }
 
     protected int getTerminalRowWidth() {
