6d033ea1521c2dad52575d3f3963510fd238210c
Maya <10861407+serenibyss@users.noreply.github.com>
2025-11-15 22:50:21 -0700

    add remaining panel classes and enum entries

--- src/main/java/gregtech/common/gui/modularui/multiblock/godforge/panel/StarColorImportPanel.java
+++ src/main/java/gregtech/common/gui/modularui/multiblock/godforge/panel/StarColorImportPanel.java
@@ -1,10 +1,9 @@
 package gregtech.common.gui.modularui.multiblock.godforge.panel;
 
 import com.cleanroommc.modularui.screen.ModularPanel;
-
 import gregtech.common.gui.modularui.multiblock.godforge.util.SyncHypervisor;
 
-public class StarCosmeticsPanel {
+public class StarColorImportPanel {
 
     public static ModularPanel openPanel(SyncHypervisor hypervisor) {
         return null; // todo
