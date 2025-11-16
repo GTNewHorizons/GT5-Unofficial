5fa2216c12ed4956d255349fd461f7742acc92e8
Maya <10861407+serenibyss@users.noreply.github.com>
2025-11-15 22:33:22 -0700

    battery config panel

--- src/main/java/gregtech/common/gui/modularui/multiblock/godforge/panel/BatteryConfigPanel.java
+++ src/main/java/gregtech/common/gui/modularui/multiblock/godforge/panel/BatteryConfigPanel.java
@@ -1,17 +1,52 @@
 package gregtech.common.gui.modularui.multiblock.godforge.panel;
 
-import com.cleanroommc.modularui.screen.ModularPanel;
-import com.cleanroommc.modularui.value.sync.PanelSyncManager;
+import static net.minecraft.util.StatCollector.translateToLocal;
 
+import net.minecraft.util.EnumChatFormatting;
+
+import com.cleanroommc.modularui.api.drawable.IKey;
+import com.cleanroommc.modularui.screen.ModularPanel;
+import com.cleanroommc.modularui.utils.Alignment;
+import com.cleanroommc.modularui.widgets.textfield.TextFieldWidget;
+
+import gregtech.common.gui.modularui.multiblock.godforge.data.Panels;
+import gregtech.common.gui.modularui.multiblock.godforge.data.SyncValues;
 import gregtech.common.gui.modularui.multiblock.godforge.util.SyncHypervisor;
 
 public class BatteryConfigPanel {
 
+    private static final int SIZE_W = 78;
+    private static final int SIZE_H = 52;
+
     public static ModularPanel openPanel(SyncHypervisor hypervisor) {
-        return null; // todo
-    }
+        ModularPanel panel = hypervisor.getModularPanel(Panels.BATTERY_CONFIG);
 
-    public static void registerSyncValues(PanelSyncManager syncManager) {
+        panel.relative(hypervisor.getModularPanel(Panels.MAIN))
+            .size(SIZE_W, SIZE_H)
+            .leftRelOffset(1, -3)
+            .topRelOffset(0, 129);
 
+        // Header
+        panel.child(
+            IKey.str(EnumChatFormatting.DARK_GRAY + translateToLocal("gt.blockmachines.multimachine.FOG.batteryinfo"))
+                .alignment(Alignment.CENTER)
+                .asWidget()
+                .width(SIZE_W - 8)
+                .align(Alignment.TopCenter)
+                .marginTop(5));
+
+        // Textbox
+        panel.child(
+            new TextFieldWidget().setFormatAsInteger(true)
+                .setNumbers(1, Integer.MAX_VALUE)
+                .setTextAlignment(Alignment.CENTER)
+                .value(SyncValues.MAX_BATTERY_CHARGE.create(hypervisor))
+                .setTooltipOverride(true)
+                .setScrollValues(1, 4, 64)
+                .size(SIZE_W - 8, 18)
+                .align(Alignment.BottomCenter)
+                .marginBottom(9));
+
+        return panel;
     }
 }
