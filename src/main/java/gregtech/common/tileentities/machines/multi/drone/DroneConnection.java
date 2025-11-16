68d2333810604106e516cb065a9a1525486cf306
RealSilverMoon <qazhhhh@outlook.com>
2025-11-17 00:29:23 +0800

    DroneConnection refactor

--- src/main/java/gregtech/common/tileentities/machines/multi/drone/DroneConnection.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/drone/DroneConnection.java
@@ -2,43 +2,51 @@ package gregtech.common.tileentities.machines.multi.drone;
 
 import static gregtech.GTMod.proxy;
 
+import java.io.IOException;
 import java.util.Optional;
+import java.util.UUID;
+
+import javax.annotation.Nullable;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.PacketBuffer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ChunkCoordinates;
+import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 import net.minecraftforge.common.DimensionManager;
 
 import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
 import gregtech.api.metatileentity.implementations.MTEMultiBlockBase;
-import gregtech.api.util.GTLanguageManager;
 import gregtech.api.util.GTUtil;
-import gregtech.api.util.GTUtility;
+import gregtech.api.util.shutdown.ShutDownReason;
 
 public class DroneConnection {
 
+    // To make it work on client, no mte should be stored inside connection
     String customName;
     String unlocalizedName;
-    MTEMultiBlockBase machine;
     ItemStack machineItem;
-    ChunkCoordinates machineCoord;
-    MTEDroneCentre centre;
+    public ChunkCoordinates machineCoord;
+    boolean machineStatus;
+    ShutDownReason shutdownReason;
     ChunkCoordinates centreCoord;
+    public UUID uuid;
     World world;
 
     public DroneConnection(MTEMultiBlockBase machine, MTEDroneCentre centre) {
-        this.machine = machine;
         this.machineItem = machine.getStackForm(1);
         machineCoord = machine.getBaseMetaTileEntity()
             .getCoords();
-        this.centre = centre;
         centreCoord = centre.getBaseMetaTileEntity()
             .getCoords();
         this.world = centre.getBaseMetaTileEntity()
             .getWorld();
+        uuid = UUID.nameUUIDFromBytes(
+            machineCoord.toString()
+                .getBytes());
         unlocalizedName = machine.mName;
         customName = Optional.ofNullable(centre.tempNameList.remove(machineCoord.toString()))
             .orElse(machine.getLocalName());
@@ -57,23 +65,36 @@ public class DroneConnection {
             machineTag.getInteger("x"),
             machineTag.getInteger("y"),
             machineTag.getInteger("z"));
-        this.machine = getLoadedGT_BaseMachineAt(machineCoord, world, true);
         centreCoord = new ChunkCoordinates(
             centreTag.getInteger("x"),
             centreTag.getInteger("y"),
             centreTag.getInteger("z"));
-        this.centre = (MTEDroneCentre) getLoadedGT_BaseMachineAt(centreCoord, world, true);
         this.customName = aNBT.getString("name");
         this.unlocalizedName = aNBT.getString("unlocalizedName");
+        if (aNBT.hasKey("uuid")) {
+            this.uuid = UUID.fromString(aNBT.getString("uuid"));
+        }
     }
 
-    public MTEMultiBlockBase getMachine() {
-        return machine;
+    @Nullable
+    public MTEMultiBlockBase getLinkedMachine() {
+        return getLoadedGTBaseMachineAt(machineCoord, world, false);
     }
 
-    public String getCustomName(boolean localized) {
-        if (localized) return GTLanguageManager.getTranslation("gt.blockmachines." + unlocalizedName + ".name");
+    public MTEMultiBlockBase getLinkedCentre() {
+        return getLoadedGTBaseMachineAt(centreCoord, world, true);
+    }
+
+    public String getCustomName() {
         return customName;
+    }
+
+    public String getLocalizedName() {
+        return StatCollector.translateToLocal("gt.blockmachines." + unlocalizedName + ".name");
+    }
+
+    public String getUUIDString() {
+        return uuid.toString();
     }
 
     public float getDistanceSquared() {
@@ -85,15 +106,7 @@ public class DroneConnection {
     }
 
     public boolean isMachineShutdown() {
-        return machine != null && machine.shouldDisplayShutDownReason()
-            && !machine.getBaseMetaTileEntity()
-                .isActive()
-            && GTUtility.isStringValid(
-                machine.getBaseMetaTileEntity()
-                    .getLastShutDownReason()
-                    .getDisplayString())
-            && machine.getBaseMetaTileEntity()
-                .wasShutdown();
+        return false;
     }
 
     public NBTTagCompound transConnectionToNBT() {
@@ -103,14 +116,15 @@ public class DroneConnection {
         aNBT.setTag("item", machineItem.writeToNBT(new NBTTagCompound()));
         aNBT.setInteger(
             "worldID",
-            centre.getBaseMetaTileEntity()
+            getLinkedCentre().getBaseMetaTileEntity()
                 .getWorld().provider.dimensionId);
-        aNBT.setString("name", getCustomName(false));
+        aNBT.setString("name", getCustomName());
         aNBT.setString("unlocalizedName", unlocalizedName);
+        aNBT.setString("uuid", this.uuid.toString());
         return aNBT;
     }
 
-    public MTEMultiBlockBase getLoadedGT_BaseMachineAt(ChunkCoordinates coords, World world, boolean isLoaded) {
+    public MTEMultiBlockBase getLoadedGTBaseMachineAt(ChunkCoordinates coords, World world, boolean isLoaded) {
         TileEntity te = GTUtil.getTileEntity(world, coords, isLoaded);
         if (te == null) return null;
         return (MTEMultiBlockBase) ((IGregTechTileEntity) te).getMetaTileEntity();
@@ -125,11 +139,33 @@ public class DroneConnection {
     }
 
     public boolean isValid() {
-        return machine != null && machine.isValid() && centre != null && centre.isValid();
+        return getLinkedMachine() != null && getLinkedMachine().isValid()
+            && getLinkedCentre() != null
+            && getLinkedCentre().isValid();
     }
-    //Todo: NO MORE MTE STORED
-    @Override
-    public boolean equals(Object obj){
-        return obj instanceof DroneConnection dc && dc.machineCoord.equals(machineCoord) && dc.centreCoord.equals(centreCoord);
+
+    public void setActive(boolean active) {
+        machineStatus = active;
+    }
+
+    public void setShutdownReason(ShutDownReason reason) {
+        shutdownReason = reason;
+    }
+
+    public static DroneConnection deserialize(PacketBuffer buf) throws IOException {
+        NBTTagCompound tag = buf.readNBTTagCompoundFromBuffer();
+        if (tag == null) return null;
+        return new DroneConnection(tag);
+    }
+
+    public static void serialize(PacketBuffer buf, DroneConnection connection) throws IOException {
+        buf.writeNBTTagCompoundToBuffer(connection.transConnectionToNBT());
+    }
+
+    public static boolean areEqual(DroneConnection a, DroneConnection b) {
+        if (a == null || b == null) return false;
+        return a.machineCoord.equals(b.machineCoord) && a.centreCoord.equals(b.centreCoord)
+            && a.customName.equals(b.customName)
+            && a.unlocalizedName.equals(b.unlocalizedName);
     }
 }
