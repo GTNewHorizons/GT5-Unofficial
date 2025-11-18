feb0a2be30028fe0e6ee61f3711508f259b5a687
RealSilverMoon <qazhhhh@outlook.com>
2025-11-19 00:05:36 +0800

    no need for a setter

--- src/main/java/gregtech/common/gui/modularui/synchandler/DroneConnectionListSyncHandler.java
+++ src/main/java/gregtech/common/gui/modularui/synchandler/DroneConnectionListSyncHandler.java
@@ -1,7 +1,6 @@
 package gregtech.common.gui.modularui.synchandler;
 
 import java.util.List;
-import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 import org.jetbrains.annotations.NotNull;
@@ -12,14 +11,7 @@ import gregtech.common.tileentities.machines.multi.drone.DroneConnection;
 
 public class DroneConnectionListSyncHandler extends GenericListSyncHandler<DroneConnection> {
 
-    public DroneConnectionListSyncHandler(@NotNull Supplier<List<DroneConnection>> getter,
-        @NotNull Consumer<List<DroneConnection>> setter) {
-        super(
-            getter,
-            setter,
-            DroneConnection::deserialize,
-            DroneConnection::serialize,
-            DroneConnection::areEqual,
-            null);
+    public DroneConnectionListSyncHandler(@NotNull Supplier<List<DroneConnection>> getter) {
+        super(getter, null, DroneConnection::deserialize, DroneConnection::serialize, DroneConnection::areEqual, null);
     }
 }
