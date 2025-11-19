9886e60226a825948f99fcb12e449d524d0aac11
boubou19 <miisterunknown@gmail.com>
2025-11-19 18:50:43 +0100

    early return

--- src/main/java/bartworks/common/loaders/StaticRecipeChangeLoaders.java
+++ src/main/java/bartworks/common/loaders/StaticRecipeChangeLoaders.java
@@ -58,61 +58,62 @@ public class StaticRecipeChangeLoaders {
     }
 
     private static void runMoltenUnificationEnforcement(Werkstoff werkstoff) {
-        if (werkstoff.hasItemType(OrePrefixes.cellMolten)) {
-            FluidContainerRegistry.FluidContainerData data = new FluidContainerRegistry.FluidContainerData(
-                new FluidStack(Objects.requireNonNull(WerkstoffLoader.molten.get(werkstoff)), 1 * INGOTS),
-                werkstoff.get(OrePrefixes.cellMolten),
-                Materials.Empty.getCells(1));
-            ItemStack toReplace = null;
-            Iterator<Map.Entry<GTItemStack, FluidContainerRegistry.FluidContainerData>> iterator = GTUtility
-                .getFilledContainerToData()
-                .entrySet()
-                .iterator();
-            while (iterator.hasNext()) {
-                Map.Entry<GTItemStack, FluidContainerRegistry.FluidContainerData> entry = iterator.next();
-                final String MODID = GameRegistry.findUniqueIdentifierFor(data.filledContainer.getItem()).modId;
-                if (MainMod.MOD_ID.equals(MODID) || BartWorksCrossmod.MOD_ID.equals(MODID)) continue;
-                if (entry.getValue().fluid.equals(data.fluid)
-                    && !entry.getValue().filledContainer.equals(data.filledContainer)) {
-                    toReplace = entry.getValue().filledContainer;
-                    iterator.remove();
-                }
+        if (!werkstoff.hasItemType(OrePrefixes.cellMolten)) return;
+
+        FluidContainerRegistry.FluidContainerData data = new FluidContainerRegistry.FluidContainerData(
+            new FluidStack(Objects.requireNonNull(WerkstoffLoader.molten.get(werkstoff)), 1 * INGOTS),
+            werkstoff.get(OrePrefixes.cellMolten),
+            Materials.Empty.getCells(1));
+        ItemStack toReplace = null;
+        Iterator<Map.Entry<GTItemStack, FluidContainerRegistry.FluidContainerData>> iterator = GTUtility
+            .getFilledContainerToData()
+            .entrySet()
+            .iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<GTItemStack, FluidContainerRegistry.FluidContainerData> entry = iterator.next();
+            final String MODID = GameRegistry.findUniqueIdentifierFor(data.filledContainer.getItem()).modId;
+            if (MainMod.MOD_ID.equals(MODID) || BartWorksCrossmod.MOD_ID.equals(MODID)) continue;
+            if (entry.getValue().fluid.equals(data.fluid)
+                && !entry.getValue().filledContainer.equals(data.filledContainer)) {
+                toReplace = entry.getValue().filledContainer;
+                iterator.remove();
             }
-            Set<GTRecipe> toremRecipeList = new HashSet<>();
-            if (toReplace != null) {
-                for (RecipeMap<?> map : RecipeMap.ALL_RECIPE_MAPS.values()) {
-                    toremRecipeList.clear();
-                    for (GTRecipe recipe : map.getAllRecipes()) {
-                        for (ItemStack mInput : recipe.mInputs) {
-                            if (GTUtility.areStacksEqual(mInput, toReplace)) {
-                                toremRecipeList.add(recipe);
-                                // recipe.mInputs[i] = data.filledContainer;
-                            }
-                        }
-                        for (ItemStack mOutput : recipe.mOutputs) {
-                            if (GTUtility.areStacksEqual(mOutput, toReplace)) {
-                                toremRecipeList.add(recipe);
-                                // recipe.mOutputs[i] = data.filledContainer;
-                                if (map == RecipeMaps.fluidCannerRecipes
-                                    && GTUtility.areStacksEqual(mOutput, data.filledContainer)
-                                    && !recipe.mFluidInputs[0].equals(data.fluid)) {
-                                    toremRecipeList.add(recipe);
-                                    // recipe.mOutputs[i] = data.filledContainer;
-                                }
-                            }
-                        }
-                        if (recipe.mSpecialItems instanceof ItemStack
-                            && GTUtility.areStacksEqual((ItemStack) recipe.mSpecialItems, toReplace)) {
+        }
+        Set<GTRecipe> toremRecipeList = new HashSet<>();
+        if (toReplace != null) {
+            for (RecipeMap<?> map : RecipeMap.ALL_RECIPE_MAPS.values()) {
+                toremRecipeList.clear();
+                for (GTRecipe recipe : map.getAllRecipes()) {
+                    for (ItemStack mInput : recipe.mInputs) {
+                        if (GTUtility.areStacksEqual(mInput, toReplace)) {
                             toremRecipeList.add(recipe);
-                            // recipe.mSpecialItems = data.filledContainer;
+                            // recipe.mInputs[i] = data.filledContainer;
                         }
                     }
-                    map.getBackend()
-                        .removeRecipes(toremRecipeList);
+                    for (ItemStack mOutput : recipe.mOutputs) {
+                        if (GTUtility.areStacksEqual(mOutput, toReplace)) {
+                            toremRecipeList.add(recipe);
+                            // recipe.mOutputs[i] = data.filledContainer;
+                            if (map == RecipeMaps.fluidCannerRecipes
+                                && GTUtility.areStacksEqual(mOutput, data.filledContainer)
+                                && !recipe.mFluidInputs[0].equals(data.fluid)) {
+                                toremRecipeList.add(recipe);
+                                // recipe.mOutputs[i] = data.filledContainer;
+                            }
+                        }
+                    }
+                    if (recipe.mSpecialItems instanceof ItemStack
+                        && GTUtility.areStacksEqual((ItemStack) recipe.mSpecialItems, toReplace)) {
+                        toremRecipeList.add(recipe);
+                        // recipe.mSpecialItems = data.filledContainer;
+                    }
                 }
+                map.getBackend()
+                    .removeRecipes(toremRecipeList);
             }
-            GTUtility.addFluidContainerData(data);
         }
+        GTUtility.addFluidContainerData(data);
+
     }
 
     private static void runUnficationDeleter(Werkstoff werkstoff) {
