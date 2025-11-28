b6c2a341edd70ff1a1388db6309d1a689f0c4c1e
chochem <40274384+chochem@users.noreply.github.com>
2025-11-28 20:03:51 +0000

    fix amount as well

--- src/main/java/gregtech/api/enums/OrePrefixes.java
+++ src/main/java/gregtech/api/enums/OrePrefixes.java
@@ -2152,7 +2152,7 @@ public class OrePrefixes {
     }
 
     private void addAspect(TCAspects aspect, int amount) {
-        new TC_AspectStack(aspect, 1).addToAspectList(mAspects);
+        new TC_AspectStack(aspect, amount).addToAspectList(mAspects);
     }
 
     public @NotNull String getDefaultLocalName() {
