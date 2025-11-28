231ced4040e9231d84b4054904ee16e1c2e9bff5
lc-1337 <62835225+lc-1337@users.noreply.github.com>
2025-11-28 05:25:20 +0200

    fix stocking crash on world load (#5405)

--- src/main/java/gregtech/common/tileentities/machines/MTEHatchInputBusME.java
+++ src/main/java/gregtech/common/tileentities/machines/MTEHatchInputBusME.java
@@ -717,7 +717,14 @@ public class MTEHatchInputBusME extends MTEHatchInputBus
         IEnergyGrid energy;
 
         try {
-            sg = getProxy().getStorage()
+            AENetworkProxy proxy = getProxy();
+
+            // on some setup endRecipeProcessing() somehow runs before onFirstTick();
+            // test world
+            // https://discord.com/channels/181078474394566657/522098956491030558/1441490828760449124
+            if (!proxy.isReady()) proxy.onReady();
+
+            sg = proxy.getStorage()
                 .getItemInventory();
             energy = getProxy().getEnergy();
         } catch (GridAccessException e) {
