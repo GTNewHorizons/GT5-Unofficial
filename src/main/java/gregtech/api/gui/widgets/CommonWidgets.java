7416f2c75d046417a2c1e78f47060f820f86927a
Maya <10861407+serenibyss@users.noreply.github.com>
2025-11-30 11:56:28 -0700

    wrap in client check and add javadoc

--- src/main/java/gregtech/api/gui/widgets/CommonWidgets.java
+++ src/main/java/gregtech/api/gui/widgets/CommonWidgets.java
@@ -7,6 +7,7 @@ import com.cleanroommc.modularui.api.drawable.IDrawable;
 import com.cleanroommc.modularui.api.drawable.IKey;
 import com.cleanroommc.modularui.drawable.DynamicDrawable;
 import com.cleanroommc.modularui.drawable.text.TextRenderer;
+import com.cleanroommc.modularui.network.NetworkUtils;
 import com.cleanroommc.modularui.value.sync.BooleanSyncValue;
 import com.cleanroommc.modularui.value.sync.IntSyncValue;
 import com.cleanroommc.modularui.value.sync.PanelSyncManager;
@@ -137,31 +138,41 @@ public class CommonWidgets {
             .size(18);
     }
 
+    /**
+     * Returns a title widget positioned on the top left above the panel. Client only!
+     *
+     * @param mte        - The machine to make the title for
+     * @param panelWidth - The width of the machine's main panel
+     * @return machine title widget on the client, null otherwise
+     */
     public static Widget<?> createMachineTitle(IMetaTileEntity mte, int panelWidth) {
-        String title = mte.getLocalName();
+        if (NetworkUtils.isClient()) {
+            String title = mte.getLocalName();
 
-        int borderRadius = 5;
-        int maxWidth = panelWidth - borderRadius * 2;
+            int borderRadius = 5;
+            int maxWidth = panelWidth - borderRadius * 2;
 
-        int titleWidth = TextRenderer.getFontRenderer()
-            .getStringWidth(title);
-        int widgetWidth = Math.min(maxWidth, titleWidth);
+            int titleWidth = TextRenderer.getFontRenderer()
+                .getStringWidth(title);
+            int widgetWidth = Math.min(maxWidth, titleWidth);
 
-        int rows = (int) Math.ceil((double) titleWidth / maxWidth);
-        int heightPerRow = TextRenderer.getFontRenderer().FONT_HEIGHT;
-        int height = heightPerRow * rows;
+            int rows = (int) Math.ceil((double) titleWidth / maxWidth);
+            int heightPerRow = TextRenderer.getFontRenderer().FONT_HEIGHT;
+            int height = heightPerRow * rows;
 
-        return new SingleChildWidget<>().coverChildren()
-            .topRelAnchor(0, 1)
-            .widgetTheme(GTWidgetThemes.BACKGROUND_TITLE)
-            .child(
-                IKey.str(title)
-                    .asWidget()
-                    .size(widgetWidth, height)
-                    .widgetTheme(GTWidgetThemes.TEXT_TITLE)
-                    .marginLeft(5)
-                    .marginRight(5)
-                    .marginTop(5)
-                    .marginBottom(1));
+            return new SingleChildWidget<>().coverChildren()
+                .topRelAnchor(0, 1)
+                .widgetTheme(GTWidgetThemes.BACKGROUND_TITLE)
+                .child(
+                    IKey.str(title)
+                        .asWidget()
+                        .size(widgetWidth, height)
+                        .widgetTheme(GTWidgetThemes.TEXT_TITLE)
+                        .marginLeft(5)
+                        .marginRight(5)
+                        .marginTop(5)
+                        .marginBottom(1));
+        }
+        return null;
     }
 }
