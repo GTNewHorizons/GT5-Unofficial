fd9103a203824b66475738cea84a11d5af1c176c
evgengoldwar <evgeniy10102003kuptsov@gmail.com>
2025-12-15 15:16:52 +0400

    Fix

--- src/main/java/gregtech/api/recipe/maps/CauldronFrontend.java
+++ src/main/java/gregtech/api/recipe/maps/CauldronFrontend.java
@@ -1,10 +1,12 @@
 package gregtech.api.recipe.maps;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
+import gregtech.common.items.ItemFluidDisplay;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumChatFormatting;
 
@@ -18,14 +20,13 @@ import gregtech.api.util.GTUtility;
 import gregtech.api.util.MethodsReturnNonnullByDefault;
 import gregtech.common.gui.modularui.UIHelper;
 import gregtech.nei.GTNEIDefaultHandler;
+import net.minecraft.util.StatCollector;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public class CauldronFrontend extends RecipeMapFrontend {
 
-    private final static List<Pos2d> positions = UIHelper.getGridPositions(2, 30, 24, 2, 1);
-    private final static Pos2d pos1 = positions.get(0);
-    private final static Pos2d pos2 = positions.get(1);
+    private final String consumed = StatCollector.translateToLocal("GT5U.nei.cauldron.consumed");
 
     public CauldronFrontend(BasicUIPropertiesBuilder uiPropertiesBuilder,
         NEIRecipePropertiesBuilder neiPropertiesBuilder) {
@@ -33,32 +34,27 @@ public class CauldronFrontend extends RecipeMapFrontend {
     }
 
     @Override
-    public List<Pos2d> getFluidInputPositions(int fluidInputCount) {
-        ArrayList<Pos2d> position = new ArrayList<>();
-        position.add(new Pos2d(pos2.x + 8, pos2.y));
-        return position;
-    }
-
-    @Override
     public List<Pos2d> getItemInputPositions(int itemInputCount) {
-        ArrayList<Pos2d> position = new ArrayList<>();
-        position.add(new Pos2d(pos1.x, pos1.y));
-        return position;
+        return Arrays.asList(UIHelper.getItemInputPositions(2).get(0));
     }
 
     @Override
     public List<Pos2d> getItemOutputPositions(int itemOutputCount) {
-        ArrayList<Pos2d> position = new ArrayList<>();
-        position.add(new Pos2d(pos2.x + 66, pos2.y));
-        return position;
+        return UIHelper.getItemOutputPositions(1);
     }
+
+    @Override
+    public List<Pos2d> getFluidInputPositions(int fluidInputCount) {
+        return Arrays.asList(UIHelper.getItemInputPositions(2).get(1));
+    }
+
 
     @Override
     public List<String> handleNEIItemTooltip(ItemStack stack, List<String> currentTip,
         GTNEIDefaultHandler.CachedDefaultRecipe neiCachedRecipe) {
-        final ItemStack water = GTUtility.getFluidDisplayStack(Materials.Water.getFluid(1), false);
-        if (stack.isItemEqual(water)) {
-            currentTip.add(EnumChatFormatting.RED + "Consumed per operation");
+
+        if (stack.getItem() instanceof ItemFluidDisplay) {
+            currentTip.add(EnumChatFormatting.RED + consumed);
         }
 
         return super.handleNEIItemTooltip(stack, currentTip, neiCachedRecipe);
