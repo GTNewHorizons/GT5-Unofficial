9c664ffdf276d1a7d38862cb83d65e7a936d4b61
evgengoldwar <69961523+evgengoldwar@users.noreply.github.com>
2025-12-15 17:03:37 +0400

    Improve Nei Cauldron Recipe (#5542)

--- src/main/java/gregtech/api/recipe/maps/CauldronFrontend.java
+++ src/main/java/gregtech/api/recipe/maps/CauldronFrontend.java
@@ -1,14 +1,23 @@
 package gregtech.api.recipe.maps;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.annotation.ParametersAreNonnullByDefault;
 
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.StatCollector;
+
+import com.gtnewhorizons.modularui.api.math.Pos2d;
 
 import gregtech.api.recipe.BasicUIPropertiesBuilder;
 import gregtech.api.recipe.NEIRecipePropertiesBuilder;
 import gregtech.api.recipe.RecipeMapFrontend;
 import gregtech.api.util.MethodsReturnNonnullByDefault;
-import gregtech.nei.RecipeDisplayInfo;
+import gregtech.common.gui.modularui.UIHelper;
+import gregtech.common.items.ItemFluidDisplay;
+import gregtech.nei.GTNEIDefaultHandler;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
@@ -20,10 +29,32 @@ public class CauldronFrontend extends RecipeMapFrontend {
     }
 
     @Override
-    public void drawDescription(RecipeDisplayInfo recipeInfo) {
-        recipeInfo.drawText(StatCollector.translateToLocal("GT5U.cauldron_recipe.supports"));
-        recipeInfo.drawText(StatCollector.translateToLocal("GT5U.cauldron_recipe.consumed"));
+    public List<Pos2d> getItemInputPositions(int itemInputCount) {
+        return Arrays.asList(
+            UIHelper.getItemInputPositions(2)
+                .get(0));
+    }
 
-        drawRecipeOwnerInfo(recipeInfo);
+    @Override
+    public List<Pos2d> getItemOutputPositions(int itemOutputCount) {
+        return UIHelper.getItemOutputPositions(1);
+    }
+
+    @Override
+    public List<Pos2d> getFluidInputPositions(int fluidInputCount) {
+        return Arrays.asList(
+            UIHelper.getItemInputPositions(2)
+                .get(1));
+    }
+
+    @Override
+    public List<String> handleNEIItemTooltip(ItemStack stack, List<String> currentTip,
+        GTNEIDefaultHandler.CachedDefaultRecipe neiCachedRecipe) {
+
+        if (stack.getItem() instanceof ItemFluidDisplay) {
+            currentTip.add(EnumChatFormatting.RED + StatCollector.translateToLocal("GT5U.nei.cauldron.consumed"));
+        }
+
+        return super.handleNEIItemTooltip(stack, currentTip, neiCachedRecipe);
     }
 }
