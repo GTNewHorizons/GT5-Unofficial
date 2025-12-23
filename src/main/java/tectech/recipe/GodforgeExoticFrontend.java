972498ad5cf93565b9aad0d34cb856fc007b0faf
Ranzu <66495944+Ranzuu@users.noreply.github.com>
2025-12-23 13:36:15 +0100

    TecTech

--- src/main/java/tectech/recipe/GodforgeExoticFrontend.java
+++ src/main/java/tectech/recipe/GodforgeExoticFrontend.java
@@ -1,6 +1,5 @@
 package tectech.recipe;
 
-import static gregtech.api.util.GTUtility.trans;
 import static tectech.loader.recipe.Godforge.magmatterItemsForNEI;
 import static tectech.loader.recipe.Godforge.magmatterSpaceFluidItemsForNEI;
 import static tectech.loader.recipe.Godforge.magmatterTimeFluidItemsForNEI;
@@ -70,9 +69,15 @@ public class GodforgeExoticFrontend extends RecipeMapFrontend {
     protected void drawEnergyInfo(RecipeDisplayInfo recipeInfo) {
         long eut = recipeInfo.recipe.mEUt;
         long duration = recipeInfo.recipe.mDuration;
-        recipeInfo.drawText(trans("152", "Total: ") + GTUtility.formatNumbers(eut * duration) + " EU");
-        recipeInfo.drawText(trans("153", "Usage: ") + GTUtility.formatNumbers(eut) + " EU/t");
-        recipeInfo.drawText(trans("158", "Time: ") + GTUtility.formatNumbers(duration / 20) + " secs");
+        recipeInfo.drawText(
+            StatCollector
+                .translateToLocalFormatted("GT5U.gui.text.total_line", GTUtility.formatNumbers(eut * duration)));
+
+        recipeInfo.drawText(
+            StatCollector.translateToLocalFormatted("GT5U.gui.text.usage_line", GTUtility.formatNumbers(eut)));
+
+        recipeInfo.drawText(
+            StatCollector.translateToLocalFormatted("GT5U.gui.text.time_line", GTUtility.formatNumbers(duration / 20)));
 
     }
 
