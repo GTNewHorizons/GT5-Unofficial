a9954f6f831a9c2bfd3cd79bb76a9809c0370dcb
Spicierspace153 <21964509+Spicierspace153@users.noreply.github.com>
2025-12-23 19:46:45 -0700

    mui1 BEGONE

--- src/main/java/gtPlusPlus/xmod/gregtech/api/metatileentity/implementations/MTEHatchChiselBus.java
+++ src/main/java/gtPlusPlus/xmod/gregtech/api/metatileentity/implementations/MTEHatchChiselBus.java
@@ -6,10 +6,6 @@ import com.cleanroommc.modularui.factory.PosGuiData;
 import com.cleanroommc.modularui.screen.ModularPanel;
 import com.cleanroommc.modularui.screen.UISettings;
 import com.cleanroommc.modularui.value.sync.PanelSyncManager;
-import com.gtnewhorizons.modularui.api.screen.ModularWindow;
-import com.gtnewhorizons.modularui.api.screen.UIBuildContext;
-import com.gtnewhorizons.modularui.common.widget.Scrollable;
-import com.gtnewhorizons.modularui.common.widget.SlotWidget;
 
 import gregtech.api.interfaces.ITexture;
 import gregtech.api.interfaces.modularui.IAddUIWidgets;
@@ -68,28 +64,6 @@ public class MTEHatchChiselBus extends MTEHatchInputBus implements IAddUIWidgets
                 + EnumChatFormatting.RED
                 + "[GT++]"
                 + EnumChatFormatting.RESET };
-    }
-
-    @Override
-    public void addUIWidgets(ModularWindow.Builder builder, UIBuildContext buildContext) {
-        int slotIndex = 0;
-        final Scrollable scrollable = new Scrollable().setVerticalScroll();
-        for (int row = 0; row * 4 < inventoryHandler.getSlots() - 1; row++) {
-            int columnsToMake = Math.min(inventoryHandler.getSlots() - row * 4, 4);
-            for (int column = 0; column < columnsToMake; column++) {
-                scrollable.widget(
-                    new SlotWidget(inventoryHandler, slotIndex++).setPos(column * 18, row * 18)
-                        .setSize(18, 18));
-
-            }
-        }
-
-        builder.widget(
-            scrollable.setSize(18 * 4 + 4, 18 * 4)
-                .setPos(52, 7)); // main slots
-        builder.widget(
-            new SlotWidget(inventoryHandler, slotIndex).setPos(18, 18)
-                .setSize(18, 18)); // slot for target
     }
 
 }
