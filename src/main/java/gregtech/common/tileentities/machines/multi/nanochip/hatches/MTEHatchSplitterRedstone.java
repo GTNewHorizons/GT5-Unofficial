5a8798962c3ab8150e6850b38fd29992fb183e16
Spicierspace153 <21964509+Spicierspace153@users.noreply.github.com>
2026-01-12 15:00:58 -0700

    spotless

--- src/main/java/gregtech/common/tileentities/machines/multi/nanochip/hatches/MTEHatchSplitterRedstone.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/nanochip/hatches/MTEHatchSplitterRedstone.java
@@ -1,29 +1,27 @@
 package gregtech.common.tileentities.machines.multi.nanochip.hatches;
 
-import gregtech.api.interfaces.ITexture;
-import gregtech.api.interfaces.metatileentity.IMetaTileEntity;
-import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
-import gregtech.api.metatileentity.implementations.MTEHatch;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.common.util.ForgeDirection;
 
-public class MTEHatchSplitterRedstone extends MTEHatch {
+import gregtech.api.interfaces.ITexture;
+import gregtech.api.interfaces.metatileentity.IMetaTileEntity;
+import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
+import gregtech.api.metatileentity.implementations.MTEHatch;
 
+public class MTEHatchSplitterRedstone extends MTEHatch {
 
     private byte redstoneInput = 0;
     private int channel = 1;
 
-
-    public MTEHatchSplitterRedstone(int aID, String aName, String aNameRegional, int aTier){
+    public MTEHatchSplitterRedstone(int aID, String aName, String aNameRegional, int aTier) {
         super(aID, aName, aNameRegional, aTier, 0, "Allows Redstone Control for Spliter");
     }
 
     public MTEHatchSplitterRedstone(String aName, int aTier, String[] aDescription, ITexture[][][] aTextures) {
         super(aName, aTier, 0, aDescription, aTextures);
     }
-
 
     @Override
     public ITexture[] getTexturesActive(ITexture aBaseTexture) {
@@ -42,14 +40,13 @@ public class MTEHatchSplitterRedstone extends MTEHatch {
 
     @Override
     public String[] getDescription() {
-        return new String[] {
-            "Can be installed in a Splitter module for Nanochip Assembly Complex",
-            "Allows for Redstone Input for rules control"
-        };
+        return new String[] { "Can be installed in a Splitter module for Nanochip Assembly Complex",
+            "Allows for Redstone Input for rules control" };
     }
 
     @Override
-    public void onScrewdriverRightClick(ForgeDirection side, EntityPlayer aPlayer, float aX, float aY, float aZ, ItemStack aTool) {
+    public void onScrewdriverRightClick(ForgeDirection side, EntityPlayer aPlayer, float aX, float aY, float aZ,
+        ItemStack aTool) {
         this.setChannel((channel + 1) % 20);
         aPlayer.addChatComponentMessage(new ChatComponentText(channel + ""));
         super.onScrewdriverRightClick(side, aPlayer, aX, aY, aZ, aTool);
@@ -57,12 +54,11 @@ public class MTEHatchSplitterRedstone extends MTEHatch {
 
     @Override
     public void onPostTick(IGregTechTileEntity baseMetaTileEntity, long tick) {
-        for  (final ForgeDirection side : ForgeDirection.VALID_DIRECTIONS) {
+        for (final ForgeDirection side : ForgeDirection.VALID_DIRECTIONS) {
             this.setRedstoneInput(baseMetaTileEntity.getInputRedstoneSignal(side));
         }
         super.onPostTick(baseMetaTileEntity, tick);
     }
-
 
     public byte getRedstoneInput() {
         return redstoneInput;
@@ -79,7 +75,5 @@ public class MTEHatchSplitterRedstone extends MTEHatch {
     public void setChannel(int channel) {
         this.channel = channel;
     }
-
-
 
 }
