a052c20341329578bea4b640631a213ff1a65299
Maya <10861407+serenibyss@users.noreply.github.com>
2026-01-12 14:29:19 -0700

    Allow dumping vacuum hatch inventories to an ae cell

--- src/main/java/gregtech/common/gui/modularui/hatch/MTEHatchVacuumConveyorGui.java
+++ src/main/java/gregtech/common/gui/modularui/hatch/MTEHatchVacuumConveyorGui.java
@@ -1,12 +1,15 @@
 package gregtech.common.gui.modularui.hatch;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumChatFormatting;
+
+import org.jetbrains.annotations.Nullable;
 
 import com.cleanroommc.modularui.api.drawable.IKey;
 import com.cleanroommc.modularui.api.widget.IWidget;
@@ -27,8 +30,19 @@ import com.cleanroommc.modularui.widgets.ButtonWidget;
 import com.cleanroommc.modularui.widgets.DynamicSyncedWidget;
 import com.cleanroommc.modularui.widgets.SlotGroupWidget;
 import com.cleanroommc.modularui.widgets.layout.Flow;
+import com.cleanroommc.modularui.widgets.slot.ItemSlot;
+import com.cleanroommc.modularui.widgets.slot.ModularSlot;
 import com.gtnewhorizons.modularui.common.internal.network.NetworkUtils;
 
+import appeng.api.AEApi;
+import appeng.api.config.Actionable;
+import appeng.api.networking.security.BaseActionSource;
+import appeng.api.storage.IMEInventory;
+import appeng.api.storage.StorageChannel;
+import appeng.api.storage.data.IAEItemStack;
+import appeng.items.storage.ItemBasicStorageCell;
+import appeng.util.item.AEItemStack;
+import cpw.mods.fml.relauncher.Side;
 import gregtech.api.modularui2.GTGuiTextures;
 import gregtech.api.util.GTUtility;
 import gregtech.api.util.StringUtils;
@@ -36,6 +50,7 @@ import gregtech.common.gui.modularui.hatch.base.MTEHatchBaseGui;
 import gregtech.common.tileentities.machines.multi.nanochip.hatches.MTEHatchVacuumConveyor;
 import gregtech.common.tileentities.machines.multi.nanochip.util.CircuitComponent;
 import gregtech.common.tileentities.machines.multi.nanochip.util.CircuitComponentPacket;
+import it.unimi.dsi.fastutil.Pair;
 
 public class MTEHatchVacuumConveyorGui extends MTEHatchBaseGui<MTEHatchVacuumConveyor> {
 
@@ -57,11 +72,13 @@ public class MTEHatchVacuumConveyorGui extends MTEHatchBaseGui<MTEHatchVacuumCon
             .width(40)
             .coverChildrenHeight()
             .crossAxisAlignment(Alignment.CrossAxis.END);
+        column.child(createCellDrainRow(syncManager));
         column.child(createVoidButton(syncManager));
         return column.align(Alignment.CenterRight)
             .marginRight(3);
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public void registerSyncValues(PanelSyncManager syncManager) {
         super.registerSyncValues(syncManager);
@@ -76,6 +93,33 @@ public class MTEHatchVacuumConveyorGui extends MTEHatchBaseGui<MTEHatchVacuumCon
             },
             null);
         syncManager.syncValue("contents", contentsSyncHandler);
+
+        syncManager.registerSyncedAction("dumpCCs", Side.SERVER, p -> {
+            GenericSyncValue<CircuitComponentPacket> syncContents = syncManager
+                .findSyncHandler("contents", GenericSyncValue.class);
+
+            ItemStack stack = hatch.inventoryHandler.getStackInSlot(0);
+            if (stack == null) return;
+            if (!(stack.getItem() instanceof ItemBasicStorageCell)) return;
+
+            CircuitComponentPacket ccs = syncContents.getValue();
+            if (ccs.getComponents()
+                .isEmpty()) return;
+
+            BaseActionSource src = new BaseActionSource();
+
+            List<Pair<ItemStack, Long>> leftovers = new ArrayList<>();
+
+            for (var entry : ccs.getComponents()
+                .entrySet()) {
+                CircuitComponent cc = entry.getKey();
+                long amountLeft = outputCircuitComponent(cc, entry.getValue(), stack, src);
+                if (amountLeft > 0) {
+                    leftovers.add(Pair.of(cc.getFakeStack(1), amountLeft));
+                }
+            }
+            hatch.contents = new CircuitComponentPacket(leftovers);
+        });
     }
 
     @SuppressWarnings("unchecked")
@@ -154,9 +198,54 @@ public class MTEHatchVacuumConveyorGui extends MTEHatchBaseGui<MTEHatchVacuumCon
                 syncContents.setValue(new CircuitComponentPacket());
                 return true;
             })
-            .tooltip(t -> {
-                t.addLine("Delete Stored Circuit Components");;
-            });
+            .tooltip(t -> { t.addLine("Delete Stored Circuit Components"); });
     }
 
+    protected IWidget createCellDrainRow(PanelSyncManager syncManager) {
+        return Flow.row()
+            .coverChildren()
+            .child(
+                new ButtonWidget<>().size(18)
+                    .overlay(GTGuiTextures.PICTURE_ARROW_WHITE_DOWN) // todo
+                    .onMousePressed(d -> {
+                        syncManager.callSyncedAction("dumpCCs", $ -> {});
+                        return true;
+                    })
+                    .tooltip(t -> t.addLine("Empty Stored Circuit Components to an AE2 Cell")))
+            .child(new ItemSlot().slot(new ModularSlot(hatch.inventoryHandler, 0) {
+
+                @Override
+                public boolean isItemValid(@Nullable ItemStack stack) {
+                    if (stack == null) return true;
+                    return AEApi.instance()
+                        .registries()
+                        .cell()
+                        .isCellHandled(stack);
+                }
+            })
+                .size(18));
+    }
+
+    @SuppressWarnings("unchecked")
+    private long outputCircuitComponent(CircuitComponent cc, long amount, ItemStack storageCell, BaseActionSource src) {
+        IMEInventory<IAEItemStack> cellInventory = AEApi.instance()
+            .registries()
+            .cell()
+            .getCellInventory(storageCell, null, StorageChannel.ITEMS);
+
+        long maxDrain = amount;
+        // todo make this be the real item not the CC/PC
+        ItemStack stack = cc.getFakeStack(1);
+        AEItemStack aeStack = AEItemStack.create(stack);
+        aeStack.setStackSize(maxDrain);
+
+        IAEItemStack retStack = cellInventory.injectItems(aeStack, Actionable.SIMULATE, src);
+        if (retStack != null && retStack.getStackSize() > 0) {
+            maxDrain = maxDrain - retStack.getStackSize();
+            aeStack.setStackSize(maxDrain);
+        }
+        cellInventory.injectItems(aeStack, Actionable.MODULATE, src);
+
+        return amount - maxDrain;
+    }
 }
