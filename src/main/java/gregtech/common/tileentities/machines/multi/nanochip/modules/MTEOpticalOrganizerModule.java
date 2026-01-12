45aa1516b9cf4a7fdc6893f1071b9206a653588c
Nockyx <95299389+Nockyx@users.noreply.github.com>
2026-01-11 23:44:47 -0300

    Standardise Module File Names

--- src/main/java/gregtech/common/tileentities/machines/multi/nanochip/modules/MTEOpticalOrganizerModule.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/nanochip/modules/MTEOpticalOrganizerModule.java
@@ -12,7 +12,6 @@ import com.gtnewhorizon.structurelib.structure.IStructureDefinition;
 import com.gtnewhorizon.structurelib.structure.ISurvivalBuildEnvironment;
 
 import gregtech.api.GregTechAPI;
-import gregtech.api.casing.Casings;
 import gregtech.api.interfaces.metatileentity.IMetaTileEntity;
 import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
 import gregtech.api.recipe.RecipeMap;
@@ -23,61 +22,58 @@ import gregtech.common.tileentities.machines.multi.nanochip.util.CircuitComponen
 import gregtech.common.tileentities.machines.multi.nanochip.util.ModuleStructureDefinition;
 import gtPlusPlus.core.material.MaterialsElements;
 
-public class CuttingChamber extends MTENanochipAssemblyModuleBase<CuttingChamber> {
+public class MTEOpticalOrganizerModule extends MTENanochipAssemblyModuleBase<MTEOpticalOrganizerModule> {
 
     protected static final String STRUCTURE_PIECE_MAIN = "main";
-    protected static final int CUTTING_OFFSET_X = 3;
-    protected static final int CUTTING_OFFSET_Y = 5;
-    protected static final int CUTTING_OFFSET_Z = 0;
-    protected static final String[][] CUTTING_STRUCTURE = new String[][] {
-        { "       ", "       ", "       ", " A   A ", " A   A " },
-        { "  CCC  ", " BBBBB ", " CEEEC ", "ACEEECA", "ACEDECA" },
-        { " CCDCC ", "B     B", "B     B", "B     B", "B  D  B" },
-        { " DDDDD ", "BCCCCCB", "EAAAAAE", "E     E", "EDDDDDE" },
-        { " CCDCC ", "B     B", "B     B", "B     B", "B  D  B" },
-        { "  CCC  ", " BBBBB ", " CEEEC ", "ACEEECA", "ACEDECA" },
-        { "       ", "       ", "       ", " A   A ", " A   A " } };
-
-    public static final IStructureDefinition<CuttingChamber> STRUCTURE_DEFINITION = ModuleStructureDefinition
-        .<CuttingChamber>builder()
-        .addShape(STRUCTURE_PIECE_MAIN, CUTTING_STRUCTURE)
-        // Celestial Tungsten Framebox
+    protected static final int OPTICAL_OFFSET_X = 3;
+    protected static final int OPTICAL_OFFSET_Y = 7;
+    protected static final int OPTICAL_OFFSET_Z = 0;
+    protected static final String[][] OPTICAL_STRING = new String[][] {
+        { "       ", " BBCBB ", " AACAA ", "   C   ", "   C   ", "   C   ", " AACAA " },
+        { " AAAAA ", "BBBCBBB", "ABD DBA", " BD DB ", " BD DB ", " BD DB ", "ABD DBA" },
+        { " A   A ", "BBCCCBB", "AD   DA", " DCCCD ", " D   D ", " DCCCD ", "AD   DA" },
+        { " A   A ", "CCCBCCC", "C  C  C", "C CCC C", "C     C", "C CCC C", "C  C  C" },
+        { " A   A ", "BBCCCBB", "AD   DA", " DCC D ", " D   D ", " DCCCD ", "AD   DA" },
+        { " AAAAA ", "BBBCBBB", "ABD DBA", " BD DB ", " BD DB ", " BD DB ", "ABD DBA" },
+        { "       ", " BBCBB ", " AACAA ", "   C   ", "   C   ", "   C   ", " AACAA " } };
+    public static final IStructureDefinition<MTEOpticalOrganizerModule> STRUCTURE_DEFINITION = ModuleStructureDefinition
+        .<MTEOpticalOrganizerModule>builder()
+        .addShape(STRUCTURE_PIECE_MAIN, OPTICAL_STRING)
+        // Hypogen Frame Box
         .addElement(
             'A',
             lazy(
                 t -> ofBlock(
                     Block.getBlockFromItem(
-                        MaterialsElements.STANDALONE.CELESTIAL_TUNGSTEN.getFrameBox(1)
+                        MaterialsElements.STANDALONE.HYPOGEN.getFrameBox(1)
                             .getItem()),
-                    MaterialsElements.STANDALONE.CELESTIAL_TUNGSTEN.getFrameBox(1)
+                    MaterialsElements.STANDALONE.HYPOGEN.getFrameBox(1)
                         .getItemDamage())))
         // White casing block
         .addElement('B', ofBlock(GregTechAPI.sBlockCasings8, 5))
         // Black casing block
         .addElement('C', ofBlock(GregTechAPI.sBlockCasings8, 10))
-        // Bulk Production Frame
-        .addElement('D', Casings.BulkProductionFrame.asElement(null))
-        // Black glass
-        .addElement('E', ofBlock(GregTechAPI.sBlockTintedGlass, 3))
+        // Non-Photonic Matter Exclusion Glass
+        .addElement('D', ofBlock(GregTechAPI.sBlockGlass1, 3))
         .build();
 
-    public CuttingChamber(int aID, String aName, String aNameRegional) {
+    public MTEOpticalOrganizerModule(int aID, String aName, String aNameRegional) {
         super(aID, aName, aNameRegional);
     }
 
-    protected CuttingChamber(String aName) {
+    protected MTEOpticalOrganizerModule(String aName) {
         super(aName);
     }
 
     @Override
-    public IStructureDefinition<CuttingChamber> getStructureDefinition() {
+    public IStructureDefinition<MTEOpticalOrganizerModule> getStructureDefinition() {
         return STRUCTURE_DEFINITION;
     }
 
     @Override
     public void construct(ItemStack trigger, boolean hintsOnly) {
         // Should only construct the main structure, since the base structure is built by the nanochip assembly complex.
-        buildPiece(STRUCTURE_PIECE_MAIN, trigger, hintsOnly, CUTTING_OFFSET_X, CUTTING_OFFSET_Y, CUTTING_OFFSET_Z);
+        buildPiece(STRUCTURE_PIECE_MAIN, trigger, hintsOnly, OPTICAL_OFFSET_X, OPTICAL_OFFSET_Y, OPTICAL_OFFSET_Z);
     }
 
     @Override
@@ -86,9 +82,9 @@ public class CuttingChamber extends MTENanochipAssemblyModuleBase<CuttingChamber
         return survivialBuildPiece(
             STRUCTURE_PIECE_MAIN,
             trigger,
-            CUTTING_OFFSET_X,
-            CUTTING_OFFSET_Y,
-            CUTTING_OFFSET_Z,
+            OPTICAL_OFFSET_X,
+            OPTICAL_OFFSET_Y,
+            OPTICAL_OFFSET_Z,
             elementBudget,
             env,
             false,
@@ -100,32 +96,31 @@ public class CuttingChamber extends MTENanochipAssemblyModuleBase<CuttingChamber
         // Check base structure
         if (!super.checkMachine(aBaseMetaTileEntity, aStack)) return false;
         // Now check module structure
-        return checkPiece(STRUCTURE_PIECE_MAIN, CUTTING_OFFSET_X, CUTTING_OFFSET_Y, CUTTING_OFFSET_Z);
+        return checkPiece(STRUCTURE_PIECE_MAIN, OPTICAL_OFFSET_X, OPTICAL_OFFSET_Y, OPTICAL_OFFSET_Z);
     }
 
     @Override
     protected MultiblockTooltipBuilder createTooltip() {
         return new MultiblockTooltipBuilder().addMachineType("NAC Module")
             .addInfo(NAC_MODULE)
-            .addInfo("Cuts your Wafer " + TOOLTIP_CC + "s")
+            .addInfo("Optimizes your Optical " + TOOLTIP_CC + "s")
             .addInfo("Outputs into the VCO with the same color as the input VCI")
             .addStructureInfo("Any base casing - Vacuum Conveyor Input")
-            .addStructureInfo("Any base casing - Input Hatch")
             .addStructureInfo("Any base casing - Vacuum Conveyor Output")
             .toolTipFinisher("GregTech");
     }
 
     @Override
     public IMetaTileEntity newMetaEntity(IGregTechTileEntity aTileEntity) {
-        return new CuttingChamber(this.mName);
+        return new MTEOpticalOrganizerModule(this.mName);
     }
 
     public static void registerLocalName(String unprocessedName, CircuitComponent component) {
-        component.fallbackLocalizedName = "Cut " + unprocessedName;
+        component.fallbackLocalizedName = "Optimized " + unprocessedName;
     }
 
     @Override
     public RecipeMap<?> getRecipeMap() {
-        return RecipeMaps.nanochipCuttingChamber;
+        return RecipeMaps.nanochipOpticalOrganizer;
     }
 }
