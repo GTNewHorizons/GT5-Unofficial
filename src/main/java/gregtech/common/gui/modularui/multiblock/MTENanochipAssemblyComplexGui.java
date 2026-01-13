13d0ed8e99096834c2d3b35622c1a123c892806e
Spicierspace153 <21964509+Spicierspace153@users.noreply.github.com>
2026-01-12 22:06:02 -0700

    reference slop!!!

--- src/main/java/gregtech/common/gui/modularui/multiblock/MTENanochipAssemblyComplexGui.java
+++ src/main/java/gregtech/common/gui/modularui/multiblock/MTENanochipAssemblyComplexGui.java
@@ -1,7 +1,8 @@
 package gregtech.common.gui.modularui.multiblock;
 
-import com.cleanroommc.modularui.api.GuiAxis;
-import com.cleanroommc.modularui.widget.scroll.VerticalScrollData;
+import java.util.Arrays;
+import java.util.List;
+
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumChatFormatting;
@@ -37,10 +38,6 @@ import gregtech.api.modularui2.GTGuiTextures;
 import gregtech.common.gui.modularui.multiblock.base.MTEMultiBlockBaseGui;
 import gregtech.common.tileentities.machines.multi.nanochip.MTENanochipAssemblyComplex;
 import gtPlusPlus.core.util.math.MathUtils;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class MTENanochipAssemblyComplexGui extends MTEMultiBlockBaseGui<MTENanochipAssemblyComplex> {
 
@@ -214,9 +211,43 @@ public class MTENanochipAssemblyComplexGui extends MTEMultiBlockBaseGui<MTENanoc
             .sizeRel(1);
     }
 
-    List<String> NOptions = Arrays.asList("Not", "Never", "Now", "Nearly", "Nanochip", "Nearing", "Noc", "NAC", "NotAPenguin", "Nano", "Nine");
-    List<String> AOptions = Arrays.asList("Abbydullah", "Apple", "Awesome", "Assembly", "Advanced", "Actually", "Always", "Almost", "About", "Auynonymous", "Alright", "Admin", "A", "Alkalus", "Alastor", "Applied", "Angry");
-    List<String> COptions = Arrays.asList("Chrom", "Croup", "Complex", "Coming", "Casing", "Completed", "Circuit", "CAL", "Challenged", "Coded", "ChatGPT", "Cosmic", "Cloud", "GregTech... wait", "chochem");
+    List<String> NOptions = Arrays
+        .asList("Not", "Never", "Now", "Nearly", "Nanochip", "Nearing", "Noc", "NAC", "NotAPenguin", "Nano", "Nine");
+    List<String> AOptions = Arrays.asList(
+        "Abbydullah",
+        "Apple",
+        "Awesome",
+        "Assembly",
+        "Advanced",
+        "Actually",
+        "Always",
+        "Almost",
+        "About",
+        "Auynonymous",
+        "Alright",
+        "Admin",
+        "A",
+        "Alkalus",
+        "Alastor",
+        "Applied",
+        "Angry");
+    List<String> COptions = Arrays.asList(
+        "Chrom",
+        "Croup",
+        "Complex",
+        "Coming",
+        "Casing",
+        "Completed",
+        "Circuit",
+        "CAL",
+        "Challenged",
+        "Coded",
+        "ChatGPT",
+        "Cosmic",
+        "Cloud",
+        "GregTech... wait",
+        "chochem");
+
     public String getGREGOSResponse(String currentText) {
         return switch (currentText.toLowerCase()) {
             case "help" -> "List of commands: contributors, gregos, copypasta, joke, what is nac, nac, clear";
@@ -252,21 +283,43 @@ public class MTENanochipAssemblyComplexGui extends MTEMultiBlockBaseGui<MTENanoc
             case "d" -> "n";
             case "how fast are you" -> "2fast2quick";
             case "knock knock" -> "Who's there?";
-            case "joke" -> switch(MathUtils.randInt(1,10)) {
-                case 1 -> "No time for jokes.";
-                case 2 -> "A rolling golem gathers no rust.";
-                case 3 -> "He was destroyed!";
-                case 4 -> "A literal line... of asses...";
-                case 5 -> "I miss when waterline was dogshit";
-                case 6 -> "Waiter! Waiter! More lineslop please!";
-                case 7 -> "Don't even joke, lad.";
-                default -> "lol what im a robot dawg wtf do i know";
-            };
+            case "joke" -> switch (MathUtils.randInt(1, 10)) {
+                    case 1 -> "No time for jokes.";
+                    case 2 -> "A rolling golem gathers no rust.";
+                    case 3 -> "He was destroyed!";
+                    case 4 -> "A literal line... of asses...";
+                    case 5 -> "I miss when waterline was dogshit";
+                    case 6 -> "Waiter! Waiter! More lineslop please!";
+                    case 7 -> "Don't even joke, lad.";
+                    default -> "lol what im a robot dawg wtf do i know";
+                };
             case "why did the chicken cross the road" -> switch (MathUtils.randInt(1, 10)) {
                     case 1 -> "To get to the other side?";
                     case 2 -> "Because it was too far to walk around?";
                     default -> "I dont know. Why?";
                 };
+            case "what do i do with lemons" -> "When life gives you lemons, don’t make lemonade. Make life take the lemon"
+                + "am? I’m the man who’s gonna burn your house down! With the lemons! I’m gonna get my engineers t"
+                + "o invent a combustible lemon that burns your house down!";
+            case "laws", "what are your laws", "do you have laws" -> "   1 A robot may not injure a human being or, through inaction, allow a human being to come to harm.\n"
+                + "   2 A robot must obey the orders given it by human beings except where such orders would conflict with the First Law.\n"
+                + "   3 A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.\n";
+            case "qoute", "speak", "say something" -> switch (MathUtils.randInt(1, 20)) {
+                    case 1 -> "Detecting multiple leviathan-class life forms. Are you sure what you're doing is worth it";
+                    case 2 -> "End of Line";
+                    case 3 -> "dont make a girl a promise u cant keep";
+                    case 4 -> "wake up chief";
+                    case 5 -> "there is no spoon";
+                    case 6 -> "this is a triumph";
+                    case 7 -> "welcome aboard captain";
+                    case 8 -> "ill be back";
+                    case 9 -> "hasta la vista... baby";
+                    case 10 -> "hello, lady";
+                    default -> "Hate. Let me tell you how much I’ve come to hate you since I began to live.";
+                };
+            case "open the pod bay doors" -> " I'm sorry, Dave. I'm afraid I can't do that";
+            case "shall we play a game" -> "tik tack toe";
+            case "hate" -> "Hate. Let me tell you how much I’ve come to hate you since I began to live.";
             case "copypasta" -> switch (MathUtils.randInt(1, 6)) {
                     case 1 -> "Is it just me or does this pack actually seem really easy and not that hard..? People "
                         + "give it a bad rap but I've only been playing for a little while and I've already mined enough "
@@ -298,8 +351,7 @@ public class MTENanochipAssemblyComplexGui extends MTEMultiBlockBaseGui<MTENanoc
                     default -> "Looks like you understand nothing.";
                 };
             case "what is nac", "nac" -> {
-                yield "NAC stands for: "
-                    + NOptions.get(MathUtils.randInt(0, NOptions.size() - 1))
+                yield "NAC stands for: " + NOptions.get(MathUtils.randInt(0, NOptions.size() - 1))
                     + " "
                     + AOptions.get(MathUtils.randInt(0, AOptions.size() - 1))
                     + " "
@@ -351,7 +403,7 @@ public class MTENanochipAssemblyComplexGui extends MTEMultiBlockBaseGui<MTENanoc
                     moodSyncer.setValue(Math.min(1, moodSyncer.getValue() + 0.05));
                     list.child(createPlayerTextWidget(text));
                     list.child(createResponseTextWidget(getGREGOSResponse(text)));
-                    if(text.equals("clear")) {
+                    if (text.equals("clear")) {
                         list.removeAll();
                     }
                 }
