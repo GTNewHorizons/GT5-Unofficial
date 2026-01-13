c06cec7ac463296ede8cbe7959a0bd41b9e68c75
Spicierspace153 <21964509+Spicierspace153@users.noreply.github.com>
2026-01-12 19:50:55 -0700

    yay

--- src/main/java/gregtech/common/tileentities/machines/multi/nanochip/hatches/MTEHatchSplitterRedstone.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/nanochip/hatches/MTEHatchSplitterRedstone.java
@@ -2,6 +2,7 @@ package gregtech.common.tileentities.machines.multi.nanochip.hatches;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.common.util.ForgeDirection;
 
@@ -11,6 +12,8 @@ import com.cleanroommc.modularui.screen.ModularPanel;
 import com.cleanroommc.modularui.screen.UISettings;
 import com.cleanroommc.modularui.value.sync.PanelSyncManager;
 
+import gregtech.api.enums.Textures;
+import gregtech.api.interfaces.IIconContainer;
 import gregtech.api.interfaces.ITexture;
 import gregtech.api.interfaces.metatileentity.IMetaTileEntity;
 import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
@@ -18,6 +21,7 @@ import gregtech.api.metatileentity.implementations.MTEHatch;
 import gregtech.api.modularui2.GTGuiTheme;
 import gregtech.api.modularui2.GTGuiThemes;
 import gregtech.api.modularui2.MetaTileEntityGuiHandler;
+import gregtech.api.render.TextureFactory;
 import gregtech.common.gui.modularui.hatch.MTEHatchSplitterRedstoneGui;
 
 public class MTEHatchSplitterRedstone extends MTEHatch {
@@ -25,6 +29,10 @@ public class MTEHatchSplitterRedstone extends MTEHatch {
     private byte redstoneInput = 0;
     private int channel = 1;
     public int MAX_CHANNEL = 20;
+
+    // todo: change these :3
+    private static final IIconContainer textureFont = Textures.BlockIcons.OVERLAY_HATCH_PH_SENSOR;
+    private static final IIconContainer textureFont_Glow = Textures.BlockIcons.OVERLAY_HATCH_PH_SENSOR_GLOW;
 
     public MTEHatchSplitterRedstone(int aID, String aName, String aNameRegional, int aTier) {
         super(aID, aName, aNameRegional, aTier, 0, "Allows Redstone Control for Spliter");
@@ -35,8 +43,21 @@ public class MTEHatchSplitterRedstone extends MTEHatch {
     }
 
     @Override
+    public boolean isFacingValid(ForgeDirection facing) {
+        return true;
+    }
+
+    @Override
+    public void initDefaultModes(NBTTagCompound aNBT) {
+        getBaseMetaTileEntity().setActive(true);
+    }
+
+    @Override
     public ITexture[] getTexturesActive(ITexture aBaseTexture) {
-        return new ITexture[0];
+        return new ITexture[] { aBaseTexture, TextureFactory.of(textureFont), TextureFactory.builder()
+            .addIcon(textureFont_Glow)
+            .glow()
+            .build() };
     }
 
     @Override
@@ -49,7 +70,7 @@ public class MTEHatchSplitterRedstone extends MTEHatch {
 
     @Override
     public ITexture[] getTexturesInactive(ITexture aBaseTexture) {
-        return new ITexture[0];
+        return new ITexture[] { aBaseTexture, TextureFactory.of(textureFont) };
     }
 
     @Override
