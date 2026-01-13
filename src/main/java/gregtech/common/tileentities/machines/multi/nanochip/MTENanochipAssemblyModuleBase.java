a70fcda734e230ee00e4bac2cfe0922ad6ad7bc2
Spicierspace153 <21964509+Spicierspace153@users.noreply.github.com>
2026-01-13 13:42:53 -0700

    allow modules to affect mood if we want it

--- src/main/java/gregtech/common/tileentities/machines/multi/nanochip/MTENanochipAssemblyModuleBase.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/nanochip/MTENanochipAssemblyModuleBase.java
@@ -441,6 +441,12 @@ public abstract class MTENanochipAssemblyModuleBase<T extends MTEExtendedPowerMu
         }
     }
 
+    //call this method to allow the module to affect nac's mood (if we want it )
+
+    protected double calculateMoodMultiplier(){
+        return 1;
+    }
+
     @Override
     public long maxEUStore() {
         return euBufferSize;
