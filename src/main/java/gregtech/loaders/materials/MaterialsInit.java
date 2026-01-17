cca0cc646c7778ed7375ba96487047a2f58300c1
GirixK <GirixK@users.noreply.github.com>
2026-01-17 05:34:59 +0100

    Revert "Molten Bisphenol A"
    This reverts commit 3882396238e57d05abd0102a26925c84bb0e5a37.

--- src/main/java/gregtech/loaders/materials/MaterialsInit.java
+++ src/main/java/gregtech/loaders/materials/MaterialsInit.java
@@ -7833,6 +7833,7 @@ public class MaterialsInit {
             .addMaterial(Materials.Carbon, 15)
             .addMaterial(Materials.Hydrogen, 16)
             .addMaterial(Materials.Oxygen, 2)
+            .addElectrolyzerRecipe()
             .constructMaterial();
     }
 
