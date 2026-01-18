73a929d9ead6c40cc8b3d4d2251d618027f8709b
boubou19 <miisterunknown@gmail.com>
2026-01-18 01:15:13 +0100

    add dna flasks and plasmid cells enum entries

--- src/main/java/bartworks/API/enums/BioDataEnum.java
+++ src/main/java/bartworks/API/enums/BioDataEnum.java
@@ -1,26 +1,27 @@
 package bartworks.API.enums;
 
+import gregtech.api.enums.ItemList;
 import gregtech.api.enums.VoltageIndex;
 import net.minecraft.item.EnumRarity;
 
 public enum BioDataEnum {
-    BetaLactamase("beta-Lactamase", 0, EnumRarity.uncommon, 100_00, VoltageIndex.ULV),
-    SaccharomycesCerevisiae("Saccharomyces cerevisiae", 1, EnumRarity.common, 75_00, VoltageIndex.ULV),
-    NullBioData("", 2, EnumRarity.epic, 75_00, VoltageIndex.ULV),
-    SaccharomycesCerevisiaeVarBayanus("Saccharomyces cerevisiae var bayanus", 3, EnumRarity.uncommon, 75_00, VoltageIndex.ULV),
-    SaccharomycesCerevisiaeVarCerevisiae("Saccharomyces cerevisiae var cerevisiae", 4, EnumRarity.uncommon, 75_00, VoltageIndex.ULV),
-    EscherichiaKoli("Escherichia koli", 5, EnumRarity.uncommon, 100_00, VoltageIndex.ULV),
-    PseudomonasVeronii("Pseudomonas Veronii", 6, EnumRarity.uncommon, 75_00, VoltageIndex.ULV),
-    PseudomonasVeronii2("Pseudomonas Veronii", 7, EnumRarity.uncommon, 50_00, VoltageIndex.LV),
-    BarnadafisArboriatoris("Barnadafis Arboriatoris", 8, EnumRarity.rare, 7_50, VoltageIndex.MV),
-    TcetieisFucusSerratus("TCetiEis Fucus Serratus", 9, EnumRarity.rare, 7_50, VoltageIndex.MV),
-    Ovumbac("OvumBac", 10, EnumRarity.rare, 15_00, VoltageIndex.MV),
-    Stemcellbac("StemCellBac", 11, EnumRarity.rare, 7_50, VoltageIndex.HV),
-    Biocellbac("BioCellBac", 12, EnumRarity.epic, 3_00, VoltageIndex.EV),
-    Binnigrowthmedium("BinniGrowthMedium", 13, EnumRarity.common, 90_00, VoltageIndex.ULV),
-    Binnibacteria("BinniBacteria", 14, EnumRarity.common, 60_00, VoltageIndex.ULV),
-    Bacterialsludgebac("BacterialSludgeBac", 15, EnumRarity.uncommon, 30_00, VoltageIndex.LV),
-    Mutagen("Mutagen", 16, EnumRarity.rare, 15_00, VoltageIndex.MV),
+    BetaLactamase("beta-Lactamase", 0, EnumRarity.uncommon, 100_00, VoltageIndex.ULV, ItemList.DNABetaLactamase, ItemList.PlasmidBetaLactamase),
+    SaccharomycesCerevisiae("Saccharomyces cerevisiae", 1, EnumRarity.common, 75_00, VoltageIndex.ULV, ItemList.DNASaccharomycesCerevisiae, ItemList.PlasmidSaccharomycesCerevisiae),
+    NullBioData("", 2, EnumRarity.epic, 75_00, VoltageIndex.ULV, ItemList.DNANull, ItemList.PlasmidNull),
+    SaccharomycesCerevisiaeVarBayanus("Saccharomyces cerevisiae var bayanus", 3, EnumRarity.uncommon, 75_00, VoltageIndex.ULV, ItemList.DNASaccharomycesCerevisiaeVarBayanus, ItemList.PlasmidSaccharomycesCerevisiaeVarBayanus ),
+    SaccharomycesCerevisiaeVarCerevisiae("Saccharomyces cerevisiae var cerevisiae", 4, EnumRarity.uncommon, 75_00, VoltageIndex.ULV, ItemList.DNASaccharomycesCerevisiaeVarCerevisiae, ItemList.PlasmidSaccharomycesCerevisiaeVarCerevisiae ),
+    EscherichiaKoli("Escherichia koli", 5, EnumRarity.uncommon, 100_00, VoltageIndex.ULV, ItemList.DNAEscherichiaKoli, ItemList.PlasmidEscherichiaKoli),
+    PseudomonasVeronii("Pseudomonas Veronii", 6, EnumRarity.uncommon, 75_00, VoltageIndex.ULV, ItemList.DNAPseudomonasVeronii, ItemList.PlasmidPseudomonasVeronii),
+    PseudomonasVeronii2("Pseudomonas Veronii", 7, EnumRarity.uncommon, 50_00, VoltageIndex.LV, ItemList.DNAPseudomonasVeronii2, ItemList.PlasmidPseudomonasVeronii2),
+    BarnadafisArboriatoris("Barnadafis Arboriatoris", 8, EnumRarity.rare, 7_50, VoltageIndex.MV, ItemList.DNABarnadafisArboriatoris, ItemList.PlasmidBarnadafisArboriatoris),
+    TcetieisFucusSerratus("TCetiEis Fucus Serratus", 9, EnumRarity.rare, 7_50, VoltageIndex.MV, ItemList.DNATcetieisFucusSerratus, ItemList.PlasmidTcetieisFucusSerratus),
+    Ovumbac("OvumBac", 10, EnumRarity.rare, 15_00, VoltageIndex.MV, ItemList.DNAOvumbac, ItemList.PlasmidOvumbac),
+    Stemcellbac("StemCellBac", 11, EnumRarity.rare, 7_50, VoltageIndex.HV, ItemList.DNAStemcellbac, ItemList.PlasmidStemcellbac),
+    Biocellbac("BioCellBac", 12, EnumRarity.epic, 3_00, VoltageIndex.EV, ItemList.DNABiocellbac, ItemList.PlasmidBiocellbac),
+    Binnigrowthmedium("BinniGrowthMedium", 13, EnumRarity.common, 90_00, VoltageIndex.ULV, ItemList.DNABinnigrowthmedium, ItemList.PlasmidBinnigrowthmedium ),
+    Binnibacteria("BinniBacteria", 14, EnumRarity.common, 60_00, VoltageIndex.ULV, ItemList.DNABinnibacteria, ItemList.PlasmidBinnibacteria),
+    Bacterialsludgebac("BacterialSludgeBac", 15, EnumRarity.uncommon, 30_00, VoltageIndex.LV, ItemList.DNABacterialsludgebac, ItemList.PlasmidBacterialsludgebac),
+    Mutagen("Mutagen", 16, EnumRarity.rare, 15_00, VoltageIndex.MV, ItemList.DNAMutagen, ItemList.PlasmidMutagen ),
 
 
     ;
@@ -29,12 +30,16 @@ public enum BioDataEnum {
     public final EnumRarity rarity;
     public final int chance;
     public final int tier;
+    public final ItemList DNASampleFlask;
+    public final ItemList plasmidCell;
 
-    BioDataEnum(String name, int id, EnumRarity rarity, int chance, int tier){
+    BioDataEnum(String name, int id, EnumRarity rarity, int chance, int tier, ItemList DNASampleFlask, ItemList plasmidCell){
         this.name = name;
         this.id = id;
         this.rarity = rarity;
         this.chance = chance;
         this.tier=tier;
+        this.DNASampleFlask = DNASampleFlask;
+        this.plasmidCell = plasmidCell;
     }
 }
