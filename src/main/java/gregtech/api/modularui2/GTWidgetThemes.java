35d6d2256ac27b9529aaf8b0b9bb32cf4181a219
chrombread <chrombread8@gmail.com>
2026-01-21 09:37:08 -0800

    no more tectec standard background

--- src/main/java/gregtech/api/modularui2/GTWidgetThemes.java
+++ src/main/java/gregtech/api/modularui2/GTWidgetThemes.java
@@ -47,8 +47,7 @@ public final class GTWidgetThemes {
     public static WidgetThemeKey<WidgetTheme> BACKGROUND_TITLE = registerThemedTexture("backgroundTitle");
     public static WidgetThemeKey<WidgetTheme> BACKGROUND_TERMINAL = themeApi
         .widgetThemeKeyBuilder("backgroundTerminal", WidgetTheme.class)
-        .defaultTheme(
-            new WidgetTheme(0, 0, GTGuiTextures.TT_BACKGROUND_TEXT_FIELD, Color.WHITE.main, 0xFAFAFA, false, 0))
+        .defaultTheme(new WidgetTheme(0, 0, GTGuiTextures.BACKGROUND_TEXT_FIELD, Color.WHITE.main, 0xFAFAFA, false, 0))
         .defaultHoverTheme(null)
         .register();
 
