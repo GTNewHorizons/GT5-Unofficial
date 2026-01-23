f9d9cbef381908677756c064075d20791dc5e465
Connor Colenso <colen@CONNORSPC>
2026-01-23 20:31:57 +0000

    Re-add attribution.

--- src/main/java/bartworks/util/NoiseUtil/SimplexNoise.java
+++ src/main/java/bartworks/util/NoiseUtil/SimplexNoise.java
@@ -1,3 +1,10 @@
+/*
+ * A speed-improved simplex noise algorithm for 2D, 3D and 4D in Java. Based on example code by Stefan Gustavson
+ * (stegu@itn.liu.se). Optimisations by Peter Eastman (peastman@drizzle.stanford.edu). Better rank ordering method by
+ * Stefan Gustavson in 2012. This could be speeded up even further, but it's useful as it is. Version 2012-03-09 This
+ * code was placed in the public domain by its original author, Stefan Gustavson. You may use it as you see fit, but
+ * attribution is appreciated.
+ */
 package bartworks.util.NoiseUtil;
 
 import bartworks.util.MathUtils;
