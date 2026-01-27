55aa667e264038ab75aa02e48d83a5121f68fc1f
chrombread <chrombread8@gmail.com>
2026-01-27 00:07:25 -0800

    remove redundant specifiers in tooltip interface

--- src/main/java/gregtech/common/tileentities/machines/multi/nanochip/util/NanochipTooltipValues.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/nanochip/util/NanochipTooltipValues.java
@@ -12,17 +12,17 @@ public interface NanochipTooltipValues {
         return machineType + ", " + NAC_MODULE;
     }
 
-    public static final String NAC_MODULE = "NAC Module";
-    public static final String TOOLTIP_CC = EnumChatFormatting.YELLOW + "CC" + EnumChatFormatting.GRAY;
-    public static final String TOOLTIP_CCs = EnumChatFormatting.YELLOW + "CCs" + EnumChatFormatting.GRAY;
+    String NAC_MODULE = "NAC Module";
+    String TOOLTIP_CC = EnumChatFormatting.YELLOW + "CC" + EnumChatFormatting.GRAY;
+    String TOOLTIP_CCs = EnumChatFormatting.YELLOW + "CCs" + EnumChatFormatting.GRAY;
 
-    public static final String TOOLTIP_VCI = EnumChatFormatting.YELLOW + "VCI" + EnumChatFormatting.GRAY;
-    public static final String TOOLTIP_VCIs = EnumChatFormatting.YELLOW + "VCIs" + EnumChatFormatting.GRAY;
+    String TOOLTIP_VCI = EnumChatFormatting.YELLOW + "VCI" + EnumChatFormatting.GRAY;
+    String TOOLTIP_VCIs = EnumChatFormatting.YELLOW + "VCIs" + EnumChatFormatting.GRAY;
 
-    public static final String TOOLTIP_VCO = EnumChatFormatting.YELLOW + "VCO" + EnumChatFormatting.GRAY;
-    public static final String TOOLTIP_VCOs = EnumChatFormatting.YELLOW + "VCOs" + EnumChatFormatting.GRAY;
+    String TOOLTIP_VCO = EnumChatFormatting.YELLOW + "VCO" + EnumChatFormatting.GRAY;
+    String TOOLTIP_VCOs = EnumChatFormatting.YELLOW + "VCOs" + EnumChatFormatting.GRAY;
 
-    public static final String TOOLTIP_COLOR = EnumChatFormatting.RED + "c"
+    String TOOLTIP_COLOR = EnumChatFormatting.RED + "c"
         + EnumChatFormatting.YELLOW
         + "o"
         + EnumChatFormatting.GREEN
@@ -33,7 +33,7 @@ public interface NanochipTooltipValues {
         + "r"
         + EnumChatFormatting.GRAY;
 
-    public static final String TOOLTIP_COLORS = EnumChatFormatting.RED + "c"
+    String TOOLTIP_COLORS = EnumChatFormatting.RED + "c"
         + EnumChatFormatting.YELLOW
         + "o"
         + EnumChatFormatting.GREEN
@@ -46,7 +46,7 @@ public interface NanochipTooltipValues {
         + "s"
         + EnumChatFormatting.GRAY;
 
-    public static final String TOOLTIP_COLORED = EnumChatFormatting.RED + "c"
+    String TOOLTIP_COLORED = EnumChatFormatting.RED + "c"
         + EnumChatFormatting.YELLOW
         + "o"
         + EnumChatFormatting.GREEN
@@ -61,22 +61,22 @@ public interface NanochipTooltipValues {
         + "d"
         + EnumChatFormatting.GRAY;
 
-    public static final String TOOLTIP_COLOR_MATCH_VCS = "Outputs are placed in the " + TOOLTIP_VCO
+    String TOOLTIP_COLOR_MATCH_VCS = "Outputs are placed in the " + TOOLTIP_VCO
         + " with the same "
         + TOOLTIP_COLOR
         + " as the input "
         + TOOLTIP_VCI;
 
-    public static final String TOOLTIP_MODULE_DESCRIPTION = "Module of the " + EnumChatFormatting.GREEN
+    String TOOLTIP_MODULE_DESCRIPTION = "Module of the " + EnumChatFormatting.GREEN
         + "Nanochip Assembly Complex"
         + EnumChatFormatting.GRAY;
 
-    public static final String TOOLTIP_INFINITE_PARALLEL = "Has " + EnumChatFormatting.GOLD
+    String TOOLTIP_INFINITE_PARALLEL = "Has " + EnumChatFormatting.GOLD
         + "infinite"
         + EnumChatFormatting.GRAY
         + " parallels!";
 
-    public static final String TOOLTIP_STRUCTURE_BASE_VCI = "Any base casing - Vacuum Conveyor Input";
-    public static final String TOOLTIP_STRUCTURE_BASE_VCO = "Any base casing - Vacuum Conveyor Output";
+    String TOOLTIP_STRUCTURE_BASE_VCI = "Any base casing - Vacuum Conveyor Input";
+    String TOOLTIP_STRUCTURE_BASE_VCO = "Any base casing - Vacuum Conveyor Output";
 
 }
