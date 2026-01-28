287af9d43e0d234eaf4bb70a92b44aa1325f6f4c
Mary Hopson <doyoumined@gmail.com>
2026-01-27 19:14:49 -0500

    Fix CC rendering for GT++ items

--- src/main/java/gregtech/common/render/items/CircuitComponentItemRenderer.java
+++ src/main/java/gregtech/common/render/items/CircuitComponentItemRenderer.java
@@ -4,6 +4,8 @@ import static gregtech.common.tileentities.machines.multi.nanochip.util.CircuitC
 import static gregtech.loaders.ExtraIcons.circuitComponentOverlay;
 import static net.minecraftforge.client.IItemRenderer.ItemRenderType.INVENTORY;
 
+import java.awt.*;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.entity.RenderItem;
@@ -37,6 +39,8 @@ public class CircuitComponentItemRenderer implements IItemRenderer {
         CircuitComponent cc = tryGetFromFakeStack(item);
         if (cc == null) return;
         if (!cc.isProcessed) {
+            GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
+
             GL11.glPushMatrix();
             GL11.glTranslatef(2.4f, 2.4f, 0);
             GL11.glScalef(0.7f, 0.7f, 0);
@@ -58,12 +62,29 @@ public class CircuitComponentItemRenderer implements IItemRenderer {
                 IItemRenderer baseRenderer = MinecraftForgeClient.getItemRenderer(realItem, INVENTORY);
                 if (baseRenderer != null) {
                     baseRenderer.renderItem(type, realItem, data);
+                } else if (realItem.getItem() != null) {
+                    // This case is just used for GT++ items because their rendering is insane. Remove this if
+                    // they ever get IItemRenderers :)
+                    int coloration = realItem.getItem()
+                        .getColorFromItemStack(realItem, 0);
+
+                    int r = (coloration & 0xFF0000) >> 16;
+                    int g = (coloration & 0xFF00) >> 8;
+                    int b = (coloration & 0xFF);
+
+                    GL11.glColor3f(r / 255F, g / 255F, b / 255F);
+
+                    ItemRenderUtil.renderItem(
+                        type,
+                        realItem.getItem()
+                            .getIcon(realItem, 0));
                 }
             }
 
             GL11.glPopMatrix();
 
             GL11.glPushMatrix();
+            GL11.glColor3f(1, 1, 1);
             GL11.glEnable(GL11.GL_BLEND);
             GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
@@ -78,6 +99,7 @@ public class CircuitComponentItemRenderer implements IItemRenderer {
             GL11.glPopMatrix();
 
             RenderHelper.enableGUIStandardItemLighting();
+            GL11.glPopAttrib();
         } else {
             ItemRenderUtil.renderItem(type, item.getIconIndex());
         }
