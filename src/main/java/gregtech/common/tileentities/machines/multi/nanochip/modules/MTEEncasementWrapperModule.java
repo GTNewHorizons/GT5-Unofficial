b7f9a92efb2451327a7db75442a18301e21a4311
NotAPenguin <michiel.vandeginste@gmail.com>
2026-01-28 20:30:04 +0100

    spootles

--- src/main/java/gregtech/common/tileentities/machines/multi/nanochip/modules/MTEEncasementWrapperModule.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/nanochip/modules/MTEEncasementWrapperModule.java
@@ -9,11 +9,9 @@ import static gregtech.common.tileentities.machines.multi.nanochip.MTENanochipAs
 import static net.minecraft.util.StatCollector.translateToLocal;
 import static net.minecraft.util.StatCollector.translateToLocalFormatted;
 
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.util.ForgeDirection;
 
 import com.gtnewhorizon.structurelib.structure.IStructureDefinition;
-import com.gtnewhorizon.structurelib.structure.ISurvivalBuildEnvironment;
 
 import gregtech.api.casing.Casings;
 import gregtech.api.enums.Materials;
@@ -106,11 +104,17 @@ public class MTEEncasementWrapperModule extends MTENanochipAssemblyModuleBase<MT
         return STRUCTURE_DEFINITION;
     }
 
-    public int structureOffsetX() { return ENCASEMENT_WRAPPER_OFFSET_X; }
+    public int structureOffsetX() {
+        return ENCASEMENT_WRAPPER_OFFSET_X;
+    }
 
-    public int structureOffsetY() { return ENCASEMENT_WRAPPER_OFFSET_Y; }
+    public int structureOffsetY() {
+        return ENCASEMENT_WRAPPER_OFFSET_Y;
+    }
 
-    public int structureOffsetZ() { return ENCASEMENT_WRAPPER_OFFSET_Z; }
+    public int structureOffsetZ() {
+        return ENCASEMENT_WRAPPER_OFFSET_Z;
+    }
 
     @Override
     protected MultiblockTooltipBuilder createTooltip() {
