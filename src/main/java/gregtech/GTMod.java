3aa429154c93830ffdd6aed14375b0cc035e4774
Maya <10861407+serenibyss@users.noreply.github.com>
2026-01-30 12:29:42 -0700

    Remove NAC module recipes

--- src/main/java/gregtech/GTMod.java
+++ src/main/java/gregtech/GTMod.java
@@ -98,7 +98,6 @@ import gregtech.common.misc.spaceprojects.commands.SPMCommand;
 import gregtech.common.misc.spaceprojects.commands.SpaceProjectCommand;
 import gregtech.common.ores.UnificationOreAdapter;
 import gregtech.common.powergoggles.handlers.PowerGogglesConfigHandler;
-import gregtech.common.tileentities.machines.multi.nanochip.util.RecipeHandlers;
 import gregtech.crossmod.ae2.AE2Compat;
 import gregtech.crossmod.holoinventory.HoloInventory;
 import gregtech.crossmod.waila.Waila;
@@ -390,10 +389,6 @@ public class GTMod {
 
         GT_FML_LOGGER.debug("Registering SpaceDimensions");
         SpaceDimRegisterer.register();
-        // This needs to run BEFORE creating any circuit assembler recipes, since the downstream
-        // recipe map for the assembly matrix relies on doing recipe lookups here.
-        // I really hope I can put this here without breaking something
-        RecipeHandlers.registerNanochipModuleRecipes();
 
         GregTechAPI.sLoadFinished = true;
         GTLog.out.println("GTMod: Load-Phase finished!");
