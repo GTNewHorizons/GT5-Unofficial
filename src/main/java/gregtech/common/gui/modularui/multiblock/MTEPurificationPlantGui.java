1c698a5fbec1e290050f7a4fae7b13adddd9dabc
VortexSo4 <kreker.maxim@mail.ru>
2026-02-04 01:26:14 +0300

    start

--- src/main/java/gregtech/common/gui/modularui/multiblock/MTEPurificationPlantGui.java
+++ src/main/java/gregtech/common/gui/modularui/multiblock/MTEPurificationPlantGui.java
@@ -1,5 +1,6 @@
 package gregtech.common.gui.modularui.multiblock;
 
+import gregtech.api.metatileentity.MetaTileEntity;
 import net.minecraft.util.StatCollector;
 
 import com.cleanroommc.modularui.api.drawable.IDrawable;
@@ -23,7 +24,28 @@ import gregtech.common.gui.modularui.multiblock.base.MTEMultiBlockBaseGui;
 import gregtech.common.tileentities.machines.multi.purification.LinkedPurificationUnit;
 import gregtech.common.tileentities.machines.multi.purification.MTEPurificationPlant;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class MTEPurificationPlantGui extends MTEMultiBlockBaseGui<MTEPurificationPlant> {
+
+    private static final Map<String, Integer> MODULE_TIERS = new HashMap<>();
+    private static final List<String> TIER_ORDER = Arrays.asList(
+        "multimachine.purificationunitclarifier",
+        "multimachine.purificationunitozonation",
+        "multimachine.purificationunitflocculator",
+        "multimachine.purificationunitphadjustment",
+        "multimachine.purificationunitplasmaheater",
+        "multimachine.purificationunituvtreatment",
+        "multimachine.purificationunitdegasifier",
+        "multimachine.purificationunitextractor"
+    );
+
+    static {
+        for (int i = 0; i < TIER_ORDER.size(); i++) {
+            MODULE_TIERS.put(TIER_ORDER.get(i), i + 1);
+        }
+    }
 
     public MTEPurificationPlantGui(MTEPurificationPlant multiblock) {
         super(multiblock);
@@ -59,13 +81,17 @@ public class MTEPurificationPlantGui extends MTEMultiBlockBaseGui<MTEPurificatio
         ListWidget<IWidget, ?> widget = super.createTerminalTextWidget(syncManager, parent);
         GenericListSyncHandler linkedPurifications = syncManager
             .findSyncHandler("linkedPurifierUnits", GenericListSyncHandler.class);
-
+        List<LinkedPurificationUnit> sortedUnits = new ArrayList<>();
         for (Object obj : linkedPurifications.getValue()) {
             if (obj instanceof LinkedPurificationUnit unit) {
-                widget.child(machineRow(unit));
-
+                sortedUnits.add(unit);
             }
         }
+        sortedUnits.sort(Comparator.comparingInt(unit -> {
+            Integer tier = getModuleTier(unit.metaTileEntity());
+            return tier != null ? tier : Integer.MAX_VALUE;
+        }));
+        for (LinkedPurificationUnit unit : sortedUnits) widget.child(machineRow(unit));
         return widget;
     }
 
@@ -73,61 +99,60 @@ public class MTEPurificationPlantGui extends MTEMultiBlockBaseGui<MTEPurificatio
     protected Flow createButtonColumn(ModularPanel panel, PanelSyncManager syncManager) {
         BooleanSyncValue debugMode = syncManager.findSyncHandler("debugMode", BooleanSyncValue.class);
         return super.createButtonColumn(panel, syncManager).child(new ButtonWidget<>().onMousePressed((a) -> {
-            if (multiblock.getBaseMetaTileEntity()
-                .isActive()) {
-                return false;
-            }
-            debugMode.setBoolValue(!debugMode.getBoolValue());
-            return true;
-        })
-            .tooltip(
-                new RichTooltip().add(StatCollector.translateToLocal("GT5U.gui.tooltip.purification_plant.debug_mode")))
-            .overlay(new DynamicDrawable(() -> {
-                if (multiblock.getBaseMetaTileEntity()
-                    .isAllowedToWork()) {
-                    return GTGuiTextures.OVERLAY_BUTTON_RECIPE_LOCKED;
-                }
-                return GTGuiTextures.OVERLAY_BUTTON_MACHINEMODE_DEFAULT;
+                    if (multiblock.getBaseMetaTileEntity()
+                        .isActive()) {
+                        return false;
+                    }
+                    debugMode.setBoolValue(!debugMode.getBoolValue());
+                    return true;
+                })
+                .tooltip(
+                    new RichTooltip().add(StatCollector.translateToLocal("GT5U.gui.tooltip.purification_plant.debug_mode")))
+                .overlay(new DynamicDrawable(() -> {
+                        if (multiblock.getBaseMetaTileEntity()
+                            .isAllowedToWork()) {
+                            return GTGuiTextures.OVERLAY_BUTTON_RECIPE_LOCKED;
+                        }
+                        return GTGuiTextures.OVERLAY_BUTTON_MACHINEMODE_DEFAULT;
 
-            })
+                    })
 
-            )
-            .background(new DynamicDrawable(() -> {
-                if (debugMode.getBoolValue()) {
-                    return GTGuiTextures.BUTTON_STANDARD_PRESSED;
-                } else {
-                    return GTGuiTextures.BUTTON_STANDARD;
-                }
-            }))
+                )
+                .background(new DynamicDrawable(() -> {
+                    if (debugMode.getBoolValue()) {
+                        return GTGuiTextures.BUTTON_STANDARD_PRESSED;
+                    } else {
+                        return GTGuiTextures.BUTTON_STANDARD;
+                    }
+                }))
 
         );
     }
 
     public Flow machineRow(LinkedPurificationUnit LinkedUnit) {
+        MetaTileEntity mte = LinkedUnit.metaTileEntity();
+        String moduleName = getLocalizedModuleName(mte);
         return new Row().paddingBottom(4)
             .paddingTop(4)
             .coverChildrenHeight()
             .child(
                 new ItemDisplayWidget().background(IDrawable.EMPTY)
                     .disableHoverBackground()
-                    .size(14)
-                    .item(
-                        LinkedUnit.metaTileEntity()
-                            .getStackForm(1)))
-            .child(
-                new ListWidget<>().paddingLeft(4)
-                    .coverChildrenHeight()
-                    // size of the terminal - size of the icon - size of border
-                    .width(186 - 16 - 22)
-                    .crossAxisAlignment(Alignment.CrossAxis.START)
-                    .child(
-                        IKey.str(
-                            LinkedUnit.metaTileEntity()
-                                .getLocalName()
-                                .replaceAll("Purification Unit", ""))
-                            .asWidget())
-                    .child(
-                        IKey.str(LinkedUnit.getStatusString())
-                            .asWidget()));
+                    .size(20)
+                    .item(mte.getStackForm(1))
+                    .tooltip((tooltip) -> {
+                        tooltip.addLine(StatCollector.translateToLocalFormatted("GT5U.gui.purification.tier", getModuleTier(mte)));
+                        tooltip.addLine(moduleName);
+                    }));
+    }
+
+    private String getLocalizedModuleName(MetaTileEntity mte) {
+        String metaName = mte.getMetaName();
+        String translationKey = "GT5U.gui." + metaName;
+        return StatCollector.translateToLocal(translationKey);
+    }
+
+    private Integer getModuleTier(MetaTileEntity mte) {
+        return MODULE_TIERS.get(mte.mName);
     }
 }
