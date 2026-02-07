bfabb91564f8f8ccffeeab8d8ced6957cae8686d
GitHub GTNH Actions <>
2026-02-07 19:35:11 +0000

    spotlessApply

--- src/main/java/gregtech/common/tileentities/machines/outputme/base/MTEHatchOutputMEBase.java
+++ src/main/java/gregtech/common/tileentities/machines/outputme/base/MTEHatchOutputMEBase.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 import javax.annotation.Nullable;
 
-import appeng.api.storage.data.IAEStackType;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.Item;
@@ -51,6 +50,7 @@ import appeng.api.storage.ISaveProvider;
 import appeng.api.storage.MEMonitorHandler;
 import appeng.api.storage.StorageChannel;
 import appeng.api.storage.data.IAEStack;
+import appeng.api.storage.data.IAEStackType;
 import appeng.api.util.AEColor;
 import appeng.core.localization.GuiText;
 import appeng.items.AEBaseCell;
@@ -270,7 +270,9 @@ public abstract class MTEHatchOutputMEBase<T extends IAEStack<T>, F extends MEFi
             return null;
         }
 
-        final MEInventoryHandler<StackType> ih = new MEInventoryHandler<>(h, (IAEStackType<StackType>) h.getStackType());
+        final MEInventoryHandler<StackType> ih = new MEInventoryHandler<>(
+            h,
+            (IAEStackType<StackType>) h.getStackType());
         ih.setPriority(this.myPriority);
         ih.setBaseAccess(myAccess);
 
