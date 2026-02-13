d126e20c143b443db48db98f63ab2c647af2dc91
AnsonYeung <nanolify@gmail.com>
2026-02-13 15:21:44 +0800

    Fix Maintenance Hatch

--- src/main/java/tectech/thing/metaTileEntity/hatch/MTEHatchCreativeMaintenance.java
+++ src/main/java/tectech/thing/metaTileEntity/hatch/MTEHatchCreativeMaintenance.java
@@ -80,6 +80,7 @@ public class MTEHatchCreativeMaintenance extends MTEHatchMaintenance {
     @Override
     public void onMaintenancePerformed(MTEMultiBlockBase aMaintenanceTarget) {
         setMaintenanceSound(SoundResource.GT_MAINTENANCE_CREATIVE_HATCH, 1.0F, 1.0F);
+        this.mWrench = this.mScrewdriver = this.mSoftMallet = this.mHardHammer = this.mCrowbar = this.mSolderingTool = true;
         super.onMaintenancePerformed(aMaintenanceTarget);
     }
 
