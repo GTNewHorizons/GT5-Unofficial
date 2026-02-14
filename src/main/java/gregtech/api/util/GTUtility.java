66ff31460e4e3503bfdb3f7d50813b77f0a1e8b0
Martin Robertz <dream-master@gmx.net>
2026-02-14 12:35:30 +0100

    fix+deps

--- src/main/java/gregtech/api/util/GTUtility.java
+++ src/main/java/gregtech/api/util/GTUtility.java
@@ -40,6 +40,7 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
