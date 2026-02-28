dcc8a8a18bc75a71e3da8370633a2911507cc0e8
loenaaaa <asusactiongamer@gmail.com>
2026-02-28 21:28:48 +0800

    add fluid panel button

--- src/main/java/gregtech/common/gui/modularui/multiblock/MTELargeNeutralizationEngineGui.java
+++ src/main/java/gregtech/common/gui/modularui/multiblock/MTELargeNeutralizationEngineGui.java
@@ -1,15 +1,22 @@
 package gregtech.common.gui.modularui.multiblock;
 
+import static gregtech.api.enums.Mods.GregTech;
+import static gregtech.api.metatileentity.BaseTileEntity.TOOLTIP_DELAY;
+
 import net.minecraft.util.StatCollector;
 
+import com.cleanroommc.modularui.api.IPanelHandler;
 import com.cleanroommc.modularui.api.drawable.IKey;
 import com.cleanroommc.modularui.api.widget.IWidget;
+import com.cleanroommc.modularui.drawable.UITexture;
 import com.cleanroommc.modularui.screen.ModularPanel;
 import com.cleanroommc.modularui.value.sync.BooleanSyncValue;
 import com.cleanroommc.modularui.value.sync.FloatSyncValue;
 import com.cleanroommc.modularui.value.sync.IntSyncValue;
 import com.cleanroommc.modularui.value.sync.PanelSyncManager;
+import com.cleanroommc.modularui.widgets.ButtonWidget;
 import com.cleanroommc.modularui.widgets.ListWidget;
+import com.cleanroommc.modularui.widgets.layout.Flow;
 
 import gregtech.common.gui.modularui.multiblock.base.MTEMultiBlockBaseGui;
 import gregtech.common.tileentities.machines.multi.MTELargeNeutralizationEngine;
@@ -56,5 +63,39 @@ public class MTELargeNeutralizationEngineGui extends MTEMultiBlockBaseGui<MTELar
                     netResidueSyncer.getStringValue()))
                 .asWidget());
         return terminalText;
+    }
+
+    @Override
+    protected Flow createRightPanelGapRow(ModularPanel parent, PanelSyncManager syncManager) {
+        Flow rightPanelGapRow = super.createRightPanelGapRow(parent, syncManager);
+        rightPanelGapRow.child(createFluidPanelButton(syncManager, parent));
+        return rightPanelGapRow;
+    }
+
+    protected IWidget createFluidPanelButton(PanelSyncManager syncManager, ModularPanel parent) {
+        IPanelHandler powerPanel = syncManager.syncedPanel(
+            "fluidPanel",
+            true,
+            (p_syncManager, syncHandler) -> openFluidControlPanel(p_syncManager, parent));
+        return new ButtonWidget<>().size(18, 18)
+            .marginLeft(4)
+            .overlay(UITexture.fullImage(GregTech.ID, "gui/overlay_button/fluid_panel"))
+            .onMousePressed(d -> {
+                if (!powerPanel.isPanelOpen()) {
+                    powerPanel.openPanel();
+                } else {
+                    powerPanel.closePanel();
+                }
+                return true;
+            })
+            .tooltipBuilder(t -> t.addLine(IKey.lang("GT5U.gui.button.fluid_panel")))
+            .tooltipShowUpTimer(TOOLTIP_DELAY);
+    }
+
+    private ModularPanel openFluidControlPanel(PanelSyncManager syncManager, ModularPanel parent) {
+        return new ModularPanel("fluidPanel").relative(parent)
+            .leftRel(1)
+            .topRel(0)
+            .size(120, 130);
     }
 }
