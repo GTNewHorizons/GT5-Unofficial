7f47167a7501edb20ada93d6fd3fc297041de8de
danyadev <danyadev@mail.ru>
2026-03-01 00:27:29 +0300

    GT fluid registry cleanup (#5859)

--- src/main/java/gregtech/common/GTProxy.java
+++ src/main/java/gregtech/common/GTProxy.java
@@ -1956,7 +1956,7 @@ public class GTProxy implements IFuelHandler {
         }
         GTOreDictUnificator.addToBlacklist(aFluidEvent.data.emptyContainer);
         GTOreDictUnificator.addToBlacklist(aFluidEvent.data.filledContainer);
-        GTUtility.addFluidContainerData(aFluidEvent.data);
+        GTUtility.insertInFluidToContainersMap(aFluidEvent.data);
     }
 
     @SubscribeEvent
