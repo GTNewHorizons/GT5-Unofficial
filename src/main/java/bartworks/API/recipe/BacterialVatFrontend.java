4d49d68928414a76465a1c8db5ed11f1610169c3
SST-03 <70825063+SST-03@users.noreply.github.com>
2026-03-01 16:07:49 +0800

    Improve the NEI recipe page and tooltips for Bac Vat (#6033)

--- src/main/java/bartworks/API/recipe/BacterialVatFrontend.java
+++ src/main/java/bartworks/API/recipe/BacterialVatFrontend.java
@@ -3,6 +3,7 @@ package bartworks.API.recipe;
 import static com.gtnewhorizon.gtnhlib.util.numberformatting.NumberFormatUtil.formatNumber;
 import static gregtech.api.util.GTRecipeConstants.GLASS;
 import static gregtech.api.util.GTUtility.getTierNameWithParentheses;
+import static net.minecraft.util.EnumChatFormatting.GRAY;
 
 import java.util.List;
 
@@ -12,12 +13,15 @@ import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.StatCollector;
 
 import com.gtnewhorizons.modularui.api.math.Alignment;
+import com.gtnewhorizons.modularui.api.math.Pos2d;
 
+import bartworks.common.loaders.BioItemList;
 import gregtech.api.enums.GTValues;
 import gregtech.api.recipe.BasicUIPropertiesBuilder;
 import gregtech.api.recipe.NEIRecipePropertiesBuilder;
 import gregtech.api.recipe.RecipeMapFrontend;
 import gregtech.api.util.GTRecipeConstants;
+import gregtech.api.util.GTUtility;
 import gregtech.api.util.MethodsReturnNonnullByDefault;
 import gregtech.api.util.recipe.Sievert;
 import gregtech.nei.GTNEIDefaultHandler;
@@ -39,6 +43,10 @@ public class BacterialVatFrontend extends RecipeMapFrontend {
             currentTip.add(EnumChatFormatting.GRAY + StatCollector.translateToLocal("nei.biovat.input.tooltip"));
             return currentTip;
         }
+        if (GTUtility.areStacksEqual(pStack.item, BioItemList.getPetriDish(null), true)) {
+            currentTip.add(GRAY + StatCollector.translateToLocal("GT5U.recipes.not_consume"));
+            return currentTip;
+        }
         return super.handleNEIItemInputTooltip(currentTip, pStack);
     }
 
@@ -56,6 +64,9 @@ public class BacterialVatFrontend extends RecipeMapFrontend {
     protected void drawNEIOverlayForInput(GTNEIDefaultHandler.FixedPositionedStack stack) {
         super.drawNEIOverlayForInput(stack);
         drawFluidOverlay(stack);
+        if (!stack.isFluid() && GTUtility.areStacksEqual(stack.item, BioItemList.getPetriDish(null), true)) {
+            drawNEIOverlayText("NC", stack);
+        }
     }
 
     @Override
@@ -99,5 +110,10 @@ public class BacterialVatFrontend extends RecipeMapFrontend {
                 recipeInfo.drawText(StatCollector.translateToLocalFormatted("nei.biovat.2.name", sievert));
             }
         }
+    }
+
+    @Override
+    public final Pos2d getSpecialItemPosition() {
+        return new Pos2d(16, 62);
     }
 }
