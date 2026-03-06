696a7565ae5919caf4bafb2bafa6dffb159a5fb8
Anson Yeung <nanolify@gmail.com>
2026-03-06 14:48:25 +0800

    Use WeakReference for wireless energy

--- src/main/java/gregtech/common/misc/WirelessEnergyHatchManager.java
+++ src/main/java/gregtech/common/misc/WirelessEnergyHatchManager.java
@@ -2,6 +2,7 @@ package gregtech.common.misc;
 
 import static gregtech.common.misc.WirelessNetworkManager.ticks_between_energy_addition;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -14,21 +15,25 @@ import gregtech.api.metatileentity.implementations.MTEWirelessEnergy;
 @EventBusSubscriber
 public class WirelessEnergyHatchManager {
 
-    private static final ArrayList<MTEWirelessEnergy> HATCHES = new ArrayList<>();
+    private static final ArrayList<WeakReference<MTEWirelessEnergy>> HATCHES = new ArrayList<>();
     private static long tickTimer = 0;
 
     public static void addHatch(MTEWirelessEnergy e) {
-        HATCHES.add(e);
+        HATCHES.add(new WeakReference<>(e));
     }
 
     @SubscribeEvent
     public static void onTick(TickEvent.ServerTickEvent event) {
         if (event.phase == TickEvent.Phase.END) return;
         if (++tickTimer % ticks_between_energy_addition != 0) return;
-        Iterator<MTEWirelessEnergy> iterator = HATCHES.iterator();
+        Iterator<WeakReference<MTEWirelessEnergy>> iterator = HATCHES.iterator();
         while (iterator.hasNext()) {
-            MTEWirelessEnergy e = iterator.next();
-            if (!e.isValid()) iterator.remove();
+            MTEWirelessEnergy e = iterator.next()
+                .get();
+            if (e == null || !e.isValid()) {
+                iterator.remove();
+                continue;
+            }
             e.tryFetchingEnergy();
         }
     }
