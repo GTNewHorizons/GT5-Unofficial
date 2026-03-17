00fabcafd3546b2ebf9c3f47dbe70030705cc100
HamCorp (Tooj) <hamknockers@gmail.com>
2026-03-17 07:42:41 +0100

    Add the Large Hadron Collider multiblock (#5135)
    Co-authored-by: chrombread <chrombread8@gmail.com>
    Co-authored-by: chrombread <119648279+chrombread@users.noreply.github.com>
    Co-authored-by: zub143 <blenahan1216@gmail.com>
    Co-authored-by: Ranzu <66495944+Ranzuu@users.noreply.github.com>
    Co-authored-by: Martin Robertz <dream-master@gmx.net>
    Co-authored-by: Maya <10861407+serenibyss@users.noreply.github.com>
    Co-authored-by: EnderProyects <45178400+EnderProyects@users.noreply.github.com>
    Co-authored-by: Léa Gris <lea.gris@noiraude.net>
    Co-authored-by: UltraProdigy <187078471+UltraProdigy@users.noreply.github.com>
    Co-authored-by: Ruling-0 <47913168+Ruling-0@users.noreply.github.com>

---
+++ src/main/java/gregtech/common/tileentities/machines/multi/beamcrafting/LHCModule.java
@@ -0,0 +1,68 @@
+package gregtech.common.tileentities.machines.multi.beamcrafting;
+
+import com.google.common.collect.ImmutableList;
+
+import gtnhlanth.common.beamline.Particle;
+
+public enum LHCModule {
+
+    EM(ImmutableList.of(
+        Particle.PHOTON,
+        Particle.WBOSON,
+        Particle.ELECTRON,
+        Particle.MUON,
+        Particle.TAU,
+        Particle.PROTON,
+        Particle.OMEGA)),
+    Weak(ImmutableList.of(
+        Particle.ZBOSON,
+        Particle.WBOSON,
+        Particle.HIGGS,
+        Particle.ELECTRON,
+        Particle.MUON,
+        Particle.TAU,
+        Particle.ELECTRONNEUTRINO,
+        Particle.MUONNEUTRINO,
+        Particle.TAUNEUTRINO,
+        Particle.PROTON,
+        Particle.NEUTRON,
+        Particle.LAMBDA,
+        Particle.OMEGA,
+        Particle.ETA,
+        Particle.JPSI,
+        Particle.UPSILON)),
+    Strong(ImmutableList.of(
+        Particle.PROTON,
+        Particle.NEUTRON,
+        Particle.LAMBDA,
+        Particle.OMEGA,
+        Particle.ETA,
+        Particle.JPSI,
+        Particle.UPSILON)),
+    Grav(ImmutableList.of(Particle.HIGGS, Particle.GRAVITON)),
+    AllParticles(ImmutableList.of(
+        Particle.PHOTON,
+        Particle.WBOSON,
+        Particle.ELECTRON,
+        Particle.MUON,
+        Particle.TAU,
+        Particle.PROTON,
+        Particle.OMEGA,
+        Particle.ZBOSON,
+        Particle.HIGGS,
+        Particle.ELECTRONNEUTRINO,
+        Particle.MUONNEUTRINO,
+        Particle.TAUNEUTRINO,
+        Particle.NEUTRON,
+        Particle.LAMBDA,
+        Particle.ETA,
+        Particle.JPSI,
+        Particle.UPSILON,
+        Particle.GRAVITON));
+
+    public final ImmutableList<Particle> acceptedParticles;
+
+    LHCModule(ImmutableList<Particle> acceptedParticles) {
+        this.acceptedParticles = acceptedParticles;
+    }
+}
