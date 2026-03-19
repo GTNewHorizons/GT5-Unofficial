088b5020a6ba4850e4b52574465fb55bc012231a
RecursivePineapple <recursive_pineapple@proton.me>
2026-03-19 14:56:19 -0400

    Enable blend

--- src/main/java/gregtech/client/GTPowerfailRenderer.java
+++ src/main/java/gregtech/client/GTPowerfailRenderer.java
@@ -73,8 +73,12 @@ public class GTPowerfailRenderer {
                 .getTextureMapBlocks()
                 .getGlTextureId());
 
+        GL11.glPushAttrib(GL11.GL_ENABLE_BIT);
+
         GL11.glDisable(GL11.GL_DEPTH_TEST);
         GL11.glEnable(GL11.GL_ALPHA);
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
         Tessellator tessellator = Tessellator.instance;
 
@@ -124,7 +128,8 @@ public class GTPowerfailRenderer {
         }
 
         tessellator.draw();
-        GL11.glEnable(GL11.GL_DEPTH_TEST);
+
+        GL11.glPopAttrib();
     }
 
     @Desugar
