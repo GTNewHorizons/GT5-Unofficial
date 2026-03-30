4074876139df483098b46166bffefeed54e7e63c
ChromaPIE <chromasetha@outlook.com>
2026-03-30 15:02:42 +0800

    Update GTAuthors.java

--- src/main/java/gregtech/api/enums/GTAuthors.java
+++ src/main/java/gregtech/api/enums/GTAuthors.java
@@ -89,12 +89,12 @@ public class GTAuthors {
     public static final String Authorminecraft7771 = "" + EnumChatFormatting.BLUE
         + EnumChatFormatting.LIGHT_PURPLE
         + "minecraft7771";
-    public static final String AuthorHamCorp = "Author: " + EnumChatFormatting.GOLD
+    public static final String AuthorHamCorp = "" + EnumChatFormatting.GOLD
         + "Ham"
         + EnumChatFormatting.WHITE
         + "Corp";
 
-    public static final String Authorzub = "Author: " + EnumChatFormatting.DARK_PURPLE
+    public static final String Authorzub = "" + EnumChatFormatting.DARK_PURPLE
         + EnumChatFormatting.ITALIC
         + "zub";
     public static final Supplier<String> AuthorCloud = chain(
