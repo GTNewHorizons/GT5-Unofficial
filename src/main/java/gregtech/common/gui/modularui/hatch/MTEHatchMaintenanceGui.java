ff5cc4a3e449d1a26b72a338a30ae527d5ad04ab
TheYoingLad <golyhagergo@gmail.com>
2026-04-02 18:45:28 +0200

    ported MTEHatchMaintenance to mui2

---
+++ src/main/java/gregtech/common/gui/modularui/hatch/MTEHatchMaintenanceGui.java
@@ -0,0 +1,118 @@
+package gregtech.common.gui.modularui.hatch;
+
+import static net.minecraft.util.StatCollector.translateToLocal;
+
+import java.util.Arrays;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+
+import org.jetbrains.annotations.NotNull;
+
+import com.cleanroommc.modularui.api.drawable.IDrawable;
+import com.cleanroommc.modularui.drawable.GuiTextures;
+import com.cleanroommc.modularui.screen.ModularPanel;
+import com.cleanroommc.modularui.utils.Alignment;
+import com.cleanroommc.modularui.utils.MouseData;
+import com.cleanroommc.modularui.value.sync.PanelSyncManager;
+import com.cleanroommc.modularui.value.sync.PhantomItemSlotSH;
+import com.cleanroommc.modularui.widget.ParentWidget;
+import com.cleanroommc.modularui.widgets.SlotGroupWidget;
+import com.cleanroommc.modularui.widgets.layout.Flow;
+import com.cleanroommc.modularui.widgets.slot.ItemSlot;
+import com.cleanroommc.modularui.widgets.slot.ModularSlot;
+import com.cleanroommc.modularui.widgets.slot.PhantomItemSlot;
+
+import gregtech.api.metatileentity.implementations.MTEHatchMaintenance;
+import gregtech.api.modularui2.GTGuiTextures;
+import gregtech.api.util.GTUtility;
+import gregtech.common.gui.modularui.hatch.base.MTEHatchBaseGui;
+
+public class MTEHatchMaintenanceGui extends MTEHatchBaseGui<MTEHatchMaintenance> {
+
+    private final boolean mAuto;
+
+    public MTEHatchMaintenanceGui(MTEHatchMaintenance hatch, boolean mAuto) {
+        super(hatch);
+        this.mAuto = mAuto;
+    }
+
+    @Override
+    protected ParentWidget<?> createContentSection(ModularPanel panel, PanelSyncManager syncManager) {
+        Flow mainRow = Flow.row()
+            .full();
+
+        // tip box
+        mainRow.child(
+            GuiTextures.BUBBLE.asWidget()
+                .background(GTGuiTextures.BUTTON_STANDARD)
+                .align(Alignment.TopRight)
+                .marginTop(3)
+                .marginRight(3)
+                .tooltip(
+                    t -> t.addStringLines(
+                        Arrays.asList(
+                            GTUtility.breakLines(
+                                translateToLocal(
+                                    mAuto ? "GT5U.gui.text.autorepair_info" : "GT5U.gui.text.repair_info"))))));
+
+        if (mAuto) {
+            // maintenance item input slots
+            mainRow.child(
+                SlotGroupWidget.builder()
+                    .matrix(new String[] { "ss", "ss" })
+                    .key(
+                        's',
+                        index -> new ItemSlot()
+                            .slot(new ModularSlot(hatch.inventoryHandler, index).slotGroup("item_inv")))
+                    .build()
+                    .align(Alignment.CENTER));
+        } else {
+            // maintenance slot
+            mainRow.child(
+                Flow.row()
+                    .align(Alignment.CENTER)
+                    .background(GTGuiTextures.SLOT_MAINTENANCE)
+                    .size(20)
+                    .child(new PhantomItemSlot() {
+
+                        @Override
+                        public boolean handleDragAndDrop(@NotNull ItemStack draggedStack, int button) {
+                            return false;
+                        }
+
+                        @Override
+                        public PhantomItemSlot slot(ModularSlot slot) {
+                            return syncHandler(new PhantomItemSlotSH(slot) {
+
+                                @Override
+                                protected void phantomClick(MouseData mouseData, ItemStack cursorStack) {
+                                    if (cursorStack == null) return;
+                                    EntityPlayer player = syncManager.getPlayer();
+
+                                    if (player == null) return;
+
+                                    hatch.onToolClick(cursorStack, player);
+                                    // refresh held stack
+                                    if (cursorStack.stackSize < 1) syncManager.setCursorItem(null);
+                                    else syncManager.setCursorItem(cursorStack);
+                                }
+                            });
+                        }
+                    }.slot(new ModularSlot(hatch.inventoryHandler, 0))
+                        .size(25)
+                        .background(IDrawable.EMPTY)
+                        .align(Alignment.CENTER)));
+
+        }
+
+        return super.createContentSection(panel, syncManager).child(mainRow);
+    }
+
+    @Override
+    public void registerSyncValues(PanelSyncManager syncManager) {
+        super.registerSyncValues(syncManager);
+
+        syncManager.registerSlotGroup("item_inv", 2);
+    }
+}
