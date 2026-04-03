542adcbb6c05ff4e78919ad6f9777c947908fb1c
TheYoingLad <golyhagergo@gmail.com>
2026-04-03 11:53:55 +0200

    small fixes

--- src/main/java/gregtech/api/modularui2/GTGuiTextures.java
+++ src/main/java/gregtech/api/modularui2/GTGuiTextures.java
@@ -1741,8 +1741,16 @@ public final class GTGuiTextures {
     public static final UITexture PICTURE_UPGRADE_CONNECTOR_RED_OPAQUE = UITexture
         .fullImage(MODID, "gui/picture/connector_red_opaque");
     public static final UITexture PICTURE_HEAT_SINK_16x8 = UITexture.fullImage(MODID, "gui/picture/heat_sink_16x8");
-    public static final UITexture PICTURE_RACK_LARGE = UITexture.fullImage(MODID, "gui/picture/rack_large");
-    public static final UITexture PICTURE_HEAT_SINK = UITexture.fullImage(MODID, "gui/picture/heat_sink");
+    public static final UITexture PICTURE_RACK_LARGE = UITexture.builder()
+        .location(MODID, "gui/picture/rack_large")
+        .fullImage()
+        .canApplyTheme()
+        .build();
+    public static final UITexture PICTURE_HEAT_SINK = UITexture.builder()
+        .location(MODID, "gui/picture/heat_sink")
+        .fullImage()
+        .canApplyTheme()
+        .build();
     public static final UITexture GREEN_CHECKMARK_11x9 = UITexture.fullImage(MODID, "gui/picture/green_checkmark");
     public static final UITexture BUTTON_OUTLINE_HOLLOW = UITexture.builder()
         .location(MODID, "gui/button/transparent_16x16")
