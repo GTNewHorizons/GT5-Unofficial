85c18b476964b39b8bd228c1140b82423e8eee05
KleinGrrmpf <50833808+KleinGrrmpf@users.noreply.github.com>
2026-04-05 16:02:57 +0200

    UI stuff

--- src/main/java/gregtech/common/gui/modularui/cover/CoverAdvancedWirelessControllerGui.java
+++ src/main/java/gregtech/common/gui/modularui/cover/CoverAdvancedWirelessControllerGui.java
@@ -10,19 +10,27 @@ import com.cleanroommc.modularui.value.sync.PanelSyncManager;
 import com.cleanroommc.modularui.value.sync.StringSyncValue;
 import com.cleanroommc.modularui.widgets.TextWidget;
 import com.cleanroommc.modularui.widgets.ToggleButton;
+import com.cleanroommc.modularui.widgets.layout.Column;
 import com.cleanroommc.modularui.widgets.layout.Flow;
 import com.cleanroommc.modularui.widgets.layout.Grid;
+import com.cleanroommc.modularui.widgets.layout.Row;
 import com.cleanroommc.modularui.widgets.textfield.TextFieldWidget;
 
 import gregtech.api.modularui2.CoverGuiData;
 import gregtech.api.modularui2.GTGuiTextures;
 import gregtech.common.covers.CoverAdvancedWirelessController;
 import gregtech.common.covers.conditions.RedstoneCondition;
+import gregtech.common.covers.redstone.CoverAdvancedRedstoneReceiverBase;
+import gregtech.common.gui.modularui.cover.base.CoverAdvancedWirelessRedstoneBaseGui;
 import gregtech.common.gui.modularui.cover.base.CoverBaseGui;
 import gregtech.common.modularui2.sync.LinkedBoolValue;
 import gregtech.common.modularui2.widget.SelectButton;
+import gregtech.common.modularui2.widget.builder.EnumColumnBuilder;
+import gregtech.common.modularui2.widget.builder.EnumRowBuilder;
 
-public class CoverAdvancedWirelessControllerGui extends CoverBaseGui<CoverAdvancedWirelessController> {
+import static net.minecraft.util.StatCollector.translateToLocal;
+
+public class CoverAdvancedWirelessControllerGui extends CoverAdvancedWirelessRedstoneBaseGui<CoverAdvancedWirelessController> {
 
     public CoverAdvancedWirelessControllerGui(CoverAdvancedWirelessController cover) {
         super(cover);
@@ -50,71 +58,93 @@ public class CoverAdvancedWirelessControllerGui extends CoverBaseGui<CoverAdvanc
             cover::getRedstoneCondition,
             cover::setRedstoneCondition);
         syncManager.syncValue("condition_mode", conditionModeSyncValue);
+        EnumSyncValue<CoverAdvancedRedstoneReceiverBase.GateMode> gateModeSync = new EnumSyncValue<>(
+            CoverAdvancedRedstoneReceiverBase.GateMode.class,
+            cover::getGateMode,
+            cover::setMode);
+        syncManager.syncValue("gateMode", gateModeSync);
         BooleanSyncValue safeModeSyncValue = new BooleanSyncValue(cover::isSafeMode, cover::setSafeMode);
         StringSyncValue frequencySyncer = new StringSyncValue(cover::getFrequency, cover::setFrequency);
         syncManager.syncValue("frequency", frequencySyncer);
         UUID uuid = data.getPlayer()
             .getUniqueID();
         column.crossAxisAlignment(Alignment.CrossAxis.START)
-            .child(makeFrequencyRow())
-            .child(makeButtonRow(uuid))
             .child(
-                new Grid().coverChildren()
-                    .minElementMarginRight(WIDGET_MARGIN)
-                    .minElementMarginBottom(2)
-                    .minElementMarginTop(0)
-                    .minElementMarginLeft(0)
-                    .alignment(Alignment.CenterLeft)
-                    .row(
-                        new SelectButton()
-                            .value(LinkedBoolValue.of(conditionModeSyncValue, RedstoneCondition.ENABLE_WITH_REDSTONE))
-                            .overlay(GTGuiTextures.OVERLAY_BUTTON_REDSTONE_ON)
-                            .size(16),
-                        IKey.lang("gt.interact.desc.MachContr.EnRedstone")
-                            .asWidget())
-                    .row(
-                        new SelectButton()
-                            .value(LinkedBoolValue.of(conditionModeSyncValue, RedstoneCondition.DISABLE_WITH_REDSTONE))
-                            .overlay(GTGuiTextures.OVERLAY_BUTTON_REDSTONE_OFF)
-                            .size(16),
-                        IKey.lang("gt.interact.desc.MachContr.DisRedstone")
-                            .asWidget())
-                    .row(
-                        new SelectButton().value(LinkedBoolValue.of(conditionModeSyncValue, RedstoneCondition.DISABLE))
-                            .overlay(GTGuiTextures.OVERLAY_BUTTON_CROSS)
-                            .size(16),
-                        IKey.lang("gt.interact.desc.MachContr.DisMachine")
-                            .asWidget())
-                    .row(
-                        new ToggleButton().value(safeModeSyncValue)
-                            .overlay(true, GTGuiTextures.OVERLAY_BUTTON_CHECKMARK)
-                            .overlay(false, GTGuiTextures.OVERLAY_BUTTON_CROSS)
-                            .size(16),
-                        IKey.lang("gt.interact.desc.MachContr.SafeMode")
-                            .asWidget()));
+                new Row().coverChildren()
+                    .crossAxisAlignment(Alignment.CrossAxis.START)
+                    .child(
+                        new Column().coverChildren()
+                            .child(
+                                makeFrequencyRow()
+                            )
+                            .child(
+                                makeButtonRow(uuid)
+                            )
+                            .child(
+                                new Grid().coverChildren()
+                                    .minElementMarginRight(WIDGET_MARGIN)
+                                    .minElementMarginBottom(2)
+                                    .minElementMarginTop(0)
+                                    .minElementMarginLeft(0)
+                                    .alignment(Alignment.CenterLeft)
+                                    .row(
+                                        new SelectButton()
+                                            .value(LinkedBoolValue.of(conditionModeSyncValue, RedstoneCondition.ENABLE_WITH_REDSTONE))
+                                            .overlay(GTGuiTextures.OVERLAY_BUTTON_REDSTONE_ON)
+                                            .size(16),
+                                        IKey.lang("gt.interact.desc.MachContr.EnRedstone")
+                                            .asWidget())
+                                    .row(
+                                        new SelectButton()
+                                            .value(LinkedBoolValue.of(conditionModeSyncValue, RedstoneCondition.DISABLE_WITH_REDSTONE))
+                                            .overlay(GTGuiTextures.OVERLAY_BUTTON_REDSTONE_OFF)
+                                            .size(16),
+                                        IKey.lang("gt.interact.desc.MachContr.DisRedstone")
+                                            .asWidget())
+                                    .row(
+                                        new SelectButton().value(LinkedBoolValue.of(conditionModeSyncValue, RedstoneCondition.DISABLE))
+                                            .overlay(GTGuiTextures.OVERLAY_BUTTON_CROSS)
+                                            .size(16),
+                                        IKey.lang("gt.interact.desc.MachContr.DisMachine")
+                                            .asWidget())
+                                    .row(
+                                        new ToggleButton().value(safeModeSyncValue)
+                                            .overlay(true, GTGuiTextures.OVERLAY_BUTTON_CHECKMARK)
+                                            .overlay(false, GTGuiTextures.OVERLAY_BUTTON_CROSS)
+                                            .size(16),
+                                        IKey.lang("gt.interact.desc.MachContr.SafeMode")
+                                            .asWidget())
+                            )
+                    )
+                .childPadding(40)
+                .child(
+                    makeThirdFlow(syncManager, data)
+                )
+            );
     }
 
-    protected Flow makeFrequencyRow() {
-        return Flow.row()
-            .height(16)
+    @Override
+    protected Flow makeThirdFlow(PanelSyncManager syncManager, CoverGuiData data) {
+        EnumSyncValue<CoverAdvancedRedstoneReceiverBase.GateMode> gateMode = (EnumSyncValue<CoverAdvancedRedstoneReceiverBase.GateMode>) syncManager
+            .getSyncHandlerFromMapKey("gateMode:0");
+        return Flow.column()
+            .size(50, 102)
+            .crossAxisAlignment(Alignment.CrossAxis.CENTER)
+            .mainAxisAlignment(Alignment.MainAxis.END)
+            .child(new TextWidget(translateToLocal("gt.interact.desc.gatemode")))
+            .childPadding(2)
             .child(
-                new TextFieldWidget().syncHandler("frequency")
-                    .height(12)
-                    .width(88)
-                    .marginRight(2))
-            .child(new TextWidget<>(IKey.lang("gt.interact.desc.freq")))
-            .marginBottom(4);
-    }
+                new EnumColumnBuilder<>(CoverAdvancedRedstoneReceiverBase.GateMode.class).value(gateMode)
+                    .overlay(
+                        GTGuiTextures.OVERLAY_BUTTON_GATE_AND,
+                        GTGuiTextures.OVERLAY_BUTTON_GATE_NAND,
+                        GTGuiTextures.OVERLAY_BUTTON_GATE_OR,
+                        GTGuiTextures.OVERLAY_BUTTON_GATE_NOR,
+                        GTGuiTextures.OVERLAY_BUTTON_ANALOG)
+                    .build()
+                    .width(18)
+                    .marginRight(1)
 
-    protected Flow makeButtonRow(UUID uuid) {
-        return Flow.row()
-            .height(20)
-            .child(
-                new ToggleButton().size(16, 16)
-                    .value(new BooleanSyncValue(cover::getPrivacyState, b -> cover.syncPrivacyState(b, uuid)))
-                    .overlay(true, GTGuiTextures.OVERLAY_BUTTON_CHECKMARK)
-                    .overlay(false, GTGuiTextures.OVERLAY_BUTTON_CROSS)
-                    .marginRight(2))
-            .child(new TextWidget<>(IKey.lang("gt.interact.desc.privfreq")));
+            );
     }
 }
