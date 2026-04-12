74601ae5da7c4be155d11eb34e4e62057cdc146c
GitHub GTNH Actions <>
2026-04-12 01:18:16 +0000

    spotlessApply

--- src/main/java/gtPlusPlus/core/entity/monster/EntityStaballoyConstruct.java
+++ src/main/java/gtPlusPlus/core/entity/monster/EntityStaballoyConstruct.java
@@ -3,10 +3,10 @@ package gtPlusPlus.core.entity.monster;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.monster.EntityIronGolem;
-import net.minecraft.village.Village;
-import net.minecraft.world.World;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.village.Village;
+import net.minecraft.world.World;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -59,23 +59,18 @@ public class EntityStaballoyConstruct extends EntityIronGolem {
      * par2 - Level of Looting used to kill this mob.
      */
     @Override
-    protected void dropFewItems(boolean p_70628_1_, int p_70628_2_)
-    {
+    protected void dropFewItems(boolean p_70628_1_, int p_70628_2_) {
         int j = this.rand.nextInt(3);
         int k;
         int lootingChance = p_70628_2_ + 1;
 
-        for (k = 0; k < j; ++k)
-        {
+        for (k = 0; k < j; ++k) {
             this.func_145778_a(Item.getItemFromBlock(Blocks.yellow_flower), 1, 0.0F);
         }
 
+        for (k = 0; k < j; ++k) k = 3 + this.rand.nextInt(3);
 
-        for (k = 0; k < j; ++k)
-            k = 3 + this.rand.nextInt(3);
-
-        for (int l = 0; l < k; ++l)
-        {
+        for (int l = 0; l < k; ++l) {
             this.entityDropItem(MaterialsAlloy.STABALLOY.getIngot(lootingChance), 0f);
         }
     }
