1f984b04fb2171341a224969f01198cfe4ff9e6c
HamCorp <hamknockers@gmail.com>
2026-04-12 19:42:10 +0200

    spotless

--- src/main/java/gregtech/common/gui/modularui/multiblock/MTEBeamCrafterGui.java
+++ src/main/java/gregtech/common/gui/modularui/multiblock/MTEBeamCrafterGui.java
@@ -26,7 +26,6 @@ import com.cleanroommc.modularui.widgets.layout.Column;
 import com.cleanroommc.modularui.widgets.layout.Flow;
 import com.cleanroommc.modularui.widgets.layout.Row;
 
-import gregtech.api.modularui2.GTWidgetThemes;
 import gregtech.common.gui.modularui.multiblock.base.MTEMultiBlockBaseGui;
 import gregtech.common.tileentities.machines.multi.beamcrafting.MTEBeamCrafter;
 import gtnhlanth.common.beamline.Particle;
