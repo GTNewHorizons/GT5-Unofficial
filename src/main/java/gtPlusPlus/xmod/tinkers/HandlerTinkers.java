243c2a250af5ec2d7d3ab1b4ecc39c4876317bc6
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-04-12 18:42:29 +0200

    kill Tinkers util

--- src/main/java/gtPlusPlus/xmod/tinkers/HandlerTinkers.java
+++ src/main/java/gtPlusPlus/xmod/tinkers/HandlerTinkers.java
@@ -1,25 +1,40 @@
 package gtPlusPlus.xmod.tinkers;
 
+import gregtech.api.enums.GTValues;
+import gregtech.api.enums.TierEU;
 import net.minecraftforge.fluids.Fluid;
 
 import gregtech.api.enums.Mods;
 import gtPlusPlus.xmod.thermalfoundation.fluid.TFFluids;
-import gtPlusPlus.xmod.tinkers.util.TinkersDryingRecipe;
-import gtPlusPlus.xmod.tinkers.util.TinkersUtils;
+import tconstruct.library.crafting.DryingRackRecipes;
+import tconstruct.library.crafting.Smeltery;
+
+import static gtPlusPlus.api.recipe.GTPPRecipeMaps.chemicalDehydratorRecipes;
 
 public class HandlerTinkers {
 
     public static void postInit() {
         if (Mods.TinkerConstruct.isModLoaded()) {
-
             Fluid pyrotheumFluid = TFFluids.fluidPyrotheum;
             if (pyrotheumFluid != null) {
                 // Enable Pyrotheum as Fuel for the Smeltery
-                TinkersUtils.addSmelteryFuel(pyrotheumFluid, 5000, 70); // pyrotheum lasts 3.5 seconds per 15 mb
+                // pyrotheum lasts 3.5 seconds per 15 mb
+                Smeltery.addSmelteryFuel(pyrotheumFluid, 5000, 70);
             }
-
             // Generate Drying Rack recipes
-            TinkersDryingRecipe.generateAllDryingRecipes();
+            generateAllDryingRecipes();
+        }
+    }
+
+    private static void generateAllDryingRecipes() {
+        for (DryingRackRecipes.DryingRecipe r : DryingRackRecipes.recipes) {
+            GTValues.RA.stdBuilder()
+                .itemInputs(r.input)
+                .circuit(16)
+                .itemOutputs(r.result)
+                .eut(TierEU.RECIPE_LV)
+                .duration(r.time / 10)
+                .addTo(chemicalDehydratorRecipes);
         }
     }
 }
