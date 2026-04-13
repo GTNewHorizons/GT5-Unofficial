6acf32e6321b714fa032a21ae36b5069909fd296
kuba6000 <kuba.123123.6000@gmail.com>
2026-04-13 15:35:12 +0200

    Update HTGRItem.java

--- src/main/java/kubatech/loaders/item/htgritem/HTGRItem.java
+++ src/main/java/kubatech/loaders/item/htgritem/HTGRItem.java
@@ -9,7 +9,6 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.StatCollector;
 import net.minecraftforge.client.MinecraftForgeClient;
 
