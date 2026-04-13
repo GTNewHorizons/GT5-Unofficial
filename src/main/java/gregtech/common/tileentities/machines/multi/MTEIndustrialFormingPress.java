6a53fa9a96888430323497ed018c775f123545a0
yoshy2002 <jascha.morzuch@gmail.com>
2026-04-13 22:18:49 +0200

    2nd author

--- src/main/java/gregtech/common/tileentities/machines/multi/MTEIndustrialFormingPress.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/MTEIndustrialFormingPress.java
@@ -11,6 +11,7 @@ import static gregtech.api.util.GTStructureUtility.buildHatchAdder;
 import static gregtech.api.util.GTStructureUtility.ofFrame;
 
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.common.util.ForgeDirection;
 
 import com.gtnewhorizon.structurelib.alignment.constructable.ISurvivalConstructable;
@@ -94,6 +95,7 @@ public class MTEIndustrialFormingPress extends MTEExtendedPowerMultiBlockBase<MT
             .addEnergyHatch("Metalworking Machine Casing", 1)
             .addMaintenanceHatch("Metalworking Machine Casing", 1)
             .addMufflerHatch("Metalworking Machine Casing", 1)
+            .addStructureAuthors(EnumChatFormatting.GOLD + "barnac")
             .toolTipFinisher();
         return tt;
     }
