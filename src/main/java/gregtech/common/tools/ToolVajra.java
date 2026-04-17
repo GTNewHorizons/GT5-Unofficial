cd3a229f59806fbafa9ff111d3481440a6236b45
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-04-17 18:25:51 +0200

    Revert "Fix vajra ghost block (#6402)"
    This reverts commit 08756e9c80a01c2c1ecef209c484b1851628440e.

--- src/main/java/gregtech/common/tools/ToolVajra.java
+++ src/main/java/gregtech/common/tools/ToolVajra.java
@@ -180,7 +180,7 @@ public class ToolVajra extends ItemTool implements IElectricItem {
         stack.getTagCompound()
             .setBoolean("harvested", true); // prevent onItemRightClick from going through
         ElectricItem.manager.use(stack, baseCost, player);
-        return true;
+        return super.onItemUse(stack, player, world, x, y, z, side, hitX, hitY, hitZ);
     }
 
     private boolean isHarvestableOwned(TileEntity tileEntity, EntityPlayer player) {
