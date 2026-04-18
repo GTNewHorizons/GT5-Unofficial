39b349fca3a45adb14dd24fa3e8b160158971938
TheYoingLad <golyhagergo@gmail.com>
2026-04-18 13:12:03 +0200

    fixed merge issue

--- src/main/java/gtPlusPlus/xmod/gregtech/api/metatileentity/implementations/MTEHatchSteamBusOutput.java
+++ src/main/java/gtPlusPlus/xmod/gregtech/api/metatileentity/implementations/MTEHatchSteamBusOutput.java
@@ -172,7 +172,5 @@ public class MTEHatchSteamBusOutput extends MTEHatchOutputBus {
                 return 2;
             }
         }.build(guiData, syncManager, uiSettings);
-    protected boolean useMui2() {
-        return false;
     }
 }
