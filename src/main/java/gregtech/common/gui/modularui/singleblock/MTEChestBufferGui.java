7c35d56869d7fc20c6f5060ed1a1ff738c6d7050
TheYoingLad <golyhagergo@gmail.com>
2026-04-20 13:09:27 +0200

    ported MTEChestBuffer to MUI2

---
+++ src/main/java/gregtech/common/gui/modularui/singleblock/MTEChestBufferGui.java
@@ -0,0 +1,52 @@
+package gregtech.common.gui.modularui.singleblock;
+
+import java.util.Arrays;
+
+import com.cleanroommc.modularui.screen.ModularPanel;
+import com.cleanroommc.modularui.value.sync.PanelSyncManager;
+import com.cleanroommc.modularui.widget.ParentWidget;
+import com.cleanroommc.modularui.widgets.SlotGroupWidget;
+import com.cleanroommc.modularui.widgets.layout.Flow;
+import com.cleanroommc.modularui.widgets.slot.ItemSlot;
+import com.cleanroommc.modularui.widgets.slot.ModularSlot;
+
+import gregtech.api.modularui2.GTGuiTextures;
+import gregtech.common.gui.modularui.singleblock.base.MTEBufferBaseGui;
+import gregtech.common.tileentities.automation.MTEChestBuffer;
+
+public class MTEChestBufferGui extends MTEBufferBaseGui<MTEChestBuffer> {
+
+    public MTEChestBufferGui(MTEChestBuffer machine) {
+        super(machine);
+    }
+
+    @Override
+    protected ParentWidget<?> createContentSection(ModularPanel panel, PanelSyncManager syncManager) {
+        String[] matrix = new String[3];
+        Arrays.fill(matrix, "sssssssss");
+
+        return super.createContentSection(panel, syncManager).child(
+            SlotGroupWidget.builder()
+                .matrix(matrix)
+                .key(
+                    's',
+                    index -> new ItemSlot()
+                        .slot(new ModularSlot(machine.inventoryHandler, index).slotGroup("item_inv")))
+                .build()
+                .horizontalCenter());
+    }
+
+    @Override
+    protected Flow createLeftCornerFlow(ModularPanel panel, PanelSyncManager syncManager) {
+        return super.createLeftCornerFlow(panel, syncManager).child(
+            createArrow(GTGuiTextures.PICTURE_ARROW_22_RED, 50, 22).marginBottom(1)
+                .marginLeft(1));
+    }
+
+    @Override
+    protected void registerSyncValues(PanelSyncManager syncManager) {
+        super.registerSyncValues(syncManager);
+
+        syncManager.registerSlotGroup("item_inv", 3);
+    }
+}
