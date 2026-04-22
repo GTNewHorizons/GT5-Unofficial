7b5b5a7b19236ac29193650f2cf4e2f34d0a5a4b
NeutronSelector <NeutronSelector@pm.me>
2026-04-22 20:10:47 +0200

    fix java import

--- src/main/java/gregtech/common/worldgen/WorldgenRubberTrees.java
+++ src/main/java/gregtech/common/worldgen/WorldgenRubberTrees.java
@@ -1,6 +1,10 @@
 package gregtech.common.worldgen;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockSapling;
