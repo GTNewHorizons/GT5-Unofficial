b6dc3a81c927cd6da3b9f81dbb9580b85b8b7513
Kogepan229 <kogepan25629@gmail.com>
2026-04-22 17:10:20 +0900

    Fix the pattern update during channel loss in LMA

--- src/main/java/gregtech/common/tileentities/machines/multi/MTELargeMolecularAssembler.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/MTELargeMolecularAssembler.java
@@ -49,7 +49,9 @@ import appeng.api.networking.IGridNode;
 import appeng.api.networking.crafting.ICraftingPatternDetails;
 import appeng.api.networking.crafting.ICraftingProvider;
 import appeng.api.networking.crafting.ICraftingProviderHelper;
+import appeng.api.networking.events.MENetworkChannelsChanged;
 import appeng.api.networking.events.MENetworkCraftingPatternChange;
+import appeng.api.networking.events.MENetworkEventSubscribe;
 import appeng.api.networking.security.BaseActionSource;
 import appeng.api.networking.security.IActionHost;
 import appeng.api.networking.security.MachineSource;
@@ -666,11 +668,24 @@ public class MTELargeMolecularAssembler extends MTEExtendedPowerMultiBlockBase<M
         return withAeJobs(($, aeJobs) -> aeJobs.size() >= 256);
     }
 
+    @MENetworkEventSubscribe
+    public void channelStateChange(final MENetworkChannelsChanged c) {
+        AENetworkProxy proxy = getProxy();
+        if (proxy == null) return;
+
+        if (proxy.isActive()) {
+            try {
+                proxy.getGrid()
+                    .postEvent(new MENetworkCraftingPatternChange(this, proxy.getNode()));
+            } catch (final GridAccessException ignored) {}
+        }
+    }
+
     @Override
     public void provideCrafting(ICraftingProviderHelper craftingTracker) {
         AENetworkProxy proxy = getProxy();
         if (proxy == null) return;
-        if (proxy.isReady()) {
+        if (proxy.isActive()) {
             for (ICraftingPatternDetails detail : cachedPatternDetail.values()) {
                 craftingTracker.addCraftingOption(this, detail);
             }
