7975edc7cbff257963ba0fc6b323e5cf9add4686
hanben02 <hannes.bengtsson02@outlook.com>
2026-04-25 15:28:58 +0200

    Add secondary color tags

--- src/main/java/tectech/thing/metaTileEntity/multi/MTEResearchStation.java
+++ src/main/java/tectech/thing/metaTileEntity/multi/MTEResearchStation.java
@@ -337,8 +337,7 @@ public class MTEResearchStation extends TTMultiblockBase implements ISurvivalCon
     }
 
     private static String getMachineModeKey(int mode) {
-        if (mode == MODE_RESEARCH_STATION) return "gt.blockmachines.multimachine.em.research.mode.Assembly_line";
-        return "gt.blockmachines.multimachine.em.research.mode.Scanner";
+        return "gt.blockmachines.multimachine.em.research.mode." + mode;
     }
 
     // endregion machine mode
@@ -856,7 +855,7 @@ public class MTEResearchStation extends TTMultiblockBase implements ISurvivalCon
         tag.setBoolean("hasProblems", (getIdealStatus() - getRepairStatus()) > 0);
         tag.setFloat("efficiency", this.mEfficiency / 100.0F);
         tag.setBoolean("incompleteStructure", (getErrorDisplayID() & 64) != 0);
-        tag.setInteger("machineMode", this.machineMode);
+        tag.setString("mode", StatCollector.translateToLocal(this.getMachineModeKey()));
         tag.setLong("computation", getComputationConsumed());
         tag.setLong("computationRequired", getComputationRequired());
     }
@@ -866,7 +865,6 @@ public class MTEResearchStation extends TTMultiblockBase implements ISurvivalCon
         IWailaConfigHandler config) {
         super.getWailaBody(itemStack, currentTip, accessor, config);
         final NBTTagCompound tag = accessor.getNBTData();
-        currentTip.add(StatCollector.translateToLocal(getMachineModeKey(tag.getInteger("machineMode"))));
         currentTip.add(
             StatCollector.translateToLocalFormatted(
                 "gt.blockmachines.multimachine.em.research.computation",
