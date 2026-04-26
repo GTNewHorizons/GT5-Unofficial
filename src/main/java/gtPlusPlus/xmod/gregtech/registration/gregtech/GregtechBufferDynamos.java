421ddbde092f36315948779b3c23874c9aaa40da
Ruling-0 <47913168+Ruling-0@users.noreply.github.com>
2026-04-25 20:43:57 -0500

    Add conversion recipes and remove old buffered dynamo recipes

--- src/main/java/gtPlusPlus/xmod/gregtech/registration/gregtech/GregtechBufferDynamos.java
+++ src/main/java/gtPlusPlus/xmod/gregtech/registration/gregtech/GregtechBufferDynamos.java
@@ -10,6 +10,7 @@ import static gregtech.api.enums.MetaTileEntityIDs.Hatch_Buffer_Dynamo_MV;
 import static gregtech.api.enums.MetaTileEntityIDs.Hatch_Buffer_Dynamo_ULV;
 import static gregtech.api.enums.MetaTileEntityIDs.Hatch_Buffer_Dynamo_UV;
 import static gregtech.api.enums.MetaTileEntityIDs.Hatch_Buffer_Dynamo_ZPM;
+import static gregtech.api.util.GTModHandler.RecipeBits.BITS;
 import static gregtech.api.util.GTModHandler.RecipeBits.BITSD;
 
 import gregtech.api.enums.ItemList;
@@ -18,6 +19,7 @@ import gregtech.api.enums.OrePrefixes;
 import gregtech.api.util.GTModHandler;
 import gtPlusPlus.xmod.gregtech.api.enums.GregtechItemList;
 import gtPlusPlus.xmod.gregtech.api.metatileentity.implementations.MTEHatchDynamoBuffer;
+import tectech.thing.CustomItemList;
 
 public class GregtechBufferDynamos {
 
@@ -86,55 +88,24 @@ public class GregtechBufferDynamos {
                 "hatch.dynamo.buffer.tier.09",
                 "UHV Dynamo Hatch [Buffered]",
                 9).getStackForm(1L));
-        GTModHandler.addCraftingRecipe(
-            GregtechItemList.Hatch_Buffer_Dynamo_ULV.get(1L),
-            BITSD,
-            new Object[] { "TMC", 'M', ItemList.Hatch_Dynamo_ULV, 'T', "circuitPrimitive", 'C',
-                OrePrefixes.cableGt04.get(Materials.Lead) });
-        GTModHandler.addCraftingRecipe(
-            GregtechItemList.Hatch_Buffer_Dynamo_LV.get(1L),
-            BITSD,
-            new Object[] { "TMC", 'M', ItemList.Hatch_Dynamo_LV, 'T', "circuitBasic", 'C',
-                OrePrefixes.cableGt04.get(Materials.Tin) });
-        GTModHandler.addCraftingRecipe(
-            GregtechItemList.Hatch_Buffer_Dynamo_MV.get(1L),
-            BITSD,
-            new Object[] { "TMC", 'M', ItemList.Hatch_Dynamo_MV, 'T', "circuitGood", 'C',
-                OrePrefixes.cableGt04.get(Materials.AnyCopper) });
-        GTModHandler.addCraftingRecipe(
-            GregtechItemList.Hatch_Buffer_Dynamo_HV.get(1L),
-            BITSD,
-            new Object[] { "TMC", 'M', ItemList.Hatch_Dynamo_HV, 'T', "circuitAdvanced", 'C',
-                OrePrefixes.cableGt04.get(Materials.Gold) });
-        GTModHandler.addCraftingRecipe(
-            GregtechItemList.Hatch_Buffer_Dynamo_EV.get(1L),
-            BITSD,
-            new Object[] { "TMC", 'M', ItemList.Hatch_Dynamo_EV, 'T', "circuitData", 'C',
-                OrePrefixes.cableGt04.get(Materials.Aluminium) });
-        GTModHandler.addCraftingRecipe(
-            GregtechItemList.Hatch_Buffer_Dynamo_IV.get(1L),
-            BITSD,
-            new Object[] { "TMC", 'M', ItemList.Hatch_Dynamo_IV, 'T', "circuitElite", 'C',
-                OrePrefixes.cableGt04.get(Materials.Tungsten) });
-        GTModHandler.addCraftingRecipe(
-            GregtechItemList.Hatch_Buffer_Dynamo_LuV.get(1L),
-            BITSD,
-            new Object[] { "TMC", 'M', ItemList.Hatch_Dynamo_LuV, 'T', "circuitMaster", 'C',
-                OrePrefixes.cableGt04.get(Materials.VanadiumGallium) });
-        GTModHandler.addCraftingRecipe(
-            GregtechItemList.Hatch_Buffer_Dynamo_ZPM.get(1L),
-            BITSD,
-            new Object[] { "TMC", 'M', ItemList.Hatch_Dynamo_ZPM, 'T', "circuitUltimate", 'C',
-                OrePrefixes.cableGt04.get(Materials.Naquadah) });
-        GTModHandler.addCraftingRecipe(
-            GregtechItemList.Hatch_Buffer_Dynamo_UV.get(1L),
-            BITSD,
-            new Object[] { "TMC", 'M', ItemList.Hatch_Dynamo_UV, 'T', "circuitSuperconductor", 'C',
-                OrePrefixes.wireGt12.get(Materials.NaquadahAlloy) });
-        GTModHandler.addCraftingRecipe(
-            GregtechItemList.Hatch_Buffer_Dynamo_MAX.get(1L),
-            BITSD,
-            new Object[] { "TMC", 'M', ItemList.Hatch_Dynamo_UHV, 'T', "circuitInfinite", 'C',
-                OrePrefixes.wireGt04.get(Materials.SuperconductorUHV) });
+        // Conversion Recipes, to be removed in 2.10 (alongside this whole class).
+        GTModHandler.addShapelessCraftingRecipe(
+            CustomItemList.eM_dynamoMulti4_HV.get(1L),
+            new Object[] {GregtechItemList.Hatch_Buffer_Dynamo_HV.get(1L)});
+        GTModHandler.addShapelessCraftingRecipe(
+            CustomItemList.eM_dynamoMulti4_EV.get(1L),
+            new Object[] {GregtechItemList.Hatch_Buffer_Dynamo_EV.get(1L)});
+        GTModHandler.addShapelessCraftingRecipe(
+            CustomItemList.eM_dynamoMulti4_IV.get(1L),
+            new Object[] {GregtechItemList.Hatch_Buffer_Dynamo_IV.get(1L)});
+        GTModHandler.addShapelessCraftingRecipe(
+            CustomItemList.eM_dynamoMulti4_LuV.get(1L),
+            new Object[] {GregtechItemList.Hatch_Buffer_Dynamo_LuV.get(1L)});
+        GTModHandler.addShapelessCraftingRecipe(
+            CustomItemList.eM_dynamoMulti4_ZPM.get(1L),
+            new Object[] {GregtechItemList.Hatch_Buffer_Dynamo_ZPM.get(1L)});
+        GTModHandler.addShapelessCraftingRecipe(
+            CustomItemList.eM_dynamoMulti4_UV.get(1L),
+            new Object[] {GregtechItemList.Hatch_Buffer_Dynamo_UV.get(1L)});
     }
 }
