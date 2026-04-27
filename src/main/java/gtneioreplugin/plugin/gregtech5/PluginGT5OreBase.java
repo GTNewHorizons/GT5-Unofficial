e571ef7bfa659c0770cecaa32b43a9f52335f47c
tiffit <10173431+tiffit@users.noreply.github.com>
2026-04-26 20:48:54 -0700

    initial small ore stat page rework

---
+++ src/main/java/gtneioreplugin/plugin/gregtech5/PluginGT5OreBase.java
@@ -0,0 +1,143 @@
+package gtneioreplugin.plugin.gregtech5;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import net.minecraft.client.resources.I18n;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
+
+import com.google.common.collect.ImmutableList;
+
+import codechicken.lib.gui.GuiDraw;
+import codechicken.nei.PositionedStack;
+import gregtech.api.enums.Materials;
+import gregtech.api.enums.OrePrefixes;
+import gregtech.api.enums.StoneType;
+import gregtech.api.interfaces.IOreMaterial;
+import gregtech.common.ores.OreInfo;
+import gregtech.common.ores.OreManager;
+import gtneioreplugin.plugin.PluginBase;
+import gtneioreplugin.plugin.item.ItemDimensionDisplay;
+import gtneioreplugin.util.DimensionHelper;
+import gtneioreplugin.util.DimensionHelper.Dimension;
+
+public abstract class PluginGT5OreBase extends PluginBase {
+
+    protected static final int LEFT_PADDING = 2;
+    protected static final int TITLE_Y_POS = 1;
+
+    // spotless:off
+    public static final List<OrePrefixes> PREFIX_WHITELIST = ImmutableList.of(
+        OrePrefixes.dust,
+        OrePrefixes.dustPure,
+        OrePrefixes.dustImpure,
+        OrePrefixes.oreBlackgranite,
+        OrePrefixes.oreRedgranite,
+        OrePrefixes.oreMarble,
+        OrePrefixes.oreBasalt,
+        OrePrefixes.oreNetherrack,
+        OrePrefixes.oreNether,
+        OrePrefixes.oreEndstone,
+        OrePrefixes.oreEnd,
+        OrePrefixes.ore,
+        OrePrefixes.crushedCentrifuged,
+        OrePrefixes.crushedPurified,
+        OrePrefixes.crushed,
+        OrePrefixes.rawOre,
+        OrePrefixes.gemChipped,
+        OrePrefixes.gemFlawed,
+        OrePrefixes.gemFlawless,
+        OrePrefixes.gemExquisite,
+        OrePrefixes.gem
+    );
+    // spotless:on
+
+    protected String[] sortDimNamesByTier(String[] dims) {
+        Arrays.sort(dims, (a, b) -> {
+            int indexA = DimensionHelper.getIndexByAbbr(a);
+            int indexB = DimensionHelper.getIndexByAbbr(b);
+            return Integer.compare(indexA, indexB);
+        });
+        return dims;
+    }
+
+    protected String getGTOreLocalizedName(IOreMaterial ore, boolean small) {
+        if (ore == Materials.DraconiumAwakened) return "Aw. Draconium Ore";
+
+        try (OreInfo<IOreMaterial> info = OreInfo.getNewInfo()) {
+            info.material = ore;
+            info.isSmall = small;
+
+            return OreManager.getLocalizedName(info);
+        }
+    }
+
+    protected void createDimensionDisplayItems(String[] dimAbbr, int headerY, List<PositionedStack> list) {
+        int count = 0;
+        int itemsPerLine = 9;
+        int itemSize = 18;
+        for (String dim : dimAbbr) {
+            ItemStack item = ItemDimensionDisplay.getItem(dim);
+            if (item != null) {
+                int xPos = LEFT_PADDING + itemSize * (count % itemsPerLine);
+                int yPos = headerY + 10 + itemSize * (count / itemsPerLine);
+                list.add(new PositionedStack(item, xPos, yPos, false));
+                count++;
+            }
+        }
+    }
+
+    protected Set<StoneType> getStoneTypesForDimensions(String[] dimAbbr) {
+        Set<StoneType> stoneTypes = new HashSet<>();
+
+        for (String abbr : dimAbbr) {
+            Dimension dimension = DimensionHelper.getByIndex(DimensionHelper.getIndexByAbbr(abbr));
+            if (dimension != null) {
+                stoneTypes.addAll(dimension.stoneTypes());
+            }
+        }
+        return stoneTypes;
+    }
+
+    protected void updateRenderPermutation(PositionedStack ps) {
+        ps.setPermutationToRender((cycleticks / 20) % ps.items.length);
+    }
+
+    protected List<String> getTitleLines(String text) {
+        return GuiDraw.fontRenderer.listFormattedStringToWidth(text, getGuiWidth() - 4);
+    }
+
+    protected void drawTitle(List<String> lines) {
+        GuiDraw.drawRect(0, TITLE_Y_POS - 1, getGuiWidth(), lines.size() * 10 + 1, 0xff939393);
+        for (int i = 0; i < lines.size(); i++) {
+            String line = lines.get(i);
+            int x = (getGuiWidth() - GuiDraw.fontRenderer.getStringWidth(line)) / 2;
+            GuiDraw.drawString(line, x, TITLE_Y_POS + i * 11, 0xfafafa, true);
+        }
+    }
+
+    protected void drawLine(String lineKey, String value, int x, int y) {
+        String text = I18n.format(lineKey) + ": " + value;
+        drawLine(text, x, y);
+    }
+
+    protected void drawLine(String text, int x, int y) {
+        String trimmed = text;
+        if (GuiDraw.fontRenderer.getStringWidth(text) > getGuiWidth() - x) {
+            trimmed = GuiDraw.fontRenderer.trimStringToWidth(text, getGuiWidth() - x - 10);
+        }
+
+        GuiDraw.drawString(trimmed + (trimmed.length() < text.length() ? "..." : ""), x, y, 0x404040, false);
+    }
+
+    protected void drawHeader(String key, int x, int y) {
+        GuiDraw.drawString(EnumChatFormatting.UNDERLINE + I18n.format(key), x, y, 0x303030, false);
+    }
+
+    protected void drawDimHeader(int y) {
+        GuiDraw.drawString(I18n.format("gtnop.gui.nei.worldNames") + ": ", 2, y, 0x404040, false);
+    }
+}
--- src/main/java/gtneioreplugin/plugin/gregtech5/PluginGT5VeinStat.java
+++ src/main/java/gtneioreplugin/plugin/gregtech5/PluginGT5VeinStat.java
@@ -3,15 +3,12 @@ package gtneioreplugin.plugin.gregtech5;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumChatFormatting;
-
-import com.google.common.collect.ImmutableList;
 
 import codechicken.nei.PositionedStack;
 import codechicken.nei.recipe.GuiRecipe;
@@ -22,45 +19,17 @@ import gregtech.api.enums.StoneType;
 import gregtech.api.interfaces.IOreMaterial;
 import gregtech.common.ores.OreManager;
 import gtneioreplugin.plugin.item.ItemDimensionDisplay;
-import gtneioreplugin.util.DimensionHelper;
-import gtneioreplugin.util.DimensionHelper.Dimension;
 import gtneioreplugin.util.GT5OreLayerHelper;
 import gtneioreplugin.util.GT5OreLayerHelper.NormalOreDimensionWrapper;
 import gtneioreplugin.util.GT5OreLayerHelper.OreLayerWrapper;
 import gtneioreplugin.util.OreVeinLayer;
 
-public class PluginGT5VeinStat extends PluginGT5Base {
+public class PluginGT5VeinStat extends PluginGT5OreBase {
 
     private static final int VEIN_LAYER_START_Y = 23;
     private static final int VEIN_LAYER_HEIGHT = 22;
     private static final int VEIN_INFO_Y_POS = 110;
     private static final int DIM_HEADER_Y_POS = 120;
-
-    // spotless:off
-    public static final List<OrePrefixes> PREFIX_WHITELIST = ImmutableList.of(
-        OrePrefixes.dust,
-        OrePrefixes.dustPure,
-        OrePrefixes.dustImpure,
-        OrePrefixes.oreBlackgranite,
-        OrePrefixes.oreRedgranite,
-        OrePrefixes.oreMarble,
-        OrePrefixes.oreBasalt,
-        OrePrefixes.oreNetherrack,
-        OrePrefixes.oreNether,
-        OrePrefixes.oreEndstone,
-        OrePrefixes.oreEnd,
-        OrePrefixes.ore,
-        OrePrefixes.crushedCentrifuged,
-        OrePrefixes.crushedPurified,
-        OrePrefixes.crushed,
-        OrePrefixes.rawOre,
-        OrePrefixes.gemChipped,
-        OrePrefixes.gemFlawed,
-        OrePrefixes.gemFlawless,
-        OrePrefixes.gemExquisite,
-        OrePrefixes.gem
-    );
-    // spotless:on
 
     @Override
     public void loadCraftingRecipes(String outputId, Object... results) {
@@ -131,14 +100,7 @@ public class PluginGT5VeinStat extends PluginGT5Base {
     private void addVeinWithLayers(OreLayerWrapper oreVein) {
         String[] dimAbbr = getDimNameArrayFromVeinName(oreVein.veinName);
 
-        Set<StoneType> stoneTypes = new HashSet<>();
-
-        for (String abbr : dimAbbr) {
-            Dimension dimension = DimensionHelper.getByIndex(DimensionHelper.getIndexByAbbr(abbr));
-            if (dimension != null) {
-                stoneTypes.addAll(dimension.stoneTypes());
-            }
-        }
+        Set<StoneType> stoneTypes = getStoneTypesForDimensions(dimAbbr);
 
         this.arecipes.add(
             new CachedVeinStatRecipe(
@@ -174,12 +136,7 @@ public class PluginGT5VeinStat extends PluginGT5Base {
     private String[] getDimNameArrayFromVeinName(String veinName) {
         OreLayerWrapper oreLayer = GT5OreLayerHelper.getVeinByName(veinName);
         String[] dims = oreLayer.abbrDimNames.toArray(new String[0]);
-
-        Arrays.sort(dims, (a, b) -> {
-            int indexA = DimensionHelper.getIndexByAbbr(a);
-            int indexB = DimensionHelper.getIndexByAbbr(b);
-            return Integer.compare(indexA, indexB);
-        });
+        sortDimNamesByTier(dims);
         return dims;
     }
 
@@ -201,30 +158,25 @@ public class PluginGT5VeinStat extends PluginGT5Base {
         public final PositionedStack positionedStackSporadic;
         private final List<PositionedStack> dimensionDisplayItems = new ArrayList<>();
 
+        private final List<String> title;
+
         public CachedVeinStatRecipe(OreLayerWrapper oreVein, String[] dimAbbr, List<ItemStack> stackListPrimary,
             List<ItemStack> stackListSecondary, List<ItemStack> stackListBetween, List<ItemStack> stackListSporadic) {
             this.oreVein = oreVein;
-            positionedStackPrimary = new PositionedStack(stackListPrimary, 0, VEIN_LAYER_START_Y);
-            positionedStackSecondary = new PositionedStack(stackListSecondary, 0, 0);
-            positionedStackBetween = new PositionedStack(stackListBetween, 0, 0);
-            positionedStackSporadic = new PositionedStack(stackListSporadic, 0, 0);
-            updateOreYPos(0);
-            setDimensionDisplayItems(dimAbbr);
-        }
 
-        private void setDimensionDisplayItems(String[] dimAbbr) {
-            int count = 0;
-            int itemsPerLine = 9;
-            int itemSize = 18;
-            for (String dim : dimAbbr) {
-                ItemStack item = ItemDimensionDisplay.getItem(dim);
-                if (item != null) {
-                    int xPos = LEFT_PADDING + itemSize * (count % itemsPerLine);
-                    int yPos = DIM_HEADER_Y_POS + 10 + itemSize * (count / itemsPerLine);
-                    dimensionDisplayItems.add(new PositionedStack(item, xPos, yPos, false));
-                    count++;
-                }
+            String titleText = oreVein.getLocalizedName() + " " + I18n.format("gtnop.gui.nei.vein");
+            title = getTitleLines(titleText);
+
+            int stackPosY = VEIN_LAYER_START_Y;
+            if (title.size() == 1) {
+                stackPosY -= 5;
             }
+            positionedStackPrimary = new PositionedStack(stackListPrimary, 0, stackPosY);
+            positionedStackSecondary = new PositionedStack(stackListSecondary, 0, stackPosY + VEIN_LAYER_HEIGHT);
+            positionedStackBetween = new PositionedStack(stackListBetween, 0, stackPosY + VEIN_LAYER_HEIGHT * 2);
+            positionedStackSporadic = new PositionedStack(stackListSporadic, 0, stackPosY + VEIN_LAYER_HEIGHT * 3);
+
+            createDimensionDisplayItems(dimAbbr, DIM_HEADER_Y_POS, dimensionDisplayItems);
         }
 
         @Override
@@ -240,10 +192,10 @@ public class PluginGT5VeinStat extends PluginGT5Base {
         @Override
         public List<PositionedStack> getOtherStacks() {
             List<PositionedStack> outputs = new ArrayList<>();
-            positionedStackPrimary.setPermutationToRender((cycleticks / 20) % positionedStackPrimary.items.length);
-            positionedStackSecondary.setPermutationToRender((cycleticks / 20) % positionedStackSecondary.items.length);
-            positionedStackBetween.setPermutationToRender((cycleticks / 20) % positionedStackBetween.items.length);
-            positionedStackSporadic.setPermutationToRender((cycleticks / 20) % positionedStackSporadic.items.length);
+            updateRenderPermutation(positionedStackPrimary);
+            updateRenderPermutation(positionedStackSecondary);
+            updateRenderPermutation(positionedStackBetween);
+            updateRenderPermutation(positionedStackSporadic);
             outputs.add(positionedStackPrimary);
             outputs.add(positionedStackSecondary);
             outputs.add(positionedStackBetween);
@@ -268,17 +220,10 @@ public class PluginGT5VeinStat extends PluginGT5Base {
             }
         }
 
-        private void updateOreYPos(int yOffset) {
-            positionedStackPrimary.rely = VEIN_LAYER_START_Y + yOffset;
-            positionedStackSecondary.rely = VEIN_LAYER_START_Y + VEIN_LAYER_HEIGHT + yOffset;
-            positionedStackBetween.rely = VEIN_LAYER_START_Y + VEIN_LAYER_HEIGHT * 2 + yOffset;
-            positionedStackSporadic.rely = VEIN_LAYER_START_Y + VEIN_LAYER_HEIGHT * 3 + yOffset;
-        }
-
         public void drawExtra() {
-            int numLines = drawVeinName(oreVein);
+            drawTitle(title);
 
-            drawVeinLayerNames(oreVein, numLines);
+            drawVeinLayerNames();
 
             drawVeinInfo(oreVein);
             drawDimHeader(DIM_HEADER_Y_POS);
@@ -286,22 +231,18 @@ public class PluginGT5VeinStat extends PluginGT5Base {
             drawSeeAllRecipesLabel();
         }
 
-        private int drawVeinName(OreLayerWrapper oreLayer) {
-            String text = oreLayer.getLocalizedName() + " " + I18n.format("gtnop.gui.nei.vein");
-            return drawTitle(text);
+        private void drawVeinLayerNames() {
+            drawVeinLayerNameLine(oreVein, OreVeinLayer.VEIN_PRIMARY, 0);
+            drawVeinLayerNameLine(oreVein, OreVeinLayer.VEIN_SECONDARY, 1);
+            drawVeinLayerNameLine(oreVein, OreVeinLayer.VEIN_BETWEEN, 2);
+            drawVeinLayerNameLine(oreVein, OreVeinLayer.VEIN_SPORADIC, 3);
         }
 
-        private void drawVeinLayerNames(OreLayerWrapper oreLayer, int numLines) {
-            int yOffset = numLines == 1 ? -5 : 0;
-            drawVeinLayerNameLine(oreLayer, OreVeinLayer.VEIN_PRIMARY, 0, yOffset);
-            drawVeinLayerNameLine(oreLayer, OreVeinLayer.VEIN_SECONDARY, 1, yOffset);
-            drawVeinLayerNameLine(oreLayer, OreVeinLayer.VEIN_BETWEEN, 2, yOffset);
-            drawVeinLayerNameLine(oreLayer, OreVeinLayer.VEIN_SPORADIC, 3, yOffset);
-            updateOreYPos(yOffset);
-        }
-
-        private void drawVeinLayerNameLine(OreLayerWrapper oreLayer, int veinLayer, int index, int yOffset) {
-            int height = yOffset + VEIN_LAYER_START_Y + VEIN_LAYER_HEIGHT * index;
+        private void drawVeinLayerNameLine(OreLayerWrapper oreLayer, int veinLayer, int index) {
+            int height = VEIN_LAYER_START_Y + VEIN_LAYER_HEIGHT * index;
+            if (title.size() == 1) {
+                height -= 5;
+            }
             drawHeader(OreVeinLayer.getOreVeinLayerName(veinLayer), LEFT_PADDING + 16, height);
             drawLine(getGTOreLocalizedName(oreLayer.ores[veinLayer], false), LEFT_PADDING + 16, height + 10);
         }
