f7864ef469a3ccaecb6f055e9d271a6bb63e3257
tiffit <10173431+tiffit@users.noreply.github.com>
2026-05-08 11:29:03 -0700

    added getTickRateButtonPadding

--- src/main/java/gregtech/common/gui/modularui/cover/base/CoverBaseGui.java
+++ src/main/java/gregtech/common/gui/modularui/cover/base/CoverBaseGui.java
@@ -97,6 +97,7 @@ public class CoverBaseGui<T extends Cover> {
             panel.child(
                 new CoverTickRateButton(cover, syncManager).right(4)
                     .bottom(4));
+            mainColumn.paddingRight(getTickRateButtonPadding());
         }
         if (!data.isPopUp() && doesBindPlayerInventory()) {
             mainColumn.child(
@@ -175,6 +176,10 @@ public class CoverBaseGui<T extends Cover> {
 
     protected boolean shouldIncludeTitleInPopUp() {
         return true;
+    }
+
+    protected int getTickRateButtonPadding() {
+        return 20;
     }
 
 }
