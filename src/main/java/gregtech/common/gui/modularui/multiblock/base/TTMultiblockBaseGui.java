8e5d87011f43623a294eda591e54eaffeab9c750
TheYoingLad <golyhagergo@gmail.com>
2026-05-16 16:28:09 +0200

    missing bracket

--- src/main/java/gregtech/common/gui/modularui/multiblock/base/TTMultiblockBaseGui.java
+++ src/main/java/gregtech/common/gui/modularui/multiblock/base/TTMultiblockBaseGui.java
@@ -269,5 +269,6 @@ public class TTMultiblockBaseGui<T extends TTMultiblockBase> extends MTEMultiBlo
         if (multiblock instanceof IParametrized parametrized) {
             parametrized.getParameters()
                 .forEach(parameter -> parameter.registerSyncValue(syncManager));
+        }
     }
 }
