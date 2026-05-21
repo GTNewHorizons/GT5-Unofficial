dcbcddfdfc7d5977d0dfbea9ddcfd90f22d25283
GDCloud <93287602+GDCloudstrike@users.noreply.github.com>
2026-05-21 04:03:30 +0200

    Apply material renderers to gtpp cells (#6745)
    Co-authored-by: Auynonymous <auynaaubin@gmail.com>
    Co-authored-by: Ruling-0 <47913168+Ruling-0@users.noreply.github.com>

--- src/main/java/gtPlusPlus/core/item/base/cell/BaseItemCell.java
+++ src/main/java/gtPlusPlus/core/item/base/cell/BaseItemCell.java
@@ -1,5 +1,7 @@
 package gtPlusPlus.core.item.base.cell;
 
+import java.awt.Color;
+
 import net.minecraft.item.ItemStack;
 
 import gtPlusPlus.core.item.base.BaseItemComponent;
@@ -16,6 +18,33 @@ public class BaseItemCell extends BaseItemComponent {
     public int getColorFromItemStack(final ItemStack stack, final int renderPass) {
         if (renderPass == 1) {
             return Utils.rgbtoHexValue(255, 255, 255);
+        }
+        if (this.componentMaterial == null) {
+            if (extraData != null) {
+                return Utils.rgbtoHexValue(extraData[0], extraData[1], extraData[2]);
+            }
+            return this.componentColour;
+        }
+
+        if (this.componentMaterial.getRGBA()[3] <= 1) {
+            return this.componentColour;
+        } else {
+            // Mild Glow Effect
+            if (this.componentMaterial.getRGBA()[3] == 2) {
+                // 4 sec cycle, 200 control point. 20ms interval.
+                int currentFrame = (int) ((System.nanoTime() % 4_000_000_000L) / 20_000_000L);
+                int value = currentFrame < 50 ? currentFrame + 1
+                    : currentFrame < 100 ? 50 : currentFrame < 150 ? 149 - currentFrame : 0;
+                return Utils.rgbtoHexValue(
+                    Math.min(255, Math.max(componentMaterial.getRGBA()[0] + value, 0)),
+                    Math.min(255, Math.max(componentMaterial.getRGBA()[1] + value, 0)),
+                    Math.min(255, Math.max(componentMaterial.getRGBA()[2] + value, 0)));
+            }
+
+            // Rainbow Hue Cycle
+            else if (this.componentMaterial.getRGBA()[3] == 3) {
+                return Color.HSBtoRGB((float) (System.nanoTime() % 8_000_000_000L) / 8_000_000_000f, 1, 1);
+            }
         }
         return this.componentColour;
     }
