848ec7f049d51544828b26cec9765149e9b67a1b
PLASMAchicken <plasmachickenmc@gmail.com>
2026-05-22 19:01:15 +0200

    Fix casing info for Steel Frame Box in MTEDroneCentre

--- src/main/java/gregtech/common/tileentities/machines/multi/drone/MTEDroneCentre.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/drone/MTEDroneCentre.java
@@ -228,7 +228,7 @@ public class MTEDroneCentre extends MTEExtendedPowerMultiBlockBase<MTEDroneCentr
             .addController("Front bottom center")
             .addCasingInfoMin("Solid Steel Machine Casing", CASINGS_MIN, false)
             .addCasingInfoExactly("Iron Frame Box", 28, false)
-            .addCasingInfoExactly("Steel Frame Box", 47, false)
+            .addCasingInfoExactly("Steel Frame Box", 48, false)
             .addCasingInfoExactly("Steel Pipe Casing", 61, false)
             .addCasingInfoExactly("Hempcrete", 29, false)
             .addInputBus("Any Solid Steel Machine Casing", 1)
