4f8ac2087230034f6dcfea25b73b5649b0bf58d7
chrombread <119648279+chrombread@users.noreply.github.com>
2026-05-22 12:41:20 -0700

    [Structure Rework] MEBF (#6548)
    Co-authored-by: Auynonymous <auynaaubin@gmail.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: danyadev <danyadev@mail.ru>
    Co-authored-by: GDCloud <93287602+GDCloudstrike@users.noreply.github.com>
    Co-authored-by: boubou19 <miisterunknown@gmail.com>
    Co-authored-by: VortexSo4 <daun.ebuchiy228@gmail.com>
    Co-authored-by: VortexSo4 <kreker.maxim@mail.ru>
    Co-authored-by: UltraProdigy <187078471+UltraProdigy@users.noreply.github.com>
    Co-authored-by: jamiehu427 <jamie.hu427@gmail.com>
    Co-authored-by: Stars-27 <notrealguy27@gmail.com>
    Co-authored-by: ROBERT-PARKOURCIVILIZATIONGOD <pozasgmale@gmail.com>
    Co-authored-by: Martin Robertz <dream-master@gmx.net>
    Co-authored-by: HamCorp (Tooj) <hamknockers@gmail.com>
    Co-authored-by: Eldrinn-Elantey <46845681+Eldrinn-Elantey@users.noreply.github.com>
    Co-authored-by: tiffit <10173431+tiffit@users.noreply.github.com>
    Co-authored-by: evgengoldwar <69961523+evgengoldwar@users.noreply.github.com>
    Co-authored-by: xXjudeXx <104434396+jude123412@users.noreply.github.com>
    Co-authored-by: Yoshy <60155833+Yoshy2002@users.noreply.github.com>
    Co-authored-by: EnderProyects <45178400+EnderProyects@users.noreply.github.com>
    Co-authored-by: June <junespaldingg@gmail.com>
    Co-authored-by: Ranzu <66495944+Ranzuu@users.noreply.github.com>
    Co-authored-by: iouter <62897714+iouter@users.noreply.github.com>
    Co-authored-by: Luca <38906805+Luca-Guettinger@users.noreply.github.com>
    Co-authored-by: Pxx500 <81298696+Pxx500@users.noreply.github.com>
    Co-authored-by: 54M44R <26176694+54M44R@users.noreply.github.com>
    Co-authored-by: Ruling-0 <47913168+Ruling-0@users.noreply.github.com>
    Co-authored-by: TheYoingLad <105537918+TheYoingLad@users.noreply.github.com>
    Co-authored-by: 6C-C6 <113111472+6C-C6@users.noreply.github.com>
    Co-authored-by: loenaaaa <asusactiongamer@gmail.com>
    Co-authored-by: AnsonYeung <nanolify@gmail.com>
    Co-authored-by: GitHub GTNH Actions <201363922+loenaaaa@users.noreply.github.com>
    Co-authored-by: Alexdoru <57050655+Alexdoru@users.noreply.github.com>
    Co-authored-by: OTPANNIEXD <163451675+OTPANNIEXD@users.noreply.github.com>
    Co-authored-by: Nana-Sakura <60061683+Nana-Sakura@users.noreply.github.com>
    Co-authored-by: yoshy2002 <jascha.morzuch@gmail.com>

--- src/main/java/gregtech/loaders/preload/LoaderMetaTileEntities.java
+++ src/main/java/gregtech/loaders/preload/LoaderMetaTileEntities.java
@@ -153,6 +153,7 @@ import gregtech.common.tileentities.machines.multi.MTEDistillationTower;
 import gregtech.common.tileentities.machines.multi.MTEElectricBlastFurnace;
 import gregtech.common.tileentities.machines.multi.MTEElectricImplosionCompressor;
 import gregtech.common.tileentities.machines.multi.MTEEntropicProcessor;
+import gregtech.common.tileentities.machines.multi.MTEExothermicHearth;
 import gregtech.common.tileentities.machines.multi.MTEExtremeCombustionEngine;
 import gregtech.common.tileentities.machines.multi.MTEExtremeDieselEngineLegacy;
 import gregtech.common.tileentities.machines.multi.MTEFluidShaper;
@@ -1082,6 +1083,10 @@ public class LoaderMetaTileEntities implements Runnable { // TODO CHECK CIRCUIT 
                 IndustrialCokeOven.ID,
                 "industrialcokeoven.controller.tier.single",
                 "Industrial Coke Oven").getStackForm(1L));
+
+        ItemList.ExothermicHearth.set(
+            new MTEExothermicHearth(ExothermicHearth.ID, "multimachine.exothermichearth", "Exothermic Hearth")
+                .getStackForm(1));
     }
 
     private static void registerSteamMachines() {
