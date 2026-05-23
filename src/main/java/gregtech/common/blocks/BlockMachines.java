16e8455620328d454794c053d830ece8314edf6e
GitHub GTNH Actions <>
2026-05-23 07:52:42 +0000

    spotlessApply

--- src/main/java/gregtech/common/blocks/BlockMachines.java
+++ src/main/java/gregtech/common/blocks/BlockMachines.java
@@ -13,7 +13,6 @@ import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
