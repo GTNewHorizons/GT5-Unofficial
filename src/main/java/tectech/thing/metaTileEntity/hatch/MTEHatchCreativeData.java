ce07c2ea0092fb1f618f01850ee7f0ac2953cda8
UltraProdigy <187078471+UltraProdigy@users.noreply.github.com>
2026-05-29 00:26:09 -0400

    fix

--- src/main/java/tectech/thing/metaTileEntity/hatch/MTEHatchCreativeData.java
+++ src/main/java/tectech/thing/metaTileEntity/hatch/MTEHatchCreativeData.java
@@ -3,7 +3,6 @@ package tectech.thing.metaTileEntity.hatch;
 import static net.minecraft.util.StatCollector.translateToLocal;
 
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.common.util.ForgeDirection;
@@ -17,30 +16,20 @@ import com.gtnewhorizons.modularui.common.widget.textfield.NumericWidget;
 
 import gregtech.api.gui.modularui.GTUITextures;
 import gregtech.api.interfaces.ITexture;
-import gregtech.api.interfaces.metatileentity.IMetaTileEntity;
 import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
 import gregtech.api.metatileentity.MetaTileEntity;
 import tectech.mechanics.dataTransport.QuantumDataPacket;
-import tectech.mechanics.pipe.IConnectsToDataPipe;
-import tectech.thing.metaTileEntity.pipe.MTEPipeData;
 import tectech.util.CommonValues;
 
 /**
  * Created by danie_000 on 27.10.2016.
  */
-public class MTEHatchCreativeData extends MTEHatchDataConnector<QuantumDataPacket> {
+public class MTEHatchCreativeData extends MTEHatchDataInput {
 
     private long production = Long.MAX_VALUE;
 
     public MTEHatchCreativeData(int aID, String aName, String aNameRegional, int aTier) {
-        super(
-            aID,
-            aName,
-            aNameRegional,
-            aTier,
-            new String[] { CommonValues.TEC_MARK_EM, translateToLocal("gt.blockmachines.debug.tt.data.desc.0"),
-                translateToLocal("gt.blockmachines.debug.tt.data.desc.1"),
-                EnumChatFormatting.AQUA + translateToLocal("gt.blockmachines.debug.tt.data.desc.2") });
+        super(aID, aName, aNameRegional, aTier);
     }
 
     public MTEHatchCreativeData(String aName, int aTier, String[] aDescription, ITexture[][][] aTextures) {
@@ -49,7 +38,14 @@ public class MTEHatchCreativeData extends MTEHatchDataConnector<QuantumDataPacke
 
     @Override
     public MetaTileEntity newMetaEntity(IGregTechTileEntity aTileEntity) {
-        return new MTEHatchCreativeData(mName, mTier, mDescriptionArray, mTextures);
+        return new MTEHatchCreativeData(mName, mTier, getDescription(), mTextures);
+    }
+
+    @Override
+    public String[] getDescription() {
+        return new String[] { CommonValues.TEC_MARK_EM, translateToLocal("gt.blockmachines.debug.tt.data.desc.0"),
+            translateToLocal("gt.blockmachines.debug.tt.data.desc.1"),
+            EnumChatFormatting.AQUA + translateToLocal("gt.blockmachines.debug.tt.data.desc.2") };
     }
 
     @Override
@@ -61,90 +57,41 @@ public class MTEHatchCreativeData extends MTEHatchDataConnector<QuantumDataPacke
     @Override
     public void loadNBTData(NBTTagCompound aNBT) {
         super.loadNBTData(aNBT);
-        production = aNBT.getLong("computation");
-    }
-
-    @Override
-    public boolean isOutputFacing(ForgeDirection side) {
-        return side == getBaseMetaTileEntity().getFrontFacing();
+        if (aNBT.hasKey("computation")) {
+            production = Math.max(1, aNBT.getLong("computation"));
+        }
     }
 
     @Override
     public boolean isDataInputFacing(ForgeDirection side) {
-        return isInputFacing(side);
-    }
-
-    @Override
-    protected QuantumDataPacket loadPacketFromNBT(NBTTagCompound nbt) {
-        return new QuantumDataPacket(nbt);
+        return false;
     }
 
     @Override
     public boolean canConnectData(ForgeDirection side) {
-        return isOutputFacing(side);
+        return false;
     }
 
     @Override
-    public void moveAround(IGregTechTileEntity aBaseMetaTileEntity) {
-        IConnectsToDataPipe current = this, source = this, next;
-        int range = 0;
-        while ((next = current.getNext(source)) != null && range++ < 1000) {
-            if (next instanceof MTEHatchDataInput) {
-                ((MTEHatchDataInput) next).setContents(q);
-                break;
-            }
-            source = current;
-            current = next;
-        }
-        q = null;
-    }
-
-    @Override
-    public IConnectsToDataPipe getNext(IConnectsToDataPipe source /* ==this */) {
-        IGregTechTileEntity base = getBaseMetaTileEntity();
-        byte color = base.getColorization();
-        if (color < 0) {
-            return null;
-        }
-        IGregTechTileEntity next = base.getIGregTechTileEntityAtSide(base.getFrontFacing());
-        if (next == null) {
-            return null;
-        }
-        IMetaTileEntity meta = next.getMetaTileEntity();
-        if (meta instanceof MTEPipeData) {
-            ((MTEPipeData) meta).markUsed();
-            return (IConnectsToDataPipe) meta;
-        } else if (meta instanceof MTEHatchDataInput && ((MTEHatchDataInput) meta).getColorization() == color
-            && ((MTEHatchDataInput) meta).canConnectData(
-                base.getFrontFacing()
-                    .getOpposite())) {
-                        return (IConnectsToDataPipe) meta;
-                    }
-        return null;
-    }
-
-    @Override
-    public void onPostTick(IGregTechTileEntity aBaseMetaTileEntity, long aTick) {
+    public void onPreTick(IGregTechTileEntity aBaseMetaTileEntity, long aTick) {
+        super.onPreTick(aBaseMetaTileEntity, aTick);
         if (aBaseMetaTileEntity.isServerSide()) {
-            if (CommonValues.MOVE_AT == aTick % 20) {
-                if (aBaseMetaTileEntity.isAllowedToWork()) {
-                    getBaseMetaTileEntity().setActive(true);
-                    if (q == null) q = new QuantumDataPacket(production);
-                    moveAround(aBaseMetaTileEntity);
-                } else {
-                    q = null;
-                    getBaseMetaTileEntity().setActive(false);
-                }
+            if (aBaseMetaTileEntity.isAllowedToWork()) {
+                setContents(new QuantumDataPacket(production));
+            } else {
+                setContents(null);
+                updateComputationHistory(0);
+                aBaseMetaTileEntity.setActive(false);
             }
         }
     }
 
     @Override
-    public void onScrewdriverRightClick(ForgeDirection side, EntityPlayer aPlayer, float aX, float aY, float aZ,
-        ItemStack aTool) {
-
-        openGui(aPlayer);
-        super.onScrewdriverRightClick(side, aPlayer, aX, aY, aZ, aTool);
+    public boolean onRightclick(IGregTechTileEntity aBaseMetaTileEntity, EntityPlayer aPlayer) {
+        if (!aPlayer.isUsingItem()) {
+            openGui(aPlayer);
+        }
+        return super.onRightclick(aBaseMetaTileEntity, aPlayer);
     }
 
     @Override
