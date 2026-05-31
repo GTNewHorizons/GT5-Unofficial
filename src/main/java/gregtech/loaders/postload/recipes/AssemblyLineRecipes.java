9a9c53fe1ed2b0a190467ccbed2df3248498a42f
Auynonymous <auynaaubin@gmail.com>
2026-05-30 22:11:16 -0500

    change recipe

--- src/main/java/gregtech/loaders/postload/recipes/AssemblyLineRecipes.java
+++ src/main/java/gregtech/loaders/postload/recipes/AssemblyLineRecipes.java
@@ -976,5 +976,26 @@ public class AssemblyLineRecipes implements Runnable {
             .eut(TierEU.RECIPE_ZPM / 2)
             .duration(1 * MINUTES)
             .addTo(AssemblyLine);
+
+        GTValues.RA.stdBuilder()
+            .metadata(RESEARCH_ITEM, ItemList.Machine_Multi_LargeChemicalReactor.get(1))
+            .metadata(SCANNING, new Scanning(1 * MINUTES, TierEU.RECIPE_LuV))
+            .itemInputs(
+                ItemList.Machine_Multi_LargeChemicalReactor.get(64),
+                GregtechItemList.ChemicalPlant_Controller.get(4),
+                new Object[] { OrePrefixes.circuit.get(Materials.LuV), 4 },
+                ItemList.Casing_Chemically_Inert.get(8),
+                ItemList.Electric_Pump_LuV.get(4),
+                ItemList.FluidRegulator_LuV.get(4),
+                ItemList.Machine_IV_ChemicalReactor.get(2),
+                GTOreDictUnificator.get(OrePrefixes.wireGt02, Materials.SuperconductorLuV, 16))
+            .fluidInputs(
+                Materials.Lubricant.getFluid(16_000),
+                MaterialsAlloy.INDALLOY_140.getFluidStack(10 * INGOTS),
+                Materials.Naquadah.getMolten(4 * INGOTS))
+            .itemOutputs(ItemList.MegaChemicalReactor.get(1))
+            .eut(TierEU.RECIPE_LuV / 2)
+            .duration(1 * MINUTES)
+            .addTo(AssemblyLine);
     }
 }
