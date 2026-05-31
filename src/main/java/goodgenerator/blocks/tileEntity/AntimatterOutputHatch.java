897f8005dffdc9c0c955777b4d258671a830a8e0
TheYoingLad <golyhagergo@gmail.com>
2026-05-31 17:28:52 +0200

    removed unused MUI code

--- src/main/java/goodgenerator/blocks/tileEntity/AntimatterOutputHatch.java
+++ src/main/java/goodgenerator/blocks/tileEntity/AntimatterOutputHatch.java
@@ -6,8 +6,6 @@ import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidStack;
 
-import com.gtnewhorizons.modularui.common.widget.FluidSlotWidget;
-
 import gregtech.api.enums.Materials;
 import gregtech.api.interfaces.ITexture;
 import gregtech.api.interfaces.metatileentity.IMetaTileEntity;
@@ -86,10 +84,5 @@ public class AntimatterOutputHatch extends MTEHatchOutput {
     @Override
     public String[] getDescription() {
         return GTSplit.splitLocalized("gt.blockmachines.output_hatch_antimatter.desc");
-    }
-
-    @Override
-    protected FluidSlotWidget createFluidSlot() {
-        return super.createFluidSlot().setFilter(f -> f == Materials.Antimatter.mFluid);
     }
 }
