0941480e947b4b5e46ae6cd7b6668a506a2969fd
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2026-06-02 13:57:58 -0400

    fix tooltips and tier pattern provider

--- src/main/java/gregtech/loaders/preload/LoaderMetaTileEntities.java
+++ src/main/java/gregtech/loaders/preload/LoaderMetaTileEntities.java
@@ -8554,11 +8554,36 @@ public class LoaderMetaTileEntities implements Runnable { // TODO CHECK CIRCUIT 
                 CRAFTING_INPUT_SLAVE.ID,
                 "hatch.crafting_input.proxy",
                 "Crafting Input Proxy").getStackForm(1L));
-        ItemList.Hatch_PatternProvider_Crafting.set(
+        ItemList.Hatch_PatternProvider_Crafting_IV.set(
             new MTEHatchPatternProvider(
-                INPUT_BUS_PATTERN_PROVIDER.ID,
-                "hatch.pattern_provider.crafting",
-                "Crafting Pattern Provider").getStackForm(1L));
+                INPUT_BUS_PATTERN_PROVIDER_IV.ID,
+                "hatch.pattern_provider.crafting.tier.05",
+                "Crafting Pattern Provider (IV)",
+                5).getStackForm(1L));
+        ItemList.Hatch_PatternProvider_Crafting_LuV.set(
+            new MTEHatchPatternProvider(
+                INPUT_BUS_PATTERN_PROVIDER_LuV.ID,
+                "hatch.pattern_provider.crafting.tier.06",
+                "Crafting Pattern Provider (LuV)",
+                6).getStackForm(1L));
+        ItemList.Hatch_PatternProvider_Crafting_ZPM.set(
+            new MTEHatchPatternProvider(
+                INPUT_BUS_PATTERN_PROVIDER_ZPM.ID,
+                "hatch.pattern_provider.crafting.tier.07",
+                "Crafting Pattern Provider (ZPM)",
+                7).getStackForm(1L));
+        ItemList.Hatch_PatternProvider_Crafting_UV.set(
+            new MTEHatchPatternProvider(
+                INPUT_BUS_PATTERN_PROVIDER_UV.ID,
+                "hatch.pattern_provider.crafting.tier.08",
+                "Crafting Pattern Provider (UV)",
+                8).getStackForm(1L));
+        ItemList.Hatch_PatternProvider_Crafting_UHV.set(
+            new MTEHatchPatternProvider(
+                INPUT_BUS_PATTERN_PROVIDER_UHV.ID,
+                "hatch.pattern_provider.crafting.tier.09",
+                "Crafting Pattern Provider (UHV)",
+                9).getStackForm(1L));
     }
 
     private static void registerMagHatch() {
