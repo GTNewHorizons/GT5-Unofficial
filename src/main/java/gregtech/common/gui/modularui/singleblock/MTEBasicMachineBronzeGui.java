7e109da07ff65a57dbce9494d4cdffb5c3abe3f6
TheYoingLad <golyhagergo@gmail.com>
2026-06-05 00:59:16 +0200

    readded extra storage slot

--- src/main/java/gregtech/common/gui/modularui/singleblock/MTEBasicMachineBronzeGui.java
+++ src/main/java/gregtech/common/gui/modularui/singleblock/MTEBasicMachineBronzeGui.java
@@ -1,13 +1,19 @@
 package gregtech.common.gui.modularui.singleblock;
 
+import static gregtech.api.metatileentity.BaseTileEntity.TOOLTIP_DELAY;
+
+import com.cleanroommc.modularui.api.drawable.IDrawable;
 import com.cleanroommc.modularui.factory.PosGuiData;
 import com.cleanroommc.modularui.screen.ModularPanel;
 import com.cleanroommc.modularui.screen.UISettings;
 import com.cleanroommc.modularui.value.sync.LongSyncValue;
 import com.cleanroommc.modularui.value.sync.PanelSyncManager;
+import com.cleanroommc.modularui.widgets.slot.ItemSlot;
+import com.cleanroommc.modularui.widgets.slot.ModularSlot;
 
 import gregtech.api.metatileentity.implementations.MTEBasicMachineBronze;
 import gregtech.api.recipe.BasicUIProperties;
+import gregtech.api.util.GTUtility;
 import gregtech.common.gui.modularui.singleblock.base.MTEBasicMachineBaseGui;
 import gregtech.common.gui.modularui.widget.SteamGaugeWidget;
 
@@ -19,8 +25,26 @@ public class MTEBasicMachineBronzeGui extends MTEBasicMachineBaseGui<MTEBasicMac
     }
 
     @Override
-    protected boolean supportsChargerSlot() {
-        return false;
+    protected ItemSlot createChargerSlot() {
+        String[] tooltipKeys = new String[2];
+        if (properties.useSpecialSlot) {
+            tooltipKeys[0] = "GT5U.machines.special_slot.tooltip";
+            tooltipKeys[1] = "GT5U.machines.special_slot.tooltip.1";
+        } else {
+            tooltipKeys[0] = "GT5U.machines.unused_slot.tooltip";
+            tooltipKeys[1] = "GT5U.machines.unused_slot.tooltip.1";
+        }
+        return new ItemSlot().marginRight(9)
+            .slot(
+                new ModularSlot(machine.inventoryHandler, machine.rechargerSlotStartIndex()).changeListener(
+                    (_, _, client, init) -> { if (!client && !init) baseMetaTileEntity.markInventoryBeenModified(); })
+                    .singletonSlotGroup(1000))
+            .backgroundOverlay(
+                properties.useSpecialSlot ? slotOverlayFunction.apply(0, false, false, true) : IDrawable.NONE)
+            .tooltip(
+                t -> t.addLine(GTUtility.translate(tooltipKeys[0]))
+                    .addLine(GTUtility.translate(tooltipKeys[1])))
+            .tooltipShowUpTimer(TOOLTIP_DELAY);
     }
 
     @Override
