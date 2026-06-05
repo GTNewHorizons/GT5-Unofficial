4c5accd35a39c048d1ea6e78ce160b9aa45a55a8
TheYoingLad <golyhagergo@gmail.com>
2026-06-05 11:32:25 +0200

    small MTEHatchChiselBus gui overhaul

--- src/main/java/gregtech/common/gui/modularui/hatch/MTEHatchChiselBusGui.java
+++ src/main/java/gregtech/common/gui/modularui/hatch/MTEHatchChiselBusGui.java
@@ -1,18 +1,13 @@
 package gregtech.common.gui.modularui.hatch;
 
-import java.util.Arrays;
-
 import com.cleanroommc.modularui.screen.ModularPanel;
 import com.cleanroommc.modularui.value.sync.PanelSyncManager;
 import com.cleanroommc.modularui.widget.ParentWidget;
-import com.cleanroommc.modularui.widgets.SlotGroupWidget;
 import com.cleanroommc.modularui.widgets.layout.Flow;
-import com.cleanroommc.modularui.widgets.slot.ItemSlot;
-import com.cleanroommc.modularui.widgets.slot.ModularSlot;
 import com.cleanroommc.modularui.widgets.slot.PhantomItemSlot;
 
-import gregtech.api.util.StringUtils;
 import gregtech.common.gui.modularui.hatch.base.MTEHatchBaseGui;
+import gregtech.common.modularui2.widget.builder.ItemSlotGridBuilder;
 import gtPlusPlus.xmod.gregtech.api.metatileentity.implementations.MTEHatchChiselBus;
 
 public class MTEHatchChiselBusGui extends MTEHatchBaseGui<MTEHatchChiselBus> {
@@ -27,15 +22,9 @@ public class MTEHatchChiselBusGui extends MTEHatchBaseGui<MTEHatchChiselBus> {
         return MTEHatchChiselBus.getSlots(machine.mTier) / INPUT_COLS;
     }
 
-    private int getGhostCols() {
+    private int getGhostGridSize() {
         int ghostCount = MTEHatchChiselBus.getGhostTargetCount(machine.mTier);
         return (int) Math.ceil(Math.sqrt(ghostCount));
-    }
-
-    private int getGhostRows() {
-        int ghostCount = MTEHatchChiselBus.getGhostTargetCount(machine.mTier);
-        int cols = getGhostCols();
-        return (int) Math.ceil((double) ghostCount / cols);
     }
 
     @Override
@@ -46,53 +35,28 @@ public class MTEHatchChiselBusGui extends MTEHatchBaseGui<MTEHatchChiselBus> {
 
     @Override
     protected int getBasePanelWidth() {
-        int ghostCols = getGhostCols();
-        int totalCols = ghostCols + INPUT_COLS;
-        return super.getBasePanelWidth() + Math.max(0, SLOT_SIZE * (totalCols - 9) + 3);
+        int totalCols = getGhostGridSize() + INPUT_COLS;
+        return super.getBasePanelWidth() + Math.max(0, SLOT_SIZE * (totalCols - 9) + SLOT_SIZE);
     }
 
     @Override
     protected ParentWidget<?> createContentSection(ModularPanel panel, PanelSyncManager syncManager) {
-        int inputRows = getInputRows();
-        int ghostCols = getGhostCols();
-        int ghostRows = getGhostRows();
+        Flow mainRow = Flow.row()
+            .coverChildren()
+            .childPadding(SLOT_SIZE)
+            .horizontalCenter();
 
-        syncManager.registerSlotGroup("inputs", INPUT_COLS);
+        // target phantom slots
+        mainRow.child(
+            new ItemSlotGridBuilder(machine.ghostTargets, syncManager).size(getGhostGridSize())
+                .itemSlotSupplier(PhantomItemSlot::new)
+                .build());
 
-        // Ghost target phantom slots
-        String[] ghostMatrix = new String[ghostRows];
-        Arrays.fill(ghostMatrix, StringUtils.getRepetitionOf('t', ghostCols));
+        // input slots
+        mainRow.child(
+            new ItemSlotGridBuilder(machine.inventoryHandler, syncManager).size(INPUT_COLS, getInputRows())
+                .build());
 
-        var ghostWidget = SlotGroupWidget.builder()
-            .matrix(ghostMatrix)
-            .key(
-                't',
-                index -> new PhantomItemSlot()
-                    .slot(new ModularSlot(machine.ghostTargets, index).accessibility(true, false)))
-            .build();
-
-        // Input slots grid
-        String[] inputMatrix = new String[inputRows];
-        Arrays.fill(inputMatrix, StringUtils.getRepetitionOf('x', INPUT_COLS));
-
-        var inputWidget = SlotGroupWidget.builder()
-            .matrix(inputMatrix)
-            .key(
-                'x',
-                index -> new ItemSlot().slot(new ModularSlot(machine.inventoryHandler, index).slotGroup("inputs")))
-            .build();
-
-        return super.createContentSection(panel, syncManager).child(
-            Flow.row()
-                .coverChildrenHeight()
-                .topRel(0)
-                .horizontalCenter()
-                .child(ghostWidget)
-                .child(inputWidget.marginLeft(3)));
-    }
-
-    @Override
-    protected boolean supportsBottomRowOverlap() {
-        return true;
+        return super.createContentSection(panel, syncManager).child(mainRow);
     }
 }
