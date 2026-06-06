a89b56739ce45ec1e8cfd4a31562f8dfc8e30540
TheYoingLad <golyhagergo@gmail.com>
2026-06-06 15:38:17 +0200

    added subpanel for template slots

--- src/main/java/gregtech/common/gui/modularui/hatch/MTEHatchChiselBusGui.java
+++ src/main/java/gregtech/common/gui/modularui/hatch/MTEHatchChiselBusGui.java
@@ -1,11 +1,17 @@
 package gregtech.common.gui.modularui.hatch;
 
+import com.cleanroommc.modularui.api.IPanelHandler;
 import com.cleanroommc.modularui.screen.ModularPanel;
 import com.cleanroommc.modularui.value.sync.PanelSyncManager;
 import com.cleanroommc.modularui.widget.ParentWidget;
+import com.cleanroommc.modularui.widgets.ButtonWidget;
 import com.cleanroommc.modularui.widgets.layout.Flow;
+import com.cleanroommc.modularui.widgets.slot.ModularSlot;
 import com.cleanroommc.modularui.widgets.slot.PhantomItemSlot;
 
+import gregtech.api.modularui2.GTGuiTextures;
+import gregtech.api.modularui2.GTGuis;
+import gregtech.api.util.GTUtility;
 import gregtech.common.gui.modularui.hatch.base.MTEHatchBaseGui;
 import gregtech.common.modularui2.widget.builder.ItemSlotGridBuilder;
 import gtPlusPlus.xmod.gregtech.api.metatileentity.implementations.MTEHatchChiselBus;
@@ -34,23 +40,10 @@ public class MTEHatchChiselBusGui extends MTEHatchBaseGui<MTEHatchChiselBus> {
     }
 
     @Override
-    protected int getBasePanelWidth() {
-        int totalCols = getGhostGridSize() + INPUT_COLS;
-        return super.getBasePanelWidth() + Math.max(0, SLOT_SIZE * (totalCols - 9) + SLOT_SIZE);
-    }
-
-    @Override
     protected ParentWidget<?> createContentSection(ModularPanel panel, PanelSyncManager syncManager) {
         Flow mainRow = Flow.row()
             .coverChildren()
-            .childPadding(SLOT_SIZE)
             .horizontalCenter();
-
-        // target phantom slots
-        mainRow.child(
-            new ItemSlotGridBuilder(machine.ghostTargets, syncManager).size(getGhostGridSize())
-                .itemSlotSupplier(PhantomItemSlot::new)
-                .build());
 
         // input slots
         mainRow.child(
@@ -58,5 +51,47 @@ public class MTEHatchChiselBusGui extends MTEHatchBaseGui<MTEHatchChiselBus> {
                 .build());
 
         return super.createContentSection(panel, syncManager).child(mainRow);
+    }
+
+    @Override
+    protected Flow createBottomLeftCornerFlow(ModularPanel panel, PanelSyncManager syncManager) {
+        IPanelHandler templatePanel = syncManager
+            .syncedPanel("template_panel", true, (_, _) -> createTemplatePanel(panel, syncManager));
+
+        int gridSize = getGhostGridSize();
+        return super.createBottomLeftCornerFlow(panel, syncManager).collapseDisabledChild()
+            .childIf(
+                gridSize == 1,
+                () -> new PhantomItemSlot().slot(new ModularSlot(machine.ghostTargets, 0))
+                    .addTooltipLine(GTUtility.translate("GT5U.hatch.chisel.configure.singleton")))
+            .childIf(gridSize > 1, () -> createTemplatePanelButton(templatePanel));
+    }
+
+    private ButtonWidget<?> createTemplatePanelButton(IPanelHandler templatePanel) {
+        return new ButtonWidget<>().onMousePressed(_ -> {
+            if (!templatePanel.isPanelOpen()) templatePanel.openPanel();
+            else templatePanel.closePanel();
+            return true;
+        })
+            .overlay(GTGuiTextures.OVERLAY_BUTTON_SCREWDRIVER)
+            .addTooltipLine(GTUtility.translate("GT5U.hatch.chisel.configure.grid"));
+    }
+
+    private ModularPanel createTemplatePanel(ModularPanel panel, PanelSyncManager syncManager) {
+        ModularPanel templatePanel = GTGuis.createPopUpPanel("template_panel")
+            .coverChildren()
+            .rightRel(1)
+            .padding(4)
+            .relative(panel);
+
+        // target phantom slots
+        templatePanel.child(
+            new ItemSlotGridBuilder(machine.ghostTargets, syncManager).size(getGhostGridSize())
+                .hasSlotGroup(false)
+                .itemSlotSupplier(PhantomItemSlot::new)
+                .build()
+                .marginTop(12));
+
+        return templatePanel;
     }
 }
