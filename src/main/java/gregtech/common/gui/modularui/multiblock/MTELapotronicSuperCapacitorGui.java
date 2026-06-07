316a34dbf53b4670975c303af8a90b25788ebcff
Luca <38906805+Luca-Guettinger@users.noreply.github.com>
2026-06-07 17:41:28 +0200

    Fix wireless rebalance being usable without UHV+ capacitors in LSC (#6870)
    Co-authored-by: Martin Robertz <dream-master@gmx.net>
    Co-authored-by: Yoshy <60155833+Yoshy2002@users.noreply.github.com>

--- src/main/java/gregtech/common/gui/modularui/multiblock/MTELapotronicSuperCapacitorGui.java
+++ src/main/java/gregtech/common/gui/modularui/multiblock/MTELapotronicSuperCapacitorGui.java
@@ -115,6 +115,7 @@ public class MTELapotronicSuperCapacitorGui extends MTEMultiBlockBaseGui<MTELapo
         BooleanSyncValue canRebalance = syncManager.findSyncHandler("canRebalance", BooleanSyncValue.class);
         BooleanSyncValue rebalanced = syncManager.findSyncHandler("rebalanced", BooleanSyncValue.class);
         BooleanSyncValue wireless = syncManager.findSyncHandler("wirelessMode", BooleanSyncValue.class);
+        BooleanSyncValue wirelessCapable = syncManager.findSyncHandler("wirelessCapable", BooleanSyncValue.class);
         IPanelHandler warningPanel = syncManager.syncedPanel("warning panel", true, ((a, b) -> warningPanel()));
 
         return super.createLeftPanelGapRow(parent, syncManager)
@@ -152,6 +153,7 @@ public class MTELapotronicSuperCapacitorGui extends MTEMultiBlockBaseGui<MTELapo
 
                     return false;
                 })
+                .setEnabledIf((w) -> wirelessCapable.getBoolValue())
 
             )
             .child(
@@ -166,7 +168,7 @@ public class MTELapotronicSuperCapacitorGui extends MTEMultiBlockBaseGui<MTELapo
                             rebalanced.setBoolValue(true);
                         }
                     }))
-                    .setEnabledIf((w) -> canRebalance.getBoolValue()));
+                    .setEnabledIf((w) -> canRebalance.getBoolValue() && wirelessCapable.getBoolValue()));
     }
 
     private ModularPanel warningPanel() {
@@ -293,6 +295,8 @@ public class MTELapotronicSuperCapacitorGui extends MTEMultiBlockBaseGui<MTELapo
         syncManager.syncValue("warning", warningSync);
         syncManager.syncValue("canRebalance", canRebalance);
         syncManager.syncValue("wireless value", wirelessValueSync);
+        BooleanSyncValue wirelessCapable = new BooleanSyncValue(multiblock::canUseWireless);
+        syncManager.syncValue("wirelessCapable", wirelessCapable);
 
     }
 
