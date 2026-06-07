c4e804a56d94ea61944cd6122da107ce6b1c90b3
Mary Hopson <doyoumined@gmail.com>
2026-06-07 10:23:46 -0400

    should be core 2

--- src/main/java/gregtech/api/items/armor/MechArmorAugmentRegistries.java
+++ src/main/java/gregtech/api/items/armor/MechArmorAugmentRegistries.java
@@ -524,7 +524,7 @@ public class MechArmorAugmentRegistries {
             .setItemId("augmentforcefield")
             .fitsInto(ArmorType.Chestplate)
             .providesBehaviors(ForceFieldBehavior.INSTANCE)
-            .setMinimumCore(3)
+            .setMinimumCore(2)
             .setCategory(AugmentCategory.Protection)
             .setRarity(EnumRarity.epic)
         ),
