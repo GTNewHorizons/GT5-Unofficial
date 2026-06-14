0163d71f2a776e0851474aaa5bbf00f9b603fb09
Ranzu <66495944+Ranzuu@users.noreply.github.com>
2026-06-14 13:23:25 +0200

    Convert Material to use ColorUtils class

--- src/main/java/gregtech/common/items/DropType.java
+++ src/main/java/gregtech/common/items/DropType.java
@@ -5,6 +5,7 @@ import net.minecraft.util.StatCollector;
 import org.apache.commons.lang3.text.WordUtils;
 
 import gregtech.api.enums.Materials;
+import gregtech.api.util.ColorUtils;
 import gregtech.api.util.GTLanguageManager;
 
 public enum DropType {
@@ -19,9 +20,16 @@ public enum DropType {
     LAPIS("lapis coolant", true),
     ENDERGOO("ender goo", true);
 
-    private static final int[][] colours = new int[][] { { 0x19191B, 0x303032 }, { 0xffc100, 0x00ff11 },
-        { 0x144F5A, 0x2494A2 }, { 0xC11F1F, 0xEBB9B9 }, { 0x872836, 0xB8132C }, { 0xD02001, 0x9C0018 },
-        { 0x003366, 0x0066BB }, { 0x1727b1, 0x008ce3 }, { 0xA005E7, 0x161616 }, };
+    private static final int[][] colours = new int[][] {
+        { ColorUtils.drop1Oil.getColor(), ColorUtils.drop2Oil.getColor() },
+        { ColorUtils.drop1Mutagen.getColor(), ColorUtils.drop2Mutagen.getColor() },
+        { ColorUtils.drop1Coolant.getColor(), ColorUtils.drop2Coolant.getColor() },
+        { ColorUtils.drop1HotCoolant.getColor(), ColorUtils.drop2HotCoolant.getColor() },
+        { ColorUtils.drop1Hydra.getColor(), ColorUtils.drop2Hydra.getColor() },
+        { ColorUtils.drop1SnowQueen.getColor(), ColorUtils.drop2SnowQueen.getColor() },
+        { ColorUtils.drop1Oxygen.getColor(), ColorUtils.drop2Oxygen.getColor() },
+        { ColorUtils.drop1Lapis.getColor(), ColorUtils.drop2Lapis.getColor() },
+        { ColorUtils.drop1Endergoo.getColor(), ColorUtils.drop2Endergoo.getColor() }, };
     public boolean showInList;
     public Materials material;
     public int chance;
