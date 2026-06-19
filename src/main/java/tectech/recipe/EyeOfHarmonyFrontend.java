f70cf13b5208e8197e224dfe39e843e6cb8ea903
DylanTaylor1 <taylor.dylan173@gmail.com>
2026-06-19 13:50:43 +0200

    Spotless

--- src/main/java/tectech/recipe/EyeOfHarmonyFrontend.java
+++ src/main/java/tectech/recipe/EyeOfHarmonyFrontend.java
@@ -159,8 +159,8 @@ public class EyeOfHarmonyFrontend extends RecipeMapFrontend {
 
             // Energy Output
             switch (ConfigHandler.visual.EOH_NOTATION) {
-                case Numerical -> result.add(
-                    StatCollector.translateToLocalFormatted("EOH.Recipe.EU.Out", recipe.getEUOutput()));
+                case Numerical -> result
+                    .add(StatCollector.translateToLocalFormatted("EOH.Recipe.EU.Out", recipe.getEUOutput()));
                 case Scientific -> result.add(
                     StatCollector.translateToLocalFormatted("EOH.Recipe.EU.Out", toExponentForm(recipe.getEUOutput())));
                 case SI -> result.add(
@@ -171,8 +171,8 @@ public class EyeOfHarmonyFrontend extends RecipeMapFrontend {
 
             // Energy Input
             switch (ConfigHandler.visual.EOH_NOTATION) {
-                case Numerical -> result.add(
-                    StatCollector.translateToLocalFormatted("EOH.Recipe.EU.In", recipe.getEUStartCost()));
+                case Numerical -> result
+                    .add(StatCollector.translateToLocalFormatted("EOH.Recipe.EU.In", recipe.getEUStartCost()));
                 case Scientific -> result.add(
                     StatCollector
                         .translateToLocalFormatted("EOH.Recipe.EU.In", toExponentForm(recipe.getEUStartCost())));
