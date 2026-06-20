3ef3ecbc7735884c51e75e81c7480a342e2108fb
Pxx500 <pbartulik@gmail.com>
2026-06-21 01:59:00 +0200

    fix arc furnace

--- src/main/java/gregtech/api/recipe/check/ResultInsufficientStartupPower.java
+++ src/main/java/gregtech/api/recipe/check/ResultInsufficientStartupPower.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 
 import javax.annotation.Nonnull;
 
+import gregtech.api.enums.GTValues;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.StatCollector;
@@ -15,9 +16,29 @@ import org.jetbrains.annotations.NotNull;
 public class ResultInsufficientStartupPower implements CheckRecipeResult {
 
     private int required;
+    private int machineTier;
 
     ResultInsufficientStartupPower(int required) {
+        this(required, -1);
+    }
+
+    ResultInsufficientStartupPower(int required, int machineTier) {
         this.required = required;
+        this.machineTier = machineTier;
+    }
+
+    private long getRequiredAmperage() {
+        long voltage = GTValues.V[machineTier];
+        return Math.max(1L, (required + voltage - 1L) / voltage);
+    }
+
+    private long getRequiredSixtyFourAmpHatches() {
+        long requiredAmperage = getRequiredAmperage();
+        return Math.max(1L, (requiredAmperage + 63L) / 64L);
+    }
+
+    private boolean hasAmperageEstimate() {
+        return machineTier >= 0 && machineTier < GTValues.V.length;
     }
 
     @Override
@@ -32,8 +53,21 @@ public class ResultInsufficientStartupPower implements CheckRecipeResult {
     }
 
     @Override
+    public boolean persistsOnShutdown() {
+        return hasAmperageEstimate();
+    }
+
+    @Override
     @Nonnull
     public @NotNull String getDisplayString() {
+        if (hasAmperageEstimate()) {
+            return Objects.requireNonNull(
+                StatCollector.translateToLocalFormatted(
+                    "GT5U.gui.text.recipe_result.insufficient_startup_power_with_amperage",
+                    formatNumber(required),
+                    formatNumber(getRequiredAmperage()),
+                    formatNumber(getRequiredSixtyFourAmpHatches())));
+        }
         return Objects.requireNonNull(
             StatCollector.translateToLocalFormatted(
                 "GT5U.gui.text.recipe_result.insufficient_startup_power",
@@ -43,12 +77,14 @@ public class ResultInsufficientStartupPower implements CheckRecipeResult {
     @Override
     public @NotNull NBTTagCompound writeToNBT(@NotNull NBTTagCompound tag) {
         tag.setInteger("required", required);
+        tag.setInteger("machineTier", machineTier);
         return tag;
     }
 
     @Override
     public void readFromNBT(@NotNull NBTTagCompound tag) {
         required = tag.getInteger("required");
+        machineTier = tag.hasKey("machineTier") ? tag.getInteger("machineTier") : -1;
     }
 
     @Override
@@ -60,11 +96,13 @@ public class ResultInsufficientStartupPower implements CheckRecipeResult {
     @Override
     public void encode(@Nonnull PacketBuffer buffer) {
         buffer.writeVarIntToBuffer(required);
+        buffer.writeVarIntToBuffer(machineTier);
     }
 
     @Override
     public void decode(@Nonnull PacketBuffer buffer) {
         required = buffer.readVarIntFromBuffer();
+        machineTier = buffer.readVarIntFromBuffer();
     }
 
     @Override
@@ -72,6 +110,6 @@ public class ResultInsufficientStartupPower implements CheckRecipeResult {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ResultInsufficientStartupPower that = (ResultInsufficientStartupPower) o;
-        return required == that.required;
+        return required == that.required && machineTier == that.machineTier;
     }
 }
