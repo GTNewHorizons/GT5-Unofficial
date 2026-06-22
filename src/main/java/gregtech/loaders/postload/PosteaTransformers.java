0b353e9148964624488d91e34351e360c5c36996
LazyFleshWasTaken <40587985+LazyFlesh@users.noreply.github.com>
2026-06-22 12:13:56 -0700

    spotless

--- src/main/java/gregtech/loaders/postload/PosteaTransformers.java
+++ src/main/java/gregtech/loaders/postload/PosteaTransformers.java
@@ -4,7 +4,6 @@ import static gregtech.api.enums.OrePrefixes.___placeholder___;
 
 import java.util.Arrays;
 
-import gregtech.api.enums.ItemList;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.nbt.NBTTagCompound;
@@ -19,6 +18,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import gregtech.api.GregTechAPI;
 import gregtech.api.casing.Casings;
 import gregtech.api.enums.GTValues;
+import gregtech.api.enums.ItemList;
 import gregtech.api.enums.Materials;
 import gregtech.api.enums.OrePrefixes;
 import gregtech.common.blocks.BlockFrameBox;
