3c7ff70fb3c72d24c68df9282e097c0f73088cb9
TheYoingLad <105537918+TheYoingLad@users.noreply.github.com>
2026-06-26 07:01:22 +0200

    [MUI2] Ported MTEBasicMachineBronze (#6953)
    Co-authored-by: Martin Robertz <dream-master@gmx.net>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: UltraProdigy <187078471+UltraProdigy@users.noreply.github.com>

--- src/main/java/gregtech/api/enums/TieredVariant.java
+++ src/main/java/gregtech/api/enums/TieredVariant.java
@@ -2,15 +2,17 @@ package gregtech.api.enums;
 
 import java.util.Locale;
 
-public enum SteamVariant {
+public enum TieredVariant {
 
     BRONZE,
     STEEL,
     PRIMITIVE,
-    NONE;
+    STANDARD;
 
     @Override
     public String toString() {
         return super.toString().toLowerCase(Locale.ENGLISH);
     }
+
+    public static final TieredVariant[] special_variants = { BRONZE, STEEL, PRIMITIVE };
 }
