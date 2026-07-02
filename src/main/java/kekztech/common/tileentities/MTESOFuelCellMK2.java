e12704423d730bd5a251201a52658b2d9be51ed8
DylanTaylor1 <taylor.dylan173@gmail.com>
2026-07-02 21:54:36 +0200

    Spotless

--- src/main/java/kekztech/common/tileentities/MTESOFuelCellMK2.java
+++ src/main/java/kekztech/common/tileentities/MTESOFuelCellMK2.java
@@ -45,8 +45,6 @@ import gregtech.api.recipe.RecipeMaps;
 import gregtech.api.recipe.check.CheckRecipeResult;
 import gregtech.api.recipe.check.CheckRecipeResultRegistry;
 import gregtech.api.structure.error.StructureError;
-import gregtech.api.structure.error.StructureErrors;
-import gregtech.api.structure.error.TranslatableText;
 import gregtech.api.util.GTRecipe;
 import gregtech.api.util.GTUtility;
 import gregtech.api.util.MultiblockTooltipBuilder;
