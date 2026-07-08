4ad408177b892ecf54c0f2c2d1051cece3f0205e
RealSilverMoon <qazhhhh@outlook.com>
2026-07-08 15:46:53 +0800

    Fix connection sync

--- src/main/java/gregtech/common/tileentities/machines/multi/drone/DroneConnection.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/drone/DroneConnection.java
@@ -14,6 +14,8 @@ import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 import net.minecraftforge.common.DimensionManager;
 
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.relauncher.Side;
 import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
 import gregtech.api.metatileentity.implementations.MTEMultiBlockBase;
 import gregtech.api.util.GTUtil;
@@ -86,8 +88,14 @@ public class DroneConnection {
         this.shutdownReason = aNBT.getString("shutdownReason");
         this.isSelected = aNBT.getBoolean("isSelected");
         this.group = aNBT.getInteger("group");
-        this.cachedCentre = getLoadedGTBaseMachineAt(centreCoord, DimensionManager.getWorld(centreWorld), false);
-        this.cachedMachine = getLoadedGTBaseMachineAt(machineCoord, DimensionManager.getWorld(machineWorld), false);
+        if (FMLCommonHandler.instance()
+            .getEffectiveSide() == Side.CLIENT) {
+            this.cachedCentre = null;
+            this.cachedMachine = null;
+        } else {
+            this.cachedCentre = getLoadedGTBaseMachineAt(centreCoord, DimensionManager.getWorld(centreWorld), false);
+            this.cachedMachine = getLoadedGTBaseMachineAt(machineCoord, DimensionManager.getWorld(machineWorld), false);
+        }
     }
 
     public MTEMultiBlockBase getLinkedMachine() {
