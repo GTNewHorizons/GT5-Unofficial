b9a0ca7c840400870a8e707182e75b9875a155c5
San32 <25187502+Sanduhr32@users.noreply.github.com>
2026-07-09 02:41:30 +0200

    partial undo for cross mods

---
+++ src/main/java/gregtech/common/WirelessDataStore.java
@@ -0,0 +1,14 @@
+package gregtech.common;
+
+import java.util.UUID;
+
+public class WirelessDataStore {
+
+    /**
+     * @deprecated Use methods provided by {@link gregtech.common.misc.WirelessNetworkManager} instead.
+     */
+    @Deprecated
+    public static WirelessDataStore getWirelessDataSticks(UUID uuid) {
+        return null;
+    }
+}
--- src/main/java/gregtech/common/misc/GlobalVariableStorage.java
+++ src/main/java/gregtech/common/misc/GlobalVariableStorage.java
@@ -5,16 +5,25 @@ import java.util.HashMap;
 import java.util.UUID;
 
 import gregtech.common.WirelessComputationPacket;
+import gregtech.common.WirelessDataStore;
 
+/**
+ * @see WirelessTeamData
+ * @see WirelessNetworkManager
+ */
+@Deprecated
 public abstract class GlobalVariableStorage {
     // --------------------- NEVER access these maps! Use the methods provided! ---------------------
 
     // Global EU map.
-    @Deprecated
     public static HashMap<UUID, BigInteger> GlobalEnergy = new HashMap<>(100, 0.9f);
 
     // Global Wireless Data map
     public static HashMap<UUID, WirelessComputationPacket> GlobalWirelessComputation = new HashMap<>(100, 0.9f);
+
+    // Global Wireless Data Stick map
+
+    public static HashMap<UUID, WirelessDataStore> GlobalWirelessDataSticks = new HashMap<>(100, 0.9f);
 
     // ----------------------------------------------------------------------------------------------
 }
