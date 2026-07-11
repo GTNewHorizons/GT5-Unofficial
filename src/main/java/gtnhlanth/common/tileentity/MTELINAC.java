fc9ce68228b522374861d9df4a79d09e85f5837f
Ruling-0 <47913168+Ruling-0@users.noreply.github.com>
2026-07-11 15:23:53 -0500

    Revert "Stop MTELINAC construct() from spamming a chat message on every call"
    This reverts commit bf81054617403d24d9fe97e8f67b625360b285cc.

--- src/main/java/gtnhlanth/common/tileentity/MTELINAC.java
+++ src/main/java/gtnhlanth/common/tileentity/MTELINAC.java
@@ -32,6 +32,7 @@ import net.minecraftforge.fluids.FluidStack;
 
 import org.jetbrains.annotations.NotNull;
 
+import com.gtnewhorizon.structurelib.StructureLib;
 import com.gtnewhorizon.structurelib.alignment.constructable.ISurvivalConstructable;
 import com.gtnewhorizon.structurelib.structure.IStructureDefinition;
 import com.gtnewhorizon.structurelib.structure.ISurvivalBuildEnvironment;
@@ -365,6 +366,8 @@ public class MTELINAC extends MTEEnhancedMultiBlockBase<MTELINAC>
         }
 
         buildPiece(STRUCTURE_PIECE_END, stackSize, hintsOnly, 3, 6, -(lLength + 2));
+
+        StructureLib.addClientSideChatMessages("Length: " + (11 + lLength) + " blocks.");
     }
 
     @Override
