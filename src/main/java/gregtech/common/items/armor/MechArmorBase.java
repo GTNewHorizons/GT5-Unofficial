0778df767c032f71b7cd64c32b74f764163c2d0f
Spaghetti <74196682+Spaghetti-OberNub@users.noreply.github.com>
2026-07-15 07:15:48 +0200

    Mechanical Armor: New armor texture with a dynamically colored overlay  (#7412)

--- src/main/java/gregtech/common/items/armor/MechArmorBase.java
+++ src/main/java/gregtech/common/items/armor/MechArmorBase.java
@@ -1,7 +1,6 @@
 package gregtech.common.items.armor;
 
 import static gregtech.api.enums.Mods.GregTech;
-import static gregtech.api.items.armor.ArmorHelper.SLOT_LEGS;
 import static gregtech.api.util.GTUtility.getOrCreateNbtCompound;
 
 import java.util.List;
@@ -277,8 +276,9 @@ public class MechArmorBase extends ItemArmor implements IKeyPressedListener, ISp
             if (slot == 2) return GregTech.getResourcePath("textures/items/mech_armor/texture_layer_skeleton2.png");
             return GregTech.getResourcePath("textures/items/mech_armor/texture_layer_skeleton1.png");
         }
-        if (slot == 2) return GregTech.getResourcePath("textures/items/mech_armor/texture_layer2.png");
-        return GregTech.getResourcePath("textures/items/mech_armor/texture_layer1.png");
+
+        if (slot == 2) return GregTech.getResourcePath("textures/items/mech_armor/texture_layer_combined2.png");
+        return GregTech.getResourcePath("textures/items/mech_armor/texture_layer_combined1.png");
     }
 
     @Override
@@ -287,7 +287,13 @@ public class MechArmorBase extends ItemArmor implements IKeyPressedListener, ISp
         if (modelLegs == null) modelLegs = new ModelMechArmor(0.25F);
         if (modelOther == null) modelOther = new ModelMechArmor(0.5F);
 
-        ModelMechArmor model = (armorSlot == SLOT_LEGS) ? modelLegs : modelOther;
+        ModelMechArmor model = (armorSlot == 2) ? modelLegs : modelOther;
+
+        if (getFrame(itemStack) != null) {
+            model.setColor(getFrame(itemStack).getColor());
+        } else {
+            model.setColor(new short[] { -1 });
+        }
 
         model.bipedHead.showModel = (armorType == 0);
         model.bipedHeadwear.showModel = (armorType == 0);
@@ -311,6 +317,19 @@ public class MechArmorBase extends ItemArmor implements IKeyPressedListener, ISp
                 case 2 -> model.core2.showModel = true;
                 case 3 -> model.core3.showModel = true;
                 case 4 -> model.core4.showModel = true;
+            }
+        }
+
+        if (entityLiving != null) {
+            model.isSneak = entityLiving.isSneaking();
+
+            model.isRiding = entityLiving.isRiding();
+            model.isChild = entityLiving.isChild();
+
+            model.heldItemRight = entityLiving.getEquipmentInSlot(0) != null ? 1 : 0;
+
+            if (entityLiving instanceof EntityPlayer player) {
+                model.aimedBow = player.getItemInUseDuration() > 0;
             }
         }
 
