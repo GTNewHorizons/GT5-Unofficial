b7b738ea1c163266e5f89b42ee7287e7a325c225
masaru05 <azusa9002@gmail.com>
2026-07-17 12:23:04 +0700

    Remove placeholder from IconComponent and unused icons

--- src/main/java/afishstash/IconComponent.java
+++ src/main/java/afishstash/IconComponent.java
@@ -1,26 +1,20 @@
 package afishstash;
 
-import com.google.gson.JsonObject;
-import com.slprime.chromatictooltips.TooltipHandler;
-import com.slprime.chromatictooltips.api.ITooltipComponent;
-import com.slprime.chromatictooltips.api.TooltipContext;
-import com.slprime.chromatictooltips.api.TooltipStyle;
-import com.slprime.chromatictooltips.component.InlineComponent;
-import com.slprime.chromatictooltips.util.TooltipAlign;
-import com.slprime.chromatictooltips.util.TooltipTexture;
-import gregtech.api.util.tooltip.macros.TooltipMacroProcessor;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.Objects;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.ResourceLocation;
 
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.util.List;
-import java.util.Objects;
+import com.google.gson.JsonObject;
+import com.slprime.chromatictooltips.api.ITooltipComponent;
+import com.slprime.chromatictooltips.api.TooltipContext;
+import com.slprime.chromatictooltips.api.TooltipStyle;
+import com.slprime.chromatictooltips.util.TooltipAlign;
+import com.slprime.chromatictooltips.util.TooltipTexture;
 
 public class IconComponent implements ITooltipComponent {
-    private static final IconComponent LASER = new IconComponent("gregtech:textures/tooltip/laser.png", 8);
-    public static final String LASER_ID = initLaser();
-    public static final TooltipMacroProcessor LASER_MACRO = TooltipMacroProcessor.of("laser", _ -> LASER_ID);
 
     protected final TooltipTexture texture;
     protected final int size;
@@ -65,20 +59,15 @@ public class IconComponent implements ITooltipComponent {
         return texture.hashCode();
     }
 
-    private static String initLaser() {
-        List<ITooltipComponent> components = List.of(LASER);
-        return TooltipHandler.getPermanentId(new InlineComponent(List.of(components), 2, 2));
-    }
-
     private static void ensurePathExists(String resourceLocation) {
         ResourceLocation rl = new ResourceLocation(resourceLocation);
 
         try {
-            Minecraft.getMinecraft().getResourceManager().getResource(rl);
+            Minecraft.getMinecraft()
+                .getResourceManager()
+                .getResource(rl);
         } catch (IOException e) {
-            throw new UncheckedIOException(
-                String.format("Cannot find resource located at %s", resourceLocation), e
-                        );
+            throw new UncheckedIOException(String.format("Cannot find resource located at %s", resourceLocation), e);
         }
     }
 }
