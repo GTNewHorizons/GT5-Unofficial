e88ea8380d6acd41fb0c13be13313ae00621cefb
Yoshy <60155833+Yoshy2002@users.noreply.github.com>
2026-07-18 14:32:07 +0200

    Fix Flotation Cell Casing Amount in Tooltip and check + increase its hatch space (#7490)

--- src/main/java/gregtech/common/tileentities/machines/multi/MTEFrothFlotationCell.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/MTEFrothFlotationCell.java
@@ -66,6 +66,7 @@ public class MTEFrothFlotationCell extends MTEExtendedPowerMultiBlockBase<MTEFro
     private static final int OFFSET_X = 5;
     private static final int OFFSET_Y = 3;
     private static final int OFFSET_Z = 0;
+    private static final int TOP_CASING_AMOUNT = 28;
 
     private int casingAmount;
     private boolean needsWaterFill = false;
@@ -122,7 +123,7 @@ public class MTEFrothFlotationCell extends MTEExtendedPowerMultiBlockBase<MTEFro
             .addPollutionAmount(getPollutionPerSecond(null))
             .beginStructureBlock(11, 11, 5, false)
             .addController("Front center, 2nd layer")
-            .addCasing("90-120", "Inconel Reinforced Casing", false)
+            .addCasing("110-120", "Inconel Reinforced Casing", false)
             .addCasing("31", "Flotation Cell Casing", false)
             .addCasing("20", "Staballoy Frame Box", false)
             .addCasing("8", "Inconel-690 Frame Box", false)
@@ -206,7 +207,7 @@ public class MTEFrothFlotationCell extends MTEExtendedPowerMultiBlockBase<MTEFro
                 'W');
             return;
         }
-        checkCasingMin(errors, casingAmount, 90);
+        checkCasingMin(errors, casingAmount + TOP_CASING_AMOUNT, 110);
         checkHasEnergyHatch(errors);
         checkHasMaintenanceHatch(errors);
         checkHasInputBus(errors);
