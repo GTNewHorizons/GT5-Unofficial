844530f15a4715bd18f17ce496eafa495a2430ab
AnsonYeung <nanolify@gmail.com>
2026-07-19 19:30:26 +0800

    Fix me hatch 0L extract

--- src/main/java/gregtech/common/tileentities/machines/MTEHatchInputME.java
+++ src/main/java/gregtech/common/tileentities/machines/MTEHatchInputME.java
@@ -362,6 +362,7 @@ public class MTEHatchInputME extends MTEHatchInput implements IPowerChannelState
         // this is an ME input hatch. allowing draining via logistics would be very wrong (and against
         // canTankBeEmptied()) but we do need to support draining from controller, which uses the UNKNOWN direction.
         if (side != ForgeDirection.UNKNOWN) return null;
+        if (amount == 0) return new FluidStack(fluid, 0);
 
         if (processingRecipe) {
             // When processing a recipe, we just extract from the slot fake stacks
