710b476f8b8132583109b6a59893d210837f2f91
yoshy2002 <jascha.morzuch@gmail.com>
2026-07-19 20:42:56 +0200

    fix

--- src/main/java/gregtech/api/metatileentity/implementations/MTEMultiBlockBase.java
+++ src/main/java/gregtech/api/metatileentity/implementations/MTEMultiBlockBase.java
@@ -2510,7 +2510,7 @@ public abstract class MTEMultiBlockBase extends MetaTileEntity
         }
 
         if (recipesDone > 0) {
-            info.add(StatCollector.translateToLocalFormatted("GT5U.multiblock.recipesDone", formatNumber(recipesDone)));
+            info.add(StatCollector.translateToLocalFormatted("GT5U.multiblock.recipesDone.fmt", formatNumber(recipesDone)));
         }
 
         info.add(StatCollector.translateToLocalFormatted(timeKey, timeValue));
