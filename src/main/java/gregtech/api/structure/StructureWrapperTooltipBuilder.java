4a866e62d865872baa5aae361fc545d3143ad510
AnsonYeung <nanolify@gmail.com>
2026-07-19 19:04:15 +0800

    Fixup

--- src/main/java/gregtech/api/structure/StructureWrapperTooltipBuilder.java
+++ src/main/java/gregtech/api/structure/StructureWrapperTooltipBuilder.java
@@ -79,18 +79,18 @@ public class StructureWrapperTooltipBuilder<MTE extends MTEEnhancedMultiBlockBas
     public StructureWrapperTooltipBuilder<MTE> beginStructureBlock() {
         if (!structure.minSize.equals(structure.maxSize)) {
             super.beginVariableStructureBlock(
-                structure.minSize.get2(),
                 structure.maxSize.get0(),
-                structure.maxSize.get1(),
-                structure.maxSize.get2(),
                 structure.minSize.get0(),
+                structure.maxSize.get1(),
                 structure.minSize.get1(),
+                structure.maxSize.get2(),
+                structure.minSize.get2(),
                 false);
         } else {
             super.beginStructureBlock(
+                structure.minSize.get0(),
                 structure.minSize.get1(),
                 structure.minSize.get2(),
-                structure.minSize.get0(),
                 false);
         }
 
