d5649940a6dcffa34e8f256018a4c207ac54903b
AnsonYeung <nanolify@gmail.com>
2026-07-19 22:17:14 +0800

    upd comment

--- src/main/java/gregtech/common/tileentities/machines/MTEHatchInputME.java
+++ src/main/java/gregtech/common/tileentities/machines/MTEHatchInputME.java
@@ -381,7 +381,9 @@ public class MTEHatchInputME extends MTEHatchInput implements IPowerChannelState
             // Outside of processing a recipe, we need to extract everything manually
             Slot slot = getMatchingSlot(fluid, false);
             if (slot == null) return null;
-            if (amount == 0) return new FluidStack(fluid, 0); // AE can't use 0 for exist check, just assume exist
+            // AE can't use 0 for exist check, just assume exist
+            // Other hatch also don't have existence check
+            if (amount == 0) return new FluidStack(fluid, 0);
 
             IAEFluidStack request = AEFluidStack.create(fluid);
             request.setStackSize(amount);
