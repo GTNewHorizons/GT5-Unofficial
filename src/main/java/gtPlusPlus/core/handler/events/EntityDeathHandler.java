d34d0759bbc50a28438b3cb44dddf942193ab3fe
Wilhelm Schuster <dev@rot13.io>
2026-07-20 16:28:52 +0200

    Drop cooked meats when mob dies while burning
    Vanilla does this already for cows and pigs that die while on fire.

--- src/main/java/gtPlusPlus/core/handler/events/EntityDeathHandler.java
+++ src/main/java/gtPlusPlus/core/handler/events/EntityDeathHandler.java
@@ -26,6 +26,7 @@ import gtPlusPlus.xmod.gregtech.api.enums.GregtechItemList;
 public class EntityDeathHandler implements IMobExtraInfoProvider {
 
     private static final HashMap<Class<?>, ArrayList<Triple<ItemStack, Integer, Integer>>> mMobDropMap = new HashMap<>();
+    private static final HashMap<Class<?>, ArrayList<Triple<ItemStack, Integer, Integer>>> mBurningMobDropMap = new HashMap<>();
     private static final ArrayList<Class<?>> mInternalClassKeyCache = new ArrayList<>();
 
     /**
@@ -36,14 +37,33 @@ public class EntityDeathHandler implements IMobExtraInfoProvider {
      * @param aMaxAmount - The maximum size of the ItemStack which drops.
      * @param aChance    - Chance out of 10000, where 100 is 1%. (1 = 0.01% - this is ok)
      */
-    public static void registerDropsForMob(Class aMobClass, ItemStack aStack, int aMaxAmount, int aChance) {
-        Triple<ItemStack, Integer, Integer> aData = Triple.of(aStack, aMaxAmount, aChance);
-        ArrayList<Triple<ItemStack, Integer, Integer>> aDataMap = mMobDropMap.get(aMobClass);
+    public static void registerDropsForMob(Class<?> aMobClass, ItemStack aStack, int aMaxAmount, int aChance) {
+        registerDropsForMob(aMobClass, aStack, aMaxAmount, aChance, false);
+    }
+
+    /**
+     * Provides the ability to provide custom drops upon the death of burning EntityLivingBase objects.
+     *
+     * @param aMobClass  - The Base Class you want to drop this item.
+     * @param aStack     - The ItemStack, stack size is not respected.
+     * @param aMaxAmount - The maximum size of the ItemStack which drops.
+     * @param aChance    - Chance out of 10000, where 100 is 1%. (1 = 0.01% - this is ok)
+     */
+    public static void registerDropsForBurningMob(Class<?> aMobClass, ItemStack aStack, int aMaxAmount, int aChance) {
+        registerDropsForMob(aMobClass, aStack, aMaxAmount, aChance, true);
+    }
+
+    private static void registerDropsForMob(Class<?> aMobClass, ItemStack aStack, int aMaxAmount, int aChance,
+        boolean dropWhenMobIsBurning) {
+        var targetDropMap = (dropWhenMobIsBurning) ? mBurningMobDropMap : mMobDropMap;
+        var aData = Triple.of(aStack, aMaxAmount, aChance);
+        var aDataMap = targetDropMap.get(aMobClass);
+
         if (aDataMap == null) {
             aDataMap = new ArrayList<>();
         }
         aDataMap.add(aData);
-        mMobDropMap.put(aMobClass, aDataMap);
+        targetDropMap.put(aMobClass, aDataMap);
         mInternalClassKeyCache.add(aMobClass);
     }
 
@@ -53,25 +73,29 @@ public class EntityDeathHandler implements IMobExtraInfoProvider {
         int aChanceOutOf10000 = aData.getRight();
         if (MathUtils.randInt(0, 10000) <= aChanceOutOf10000) {
             aLoot = GTUtility.copyAmount(MathUtils.randInt(1, aMaxDrop), aLoot);
-            if (aLoot != null) {
-                return aLoot;
-            }
+            return aLoot;
         }
         return null;
     }
 
     private static boolean processDropsForMob(EntityLivingBase entityLiving) {
-        ArrayList<Triple<ItemStack, Integer, Integer>> aMobData = mMobDropMap.get(entityLiving.getClass());
+        ArrayList<Triple<ItemStack, Integer, Integer>> aMobData = null;
+        if (entityLiving.isBurning()) {
+            aMobData = mBurningMobDropMap.get(entityLiving.getClass());
+            // falls back to the normal drop map below if nothing is defined in the burning drop map
+        }
+        if (aMobData == null || aMobData.isEmpty()) {
+            aMobData = mMobDropMap.get(entityLiving.getClass());
+        }
+
         boolean aDidDrop = false;
-        if (aMobData != null) {
-            if (!aMobData.isEmpty()) {
-                ItemStack aPossibleDrop;
-                for (Triple<ItemStack, Integer, Integer> g : aMobData) {
-                    aPossibleDrop = processItemDropTriplet(g);
-                    if (aPossibleDrop != null) {
-                        if (entityLiving.entityDropItem(aPossibleDrop, MathUtils.randFloat(0, 1)) != null) {
-                            aDidDrop = true;
-                        }
+        if (aMobData != null && !aMobData.isEmpty()) {
+            ItemStack aPossibleDrop;
+            for (Triple<ItemStack, Integer, Integer> g : aMobData) {
+                aPossibleDrop = processItemDropTriplet(g);
+                if (aPossibleDrop != null) {
+                    if (entityLiving.entityDropItem(aPossibleDrop, MathUtils.randFloat(0, 1)) != null) {
+                        aDidDrop = true;
                     }
                 }
             }
@@ -80,23 +104,24 @@ public class EntityDeathHandler implements IMobExtraInfoProvider {
     }
 
     private static void dropMeatFromPlayer(EntityPlayer aPlayer) {
+        var meatVariant = (aPlayer.isBurning()) ? GregtechItemList.CookedHumanMeat : GregtechItemList.RawHumanMeat;
 
         // always drop some meat.
         int aBigMeatStackSize1 = MathUtils.randInt(4, 8);
-        aPlayer.entityDropItem(GregtechItemList.RawHumanMeat.get(aBigMeatStackSize1), MathUtils.randInt(0, 1));
+        aPlayer.entityDropItem(meatVariant.get(aBigMeatStackSize1), MathUtils.randInt(0, 1));
 
         // additional chances for more meat.
         if (MathUtils.randInt(0, 10) < 7) {
             int aBigMeatStackSize2 = MathUtils.randInt(4, 8);
-            aPlayer.entityDropItem(GregtechItemList.RawHumanMeat.get(aBigMeatStackSize2), MathUtils.randInt(0, 1));
+            aPlayer.entityDropItem(meatVariant.get(aBigMeatStackSize2), MathUtils.randInt(0, 1));
         }
         if (MathUtils.randInt(0, 10) < 4) {
             int aBigMeatStackSize3 = MathUtils.randInt(4, 8);
-            aPlayer.entityDropItem(GregtechItemList.RawHumanMeat.get(aBigMeatStackSize3), MathUtils.randInt(0, 1));
+            aPlayer.entityDropItem(meatVariant.get(aBigMeatStackSize3), MathUtils.randInt(0, 1));
         }
         if (MathUtils.randInt(0, 10) < 2) {
             int aBigMeatStackSize4 = MathUtils.randInt(4, 8);
-            aPlayer.entityDropItem(GregtechItemList.RawHumanMeat.get(aBigMeatStackSize4), MathUtils.randInt(0, 1));
+            aPlayer.entityDropItem(meatVariant.get(aBigMeatStackSize4), MathUtils.randInt(0, 1));
         }
     }
 
@@ -121,7 +146,7 @@ public class EntityDeathHandler implements IMobExtraInfoProvider {
     @Override
     public void provideExtraDropsInformation(@NotNull String entityString, @NotNull ArrayList<MobDrop> drops,
         @NotNull MobRecipe recipe) {
-        ArrayList<Triple<ItemStack, Integer, Integer>> dropEntry = mMobDropMap.get(recipe.entity.getClass());
+        var dropEntry = mMobDropMap.get(recipe.entity.getClass());
 
         if (dropEntry != null && !dropEntry.isEmpty()) {
             for (Triple<ItemStack, Integer, Integer> data : dropEntry) {
