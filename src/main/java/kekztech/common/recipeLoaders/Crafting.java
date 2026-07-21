a641eed227b14775a64086da3d28aabe4763bd86
Ruling-0 <47913168+Ruling-0@users.noreply.github.com>
2026-07-20 20:46:12 -0500

    Convert kekztech Crafting to the typed ore prefix accessors

--- src/main/java/kekztech/common/recipeLoaders/Crafting.java
+++ src/main/java/kekztech/common/recipeLoaders/Crafting.java
@@ -53,24 +53,26 @@ public class Crafting implements Runnable {
         GTModHandler.addCraftingRecipe(TileEntities.lsc.getStackForm(1), lsc_recipe);
 
         // LSC casing
-        final Object[] lcBase_recipe = { "WBW", "RLR", "WBW", 'W', OrePrefixes.plate.get(Materials.Tantalum), 'B',
-            OrePrefixes.frameGt.get(Materials.TungstenSteel), 'R', OrePrefixes.stickLong.get(Materials.TungstenSteel),
-            'L', OrePrefixes.block.get(Materials.Lapis) };
+        final Object[] lcBase_recipe = { "WBW", "RLR", "WBW", 'W', OrePrefixes.plate.ingredient(Materials.Tantalum),
+            'B', OrePrefixes.frameGt.ingredient(Materials.TungstenSteel), 'R',
+            OrePrefixes.stickLong.ingredient(Materials.TungstenSteel), 'L',
+            OrePrefixes.block.ingredient(Materials.Lapis) };
         GTModHandler.addCraftingRecipe(new ItemStack(Blocks.lscLapotronicEnergyUnit, 1, 0), lcBase_recipe);
 
         // Empty Capacitor
-        final Object[] lcEmpty_recipe = { "SLS", "L L", "SLS", 'S', OrePrefixes.screw.get(Materials.Lapis), 'L',
-            OrePrefixes.plate.get(Materials.Lapis) };
+        final Object[] lcEmpty_recipe = { "SLS", "L L", "SLS", 'S', OrePrefixes.screw.ingredient(Materials.Lapis), 'L',
+            OrePrefixes.plate.ingredient(Materials.Lapis) };
         GTModHandler.addCraftingRecipe(new ItemStack(Blocks.lscLapotronicEnergyUnit, 1, 6), lcEmpty_recipe);
 
         // EV Capacitor
-        final Object[] lcEV_recipe = { "SLS", "LCL", "SLS", 'S', OrePrefixes.screw.get(Materials.Lapis), 'L',
-            OrePrefixes.plate.get(Materials.Lapis), 'C', GTModHandler.getIC2Item("lapotronCrystal", 1L, WILDCARD) };
+        final Object[] lcEV_recipe = { "SLS", "LCL", "SLS", 'S', OrePrefixes.screw.ingredient(Materials.Lapis), 'L',
+            OrePrefixes.plate.ingredient(Materials.Lapis), 'C',
+            GTModHandler.getIC2Item("lapotronCrystal", 1L, WILDCARD) };
         GTModHandler.addCraftingRecipe(new ItemStack(Blocks.lscLapotronicEnergyUnit, 1, 7), lcEV_recipe);
 
         // IV Capacitor
-        final Object[] lcIV_recipe = { "SLS", "LOL", "SLS", 'S', OrePrefixes.screw.get(Materials.Lapis), 'L',
-            OrePrefixes.plate.get(Materials.Lapis), 'O', ItemList.Energy_LapotronicOrb.get(1L) };
+        final Object[] lcIV_recipe = { "SLS", "LOL", "SLS", 'S', OrePrefixes.screw.ingredient(Materials.Lapis), 'L',
+            OrePrefixes.plate.ingredient(Materials.Lapis), 'O', ItemList.Energy_LapotronicOrb.get(1L) };
         GTModHandler.addCraftingRecipe(new ItemStack(Blocks.lscLapotronicEnergyUnit, 1, 1), lcIV_recipe);
 
     }
