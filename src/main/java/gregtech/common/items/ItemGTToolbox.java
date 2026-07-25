66836ad5802705a57781dc972cba94386de9c393
Querns <querns@gbs.io>
2026-07-25 13:40:34 -0500

    Toolbox can now have multiple tool results for pick block

--- src/main/java/gregtech/common/items/ItemGTToolbox.java
+++ src/main/java/gregtech/common/items/ItemGTToolbox.java
@@ -92,6 +92,9 @@ public class ItemGTToolbox extends GTGenericItem implements IGuiHolder<PlayerInv
     public static final String BROKEN_TOOL_ANIMATION_END_KEY = "gt5u.toolbox:BrokenToolAnimationEnd";
     public static final int NO_TOOL_SELECTED = -1;
 
+    private static final String LAST_PICKED_COORDS_KEY = "gt5u.toolbox:LastPickedBlock";
+    private static final String LAST_PICKED_INDEX_KEY = "gt5u.toolbox:LastPickedIndex";
+
     /**
      * The charging mechanic of the toolbox happens every one in CHARGE_TICK ticks. All numbers are multiplied by this
      * factor, so this value can be freely changed without having to do anything else. All tier limits imposed by the
@@ -540,6 +543,7 @@ public class ItemGTToolbox extends GTGenericItem implements IGuiHolder<PlayerInv
 
         if (player.isSneaking()) {
             if (selectedToolType.isPresent()) {
+                removeLastPickedData(player);
                 sendChangeToolPacket(inventorySlot, NO_TOOL_SELECTED);
                 return true;
             }
@@ -569,6 +573,7 @@ public class ItemGTToolbox extends GTGenericItem implements IGuiHolder<PlayerInv
 
         if (!player.isSneaking()) {
             if (toolCount == 1) {
+                removeLastPickedData(player);
                 sendChangeToolPacket(inventorySlot, selectedToolType.isPresent() ? NO_TOOL_SELECTED : lastSlot);
                 return true;
             } else {
@@ -583,17 +588,54 @@ public class ItemGTToolbox extends GTGenericItem implements IGuiHolder<PlayerInv
                     return true;
                 }
 
-                for (ToolboxSlot suggested : pickResults.suggestedTools()) {
-                    if (handler.getStackInSlot(suggested.getSlotID()) != null) {
-                        sendChangeToolPacket(inventorySlot, suggested.getSlotID());
-                        return true;
+                final List<ToolboxSlot> suggestedTools = pickResults.suggestedTools();
+                final long currentCoords = pickResults.packedCoordinates();
+
+                if (!suggestedTools.isEmpty() && currentCoords != 0L) {
+                    final NBTTagCompound tag = player.getEntityData()
+                        .getCompoundTag(EntityPlayer.PERSISTED_NBT_TAG);
+                    final int lastIndex = tag.getInteger(LAST_PICKED_INDEX_KEY);
+
+                    int startingIndex = 0;
+                    if (suggestedTools.size() > 1 && tag.getLong(LAST_PICKED_COORDS_KEY) == currentCoords
+                        && lastIndex + 1 < suggestedTools.size()) {
+                        startingIndex = lastIndex + 1;
                     }
+
+                    for (int i = startingIndex; i < suggestedTools.size(); i++) {
+                        final int slotID = suggestedTools.get(i)
+                            .getSlotID();
+                        if (handler.getStackInSlot(slotID) != null) {
+                            sendChangeToolPacket(inventorySlot, slotID);
+
+                            tag.setLong(LAST_PICKED_COORDS_KEY, currentCoords);
+                            tag.setInteger(LAST_PICKED_INDEX_KEY, i);
+                            player.getEntityData()
+                                .setTag(EntityPlayer.PERSISTED_NBT_TAG, tag);
+
+                            return true;
+                        }
+                    }
+
                 }
             }
         }
 
+        removeLastPickedData(player);
         ToolboxSelectGuiFactory.INSTANCE.open(player);
         return true;
+    }
+
+    private static void removeLastPickedData(final EntityPlayer player) {
+        final NBTTagCompound tag = player.getEntityData()
+            .getCompoundTag(EntityPlayer.PERSISTED_NBT_TAG);
+
+        if (tag.hasKey(LAST_PICKED_COORDS_KEY)) {
+            tag.removeTag(LAST_PICKED_COORDS_KEY);
+            tag.removeTag(LAST_PICKED_INDEX_KEY);
+            player.getEntityData()
+                .setTag(EntityPlayer.PERSISTED_NBT_TAG, tag);
+        }
     }
 
     /**
