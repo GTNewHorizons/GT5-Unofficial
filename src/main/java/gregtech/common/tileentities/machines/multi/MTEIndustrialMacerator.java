1292e99e67bb2b79df32800617b846be6b5df5a9
metalcupcake5 <43897385+metalcupcake5@users.noreply.github.com>
2026-07-26 20:09:43 -0400

    fix typo in IMS tooltip

--- src/main/java/gregtech/common/tileentities/machines/multi/MTEIndustrialMacerator.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/MTEIndustrialMacerator.java
@@ -121,7 +121,7 @@ public class MTEIndustrialMacerator extends MTEExtendedPowerMultiBlockBase<MTEIn
             .addOutputBus("1+", "Any casing", 1)
             .addStructureInfo("")
             .addStructureInfo(StatCollector.translateToLocal("GT5U.MBTT.Tiers.One"))
-            .addCasing("26-44", "Stable Titanium MAchine Casing", false)
+            .addCasing("26-44", "Stable Titanium Machine Casing", false)
             .addStructureInfo("")
             .addStructureInfo(StatCollector.translateToLocal("GT5U.MBTT.Tiers.Two"))
             .addCasing("69-87", "Maceration Stack Casing", false)
