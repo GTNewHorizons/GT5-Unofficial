2f9adb1f81bc23a3451141df1fa2757e2defd9ac
RecursivePineapple <recursive_pineapple@proton.me>
2026-07-31 09:56:29 -0400

    Clear NAC grid on server stop

--- src/main/java/gregtech/common/tileentities/machines/multi/nanochip/factory/VacuumFactoryGrid.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/nanochip/factory/VacuumFactoryGrid.java
@@ -1,7 +1,13 @@
 package gregtech.common.tileentities.machines.multi.nanochip.factory;
 
+import net.minecraftforge.event.world.WorldEvent;
+
+import com.gtnewhorizon.gtnhlib.eventbus.EventBusSubscriber;
+
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import gregtech.api.factory.standard.StandardFactoryGrid;
 
+@EventBusSubscriber
 public class VacuumFactoryGrid
     extends StandardFactoryGrid<VacuumFactoryGrid, VacuumFactoryElement, VacuumFactoryNetwork> {
 
@@ -10,5 +16,22 @@ public class VacuumFactoryGrid
     @Override
     protected VacuumFactoryNetwork createNetwork() {
         return new VacuumFactoryNetwork();
+    }
+
+    @SubscribeEvent
+    public static void onServerClosed(WorldEvent.Unload event) {
+        if (event.world.provider.dimensionId != 0) return;
+        if (event.world.isRemote) return;
+
+        INSTANCE.networks.forEach(network -> {
+            network.elements.forEach(element -> { element.setNetwork(null); });
+
+            network.elements.clear();
+            network.components.clear();
+        });
+
+        INSTANCE.networks.clear();
+        INSTANCE.edges.clear();
+        INSTANCE.vertices.clear();
     }
 }
