6cd98fa96443aaa151434efc917e3c17aa43bd9f
RecursivePineapple <recursive_pineapple@proton.me>
2026-07-31 11:03:53 -0400

    Add warnings if BEC or NAC grids aren't empty on server stop

--- src/main/java/gregtech/common/tileentities/machines/multi/nanochip/factory/VacuumFactoryGrid.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/nanochip/factory/VacuumFactoryGrid.java
@@ -5,6 +5,7 @@ import net.minecraftforge.event.world.WorldEvent;
 import com.gtnewhorizon.gtnhlib.eventbus.EventBusSubscriber;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import gregtech.GTMod;
 import gregtech.api.factory.standard.StandardFactoryGrid;
 
 @EventBusSubscriber
@@ -22,6 +23,18 @@ public class VacuumFactoryGrid
     public static void onServerClosed(WorldEvent.Unload event) {
         if (event.world.provider.dimensionId != 0) return;
         if (event.world.isRemote) return;
+
+        if (!INSTANCE.networks.isEmpty()) {
+            GTMod.GT_FML_LOGGER.warn(
+                "VacuumFactoryGrid had networks that weren't removed before the server stopped: this could indicate a memory leak.");
+        }
+
+        if (!INSTANCE.vertices.isEmpty()) {
+            GTMod.GT_FML_LOGGER.warn(
+                "VacuumFactoryGrid had vertices that weren't removed before the server stopped: this could indicate a memory leak.");
+        }
+
+        // Make sure everything is unloaded, even if something didn't remove itself properly
 
         INSTANCE.networks.forEach(network -> {
             network.elements.forEach(element -> { element.setNetwork(null); });
--- src/main/java/tectech/mechanics/boseEinsteinCondensate/BECFactoryGrid.java
+++ src/main/java/tectech/mechanics/boseEinsteinCondensate/BECFactoryGrid.java
@@ -5,6 +5,7 @@ import net.minecraftforge.event.world.WorldEvent;
 import com.gtnewhorizon.gtnhlib.eventbus.EventBusSubscriber;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import gregtech.GTMod;
 import gregtech.api.factory.standard.StandardFactoryGrid;
 
 @EventBusSubscriber
@@ -21,6 +22,18 @@ public class BECFactoryGrid extends StandardFactoryGrid<BECFactoryGrid, BECFacto
     public static void onServerClosed(WorldEvent.Unload event) {
         if (event.world.provider.dimensionId != 0) return;
         if (event.world.isRemote) return;
+
+        if (!INSTANCE.networks.isEmpty()) {
+            GTMod.GT_FML_LOGGER.warn(
+                "BECFactoryGrid had networks that weren't removed before the server stopped: this could indicate a memory leak.");
+        }
+
+        if (!INSTANCE.vertices.isEmpty()) {
+            GTMod.GT_FML_LOGGER.warn(
+                "BECFactoryGrid had vertices that weren't removed before the server stopped: this could indicate a memory leak.");
+        }
+
+        // Make sure everything is unloaded, even if something didn't remove itself properly
 
         INSTANCE.networks.forEach(network -> {
             network.elements.forEach(element -> { element.setNetwork(null); });
