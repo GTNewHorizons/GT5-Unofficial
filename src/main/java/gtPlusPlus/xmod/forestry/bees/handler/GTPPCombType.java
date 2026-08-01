f0e5d47b7ba4b31c5ba4541b4889e3591c0d5574
boubou19 <miisterunknown@gmail.com>
2026-08-02 00:51:24 +0200

    make enum fields final

--- src/main/java/gtPlusPlus/xmod/forestry/bees/handler/GTPPCombType.java
+++ src/main/java/gtPlusPlus/xmod/forestry/bees/handler/GTPPCombType.java
@@ -13,7 +13,7 @@ public enum GTPPCombType {
     DRAGONBLOOD(0, "Dragonblood", true, Utils.rgbtoHexValue(220, 20, 20), Utils.rgbtoHexValue(20, 20, 20)),
     FORCE(1, "Force", true, Utils.rgbtoHexValue(250, 250, 20), Utils.rgbtoHexValue(200, 200, 5));
 
-    public boolean mShowInList;
+    public final boolean mShowInList;
     public final Material mMaterial;
     public final int mID;
 
