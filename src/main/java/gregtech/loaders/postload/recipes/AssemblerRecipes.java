b476565d27496cbd78fc216af541c6793c55c70e
Mykhailo Balaker <83900885+Angry3vilbot@users.noreply.github.com>
2026-08-02 21:14:47 +0100

    Add assembler recipes to metal swords

--- src/main/java/gregtech/loaders/postload/recipes/AssemblerRecipes.java
+++ src/main/java/gregtech/loaders/postload/recipes/AssemblerRecipes.java
@@ -3386,6 +3386,31 @@ public class AssemblerRecipes implements Runnable {
             .duration(2 * SECONDS)
             .eut(TierEU.RECIPE_LuV)
             .addTo(assemblerRecipes);
+
+        GTValues.RA.stdBuilder()
+            .itemInputs(
+                GTOreDictUnificator.get(OrePrefixes.plate, Materials.Diamond, 2),
+                GTOreDictUnificator.get(OrePrefixes.rod, Materials.Wood, 1))
+            .itemOutputs(new ItemStack(Items.diamond_sword))
+            .duration(4 * SECONDS)
+            .eut(TierEU.RECIPE_MV)
+            .addTo(assemblerRecipes);
+        GTValues.RA.stdBuilder()
+            .itemInputs(
+                GTOreDictUnificator.get(OrePrefixes.plate, Materials.Gold, 2),
+                GTOreDictUnificator.get(OrePrefixes.rod, Materials.Wood, 1))
+            .itemOutputs(new ItemStack(Items.golden_sword))
+            .duration(4 * SECONDS)
+            .eut(TierEU.RECIPE_MV)
+            .addTo(assemblerRecipes);
+        GTValues.RA.stdBuilder()
+            .itemInputs(
+                GTOreDictUnificator.get(OrePrefixes.plate, Materials.Iron, 2),
+                GTOreDictUnificator.get(OrePrefixes.rod, Materials.Wood, 1))
+            .itemOutputs(new ItemStack(Items.iron_sword))
+            .duration(4 * SECONDS)
+            .eut(TierEU.RECIPE_MV)
+            .addTo(assemblerRecipes);
     }
 
     /**
