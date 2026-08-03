c7050f07762430984a3023da3a13139aa7583343
Mykhailo Balaker <83900885+Angry3vilbot@users.noreply.github.com>
2026-08-03 16:45:52 +0100

    Move to circuit 2

--- src/main/java/gregtech/loaders/postload/recipes/AssemblerRecipes.java
+++ src/main/java/gregtech/loaders/postload/recipes/AssemblerRecipes.java
@@ -3392,6 +3392,7 @@ public class AssemblerRecipes implements Runnable {
                 GTOreDictUnificator.get(OrePrefixes.plate, Materials.Diamond, 2),
                 GTOreDictUnificator.get(OrePrefixes.stick, Materials.Wood, 1))
             .itemOutputs(new ItemStack(Items.diamond_sword))
+            .circuit(2)
             .duration(4 * SECONDS)
             .eut(TierEU.RECIPE_MV)
             .addTo(assemblerRecipes);
@@ -3400,6 +3401,7 @@ public class AssemblerRecipes implements Runnable {
                 GTOreDictUnificator.get(OrePrefixes.plate, Materials.Gold, 2),
                 GTOreDictUnificator.get(OrePrefixes.stick, Materials.Wood, 1))
             .itemOutputs(new ItemStack(Items.golden_sword))
+            .circuit(2)
             .duration(4 * SECONDS)
             .eut(TierEU.RECIPE_MV)
             .addTo(assemblerRecipes);
@@ -3408,6 +3410,7 @@ public class AssemblerRecipes implements Runnable {
                 GTOreDictUnificator.get(OrePrefixes.plate, Materials.Iron, 2),
                 GTOreDictUnificator.get(OrePrefixes.stick, Materials.Wood, 1))
             .itemOutputs(new ItemStack(Items.iron_sword))
+            .circuit(2)
             .duration(4 * SECONDS)
             .eut(TierEU.RECIPE_MV)
             .addTo(assemblerRecipes);
