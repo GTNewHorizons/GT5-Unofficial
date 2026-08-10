5cff9896ed46fc8c69b78df982cae40795bf4ec7
Nana Sakura <nana-sakura@qq.com>
2026-08-10 10:09:11 +0800

    add texture and definitions

--- src/main/java/gregtech/common/blocks/BlockCasings14.java
+++ src/main/java/gregtech/common/blocks/BlockCasings14.java
@@ -40,6 +40,7 @@ public class BlockCasings14 extends BlockCasingsAbstract {
                 yield Textures.BlockIcons.MACHINE_CASING_FRIDGE_SIDE.getIcon();
             }
             case 5 -> Textures.BlockIcons.MACHINE_CASING_NAQUADAH_REINFORCED_DISTILLATION.getIcon();
+            case 6 -> Textures.BlockIcons.EXTRUDER_MACHINE_CASING.getIcon();
             case 7 -> Textures.BlockIcons.TENSION_RESISTANT_MACHINE_CASING.getIcon();
             default -> Textures.BlockIcons.MACHINE_CASING_ROBUST_TUNGSTENSTEEL.getIcon();
         };
