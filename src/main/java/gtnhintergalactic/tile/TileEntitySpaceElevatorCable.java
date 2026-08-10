2e8ee80bf054c605bd6716818957f0f610b4fcb5
boubou19 <miisterunknown@gmail.com>
2026-08-10 06:53:18 +0200

    ClimberAnimation.VALUES

--- src/main/java/gtnhintergalactic/tile/TileEntitySpaceElevatorCable.java
+++ src/main/java/gtnhintergalactic/tile/TileEntitySpaceElevatorCable.java
@@ -24,7 +24,9 @@ public class TileEntitySpaceElevatorCable extends TileEntityAdvanced {
         /** Animation that drives the climber into orbit, pauses a bit and then drives down */
         DELIVER_ANIMATION,
         /** Animation that spawns the climber in orbit and then drives it down */
-        FORMATION_ANIMATION,
+        FORMATION_ANIMATION;
+
+        public static final ClimberAnimation[] VALUES = values();
     }
 
     /** Vertical movement speed of the climber */
@@ -206,8 +208,8 @@ public class TileEntitySpaceElevatorCable extends TileEntityAdvanced {
     public void readExtraNetworkedData(ByteBuf dataStream) {
         if (worldObj.isRemote) {
             int animationOrdinal = dataStream.readInt();
-            if (animationOrdinal < ClimberAnimation.values().length) {
-                animation = ClimberAnimation.values()[animationOrdinal];
+            if (animationOrdinal < ClimberAnimation.VALUES.length) {
+                animation = ClimberAnimation.VALUES[animationOrdinal];
             } else {
                 animation = ClimberAnimation.NO_ANIMATION;
             }
