c2e1219aaaa82d86de126b1bf8482ca00d99c4d4
UltraProdigy <187078471+UltraProdigy@users.noreply.github.com>
2026-08-11 23:20:03 -0400

    fix double plates

--- src/main/java/gtPlusPlus/xmod/gregtech/loaders/RecipeGenPlates.java
+++ src/main/java/gtPlusPlus/xmod/gregtech/loaders/RecipeGenPlates.java
@@ -112,7 +112,7 @@ public class RecipeGenPlates extends RecipeGenBase {
                 .circuit(2)
                 .itemOutputs(plate_Double)
                 .duration(Math.max(material.getMass() * 2L, 1L))
-                .eut(material.voltageMultiplier)
+                .eut(Math.max(TierEU.RECIPE_MV, material.voltageMultiplier))
                 .addTo(benderRecipes);
         }
 
@@ -122,7 +122,7 @@ public class RecipeGenPlates extends RecipeGenBase {
                 .circuit(2)
                 .itemOutputs(plate_Double)
                 .duration(Math.max(material.getMass() * 2L, 1L))
-                .eut(material.voltageMultiplier)
+                .eut(Math.max(TierEU.RECIPE_MV, material.voltageMultiplier))
                 .addTo(benderRecipes);
         }
 
