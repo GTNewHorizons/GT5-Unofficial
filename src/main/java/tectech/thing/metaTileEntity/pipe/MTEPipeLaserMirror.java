b2e8e343cac26f9ec0b21456b10471b496b23faa
AnsonYeung <nanolify@gmail.com>
2026-08-13 16:24:31 +0800

    remove stale comment

--- src/main/java/tectech/thing/metaTileEntity/pipe/MTEPipeLaserMirror.java
+++ src/main/java/tectech/thing/metaTileEntity/pipe/MTEPipeLaserMirror.java
@@ -112,7 +112,6 @@ public class MTEPipeLaserMirror extends MTEPipeLaser {
         ForgeDirection a = connectedSides[0];
         ForgeDirection b = connectedSides[1];
 
-        // Race condition of updateNetwork() and onPostTick()
         if (a == null || b == null) {
             return null;
         }
