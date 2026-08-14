0e0c6de9dfe87527253d7091dfa5e8c4ecf15270
Ranzu <66495944+Ranzuu@users.noreply.github.com>
2026-08-14 15:35:57 +0200

    Lower the chances even more (its too much)

--- src/main/java/gregtech/common/tileentities/machines/basic/MTEIceCreamMachine.java
+++ src/main/java/gregtech/common/tileentities/machines/basic/MTEIceCreamMachine.java
@@ -124,9 +124,9 @@ public class MTEIceCreamMachine extends MTEBasicMachine implements IMTERenderer,
     }
 
     /** Chance out of 100 that the machine works on that day */
-    private static final int WORK_CHANCE_PERCENT = 40;
+    private static final int WORK_CHANCE_PERCENT = 30;
     /** Chance out of 100 that it also asks for a repair item */
-    private static final int REPAIR_REQUEST_CHANCE_PERCENT = 60;
+    private static final int REPAIR_REQUEST_CHANCE_PERCENT = 40;
     private static final int BROKEN_TOOLTIP_COUNT = 4;
     private static final long TICKS_PER_DAY = 24000L;
 
