a4e04388b8e3ca74042806780dfb79646dae7c60
boubou19 <miisterunknown@gmail.com>
2026-08-16 23:08:11 +0200

    avoid registry lookups

--- src/main/java/gregtech/loaders/oreprocessing/ProcessingUtils.java
+++ src/main/java/gregtech/loaders/oreprocessing/ProcessingUtils.java
@@ -9,8 +9,7 @@ public class ProcessingUtils {
     public static String itemStackKey(ItemStack stack) {
         if (stack == null || stack.getItem() == null) return null;
 
-        GameRegistry.UniqueIdentifier id = GameRegistry.findUniqueIdentifierFor(stack.getItem());
-        if (id == null) return null;
+        String id = stack.getItem().delegate.name();
 
         String key = id + "@" + Items.feather.getDamage(stack);
         if (stack.hasTagCompound()) key += "#nbt=" + NBTPersist.toJsonObjectExact(stack.getTagCompound());
