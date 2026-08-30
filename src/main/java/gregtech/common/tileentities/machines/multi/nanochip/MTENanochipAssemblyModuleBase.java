b15edf717bccb46127a30421c5b4dba6e4ee9107
Maya <10861407+serenibyss@users.noreply.github.com>
2026-08-30 17:47:19 -0600

    NAC improvements (#7860)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Martin Robertz <dream-master@gmx.net>
    Co-authored-by: Auynonymous <auynaaubin@gmail.com>

--- src/main/java/gregtech/common/tileentities/machines/multi/nanochip/MTENanochipAssemblyModuleBase.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/nanochip/MTENanochipAssemblyModuleBase.java
@@ -124,7 +124,7 @@ public abstract class MTENanochipAssemblyModuleBase<T extends MTEExtendedPowerMu
 
     public int getMaxRecipeDuration() {
         return ((NACRecipeMapBackend) (this.getRecipeMap()
-            .getBackend())).getMaxDuration();
+            .getBackend())).getMaxDuration(-1);
     }
 
     protected final VacuumConveyorHatchMap<MTEHatchVacuumConveyorInput> vacuumConveyorInputs = new VacuumConveyorHatchMap<>();
@@ -685,11 +685,11 @@ public abstract class MTENanochipAssemblyModuleBase<T extends MTEExtendedPowerMu
         return vacuumConveyorOutputs.findAnyColoredHatch(color);
     }
 
-    protected boolean removeItemFromInputByColor(ItemStack stack, byte color) {
+    protected boolean removeItemFromInputByColor(ItemStack stack, byte color, boolean withName) {
         int totalToConsome = stack.stackSize;
         List<MTEHatchVacuumConveyorInput> hatches = vacuumConveyorInputs.findColoredHatches(color);
         for (MTEHatchVacuumConveyorInput inputHatch : hatches) {
-            int amountConsumed = inputHatch.tryConsume(stack);
+            int amountConsumed = inputHatch.tryConsume(stack, withName);
             totalToConsome -= amountConsumed;
             if (totalToConsome <= 0) {
                 return true;
@@ -722,7 +722,8 @@ public abstract class MTENanochipAssemblyModuleBase<T extends MTEExtendedPowerMu
         }
         // Look up component from this output fake stack and unify it with the packet inside the output hatch
         CircuitComponent component = CircuitComponent.getFromFakeStackUnsafe(aStack);
-        CircuitComponentPacket outputPacket = new CircuitComponentPacket(component, aStack.stackSize);
+        String customName = GTUtility.getStackCustomName(aStack);
+        CircuitComponentPacket outputPacket = new CircuitComponentPacket(component, aStack.stackSize, customName);
         hatch.unifyPacket(outputPacket);
     }
 
