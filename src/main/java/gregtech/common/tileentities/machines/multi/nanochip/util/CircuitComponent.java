2a3121af34753ce4889b054cc78431a73af3c28f
Auynonymous <auynaaubin@gmail.com>
2026-09-05 17:06:17 -0500

    manual spotless is a deadend job

--- src/main/java/gregtech/common/tileentities/machines/multi/nanochip/util/CircuitComponent.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/nanochip/util/CircuitComponent.java
@@ -442,13 +442,13 @@ public enum CircuitComponent {
     SisterSpool(1816,"gt.circuitcomponent.specialty.processed.sisterspool",null,true),
     BundledPeace(1817,"gt.circuitcomponent.specialty.processed.bundledpeace",null,true),
     //Planck Chain
-     CollapsingResidue(1818, "gt.circuitcomponent.specialty.processed.collapsingresidue",null,true),
-     ProcessedOrganizedQuantumCircuit(1819,"gt.circuitcomponent.specialty.processed.organizedquantum",null,true),
-     ProcessedEtchedQuantumCircuits(1820,"gt.circuitcomponent.specialty.processed.etchedquantums",null,true),
-     UnrefinedPlanckCPU(1821,"gt.circuitcomponent.specialty.processed.unrefinedplanckcpu",null,true),
-     PlanckCPU(1822,"gt.circuitcomponent.specialty.processed.sisterspool",()->ItemList.Planck_CPU.get(1)),
-     BundledHarmony(1823,"gt.circuitcomponent.specialty.processed.bundledharmony",null,true),
-     CalabiYauManifold(1824,"gt.circuitcomponent.specialty.processed.calabiyaumanifold", ()->ItemList.CalabiYauManifold.get(1)),
+    CollapsingResidue(1818, "gt.circuitcomponent.specialty.processed.collapsingresidue",null,true),
+    ProcessedOrganizedQuantumCircuit(1819,"gt.circuitcomponent.specialty.processed.organizedquantum",null,true),
+    ProcessedEtchedQuantumCircuits(1820,"gt.circuitcomponent.specialty.processed.etchedquantums",null,true),
+    UnrefinedPlanckCPU(1821,"gt.circuitcomponent.specialty.processed.unrefinedplanckcpu",null,true),
+    PlanckCPU(1822,"gt.circuitcomponent.specialty.processed.sisterspool",()->ItemList.Planck_CPU.get(1)),
+    BundledHarmony(1823,"gt.circuitcomponent.specialty.processed.bundledharmony",null,true),
+    CalabiYauManifold(1824,"gt.circuitcomponent.specialty.processed.calabiyaumanifold", ()->ItemList.CalabiYauManifold.get(1)),
 
     // Bolts and other small components
     // IDs: 1900-1999 CC, 2000-2099 PC
