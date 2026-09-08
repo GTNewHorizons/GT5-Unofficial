844daa754c932b31ec6e102e3b30e318123f82d7
Ranzu <66495944+Ranzuu@users.noreply.github.com>
2026-09-08 10:28:05 +0200

    Mark dirty after the loop(s)

--- src/main/java/gregtech/common/tileentities/machines/basic/MTEIceCreamMachine.java
+++ src/main/java/gregtech/common/tileentities/machines/basic/MTEIceCreamMachine.java
@@ -214,6 +214,7 @@ public class MTEIceCreamMachine extends MTEBasicMachine implements IMTERenderer,
         if (mRepairItemIndex < 0) {
             heldStack.stackSize -= 1;
             mBrokenToday = false;
+            getBaseMetaTileEntity().markInventoryBeenModified();
             return;
         }
 
@@ -223,6 +224,7 @@ public class MTEIceCreamMachine extends MTEBasicMachine implements IMTERenderer,
         heldStack.stackSize -= needed.stackSize;
         mBrokenToday = false;
         mRepairItemIndex = -1;
+        getBaseMetaTileEntity().markInventoryBeenModified();
     }
 
     @Override
