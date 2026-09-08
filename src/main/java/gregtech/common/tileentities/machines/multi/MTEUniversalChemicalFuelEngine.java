4239a0d43479bcdb5f7f4a571e0a79693cf88393
MLGfruitshoot <benhand2006@gmail.com>
2026-09-09 00:17:43 +0100

    make ucfe tooltip easier to understand

--- src/main/java/gregtech/common/tileentities/machines/multi/MTEUniversalChemicalFuelEngine.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/MTEUniversalChemicalFuelEngine.java
@@ -67,11 +67,11 @@ public class MTEUniversalChemicalFuelEngine extends TTMultiblockBase
     private static final int OFFSET_Z = 0;
     private static final String STRUCTURE_PIECE_MAIN = "main";
 
-    protected final double DIESEL_EFFICIENCY_COEFFICIENT = 0.04D;
-    protected final double GAS_EFFICIENCY_COEFFICIENT = 0.04D;
-    protected final double ROCKET_EFFICIENCY_COEFFICIENT = 0.005D;
-    protected final double EFFICIENCY_CEILING = 1.5D;
-    protected final int HEATING_TIMER = TickTime.SECOND * 10;
+    private static final double DIESEL_EFFICIENCY_COEFFICIENT = 0.04D;
+    private static final double GAS_EFFICIENCY_COEFFICIENT = 0.04D;
+    private static final double ROCKET_EFFICIENCY_COEFFICIENT = 0.005D;
+    private static final double EFFICIENCY_CEILING = 1.5D;
+    private static final int HEATING_TIMER = TickTime.SECOND * 10;
 
     private long tEff;
     private int heatingTicks;
@@ -158,28 +158,37 @@ public class MTEUniversalChemicalFuelEngine extends TTMultiblockBase
         tt.addMachineType("Chemical Engine, UCFE")
             .addInfo("BURNING BURNING BURNING")
             .addInfo("Use combustible liquid to generate power")
-            .addInfo("You need to supply Combustion Promoter to keep it running")
-            .addInfo("It will consume all the fuel and combustion promoter in the hatch every second")
-            .addInfo("Energy output to the dynamo will be distributed over the next second")
+            .addInfo("You need to supply Combustion Promoter to generate power")
+            .addInfo("Energy is passed to the dynamo over the next second")
             .addInfo("If the Dynamo Hatch's buffer fills up, the machine will stop")
             .addInfo(
                 "If the amount of energy to be produced is higher "
                     + "than the dynamo hatch can handle then all produced energy will void")
-            .addInfo("When turned on, there is a 10-second period where the machine will not stop")
-            .addInfo("Even if it doesn't stop, all the fuel in the hatch will be consumed")
+            .addSeparator()
             .addInfo("The efficiency is determined by the proportion of Combustion Promoter to fuel")
             .addInfo("The higher the amount of promoter, the higher the efficiency")
             .addInfo(
                 "Follows an exponential curve exp(-C/(p/x))*1.5, "
                     + "where x is the amount of fuel in liters, p is the amount of promoter in liters")
-            .addInfo("and C depends on the fuel type. Diesel: C=0.04; Gas: C=0.04; Rocket fuel: C=0.005")
-            .addInfo("It creates sqrt(Current Output Power) pollution every second")
+            .addInfo(
+                "C depends on the fuel type. Diesel: C=" + DIESEL_EFFICIENCY_COEFFICIENT
+                    + "; Gas: C="
+                    + GAS_EFFICIENCY_COEFFICIENT
+                    + "; Rocket fuel: C="
+                    + ROCKET_EFFICIENCY_COEFFICIENT)
+            .addSeparator()
             .addInfo(
                 "If you forget to supply Combustion Promoter, this engine will swallow all the fuel "
                     + EnumChatFormatting.YELLOW
                     + "without outputting energy")
-            .addInfo("The efficiency is up to 150%")
+            .addInfo(
+                "The efficiency is up to " + EnumChatFormatting.GOLD
+                    + formatNumber(EFFICIENCY_CEILING * 100)
+                    + "%"
+                    + EnumChatFormatting.GRAY)
+            .addInfo("Creates sqrt(Current Output Power) pollution per second")
             .addSupportAny()
+            .addPollutionAmount(getPollutionPerSecond(null))
             .beginStructureBlock(7, 7, 13, true)
             .addController("Front center, 3rd layer")
             .addCasing("100-115", "Stable Titanium Machine Casing", false)
