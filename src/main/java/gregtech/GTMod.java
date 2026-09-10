24092547d488862edcb635b0db12054d29b2a749
danyadev <danyadev@mail.ru>
2026-09-10 03:30:47 +0300

    make overridden stacks canonical at the moment of their registration
    it works only when there were no prior stacks for the same oredict tho

--- src/main/java/gregtech/GTMod.java
+++ src/main/java/gregtech/GTMod.java
@@ -635,7 +635,7 @@ public class GTMod {
             GT_FML_LOGGER
                 .info("Executed 2nd pass of delayed Crafting Recipes ({}). Have another Cake.", stopwatch.stop());
         }
-        OreDictUnificationOverrides.apply();
+        OreDictUnificationOverrides.finalizeUnification();
         GregTechAPI.sGTCompleteLoad = null;
         GregTechAPI.sFullLoadFinished = true;
     }
