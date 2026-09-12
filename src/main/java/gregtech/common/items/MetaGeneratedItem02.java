648dc0e14317f917ef6aec0c5a8ad5d8a385e95b
Ranzu <66495944+Ranzuu@users.noreply.github.com>
2026-09-12 12:43:57 +0200

    exploison radius change

--- src/main/java/gregtech/common/items/MetaGeneratedItem02.java
+++ src/main/java/gregtech/common/items/MetaGeneratedItem02.java
@@ -2629,7 +2629,7 @@ public class MetaGeneratedItem02 extends MetaGeneratedItemX32 {
                 Food_IceCream_Explosive.ID,
                 "gt.item.food.icecream_explosive.name",
                 "gt.item.food.icecream_explosive.tooltip",
-                new GTFoodStat(2, 0.1F, EnumAction.eat, null, false, true, false).setExplosive(150f),
+                new GTFoodStat(2, 0.1F, EnumAction.eat, null, false, true, false).setExplosive(20f),
                 new TCAspects.TC_AspectStack(TCAspects.GELUM, 1L)));
         ItemList.Ice_Cream_NotFound404.set(
             addItemWithLocalizationKeys(
