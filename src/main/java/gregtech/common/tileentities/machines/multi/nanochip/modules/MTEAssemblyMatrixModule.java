045f3b15867ca7d3f2152349baf6388538747049
chrombread <chrombread8@gmail.com>
2026-09-11 20:52:26 -0700

    Revert "make it use structure channel yep yep yep"
    This reverts commit c6e861976d1829a35ab2ae4d741eedeb8f49892a.

--- src/main/java/gregtech/common/tileentities/machines/multi/nanochip/modules/MTEAssemblyMatrixModule.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/nanochip/modules/MTEAssemblyMatrixModule.java
@@ -85,7 +85,6 @@ public class MTEAssemblyMatrixModule extends MTENanochipAssemblyModuleBase<MTEAs
         // CoAL casing
         .addElement(
             'B',
-            GTStructureChannels.COMPONENT_ASSEMBLYLINE_CASING.use(
             ofBlocksTiered(
                 (block, meta) -> block == Loaders.componentAssemblylineCasing ? meta + 1 : null,
                 IntStream.range(0, 14)
@@ -93,7 +92,7 @@ public class MTEAssemblyMatrixModule extends MTENanochipAssemblyModuleBase<MTEAs
                     .collect(Collectors.toList()),
                 -1,
                 MTEAssemblyMatrixModule::setCasingTier,
-                MTEAssemblyMatrixModule::getCasingTier)))
+                MTEAssemblyMatrixModule::getCasingTier))
         // Nanochip Mesh Interface Casing
         .addElement('C', Casings.NanochipMeshInterfaceCasing.asElement())
         // Nanochip Reinforcement Casing
