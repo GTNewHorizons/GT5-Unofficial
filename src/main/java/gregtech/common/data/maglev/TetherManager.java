206c932fc0db198cdddb846cf89ab33134663ec0
DreamYao520 <123301307+DreamYao520@users.noreply.github.com>
2026-09-15 16:56:25 +0800

    Increase MagLev Pylon ranges and remove vertical limits

--- src/main/java/gregtech/common/data/maglev/TetherManager.java
+++ src/main/java/gregtech/common/data/maglev/TetherManager.java
@@ -7,9 +7,6 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 
-import com.gtnewhorizon.gtnhlib.datastructs.space.ArrayProximityMap4D;
-import com.gtnewhorizon.gtnhlib.datastructs.space.VolumeShape;
-
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerChangedDimensionEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedOutEvent;
@@ -18,23 +15,38 @@ import gregtech.common.tileentities.machines.basic.MTEMagLevPylon;
 
 public class TetherManager {
 
-    /** Map storage of active pylons (dim, x, y, z, range, tether) **/
-    private final ArrayProximityMap4D<MTEMagLevPylon> ACTIVE_PYLONS = new ArrayProximityMap4D<>(VolumeShape.CUBE);
+    private final Map<MTEMagLevPylon, Integer> ACTIVE_PYLONS = new HashMap<>();
 
     /** Used by pylons to determine if a player is connected */
     private final Map<EntityPlayerMP, MTEMagLevPylon> PLAYER_TETHERS = new HashMap<>();
 
-    public void registerPylon(IGregTechTileEntity mte, MTEMagLevPylon tether, int range) {
-        ACTIVE_PYLONS
-            .put(tether, mte.getWorld().provider.dimensionId, mte.getXCoord(), mte.getYCoord(), mte.getZCoord(), range);
+    public void registerPylon(MTEMagLevPylon tether, int range) {
+        ACTIVE_PYLONS.put(tether, range);
     }
 
-    public void unregisterPylon(IGregTechTileEntity mte) {
-        ACTIVE_PYLONS.remove(mte.getWorld().provider.dimensionId, mte.getXCoord(), mte.getYCoord(), mte.getZCoord());
+    public void unregisterPylon(MTEMagLevPylon tether) {
+        ACTIVE_PYLONS.remove(tether);
     }
 
     public MTEMagLevPylon getClosestActivePylon(EntityPlayer player) {
-        return ACTIVE_PYLONS.getClosest(player.dimension, player.posX, player.posY, player.posZ);
+        MTEMagLevPylon closest = null;
+        double closestDistance = Double.MAX_VALUE;
+        for (Map.Entry<MTEMagLevPylon, Integer> entry : ACTIVE_PYLONS.entrySet()) {
+            IGregTechTileEntity mte = entry.getKey()
+                .getBaseMetaTileEntity();
+            if (mte.getWorld().provider.dimensionId != player.dimension) continue;
+            double dx = player.posX - mte.getXCoord() - 0.5;
+            double dz = player.posZ - mte.getZCoord() - 0.5;
+            double range = entry.getValue() + 0.5;
+            if (Math.abs(dx) >= range || Math.abs(dz) >= range) continue;
+            double dy = player.posY - mte.getYCoord() - 0.5;
+            double distance = dx * dx + dy * dy + dz * dz;
+            if (distance < closestDistance) {
+                closest = entry.getKey();
+                closestDistance = distance;
+            }
+        }
+        return closest;
     }
 
     public void connectPlayer(EntityPlayer player, MTEMagLevPylon tether) {
